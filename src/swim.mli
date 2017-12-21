1f7521de008aeff2426f8350455cf40f38333c31
Brandon Kase <brandon.kase@gmail.com>
2017-12-20 23:41:09 -0800

    Messager functor for SWIM typechecks

--- src/swim.mli
+++ src/swim.mli
@@ -1,8 +1,14 @@
 open Async_kernel
 
 module type S = sig
+  type t
+
   val connect
-    : initial_peers:Peer.t list -> Peer.Event.t Pipe.Reader.t
+    : initial_peers:Peer.t list -> t
+
+  val peers : t -> Peer.t list
+
+  val changes : t -> Peer.Event.t Pipe.Reader.t
 end
 
 module Udp : S
