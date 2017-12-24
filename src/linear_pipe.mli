749850201f0f5fdc56692e21ff2664bfa8d0b4a7
Evan Shapiro <evan.a.shapiro@gmail.com>
2017-12-24 02:08:43 -0500

    add linear pipe

---
+++ src/linear_pipe.mli
@@ -0,0 +1,37 @@
+open Core_kernel
+open Async_kernel
+
+module Writer : sig
+  type 'a t = 'a Pipe.Writer.t
+end
+
+module Reader : sig
+  type 'a t 
+end
+
+val create : unit -> 'a Reader.t * 'a Writer.t
+
+val iter
+  :  ?consumer          : Pipe.Consumer.t
+  -> ?continue_on_error : bool  (** default is [false] *)
+  -> 'a Reader.t
+  -> f                  : ('a -> unit Deferred.t)
+  -> unit Deferred.t
+
+val map : 'a Reader.t -> f:('a -> 'b) -> 'b Async_kernel.Pipe.Reader.t
+
+val transfer : 'a Reader.t -> 'b Writer.t -> f:('a -> 'b ) -> unit Deferred.t
+
+val close_read : 'a Reader.t -> unit
+
+val closed : 'a Reader.t -> unit Deferred.t
+
+val fork : 'a Reader.t -> int -> 'a Reader.t List.t
+
+val fork2 : 'a Reader.t -> 'a Reader.t * 'a Reader.t
+
+val fork3 : 'a Reader.t -> 'a Reader.t * 'a Reader.t * 'a Reader.t
+
+val partition_map2 : 'a Reader.t -> f:('a -> [ `Fst of 'b  | `Snd of 'c ]) -> 'b Reader.t * 'c Reader.t
+
+val partition_map3 : 'a Reader.t -> f:('a -> [ `Fst of 'b  | `Snd of 'c | `Trd of 'd ]) -> 'b Reader.t * 'c Reader.t * 'd Reader.t
