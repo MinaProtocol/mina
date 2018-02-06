769d493ef7b0d1dbcbb5533a7f85781dba292e2d
Brandon Kase <brandon.kase@gmail.com>
2018-02-05 16:21:25 -0800

    Dockerfiles broken into pieces
    Depends on o1-labs/camlsnark#1
    
    container/base -> ocaml-base:latest
    ../camlsnark -> ocaml-camlsnark:latest
    . -> nanotest:latest
    
    Nanobit builds inside nanotest by `jbuilder b`-ing manually.
    
    ```shell
    docker run -v /Users/bkase/coins/nanobit:/nanobit -ti nanotest:latest bash
    ```
    
    If vim were here, you could develop in the docker container
    
    Makefiles are there

--- rebuild-googlecloud.sh
+++ rebuild-googlecloud.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 set -e
 
-img=testbridge:latest
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+$SCRIPTPATH/rebuild-docker.sh $1
+
+img=$1:latest
 project=$(gcloud config get-value project)
 
-cd container
-jbuilder build
 docker build -t $img .
 docker tag $img gcr.io/$project/$img
 gcloud docker -- push gcr.io/$project/$img
+
