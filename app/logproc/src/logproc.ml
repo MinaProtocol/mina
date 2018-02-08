8fd634ddebc1779b818417de7dbf702d86b16924
Izaak Meckler <ihmeckler@gmail.com>
2018-02-07 23:41:26 -0800

    Logging (#64)
    This PR implements a logging library, along with a program logproc for pretty printing and filtering the logs generated by that library.

---
+++ app/logproc/src/logproc.ml
@@ -0,0 +1,100 @@
+open Core
+open Async
+
+module Color = struct
+  type t =
+    | Black
+    | Red
+    | Green
+    | Yellow
+    | Blue
+    | Magenta
+    | Cyan
+    | White
+
+  let to_int = function
+    | Black -> 0 | Red     -> 1 | Green -> 2 | Yellow -> 3
+    | Blue  -> 4 | Magenta -> 5 | Cyan  -> 6 | White  -> 7
+
+  let color color text =
+    sprintf "\027[38;5;%dm%s\027[0m" (to_int color) text
+end
+
+let color_of_level : Logger.Level.t -> Color.t = function
+  | Trace -> Blue
+  | Debug -> Green
+  | Info -> Cyan
+  | Warn -> Yellow
+  | Error -> Red
+  | Fatal -> Magenta
+
+let colored_level level =
+  Color.color (color_of_level level) (sprintf !"%{sexp:Logger.Level.t}" level)
+
+let pretty_print_message
+      { Logger.Message.attributes
+      ; path
+      ; level
+      ; pid
+      ; host
+      ; time
+      ; location
+      ; message
+      }
+  =
+  printf !"[%{Time}] %s (%{Pid} on %s): %s\n"
+    time
+    (colored_level level)
+    pid
+    host
+    (Color.color Green message);
+  if not (Map.is_empty attributes)
+  then begin
+    printf "    %s\n"
+      (Sexp.to_string_hum ~indent:4
+         ([%sexp_of: Sexp.t String.Map.t] attributes));
+  end
+;;
+
+
+let pos_string lexbuf =
+  let open Lexing in
+  let pos = lexbuf.lex_curr_p in
+  sprintf "%s:%d:%d" pos.pos_fname
+    pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
+
+let parse_filter =
+  let parse_with_error lexbuf =
+    match Filter_parser.prog Filter_lexer.read lexbuf with
+    | None -> Or_error.error_string "none"
+    | Some filter -> Ok filter
+    | exception Filter_lexer.SyntaxError msg ->
+      Or_error.error_string
+        (sprintf "%s: %s\n" (pos_string lexbuf) msg)
+    | exception Filter_parser.Error ->
+      Or_error.error_string (sprintf "%s: syntax error\n" (pos_string lexbuf))
+  in
+  fun s ->
+    parse_with_error (Lexing.from_string s)
+;;
+
+let parse_filter_exn s = Or_error.ok_exn (parse_filter s)
+
+let filter_arg = Command.Arg_type.create parse_filter_exn
+
+let main filter () =
+  Pipe.iter_without_pushback (Reader.lines (Lazy.force Reader.stdin)) ~f:(fun l ->
+    let m = Sexp.of_string_conv_exn l Logger.Message.t_of_sexp in
+    if Filter.eval filter m
+    then pretty_print_message m)
+
+let () =
+  Command.async ~summary:"Pretty print logs" begin
+    let open Command.Let_syntax in
+    let%map_open filter =
+      flag "c" ~doc:"filter" (optional_with_default Filter.True filter_arg)
+    in
+    main filter
+  end
+  |> Command.run
+
---
+++ lib/logger/logger.ml
@@ -0,0 +1,97 @@
+open Core
+
+module Level = struct
+  type t =
+    | Trace
+    | Debug
+    | Info
+    | Warn
+    | Error
+    | Fatal
+  [@@deriving sexp, bin_io, compare]
+end
+
+module Attribute = struct
+  type t = string * Sexp.t
+
+  let (^=) k v = (k, v)
+end
+
+module Message = struct
+  type t =
+    { attributes : Sexp.t String.Map.t
+    ; path       : string list
+    ; level      : Level.t
+    ; pid        : Pid.t
+    ; host       : string
+    ; time       : Time.t
+    ; location   : string option
+    ; message    : string
+    }
+  [@@deriving sexp, bin_io]
+end
+
+type t =
+  { attributes : Sexp.t String.Map.t
+  ; pid        : Pid.t
+  ; host       : string
+  ; path       : string list
+  }
+[@@deriving sexp, bin_io]
+
+let create () =
+  { attributes = String.Map.empty
+  ; pid        = Unix.getpid ()
+  ; host       = Unix.gethostname ()
+  ; path       = []
+  }
+
+let log ~level ?loc ?(attrs=[]) t fmt =
+  ksprintf
+    (fun message ->
+       let m : Message.t =
+         { attributes =
+             List.fold attrs ~init:t.attributes ~f:(fun acc (key, data) ->
+               Map.set acc ~key ~data)
+         ; path = t.path
+         ; pid = t.pid
+         ; host = t.host
+         ; time = Time.now ()
+         ; level
+         ; message
+         ; location = loc
+         }
+       in
+       printf !"%s\n" (Sexp.to_string_mach (Message.sexp_of_t m)))
+  fmt
+;;
+
+let extend t attrs =
+  { t with
+    attributes =
+      List.fold attrs ~init:t.attributes ~f:(fun acc (key, data) ->
+        Map.set acc ~key ~data)
+  }
+
+let child t s =
+  { t with
+    path = s :: t.path;
+    attributes =
+      t.attributes
+      |> Map.set ~key:"module" ~data:([%sexp_of: string] s)
+      |> Map.set ~key:(sprintf "module-%s" s)  ~data:([%sexp_of: bool] true)
+  }
+
+type 'a logger =
+  ?loc:string
+  -> ?attrs:Attribute.t list
+  -> t
+  -> ('a, unit, string, unit) format4
+  -> 'a
+
+let trace ?loc ?attrs t fmt = log ~level:Trace ?loc ?attrs t fmt
+let debug ?loc ?attrs t fmt = log ~level:Debug ?loc ?attrs t fmt
+let info ?loc ?attrs t fmt  = log ~level:Info ?loc ?attrs t fmt
+let warn ?loc ?attrs t fmt  = log ~level:Warn ?loc ?attrs t fmt
+let error ?loc ?attrs t fmt = log ~level:Error ?loc ?attrs t fmt
+let fatal ?loc ?attrs t fmt = log ~level:Fatal ?loc ?attrs t fmt
