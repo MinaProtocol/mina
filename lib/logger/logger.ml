495a6c524398f6a7586272c25f14a1cc59747708
Izaak Meckler <ihmeckler@gmail.com>
2018-02-07 16:58:52 -0800

    flesh out logger

--- lib/logger/logger.ml
+++ lib/logger/logger.ml
@@ -1,19 +1,71 @@
 open Core
 
-type t
-
 module Level = struct
   type t =
-    | Warn
-    | Log
+    | Trace
     | Debug
+    | Info
+    | Warn
     | Error
-  [@@deriving sexp, bin_io]
+    | Fatal
+  [@@deriving sexp, bin_io, compare]
 end
 
 module Attribute = struct
   type t = string * Sexp.t
 
-  let (:=) k v = (k, v)
+  let (^=) k v = (k, v)
 end
 
+module Message = struct
+  type t =
+    { attributes : Sexp.t String.Map.t
+    ; level      : Level.t
+    ; pid        : Pid.t
+    ; host       : string
+    ; time       : Time.t
+    ; message    : string
+    }
+  [@@deriving sexp, bin_io]
+end
+
+type t =
+  { attributes : Sexp.t String.Map.t
+  ; level      : Level.t
+  ; pid        : Pid.t
+  ; host       : string
+  }
+[@@deriving sexp, bin_io]
+
+let create ?(level=Level.Info) () =
+  { attributes = String.Map.empty
+  ; pid        = Unix.getpid ()
+  ; host       = Unix.gethostname ()
+  ; level
+  }
+
+let log ?level ?(attrs=[]) t fmt =
+  ksprintf
+    (fun message ->
+       let m : Message.t =
+         { attributes =
+             List.fold attrs ~init:t.attributes ~f:(fun acc (key, data) ->
+               String.Map.set acc ~key ~data)
+         ; level = Option.value level ~default:t.level
+         ; pid = t.pid
+         ; host = t.host
+         ; time = Time.now ()
+         ; message
+         }
+       in
+       printf !"%{sexp:Message.t}\n" m)
+  fmt
+;;
+
+let () =
+  let open Attribute in
+  let t = create () in
+  log t "%d" 23;
+  log t ~attrs:[ "module" ^= [%sexp_of: string] "logger.ml"; "hello" ^=  [%sexp_of: int] 14]
+    "%s = %d\n" "hello" 234;
+;;
--- lib/logger/logger.mli
+++ lib/logger/logger.mli
@@ -14,25 +14,30 @@ end
 module Attribute : sig
   type t
 
-  val (:=) : string -> Sexp.t -> t
+  val (^=) : string -> Sexp.t -> t
 end
 
 module Message : sig
   type t =
     { attributes : Sexp.t String.Map.t
     ; level      : Level.t
-    ; time       : Time.t
     ; pid        : Pid.t
     ; host       : string
+    ; time       : Time.t
     ; message    : string
     }
+  [@@deriving sexp, bin_io]
 end
 
-val create : unit -> t
+val create
+  : ?level:Level.t
+  -> unit
+  -> t
 
 val log
   : ?level:Level.t
   -> ?attrs:Attribute.t list
+  -> t
   -> ('b, unit, string, unit) format4
   -> 'b
 
