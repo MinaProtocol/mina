53fc605fb7f9de358276220a10cf63060aac14be
Izaak Meckler <ihmeckler@gmail.com>
2018-02-12 12:52:38 -0800

    turn back on insecure mode

--- lib/nanobit_base/snark_params.ml
+++ lib/nanobit_base/snark_params.ml
@@ -11,12 +11,12 @@ type functionalities =
 
 (* These should all be false. *)
 let insecure_functionalities =
-  { strength_calculation = false
+  { strength_calculation = true
   ; verify_blockchain    = true
   ; check_target         = true
-  ; compute_base_hash    = false
-  ; compute_base_proof   = false
-  ; extend_blockchain    = false
+  ; compute_base_hash    = true
+  ; compute_base_proof   = true
+  ; extend_blockchain    = true
   }
 
 module Tick_curve = Camlsnark.Backends.Mnt4
