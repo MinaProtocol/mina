5d35b2d3e3507dbf52fe8fdaec54049ed75a6866
Izaak Meckler <ihmeckler@gmail.com>
2018-02-20 13:59:17 -0800

    group tests together to do seeded randomness

--- lib/nanobit_base/snark_util.ml
+++ lib/nanobit_base/snark_util.ml
@@ -34,34 +34,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
       { less; less_or_equal }
     end
 
-  let random_n_bit_field_elt n =
-    Field.project (List.init n ~f:(fun _ -> Random.bool ()))
-
-  let%test_unit "compare" =
-    let bit_length = Field.size_in_bits - 2 in
-    let random () = random_n_bit_field_elt bit_length in
-    let test () =
-      let x = random () in
-      let y = random () in
-      let ((), (less, less_or_equal), passed) =
-        run_and_check
-          (let%map { less; less_or_equal } =
-             compare ~bit_length (Cvar.constant x) (Cvar.constant y)
-           in
-           As_prover.(
-             map2 (read Boolean.spec less) (read Boolean.spec less_or_equal)
-               ~f:Tuple2.create))
-          ()
-      in
-      assert passed;
-      let r = Bigint.(compare (of_field x) (of_field y)) in
-      assert (less = (r < 0));
-      assert (less_or_equal = (r <= 0))
-    in
-    for i = 0 to 100 do
-      test ()
-    done
-
   module Assert = struct
     let lt ~bit_length x y =
       let%bind { less; _ } = compare ~bit_length x y in
@@ -99,20 +71,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
       (Cvar.constant Field.zero)
   ;;
 
-  let%test_unit "boolean_assert_lte" =
-    assert (
-      check
-        (Checked.all_ignore
-        [ boolean_assert_lte Boolean.false_ Boolean.false_ 
-        ; boolean_assert_lte Boolean.false_ Boolean.true_
-        ; boolean_assert_lte Boolean.true_ Boolean.true_
-        ])
-        ());
-    assert (not (
-      check
-        (boolean_assert_lte Boolean.true_ Boolean.false_) ()))
-  ;;
-
   let assert_decreasing : Boolean.var list -> (unit, _) Checked.t =
     let rec go prev (bs0 : Boolean.var list) =
       match bs0 with
@@ -124,15 +82,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
     function
     | [] -> return ()
     | b :: bs -> go b bs
-  ;;
-
-  let%test_unit "assert_decreasing" =
-    let decreasing bs = 
-      check (assert_decreasing (List.map ~f:Boolean.var_of_value bs)) ()
-    in
-    assert (decreasing [true; true; true; false]);
-    assert (decreasing [true; true; false; false]);
-    assert (not (decreasing [true; true; false; true]));
   ;;
 
   let nth_bit x ~n = (x lsr n) land 1 = 1
@@ -172,29 +121,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
     and () = assert_decreasing bs in
     bs
 
-  let%test_unit "n_ones" =
-    let total_length = 6 in
-    let test n =
-      let t = n_ones ~total_length (Cvar.constant (Field.of_int n)) in
-      let handle_with (resp : bool list) = 
-        handle t (fun (With {request; respond}) ->
-          match request with
-          | N_ones -> respond resp
-          | _ -> unhandled)
-      in
-      let correct = Int.pow 2 n - 1 in
-      let to_bits k = List.init total_length ~f:(fun i -> (k lsr i) land 1 = 1) in
-      for i = 0 to Int.pow 2 total_length - 1 do
-        if i = correct
-        then assert (check (handle_with (to_bits i)) ())
-        else assert (not (check (handle_with (to_bits i)) ()))
-      done
-    in
-    for n = 0 to total_length do
-      test n
-    done
-  ;;
-
   let assert_num_bits_upper_bound bs u =
     let total_length = List.length bs in
     assert (total_length < Field.size_in_bits);
@@ -212,12 +138,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
     go 0
   ;;
 
-  let%test_unit "num_bits_int" =
-    assert (num_bits_int 1 = 1);
-    assert (num_bits_int 5 = 3);
-    assert (num_bits_int 17 = 5);
-  ;;
-
   let size_in_bits_size_in_bits = num_bits_int Field.size_in_bits
 
   type _ Camlsnark.Request.t +=
@@ -233,16 +153,6 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
       | None -> 0
     in
     num_bits
-
-  let%test_unit "num_bits_upper_bound_unchecked" =
-    let f k bs =
-      assert (num_bits_upper_bound_unchecked (Field.project bs) = k)
-    in
-    f 3 [true; true; true; false; false];
-    f 4 [true; true; true; true; false];
-    f 3 [true; false; true; false; false];
-    f 5 [true; false; true; false; true]
-  ;;
 
   (* Someday: this could definitely be made more efficient *)
   let num_bits_upper_bound_unpacked : Boolean.var list -> (Cvar.t, _) Checked.t =
@@ -263,29 +173,123 @@ module Make (Impl : Camlsnark.Snark_intf.S) = struct
     >>= num_bits_upper_bound_unpacked
   ;;
 
-  let%test_unit "num_bits_upper_bound" =
-    let max_length = Field.size_in_bits - 1 in
-    let test x =
-      let handle_with resp =
-        handle
-          (num_bits_upper_bound ~max_length (Cvar.constant x))
-          (fun (With {request; respond}) ->
-             match request with
-             | Num_bits_upper_bound -> respond (Field.of_int resp)
-             | _ -> unhandled)
+  let%test_module "Snark_util" = (module struct
+    let () = Random.init 123456789
+
+    let random_n_bit_field_elt n =
+      Field.project (List.init n ~f:(fun _ -> Random.bool ()))
+
+    let%test_unit "compare" =
+      let bit_length = Field.size_in_bits - 2 in
+      let random () = random_n_bit_field_elt bit_length in
+      let test () =
+        let x = random () in
+        let y = random () in
+        let ((), (less, less_or_equal), passed) =
+          run_and_check
+            (let%map { less; less_or_equal } =
+              compare ~bit_length (Cvar.constant x) (Cvar.constant y)
+            in
+            As_prover.(
+              map2 (read Boolean.spec less) (read Boolean.spec less_or_equal)
+                ~f:Tuple2.create))
+            ()
+        in
+        assert passed;
+        let r = Bigint.(compare (of_field x) (of_field y)) in
+        assert (less = (r < 0));
+        assert (less_or_equal = (r <= 0))
       in
-      let true_answer = num_bits_upper_bound_unchecked x in
-      for i = 0 to true_answer - 1 do
-        if check (handle_with i) ()
-        then begin
-          let n = Bigint.of_field x in
-          failwithf !"Shouldn't have passed: x=%s, i=%d"
-            (String.init max_length ~f:(fun j -> if Bigint.test_bit n j then '1' else '0'))
-            i ();
-        end;
-      done;
-      assert (check (handle_with true_answer) ())
-    in
-    test (random_n_bit_field_elt max_length)
-  ;;
+      for i = 0 to 100 do
+        test ()
+      done
+
+    let%test_unit "boolean_assert_lte" =
+      assert (
+        check
+          (Checked.all_ignore
+          [ boolean_assert_lte Boolean.false_ Boolean.false_ 
+          ; boolean_assert_lte Boolean.false_ Boolean.true_
+          ; boolean_assert_lte Boolean.true_ Boolean.true_
+          ])
+          ());
+      assert (not (
+        check
+          (boolean_assert_lte Boolean.true_ Boolean.false_) ()))
+    ;;
+
+    let%test_unit "assert_decreasing" =
+      let decreasing bs = 
+        check (assert_decreasing (List.map ~f:Boolean.var_of_value bs)) ()
+      in
+      assert (decreasing [true; true; true; false]);
+      assert (decreasing [true; true; false; false]);
+      assert (not (decreasing [true; true; false; true]));
+    ;;
+
+    let%test_unit "n_ones" =
+      let total_length = 6 in
+      let test n =
+        let t = n_ones ~total_length (Cvar.constant (Field.of_int n)) in
+        let handle_with (resp : bool list) = 
+          handle t (fun (With {request; respond}) ->
+            match request with
+            | N_ones -> respond resp
+            | _ -> unhandled)
+        in
+        let correct = Int.pow 2 n - 1 in
+        let to_bits k = List.init total_length ~f:(fun i -> (k lsr i) land 1 = 1) in
+        for i = 0 to Int.pow 2 total_length - 1 do
+          if i = correct
+          then assert (check (handle_with (to_bits i)) ())
+          else assert (not (check (handle_with (to_bits i)) ()))
+        done
+      in
+      for n = 0 to total_length do
+        test n
+      done
+    ;;
+
+    let%test_unit "num_bits_int" =
+      assert (num_bits_int 1 = 1);
+      assert (num_bits_int 5 = 3);
+      assert (num_bits_int 17 = 5);
+    ;;
+
+    let%test_unit "num_bits_upper_bound_unchecked" =
+      let f k bs =
+        assert (num_bits_upper_bound_unchecked (Field.project bs) = k)
+      in
+      f 3 [true; true; true; false; false];
+      f 4 [true; true; true; true; false];
+      f 3 [true; false; true; false; false];
+      f 5 [true; false; true; false; true]
+    ;;
+
+    let%test_unit "num_bits_upper_bound" =
+      let max_length = Field.size_in_bits - 1 in
+      let test x =
+        let handle_with resp =
+          handle
+            (num_bits_upper_bound ~max_length (Cvar.constant x))
+            (fun (With {request; respond}) ->
+              match request with
+              | Num_bits_upper_bound -> respond (Field.of_int resp)
+              | _ -> unhandled)
+        in
+        let true_answer = num_bits_upper_bound_unchecked x in
+        for i = 0 to true_answer - 1 do
+          if check (handle_with i) ()
+          then begin
+            let n = Bigint.of_field x in
+            failwithf !"Shouldn't have passed: x=%s, i=%d"
+              (String.init max_length ~f:(fun j -> if Bigint.test_bit n j then '1' else '0'))
+              i ();
+          end;
+        done;
+        assert (check (handle_with true_answer) ())
+      in
+      test (random_n_bit_field_elt max_length)
+    ;;
+  end)
 end
