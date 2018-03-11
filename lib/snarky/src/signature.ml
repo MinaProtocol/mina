0551018d8026ea81997fbd0a08b772eca20658fc
Izaak Meckler <ihmeckler@gmail.com>
2018-03-10 22:12:25 -0800

    update snarky to use bigint in signature

--- lib/snarky/src/signature.ml
+++ lib/snarky/src/signature.ml
@@ -1,35 +1,23 @@
 open Core
 
-module type Scalar_field_intf = sig
-  module Field : Field_intf.S
-
-  module Bigint : sig
-    type t
-
-    val of_field : Field.t -> t
-    val to_field : t -> Field.t
-
-    val test_bit : t -> int -> bool
-  end
-end
-
 module Schnorr
     (Impl : Snark_intf.S)
-    (Scalar : Scalar_field_intf)
     (Curve : Curves.Edwards.S
      with type ('a, 'b) checked := ('a, 'b) Impl.Checked.t
-      and type Scalar.value = Scalar.Bigint.t
+      and type Scalar.value = Bignum.Bigint.t
       and type ('a, 'b) typ := ('a, 'b) Impl.Typ.t
       and type boolean_var := Impl.Boolean.var
       and type var = Impl.Cvar.t * Impl.Cvar.t
       and type field := Impl.Field.t)
     (Hash : sig
-       val hash : bool list -> Scalar.Field.t
+       val hash : bool list -> Bignum.Bigint.t
        val hash_checked : Impl.Boolean.var list -> (Curve.Scalar.var, _) Impl.Checked.t
       end)
 =
 struct
   open Impl
+
+  module Scalar = Bignum.Bigint
 
   module Signature = struct
     type 'a t = 'a * 'a
@@ -41,7 +29,7 @@ struct
   end
 
   module Private_key = struct
-    type t = Scalar.Field.t
+    type t = Scalar.t
   end
 
   let compress ((x, _) : Curve.value) = Field.unpack x
@@ -52,31 +40,42 @@ struct
     val typ : (var, value) Typ.t
   end = Curve
 
-  let scale (x : Curve.value) (s : Scalar.Bigint.t) =
-    let n = Scalar.Field.size_in_bits in
+  let bigint_ceil_log2 =
+    let rec go acc n =
+      if Bignum.Bigint.(n = zero)
+      then acc
+      else go (acc + 1) (Bignum.Bigint.shift_right n 1)
+    in
+    go 0
+
+  let bigint_ith_bit n i = Z.testbit (Bignum.Bigint.to_zarith_bigint n) i
+
+  let scalar_size_in_bits = Z.numbits (Bignum.Bigint.to_zarith_bigint Curve.Params.order)
+
+  let scale (x : Curve.value) (s : Scalar.t) =
     let rec go i pt acc =
-      if i >= n
+      if i >= scalar_size_in_bits
       then acc
       else
         go (i + 1) (Curve.add pt pt)
-          (if Scalar.Bigint.test_bit s i then Curve.add acc pt else acc)
+          (if bigint_ith_bit s i then Curve.add acc pt else acc)
     in
     go 0 x Curve.identity
   ;;
 
   let sign (k : Private_key.t) m =
-    let e_r = Scalar.Field.random () in
-    let r = compress (scale Curve.generator (Scalar.Bigint.of_field e_r)) in
+    let e_r = Scalar.random Curve.Params.order in
+    let r = compress (scale Curve.generator e_r) in
     let h = Hash.hash (r @ m) in
-    let s = Scalar.Field.(sub e_r (mul k h)) in
+    let s = Scalar.((e_r - (k * h)) % Curve.Params.order) in
     (s, h)
   ;;
 
   (* TODO: Have expect test for this *)
   (* TODO: Have optimized double function *)
   let shamir_sum
-        ((sp, p) : Scalar.Bigint.t * Curve.value)
-        ((sq, q) : Scalar.Bigint.t * Curve.value)
+        ((sp, p) : Scalar.t * Curve.value)
+        ((sq, q) : Scalar.t * Curve.value)
     =
     let pq = Curve.add p q in
     let rec go i acc =
@@ -85,7 +84,7 @@ struct
       else
         let acc = Curve.add acc acc in
         let acc =
-          match Scalar.Bigint.test_bit sp i, Scalar.Bigint.test_bit sq i with
+          match bigint_ith_bit sp i, bigint_ith_bit sq i with
           | true, false -> Curve.add p acc
           | false, true -> Curve.add q acc
           | true, true -> Curve.add pq acc
@@ -93,7 +92,7 @@ struct
         in
         go (i - 1) acc
     in
-    go (Scalar.Field.size_in_bits - 1) Curve.identity
+    go (scalar_size_in_bits - 1) Curve.identity
 
   let verify
         ((s, h) : Signature.value)
@@ -103,7 +102,7 @@ struct
     let r =
       compress (shamir_sum (s, Curve.generator) (h, pk))
     in
-    Scalar.Field.equal (Hash.hash (r @ m)) (Scalar.Bigint.to_field h)
+    Scalar.equal (Hash.hash (r @ m)) h
   ;;
 
   module Checked = struct
