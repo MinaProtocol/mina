e3235a9de05a032273f6061f1d2107fc3f062b29
Izaak Meckler <ihmeckler@gmail.com>
2018-03-13 14:16:03 -0700

    ledger type

---
+++ lib/nanobit_base/ledger.mli
@@ -0,0 +1,35 @@
+open Core
+open Snark_params
+open Snarky
+open Tick
+
+type t = private Pedersen.Digest.t
+[@@deriving sexp]
+
+module Stable : sig
+  module V1 : sig
+    type nonrec t = t
+    [@@deriving bin_io, sexp]
+  end
+end
+
+type var
+
+type path = Pedersen.Digest.t list
+
+type _ Request.t +=
+  | Get_path    : Account.Index.t -> path Request.t
+  | Set         : Account.Index.t * Account.t -> unit Request.t
+  | Empty_entry : (Account.Index.t * path) Request.t
+  | Find_index  : Public_key.Compressed.t -> Account.Index.t Request.t
+
+val modify_account
+  : var
+  -> Public_key.Compressed.var
+  -> f:(Account.var -> (Account.var, 's) Checked.t)
+  -> (var, 's) Checked.t
+
+val create_account
+  : var
+  -> Public_key.Compressed.var
+  -> (var, _) Checked.t
