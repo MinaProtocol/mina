b415a0470709b833cc9abae7099e56116dec9cfa
Evan Shapiro <evan.a.shapiro@gmail.com>
2018-03-13 11:50:32 -0700

    extend distributed interface

--- lib/distributed_dsl/distributed_dsl.mli
+++ lib/distributed_dsl/distributed_dsl.mli
@@ -1,15 +1,6 @@
 open Core_kernel
 open Async_kernel
 
-module type Transport_intf = sig
-  type t
-  type message
-  type peer
-
-  val send : t -> recipient:peer -> message -> unit Or_error.t Deferred.t
-  val listen : t -> message Linear_pipe.Reader.t
-end
-
 module type S =
   functor 
     (State : sig type t [@@deriving eq] end)
@@ -20,37 +11,25 @@ module type S =
        type label [@@deriving enum]
        include Hashable.S with type t = label
      end)
-    (Transport : Transport_intf with type message := Message.t 
-                                 and type peer := Peer.t) 
+    (Transport : Node.Transport_intf with type message := Message.t 
+                                      and type peer := Peer.t) 
+    (Node : Node.S with type message := Message.t
+                    and type state := State.t
+                    and module Condition_label := Condition_label)
     -> sig
 
-    type condition = 
-      | Interval of Time.Span.t
-      | Timeout of Time.Span.t
-      | Message of Message.t
-      | Predicate of (State.t -> bool)
-
     type t = 
-      { state : State.t
-      ; last_state : State.t
-      ; conditions : edge Condition_label.Table.t
-      ; message_pipe : Message.t Linear_pipe.Reader.t
-      ; work : transition Linear_pipe.Reader.t
-      }
-    and transition = t -> State.t -> State.t
-    and edge = condition * transition
+      { nodes : Node.t list }
 
-    val on
-      : condition
-      -> f:transition
-      -> edge
+    type change = 
+      | Delete of int
+      | Add of Node.t
 
-    val make_node 
-      : messages : Message.t Linear_pipe.Reader.t
-      -> edge list 
-      -> t
+    type changes = State.t list -> change list
 
-    val step : t -> t
+    val step : t -> t Deferred.t
+
+    val loop : t -> stop : (unit -> unit Deferred.t) -> unit Deferred.t
 
   end
 
