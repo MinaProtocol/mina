6ab9b383d736cef65135d3c26d40781a5b7ab384
Izaak Meckler <ihmeckler@gmail.com>
2018-03-20 14:14:01 -0700

    insecure flag for private key generation

--- lib/nanobit_base/private_key.ml
+++ lib/nanobit_base/private_key.ml
@@ -1,6 +1,9 @@
 include Snark_params.Tick.Signature.Private_key
 
-(* TODO: Insecure *)
 let create () =
-  Bignum.Bigint.random
-    Snark_params.Tick.Hash_curve.Params.order
+  if Insecure.private_key_generation
+  then
+    Bignum.Bigint.random
+      Snark_params.Tick.Hash_curve.Params.order
+  else
+    failwith "Insecure.private_key_generation"
