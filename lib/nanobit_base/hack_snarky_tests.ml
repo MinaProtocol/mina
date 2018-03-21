1c4b7c64570df7ac3f790a2a2344e36e2c945dc2
Izaak Meckler <ihmeckler@gmail.com>
2018-03-21 11:45:26 -0700

    add merkle tree tests

--- lib/nanobit_base/hack_snarky_tests.ml
+++ lib/nanobit_base/hack_snarky_tests.ml
@@ -1,1 +1,47 @@
-let%test "foomerkle" = false
+open Core
+
+let%test_module "merkle_tree" =
+  (module (struct
+    open Snarky.Merkle_tree
+
+    let compress x y = Free_hash.Compress (x, y)
+    let hash =
+      Option.value_map ~default:Free_hash.Hash_empty
+        ~f:(fun x -> Free_hash.Hash_value x)
+
+    let create_tree n = 
+      let tree = create ~hash ~compress 0 in
+      add_many tree (List.init (n - 1) ~f:(fun i -> i + 1))
+
+    let n = 10
+    let tree = create_tree n
+
+    let%test "length" = 
+      List.length (to_list tree) = n
+    ;;
+
+    let%test_unit "key_retrieval" = 
+      for i = 0 to n - 1 do
+        assert (get_exn tree i = i)
+      done
+    ;;
+
+    let%test "key_nonexist" = 
+      None = get tree n
+    ;;
+
+    let%test_unit "modify" = 
+      let key = 5 in
+      assert (key = get_exn tree key);
+      let new_value = 123 in
+      let tree = update tree key new_value in
+      assert (new_value = get_exn tree key)
+    ;;
+
+    let%test_unit "merkle_root" =
+      for key = 0 to n - 1 do
+        let path = get_path tree key in
+        assert (implied_root ~compress key (hash (Some key)) path = root tree)
+      done
+    ;;
+  end))
