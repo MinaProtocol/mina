7bce5f4a1ed4a748ab52b42f0b23bf5ead02d74c
Izaak Meckler <ihmeckler@gmail.com>
2018-03-21 14:57:41 -0700

    public key, currency, account

--- lib/nanobit_base/public_key.ml
+++ lib/nanobit_base/public_key.ml
@@ -11,6 +11,9 @@ let typ : (var, t) Tick.Typ.t = Tick.Typ.(field * field)
 (* TODO: We can move it onto the subgroup during account creation. No need to check with
   every transaction *)
 
+let of_private_key pk =
+  Tick.Hash_curve.scale Tick.Hash_curve.generator pk
+
 module Compressed = struct
   open Tick
   type t = Field.t [@@deriving bin_io]
@@ -18,15 +21,25 @@ module Compressed = struct
   let typ : (var, t) Typ.t = Typ.field
   let assert_equal (x : var) (y : var) = assert_equal x y
 
+  include (Bits.Make_field(Field)(Bigint) : Bits_intf.S with type t := t)
+
 (* TODO: Right now everyone could switch to using the other unpacking...
    Either decide this is ok or assert bitstring lt field size *)
   let var_to_bits (pk : var) =
-    Checked.choose_preimage pk ~length:Field.size_in_bits
+    with_label "Public_key.Compressed.var_to_bits"
+      (Checked.choose_preimage pk ~length:Field.size_in_bits)
 end
 
-let compress_var : var -> Compressed.var = fun _ -> failwith "TODO"
+let compress_var : var -> Compressed.var = fun (x, _) -> x
 let decompress_var : Compressed.var -> var = fun _ -> failwith "TODO"
-let assert_equal : var -> var -> (unit, _) Tick.Checked.t = fun _ _ -> failwith "TODO"
+
+let assert_equal : var -> var -> (unit, _) Tick.Checked.t =
+  fun (x1, y1) (x2, y2) ->
+    let open Tick in let open Let_syntax in
+    let%map () = assert_equal x1 x2
+    and () = assert_equal y1 y2
+    in
+    ()
 
 let of_bigstring bs =
   let open Or_error.Let_syntax in
