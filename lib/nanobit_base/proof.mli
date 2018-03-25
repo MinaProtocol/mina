89b562883f0223d384a44cac9c0d4f563fd94a97
Izaak Meckler <ihmeckler@gmail.com>
2018-03-24 22:35:52 -0700

    switch proof to being a tock proof

--- lib/nanobit_base/proof.mli
+++ lib/nanobit_base/proof.mli
@@ -1,10 +1,10 @@
 open Snark_params
 
-type t = Tick.Proof.t
+type t = Tock.Proof.t
 
 module Stable : sig
   module V1 : sig
     type nonrec t = t
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
   end
 end
