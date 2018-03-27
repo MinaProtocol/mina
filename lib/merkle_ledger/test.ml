ca707747f5fe967ce8c2c209660d1c75cc632d16
Izaak Meckler <ihmeckler@gmail.com>
2018-03-27 13:22:35 -0700

    tests for new merkle functions

--- lib/merkle_ledger/test.ml
+++ lib/merkle_ledger/test.ml
@@ -26,11 +26,25 @@ let%test "key_retrieval" =
   Some 100 = Test_ledger.get ledger (List.nth_exn keys 0)
 ;;
 
+let%test "idx_retrieval" = 
+  let b = 100 in
+  let d = 16 in
+  let ledger, _keys = load_ledger d 10 b in
+  `Ok 100 = Test_ledger.get_at_index ledger 0
+;;
+
 let%test "key_nonexist" = 
   let b = 100 in
   let d = 16 in
   let ledger, keys = load_ledger d 10 b in
   None = Test_ledger.get ledger "aintioaerntnearst"
+;;
+
+let%test "idx_nonexist" = 
+  let b = 100 in
+  let d = 16 in
+  let ledger, _keys = load_ledger d 10 b in
+  `Index_not_found = Test_ledger.get_at_index ledger 0
 ;;
 
 let%test_unit "modify_account" = 
@@ -41,6 +55,16 @@ let%test_unit "modify_account" =
   assert (Some 100 = Test_ledger.get ledger key);
   Test_ledger.update ledger key 50;
   assert (Some 50 = Test_ledger.get ledger key);
+;;
+
+let%test_unit "modify_account_by_idx" = 
+  let b = 100 in
+  let d = 16 in
+  let ledger, keys = load_ledger d 10 b in
+  let idx = 0 in
+  assert (`Ok 100 = Test_ledger.get_at_index ledger idx);
+  Test_ledger.update_at_index_exn ledger idx 50;
+  assert (`Ok 50 = Test_ledger.get_at_index ledger idx);
 ;;
 
 let rec compose_hash i hash =
@@ -108,6 +132,21 @@ let%test_unit "merkle_path" =
       let key = List.nth_exn keys 0 in
       let path = Test_ledger.merkle_path ledger key |> Option.value_exn in
       let account = Test_ledger.get ledger key |> Option.value_exn in
+      let root = Test_ledger.merkle_root ledger in
+      assert (List.length path = d);
+      assert (check_path account path root)
+    );
+;;
+
+let%test_unit "merkle_path_at_index" =
+  let b1 = 10 in
+  let d = 16 in
+  let idx = 0 in
+  List.iter (List.range 1 20)
+    ~f:(fun n -> 
+      let ledger, keys = load_ledger d n b1 in
+      let path = Test_ledger.merkle_path_at_index_exn ledger idx in
+      let account = Test_ledger.get_at_index_exn ledger idx in
       let root = Test_ledger.merkle_root ledger in
       assert (List.length path = d);
       assert (check_path account path root)
