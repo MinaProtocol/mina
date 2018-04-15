5dcc924f2db0ef782356c964ff046ffd346efae6
Izaak Meckler <ihmeckler@gmail.com>
2018-04-14 18:01:14 -0700

    remove most_recent_block from blockchain

--- lib/blockchain_snark/blockchain.mli
+++ lib/blockchain_snark/blockchain.mli
@@ -6,16 +6,14 @@ open Snark_params
 module State = Blockchain_state
 
 type t =
-  { state             : State.t
-  ; most_recent_block : Block.t
-  ; proof             : Proof.t
+  { state : State.t
+  ; proof : Proof.t
   }
 
 module Stable : sig
   module V1 : sig
     type nonrec t = t =
       { state : State.Stable.V1.t
-      ; most_recent_block : Block.Stable.V1.t
       ; proof : Proof.Stable.V1.t
       }
     [@@deriving bin_io]
