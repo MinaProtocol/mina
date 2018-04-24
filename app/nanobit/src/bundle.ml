574dc4659fe5db5a88cf32ac7e146c3065b8d4b1
Brandon Kase <bkase@o1labs.org>
2018-04-24 11:46:57 -0700

    Removed duplicated TODO comment

--- app/nanobit/src/bundle.ml
+++ app/nanobit/src/bundle.ml
@@ -9,11 +9,6 @@ module type S = sig
 
   val cancel : t -> unit
 
-  (* TODO: Need a mechanism for preventing malleability
-    of transaction bundle (so no one can steal fees).
-    One idea is to have a "drain" snark at the end that
-    takes the built up fees and transfers them into one
-    account. *)
   val create : conf_dir:string -> Ledger.t -> Transaction.With_valid_signature.t list -> t
 
   val target_hash : t -> Ledger_hash.t
