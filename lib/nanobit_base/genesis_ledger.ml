6055048d5dc3f8916467d5a38e5b90a0483771a3
Brandon Kase <bkase@o1labs.org>
2018-04-27 14:05:00 -0700

    Make two people live in genesis ledger

--- lib/nanobit_base/genesis_ledger.ml
+++ lib/nanobit_base/genesis_ledger.ml
@@ -1,25 +1,46 @@
 open Core_kernel
 
+let rich_sk =
+  "IgAAAAAAAAABIJLfK6/afuZTpDqzVSI/eMo7h/HuH/CcZozCtSEgsoLc"
+    |> B64.decode
+    |> Bigstring.of_string
+    |> Private_key.of_bigstring
+    |> Or_error.ok_exn
+
+let poor_sk =
+  "KgAAAAAAAAABKEHfd5r8nKEMPSVcgvbWS6CdErbzB4eYaxpr9qJqtKy5JAAAAAAAAAA="
+    |> B64.decode
+    |> Bigstring.of_string
+    |> Private_key.of_bigstring
+    |> Or_error.ok_exn
+
+let poor_pk = Public_key.of_private_key poor_sk
+
+let rich_pk = Public_key.of_private_key rich_sk
+
+let initial_rich_balance = Currency.Balance.of_int 10_000
+let initial_poor_balance = Currency.Balance.of_int 100
+
 (* Genesis ledger has a single rich person *)
 let ledger =
   let ledger = Ledger.create () in
-  let rich_sk =
-    "IgAAAAAAAAABIJLfK6/afuZTpDqzVSI/eMo7h/HuH/CcZozCtSEgsoLc"
-        |> B64.decode
-        |> Bigstring.of_string
-        |> Private_key.of_bigstring
-        |> Or_error.ok_exn
-  in
-  let rich_pk = Public_key.of_private_key rich_sk in
   let compressed_rich_pk =
     Public_key.compress rich_pk
+  and compressed_poor_pk =
+    Public_key.compress poor_pk
   in
   Ledger.update
     ledger
     compressed_rich_pk
     { Account.public_key = compressed_rich_pk
-    ; balance = Currency.Balance.of_int 100
+    ; balance = initial_rich_balance
     };
 
+  Ledger.update
+    ledger
+    compressed_poor_pk
+    { Account.public_key = compressed_poor_pk
+    ; balance = initial_poor_balance
+    };
   ledger
 
