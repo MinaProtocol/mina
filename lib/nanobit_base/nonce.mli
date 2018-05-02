e2373f9726e7cca3dd5a772538f6c53abc9a0790
Brandon Kase <bkase@o1labs.org>
2018-05-01 20:41:21 -0700

    Nonce uses the new "small" bits

--- lib/nanobit_base/nonce.mli
+++ lib/nanobit_base/nonce.mli
@@ -2,12 +2,12 @@ open Core_kernel
 
 module type S = sig
   type t
-  [@@deriving sexp]
+  [@@deriving sexp, compare, eq, hash]
 
   module Stable : sig
     module V1 : sig
       type nonrec t = t
-      [@@deriving bin_io, sexp, eq]
+      [@@deriving bin_io, sexp, eq, compare, hash]
     end
   end
 
@@ -19,28 +19,26 @@ module type S = sig
     selecting the nonce *)
   val random : unit -> t
 
+  val of_string : string -> t
+  val to_string : t -> string
+
   module Bits : Bits_intf.S with type t := t
 
-  include Snark_params.Tick.Snarkable.Bits.Faithful
+  include Snark_params.Tick.Snarkable.Bits.Small
     with type Unpacked.value = t
      and type Packed.value = t
 end
 
 module type F = functor
   (N : sig
-    type t [@@deriving bin_io, sexp, eq]
+    type t [@@deriving bin_io, sexp, eq, compare, hash]
     include Unsigned_extended.S with type t := t
     val random : unit -> t
   end)
   (Bits : Bits_intf.S with type t := N.t)
-  (Bits_snarkable : functor (Impl : Snarky.Snark_intf.S) -> Bits_intf.Snarkable.Faithful
-       with type ('a, 'b) typ := ('a, 'b) Impl.Typ.t
-        and type ('a, 'b) checked := ('a, 'b) Impl.Checked.t
-        and type boolean_var := Impl.Boolean.var
-        and type Packed.var = Impl.Cvar.t
-        and type Packed.value = N.t
-        and type Unpacked.var = Impl.Boolean.var list
-        and type Unpacked.value = N.t) ->
+  (Bits_snarkable : Snark_params.Tick.Snarkable.Bits.Small
+        with type Packed.value = N.t
+         and type Unpacked.value = N.t) ->
           S with type t := N.t
              and module Bits := Bits
 
