5704dd5c3dc5ebb1bc377a4bc852ba1d1d212da5
Corey Richardson <cmr@o1labs.org>
2018-06-04 11:33:16 -0700

    look at more places to try and find our IP

--- app/nanobit/src/find_ip.ml
+++ app/nanobit/src/find_ip.ml
@@ -2,10 +2,21 @@ open Core
 open Async
 open Cohttp_async
 
+type ip_service = { uri : string ; body_handler : string -> string; }
 
-let uri = Uri.of_string "https://api.ipify.org"
+let services = [
+  { uri = "https://api.ipify.org"; body_handler = Fn.id };
+  { uri = "https://bot.whatismyipaddress.com"; body_handler = Fn.id };
+  { uri = "http://ifconfig.co/ip"; body_handler = String.rstrip ~drop:(fun c -> c = '\n') }
+];;
 
 let find () =
-  let%bind (_resp, body) = Client.get uri in
-  Body.to_string body
+  let handler : string option -> ip_service -> string option Deferred.t = fun acc elem -> match acc with
+    | None ->
+        let%bind (resp, body) = Client.get (Uri.of_string elem.uri) in
+        Body.to_string body >>| elem.body_handler >>|
+        (fun s -> if resp.status = `OK then Some s else None)
+    | Some x -> return acc
+  in (Deferred.List.fold services ~init:None ~f:handler >>|
+    (fun x -> Option.value_exn ~message:"couldn't figure out own IP from the internet" x))
 ;;
