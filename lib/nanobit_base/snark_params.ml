1c6b14455b443cce1737f071a2c89e0b919d1d10
Izaak Meckler <ihmeckler@gmail.com>
2018-06-09 20:22:15 -0700

    factor out curve choices into crypto_params library

--- lib/nanobit_base/snark_params.ml
+++ lib/nanobit_base/snark_params.ml
@@ -1,7 +1,7 @@
 open Core_kernel
 
-module Tick_curve = Snarky.Backends.Mnt4
-module Tock_curve = Snarky.Backends.Mnt6
+module Tick_curve = Crypto_params.Tick_curve
+module Tock_curve = Crypto_params.Tock_curve
 
 module Extend (Impl : Snarky.Snark_intf.S) = struct
   include Impl
@@ -40,14 +40,7 @@ module Tock = struct
   module Proof = struct
     include Tock0.Proof
 
-    (* TODO: Do at compile time *)
-    let dummy =
-      lazy begin
-        let exposing = Data_spec.([ Typ.field ]) in
-        let main x = assert_equal x x in
-        let keypair = generate_keypair main ~exposing in
-        prove (Keypair.pk keypair) exposing () main Field.one
-      end
+    let dummy = Dummy_proof.tock
   end
 end
 
@@ -93,22 +86,7 @@ module Tick = struct
       (* someday: Compute this from the number inside of ocaml *)
       let bit_length = 262
 
-      include Snarky.Curves.Edwards.Basic.Make(Field)(struct
-          (*
-  Curve params:
-  d = 20
-  cardinality = 475922286169261325753349249653048451545124878135421791758205297448378458996221426427165320
-  2^3 * 5 * 7 * 399699743 * 4252498232415687930110553454452223399041429939925660931491171303058234989338533 *)
-
-          let d = Field.of_int 20
-          let cofactor = Bignum_bigint.(of_int 8 * of_int 5 * of_int 7 * of_int 399699743)
-          let order = Bignum_bigint.of_string "4252498232415687930110553454452223399041429939925660931491171303058234989338533"
-
-          let generator = 
-            let f s = Tick_curve.Bigint.R.(to_field (of_decimal_string s)) in
-            f "327139552581206216694048482879340715614392408122535065054918285794885302348678908604813232",
-            f "269570906944652130755537879906638127626718348459103982395416666003851617088183934285066554"
-        end)
+      include Snarky.Curves.Edwards.Basic.Make(Field)(Crypto_params.Hash_curve)
 
       module Scalar (Impl : Snarky.Snark_intf.S) = struct
         (* Someday: Make more efficient *)
