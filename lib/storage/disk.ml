c7ecae14e08f89525c26002e4b5f3146c8e940c5
Izaak Meckler <ihmeckler@gmail.com>
2018-06-09 20:21:18 -0700

    add functions to storage that expose the checksums

--- lib/storage/disk.ml
+++ lib/storage/disk.ml
@@ -17,15 +17,26 @@ module Controller = struct
     }
 end
 
-let load (c : 'a Controller.t) location =
-  match%map Reader.load_bin_prot location (bin_reader_t c.tc.reader) with
+let load_with_checksum (c : 'a Controller.t) location =
+  match%map Reader.load_bin_prot ~max_len:(1000 * 1024 * 1024) location (bin_reader_t c.tc.reader) with
   | Ok t ->
     if valid c.tc t
-    then Ok t.data
+    then Ok t
     else Error `Checksum_no_match
   | Error e -> Error (`IO_error e)
 
-let store (c : 'a Controller.t) location data = 
+let load c location =
+  Deferred.Result.map (load_with_checksum c location) ~f:(fun t -> t.data)
+
+let store_with_checksum (c : 'a Controller.t) location data =
+  let t = wrap c.tc data in
+  Writer.save_bin_prot
+    location
+    (bin_writer_t c.tc.writer)
+    t
+  >>| fun () -> t.checksum
+
+let store (c : 'a Controller.t) location data =
   Writer.save_bin_prot
     location
     (bin_writer_t c.tc.writer)
--- lib/storage/memory.ml
+++ lib/storage/memory.ml
@@ -19,18 +19,28 @@ module Controller = struct
     }
 end
 
-let load (c : 'a Controller.t) location =
+let load_with_checksum (c : 'a Controller.t) location =
   Deferred.return begin
     match Location.Table.find c.mem location with
     | Some t ->
       if valid c.tc t
-      then Ok t.data
+      then Ok t
       else Error `Checksum_no_match
     | None -> Error `No_exist
   end
 
+let load c location =
+  Deferred.Result.map (load_with_checksum c location) ~f:(fun t -> t.data)
+
 let store (c : 'a Controller.t) location data =
   Deferred.return begin
     Location.Table.set c.mem ~key:location ~data:(wrap c.tc data)
+  end
+
+let store_with_checksum (c : 'a Controller.t) location data =
+  let data = wrap c.tc data in
+  Deferred.return begin
+    Location.Table.set c.mem ~key:location ~data;
+    data.checksum
   end
 
