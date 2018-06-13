8c2e7e499d9d6770e144aca2a7f4280b5ca3965b
Izaak Meckler <ihmeckler@gmail.com>
2018-06-13 15:27:32 -0700

    add some functions to interval union

--- lib/interval_union/interval_union.mli
+++ lib/interval_union/interval_union.mli
@@ -2,10 +2,13 @@ module Interval : sig
   type t = int * int
 end
 
-type t
-[@@deriving eq]
+type t [@@deriving eq]
+
+val empty : t
 
 val of_interval : Interval.t -> t
+
+val of_intervals_exn : Interval.t list -> t
 
 val disjoint_union_exn : t -> t -> t
 
