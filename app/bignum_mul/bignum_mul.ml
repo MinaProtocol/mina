098f70cfaf9590cbbd70266bb8a2353c5fbf6467
Brandon Kase <bkase@o1labs.org>
2018-06-16 13:01:37 -0700

    15bit limbs, whole computation fits in 32bits

--- app/bignum_mul/bignum_mul.ml
+++ app/bignum_mul/bignum_mul.ml
@@ -1,32 +1,29 @@
 open Core
 
-type t = Int64.t array
+type t = int array
 
-let two_to_the_30 = Int64.of_int 1073741824
+let two_to_the_15 = 32768
 
-let info_per_limb = 30
+let info_per_limb = 15
 
 let len ~max_bits : int =
   (max_bits / info_per_limb) + if max_bits % info_per_limb <> 0 then 1 else 0
 
-let alloc ~max_bits () = Array.init (len ~max_bits) ~f:(fun _ -> Int64.zero)
+let alloc ~max_bits () = Array.init (len ~max_bits) ~f:(fun _ -> 0)
 
-let of_int30 ~max_bits x =
-  let x = Int64.of_int x in
-  assert (Int64.( < ) x two_to_the_30) ;
+let of_int15 ~max_bits x =
+  assert (x < two_to_the_15) ;
   let t = alloc ~max_bits () in
   t.(0) <- x ; t
 
 let gen ~max_bits =
   let open Quickcheck.Generator in
-  list_with_length (len ~max_bits) Int64.(gen_incl zero (two_to_the_30 - one))
+  list_with_length (len ~max_bits) Int.(gen_incl 0 (two_to_the_15 - 1))
   >>| Array.of_list
-
-let zero ~max_bits = Array.init (len ~max_bits) ~f:(fun _ -> 0)
 
 let to_bigint =
   Array.foldi ~init:Bigint.zero ~f:(fun i acc x ->
-      Bigint.(acc + (of_int64 x * pow (of_int64 two_to_the_30) (of_int i))) )
+      Bigint.(acc + (of_int x * pow (of_int two_to_the_15) (of_int i))) )
 
 let mul a b : t =
   assert (Array.length a = Array.length b) ;
@@ -38,36 +35,34 @@ let mul a b : t =
       let rec go' j carry =
         if j = go'_stop then carry
         else
-          let open Int64 in
-          let digit = res.(j) + (b.(i) * a.(Int.( - ) j i)) + carry in
-          let carry = digit / two_to_the_30 in
-          res.(j) <- digit % two_to_the_30 ;
-          go' (Int.( + ) j 1) carry
+          let digit = res.(j) + (b.(i) * a.(j - i)) + carry in
+          let carry = digit / two_to_the_15 in
+          res.(j) <- digit % two_to_the_15 ;
+          go' (j + 1) carry
       in
       let carry = go' i carry in
       let carry =
-        let open Int64 in
-        if Int64.( <> ) carry Int64.zero then (
+        if carry <> 0 then (
           let digit = res.(go'_stop) + carry in
-          res.(go'_stop) <- digit % two_to_the_30 ;
-          let carry = digit / two_to_the_30 in
+          res.(go'_stop) <- digit % two_to_the_15 ;
+          let carry = digit / two_to_the_15 in
           carry )
         else carry
       in
       go (i + 1) carry
   in
-  go 0 Int64.zero ; res
+  go 0 0 ; res
 
 let () =
   let size = 768 in
-  (* Limbs are 30bits each, so we'll need 26 of them for a 768 bit number *)
-  assert (len size = 26) ;
+  (* Limbs are 15bits each, so we'll need 52 of them for a 768 bit number *)
+  assert (len size = 52) ;
   (* Make sure bigint conversion is sane *)
   assert (
-    Bigint.( = ) (to_bigint (of_int30 ~max_bits:size 4)) (Bigint.of_int 4) ) ;
+    Bigint.( = ) (to_bigint (of_int15 ~max_bits:size 4)) (Bigint.of_int 4) ) ;
   (* Quickcheck the shit out of multiplication *)
   Quickcheck.test ~trials:100000
-    ~sexp_of:[%sexp_of : Int64.t Array.t * Int64.t Array.t]
+    ~sexp_of:[%sexp_of : int Array.t * int Array.t]
     (Quickcheck.Generator.tuple2 (gen ~max_bits:size) (gen ~max_bits:size))
     ~f:(fun (a, b) ->
       let work = mul a b |> to_bigint in
