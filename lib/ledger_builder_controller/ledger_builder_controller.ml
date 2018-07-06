e6b6ddba1b986ec500c4fa176b68916a613347ca
Brandon Kase <bkase@o1labs.org>
2018-07-06 16:10:20 -0700

    Use Inputs struct, too many params

--- lib/ledger_builder_controller/ledger_builder_controller.ml
+++ lib/ledger_builder_controller/ledger_builder_controller.ml
@@ -1,68 +1,89 @@
 open Core_kernel
 open Async_kernel
 
-module type S = sig
-  include Coda.Ledger_builder_controller_intf
+module type Inputs_intf = sig
+  module Ledger_builder_hash : sig
+    type t [@@deriving eq, bin_io]
+  end
+
+  module Ledger_hash : sig
+    type t [@@deriving bin_io]
+  end
+
+  module Ledger_builder_transition : sig
+    type t [@@deriving eq, sexp, compare, bin_io]
+  end
+
+  module Ledger : sig
+    type t
+
+    val merkle_root : t -> Ledger_hash.t
+  end
+
+  module Ledger_builder : sig
+    type t [@@deriving bin_io]
+
+    type proof
+
+    val ledger : t -> Ledger.t
+
+    val create : Ledger.t -> t
+
+    val copy : t -> t
+
+    val hash : t -> Ledger_builder_hash.t
+
+    val apply :
+         t
+      -> Ledger_builder_transition.t
+      -> (Ledger_hash.t * proof) option Deferred.Or_error.t
+  end
+
+  module State_hash : sig
+    type t [@@deriving eq]
+  end
+
+  module State : sig
+    type t [@@deriving eq, sexp, compare, bin_io]
+
+    val ledger_builder_hash : t -> Ledger_builder_hash.t
+
+    val hash : t -> State_hash.t
+
+    val previous_state_hash : t -> State_hash.t
+  end
+
+  module Valid_transaction : sig
+    type t [@@deriving eq, sexp, compare, bin_io]
+  end
+
+  module Net : sig
+    include Coda.Ledger_builder_io_intf
+            with type ledger_builder := Ledger_builder.t
+             and type ledger_builder_hash := Ledger_builder_hash.t
+             and type state := State.t
+  end
+
+  module Snark_pool : sig
+    type t
+  end
+
+  module Store : Storage.With_checksum_intf
 end
 
-module Make (Ledger_builder_hash : sig
-  type t [@@deriving eq, bin_io]
-end) (Ledger_hash : sig
-  type t [@@deriving bin_io]
-end) (Ledger_builder_transition : sig
-  type t [@@deriving eq, sexp, compare, bin_io]
-end) (Ledger : sig
-  type t
-
-  val merkle_root : t -> Ledger_hash.t
-end) (Ledger_builder : sig
-  type t [@@deriving bin_io]
-
-  type proof
-
-  val ledger : t -> Ledger.t
-
-  val create : Ledger.t -> t
-
-  val copy : t -> t
-
-  val hash : t -> Ledger_builder_hash.t
-
-  val apply :
-       t
-    -> Ledger_builder_transition.t
-    -> (Ledger_hash.t * proof) option Deferred.Or_error.t
-end) (State_hash : sig
-  type t [@@deriving eq]
-end) (State : sig
-  type t [@@deriving eq, sexp, compare, bin_io]
-
-  val ledger_builder_hash : t -> Ledger_builder_hash.t
-
-  val hash : t -> State_hash.t
-
-  val previous_state_hash : t -> State_hash.t
-end) (Valid_transaction : sig
-  type t [@@deriving eq, sexp, compare, bin_io]
-end) (Net : sig
-  include Coda.Ledger_builder_io_intf
-          with type ledger_builder := Ledger_builder.t
-           and type ledger_builder_hash := Ledger_builder_hash.t
-           and type state := State.t
-end) (Snark_pool : sig
-  type t
-end)
-(Store : Storage.With_checksum_intf) :
+module Make (Inputs : Inputs_intf) :
   Coda.Ledger_builder_controller_intf
-  with type ledger_builder := Ledger_builder.t
-   and type ledger_builder_hash := Ledger_builder_hash.t
-   and type ledger_builder_transition := Ledger_builder_transition.t
-   and type ledger := Ledger.t
-   and type transaction_with_valid_signature := Valid_transaction.t
-   and type net := Net.net
-   and type state := State.t
-   and type snark_pool := Snark_pool.t =
+  with type ledger_builder := Inputs.Ledger_builder.t
+   and type ledger_builder_hash := Inputs.Ledger_builder_hash.t
+   and type ledger_builder_transition := Inputs.Ledger_builder_transition.t
+   and type ledger := Inputs.Ledger.t
+   and type transaction_with_valid_signature := Inputs.Valid_transaction.t
+   and type net := Inputs.Net.net
+   and type state := Inputs.State.t
+   and type snark_pool := Inputs.Snark_pool.t =
 struct
+  open Inputs
+
   module Config = struct
     type t =
       { keep_count: int [@default 50]
