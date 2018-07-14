aa75874fffd7a544fc41909c48b97a756250e2d3
Izaak Meckler <ihmeckler@gmail.com>
2018-07-13 18:34:51 -0700

    verifier module

---
+++ app/nanobit/src/verifier.mli
@@ -0,0 +1,12 @@
+open Core
+open Async
+
+type t
+
+val create : conf_dir:string -> t Deferred.t
+
+val verify_blockchain
+  : t -> Blockchain_snark.Blockchain.t -> bool Or_error.t Deferred.t
+
+val verify_transaction_snark
+  : t -> Transaction_snark.t -> bool Or_error.t Deferred.t
