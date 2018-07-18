c181e890392fd42a5e07458a38c9cf27202f641c
akis <github@kattis.io>
2018-07-18 13:40:16 -0700

    formatted and added snarky libraries

--- lib/vrf_lib/vrf.ml
+++ lib/vrf_lib/vrf.ml
@@ -1,9 +1,19 @@
 module Make
+         (Impl : Snarky.Snark_intf.S)
          (Hash : sig
             type t
 
             val hash : bool list -> t
             val equals: t -> t -> bool
+
+            module Checked : sig
+              open Impl
+
+              type var
+
+              val hash : Boolean.var list -> (var, _) Checked.t
+              val equals : var -> var -> (Boolean.var, _) Checked.t
+            end
           end)
          (Scalar : sig
             type t
@@ -15,8 +25,9 @@ module Make
 
             val from_hash: Hash.t -> t
 
-            val fromBits: bool list -> t
-            val toBits: t   -> bool list
+            val of_bits: bool list -> t
+            val to_bits: t   -> bool list
+
           end)
 
            (Group : sig
@@ -28,8 +39,18 @@ module Make
 
             val generator : t
 
-            val fromBits: bool list -> t
-            val toBits: t   -> bool list
+            val of_bits: bool list -> t
+            val to_bits: t   -> bool list
+
+
+            module Checked : sig
+              open Impl
+              type var
+
+              val add : var -> var -> (var, _) Checked.t
+              val inv : var -> (var, _) Checked.t
+            end
+
           end)
          (Hash_to_group : sig
             val hash : bool list -> Group.t
@@ -44,12 +65,7 @@ module Make
   module Private_key : sig
     type t
 
-    val toScalar: t -> Scalar.t
-  end
-  module P_EQDL : sig
-    type t
-
-    val toScalar : t -> Scalar.t
+    val to_scalar: t -> Scalar.t
   end
 
   val eval : bool list -> Private_key.t -> evaluation
@@ -65,7 +81,7 @@ end =
     module Private_key = struct
       type t = bool list
 
-      let toScalar = Scalar.fromBits
+      let to_scalar = Scalar.of_bits
     end
 
     module P_EQDL = struct
@@ -79,17 +95,22 @@ end =
     type proof = Group.t * P_EQDL.t
     type evaluation = bool list * Hash.t * proof * Public_key.t
 
+
+    type t = { x : int; y : string }
+
+     let a = { x = 34; y  = "a" }
+
     let eval m prk =
-      let k = Private_key.toScalar prk in
+      let k = Private_key.to_scalar prk in
       let hgm = Hash_to_group.hash m in
       let u = (Group.scale hgm k) in
-      let y = Hash.hash (List.append m (Group.toBits u)) in
+      let y = Hash.hash (List.append m (Group.to_bits u)) in
       let g = Group.generator in
       let v = Group.scale g k in
       let r = Scalar.random in
       let gr = Group.scale g r in
       let hmr = Group.scale hgm r in
-      let proof1 = Hash.hash (List.append (List.append m (Group.toBits v)) (List.append (Group.toBits gr) (Group.toBits hmr))) in
+      let proof1 = Hash.hash (m @ (Group.to_bits v) @ (Group.to_bits gr) @ (Group.to_bits hmr)) in
       let s = (Scalar.add r (Scalar.mul k (Scalar.from_hash proof1))) in
       let eqdl = (proof1, s) in
       let proof = (u, eqdl) in
@@ -98,7 +119,7 @@ end =
     let verify (m,y,proof,v) =
       let (u, eqdl) = proof in
       let (proof1, s) = eqdl in
-      let y1 = Hash.hash (List.append m (Group.toBits u)) in
+      let y1 = Hash.hash (m @ (Group.to_bits u)) in
       let g = Group.generator in
       let gs = Group.scale g s in
       let c = P_EQDL.toScalar eqdl in
@@ -107,7 +128,7 @@ end =
       let hms = Group.scale (Hash_to_group.hash m) s in
       let unegc = Group.scale (Group.inv u) c in
       let hmsuc = Group.add hms unegc in
-      let c1 = Hash.hash (List.append (List.append m (Group.toBits v)) (List.append (Group.toBits gsvc) (Group.toBits hmsuc))) in
+      let c1 = Hash.hash (m @ (Group.to_bits v) @ (Group.to_bits gsvc) @ (Group.to_bits hmsuc)) in
       let b1 = Hash.equals y y1 in
       let b2 = Hash.equals proof1 c1 in
       b1 && b2
