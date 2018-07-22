d0b0ffd438960a2e20405d2961e12563269c5df3
Izaak Meckler <ihmeckler@gmail.com>
2018-07-22 11:22:02 -0700

    move around the flags in insecure mode

--- lib/nanobit_base/insecure.ml
+++ lib/nanobit_base/insecure.ml
@@ -1,17 +1,9 @@
 (* These should all be false! *)
 let strength_calculation = true
 
-let verify_blockchain = false
-
 let check_target = true
 
-let compute_base_hash = false
-
-let compute_base_proof = false
-
-let extend_blockchain = false
-
-let private_key_generation = true
+let initial_difficulty = true
 
 let transaction_replay = true
 
@@ -19,9 +11,17 @@ let fee_collection = true
 
 let signature_hash_function = true
 
-let initial_difficulty = true
+let private_key_generation = true
+
+let randomness = true
 
 (* Enabling this will SPEED UP builds as keys are generated at compile-time *)
 let key_generation = false
 
-let randomness = true
+let verify_blockchain = false
+
+let compute_base_hash = false
+
+let compute_base_proof = false
+
+let extend_blockchain = false
