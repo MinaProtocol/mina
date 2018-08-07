9a077b3daff469e7b27be574d2bad9fdea6cf6e6
Izaak Meckler <ihmeckler@gmail.com>
2018-08-06 17:18:41 -0700

    fix problems in gm_verifier_gadget

--- lib/snarky/src/gm_verifier_gadget.ml
+++ lib/snarky/src/gm_verifier_gadget.ml
@@ -11,6 +11,7 @@ module Make
                                  Impl.R1CS_constraint_system.t)
     (Other_curve : Libsnark.S) (Info : sig
         val input_size : int
+        val fqe_size_in_field_elements : int
     end) =
 struct
   open Impl
@@ -37,9 +38,8 @@ struct
     val characterizing_vars_up_to_sign : t -> Libsnark.Linear_combination.Vector.t
     val sign_vars : t -> Libsnark.Linear_combination.Vector.t
 
-    val characterizing_elts_up_to_sign : Libsnark.Verification_key.t -> Other_curve.Field.Vector.t
-    val sign_elts : Libsnark.Verification_key.t -> Other_curve.Field.Vector.t
-    val characterizing_bits : Libsnark.Verification_key.t -> bool list
+    val characterizing_elts_up_to_sign : Other_curve.Verification_key.t -> Field.Vector.t
+    val sign_elts : Other_curve.Verification_key.t -> Field.Vector.t
 
     val generate_witness : t -> Other_curve.Verification_key.t -> unit
   end = struct
@@ -79,21 +79,21 @@ struct
     let characterizing_elts_up_to_sign =
       let stub =
         foreign (with_prefix Prefix.prefix "gm_verification_key_characterizing_elts_up_to_sign")
-          (Libsnark.Verification_key.typ @-> returning Other_curve.Field.Vector.typ)
+          (Other_curve.Verification_key.typ @-> returning Field.Vector.typ)
       in
       fun t ->
         let v = stub t in
-        Caml.Gc.finalise Other_curve.Field.Vector.delete v;
+        Caml.Gc.finalise Field.Vector.delete v;
         v
 
     let sign_elts =
       let stub =
         foreign (with_prefix Prefix.prefix "gm_verification_key_sign_elts")
-          (Libsnark.Verification_key.typ @-> returning Other_curve.Field.Vector.typ)
+          (Other_curve.Verification_key.typ @-> returning Field.Vector.typ)
       in
       fun t ->
         let v = stub t in
-        Caml.Gc.finalise Other_curve.Field.Vector.delete v;
+        Caml.Gc.finalise Field.Vector.delete v;
         v
 
     let characterizing_vars_up_to_sign =
@@ -106,27 +106,6 @@ struct
         Caml.Gc.finalise Libsnark.Linear_combination.Vector.delete res;
         res
 
-    let characterizing_bits vk =
-      let open Other_curve in
-      let unpack x =
-        let n = Bigint.R.of_field x in
-        List.init Other_curve.Field.size_in_bits ~f:(fun i ->
-          Bigint.R.test_bit n i)
-      in
-      let bs1 =
-        let v = characterizing_elts_up_to_sign vk in
-        List.init (Field.Vector.length v) ~f:(fun i ->
-          unpack (Field.Vector.get v i))
-        |> List.concat
-      in
-      let bs2 =
-        let v = sign_elts vk in
-        List.init (Field.Vector.length v) ~f:(fun i ->
-          Bigint.R.test_bit
-            (Bigint.R.of_field (Field.Vector.get v i)) 0)
-      in
-      bs1 @ bs2
-
     let sign_vars =
       let stub =
         foreign (func_name "sign_vars")
@@ -138,13 +117,109 @@ struct
         res
   end
 
+  module Verification_key_data = struct
+    type 'field t_ =
+      { characterizing_up_to_sign : 'field list
+      ; sign : 'field list
+      }
+    [@@deriving sexp]
+
+    let characterizing_length, sign_length =
+      let g1_count = 2 + (1 + Info.input_size) in
+      let g2_count = 3 in
+      let gt_count = 1 in
+      ( g1_count * 1 + g2_count * Info.fqe_size_in_field_elements + gt_count * Info.fqe_size_in_field_elements
+      , g1_count + g2_count + gt_count )
+
+    type t = Field.t t_
+    [@@deriving sexp]
+
+    type var = Field.Checked.t t_
+
+    let bit_length = characterizing_length * Field.size_in_bits + sign_length
+
+    let typ =
+      let open H_list in
+      Typ.of_hlistable
+        [ Typ.list ~length:characterizing_length Field.typ
+        ; Typ.list ~length:sign_length Field.typ
+        ]
+        ~var_to_hlist:(fun { characterizing_up_to_sign; sign } -> [characterizing_up_to_sign; sign])
+        ~value_to_hlist:(fun { characterizing_up_to_sign; sign } -> [characterizing_up_to_sign; sign])
+        ~var_of_hlist:(fun [ characterizing_up_to_sign; sign ] -> {characterizing_up_to_sign; sign})
+        ~value_of_hlist:(fun [ characterizing_up_to_sign; sign ] -> {characterizing_up_to_sign; sign})
+
+    let of_verification_key vk : t =
+      let elts = Verification_key.characterizing_elts_up_to_sign vk in
+      let signs = Verification_key.sign_elts vk in
+      let module V = Field.Vector in
+      assert (V.length elts = characterizing_length);
+      assert (V.length signs = sign_length);
+      { characterizing_up_to_sign =
+          List.init (V.length elts) ~f:(fun i -> V.get elts i)
+      ; sign = 
+          List.init (V.length signs) ~f:(fun i -> V.get signs i)
+      }
+
+    let to_bits ({ characterizing_up_to_sign; sign } as t : t) = 
+      printf !"To bits: %{sexp:t}\n%!" t;
+      let bs1 =
+        (* It's ok to use choose_preimage here *)
+        List.concat_map characterizing_up_to_sign ~f:(fun x ->
+          Field.unpack x)
+      and bs2 =
+        (* but not here *)
+        List.map sign ~f:(fun x ->
+          assert (not (Field.equal Field.zero x));
+          Bigint.test_bit (Bigint.of_field x) 0)
+      in
+      bs1 @ bs2
+
+    module Checked = struct
+      let constant ({ sign; characterizing_up_to_sign } : t) : var =
+        { sign = List.map sign ~f:Field.Checked.constant
+        ; characterizing_up_to_sign = List.map characterizing_up_to_sign ~f:Field.Checked.constant
+        }
+
+      module Assert = struct
+        let equal t1 t2 =
+          let check xs ys =
+            Checked.List.iter (List.zip_exn xs ys) ~f:(fun (x, y) ->
+              Field.Checked.Assert.equal x y)
+          in
+          let open Checked.Let_syntax in
+          let%map () = check t1.characterizing_up_to_sign t2.characterizing_up_to_sign
+          and () = check t1.sign t2.sign
+          in
+          ()
+      end
+
+      let to_bits ({ characterizing_up_to_sign; sign } : var) = 
+        let open Checked.Let_syntax in
+        let%map bs1 =
+          (* It's ok to use choose_preimage here *)
+          Checked.List.map characterizing_up_to_sign ~f:(fun x ->
+            Field.Checked.choose_preimage_var x ~length:Field.size_in_bits)
+          >>| List.concat
+        and bs2 =
+          (* but not here *)
+          Checked.List.map sign ~f:(fun x ->
+            let%bind () = Field.Checked.Assert.non_zero x in
+            Field.Checked.unpack_full x
+            >>| Bitstring_lib.Bitstring.Lsb_first.to_list
+            >>| List.hd_exn)
+        in
+        bs1 @ bs2
+    end
+  end
+
   module Preprocessed_verification_key = struct
     type t = unit ptr
     let typ = ptr void
 
     let prefix =
       with_prefix Prefix.prefix
-        "r1cs_se_ppzksnark_preprocessed_r1cs_se_ppzksnark_verification_key_variable "
+        "r1cs_se_ppzksnark_preprocessed_r1cs_se_ppzksnark_verification_key_variable"
 
     let func_name = with_prefix prefix
 
@@ -582,12 +657,20 @@ struct
           Proof.generate_witness proof w.proof;
           gadget_generate_witness gadget)
     end
-
+    include T
     include Gadget.Make(Impl)(Libsnark)(T)
 
-    let choose_verification_key_bits_and_proof_and_check_result input get_witness =
+    let choose_verification_key_data_and_proof_and_check_result input get_witness =
       let open Let_syntax in
-      let%bind t = create input get_witness in
+      let%map t = create input get_witness in
+      assert (List.length t.vk_characterizing_vars_up_to_sign
+              = Verification_key_data.characterizing_length);
+      assert (List.length t.vk_sign_vars = Verification_key_data.sign_length);
+      ( { Verification_key_data.characterizing_up_to_sign = t.vk_characterizing_vars_up_to_sign
+        ; sign = t.vk_sign_vars
+        }
+      , t.result )
+      (*
       let%map bs1 =
         Checked.List.map t.vk_characterizing_vars_up_to_sign ~f:(fun x ->
           Field.Checked.choose_preimage_var x ~length:Field.size_in_bits)
@@ -598,7 +681,7 @@ struct
           >>| Bitstring_lib.Bitstring.Lsb_first.to_list
           >>| List.hd_exn)
       in
-      (List.concat bs1 @ bs2, t.result)
+      (List.concat bs1 @ bs2, t.result) *)
     ;;
   end
 end
@@ -614,9 +697,12 @@ module Mnt4
     end) =
   Make (Impl)
     (struct let prefix = "camlsnark_mnt4" end)
-    (Libsnark.Mnt4.Default)
-    (Libsnark.Mnt6.Default)
-    (Info)
+    (Libsnark.Mnt4.GM)
+    (Libsnark.Mnt6.GM)
+    (struct
+      include Info
+      let fqe_size_in_field_elements = 3
+    end)
 
 module Mnt6
     (Impl : Snark_intf.S
@@ -628,4 +714,8 @@ module Mnt6
         val input_size : int
     end) =
   Make (Impl) (struct let prefix = "camlsnark_mnt6" end)
-    (Libsnark.Mnt6.Default) (Libsnark.Mnt4.Default) (Info)
+    (Libsnark.Mnt6.GM) (Libsnark.Mnt4.GM)
+    (struct
+      include Info
+      let fqe_size_in_field_elements = 2
+    end)
