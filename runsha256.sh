65ce6e2a974a933bb01a28e52d30313c114c5c36
Izaak Meckler <ihmeckler@gmail.com>
2018-08-16 12:05:44 -0700

    rm runsha256.sh

--- runsha256.sh
+++
@@ -1,6 +0,0 @@
-
-
-python2 /home/evan/projects/nanobit/lib/snarky/src/camlsnark_c/libsnark-caml/libsnark/gadgetlib1/gadgets/hashes/sha256/tests/generate_sha256_gadget_tests.py
-run-in-docker jbuilder b -p digestif-test-ocaml
-run-in-docker jbuilder exec digestif_test
-
