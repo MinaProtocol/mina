fe32c1d27f6fc4bcbe77c70d1132b993e8dfde1c
Izaak Meckler <ihmeckler@gmail.com>
2018-08-18 16:32:49 -0700

    fix mnt6 cpp file

--- lib/snarky/src/camlsnark_c/libsnark-caml/libsnark/caml/caml_mnt6_specific.cpp
+++ lib/snarky/src/camlsnark_c/libsnark-caml/libsnark/caml/caml_mnt6_specific.cpp
@@ -409,7 +409,7 @@ libff::G1<ppT>* camlsnark_mnt6_g1_add (libff::G1<ppT>* a, libff::G1<ppT>* b) {
   return new libff::G1<ppT>((*a) + (*b));
 }
 
-libff::G1<ppT>* camlsnark_mnt6_g1_scale (FieldT *a, libff::G1<ppT>* x) {
+libff::G1<ppT>* camlsnark_mnt6_g1_scale (libff::bigint<libff::mnt6_r_limbs> *a, libff::G1<ppT>* x) {
   return new libff::G1<ppT>((*a) * (*x));
 }
 
