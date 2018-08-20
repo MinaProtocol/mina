ecf7d77d5bdf8d14b52ad88297da191de32f5238
Evan Shapiro <evan@o1labs.org>
2018-08-20 16:16:18 -0700

    add kademlia test

---
+++ app/nanobit/src/coda_peers_test.ml
@@ -0,0 +1,31 @@
+open Core
+open Async
+open Coda_worker
+open Coda_processes
+
+let main () =
+  let%bind program_dir = Unix.getcwd () in
+  let n = 3 in
+  Coda_processes.init () ;
+  Coda_processes.spawn_local_processes_exn n
+    ~program_dir
+    ~f:(fun workers -> 
+        let _, _, expected_peers = Coda_processes.net_configs n in
+        let%bind _ = after (Time.Span.of_sec 10.) in
+        Deferred.all_unit begin
+          List.map2_exn workers expected_peers
+            ~f:(fun worker expected_peers -> 
+                let%bind peers = Coda_process.peers_exn worker in
+                (*Print.printf !"peers: %{sexp: Host_and_port.t list} %{sexp: Host_and_port.t list}\n" peers expected_peers;*)
+                assert (peers = expected_peers);
+                Deferred.unit
+              )
+        end
+      )
+
+let name = "coda-peers-test"
+
+let command =
+  Command.async_spec ~summary:"Simple use of Async Rpc_parallel V2"
+    Command.Spec.(empty)
+    main
