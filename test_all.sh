48eee354e4a090910aafe182e17b9bac0da49c37
Evan Shapiro <evan@o1labs.org>
2018-08-22 15:15:59 -0700

    run tests in parallel

--- test_all.sh
+++ test_all.sh
@@ -3,9 +3,16 @@
 set -eo pipefail
 
 eval `opam config env`
-dune runtest --verbose -j8
 
-dune exec cli -- full-test
+dune runtest --verbose -j8 & p1=$!
 
-dune exec cli -- coda-peers-test
-dune exec cli -- transaction-snark-profiler -check-only
+integration () {
+  dune exec cli -- full-test
+  dune exec cli -- coda-peers-test
+  dune exec cli -- transaction-snark-profiler -check-only
+}
+
+integration & p2=$!
+
+wait $p1
+wait $p2
