ccf626fcfadfe155a3ebcfc8112b43c85638d887
Brandon Kase <bkase@o1labs.org>
2018-08-22 17:53:17 -0700

    Transaction_snark test speedup
    Transaction_snark_profiler basically tests what transaction_snark unit test was
    doing, but faster.

--- test_all.sh
+++ test_all.sh
@@ -8,4 +8,4 @@ dune runtest --verbose -j8
 dune exec cli -- full-test
 
 dune exec cli -- coda-peers-test
-dune exec cli -- transaction-snark-profiler -check-only
+dune exec cli -- transaction-snark-profiler
