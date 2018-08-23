c53c36fa043894f606687441775957e24475b540
Brandon Kase <bkase@o1labs.org>
2018-08-22 18:57:07 -0700

    Explicit k2, warning message

--- test_all.sh
+++ test_all.sh
@@ -8,4 +8,6 @@ dune runtest --verbose -j8
 dune exec cli -- full-test
 
 dune exec cli -- coda-peers-test
-dune exec cli -- transaction-snark-profiler
+
+# This test actually creates SNARKs (is slow)
+dune exec cli -- transaction-snark-profiler -k 2
