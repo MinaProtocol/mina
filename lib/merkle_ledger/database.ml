2806a2426d157792af9777f5696e31240ce8079a
John Wu <johnwu93@gmail.com>
2018-08-23 15:44:08 -0700

    moved ledgers into one file

--- lib/merkle_ledger/database.ml
+++ lib/merkle_ledger/database.ml
@@ -8,7 +8,9 @@ module Make
     (Depth : Intf.Depth)
     (Kvdb : Intf.Key_value_database)
     (Sdb : Intf.Stack_database) : sig
-  include Intf.S with type account := Account.t and type hash := Hash.t
+  include Intf.Database_S
+          with type account := Account.t
+           and type hash := Hash.t
 
   val gen_account_key : key Core.Quickcheck.Generator.t
 end = struct
