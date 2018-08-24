4fea32587a22ea4c3486b426dfddd7ed2b3da148
Nathan Holland <nholland94@gmail.com>
2018-08-24 11:13:35 -0700

    Fix wrong name on environment variable in test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -10,7 +10,7 @@ date
 dune runtest --verbose -j${myprocs}
 
 for consensus_mechanism in proof_of_signature proof_of_stake; do
-  export CONSENSUS_MECHANISM="$consensus_mechanism"
+  export CODA_CONSENSUS_MECHANISM="$consensus_mechanism"
   for test_args in full-test coda-peers-test coda-block-production-test 'transaction-snark-profiler -check-only'; do
     date
     dune exec cli -- $test_args
