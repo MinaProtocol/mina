cded2d47e338ddc31490505d2d06678995bc670b
John Wu <johnwu93@gmail.com>
2018-08-23 17:58:43 -0700

    organized tests

---
+++ lib/merkle_ledger/test_stubs.ml
@@ -0,0 +1,104 @@
+open Core
+open Unsigned
+
+module Balance = struct
+  include UInt64
+  include Binable.Of_stringable (UInt64)
+
+  let equal x y = UInt64.compare x y = 0
+end
+
+module Account = struct
+  (* type balance = Balance.t sexp_opaque [@@deriving bin_io, eq, show, sexp] *)
+  type t =
+    { public_key: string
+    ; balance: Balance.t
+           [@printer
+             fun fmt balance ->
+               Format.pp_print_string fmt (Balance.to_string balance)] }
+  [@@deriving bin_io, eq, show, fields]
+
+  let sexp_of_t {public_key; balance} =
+    [%sexp_of : string * string] (public_key, Balance.to_string balance)
+
+  let t_of_sexp sexp : t =
+    let public_key, string_balance = [%of_sexp : string * string] sexp in
+    let balance = Balance.of_string string_balance in
+    ({public_key; balance} : t)
+
+  let empty = {public_key= ""; balance= Balance.zero}
+
+  let set_balance {public_key; _} balance = {public_key; balance}
+
+  let gen =
+    let open Quickcheck.Let_syntax in
+    let%bind public_key = String.gen in
+    let%map int_balance = Int.gen in
+    let nat_balance = abs int_balance in
+    let balance = Balance.of_int nat_balance in
+    {public_key; balance}
+end
+
+module Hash = struct
+  type t = Md5.t [@@deriving sexp, hash, compare, bin_io, eq]
+
+  (* to prevent pre-image attack,
+   * important impossible to create an account such that (merge a b = hash_account account) *)
+
+  let hash_account account = Md5.digest_string ("0" ^ Account.show account)
+
+  let empty = Md5.digest_string ""
+
+  let merge ~height a b =
+    let res =
+      Md5.digest_string
+        (sprintf "test_ledger_%d:" height ^ Md5.to_hex a ^ Md5.to_hex b)
+    in
+    res
+end
+
+module In_memory_kvdb : Intf.Key_value_database = struct
+  type t = (string, Bigstring.t) Hashtbl.t
+
+  let create ~directory:_ = Hashtbl.create (module String)
+
+  let destroy _ = ()
+
+  let get tbl ~key = Hashtbl.find tbl (Bigstring.to_string key)
+
+  let set tbl ~key ~data = Hashtbl.set tbl ~key:(Bigstring.to_string key) ~data
+
+  let delete tbl ~key = Hashtbl.remove tbl (Bigstring.to_string key)
+end
+
+module In_memory_sdb : Intf.Stack_database = struct
+  type t = Bigstring.t list ref
+
+  let create ~filename:_ = ref []
+
+  let destroy _ = ()
+
+  let push ls v = ls := v :: !ls
+
+  let pop ls =
+    match !ls with
+    | [] -> None
+    | h :: t ->
+        ls := t ;
+        Some h
+
+  let length ls = List.length !ls
+end
+
+module Key = struct
+  module T = struct
+    type t = string [@@deriving sexp, compare, hash, bin_io]
+
+    type key = t [@@deriving sexp, bin_io]
+  end
+
+  let empty = ""
+
+  include T
+  include Hashable.Make_binable (T)
+end
