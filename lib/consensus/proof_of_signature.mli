e8805cf630759c003ebf37f576d4d3f5b822e2f7
Nathan Holland <nholland94@gmail.com>
2018-08-27 16:52:36 -0700

    Remove proof from consensus mechanism functors and signatures (#505)

--- lib/consensus/proof_of_signature.mli
+++ lib/consensus/proof_of_signature.mli
@@ -1,8 +1,4 @@
 module type Inputs_intf = sig
-  module Proof : sig
-    type t [@@deriving bin_io, sexp]
-  end
-
   module Ledger_builder_diff : sig
     type t [@@deriving bin_io, sexp]
   end
@@ -10,8 +6,7 @@ end
 
 module Make (Inputs : Inputs_intf) :
   Mechanism.S
-  with type Proof.t = Inputs.Proof.t
-   and type Internal_transition.Ledger_builder_diff.t =
+  with type Internal_transition.Ledger_builder_diff.t =
               Inputs.Ledger_builder_diff.t
    and type External_transition.Ledger_builder_diff.t =
               Inputs.Ledger_builder_diff.t
--- lib/nanobit_base/snark_transition.ml
+++ lib/nanobit_base/snark_transition.ml
@@ -11,10 +11,6 @@ end
 module type S = sig
   module Consensus_data : Consensus_data_intf
 
-  module Proof : sig
-    type t [@@deriving bin_io, sexp]
-  end
-
   type ('blockchain_state, 'consensus_data) t [@@deriving sexp]
 
   type value = (Blockchain_state.value, Consensus_data.value) t
@@ -41,14 +37,10 @@ module type S = sig
   val genesis : value
 end
 
-module Make
-    (Consensus_data : Consensus_data_intf) (Proof : sig
-        type t [@@deriving bin_io, sexp]
-    end) :
-  S with module Consensus_data = Consensus_data and module Proof = Proof =
+module Make (Consensus_data : Consensus_data_intf) :
+  S with module Consensus_data = Consensus_data =
 struct
   module Consensus_data = Consensus_data
-  module Proof = Proof
 
   type ('blockchain_state, 'consensus_data) t =
     { blockchain_state: 'blockchain_state
