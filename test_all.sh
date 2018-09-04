ca17d3186b969befb0c6a582f8a956e707ea41ea
Joel Krauska <jkrauska@gmail.com>
2018-09-04 18:50:28 -0400

    put tests back in sh

--- test_all.sh
+++ test_all.sh
@@ -4,18 +4,27 @@ set -eo pipefail
 
 eval `opam config env`
 
-# FIXME: Linux specific
-myprocs=`nproc --all`
-date
-dune runtest --verbose -j${myprocs}
+test_runtest() {
+  date
+  myprocs=`nproc --all`  # Linux specific
+  dune runtest --verbose -j${myprocs}
+}
 
-for consensus_mechanism in proof_of_signature proof_of_stake; do
-  export CODA_CONSENSUS_MECHANISM="$consensus_mechanism"
-  for test_args in full-test coda-peers-test coda-block-production-test 'coda-shared-prefix-test -who-proposes 0' 'coda-shared-prefix-test -who-proposes 1' coda-shared-state-test 'transaction-snark-profiler -check-only'; do
->>>>>>> add multiple transaction test
+test_method() {
+  export CODA_CONSENSUS_MECHANISM="$1"
+  for test in full-test coda-peers-test coda-block-production-test 'coda-shared-prefix-test -who-proposes 0' 'coda-shared-prefix-test -who-proposes 1' coda-shared-state-test 'transaction-snark-profiler -check-only'; do
     date
-    dune exec cli -- $test_args
+    dune exec cli -- $test
   done
-done
+}
 
-date
+main() {
+  test_runtest
+  test_method 'proof_of_signature'
+  test_method 'proof_of_stake'
+}
+
+# Only run main if called directly
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
