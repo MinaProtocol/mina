32517ad63ccdd7f13cd7bfe27f34587198dfc9c7
Nathan Holland <nholland94@gmail.com>
2018-09-05 16:47:42 -0700

    Scaffold full vrf instantiation in proof of stake

--- lib/consensus/proof_of_stake.ml
+++ lib/consensus/proof_of_stake.ml
@@ -88,20 +88,6 @@ let uint32_of_int64 x = x |> Int64.to_int64 |> UInt32.of_int64
 
 let int64_of_uint32 x = x |> UInt32.to_int64 |> Int64.of_int64
 
-module Vrf =
-  Vrf_lib.Integrated.Make (Snark_params.Tick)
-    (struct
-      type var =
-        Snark_params.Tick.Boolean.var Bitstring_lib.Bitstring.Lsb_first.t
-    end)
-    (struct
-      include Snark_params.Tick.Inner_curve
-      include Checked
-
-      let scale_generator shifted s ~init =
-        scale_known shifted Snark_params.Tick.Inner_curve.one s ~init
-    end)
-
 module Make (Inputs : Inputs_intf) :
   Mechanism.S
   with type Internal_transition.Ledger_builder_diff.t =
@@ -227,6 +213,115 @@ struct
       in
       (epoch, slot)
   end
+
+  module Vrf_scalar = struct
+    type value =
+      Crypto_params.Tock_curve.Field.t (* TODO? *)
+
+    type var =
+      Snark_params.Tick.Boolean.var Bitstring_lib.Bitstring.Lsb_first.t
+  end
+
+  module Vrf_group = struct
+    open Snark_params.Tick
+
+    type value = Inner_curve.t
+    type var = Inner_curve.var
+
+    let scale = Inner_curve.scale
+
+    module Checked = struct
+      include Inner_curve.Checked
+
+      let scale_generator shifted s ~init =
+        scale_known shifted Inner_curve.one s ~init
+    end
+  end
+
+  module Vrf_message = struct
+    type ('epoch, 'slot, 'epoch_seed, 'ledger_hash) t =
+      { epoch: 'epoch
+      ; slot: 'slot
+      ; last_epoch_seed: 'epoch_seed
+      ; last_epoch_ledger_hash: 'ledger_hash }
+
+    type value = (Epoch.t, Epoch.Slot.t, Epoch_seed.t, Nanobit_base.Ledger_hash.t) t
+    type var = (Epoch.Unpacked.var, Epoch.Slot.Unpacked.var, Epoch_seed.var, Nanobit_base.Ledger_hash.var) t
+
+    let fold {epoch; slot; last_epoch_seed; last_epoch_ledger_hash} =
+      let open Fold in
+      Epoch.fold epoch
+      +> Epoch.Slot.fold slot
+      +> Epoch_seed.fold last_epoch_seed
+      +> Nanobit_base.Ledger_hash.fold last_epoch_ledger_hash
+
+    let hash_to_group msg =
+      let msg_hash_state =
+        Snark_params.Tick.Pedersen.hash_fold
+          Nanobit_base.Hash_prefix.vrf_message
+          (fold msg)
+      in
+      msg_hash_state.acc
+
+    module Checked = struct
+      let var_to_triples {epoch; slot; last_epoch_seed; last_epoch_ledger_hash} =
+        let open Snark_params.Tick.Let_syntax in
+        let%map epoch_seed_triples = Epoch_seed.var_to_triples last_epoch_seed
+        and last_epoch_ledger_hash_triples = Nanobit_base.Ledger_hash.var_to_triples last_epoch_ledger_hash
+        in
+        Epoch.Unpacked.var_to_triples epoch
+        @ Epoch.Slot.Unpacked.var_to_triples slot
+        @ epoch_seed_triples
+        @ last_epoch_ledger_hash_triples
+
+      let hash_to_group msg =
+        let open Snark_params.Tick in
+        let open Snark_params.Tick.Let_syntax in
+        let%bind msg_triples = var_to_triples msg in
+        Pedersen.Checked.hash_triples
+          ~init:Nanobit_base.Hash_prefix.vrf_message
+          msg_triples
+    end
+  end
+
+  module Vrf_output = struct
+    type value = Sha256.Digest.t
+    type var = Sha256.Digest.var
+
+    let hash msg g =
+      let open Fold in
+      let compressed_g = Non_zero_curve_point.(g |> of_inner_curve |> compress)
+      let digest =
+        Snark_params.Tick.Pedersen.digest_fold
+          Nanobit_base.Hash_prefix.vrf_output
+          (Vrf_message.fold msg +> Non_zero_curve_point.Compressed.fold compressed_g)
+      in
+      Sha256.digest (Snark_params.Tick.Pedersen.Digest.Bits.to_bits digest)
+        |> Non_zero_curve_point.Compressed.of_bits
+        |> Non_zero_curve_point.decompress
+        |> Non_zero_curve_point.to_inner_curve
+
+    module Checked = struct
+      let hash msg g =
+        let open Snark_params.Tick.Let_syntax in
+        let%bind msg_triples = Vrf_message.Checked.var_to_triples msg in
+        let%bind g_triples = Non_zero_curve_point.(compress_checked g >>= Compressed.var_to_triples) in
+        let%bind pedersen_digest =
+          Snark_params.Tick.Pedersen.Checked.digest_triples
+            ~init:Nanobit_base.Hash_prefix.vrf_output
+            (msg_triples @ g_triples)
+          >>= Snark_params.Tick.Pedersen.Checked.Digest.choose_preimage
+        in
+        Sha256.Checked.digest (pedersen_digest :> Snark_params.Tick.Boolean.var list)
+    end
+  end
+
+  module Vrf = Vrf_lib.Integrated.Make
+    (Snark_params.Tick)
+    (Vrf_scalar)
+    (Vrf_group)
+    (Vrf_message)
+    (Vrf_output)
 
   module Epoch_ledger = struct
     type ('ledger_hash, 'amount) t =
--- lib/vrf_lib/integrated.ml
+++ lib/vrf_lib/integrated.ml
@@ -1,11 +1,23 @@
 module Make
-    (Impl : Snarky.Snark_intf.S) (Scalar : sig
-        type var
-    end) (Group : sig
-      open Impl
+  (Impl : Snarky.Snark_intf.S)
+  (Scalar : sig
+      type value
 
       type var
+  end)
+  (Group : sig
+    open Impl
 
+    type value
+
+    type var
+
+    val scale :
+         value
+      -> Scalar.value
+      -> value
+
+    module Checked : sig
       module Shifted : sig
         module type S =
           Snarky.Curves.Shifted_intf
@@ -28,49 +40,77 @@ module Make
         -> Scalar.var
         -> init:'shifted
         -> ('shifted, _) Checked.t
-    end) (Message : sig
-      type var
+    end
+  end)
+  (Message : sig
+    type value
 
+    type var
+
+    val hash_to_group : value -> Group.value
+
+    module Checked : sig
       val hash_to_group : var -> (Group.var, _) Impl.Checked.t
-    end) (Output_hash : sig
-      type var
+    end
+  end)
+  (Output_hash : sig
+    type value
 
+    type var
+
+    val hash : Message.value -> Group.value -> value
+
+    module Checked : sig
       val hash : Message.var -> Group.var -> (var, _) Impl.Checked.t
-    end) : sig
-  val eval :
-       'shifted Group.Shifted.m
-    -> private_key:Scalar.var
-    -> Message.var
-    -> (Output_hash.var, _) Impl.Checked.t
+    end
+  end)
+: sig
+  val eval : private_key:Scalar.value -> Message.value -> Output_hash.value
 
-  val eval_and_check_public_key :
-       'shifted Group.Shifted.m
-    -> private_key:Scalar.var
-    -> public_key:Group.var
-    -> Message.var
-    -> (Output_hash.var, _) Impl.Checked.t
+  module Checked : sig
+    val eval :
+         'shifted Group.Checked.Shifted.m
+      -> private_key:Scalar.var
+      -> Message.var
+      -> (Output_hash.var, _) Impl.Checked.t
+
+    val eval_and_check_public_key :
+         'shifted Group.Checked.Shifted.m
+      -> private_key:Scalar.var
+      -> public_key:Group.var
+      -> Message.var
+      -> (Output_hash.var, _) Impl.Checked.t
+  end
 end = struct
   open Impl
-  open Let_syntax
 
-  let eval (type shifted) ((module Shifted): shifted Group.Shifted.m)
-      ~private_key m =
-    let%bind h = Message.hash_to_group m in
-    let%bind u =
-      (* This use of unshift_nonzero is acceptable since if h^private_key = 0 then
-         the prover had a bad private key *)
-      Group.scale (module Shifted) h private_key ~init:Shifted.zero
-      >>= Shifted.unshift_nonzero
-    in
+  let eval ~private_key m =
+    let h = Message.hash_to_group m in
+    let u = Group.scale h private_key in
     Output_hash.hash m u
 
-  let eval_and_check_public_key (type shifted)
-      ((module Shifted): shifted Group.Shifted.m) ~private_key ~public_key
-      message =
-    let%bind () =
-      let%bind public_key_shifted = Shifted.(add zero public_key) in
-      Group.scale_generator (module Shifted) private_key ~init:Shifted.zero
-      >>= Shifted.Assert.equal public_key_shifted
-    in
-    eval (module Shifted) ~private_key message
+  module Checked = struct
+    open Let_syntax
+
+    let eval (type shifted) ((module Shifted): shifted Group.Checked.Shifted.m)
+        ~private_key m =
+      let%bind h = Message.Checked.hash_to_group m in
+      let%bind u =
+        (* This use of unshift_nonzero is acceptable since if h^private_key = 0 then
+           the prover had a bad private key *)
+        Group.Checked.scale (module Shifted) h private_key ~init:Shifted.zero
+        >>= Shifted.unshift_nonzero
+      in
+      Output_hash.Checked.hash m u
+
+    let eval_and_check_public_key (type shifted)
+        ((module Shifted): shifted Group.Checked.Shifted.m) ~private_key ~public_key
+        message =
+      let%bind () =
+        let%bind public_key_shifted = Shifted.(add zero public_key) in
+        Group.Checked.scale_generator (module Shifted) private_key ~init:Shifted.zero
+        >>= Shifted.Assert.equal public_key_shifted
+      in
+      eval (module Shifted) ~private_key message
+  end
 end
