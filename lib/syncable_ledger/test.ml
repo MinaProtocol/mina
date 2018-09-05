e11bca0df79a31436fdc98bd6eb69dc104aed876
John Wu <johnwu93@gmail.com>
2018-09-04 22:04:27 -0700

    One off error (#545)

--- lib/syncable_ledger/test.ml
+++ lib/syncable_ledger/test.ml
@@ -156,7 +156,7 @@ end))
 module TestDB3_8 = Make (Test_db.Make (struct
   let depth = 3
 
-  let num_accts = 7
+  let num_accts = 8
 end))
 
 module TestDB16_20 = Make (Test_db.Make (struct
@@ -171,14 +171,14 @@ module TestDB16_1024 = Make (Test_db.Make (struct
   let num_accts = 1024
 end))
 
-module TestDB16_1025 = Make (Test_db.Make (struct
+module TestDB16_1026 = Make (Test_db.Make (struct
   let depth = 16
 
-  let num_accts = 80
+  let num_accts = 1026
 end))
 
 module TestDB16_65536 = Make (Test_db.Make (struct
   let depth = 16
 
-  let num_accts = 65535
+  let num_accts = 65536
 end))
