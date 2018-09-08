508714af854b68082968cca919700eb280c3db40
Izaak Meckler <ihmeckler@gmail.com>
2018-09-08 11:35:32 -0700

    add sexp functions to snark work lib

--- lib/snark_work_lib/work.ml
+++ lib/snark_work_lib/work.ml
@@ -5,7 +5,7 @@ module Single = struct
     type ('statement, 'transition, 'sparse_ledger, 'ledger_proof) t =
       | Transition of 'statement * 'transition * 'sparse_ledger
       | Merge of 'statement * 'ledger_proof * 'ledger_proof
-    [@@deriving bin_io]
+    [@@deriving bin_io, sexp]
 
     let statement = function Transition (s, _, _) -> s | Merge (s, _, _) -> s
   end
@@ -13,7 +13,7 @@ end
 
 module Spec = struct
   type 'single t = {instances: 'single list; fee: Currency.Fee.Stable.V1.t}
-  [@@deriving bin_io, fields]
+  [@@deriving bin_io, fields, sexp]
 end
 
 module Result = struct
@@ -21,7 +21,7 @@ module Result = struct
     { proofs: 'single list
     ; spec: 'spec
     ; prover: Signature_lib.Public_key.Compressed.t }
-  [@@deriving bin_io, fields]
+  [@@deriving bin_io, fields, sexp]
 end
 
 let proofs_per_work = 2
