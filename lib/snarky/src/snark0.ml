e17984e84a2e4b31f9241d1b04fa7a4591916f6f
Izaak Meckler <ihmeckler@gmail.com>
2018-09-12 21:38:58 -0700

    _

--- lib/snarky/src/snark0.ml
+++ lib/snarky/src/snark0.ml
@@ -1134,18 +1134,6 @@ module Make_basic (Backend : Backend_intf.S) = struct
         let%map () = assert_square x z in
         z)
 
-    let div ?(label= "Checked.div") x y =
-      with_label label
-        (let open Let_syntax in
-        let%bind z =
-          provide_witness Typ.field
-            (let open As_prover.Let_syntax in
-            let%map x = As_prover.read_var x and y = As_prover.read_var y in
-            Field0.div x y)
-        in
-        let%map () = assert_r1cs y z x in
-        z)
-
     (* We get a better stack trace by failing at the call to is_satisfied, so we
      put a bogus value for the inverse to make the constraint system unsat if
      x is zero. *)
@@ -1163,6 +1151,12 @@ module Make_basic (Backend : Backend_intf.S) = struct
           assert_r1cs ~label:"field_inverse" x x_inv (Cvar.constant Field.one)
         in
         x_inv)
+
+    let div ?(label= "Checked.div") x y =
+      with_label label
+        (let open Let_syntax in
+        let%bind y_inv = inv y in
+        mul x y_inv)
 
     let assert_non_zero (v: Cvar.t) =
       with_label __LOC__
