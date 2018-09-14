d04747a84faf60277d7b36dfd4069486dc1cd2de
Evan <evan.a.shapiro@gmail.com>
2018-09-13 17:35:53 -0700

    base layer for making / testing testnets (#618)
    * add functors
    
    * implement some of api with failwiths
    
    * add necessary components for testing sharing a prefix
    
    * replace shared prefix test
    
    * add proposal_interval to coda worker testnet
    
    * reformat
    
    * do some changes
    
    * reformat
    
    * change api to follow module pattern
    
    * fix shutdown testnet flow
    
    * reformat

--- app/nanobit/src/coda_shared_prefix_test.ml
+++ app/nanobit/src/coda_shared_prefix_test.ml
@@ -11,78 +11,23 @@ module Make
 struct
   module Coda_processes = Coda_processes.Make (Ledger_proof) (Kernel) (Coda)
   open Coda_processes
+  module Coda_worker_testnet =
+    Coda_worker_testnet.Make (Ledger_proof) (Kernel) (Coda)
 
   let name = "coda-shared-prefix-test"
 
   let main who_proposes proposal_interval () =
-    let%bind program_dir = Unix.getcwd () in
-    let n = 2 in
     let log = Logger.create () in
     let log = Logger.child log name in
-    Coda_processes.init () ;
-    Coda_processes.spawn_local_processes_exn n ?proposal_interval ~program_dir
-      ~should_propose:(fun i -> i = who_proposes)
-      ~snark_worker_public_keys:None
-      ~f:(fun workers ->
-        let chains = Array.init (List.length workers) ~f:(fun _ -> []) in
-        let check_chains () =
-          let lengths =
-            Array.to_list (Array.map chains ~f:(fun c -> List.length c))
-          in
-          let first = chains.(0) in
-          let rest = Array.slice chains 1 0 in
-          let newest_shared =
-            List.find first ~f:(fun x ->
-                Array.for_all rest ~f:(fun c ->
-                    List.exists c ~f:(fun y -> x = y) ) )
-          in
-          let shared_idx =
-            match newest_shared with
-            | None -> List.length first
-            | Some shared ->
-                Option.value_exn
-                  (Option.map
-                     (List.findi first ~f:(fun _ x -> x = shared))
-                     ~f:(fun x -> fst x))
-          in
-          let shared_prefix_dist = shared_idx in
-          Logger.info log
-            !"lengths: %{sexp: int list} shared_prefix: %{sexp: string \
-              option} shared_prefix_dist: %d"
-            lengths newest_shared shared_prefix_dist ;
-          assert (shared_prefix_dist <= 5) ;
-          if Array.fold ~init:0 (Array.map chains List.length) ~f:Int.max > 30
-          then exit 0
-          else Deferred.unit
-        in
-        let%bind () =
-          Deferred.List.all_unit
-            (List.mapi workers ~f:(fun i worker ->
-                 let%bind strongest_ledgers =
-                   Coda_process.strongest_ledgers_exn worker
-                 in
-                 don't_wait_for
-                   (Linear_pipe.iter strongest_ledgers ~f:(fun (prev, curr) ->
-                        let bits_to_str b =
-                          let str =
-                            String.concat
-                              (List.map b ~f:(fun x -> if x then "1" else "0"))
-                          in
-                          let hash = Md5.digest_string str in
-                          Md5.to_hex hash
-                        in
-                        let prev_str = bits_to_str prev in
-                        let curr_str = bits_to_str curr in
-                        let chain = chains.(i) in
-                        let chain = curr_str :: chain in
-                        chains.(i) <- chain ;
-                        let%bind () = check_chains () in
-                        Logger.debug log "%d got tip %s %s" i prev_str curr_str ;
-                        return () )) ;
-                 Deferred.unit ))
-        in
-        let%bind () = after (Time.Span.of_sec 1000000.) in
-        return () )
+    let n = 2 in
+    let should_propose i = i = who_proposes in
+    let snark_work_public_keys i = None in
+    let%bind testnet =
+      Coda_worker_testnet.test log n ?proposal_interval should_propose
+        snark_work_public_keys
+    in
+    let%bind () = after (Time.Span.of_sec 30.) in
+    Coda_worker_testnet.Api.shutdown_testnet testnet
 
   let command =
     let open Command.Let_syntax in
