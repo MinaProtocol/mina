0c6b95ac3384b834c0fbfcef20f556254f0881c6
Izaak Meckler <ihmeckler@gmail.com>
2018-09-14 11:16:04 -0700

    remove cryptokit, replace uses with digestif

--- lib/ledger_builder/ledger_builder.ml
+++ lib/ledger_builder/ledger_builder.ml
@@ -193,12 +193,9 @@ end = struct
     [@@deriving sexp, bin_io]
 
     let hash_to_string scan_state =
-      let h =
-        Parallel_scan.State.hash scan_state
-          (Binable.to_string (module Snark_with_statement))
-          (Binable.to_string (module Super_transaction_with_witness))
-      in
-      h#result
+      (Parallel_scan.State.hash scan_state
+        (Binable.to_string (module Snark_with_statement))
+        (Binable.to_string (module Super_transaction_with_witness)) :> string)
 
     let hash t = Ledger_builder_aux_hash.of_bytes (hash_to_string t)
   end
