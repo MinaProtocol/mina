59b2a899683f82be20ddeff44d52f405e9823de5
Brandon Kase <brandon.kase@gmail.com>
2018-09-15 20:17:53 -0700

    Git SHA1 in client status (#677)
    Introduced a Git_sha.t while keeping update check behavior the same.
    Passed commit_id through to coda_main so we can return it in status.

--- app/nanobit/src/full_test.ml
+++ app/nanobit/src/full_test.ml
@@ -27,6 +27,8 @@ let run_test (type ledger_proof) (with_snark: bool) (module Kernel
     let genesis_proof = Precomputed_values.base_proof
 
     let transaction_capacity_log_2 = 3
+
+    let commit_id = None
   end in
   let%bind (module Init) = make_init (module Config) (module Kernel) in
   let module Main = Coda.Make (Init) () in
