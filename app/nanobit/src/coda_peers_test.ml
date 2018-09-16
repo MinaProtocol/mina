6d26f84ce1ce18ba2358d9b47fd444c8b33bf4fa
Evan Shapiro <evan@o1labs.org>
2018-09-16 12:07:58 -0700

    revert test to real contents

--- app/nanobit/src/coda_peers_test.ml
+++ app/nanobit/src/coda_peers_test.ml
@@ -16,21 +16,6 @@ struct
 
   let main () =
     let%bind program_dir = Unix.getcwd () in
-    let n = 1 in
-    let log = Logger.create () in
-    let log = Logger.child log name in
-    Coda_processes.init () ;
-    Logger.info log "A";
-    let configs = Coda_processes.local_configs  n ~program_dir
-        ~snark_worker_public_keys:None ~should_propose:(Fn.const false)
-    in
-    let config = List.hd_exn configs in
-    Logger.info log "B";
-    let%bind worker = Coda_process.spawn_exn config in
-    Logger.info log "C";
-    after (Time.Span.of_sec 10.)
-    (*
-    let%bind program_dir = Unix.getcwd () in
     let n = 3 in
     let log = Logger.create () in
     let log = Logger.child log name in
@@ -53,7 +38,7 @@ struct
             assert (
               S.equal
                 (S.of_list (peers |> List.map ~f:fst))
-                (S.of_list expected_peers) )))*)
+                (S.of_list expected_peers) )))
 
   let command =
     Command.async_spec ~summary:"Simple use of Async Rpc_parallel V2"
