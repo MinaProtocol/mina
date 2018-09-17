5c0da5b299df63272917cae8e21d869af4b2e096
Brandon Kase <brandon.kase@gmail.com>
2018-09-17 12:00:06 -0700

    Better logging around networking related code (#693)

--- lib/network_pool/snark_pool_diff.ml
+++ lib/network_pool/snark_pool_diff.ml
@@ -2,23 +2,24 @@ open Core_kernel
 open Async
 
 type ('work, 'priced_proof) diff = Add_solved_work of 'work * 'priced_proof
-[@@deriving bin_io]
+[@@deriving bin_io, sexp]
 
 module Make (Proof : sig
   type t [@@deriving bin_io]
 end) (Fee : sig
-  type t [@@deriving bin_io]
+  type t [@@deriving bin_io, sexp]
 end) (Work : sig
-  type t [@@deriving bin_io]
+  type t [@@deriving bin_io, sexp]
 end)
 (Pool : Snark_pool.S
         with type work := Work.t
          and type proof := Proof.t
          and type fee := Fee.t) =
 struct
-  type priced_proof = {proof: Proof.t; fee: Fee.t} [@@deriving bin_io]
+  type priced_proof = {proof: Proof.t sexp_opaque; fee: Fee.t}
+  [@@deriving bin_io, sexp]
 
-  type t = (Work.t, priced_proof) diff [@@deriving bin_io]
+  type t = (Work.t, priced_proof) diff [@@deriving bin_io, sexp]
 
   let apply (pool: Pool.t) (t: t) : t Or_error.t Deferred.t =
     let to_or_error = function
