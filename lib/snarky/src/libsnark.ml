47faca0a3d8e9ee6d3bf558d5e0875978e9782f0
Nathan Holland <nholland94@gmail.com>
2018-09-19 14:22:59 -0700

    Reformat

--- lib/snarky/src/libsnark.ml
+++ lib/snarky/src/libsnark.ml
@@ -1,35 +1,40 @@
-[%%import "../../../../../config.mlh"]
+[%%import
+"../../../../../config.mlh"]
 
 open Core
 open Ctypes
 open Foreign
 
-[%%if profile_ffi]
+[%%if
+profile_ffi]
+
 let genesis = Time.now ()
+
 let epoch = ref 0
 
 module Call_ring = struct
   type t = string * int ref * int array
+
   let window = 8
+
   let create name = (name, ref 0, Array.create ~len:window 0)
 
   let report (name, epoch, cs) =
-    Printf.printf !"$$$ %s@%d: [%s]\n%!"
-      name
-      !epoch
-      (String.concat ~sep:","
-         (Array.to_list (Array.map cs ~f:string_of_int)))
+    Printf.printf !"$$$ %s@%d: [%s]\n%!" name !epoch
+      (String.concat ~sep:"," (Array.to_list (Array.map cs ~f:string_of_int)))
 
   let record (name, epoch, cs) =
     let now = Time.now () in
     let span_since_genesis = Time.diff now genesis in
-    let mins_since_genesis = int_of_float (Time.Span.to_min span_since_genesis) in
+    let mins_since_genesis =
+      int_of_float (Time.Span.to_min span_since_genesis)
+    in
     let curr_epoch = mins_since_genesis / window in
     let curr_index = mins_since_genesis mod window in
-    (if curr_epoch > !epoch then (
-      report (name, epoch, cs);
-      epoch := curr_epoch;
-      Array.fill cs ~pos:0 ~len:(Array.length cs) 0));
+    if curr_epoch > !epoch then (
+      report (name, epoch, cs) ;
+      epoch := curr_epoch ;
+      Array.fill cs ~pos:0 ~len:(Array.length cs) 0 ) ;
     cs.(curr_index) <- cs.(curr_index) + 1
 end
 
@@ -37,17 +42,17 @@ let rings = String.Table.create ()
 
 let foreign name t =
   let f = foreign name t in
-  (fun x ->
+  fun x ->
     let ring =
       match Hashtbl.find rings name with
       | Some ring -> ring
       | None ->
           let ring = Call_ring.create name in
-          Hashtbl.set rings ~key:name ~data:ring;
+          Hashtbl.set rings ~key:name ~data:ring ;
           ring
     in
-    Call_ring.record ring;
-    f x)
+    Call_ring.record ring ; f x
+
 [%%endif]
 
 let with_prefix prefix s = sprintf "%s_%s" prefix s
