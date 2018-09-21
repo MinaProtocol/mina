fcd6e8136039bdc46573cb69b59adc38e8476a54
Izaak Meckler <ihmeckler@gmail.com>
2018-09-21 08:53:57 -0700

    update lite params

--- lib/lite_params/gen/gen.ml
+++ lib/lite_params/gen/gen.ml
@@ -35,19 +35,7 @@ let wrap_vk ~loc =
         let input_size = 1
       end)
       (Snark_params.Tick.Inner_curve) in
-  let native_bits =
-    V.Verification_key_data.(to_bits (full_data_of_verification_key vk))
-  in
   let vk = Lite_compat.verification_key vk in
-  let caml_bits =
-    Fold_lib.Fold.to_list
-      (Snarkette.Mnt6.Groth_maller.Verification_key.fold_bits vk)
-  in
-  let bitstring bs =
-    List.map bs ~f:(fun b -> if b then '1' else '0') |> String.of_char_list
-  in
-  printf "Native: %s\n" (bitstring native_bits) ;
-  printf "Caml: %s\n" (bitstring caml_bits) ;
   let module E = Ppxlib.Ast_builder.Make (struct
     let loc = loc
   end) in
@@ -63,6 +51,51 @@ let wrap_vk ~loc =
                    (module Snarkette.Mnt6.Groth_maller.Verification_key)
                    vk))])]
 
+module Proof_of_signature = Consensus.Proof_of_signature.Make(struct
+    module Time = Coda_base.Block_time
+    let proposal_interval = Time.Span.of_ms Int64.zero
+    module Ledger_builder_diff = Unit
+  end)
+
+let protocol_state (s : Proof_of_signature.Protocol_state.value) : Lite_base.Protocol_state.t =
+  let open Proof_of_signature in
+  let consensus_state =
+    Protocol_state.consensus_state s
+    |> Option.value_exn Consensus_state.to_lite
+  in
+  { Lite_base.Protocol_state.previous_state_hash =
+      Lite_compat.digest
+        (Protocol_state.previous_state_hash s :> Snark_params.Tick.Pedersen.Digest.t)
+  ; blockchain_state =
+      Lite_compat.blockchain_state (Protocol_state.blockchain_state s)
+  ; consensus_state
+  }
+
+let genesis ~loc =
+  let module E = Ppxlib.Ast_builder.Make (struct
+    let loc = loc
+  end) in
+  let open E in
+  let protocol_state = protocol_state Proof_of_signature.genesis_protocol_state in
+  let ledger =
+    Sparse_ledger_lib.Sparse_ledger.of_hash ~depth:0
+      protocol_state.blockchain_state.ledger_builder_hash.ledger_hash
+  in
+  let proof =
+    Lite_compat.proof Precomputed_values.base_proof
+  in
+  let chain = { Lite_base.Lite_chain.protocol_state; ledger; proof } in
+  [%expr
+    Core_kernel.Binable.of_string
+      (module Lite_base.Lite_chain)
+      (B64.decode
+         [%e
+           estring
+             (B64.encode
+                (Binable.to_string
+                   (module Lite_base.Lite_chain)
+                   chain))])]
+
 open Async
 
 let main () =
@@ -75,7 +108,9 @@ let main () =
     [%str
       let pedersen_params = [%e pedersen_params ~loc]
 
-      let wrap_vk = [%e wrap_vk_expr]]
+      let wrap_vk = [%e wrap_vk_expr]
+
+      let genesis_chain = [%e genesis ~loc]]
   in
   Pprintast.top_phrase fmt (Ptop_def structure) ;
   exit 0
