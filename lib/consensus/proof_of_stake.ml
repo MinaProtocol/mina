6935507b9b8e429ab0dab9b50870519f50b85652
John Wu <johnwu93@gmail.com>
2018-09-25 14:59:45 -0700

    Merge branch 'master' into feature/s3-pipe

--- lib/consensus/proof_of_stake.ml
+++ lib/consensus/proof_of_stake.ml
@@ -895,6 +895,8 @@ struct
       ; curr_epoch_data }
 
     let length (t: value) = t.length
+
+    let to_lite = None
   end
 
   module Protocol_state = Coda_base.Protocol_state.Make (Consensus_state)
