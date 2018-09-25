6d1d30554075b008e100116230906f57aceb7878
Nathan Holland <nholland94@gmail.com>
2018-09-24 19:53:37 -0700

    Revert "Proof of stake proposer rewrite (#743)" (#771)
    This reverts commit f2960ec58d01096092555ac3b656d7b5ba151478.

--- app/cli/src/coda_main.ml
+++ app/cli/src/coda_main.ml
@@ -159,6 +159,8 @@ module type Config_intf = sig
 
   val lbc_tree_max_depth : [`Infinity | `Finite of int]
 
+  val transition_interval : Time.Span.t
+
   val keypair : Keypair.t
 
   val genesis_proof : Snark_params.Tock.Proof.t
@@ -655,6 +657,10 @@ struct
           (Init.Consensus_mechanism.Internal_transition.snark_transition
              transition)
         >>| fun {Init.Blockchain.proof; _} -> proof
+    end
+
+    module Proposal_interval = struct
+      let t = Time.Span.of_time_span Init.transition_interval
     end
   end)
 
