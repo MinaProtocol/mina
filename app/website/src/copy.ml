b4fbf4175cac9875cdc3a792c0a8ee051331aed8
Evan Shapiro <evan@o1labs.org>
2018-09-29 17:16:11 -0700

    add copy module

---
+++ app/website/src/copy.ml
@@ -0,0 +1,30 @@
+
+
+let intro_slide = [
+          "This is a fully-verifying state explorer for the Coda testnet. \
+           Coda is so lightweight, it can even run in your browser."
+        ; "On this page you can learn more about Coda, the testnet, \
+           and how the protocol works."
+        ]
+
+let problem_slide = [ "Coda uses recursive composition of zk-SNARKs to compress \
+                  the whole\n                \
+                  blockchain down to the size of a few tweets."
+               ; "No one needs to store\n                \
+                  or download transaction history in order to verify the \
+                  blockchain." ]
+
+let coda_slide_1 = [
+          "In contrast, the resource requirments for verifying \
+           Coda are very low and don't increase with time."
+          ; "Coda is so efficient it can even run in your browser."]
+
+let coda_slide_2 = [
+          "Because of its efficiency, \
+           Coda stays decentralized and in the hands of its users, even at scale."]
+
+let conclusion = [ "Below is the live state of the Coda protocol testnet.
+            Your browser is fully verifying the state's integrity, and is essentially
+            acting as a full node."
+
+; "Coda puts its users back in control of their cryptocurrency. It's our first step towards building computer systems that put users back in control of their digital lives." ]
