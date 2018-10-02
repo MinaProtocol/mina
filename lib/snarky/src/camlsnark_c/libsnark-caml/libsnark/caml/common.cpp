2219f6ce65a34105b9c18a0fff321dc2e560331d
Izaak Meckler <ihmeckler@gmail.com>
2018-10-02 11:33:45 -0700

    turn off profiling in libsnark (#846)

--- lib/snarky/src/camlsnark_c/libsnark-caml/libsnark/caml/common.cpp
+++ lib/snarky/src/camlsnark_c/libsnark-caml/libsnark/caml/common.cpp
@@ -5,8 +5,14 @@
 #include <libff/algebra/curves/mnt/mnt6/mnt6_g1.hpp>
 #include <libff/algebra/curves/mnt/mnt4/mnt4_pp.hpp>
 #include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
+#include <libff/common/profiling.hpp>
 
 extern "C" {
+
+void camlsnark_set_profiling(bool b) {
+  libff::inhibit_profiling_counters = b;
+  libff::inhibit_profiling_info = b;
+}
 
 const libff::bigint<libff::mnt6_q_limbs>* camlsnark_mnt6_final_exponent_last_chunk_abs_of_w0 =
   &libff::mnt6_final_exponent_last_chunk_abs_of_w0;
