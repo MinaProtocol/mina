a593d4f07e716855008a396823e24fbf63190567
John Wu <johnwu93@gmail.com>
2018-10-09 14:48:38 -0700

    reformat

--- lib/file_system/file_system.ml
+++ lib/file_system/file_system.ml
@@ -3,7 +3,7 @@ open Async
 let create_dir dir = Unix.mkdir dir ~p:()
 
 let remove_dir dir =
-  let%bind _ = Process.run_exn ~prog:"rm" ~args:(["-rf"; dir] ) () in
+  let%bind _ = Process.run_exn ~prog:"rm" ~args:["-rf"; dir] () in
   Deferred.unit
 
 let try_finally ~(f: unit -> unit Deferred.t) ~finally =
@@ -12,9 +12,7 @@ let try_finally ~(f: unit -> unit Deferred.t) ~finally =
 
 let with_temp_dir ~f dir =
   let%bind temp_dir = Async.Unix.mkdtemp dir in
-  try_finally
-    ~f:(fun () -> f temp_dir)
-    ~finally:(fun () -> remove_dir temp_dir)
+  try_finally ~f:(fun () -> f temp_dir) ~finally:(fun () -> remove_dir temp_dir)
 
 let dup_stdout ?(f= Core.Fn.id) (process: Process.t) =
   Pipe.transfer ~f
