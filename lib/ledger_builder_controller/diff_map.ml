cf13ee660a77fd8e562b2b737146a8769de846b4
Paul Steckler <psteckler@o1labs.org>
2018-10-09 18:04:19 -0700

    propagate overflow error; use Core.Hashtbl; some renaming

--- lib/ledger_builder_controller/diff_map.ml
+++ lib/ledger_builder_controller/diff_map.ml
@@ -1,6 +1,8 @@
 (* diff_map.ml -- map to represent difference between locked, best tip *)
 
-module type MapWithGroupOpsS = sig
+open Core
+
+module type S = sig
   type key
 
   type value
@@ -19,7 +21,7 @@ module type MapWithGroupOpsS = sig
 
   val identity : unit -> (key, value) t
 
-  val add : (key, value) t -> (key, value) t -> (key, value) t
+  val add : (key, value) t -> (key, value) t -> (key, value) t Or_error.t
 
   val inverse : (key, value) t -> (key, value) t
 
@@ -31,70 +33,77 @@ end
 module Test = struct
   module Amount = Currency.Amount.Signed
 
-  module UserAmountMap :
-    MapWithGroupOpsS with type key = string and type value = Amount.t =
+  module User_amount_map :
+    S with type key = string and type value = Amount.t =
   struct
     type key = string
 
     type value = Amount.t
 
-    (* flag, true iff amount is negative *)
-
     type ('key, 'value) t = ('key, 'value) Hashtbl.t
 
     let mem map key = Hashtbl.mem map key
 
-    let find map key = Hashtbl.find_opt map key
+    let find map key = Hashtbl.find map key
 
     let set map key value =
       (* NB: mutation *)
-      Hashtbl.replace map key value ;
+      Hashtbl.set map ~key ~data:value ;
       map
 
     (* make a fresh map each time; a particular instance may be mutated *)
-    let identity () = Hashtbl.create 17
+    let identity () : (key, value) t = Hashtbl.create (module String)
 
     let add map1 map2 =
       let new_map = Hashtbl.copy map1 in
-      let keys2 = Hashtbl.to_seq_keys map2 in
-      let sum_points key2 =
-        let amount2 =
-          match Hashtbl.find_opt map2 key2 with
-          | Some v -> v
-          | None -> assert false
-        in
-        match Hashtbl.find_opt new_map key2 with
-        | Some amount1 ->
-            let sum =
-              match Amount.add amount1 amount2 with
-              | Some v -> v
-              | None -> assert false
-            in
-            Hashtbl.replace new_map key2 sum
-        | None -> Hashtbl.add new_map key2 amount2
+      let keys2 = Hashtbl.keys map2 in
+      let sum_points valid key2 =
+        if not valid then false (* don't do any work if error *)
+        else
+          let amount2 =
+            match Hashtbl.find map2 key2 with
+            | Some v -> v
+            | None -> assert false
+          in
+          match Hashtbl.find new_map key2 with
+          | Some amount1 -> (
+            match Amount.add amount1 amount2 with
+            | Some sum ->
+                Hashtbl.set new_map ~key:key2 ~data:sum ;
+                true
+            | None -> false (* overflow *) )
+          | None ->
+            match Hashtbl.add new_map ~key:key2 ~data:amount2 with
+            | `Ok -> true
+            | `Duplicate -> assert false
       in
-      Seq.iter sum_points keys2 ; new_map
+      if List.fold_left keys2 ~f:sum_points ~init:true then Ok new_map
+      else
+        Or_error.error_string
+          "User_amount_map.add: overflow when adding map leaves"
 
     let inverse map =
       let new_map = identity () in
-      Hashtbl.iter
-        (fun key amount -> Hashtbl.add new_map key (Amount.negate amount))
-        map ;
+      Hashtbl.iteri map ~f:(fun ~key ~data:amount ->
+          match Hashtbl.add new_map ~key ~data:(Amount.negate amount) with
+          | `Ok -> ()
+          | `Duplicate -> assert false ) ;
       new_map
 
     let equal map1 map2 =
       (* cheap check first *)
-      if Hashtbl.length map1 != Hashtbl.length map2 then false
+      if not (phys_equal (Hashtbl.length map1) (Hashtbl.length map2)) then
+        false
       else
-        let keys1 = Hashtbl.to_seq_keys map1 in
-        let keys2 = Hashtbl.to_seq_keys map2 in
+        let keys1 = Hashtbl.keys map1 in
+        let keys2 = Hashtbl.keys map2 in
         let check map_a map_b accum key =
-          accum (* invariant: key is in mapa *)
+          accum (* invariant: key is in map_a *)
           && Hashtbl.mem map_b key
-          && Hashtbl.find_opt map_a key = Hashtbl.find_opt map_b key
+          && Hashtbl.find map_a key = Hashtbl.find map_b key
         in
-        Seq.fold_left (check map1 map2) true keys1
-        && Seq.fold_left (check map2 map1) true keys2
+        List.fold_left keys1 ~init:true ~f:(check map1 map2)
+        && List.fold_left keys2 ~init:true ~f:(check map2 map1)
   end
 
   let make_test_map (entries: (string * int) list) =
@@ -103,9 +112,9 @@ module Test = struct
       else Currency.Amount.of_int (-n) |> Amount.of_unsigned |> Amount.negate
     in
     Core.List.fold
-      ~init:(UserAmountMap.identity ())
+      ~init:(User_amount_map.identity ())
       ~f:(fun map (acct_key, amount) ->
-        UserAmountMap.set map acct_key (amount_of_int amount) )
+        User_amount_map.set map acct_key (amount_of_int amount) )
       entries
 
   let test_map1 = make_test_map [("Alice", 42); ("Bob", 99); ("Charlie", 101)]
@@ -120,29 +129,37 @@ module Test = struct
   let test_sum_map =
     make_test_map [("Alice", 75); ("Bob", 201); ("Charlie", 101)]
 
-  let%test "equal_maps_1" = UserAmountMap.equal test_map1 test_map1
+  let%test "equal_maps_1" = User_amount_map.equal test_map1 test_map1
 
-  let%test "equal_maps_2" = UserAmountMap.equal test_map2 test_map2
+  let%test "equal_maps_2" = User_amount_map.equal test_map2 test_map2
 
-  let%test "equal_maps_3" = not (UserAmountMap.equal test_map1 test_map2)
+  let%test "equal_maps_3" = not (User_amount_map.equal test_map1 test_map2)
 
   let%test "add_maps_1" =
-    UserAmountMap.equal test_sum_map (UserAmountMap.add test_map1 test_map2)
+    match User_amount_map.add test_map1 test_map2 with
+    | Ok map -> User_amount_map.equal map test_sum_map
+    | Error _s -> false
+
+  (* overflow *)
 
   let%test "add_maps_2" =
-    UserAmountMap.equal test_sum_map (UserAmountMap.add test_map2 test_map1)
+    match User_amount_map.add test_map2 test_map1 with
+    | Ok map -> User_amount_map.equal map test_sum_map
+    | Error _s -> false
+
+  (* overflow *)
 
   let%test "map_inverse_1" =
-    UserAmountMap.equal test_inverse_map1 (UserAmountMap.inverse test_map1)
+    User_amount_map.equal test_inverse_map1 (User_amount_map.inverse test_map1)
 
   let%test "map_inverse_2" =
-    UserAmountMap.equal test_inverse_map2 (UserAmountMap.inverse test_map2)
+    User_amount_map.equal test_inverse_map2 (User_amount_map.inverse test_map2)
 
   let%test "map_inverse_3" =
-    UserAmountMap.equal test_map1
-      (UserAmountMap.inverse (UserAmountMap.inverse test_map1))
+    User_amount_map.equal test_map1
+      (User_amount_map.inverse (User_amount_map.inverse test_map1))
 
   let%test "map_inverse_4" =
-    UserAmountMap.equal test_map2
-      (UserAmountMap.inverse (UserAmountMap.inverse test_map2))
+    User_amount_map.equal test_map2
+      (User_amount_map.inverse (User_amount_map.inverse test_map2))
 end
