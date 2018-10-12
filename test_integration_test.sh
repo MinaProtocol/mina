1198a3b5b4f07df029be5fc64a41a9f2be27bcaa
Evan <evan.a.shapiro@gmail.com>
2018-10-11 18:06:28 -0700

    split out integration tests for circleci (#937)
    * split out integration tests
    
    * add test of separate circle steps
    
    * add test integration test file
    
    * update script name
    
    * remove PATH
    
    * partial work with templates
    
    * add render check
    
    * add check render to circle steps
    
    * update config
    
    * add python
    
    * render config
    
    * add jinja
    
    * check missed render
    
    * fix render state
    
    * change -e in integration_test code
    
    * move python+jinja2 to dockerfile
    
    * fix .PHONY
    
    * update-deps

---
+++ test_integration_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eo pipefail
+
+eval `opam config env`
+
+TEST=$1
+
+set +e
+# ugly hack to clean up dead processes
+pkill -9 exe
+pkill -9 kademlia
+pkill -9 coda
+sleep 1
+set -e
+dune exec coda -- integration-tests ${TEST}
