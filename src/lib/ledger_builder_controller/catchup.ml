c6a9a278416126d9cace678add655acb16599f5d
John Wu <johnwu93@gmail.com>
2018-10-18 15:11:26 -0700

    Made Ledger Builder Controller synchronous #770 (#973)
    * Made Ledger Builder Controller synchronous
    
    - Immediately applied all changes when they are created in
    Transition_logic
    
    - The output of jobs returns unit.
    
    
    * reformat
    
    * Addressed PR comments
    
    * Addressed Pr comments

--- src/lib/ledger_builder_controller/catchup.ml
+++ src/lib/ledger_builder_controller/catchup.ml
@@ -35,10 +35,15 @@ struct
     {net; log= Logger.child parent_log __MODULE__; sl_ref= ref None; public_key}
 
   (* Perform the `Sync interruptible work *)
-  let do_sync {net; log; sl_ref; public_key} (state: Transition_logic_state.t)
-      transition_with_hash =
+  let do_sync {net; log; sl_ref; public_key}
+      ~(old_state: Transition_logic_state.t)
+      ~(state_mutator:
+            Transition_logic_state.t
+         -> Transition_logic_state.Change.t list
+         -> External_transition.t
+         -> unit Deferred.t) transition_with_hash =
     let {With_hash.data= locked_tip; hash= _} =
-      Transition_logic_state.locked_tip state
+      Transition_logic_state.locked_tip old_state
     in
     let {With_hash.data= transition; hash= transition_state_hash} =
       transition_with_hash
@@ -80,7 +85,7 @@ struct
             !"Successfully caught up to ledger %{sexp: Ledger_hash.t}"
             h ;
           (* TODO: This should be parallelized with the syncing *)
-          match%map
+          match%bind
             Interruptible.uninterruptible
               (Net.get_ledger_builder_aux_at_hash net
                  (ledger_builder_hash_of_transition transition))
@@ -109,26 +114,32 @@ struct
                     Ledger_builder_hash.t}"
                   (Ledger_builder.hash lb) ;
                 let open Transition_logic_state.Change in
-                [Ktree new_tree; Locked_tip new_tip; Longest_branch_tip new_tip]
+                Interruptible.uninterruptible
+                  (state_mutator old_state
+                     [ Ktree new_tree
+                     ; Locked_tip new_tip
+                     ; Longest_branch_tip new_tip ]
+                     (With_hash.data transition_with_hash))
             | Error e ->
                 Logger.faulty_peer log
                   "Malicious aux data received from net %s"
                   (Error.to_string_hum e) ;
-                (* TODO: Retry? see #361 *)
-                [] )
+                return ()
+                (* TODO: Retry? see #361 *) )
           | Error e ->
               Logger.faulty_peer log "Network failed to send aux %s"
                 (Error.to_string_hum e) ;
-              [] )
+              return () )
       | `Target_changed (old_target, new_target) ->
           Logger.debug log
             !"Existing sync-ledger target_changed from %{sexp: Ledger_hash.t \
               option} to %{sexp: Ledger_hash.t}"
             old_target new_target ;
-          return []
+          return ()
     in
     (work, ivar)
 
-  let sync (t: t) (state: Transition_logic_state.t) transition_with_hash =
-    Job.create transition_with_hash ~f:(do_sync t state)
+  let sync (t: t) ~(old_state: Transition_logic_state.t) ~state_mutator
+      transition_with_hash =
+    Job.create transition_with_hash ~f:(do_sync ~old_state ~state_mutator t)
 end
