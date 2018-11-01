f373289b9b42acb011beb8c9dc133f59050f9b3e
Corey Richardson <cmr@o1labs.org>
2018-11-01 16:07:52 -0700

    use env var instead of flags

--- .circleci/render.py
+++ .circleci/render.py
@@ -7,16 +7,16 @@ base_actions = [
     'coda-peers-test',
     'coda-transitive-peers-test',
     'coda-block-production-test',
-    'coda-shared-prefix-test -who-proposes 0 -proposal-interval 8000',
-    'coda-shared-prefix-test -who-proposes 1 -proposal-interval 8000',
-    'coda-shared-state-test -proposal-interval 6000',
-    'coda-restart-node-test -proposal-interval 6000',
+    'coda-shared-prefix-test -who-proposes 0',
+    'coda-shared-prefix-test -who-proposes 1',
+    'coda-shared-state-test',
+    'coda-restart-node-test',
     'transaction-snark-profiler -check-only'
 ]
 
 tests = [
-   {'friendly': 'Sig Tests', 'name': 'all_sig_integration_tests', 'env_str': "CODA_CONSENSUS_MECHANISM=proof_of_signature CODA_PROPOSAL_INTERVAL=1000", 'actions': base_actions},
-   {'friendly': 'Stake Tests', 'name': 'all_stake_integration_tests', 'env_str': "CODA_CONSENSUS_MECHANISM=proof_of_stake CODA_SLOT_INTERVAL=1000 CODA_UNFORKABLE_TRANSITION_COUNT=24 CODA_PROBABLE_SLOTS_PER_TRANSITION_COUNT=8", 'actions': base_actions}
+   {'friendly': 'Sig Tests', 'name': 'all_sig_integration_tests', 'env_str': "CODA_CONSENSUS_MECHANISM=proof_of_signature CODA_PROPOSAL_INTERVAL=8000", 'actions': base_actions},
+   {'friendly': 'Stake Tests', 'name': 'all_stake_integration_tests', 'env_str': "CODA_CONSENSUS_MECHANISM=proof_of_stake CODA_SLOT_INTERVAL=8000 CODA_UNFORKABLE_TRANSITION_COUNT=24 CODA_PROBABLE_SLOTS_PER_TRANSITION_COUNT=8", 'actions': base_actions}
 ]
 
 # Render it
