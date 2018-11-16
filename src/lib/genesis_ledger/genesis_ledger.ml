43fa9a3f91e9bebba3ede0e226c594f287949e9b
Nathan Holland <nholland94@gmail.com>
2018-11-16 15:52:34 -0800

    Fix initial proof of stake bugs (#1146)

--- src/lib/genesis_ledger/genesis_ledger.ml
+++ src/lib/genesis_ledger/genesis_ledger.ml
@@ -15,14 +15,25 @@ genesis_ledger = "test"]
 include Test_ledger
 
 [%%elif
-genesis_ledger = "testnet"]
+genesis_ledger = "testnet_posig"]
 
-include Testnet_ledger
+include Testnet_posig_ledger
+
+[%%elif
+genesis_ledger = "testnet_postake"]
+
+include Testnet_postake_ledger
+
+[%%else]
+
+[%%error
+"\"genesis_ledger\" is set to invalid value in config.mlh"]
 
 [%%endif]
 
 [%%else]
 
-(* [%%error "\"genesis_ledger\" not set in config.mlh"] *)
+[%%error
+"\"genesis_ledger\" not set in config.mlh"]
 
 [%%endif]
