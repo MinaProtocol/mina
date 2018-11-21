32f2d921e957ad841f4cedd85ec272421df9428f
Brandon Kase <bkase@o1labs.org>
2018-11-21 11:16:05 -0800

    Scaffold for catchup/sync_handler
    Intentionally crashing for now. This will let us better debug the rest
    of the transition-frontier-controller system.

--- src/lib/ledger_catchup/ledger_catchup.ml
+++ src/lib/ledger_catchup/ledger_catchup.ml
@@ -1,1 +1,20 @@
+open Core_kernel
+open Async_kernel
+open Protocols.Coda_pow
 
+module type Inputs_intf = sig
+  module Consensus_mechanism : Consensus_mechanism_intf
+
+  module Transition_frontier : Transition_frontier_intf
+end
+
+module Make (Inputs : Inputs_intf) :
+  Catchup_intf
+  with type external_transition :=
+              Inputs.Consensus_mechanism.External_transition.t
+   and type transition_frontier := Inputs.Transition_frontier.t = struct
+  let run ~catchup_job_reader _transition_frontier =
+    don't_wait_for
+      (Linear_pipe.iter catchup_job_reader ~f:(fun _ ->
+           failwith "Intentionally unimplemented catchup" ))
+end
