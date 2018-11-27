a2ffd4fcbc72d76060299cd5ee452499682c782e
Corey Richardson <cmr@o1labs.org>
2018-11-26 20:42:44 -0800

    remove -fork from runner.sh

--- src/app/bench/runner.sh
+++ src/app/bench/runner.sh
@@ -2,4 +2,4 @@
 export BENCHMARKS_RUNNER=TRUE
 export BENCH_LIB=dune_bench
 export CONSENSUS_MECHANISM=PROOF_OF_STAKE
-exec dune exec -- ./main.exe -fork -run-without-cross-library-inlining "$@"
+exec dune exec -- ./main.exe -run-without-cross-library-inlining "$@"
