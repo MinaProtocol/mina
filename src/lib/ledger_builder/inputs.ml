30bab0220608737b7657225a7e95e37e19c007bf
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2018-12-03 11:21:21 -0800

    move-around refactoring

--- src/lib/ledger_builder/inputs.ml
+++ src/lib/ledger_builder/inputs.ml
@@ -114,3 +114,167 @@ module type S = sig
     -> Ledger_proof_statement.t list
     -> Completed_work.Checked.t option Async_kernel.Deferred.t
 end
+
+module type S' = sig
+  open Coda_pow
+
+  module Compressed_public_key : Compressed_public_key_intf
+
+  module User_command :
+    Coda_pow.User_command_intf with type public_key := Compressed_public_key.t
+
+  module Fee_transfer :
+    Coda_pow.Fee_transfer_intf with type public_key := Compressed_public_key.t
+
+  module Coinbase :
+    Coda_pow.Coinbase_intf
+    with type public_key := Compressed_public_key.t
+     and type fee_transfer := Fee_transfer.single
+
+  module Transaction :
+    Coda_pow.Transaction_intf
+    with type valid_user_command := User_command.With_valid_signature.t
+     and type fee_transfer := Fee_transfer.t
+     and type coinbase := Coinbase.t
+
+  module Ledger_hash : Coda_pow.Ledger_hash_intf
+
+  module Frozen_ledger_hash : sig
+    include Coda_pow.Ledger_hash_intf
+
+    val of_ledger_hash : Ledger_hash.t -> t
+  end
+
+  module Ledger_proof_statement :
+    Coda_pow.Ledger_proof_statement_intf
+    with type ledger_hash := Frozen_ledger_hash.t
+
+  module Proof : sig
+    type t
+  end
+
+  module Sok_message :
+    Sok_message_intf with type public_key_compressed := Compressed_public_key.t
+
+  module Ledger_proof : sig
+    include
+      Coda_pow.Ledger_proof_intf
+      with type statement := Ledger_proof_statement.t
+       and type ledger_hash := Frozen_ledger_hash.t
+       and type proof := Proof.t
+       and type sok_digest := Sok_message.Digest.t
+
+    include Binable.S with type t := t
+
+    include Sexpable.S with type t := t
+  end
+
+  module Ledger_proof_verifier :
+    Ledger_proof_verifier_intf
+    with type statement := Ledger_proof_statement.t
+     and type message := Sok_message.t
+     and type ledger_proof := Ledger_proof.t
+
+  module Staged_ledger_aux_hash : Coda_pow.Ledger_builder_aux_hash_intf
+
+  module Staged_ledger_hash :
+    Coda_pow.Ledger_builder_hash_intf
+    with type ledger_hash := Ledger_hash.t
+     and type ledger_builder_aux_hash := Staged_ledger_aux_hash.t
+
+  module Transaction_snark_work :
+    Coda_pow.Completed_work_intf
+    with type proof := Ledger_proof.t
+     and type statement := Ledger_proof_statement.t
+     and type public_key := Compressed_public_key.t
+
+  module Ledger_builder_diff :
+    Coda_pow.Ledger_builder_diff_intf
+    with type completed_work := Transaction_snark_work.t
+     and type completed_work_checked := Transaction_snark_work.Checked.t
+     and type user_command := User_command.t
+     and type user_command_with_valid_signature :=
+                User_command.With_valid_signature.t
+     and type public_key := Compressed_public_key.t
+     and type ledger_builder_hash := Staged_ledger_hash.t
+
+  module Merkle_address : Merkle_address.S
+
+  module Syncable_ledger :
+    Syncable_ledger.S
+    with type addr := Merkle_address.t
+     and type hash := Ledger_hash.t
+
+  module Key : Merkle_ledger.Intf.Key
+
+  module Account : Merkle_ledger.Intf.Account with type key := Key.t
+
+  module Location : Merkle_ledger.Location_intf.S
+
+  module Ledger_diff : sig
+    type t
+
+    val empty : t
+  end
+
+  module Any_base :
+    Merkle_mask.Base_merkle_tree_intf.S
+    with module Addr = Location.Addr
+     and module Location = Location
+     and type account := Account.t
+     and type root_hash := Ledger_hash.t
+     and type hash := Ledger_hash.t
+     and type key := Key.t
+
+  module Ledger_mask : sig
+    include
+      Merkle_mask.Masking_merkle_tree_intf.S
+      with module Addr = Location.Addr
+       and module Location = Location
+       and module Attached.Addr = Location.Addr
+       and type account := Account.t
+       and type location := Location.t
+       and type key := Key.t
+       and type hash := Ledger_hash.t
+       and type parent := Any_base.t
+
+    val merkle_root : t -> Ledger_hash.t
+
+    val apply : t -> Ledger_diff.t -> unit
+
+    val commit : t -> unit
+  end
+
+  module Transaction_snark_scan_state : sig
+    type t
+
+    val empty : t
+
+    module Diff : sig
+      type t
+
+      (* hack until Parallel_scan_state().Diff.t fully diverges from Ledger_builder_diff.t and is included in External_transition *)
+      val of_ledger_builder_diff : Ledger_builder_diff.t -> t
+    end
+  end
+
+  module Staged_ledger : sig
+    type t
+
+    val create :
+         transaction_snark_scan_state:Transaction_snark_scan_state.t
+      -> ledger_mask:Ledger_mask.t
+      -> t
+
+    val apply : t -> Transaction_snark_scan_state.Diff.t -> t Or_error.t
+  end
+
+  module Config : sig
+    val transaction_capacity_log_2 : int
+  end
+
+  val check :
+       Transaction_snark_work.t
+    -> Ledger_proof_statement.t list
+    -> Transaction_snark_work.Checked.t option Async_kernel.Deferred.t
+end
---
+++ src/lib/ledger_builder/transaction_snark_work.ml
@@ -0,0 +1,53 @@
+open Core_kernel
+open Async_kernel
+open Protocols
+open Coda_pow
+
+module Make
+    (Compressed_public_key : Compressed_public_key_intf) (Ledger_proof : sig
+        type t [@@deriving sexp, bin_io]
+    end) (Ledger_proof_statement : sig
+      type t [@@deriving sexp, bin_io, hash, compare]
+
+      val gen : t Quickcheck.Generator.t
+    end) :
+  Coda_pow.Completed_work_intf
+  with type proof := Ledger_proof.t
+   and type statement := Ledger_proof_statement.t
+   and type public_key := Compressed_public_key.t = struct
+  let proofs_length = 2
+
+  module Statement = struct
+    module T = struct
+      type t = Ledger_proof_statement.t list
+      [@@deriving bin_io, sexp, hash, compare]
+    end
+
+    include T
+    include Hashable.Make_binable (T)
+
+    let gen =
+      Quickcheck.Generator.list_with_length proofs_length
+        Ledger_proof_statement.gen
+  end
+
+  module T = struct
+    type t =
+      { fee: Fee.Unsigned.t
+      ; proofs: Ledger_proof.t list
+      ; prover: Compressed_public_key.t }
+    [@@deriving sexp, bin_io]
+  end
+
+  include T
+
+  type unchecked = t
+
+  module Checked = struct
+    include T
+
+    let create_unsafe = Fn.id
+  end
+
+  let forget = Fn.id
+end
