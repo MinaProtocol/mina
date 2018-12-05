9e722c4feadbf7b1164f238442d8892aad83d5b9
Paul Steckler <psteck@mit.edu>
2018-12-05 12:38:05 -0800

    save original file as .merlin.DOCKER

--- scripts/merlin-fixup.sh
+++ scripts/merlin-fixup.sh
@@ -9,6 +9,7 @@ OCAML_VERSION=4.07
 
 for file in `find src -name .merlin`
 do
+    cp -p $file $file.DOCKER
     sed --in-place s+.opam/$OCAML_VERSION/+.opam/$OPAM_SWITCH/+g $file
     sed --in-place s+/home/opam/.opam/+$HOME/.opam/+g $file
     sed --in-place s+/home/opam/app/src/_build/+$(git-root)/src/_build/+g $file
