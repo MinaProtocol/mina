46149f848a514cf9ea0bbb078333b1ce09c56381
Izaak Meckler <ihmeckler@gmail.com>
2018-12-10 10:26:30 -0800

    _

---
+++ src/lib/snarky_field_extensions/intf.ml
@@ -0,0 +1,55 @@
+module type Basic = sig
+  module Impl : Snarky.Snark_intf.S
+  open Impl
+
+  module Unchecked : Snarkette.Fields.Intf
+
+  type t
+
+  val typ : (t, Unchecked.t) Typ.t
+
+  val constant : Unchecked.t -> t
+
+  val scale : t -> Field.t -> t
+
+  val assert_r1cs : t -> t -> t -> (unit, _) Checked.t
+
+  val assert_square : [`Define | `Custom of t -> t -> (unit, _) Checked.t ]
+
+  val ( + ) : t -> t -> t
+
+  val ( - ) : t -> t -> t
+
+  val negate : t -> t
+
+  val ( * ) : [ `Define | `Custom of t -> t -> (t,_) Checked.t ]
+
+  val square : [ `Define | `Custom of t -> (t, _) Checked.t ]
+
+  val inv : [ `Define | `Custom of t -> (t, _) Checked.t ]
+end
+
+module type S = sig
+  include Basic
+  open Impl
+
+  val assert_square : t -> t -> (unit, _) Checked.t
+
+  val ( * ) : t -> t -> (t, _) Checked.t
+
+  val square :  t -> (t, _) Checked.t
+
+  val inv :  t -> (t, _) Checked.t 
+
+  val zero : t
+
+  val one : t
+end
+
+module type Applicative = sig
+  type _ t
+
+  val map : 'a t -> f:('a -> 'b) -> 'b t
+
+  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+end
