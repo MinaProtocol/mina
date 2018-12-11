59e96a1fc1bd6801aae4d22ed7f0ec52e2a4d3d7
Corey Richardson <corey@octayn.net>
2018-12-11 13:41:31 -0800

    Fix Rocksdb.Database.to_alist and Ledger_transfer (#1295)
    * copy strings before the iterator invalidates them
    
    * Ledger-transfer makes sure to sort by the addresses
    
    We unfortunately need to allocate a list to do this properly, but in the
    short-term we should migrate the genesis ledgers over to databases
    anyway.
    
    First assertion passes, second one fails now.

--- src/lib/rocksdb/database.ml
+++ src/lib/rocksdb/database.ml
@@ -38,12 +38,38 @@ let to_alist t : (Bigstring.t * Bigstring.t) list =
   let iterator = Rocks.Iterator.create t.db in
   Rocks.Iterator.seek_to_last iterator ;
   (* iterate backwards and cons, to build list sorted by key *)
+  let copy t =
+    let tlen = Bigstring.length t in
+    let new_t = Bigstring.create tlen in
+    Bigstring.blit ~src:t ~dst:new_t ~src_pos:0 ~dst_pos:0 ~len:tlen ;
+    new_t
+  in
   let rec loop accum =
     if Rocks.Iterator.is_valid iterator then (
-      let key = Rocks.Iterator.get_key iterator in
-      let value = Rocks.Iterator.get_value iterator in
+      let key = copy (Rocks.Iterator.get_key iterator) in
+      let value = copy (Rocks.Iterator.get_value iterator) in
       Rocks.Iterator.prev iterator ;
       loop ((key, value) :: accum) )
     else accum
   in
   loop []
+
+let%test_unit "to_alist (of_alist l) = l" =
+  Quickcheck.test
+    Quickcheck.Generator.(tuple2 String.gen String.gen |> list)
+    ~f:(fun kvs ->
+      File_system.with_temp_dir "/tmp/coda-test" ~f:(fun directory ->
+          let s = Bigstring.of_string in
+          let sorted =
+            List.sort kvs ~compare:[%compare: string * string]
+            |> List.map ~f:(fun (k, v) -> (s k, s v))
+          in
+          let db = create ~directory in
+          List.iter sorted ~f:(fun (key, data) -> set db ~key ~data) ;
+          let alist =
+            List.sort (to_alist db)
+              ~compare:[%compare: Bigstring.t * Bigstring.t]
+          in
+          [%test_result: (Bigstring.t * Bigstring.t) list] ~expect:sorted alist ;
+          Async.Deferred.unit )
+      |> Async.don't_wait_for )
