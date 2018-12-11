499cdb71a2826ff552bc4a8596145c0f90a836ba
Corey Richardson <cmr@o1labs.org>
2018-12-11 14:34:30 -0800

    reformat

--- src/lib/non_zero_curve_point/non_zero_curve_point.ml
+++ src/lib/non_zero_curve_point/non_zero_curve_point.ml
@@ -42,9 +42,7 @@ module Compressed = struct
   module Stable = struct
     module V1 = struct
       module T = struct
-        type t = (Field.t, bool) t_
-        [@@deriving bin_io, eq, compare, hash]
-
+        type t = (Field.t, bool) t_ [@@deriving bin_io, eq, compare, hash]
       end
 
       include T
