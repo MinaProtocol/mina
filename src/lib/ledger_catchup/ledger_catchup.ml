08e6c11acd0290189dda0f8f746ab496a71419af
John Wu <johnwu93@gmail.com>
2018-12-12 11:33:04 -0800

    Created ledger_catchup and  catchup_worker (#1293)
    Created ledger_catchup and  catchup_worker
    - Catchup_worker will use peers from gossip_network to talk to other
    peers about their external_transition
    - Ledger_catchup uses the  worker's external_transitions from
    catchup_worker and map them into breadcrumbs

--- src/lib/ledger_catchup/ledger_catchup.ml
+++ src/lib/ledger_catchup/ledger_catchup.ml
@@ -1,31 +1,116 @@
-open Core_kernel
-open Async_kernel
+open Core
+open Async
 open Protocols.Coda_transition_frontier
 open Pipe_lib
 open Coda_base
 
-module type Inputs_intf = sig
-  include Transition_frontier.Inputs_intf
-
-  module Transition_frontier :
-    Transition_frontier_intf
-    with type state_hash := State_hash.t
-     and type external_transition := External_transition.t
-     and type ledger_database := Ledger.Db.t
-     and type staged_ledger := Staged_ledger.t
-     and type masked_ledger := Ledger.Mask.Attached.t
-     and type transaction_snark_scan_state := Staged_ledger.Scan_state.t
-end
-
-module Make (Inputs : Inputs_intf) :
+module Make (Inputs : Inputs.S) :
   Catchup_intf
   with type external_transition := Inputs.External_transition.t
    and type transition_frontier := Inputs.Transition_frontier.t
    and type transition_frontier_breadcrumb :=
               Inputs.Transition_frontier.Breadcrumb.t
-   and type state_hash := State_hash.t = struct
-  let run ~frontier:_ ~catchup_job_reader ~catchup_breadcrumbs_writer:_ =
-    don't_wait_for
-      (Strict_pipe.Reader.iter catchup_job_reader ~f:(fun _ ->
-           failwith "Intentionally unimplemented catchup" ))
+   and type state_hash := State_hash.t
+   and type network := Inputs.Network.t = struct
+  open Inputs
+
+  let fold_result_seq list ~init ~f =
+    Deferred.List.fold list ~init:(Ok init) ~f:(fun acc elem ->
+        match acc with
+        | Error e -> Deferred.return (Error e)
+        | Ok acc -> f acc elem )
+
+  (* We would like the async scheduler to context switch between each iteration 
+  of external transitions when trying to build breadcrumb_path. Therefore, this 
+  function needs to return a Deferred *)
+  let construct_breadcrumb_path ~logger initial_staged_ledger
+      external_transitions =
+    let open Deferred.Or_error.Let_syntax in
+    let%map _, breadcrumbs =
+      fold_result_seq external_transitions ~init:(initial_staged_ledger, [])
+        ~f:(fun (staged_ledger, acc) external_transition ->
+          let diff =
+            External_transition.staged_ledger_diff external_transition
+          in
+          let%map _, _, `Updated_staged_ledger staged_ledger =
+            Deferred.create (fun ivar ->
+                Ivar.fill ivar
+                @@ Staged_ledger.apply ~logger staged_ledger diff )
+          in
+          let transition_with_hash =
+            With_hash.of_data external_transition
+              ~hash_data:
+                (Fn.compose Consensus.Mechanism.Protocol_state.hash
+                   External_transition.protocol_state)
+          in
+          let new_breadcrumb =
+            Transition_frontier.Breadcrumb.create transition_with_hash
+              staged_ledger
+          in
+          (staged_ledger, new_breadcrumb :: acc) )
+    in
+    List.rev breadcrumbs
+
+  let materialize_breadcrumbs ~frontier ~logger ~peer external_transitions =
+    let root_transition = List.hd_exn external_transitions in
+    let initial_state_hash =
+      root_transition |> External_transition.protocol_state
+      |> External_transition.Protocol_state.previous_state_hash
+    in
+    match Transition_frontier.find frontier initial_state_hash with
+    | None ->
+        let message =
+          sprintf
+            !"Could not find root hash, %{sexp:State_hash.t}.Peer \
+              %{sexp:Kademlia.Peer.t} is seen as malicious"
+            initial_state_hash peer
+        in
+        Logger.faulty_peer logger !"%s" message ;
+        Deferred.return @@ Or_error.error_string message
+    | Some initial_breadcrumb ->
+        let initial_staged_ledger =
+          Transition_frontier.Breadcrumb.staged_ledger initial_breadcrumb
+        in
+        construct_breadcrumb_path ~logger initial_staged_ledger
+          external_transitions
+
+  let get_transitions_and_compute_breadcrumbs ~logger ~network ~frontier
+      ~num_peers hash =
+    let peers = Network.random_peers network num_peers in
+    let open Deferred.Or_error.Let_syntax in
+    Deferred.Or_error.find_map_ok peers ~f:(fun peer ->
+        match%bind Network.catchup_transition network peer hash with
+        | None ->
+            Deferred.return
+            @@ Or_error.errorf
+                 !"Peer %{sexp:Kademlia.Peer.t} did not have transition"
+                 peer
+        | Some [] ->
+            let message =
+              sprintf
+                !"Peer %{sexp:Kademlia.Peer.t} gave an empty list of \
+                  transitions. They should respond with none"
+                peer
+            in
+            Logger.faulty_peer logger !"%s" message ;
+            Deferred.return @@ Or_error.error_string message
+        | Some queried_transitions ->
+            materialize_breadcrumbs ~frontier ~logger ~peer queried_transitions
+    )
+
+  let run ~logger ~network ~frontier ~catchup_job_reader
+      ~catchup_breadcrumbs_writer =
+    Strict_pipe.Reader.iter catchup_job_reader ~f:(fun transition_with_hash ->
+        let hash = With_hash.hash transition_with_hash in
+        match%map
+          get_transitions_and_compute_breadcrumbs ~logger ~network ~frontier
+            ~num_peers:8 hash
+        with
+        | Ok breadcrumbs ->
+            Strict_pipe.Writer.write catchup_breadcrumbs_writer breadcrumbs
+        | Error e ->
+            Logger.info logger
+              !"None of the peers have a transition with state hash:\n%s"
+              (Error.to_string_hum e) )
+    |> don't_wait_for
 end
