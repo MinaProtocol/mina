e2c00d4e7f8292886d56be0453a89a0c0fbc55f2
Nathan Holland <nholland94@gmail.com>
2018-12-17 18:31:58 -0800

    Document Capped_supervisor

--- src/lib/otp_lib/capped_supervisor.mli
+++ src/lib/otp_lib/capped_supervisor.mli
@@ -1,3 +1,11 @@
+(**
+ * A [Capped_supervisor] is a process supervisor which which will limit the
+ * number of active processes to some specified capacity. Specifically, it
+ * allows the definition of a job runner which jobs can be dispatched to,
+ * where dispatched jobs are added to a queue and a pool a processes work
+ * off the queue, with at max [job_capacity] jobs.
+ *)
+
 open Async_kernel
 
 type _ t
