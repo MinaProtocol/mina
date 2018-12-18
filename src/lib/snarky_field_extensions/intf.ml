700201be18c8e36ab8bfbd85fe26eec2853d1e6d
Izaak Meckler <ihmeckler@gmail.com>
2018-12-18 11:44:45 -0800

    field extension changes

--- src/lib/snarky_field_extensions/intf.ml
+++ src/lib/snarky_field_extensions/intf.ml
@@ -1,10 +1,37 @@
+module type Applicative = sig
+  type _ t
+
+  val map : 'a t -> f:('a -> 'b) -> 'b t
+
+  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
+end
+
 module type Basic = sig
   module Impl : Snarky.Snark_intf.S
+
   open Impl
 
-  module Unchecked : Snarkette.Fields.Intf
+  module Base : sig
+    type _ t_
 
-  type t
+    val map_ : 'a t_ -> f:('a -> 'b) -> 'b t_
+
+    module Unchecked : sig
+      type t = Field.t t_
+    end
+
+    type t = Field.Checked.t t_
+  end
+
+  module A : Applicative
+
+  type 'a t_ = 'a Base.t_ A.t
+
+  val map_ : 'a t_ -> f:('a -> 'b) -> 'b t_
+
+  module Unchecked : Snarkette.Fields.Intf with type t = Base.Unchecked.t A.t
+
+  type t = Base.t A.t
 
   val typ : (t, Unchecked.t) Typ.t
 
@@ -14,7 +41,7 @@ module type Basic = sig
 
   val assert_r1cs : t -> t -> t -> (unit, _) Checked.t
 
-  val assert_square : [`Define | `Custom of t -> t -> (unit, _) Checked.t ]
+  val assert_square : [`Define | `Custom of t -> t -> (unit, _) Checked.t]
 
   val ( + ) : t -> t -> t
 
@@ -22,34 +49,31 @@ module type Basic = sig
 
   val negate : t -> t
 
-  val ( * ) : [ `Define | `Custom of t -> t -> (t,_) Checked.t ]
+  val ( * ) : [`Define | `Custom of t -> t -> (t, _) Checked.t]
 
-  val square : [ `Define | `Custom of t -> (t, _) Checked.t ]
+  val square : [`Define | `Custom of t -> (t, _) Checked.t]
 
-  val inv : [ `Define | `Custom of t -> (t, _) Checked.t ]
+  val inv : [`Define | `Custom of t -> (t, _) Checked.t]
 end
 
 module type S = sig
   include Basic
+
   open Impl
 
   val assert_square : t -> t -> (unit, _) Checked.t
 
   val ( * ) : t -> t -> (t, _) Checked.t
 
-  val square :  t -> (t, _) Checked.t
+  val square : t -> (t, _) Checked.t
 
-  val inv :  t -> (t, _) Checked.t 
+  val inv : t -> (t, _) Checked.t
+
+  (* This function MUST NOT be called on two arguments which are both potentially
+   zero *)
+  val div_unsafe : t -> t -> (t, _) Checked.t
 
   val zero : t
 
   val one : t
-end
-
-module type Applicative = sig
-  type _ t
-
-  val map : 'a t -> f:('a -> 'b) -> 'b t
-
-  val map2 : 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
 end
