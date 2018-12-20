01d2db10948b488693f24239d73f67beceef377d
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2018-12-19 17:48:37 -0800

    change wait time and reduce transaction_capcity

--- src/app/cli/src/full_test.ml
+++ src/app/cli/src/full_test.ml
@@ -42,7 +42,7 @@ let run_test () : unit Deferred.t =
     let transaction_capacity_log_2 =
       if with_snark then 1
         (*this works because we don't have prover fees. Once we have that, the transaction_capacity_log_2 has to be at least 2 for transactions to be included*)
-      else 3
+      else 2
 
     let commit_id = None
 
@@ -288,12 +288,12 @@ let run_test () : unit Deferred.t =
     in
     (*wait for a block after the ledger_proof is emitted*)
     let%map () =
-      wait_until_cond ~f:(fun t -> block_count t > block_count') ~timeout:5.
+      wait_until_cond ~f:(fun t -> block_count t > block_count') ~timeout:10.
     in
     assert (block_count coda > block_count')
   else
     let%bind _ =
-      test_multiple_payments other_accounts (pks other_accounts) 3.
+      test_multiple_payments other_accounts (pks other_accounts) 5.
     in
     test_duplicate_payments sender_keypair receiver_keypair
 
