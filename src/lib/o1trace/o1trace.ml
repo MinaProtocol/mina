7b480e5f000378b19ff1170f67e610458788b4a8
mrmr1993 <mr_1993@hotmail.co.uk>
2018-12-28 15:22:29 +0000

    [O1trace] wrap emitting events into a utility function

--- src/lib/o1trace/o1trace.ml
+++ src/lib/o1trace/o1trace.ml
@@ -49,20 +49,30 @@ let timestamp () =
   Time_stamp_counter.now () |> Time_stamp_counter.to_time_ns
   |> Core.Time_ns.to_int63_ns_since_epoch |> Int63.to_int_exn
 
+let emit_event wr ?(name = "") ?(tid = 0) (k : event_kind) =
+  match k with
+  | New_thread ->
+      emitk New_thread 0
+      |> emiti (timestamp ())
+      |> emiti tid |> emits name |> finish wr
+  | Thread_switch ->
+      emitk Thread_switch 0 |> emiti (timestamp ()) |> emiti tid |> finish wr
+  | Cycle_end -> emitk Cycle_end 0 |> emiti (timestamp ()) |> finish wr
+  | Pid_is ->
+      emitk Pid_is 0 |> emiti (Unix.getpid () |> Pid.to_int) |> finish wr
+  | Event -> emitk Event 0 |> emiti (timestamp ()) |> emits name |> finish wr
+  | Start -> emitk Start 0 |> emiti (timestamp ()) |> emits name |> finish wr
+  | End -> emitk End 0 |> emiti (timestamp ()) |> finish wr
+
 let trace_new_thread' wr (name : string) (ctx : Execution_context.t) =
-  emitk New_thread 0
-  |> emiti (timestamp ())
-  |> emiti ctx.tid |> emits name |> finish wr
+  emit_event wr ~name ~tid:ctx.tid New_thread
 
 let trace_thread_switch' wr (new_ctx : Execution_context.t) =
-  emitk Thread_switch 0
-  |> emiti (timestamp ())
-  |> emiti new_ctx.tid |> finish wr
+  emit_event wr ~tid:new_ctx.tid Thread_switch
 
 let tid = ref 1
 
-let trace_event' wr (name : string) =
-  emitk Event 0 |> emiti (timestamp ()) |> emits name |> finish wr
+let trace_event' wr (name : string) = emit_event wr ~name Event
 
 let trace_event_impl = ref (fun _ -> ())
 
@@ -87,10 +97,9 @@ let trace_recurring_task (name : string) (f : unit -> 'a) =
       f () )
 
 let measure' wr (name : string) (f : unit -> 'a) : 'a =
-  emitk Start 0 |> emiti (timestamp ()) |> emits name |> finish wr ;
+  emit_event wr ~name Start ;
   let res = f () in
-  emitk End 0 |> emiti (timestamp ()) |> finish wr ;
-  res
+  emit_event wr End ; res
 
 let measure_impl = ref (fun _ f -> f ())
 
@@ -100,7 +109,7 @@ let measure name (f : unit -> 'a) : 'a =
   (Obj.magic !measure_impl : string -> (unit -> 'a) -> 'a) name f
 
 let start_tracing wr =
-  emitk Pid_is 0 |> emiti (Unix.getpid () |> Pid.to_int) |> finish wr ;
+  emit_event wr Pid_is ;
   Async_kernel.Tracing.fns :=
     { trace_thread_switch= trace_thread_switch' wr
     ; trace_new_thread= trace_new_thread' wr } ;
@@ -109,8 +118,7 @@ let start_tracing wr =
   let sch = Scheduler.t () in
   Scheduler.set_on_end_of_cycle sch (fun () ->
       sch.cycle_started <- true ;
-      if sch.current_execution_context.tid <> 0 then
-        emitk Cycle_end 0 |> emiti (timestamp ()) |> finish wr )
+      if sch.current_execution_context.tid <> 0 then emit_event wr Cycle_end )
 
 [%%else]
 
