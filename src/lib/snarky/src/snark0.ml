5eb9ffc9978eee708a4b72d4efc23a043fc1a888
mrmr1993 <mr_1993@hotmail.co.uk>
2019-01-09 00:28:53 +0000

    [Snarky] Break out most of Snark0.Typ into toplevel Typ module

--- src/lib/snarky/src/snark0.ml
+++ src/lib/snarky/src/snark0.ml
@@ -279,6 +279,7 @@ module Make_basic (Backend : Backend_intf.S) = struct
   module Typ = struct
     open Types.Typ
     include Typ_monads
+    include Typ.T
 
     type ('var, 'value) t =
       ('var, 'value, Field.t, Cvar.t, R1CS_constraint_system.t) Types.Typ.t
@@ -303,32 +304,9 @@ module Make_basic (Backend : Backend_intf.S) = struct
         go 0 t
     end
 
-    let store ({store; _} : ('var, 'value) t) (x : 'value) : 'var Store.t =
-      store x
+    let unit : (unit, unit) t = unit ()
 
-    let read ({read; _} : ('var, 'value) t) (v : 'var) : 'value Read.t = read v
-
-    let alloc ({alloc; _} : ('var, 'value) t) : 'var Alloc.t = alloc
-
-    let check ({check; _} : ('var, 'value) t) (v : 'var) :
-        (unit, 's) Checked0.t =
-      let do_nothing : (unit, _) As_prover0.t = fun _ s -> (s, ()) in
-      With_state (do_nothing, (fun () -> do_nothing), check v, Checked0.return)
-
-    let unit : (unit, unit) t =
-      let s = Store.return () in
-      let r = Read.return () in
-      let c = Checked0.return () in
-      { store= (fun () -> s)
-      ; read= (fun () -> r)
-      ; check= (fun () -> c)
-      ; alloc= Alloc.return () }
-
-    let field : (Cvar.t, Field.t) t =
-      { store= Store.store
-      ; read= Read.read
-      ; alloc= Alloc.alloc
-      ; check= (fun _ -> Checked0.return ()) }
+    let field : (Cvar.t, Field.t) t = field ()
 
     let hlist (type k_var k_value)
         (spec0 : (unit, unit, k_var, k_value) Data_spec.t) :
@@ -402,22 +380,6 @@ module Make_basic (Backend : Backend_intf.S) = struct
       in
       {read; store; alloc; check}
 
-    let transport ({read; store; alloc; check} : ('var1, 'value1) t)
-        ~(there : 'value2 -> 'value1) ~(back : 'value1 -> 'value2) :
-        ('var1, 'value2) t =
-      { alloc
-      ; store= (fun x -> store (there x))
-      ; read= (fun v -> Read.map ~f:back (read v))
-      ; check }
-
-    let transport_var ({read; store; alloc; check} : ('var1, 'value) t)
-        ~(there : 'var2 -> 'var1) ~(back : 'var1 -> 'var2) : ('var2, 'value) t
-        =
-      { alloc= Alloc.map alloc ~f:back
-      ; store= (fun x -> Store.map (store x) ~f:back)
-      ; read= (fun x -> read (there x))
-      ; check= (fun x -> check (there x)) }
-
     (* TODO: Do a CPS style thing instead if it ends up being an issue converting
      back and forth. *)
     let of_hlistable (spec : (unit, unit, 'k_var, 'k_value) Data_spec.t)
@@ -431,50 +393,6 @@ module Make_basic (Backend : Backend_intf.S) = struct
       ; store= (fun x -> Store.map ~f:var_of_hlist (store (value_to_hlist x)))
       ; alloc= Alloc.map ~f:var_of_hlist alloc
       ; check= (fun v -> check (var_to_hlist v)) }
-
-    let list ~length ({read; store; alloc; check} : ('elt_var, 'elt_value) t) :
-        ('elt_var list, 'elt_value list) t =
-      let store ts =
-        let n = List.length ts in
-        if n <> length then
-          failwithf "Typ.list: Expected length %d, got %d" length n () ;
-        Store.all (List.map ~f:store ts)
-      in
-      let alloc = Alloc.all (List.init length ~f:(fun _ -> alloc)) in
-      let check ts = Checked0.all_unit (List.map ts ~f:check) in
-      let read vs = Read.all (List.map vs ~f:read) in
-      {read; store; alloc; check}
-
-    (* TODO-someday: Make more efficient *)
-    let array ~length ({read; store; alloc; check} : ('elt_var, 'elt_value) t)
-        : ('elt_var array, 'elt_value array) t =
-      let store ts =
-        assert (Array.length ts = length) ;
-        Store.map ~f:Array.of_list
-          (Store.all (List.map ~f:store (Array.to_list ts)))
-      in
-      let alloc =
-        let open Alloc.Let_syntax in
-        let%map vs = Alloc.all (List.init length ~f:(fun _ -> alloc)) in
-        Array.of_list vs
-      in
-      let read vs =
-        assert (Array.length vs = length) ;
-        Read.map ~f:Array.of_list
-          (Read.all (List.map ~f:read (Array.to_list vs)))
-      in
-      let check ts =
-        assert (Array.length ts = length) ;
-        let open Checked0.Let_syntax in
-        let rec go i =
-          if i = length then return ()
-          else
-            let%map () = check ts.(i) and () = go (i + 1) in
-            ()
-        in
-        go 0
-      in
-      {read; store; alloc; check}
 
     (* TODO: Assert that a stored value has the same shape as the template. *)
     module Of_traversable (T : Traversable.S) = struct
@@ -509,59 +427,6 @@ module Make_basic (Backend : Backend_intf.S) = struct
         let check t = Checked0.map (traverse_checked t ~f:check) ~f:ignore in
         {read; store; alloc; check}
     end
-
-    let tuple2 (typ1 : ('var1, 'value1) t) (typ2 : ('var2, 'value2) t) :
-        ('var1 * 'var2, 'value1 * 'value2) t =
-      let alloc =
-        let open Alloc.Let_syntax in
-        let%map x = typ1.alloc and y = typ2.alloc in
-        (x, y)
-      in
-      let read (x, y) =
-        let open Read.Let_syntax in
-        let%map x = typ1.read x and y = typ2.read y in
-        (x, y)
-      in
-      let store (x, y) =
-        let open Store.Let_syntax in
-        let%map x = typ1.store x and y = typ2.store y in
-        (x, y)
-      in
-      let check (x, y) =
-        let open Checked0.Let_syntax in
-        let%map () = typ1.check x and () = typ2.check y in
-        ()
-      in
-      {read; store; alloc; check}
-
-    let ( * ) = tuple2
-
-    let tuple3 (typ1 : ('var1, 'value1) t) (typ2 : ('var2, 'value2) t)
-        (typ3 : ('var3, 'value3) t) :
-        ('var1 * 'var2 * 'var3, 'value1 * 'value2 * 'value3) t =
-      let alloc =
-        let open Alloc.Let_syntax in
-        let%map x = typ1.alloc and y = typ2.alloc and z = typ3.alloc in
-        (x, y, z)
-      in
-      let read (x, y, z) =
-        let open Read.Let_syntax in
-        let%map x = typ1.read x and y = typ2.read y and z = typ3.read z in
-        (x, y, z)
-      in
-      let store (x, y, z) =
-        let open Store.Let_syntax in
-        let%map x = typ1.store x and y = typ2.store y and z = typ3.store z in
-        (x, y, z)
-      in
-      let check (x, y, z) =
-        let open Checked0.Let_syntax in
-        let%map () = typ1.check x
-        and () = typ2.check y
-        and () = typ3.check z in
-        ()
-      in
-      {read; store; alloc; check}
   end
 
   module As_prover = struct
---
+++ src/lib/snarky/src/typ.ml
@@ -0,0 +1,173 @@
+open Core_kernel
+
+type ('var, 'value, 'field, 'cvar, 'sys) t =
+  ('var, 'value, 'field, 'cvar, 'sys) Types.Typ.t
+
+type ('var, 'value, 'field, 'cvar, 'sys) typ =
+  ('var, 'value, 'field, 'cvar, 'sys) t
+
+module T = struct
+  open Types.Typ
+  open Typ_monads
+
+  let store ({store; _} : ('var, 'value, 'field, 'cvar, 'sys) t) (x : 'value) :
+      ('var, 'field, 'cvar) Store.t =
+    store x
+
+  let read ({read; _} : ('var, 'value, 'field, 'cvar, 'sys) t) (v : 'var) :
+      ('value, 'field, 'cvar) Read.t =
+    read v
+
+  let alloc ({alloc; _} : ('var, 'value, 'field, 'cvar, 'sys) t) :
+      ('var, 'cvar) Alloc.t =
+    alloc
+
+  let check (type field cvar)
+      ({check; _} : ('var, 'value, field, cvar, 'sys) t) (v : 'var) :
+      (unit, 's, field, cvar, 'sys) Types.Checked.t =
+    let do_nothing : (unit, cvar -> field, _) As_prover.t =
+     fun _ s -> (s, ())
+    in
+    With_state (do_nothing, (fun () -> do_nothing), check v, Checked.return)
+
+  let unit () : (unit, unit, 'field, 'cvar, 'sys) t =
+    let s = Store.return () in
+    let r = Read.return () in
+    let c = Checked.return () in
+    { store= (fun () -> s)
+    ; read= (fun () -> r)
+    ; check= (fun () -> c)
+    ; alloc= Alloc.return () }
+
+  let field () : ('cvar, 'field, 'field, 'cvar, 'sys) t =
+    { store= Store.store
+    ; read= Read.read
+    ; alloc= Alloc.alloc
+    ; check= (fun _ -> Checked.return ()) }
+
+  let transport
+      ({read; store; alloc; check} : ('var1, 'value1, 'field, 'cvar, 'sys) t)
+      ~(there : 'value2 -> 'value1) ~(back : 'value1 -> 'value2) :
+      ('var1, 'value2, 'field, 'cvar, 'sys) t =
+    { alloc
+    ; store= (fun x -> store (there x))
+    ; read= (fun v -> Read.map ~f:back (read v))
+    ; check }
+
+  let transport_var
+      ({read; store; alloc; check} : ('var1, 'value, 'field, 'cvar, 'sys) t)
+      ~(there : 'var2 -> 'var1) ~(back : 'var1 -> 'var2) :
+      ('var2, 'value, 'field, 'cvar, 'sys) t =
+    { alloc= Alloc.map alloc ~f:back
+    ; store= (fun x -> Store.map (store x) ~f:back)
+    ; read= (fun x -> read (there x))
+    ; check= (fun x -> check (there x)) }
+
+  let list ~length
+      ({read; store; alloc; check} :
+        ('elt_var, 'elt_value, 'field, 'cvar, 'sys) t) :
+      ('elt_var list, 'elt_value list, 'field, 'cvar, 'sys) t =
+    let store ts =
+      let n = List.length ts in
+      if n <> length then
+        failwithf "Typ.list: Expected length %d, got %d" length n () ;
+      Store.all (List.map ~f:store ts)
+    in
+    let alloc = Alloc.all (List.init length ~f:(fun _ -> alloc)) in
+    let check ts = Checked.all_unit (List.map ts ~f:check) in
+    let read vs = Read.all (List.map vs ~f:read) in
+    {read; store; alloc; check}
+
+  (* TODO-someday: Make more efficient *)
+  let array ~length
+      ({read; store; alloc; check} :
+        ('elt_var, 'elt_value, 'field, 'cvar, 'sys) t) :
+      ('elt_var array, 'elt_value array, 'field, 'cvar, 'sys) t =
+    let store ts =
+      assert (Array.length ts = length) ;
+      Store.map ~f:Array.of_list
+        (Store.all (List.map ~f:store (Array.to_list ts)))
+    in
+    let alloc =
+      let open Alloc.Let_syntax in
+      let%map vs = Alloc.all (List.init length ~f:(fun _ -> alloc)) in
+      Array.of_list vs
+    in
+    let read vs =
+      assert (Array.length vs = length) ;
+      Read.map ~f:Array.of_list
+        (Read.all (List.map ~f:read (Array.to_list vs)))
+    in
+    let check ts =
+      assert (Array.length ts = length) ;
+      let open Checked in
+      let rec go i =
+        if i = length then return ()
+        else
+          let%map () = check ts.(i) and () = go (i + 1) in
+          ()
+      in
+      go 0
+    in
+    {read; store; alloc; check}
+
+  let tuple2 (typ1 : ('var1, 'value1, 'field, 'cvar, 'sys) t)
+      (typ2 : ('var2, 'value2, 'field, 'cvar, 'sys) t) :
+      ('var1 * 'var2, 'value1 * 'value2, 'field, 'cvar, 'sys) t =
+    let alloc =
+      let open Alloc.Let_syntax in
+      let%map x = typ1.alloc and y = typ2.alloc in
+      (x, y)
+    in
+    let read (x, y) =
+      let open Read.Let_syntax in
+      let%map x = typ1.read x and y = typ2.read y in
+      (x, y)
+    in
+    let store (x, y) =
+      let open Store.Let_syntax in
+      let%map x = typ1.store x and y = typ2.store y in
+      (x, y)
+    in
+    let check (x, y) =
+      let open Checked in
+      let%map () = typ1.check x and () = typ2.check y in
+      ()
+    in
+    {read; store; alloc; check}
+
+  let ( * ) = tuple2
+
+  let tuple3 (typ1 : ('var1, 'value1, 'field, 'cvar, 'sys) t)
+      (typ2 : ('var2, 'value2, 'field, 'cvar, 'sys) t)
+      (typ3 : ('var3, 'value3, 'field, 'cvar, 'sys) t) :
+      ( 'var1 * 'var2 * 'var3
+      , 'value1 * 'value2 * 'value3
+      , 'field
+      , 'cvar
+      , 'sys )
+      t =
+    let alloc =
+      let open Alloc.Let_syntax in
+      let%map x = typ1.alloc and y = typ2.alloc and z = typ3.alloc in
+      (x, y, z)
+    in
+    let read (x, y, z) =
+      let open Read.Let_syntax in
+      let%map x = typ1.read x and y = typ2.read y and z = typ3.read z in
+      (x, y, z)
+    in
+    let store (x, y, z) =
+      let open Store.Let_syntax in
+      let%map x = typ1.store x and y = typ2.store y and z = typ3.store z in
+      (x, y, z)
+    in
+    let check (x, y, z) =
+      let open Checked in
+      let%map () = typ1.check x and () = typ2.check y and () = typ3.check z in
+      ()
+    in
+    {read; store; alloc; check}
+end
+
+include T
