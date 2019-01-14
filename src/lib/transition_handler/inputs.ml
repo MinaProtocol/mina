a65c586aa5cb73461ec27eca57ad016376ef0244
John Wu <johnwu93@gmail.com>
2019-01-13 22:20:39 -0800

    Created test stubs for transition frontier controller. Created a unit test for transition handler processor
    Co-authored-by: johnwu93 <johnwu93@users.noreply.github.com> Co-authored-by: nholland94 <nholland94@users.noreply.github.com>

--- src/lib/transition_handler/inputs.ml
+++ src/lib/transition_handler/inputs.ml
@@ -21,21 +21,3 @@ module type S = sig
      and type staged_ledger_diff := Staged_ledger_diff.t
      and type transaction_snark_scan_state := Staged_ledger.Scan_state.t
 end
-
-(*
-module Test : S = struct
-  module Proof = Test_mocks.Proof.Bool
-
-  module Consensus_mechanism = struct
-    (* we are only interested in mocking the external transition *)
-    include Test_mocks.Consensus.Mechanism.Stubs.Make (struct
-      module Consensus_state = Test_mocks.Consensus.State.Stubs.Full
-      module Blockchain_state = Test_mocks.Blockchain_state.Stubs.Full
-      module Protocol_state = Test_mocks.Protocol_state.Stubs.Full (Blockchain_state) (Consensus_state)
-      module External_transition = Test_mocks.External_transition.Int (Protocol_state)
-      module Snark_transition = Test_mocks.Snark_Transition.Stubs.Full (Protocol_state)
-      module Internal_transition = Test_mocks.Internal_transition.Stubs.Full (Protocol_state) (Snark_transition)
-    end)
-  end
-end
- *)
