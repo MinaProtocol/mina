b617bbf78f1ce25cc2e85c7977fefa8be61b3dbc
Matthew Ryan <MR_1993@hotmail.co.uk>
2019-01-17 18:14:09 -0800

    Pull in snarky as a submodule (#1463)
    * Move snarky to snarky-old
    
    * Add snarky as a submodule
    
    * Remove snarky-old
    
    * Mention the snarky submodule in the developer readme
    
    * Add --recursive to git submodule init in docs
    
    * Pull in submodules in circleci

--- src/lib/snarky/src/backends.ml
+++
@@ -1,3 +0,0 @@
-module Mnt4 = Libsnark.Mnt4
-module Mnt6 = Libsnark.Mnt6
-module Bn128 = Libsnark.Bn128
--- src/lib/snarky/src/camlsnark_c/libsnark-caml/depends/libsnark-supercop/src/crypto_hash/sha512/openssl/hash.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <openssl/sha.h>
-#include "crypto_hash_sha512.h"
-
-int crypto_hash_sha512(unsigned char *out,const unsigned char *in,unsigned long long inlen)
-{
-  SHA512(in,inlen,out);
-  return 0;
-}
