a4bbb55f0e721bc49cd4dce71d47c94c07314bfd
Nathan Holland <nholland94@gmail.com>
2019-01-30 17:57:21 -0800

    Update circleci for new configs

--- .circleci/render.py
+++ .circleci/render.py
@@ -14,14 +14,21 @@ base_actions = [
     'transaction-snark-profiler -check-only'
 ]
 
+build_profiles = [
+    'dev',
+    'testnet_posig',
+    'testnet_postake',
+    'testnet_public'
+]
+
 tests = [
    {'friendly': 'Fake Hash', 'config':'fake_hash',  'name': 'fake_hash_full_test', 'env_str': "CODA_PROPOSAL_INTERVAL=8000", 'actions': ['full-test']},
-   {'friendly': 'Sig Tests', 'config':'test_sigs',  'name': 'all_sig_integration_tests', 'env_str': "CODA_PROPOSAL_INTERVAL=8000", 'actions': base_actions},
-   {'friendly': 'Stake Tests', 'config':'test_stakes', 'name': 'all_stake_integration_tests', 'env_str': "CODA_SLOT_INTERVAL=8000 CODA_UNFORKABLE_TRANSITION_COUNT=24 CODA_PROBABLE_SLOTS_PER_TRANSITION_COUNT=8", 'actions': base_actions}
+   {'friendly': 'Sig Tests', 'config':'test_posig',  'name': 'all_sig_integration_tests', 'env_str': "CODA_PROPOSAL_INTERVAL=8000", 'actions': base_actions},
+   {'friendly': 'Stake Tests', 'config':'test_postake', 'name': 'all_stake_integration_tests', 'env_str': "CODA_SLOT_INTERVAL=8000 CODA_UNFORKABLE_TRANSITION_COUNT=24 CODA_PROBABLE_SLOTS_PER_TRANSITION_COUNT=8", 'actions': base_actions}
 ]
 
 # Render it
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
 template = env.get_template('./config.yml.jinja')
-rendered = template.render(tests=tests)
+rendered = template.render(tests=tests, build_profiles=build_profiles)
 print(rendered)
