b153267362c26639edf6b6abd5071a41c398b6b8
Nathan Holland <nholland94@gmail.com>
2019-01-31 13:55:00 -0800

    Reformat python dict and remove unused env vars

--- .circleci/render.py
+++ .circleci/render.py
@@ -22,9 +22,24 @@ build_profiles = [
 ]
 
 tests = [
-   {'friendly': 'Fake Hash', 'config':'fake_hash',  'name': 'fake_hash_full_test', 'env_str': "CODA_PROPOSAL_INTERVAL=8000", 'actions': ['full-test']},
-   {'friendly': 'Proof of Signature Tests', 'config':'test_posig_snarkless',  'name': 'posig_integration_tests', 'env_str': "CODA_PROPOSAL_INTERVAL=8000", 'actions': base_actions},
-   {'friendly': 'Proof of Stake Tests', 'config':'test_postake_snarkless', 'name': 'postake_integration_tests', 'env_str': "CODA_SLOT_INTERVAL=8000 CODA_UNFORKABLE_TRANSITION_COUNT=24 CODA_PROBABLE_SLOTS_PER_TRANSITION_COUNT=8", 'actions': base_actions}
+   {
+       'friendly': 'Fake Hash',
+       'config': 'fake_hash',
+       'name': 'fake_hash_full_test',
+       'actions': ['full-test']
+   },
+   {
+       'friendly': 'Proof of Signature Tests',
+       'config': 'test_posig_snarkless',
+       'name': 'posig_integration_tests',
+       'actions': base_actions
+   },
+   {
+       'friendly': 'Proof of Stake Tests',
+       'config': 'test_postake_snarkless',
+       'name': 'postake_integration_tests',
+       'actions': base_actions
+   }
 ]
 
 # Render it
