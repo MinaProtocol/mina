25f6fba4de06de77f3f81e603a85274d7a01c859
John Wu <johnwu93@gmail.com>
2019-02-08 14:50:28 -0800

    Made Visualizable_ledger even more robust

--- src/lib/merkle_ledger_tests/visualizable_ledger.ml
+++ src/lib/merkle_ledger_tests/visualizable_ledger.ml
@@ -45,8 +45,8 @@ module type Inputs_intf = sig
      and type account := Account.t
 end
 
-module Make (Inputs : Inputs_intf) =
-(* : S with type addr := Inputs.Location.Addr.t and type ledger := Inputs.Ledger.t *)
+module Make (Inputs : Inputs_intf) :
+  S with type addr := Inputs.Location.Addr.t and type ledger := Inputs.Ledger.t =
 struct
   open Inputs
 
@@ -59,15 +59,23 @@ struct
 
   type ('source, 'target) edge = {source: 'source; target: 'target}
 
-  type merkle_tree_edge =
-    | Inner of (Hash.t, Hash.t) edge
-    | Leaf of (Hash.t, Account.t option) edge
+  type target =
+    | Hash of Hash.t
+    | Empty_hash
+    | Account of Account.t
+    | Empty_account
+
+  type merkle_tree_edge = (Hash.t, target) edge
 
   type pretty_format_account = {public_key: string; balance: int}
 
-  type node = Account of pretty_format_account | Empty_hash of string
+  type pretty_target =
+    | Pretty_hash of string
+    | Pretty_account of pretty_format_account
+    | Pretty_empty_hash of int
+    | Pretty_empty_account of int
 
-  type t = {edges: (string, string) edge list; nodes: node list}
+  type t = (string, pretty_target) edge list
 
   let display_prefix_of_string string = String.prefix string 8
 
@@ -84,86 +92,101 @@ struct
     |> Immutable_array.to_list
 
   let visualize t ~(initial_address : Ledger.Addr.t) =
-    let rec bfs ~edges ~accounts ~seen_empty_hashes jobs =
+    let rec bfs ~(edges : merkle_tree_edge list) ~accounts jobs =
       match Queue.dequeue jobs with
-      | None -> (List.rev edges, accounts)
+      | None -> List.rev edges
       | Some address ->
           let parent_address = Addr.parent_exn address in
           let parent_hash =
             Ledger.get_inner_hash_at_addr_exn t parent_address
           in
           if Addr.is_leaf address then
-            let account = Ledger.get t (Location.Account address) in
-            let new_accounts =
-              Option.value_map account ~default:accounts ~f:(fun new_account ->
-                  assert (not @@ Set.mem accounts new_account) ;
-                  Set.add accounts new_account )
-            in
-            bfs
-              ~edges:(Leaf {source= parent_hash; target= account} :: edges)
-              ~seen_empty_hashes ~accounts:new_accounts jobs
+            match Ledger.get t (Location.Account address) with
+            | Some new_account ->
+                assert (not @@ Set.mem accounts new_account) ;
+                let new_accounts = Set.add accounts new_account in
+                bfs
+                  ~edges:
+                    ( {source= parent_hash; target= Account new_account}
+                    :: edges )
+                  ~accounts:new_accounts jobs
+            | None ->
+                bfs
+                  ~edges:({source= parent_hash; target= Empty_account} :: edges)
+                  ~accounts jobs
           else
             let current_hash = Ledger.get_inner_hash_at_addr_exn t address in
-            if
-              not
-              @@ Hash_set.mem
-                   (empty_hashes |> Hash.Hash_set.of_list)
-                   current_hash
-            then (
-              Queue.enqueue jobs (Addr.child_exn address Direction.Left) ;
-              Queue.enqueue jobs (Addr.child_exn address Direction.Right) )
-            else Hash_set.add seen_empty_hashes current_hash ;
-            bfs
-              ~edges:
-                (Inner {source= parent_hash; target= current_hash} :: edges)
-              ~accounts ~seen_empty_hashes jobs
+            let target : target =
+              if
+                not
+                @@ Hash_set.mem
+                     (empty_hashes |> Hash.Hash_set.of_list)
+                     current_hash
+              then (
+                Queue.enqueue jobs (Addr.child_exn address Direction.Left) ;
+                Queue.enqueue jobs (Addr.child_exn address Direction.Right) ;
+                Hash current_hash )
+              else Empty_hash
+            in
+            bfs ~edges:({source= parent_hash; target} :: edges) ~accounts jobs
     in
-    let seen_empty_hashes = Hash.Hash_set.of_list [] in
-    let edges, accounts =
+    let edges =
       bfs ~edges:[]
         ~accounts:(Set.empty (module Account))
-        ~seen_empty_hashes
         (Queue.of_list
            [ Addr.child_exn initial_address Direction.Left
            ; Addr.child_exn initial_address Direction.Right ])
     in
-    let string_edges =
-      List.map edges ~f:(function
-        | Inner {source; target} ->
-            {source= string_of_hash source; target= string_of_hash target}
-        | Leaf {source= source_hash; target= account_option} ->
-            let open Option.Let_syntax in
-            let account_string =
-              (let%map account = account_option in
-               string_of_account_key account)
-              |> Option.value ~default:"EMPTY_ACCOUNT"
-            in
-            {source= string_of_hash source_hash; target= account_string} )
+    let edges =
+      List.folding_map edges ~init:(0, 0)
+        ~f:(fun (empty_account_counter, empty_hash_counter) {source; target} ->
+          let source = string_of_hash source in
+          match target with
+          | Hash target_hash ->
+              ( (empty_account_counter, empty_hash_counter)
+              , {source; target= Pretty_hash (string_of_hash target_hash)} )
+          | Account account ->
+              let string_key = string_of_account_key account in
+              let pretty_account =
+                { public_key= string_key
+                ; balance= Account.balance account |> Balance.to_int }
+              in
+              ( (empty_account_counter, empty_hash_counter)
+              , {source; target= Pretty_account pretty_account} )
+          | Empty_hash ->
+              let new_empty_hash_counter = empty_hash_counter + 1 in
+              ( (empty_account_counter, new_empty_hash_counter)
+              , {source; target= Pretty_empty_hash new_empty_hash_counter} )
+          | Empty_account ->
+              let new_empty_account_counter = empty_account_counter + 1 in
+              ( (new_empty_account_counter, empty_hash_counter)
+              , {source; target= Pretty_empty_account new_empty_account_counter}
+              ) )
     in
-    let nodes =
-      List.map (Set.to_list accounts) ~f:(fun account ->
-          let string_key = string_of_account_key account in
-          Account
-            { public_key= string_key
-            ; balance= Account.balance account |> Balance.to_int } )
-      @ List.map (seen_empty_hashes |> Hash_set.to_list) ~f:(fun hash ->
-            Empty_hash (string_of_hash hash) )
-    in
-    {edges= string_edges; nodes}
+    edges
 
   module Dot_writer = struct
     let wrapper ~name body = sprintf "digraph %s { \n %s\n}" name body
 
-    let write ~path ~name {edges; nodes} =
+    let write_empty_entry ~id source count =
+      let empty_hash_id = sprintf "EMPTY_%s_%d" id count in
+      [ sprintf "\"%s\" -> \"%s\" " source empty_hash_id
+      ; sprintf "\"%s\" [shape=point]" empty_hash_id ]
+
+    let write ~path ~name edges =
       let body =
         List.map edges ~f:(fun {source; target} ->
-            sprintf "\"%s\" -> \"%s\" " source target )
-        @ List.map nodes ~f:(function
-            | Account {public_key; balance} ->
-                sprintf "\"%s\" [shape=record,label=\"{%s|%d}\"]" public_key
-                  public_key balance
-            | Empty_hash hash -> sprintf "\"%s\" [shape=point]" hash )
-        |> String.concat ~sep:"\n"
+            match target with
+            | Pretty_hash hash -> [sprintf "\"%s\" -> \"%s\" " source hash]
+            | Pretty_account {public_key; balance} ->
+                [ sprintf "\"%s\" -> \"%s\" " source public_key
+                ; sprintf "\"%s\" [shape=record,label=\"{%s|%d}\"]" public_key
+                    public_key balance ]
+            | Pretty_empty_hash count ->
+                write_empty_entry ~id:"HASH" source count
+            | Pretty_empty_account count ->
+                write_empty_entry ~id:"ACCOUNT" source count )
+        |> List.concat |> String.concat ~sep:"\n"
       in
       let code = wrapper ~name body in
       Writer.save path ~contents:code
