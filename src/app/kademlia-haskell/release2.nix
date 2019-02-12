46d276c8409a468d1fbf4ba36b16c294a482e17b
Echo Nolan <echo@echonolan.net>
2019-02-12 10:18:16 -0800

    Use stack.yaml as single source of truth for Haskell builds
    Updated stack.yaml to actually build the Kademlia executable properly, and
    replaced Brandon's hand written Nix expressions with ones auto-generated from
    stack.yaml with stack2nix. Also added .stack-work to .gitignore.

--- src/app/kademlia-haskell/release2.nix
+++ src/app/kademlia-haskell/release2.nix
@@ -1,26 +1,8 @@
-{ compiler ? "ghc863" }:
+# We use stack.yaml as a single source of truth for our Haskell builds. You can
+# use stack to build the package when you're writing Haskell, and nix to build
+# it for use in Coda. stack2nix is used to build packages.nix, based on the the
+# Stackage snapshot we use. To rebuild it, get stack2nix from
+# https://github.com/input-output-hk/stack2nix and run:
+# cd src/app/kademlia-haskell; stack2nix . > packages.nix
 
-let
-  config = {
-    packageOverrides = pkgs: rec {
-      haskell = pkgs.haskell // {
-        packages = pkgs.haskell.packages // {
-          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
-            overrides = haskellPackagesNew: haskellPackagesOld: rec {
-              kademlia = haskellPackagesNew.callPackage ./prefetch/kademlia1101.nix { };
-
-              project1 =
-                haskellPackagesNew.callPackage ./default.nix { };
-            };
-          };
-        };
-      };
-    };
-  };
-
-  pkgs = import <nixpkgs> { inherit config; };
-
-in
-  { project1 = pkgs.haskell.packages.${compiler}.project1;
-  }
-
+(import ./packages.nix {}).kademlia-haskell
