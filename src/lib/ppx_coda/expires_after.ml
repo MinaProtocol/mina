e498ac08bd06beb6cc3a0531518e0f850c34c4ed
Paul Steckler <psteckler@o1labs.org>
2019-02-12 14:45:47 -0800

    make ppx a dummy structure item if it succeeds

--- src/lib/ppx_coda/expires_after.ml
+++ src/lib/ppx_coda/expires_after.ml
@@ -7,7 +7,7 @@ open Asttypes
     Motivation: RFC 0012 indicates that RPC versions should eventually expire, in order
      to encourage nodes to upgrade their software
 
-    Usage (at top-level): let _ = [%%expires_after "20201231"]
+    Usage: [%%expires_after "20201231"]
 
     There will be a compile-time error if 
       - the date of compilation is past the given date, or
@@ -16,21 +16,28 @@ open Asttypes
 
 let name = "expires_after"
 
-let expand ~loc ~path:_ s =
+let expand ~loc ~path:_ str _delimiter =
   let date =
-    try Date.of_string_iso8601_basic s ~pos:0 with _ ->
-      Location.raise_errorf ~loc "Not a valid date; must be in form YYYYMMDD"
+    try
+      (* the of_string function below allows too-long strings, as long as it starts
+         with a valid date, so we do our own length check
+        *)
+      if String.length str.txt > 8 then
+        Location.raise_errorf ~loc:str.loc
+          "Not a valid date, string too long; must be in form YYYYMMDD" ;
+      Core_kernel.Date.of_string_iso8601_basic ~pos:0 str.txt
+    with _ ->
+      Location.raise_errorf ~loc:str.loc
+        "Not a valid date; must be in form YYYYMMDD"
   in
   let today = Date.today ~zone:Time.Zone.utc in
-  if Date.( >= ) today date then Location.raise_errorf ~loc "Code is expired"
-  else
-    [%expr
-      let _ = () in
-      ()]
+  if Date.( >= ) today date then
+    Location.raise_errorf ~loc:str.loc "Code is expired" ;
+  [%stri let () = ()]
 
 let ext =
-  Extension.declare name Extension.Context.expression
-    Ast_pattern.(single_expr_payload (estring __))
+  Extension.declare name Extension.Context.structure_item
+    Ast_pattern.(single_expr_payload (pexp_constant (pconst_string __' __)))
     expand
 
 let () =
