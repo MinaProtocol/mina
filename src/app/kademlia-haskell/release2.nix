ad3b72329fa380cb1a275af885c7010dc111e785
Echo Nolan <echo@echonolan.net>
2019-02-13 12:33:58 -0800

    Improve comment

--- src/app/kademlia-haskell/release2.nix
+++ src/app/kademlia-haskell/release2.nix
@@ -1,8 +1,9 @@
-# We use stack.yaml as a single source of truth for our Haskell builds. You can
-# use stack to build the package when you're writing Haskell, and nix to build
-# it for use in Coda. stack2nix is used to build packages.nix, based on the
-# Stackage snapshot we use. To rebuild it, get stack2nix from
-# https://github.com/input-output-hk/stack2nix and run:
+# We use stack.yaml as a single source of truth for our Haskell build
+# configuration. You can use Stack to build the package when you're writing
+# Haskell, and Nix to build it for use in Coda, and they should both use the
+# same dependencies, compiler version, etc. stack2nix is used to build
+# packages.nix, based on the Stackage snapshot we use. To rebuild it, get
+# stack2nix from https://github.com/input-output-hk/stack2nix and run:
 # cd src/app/kademlia-haskell; stack2nix . > packages.nix
 
 (import ./packages.nix {}).kademlia-haskell
