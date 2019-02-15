9f7c791de34932956085a6fc52756ccd1aedcc08
Corey Richardson <cmr@o1labs.org>
2019-02-14 16:33:41 -0800

    reformat

--- src/app/cli/src/coda_tracing.ml
+++ src/app/cli/src/coda_tracing.ml
@@ -4,9 +4,9 @@ open Async
 (** [start dir] starts writing the trace output to [dir ^/ (current_pid ^ ".trace")]. *)
 let start dir =
   O1trace.forget_tid (fun () ->
-  Writer.open_file ~append:true
-    (dir ^/ sprintf "%d.trace" (Unix.getpid () |> Pid.to_int))
-  >>| O1trace.start_tracing)
+      Writer.open_file ~append:true
+        (dir ^/ sprintf "%d.trace" (Unix.getpid () |> Pid.to_int))
+      >>| O1trace.start_tracing )
 
 (** Stop tracing. *)
 let stop () = O1trace.stop_tracing ()
