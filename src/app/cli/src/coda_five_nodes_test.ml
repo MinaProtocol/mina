b564b88b7ac82c7b04e336449c595d0596caaab7
Brandon Kase <bkase@o1labs.org>
2019-03-04 17:31:03 -0800

    Test ledger and integration test for 5 even stakes
    Purposefully, not adding this test to CI yet. Still needs to be tuned.

---
+++ src/app/cli/src/coda_five_nodes_test.ml
@@ -0,0 +1,28 @@
+open Core
+open Async
+open Coda_worker
+open Coda_main
+open Coda_base
+
+let name = "coda-five-nodes-test"
+
+let main () =
+  let log = Logger.create () in
+  let log = Logger.child log name in
+  let n = 5 in
+  let snark_work_public_keys = function
+    | 0 ->
+        Some
+          (List.nth_exn Genesis_ledger.accounts 0 |> snd |> Account.public_key)
+    | _ -> None
+  in
+  let%bind testnet =
+    Coda_worker_testnet.test log n Option.some snark_work_public_keys
+      Protocols.Coda_pow.Work_selection.Seq
+  in
+  after (Time.Span.of_min 45.)
+
+let command =
+  let open Command.Let_syntax in
+  Command.async ~summary:"Test that five nodes work"
+    (Command.Param.return main)
---
+++ src/lib/genesis_ledger/test_five_even_stakes.ml
@@ -0,0 +1,12 @@
+open Functor.With_private
+open Core
+
+(* TODO: generate new keypairs before public testnet *)
+include Make (struct
+  let accounts =
+    let balances = [1_000; 1_000; 1_000; 1_000; 1_000] in
+    List.mapi balances ~f:(fun i b ->
+        { balance= b
+        ; pk= fst Coda_base.Sample_keypairs.keypairs.(i)
+        ; sk= snd Coda_base.Sample_keypairs.keypairs.(i) } )
+end)
