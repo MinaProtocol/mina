e85dad4939ff29b58e7fe8474f042cb30fdd1205
John Wu <johnwu93@gmail.com>
2019-03-06 11:47:58 -0800

    Added visualizations to maskable merkle tree (#1785)
    * Added visualizations to maskable merkle tree
    
    * Closed output channels for visualizations
    
    * Fix compilation errors and syncable_ledger

--- src/lib/coda_base/sync_ledger.ml
+++ src/lib/coda_base/sync_ledger.ml
@@ -1,27 +1,39 @@
 open Core_kernel
 
-include Syncable_ledger.Make (Ledger.Location.Addr) (Account.Stable.V1)
-          (struct
-            include Ledger_hash
+module Hash = struct
+  include Ledger_hash
 
-            let hash_account = Fn.compose Ledger_hash.of_digest Account.digest
+  let hash_account = Fn.compose Ledger_hash.of_digest Account.digest
 
-            let empty_account = hash_account Account.empty
-          end)
-          (struct
-            include Ledger_hash
+  let empty_account = hash_account Account.empty
+end
 
-            let to_hash (h : t) =
-              Ledger_hash.of_digest (h :> Snark_params.Tick.Pedersen.Digest.t)
-          end)
-          (struct
-            include Ledger
+module Root_hash = struct
+  include Ledger_hash
 
-            let f = Account.hash
-          end)
-          (struct
-            let subtree_height = 3
-          end)
+  let to_hash (h : t) =
+    Ledger_hash.of_digest (h :> Snark_params.Tick.Pedersen.Digest.t)
+end
+
+module Mask = Syncable_ledger.Make (struct
+  module Addr = Ledger.Location.Addr
+  module MT = Ledger
+  module Account = Account.Stable.V1
+  module Hash = Hash
+  module Root_hash = Root_hash
+
+  let subtree_height = 3
+end)
+
+module Db = Syncable_ledger.Make (struct
+  module Addr = Ledger.Db.Addr
+  module MT = Ledger.Db
+  module Account = Account.Stable.V1
+  module Hash = Hash
+  module Root_hash = Root_hash
+
+  let subtree_height = 3
+end)
 
 type answer =
   ( Ledger.Location.Addr.t
--- src/lib/merkle_mask/masking_merkle_tree.ml
+++ src/lib/merkle_mask/masking_merkle_tree.ml
@@ -5,20 +5,9 @@ open Core
 
 (* builds a Merkle tree mask; it's a Merkle tree, with some additional
    operations *)
-module Make
-    (Key : Merkle_ledger.Intf.Key)
-    (Account : Merkle_ledger.Intf.Account with type key := Key.t)
-    (Hash : Merkle_ledger.Intf.Hash with type account := Account.t)
-    (Location : Merkle_ledger.Location_intf.S)
-    (Base : Base_merkle_tree_intf.S
-            with module Addr = Location.Addr
-             and module Location = Location
-            with type key := Key.t
-             and type key_set := Key.Set.t
-             and type hash := Hash.t
-             and type root_hash := Hash.t
-             and type account := Account.t) =
-struct
+module Make (Inputs : Inputs_intf.S) = struct
+  open Inputs
+
   type account = Account.t
 
   type hash = Hash.t
@@ -371,6 +360,7 @@ struct
       module Key = Key
       module Account = Account
       module Hash = Hash
+      module Balance = Balance
 
       module Depth = struct
         let depth = Base.depth
