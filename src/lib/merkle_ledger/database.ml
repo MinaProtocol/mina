4e53a9bebe23c24aca6bcba3f18a86bb0e42eb6a
Corey Richardson <cmr@o1labs.org>
2019-03-05 19:04:15 -0800

    remove that assertion for now
    our tests violate it a lot.

--- src/lib/merkle_ledger/database.ml
+++ src/lib/merkle_ledger/database.ml
@@ -256,16 +256,6 @@ module Make
   end
 
   let set mdb location account =
-    let location_stored_for_key =
-      Account_location.get mdb (Account.public_key account)
-      |> Result.map_error ~f:(Fn.compose Sexp.to_string_hum Db_error.sexp_of_t)
-      |> Result.ok_or_failwith
-    in
-    [%test_result: Location.t]
-      ~message:
-        "Database.set location for account differed from stored location \
-         mapping"
-      ~expect:location_stored_for_key location ;
     set_bin mdb location Account.bin_size_t Account.bin_write_t account ;
     set_hash mdb
       (Location.Hash (Location.to_path_exn location))
