81a9a7555abe3f0fa6257b6f369cc1fa10257875
Brandon Kase <bkase@o1labs.org>
2019-03-06 17:56:19 -0800

    Important for this pin to go before async_kernel

--- scripts/macos-setup.sh
+++ scripts/macos-setup.sh
@@ -58,8 +58,8 @@ if [[ $COMPILE_THINGS == "YES" ]]; then
   # Our pins
   env TERM=xterm opam pin -y add src/external/ocaml-sodium
   env TERM=xterm opam pin -y add src/external/rpc_parallel
-  env TERM=xterm opam pin -y add src/external/async_kernel
   env TERM=xterm opam pin -y add src/external/ocaml-extlib
+  env TERM=xterm opam pin -y add src/external/async_kernel
   eval $(opam config env)
 
   # Kademlia
