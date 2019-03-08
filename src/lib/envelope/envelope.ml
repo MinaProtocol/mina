13ca48c3c71f8a15ac0d787947d0c480d5f1606c
Brandon Kase <bkase@o1labs.org>
2019-03-07 18:50:39 -0800

    WIp

--- src/lib/envelope/envelope.ml
+++ src/lib/envelope/envelope.ml
@@ -1,12 +1,62 @@
 open Core
 open Network_peer
+open Module_version
 
 module Sender = struct
-  type t = Local | Remote of Peer.t [@@deriving sexp, bin_io]
+  module Stable = struct
+    module V1 = struct
+      module T = struct
+        let version = 1
+
+        type t = Local | Remote of Peer.t [@@deriving sexp, bin_io]
+      end
+
+      include T
+      include Registration.Make_latest_version (T)
+    end
+
+    module Latest = V1
+
+    module Module_decl = struct
+      let name = "envelope_sender"
+
+      type latest = Latest.t
+    end
+
+    module Registrar = Registration.Make (Module_decl)
+    module Registered_V1 = Registrar.Register (V1)
+  end
+
+  include Stable.Latest
 end
 
 module Incoming = struct
-  type 'a t = {data: 'a; sender: Sender.t} [@@deriving sexp, bin_io]
+  module Stable = struct
+    module V1 = struct
+      module T = struct
+        let version = 1
+
+        type 'a t = {data: 'a; sender: Sender.Stable.V1.t}
+        [@@deriving sexp, bin_io]
+      end
+
+      include T
+      include Registration.Make_latest_version (T)
+    end
+
+    module Latest = V1
+
+    module Module_decl = struct
+      let name = "envelope_incoming"
+
+      type latest = Latest.t
+    end
+
+    module Registrar = Registration.Make (Module_decl)
+    module Registered_V1 = Registrar.Register (V1)
+  end
+
+  include Stable.V1
 
   let sender {sender; _} = sender
 
