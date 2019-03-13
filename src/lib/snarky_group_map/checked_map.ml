05d5165ea7d402e628be8a30fcc2294db7d6ad11
Rebekah Mercer <rebekahmercer@hotmail.co.uk>
2019-03-13 09:58:24 -0700

    ocamlformating

--- src/lib/snarky_group_map/checked_map.ml
+++ src/lib/snarky_group_map/checked_map.ml
@@ -1,31 +1,36 @@
 module Make
     (M : Snarky.Snark_intf.Run) (Params : sig
         val a : M.Field.t
+
         val b : M.Field.t
     end) =
-    struct
+struct
+  module B =
+    Group_map.Make_group_map (struct
+        include M.Field
 
-  module B = Group_map.Make_group_map(struct
-    include M.Field
-    let t_of_sexp x = constant ( Constant.t_of_sexp x )
-    let negate x = zero - x
-  end)(Params)
+        let t_of_sexp x = constant (Constant.t_of_sexp x)
+
+        let negate x = zero - x
+      end)
+      (Params)
 
   open M
 
   (* let sqrt_flagged : Field.t -> Field.t * Boolean.var = *)
- let sqrt_flagged x =
+  let sqrt_flagged x =
     (* z = sqrt of x (if one exists) *)
-    let z = exists Field.typ
-        ~compute:As_prover.(map (read_var x) ~f:(
-            fun x ->
-              if M.Field.Constant.is_square x
-              then M.Field.Constant.sqrt x
-              else M.Field.Constant.one))
+    let z =
+      exists Field.typ
+        ~compute:
+          As_prover.(
+            map (read_var x) ~f:(fun x ->
+                if M.Field.Constant.is_square x then M.Field.Constant.sqrt x
+                else M.Field.Constant.one ))
     in
     let z2 = Field.(z * z) in
     let b = Field.equal z2 x in
-    z, b
+    (z, b)
 
   (* with (x1, b1), (x2, b2), (x3, b3), we need to take
    * only the first square. this means we can do
@@ -33,19 +38,17 @@ module Make
    *)
 
   let to_group x =
-    let f x = Field.(x * x * x + Params.a * x + Params.b) in
+    let f x = Field.((x * x * x) + (Params.a * x) + Params.b) in
     let x1 = B.make_x1 x in
     let x2 = B.make_x2 x in
     let x3 = B.make_x3 x in
     let y1, b1 = sqrt_flagged (f x1) in
     let y2, b2 = sqrt_flagged (f x2) in
     let y3, b3 = sqrt_flagged (f x3) in
-    Boolean.Assert.any [b1; b2; b3];
-
+    Boolean.Assert.any [b1; b2; b3] ;
     let x1_is_first = (b1 :> Field.t) in
-    let x2_is_first = (Boolean.(not b1 && b2) :> Field.t) in
+    let x2_is_first = (Boolean.((not b1) && b2) :> Field.t) in
     let x3_is_first = (b3 :> Field.t) in
-    (Field.(x1_is_first * x1 + x2_is_first * x2 + x3_is_first * x3),
-    Field.(x1_is_first * y1 + x2_is_first * y2 + x3_is_first * y3))
-
+    ( Field.((x1_is_first * x1) + (x2_is_first * x2) + (x3_is_first * x3))
+    , Field.((x1_is_first * y1) + (x2_is_first * y2) + (x3_is_first * y3)) )
 end
