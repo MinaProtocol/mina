bea3e92a35e86f149d597279ffa18ccb55cb4f56
Corey Richardson <cmr@o1labs.org>
2019-03-12 18:36:24 -0700

    tweak five-proposer test

--- src/app/cli/src/coda_shared_prefix_multiproposer_test.ml
+++ src/app/cli/src/coda_shared_prefix_multiproposer_test.ml
@@ -5,18 +5,21 @@ open Coda_main
 
 let name = "coda-shared-prefix-multiproposer-test"
 
-let main () =
+let main n () =
   let logger = Logger.create () in
-  let n = 2 in
   let snark_work_public_keys i = None in
   let%bind testnet =
     Coda_worker_testnet.test logger n Option.some snark_work_public_keys
       Protocols.Coda_pow.Work_selection.Seq
   in
-  let%bind () = after (Time.Span.of_sec 30.) in
+  let%bind () = after (Time.Span.of_min 3.) in
   Coda_worker_testnet.Api.teardown testnet
 
 let command =
   let open Command.Let_syntax in
   Command.async ~summary:"Test that workers share prefixes"
-    (Command.Param.return main)
+    (let%map_open num_proposers =
+       flag "num-proposers" ~doc:"NUM number of proposers to have"
+         (required int)
+     in
+     main num_proposers)
