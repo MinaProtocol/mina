dba844cf768766278a3978ca494ab2498c5225a6
Joel Krauska <jkrauska@gmail.com>
2019-03-14 11:56:05 -0700

    eq

--- scripts/skip_if_only_frontend.sh
+++ scripts/skip_if_only_frontend.sh
@@ -5,7 +5,7 @@ set -e
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 MASTER_HASH=$(git merge-base origin/master HEAD)
 
-if [ "${CURRENT_BRANCH}" = "master" ] ; then
+if [ "${CURRENT_BRANCH}" -eq "master" ] ; then
   # always run master
   exec "$@"
 elif git diff HEAD..${MASTER_HASH} --name-only | grep -E -q -v '^frontend'; then
