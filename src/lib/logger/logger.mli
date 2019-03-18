e983809ff5254f41013535e3868c53238c3c46d3
Corey Richardson <cmr@o1labs.org>
2019-03-18 14:38:34 -0700

    remove extra faulty_peer decl

--- src/lib/logger/logger.mli
+++ src/lib/logger/logger.mli
@@ -65,5 +65,3 @@ val faulty_peer : _ log_function [@@deprecated "use Trust_system.record"]
 val faulty_peer_without_punishment : _ log_function
 
 val fatal : _ log_function
-
-val faulty_peer : _ log_function
