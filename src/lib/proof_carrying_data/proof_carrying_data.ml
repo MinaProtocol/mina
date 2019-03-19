211632acbe3cf0c1f54e8594294c997134a50ff5
Paul Steckler <psteckler@o1labs.org>
2019-03-19 13:57:05 -0700

    Version Proof_carrying_data

--- src/lib/proof_carrying_data/proof_carrying_data.ml
+++ src/lib/proof_carrying_data/proof_carrying_data.ml
@@ -1,2 +1,14 @@
 (* A convenient data structure to bundle statements along with their proofs together *)
-type ('a, 'b) t = {data: 'a; proof: 'b} [@@deriving sexp, fields, bin_io]
+
+module Stable = struct
+  (* don't register versions, because of type parameters *)
+  module V1 = struct
+    type ('a, 'b) t = {data: 'a; proof: 'b} [@@deriving sexp, fields, bin_io]
+  end
+
+  module Latest = V1
+end
+
+(* bin_io omitted *)
+type ('a, 'b) t = ('a, 'b) Stable.Latest.t = {data: 'a; proof: 'b}
+[@@deriving sexp, fields]
