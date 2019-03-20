986d3384d5b11135993c43f456412d9dba016b31
Joel Krauska <jkrauska@gmail.com>
2019-03-20 14:46:39 -0700

    reformat

---
+++ src/lib/genesis_ledger/testnet_postake_ledger_many_proposers.ml
@@ -0,0 +1,46 @@
+open Functor.With_private
+open Core
+
+(* TODO: generate new keypairs before public testnet *)
+include Make (struct
+  let accounts =
+    let balances =
+      [ 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 5_000_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000
+      ; 1_000 ]
+    in
+    List.mapi balances ~f:(fun i b ->
+        { balance= b
+        ; pk= fst Coda_base.Sample_keypairs.keypairs.(i)
+        ; sk= snd Coda_base.Sample_keypairs.keypairs.(i) } )
+end)
