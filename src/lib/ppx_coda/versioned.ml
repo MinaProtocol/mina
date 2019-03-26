0b10367c6e4f23a6971add2f3dec10e703aff147
Paul Steckler <psteckler@o1labs.org>
2019-03-26 12:39:02 -0700

    add possibility of options; address PR comments

--- src/lib/ppx_coda/versioned.ml
+++ src/lib/ppx_coda/versioned.ml
@@ -11,9 +11,12 @@ open Core_kernel
 open Ppxlib
 
 let is_versioned_ident item =
+  let versioned_id id =
+    match id.txt with Lident s -> String.equal s "versioned" | _ -> false
+  in
   match item with
-  | Pexp_ident id -> (
-    match id.txt with Lident s -> String.equal s "versioned" | _ -> false )
+  | Pexp_ident id -> versioned_id id
+  | Pexp_apply ({pexp_desc= Pexp_ident id; _}, _) -> versioned_id id
   | _ -> false
 
 let payload_has_versioned payload =
@@ -25,6 +28,7 @@ let payload_has_versioned payload =
             (* the "versioned" can appear as a singleton ident, or in a tuple *)
             match expr.pexp_desc with
             | Pexp_ident _ -> is_versioned_ident expr.pexp_desc
+            | Pexp_apply ({pexp_desc; _}, _) -> is_versioned_ident pexp_desc
             | Pexp_tuple items ->
                 List.exists items ~f:(fun item ->
                     is_versioned_ident item.pexp_desc )
@@ -49,26 +53,28 @@ let validate_versioned type_decl inner3_modules =
         "Versioned type must be named \"t\", got: \"%s\""
         type_decl.ptype_name.txt ;
     match inner3_modules with
-    | ["T"; version_module; "Stable"] ->
-        let len = String.length version_module in
-        if (not (Char.equal version_module.[0] 'V')) && len > 1 then
-          Location.raise_errorf ~loc:type_decl.ptype_loc
+    | [ {txt= "T"; _}
+      ; {txt= version_name; loc= version_name_loc}
+      ; {txt= "Stable"; _} ] ->
+        let len = String.length version_name in
+        if not (Char.equal version_name.[0] 'V' && len > 1) then
+          Location.raise_errorf ~loc:version_name_loc
             "Versioning module containing versioned type must be named Vn, \
              for some number n"
         else
-          let numeric_part = String.sub version_module ~pos:1 ~len:(len - 1) in
+          let numeric_part = String.sub version_name ~pos:1 ~len:(len - 1) in
           String.iter numeric_part ~f:(fun c ->
               if not (Char.is_digit c) then
-                Location.raise_errorf ~loc:type_decl.ptype_loc
+                Location.raise_errorf ~loc:version_name_loc
                   "Versioning module name must be Vn, for some number n, got: \
                    \"%s\""
-                  version_module ) ;
+                  version_name ) ;
           (* invariant: 0th char is digit *)
           if Int.equal (Char.get_digit_exn numeric_part.[0]) 0 then
-            Location.raise_errorf ~loc:type_decl.ptype_loc
+            Location.raise_errorf ~loc:version_name_loc
               "Versioning module name must be Vn, for a number n, but n \
                cannot begin with 0, got: \"%s\""
-              version_module
+              version_name
     | _ ->
         Location.raise_errorf ~loc:type_decl.ptype_loc
           "Versioned type must be contained in module structure Stable.Vn.T, \
@@ -77,7 +83,7 @@ let validate_versioned type_decl inner3_modules =
 (* traverse AST *)
 let versioned_syntactic_check =
   object (self)
-    inherit [string list] Ast_traverse.fold_map as super
+    inherit [string loc list] Ast_traverse.fold_map as super
 
     (* acc is list of module names traversed, from innermost to outer *)
     method! structure_item str acc =
@@ -86,7 +92,7 @@ let versioned_syntactic_check =
       | Pstr_module ({pmb_name; pmb_expr; _} as module_details) -> (
         match pmb_expr.pmod_desc with
         | Pmod_structure structure ->
-            let new_acc = pmb_name.txt :: acc in
+            let new_acc = pmb_name :: acc in
             let results =
               List.map structure ~f:(fun si -> self#structure_item si new_acc)
             in
