18103cba27f751d9fab9713088b02556ae00195b
ghost!@shell <ghost.not.in.the.shell@gmail.com>
2019-03-25 18:10:10 -0700

    consult consensus to determine the transition_frontier_diff (#2019)
    * consult consensus to determine the transition_frontier_diff
    
    * minor modifications

--- src/lib/transition_frontier/transition_frontier.ml
+++ src/lib/transition_frontier/transition_frontier.ml
@@ -707,18 +707,18 @@ struct
         let distance_to_parent = node.length - root_node.length in
         let best_tip_node = Hashtbl.find_exn t.table t.best_tip in
         (* 3 *)
+        let best_tip_change =
+          Consensus.select
+            ~existing:(consensus_state_of_breadcrumb best_tip_node.breadcrumb)
+            ~candidate:(consensus_state_of_breadcrumb node.breadcrumb)
+            ~logger:
+              (Logger.create ()
+                 ~metadata:
+                   [ ( "selection context"
+                     , `String "comparing new breadcrumb to best tip" ) ])
+        in
         let added_to_best_tip_path, removed_from_best_tip_path =
-          match
-            Consensus.select
-              ~existing:
-                (consensus_state_of_breadcrumb best_tip_node.breadcrumb)
-              ~candidate:(consensus_state_of_breadcrumb breadcrumb)
-              ~logger:
-                (Logger.create ()
-                   ~metadata:
-                     [ ( "selection context"
-                       , `String "comparing new breadcrumb to best tip" ) ])
-          with
+          match best_tip_change with
           | `Keep -> ([], [])
           | `Take ->
               t.best_tip <- hash ;
@@ -839,18 +839,19 @@ struct
         in
         (* 5 *)
         Extensions.handle_diff t.extensions t.extension_writers
-          ( if node.length > best_tip_node.length then
-            Transition_frontier_diff.New_best_tip
-              { old_root= root_node.breadcrumb
-              ; old_root_length= root_node.length
-              ; new_root= new_root_node.breadcrumb
-              ; added_to_best_tip_path=
-                  Non_empty_list.of_list_opt added_to_best_tip_path
-                  |> Option.value_exn
-              ; new_best_tip_length= node.length
-              ; removed_from_best_tip_path
-              ; garbage= garbage_breadcrumbs }
-          else Transition_frontier_diff.New_breadcrumb node.breadcrumb ) )
+          ( match best_tip_change with
+          | `Keep -> Transition_frontier_diff.New_breadcrumb node.breadcrumb
+          | `Take ->
+              Transition_frontier_diff.New_best_tip
+                { old_root= root_node.breadcrumb
+                ; old_root_length= root_node.length
+                ; new_root= new_root_node.breadcrumb
+                ; added_to_best_tip_path=
+                    Non_empty_list.of_list_opt added_to_best_tip_path
+                    |> Option.value_exn
+                ; new_best_tip_length= node.length
+                ; removed_from_best_tip_path
+                ; garbage= garbage_breadcrumbs } ) )
 
   let add_breadcrumb_if_present_exn t breadcrumb =
     let parent_hash = Breadcrumb.parent_hash breadcrumb in
