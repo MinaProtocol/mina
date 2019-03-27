62a31f8c83308649961ed9ebb5e72a487ddd8b1d
ghost!@shell <ghost.not.in.the.shell@gmail.com>
2019-03-27 15:53:17 -0700

    five proposers with transactions (#2036)

--- src/app/cli/src/coda_shared_prefix_multiproposer_test.ml
+++ src/app/cli/src/coda_shared_prefix_multiproposer_test.ml
@@ -2,19 +2,22 @@ open Core
 open Async
 open Coda_worker
 open Coda_main
+open Signature_lib
 
 let name = "coda-shared-prefix-multiproposer-test"
 
 let main n enable_payments () =
   let logger = Logger.create () in
-  let snark_work_public_keys i = None in
-  let%bind testnet =
-    Coda_worker_testnet.test logger n Option.some snark_work_public_keys
-      Protocols.Coda_pow.Work_selection.Seq
-  in
   let keypairs =
     List.map Genesis_ledger.accounts
       ~f:Genesis_ledger.keypair_of_account_record_exn
+  in
+  let snark_work_public_keys i =
+    Some ((List.nth_exn keypairs i).public_key |> Public_key.compress)
+  in
+  let%bind testnet =
+    Coda_worker_testnet.test logger n Option.some snark_work_public_keys
+      Protocols.Coda_pow.Work_selection.Seq
   in
   let%bind () =
     if enable_payments then
