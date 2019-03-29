ed8e28c2d1ad7c03eec28470bb573fbc07568909
ghost!@shell <ghost.not.in.the.shell@gmail.com>
2019-03-29 00:28:04 -0700

    common ancestor should return the youngest ancestor instead of the oldest one (#2056)
    * common ancestor should return the youngest ancestor instead of the oldest one
    
    * tests for common ancestors
    
    * make the test more randomized
    
    * adding test for branches with same lengths

--- src/lib/transition_frontier_controller_tests/test_transition_frontier.ml
+++ src/lib/transition_frontier_controller_tests/test_transition_frontier.ml
@@ -1,9 +1,10 @@
 open Core
 open Async
+open Coda_base
 
 let%test_module "Root_history and Transition_frontier" =
   ( module struct
-    let max_length = 3
+    let max_length = 5
 
     module Stubs = Stubs.Make (struct
       let max_length = max_length
@@ -27,6 +28,48 @@ let%test_module "Root_history and Transition_frontier" =
       List.equal ~equal:Transition_frontier.Breadcrumb.equal
 
     let logger = Logger.null ()
+
+    let common_ancestor_test ancestor_length branch1_length branch2_length =
+      Async.Thread_safe.block_on_async_exn (fun () ->
+          let%bind frontier = create_root_frontier ~logger in
+          let root = Transition_frontier.root frontier in
+          let%bind ancestors =
+            create_breadcrumbs ~logger ~size:ancestor_length root
+          in
+          let youngest_ancestor = List.last_exn ancestors in
+          let%bind () =
+            Deferred.List.iter ancestors ~f:(fun ancestor ->
+                Transition_frontier.add_breadcrumb_exn frontier ancestor )
+          in
+          let%bind branch1 =
+            create_breadcrumbs ~logger ~size:branch1_length youngest_ancestor
+          in
+          let%bind branch2 =
+            create_breadcrumbs ~logger ~size:branch2_length youngest_ancestor
+          in
+          let bc1, bc2 = (List.last_exn branch1, List.last_exn branch2) in
+          let%map () =
+            Deferred.List.iter (List.append branch1 branch2)
+              ~f:(fun breadcrumb ->
+                Transition_frontier.add_breadcrumb_exn frontier breadcrumb )
+          in
+          State_hash.equal
+            (Transition_frontier.common_ancestor frontier bc1 bc2)
+            (Transition_frontier.Breadcrumb.state_hash youngest_ancestor) )
+
+    let%test "common_ancestor should find the youngest ancestor when two \
+              branches have the same lengths" =
+      let ancestor_length = max_length / 2 in
+      common_ancestor_test ancestor_length
+        (max_length - ancestor_length)
+        (max_length - ancestor_length)
+
+    let%test "common_ancestor should find the youngest ancestor when two \
+              branches have randomized lengths" =
+      let ancestor_length = max_length / 2 in
+      common_ancestor_test ancestor_length
+        (1 + Random.int (max_length - ancestor_length))
+        (1 + Random.int (max_length - ancestor_length))
 
     let%test "If a transition does not exists in the transition_frontier or \
               in the root_history, then we should not get an answer" =
