153d67697e69a42113dbd2a210079b5275691c28
Joel Krauska <jkrauska@gmail.com>
2019-04-03 12:02:52 -0700

    removed commented line

--- scripts/test.py
+++ scripts/test.py
@@ -12,7 +12,6 @@ from itertools import chain
 build_artifact_profiles = [
     'testnet_postake',
     'testnet_postake_many_proposers',
-    # 'testnet_postake_medium_curves',
     'testnet_postake_snarkless_fake_hash',
 ]
 
