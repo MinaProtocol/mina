71db724c28c374409ee4523e3ed06e4efbe1f780
Izaak Meckler <ihmeckler@gmail.com>
2019-04-09 14:50:23 -0700

    use correct parameters for big curves

--- src/lib/crypto_params/crypto_params_init/crypto_params_init.ml
+++ src/lib/crypto_params/crypto_params_init/crypto_params_init.ml
@@ -5,11 +5,15 @@
 curve_size = 298]
 
 module Cycle = Snarky.Libsnark.Mnt298
+module Snarkette_tick = Snarkette.Mnt6_80
+module Snarkette_tock = Snarkette.Mnt4_80
 
 [%%elif
 curve_size = 753]
 
 module Cycle = Snarky.Libsnark.Mnt753
+module Snarkette_tick = Snarkette.Mnt6753
+module Snarkette_tock = Snarkette.Mnt4753
 
 [%%else]
 
--- src/lib/snark_params/snark_params.ml
+++ src/lib/snark_params/snark_params.ml
@@ -6,6 +6,8 @@ open Bitstring_lib
 open Snark_bits
 module Tick_backend = Crypto_params.Tick_backend
 module Tock_backend = Crypto_params.Tock_backend
+module Snarkette_tick = Crypto_params.Snarkette_tick
+module Snarkette_tock = Crypto_params.Snarkette_tock
 
 module Make_snarkable (Impl : Snarky.Snark_intf.S) = struct
   open Impl
@@ -128,7 +130,6 @@ module Tock = struct
 
   module Groth16 = Snarky.Snark.Make (Tock_backend.Full.Default)
   module Fq = Snarky_field_extensions.Field_extensions.F (Tock0)
-  module Snarkette_tock = Snarkette.Mnt4_80
 
   module Inner_curve = struct
     include Tock_backend.Inner_curve
@@ -499,7 +500,6 @@ module Tick = struct
   end
 
   module Util = Snark_util.Make (Tick0)
-  module Snarkette_tick = Snarkette.Mnt6_80
 
   module Pairing = struct
     module T = struct
