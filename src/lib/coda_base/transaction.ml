41059138268627a3abbf4b5d97d410dc2b6a815f
John Wu <johnwu93@gmail.com>
2019-04-10 18:47:09 -0700

    Versioning Snark Worker and Pending Coinbase (#2194)
    * Versioning Snark Worker and Pending Coinbase
    
    * Addressed PR comments
    
    * Added Stable module into Statment module

--- src/lib/coda_base/transaction.ml
+++ src/lib/coda_base/transaction.ml
@@ -1,14 +1,26 @@
 open Core
 
-(* TODO : version *)
-type fee_transfer = Fee_transfer.Stable.V1.t [@@deriving bin_io, sexp]
+module Stable = struct
+  module V1 = struct
+    module T = struct
+      type t =
+        | User_command of User_command.With_valid_signature.Stable.V1.t
+        | Fee_transfer of Fee_transfer.Stable.V1.t
+        | Coinbase of Coinbase.Stable.V1.t
+      [@@deriving bin_io, sexp, version, compare, eq]
+    end
 
-(* TODO : version *)
-type t =
-  | User_command of User_command.With_valid_signature.t
-  | Fee_transfer of Fee_transfer.Stable.V1.t
-  | Coinbase of Coinbase.Stable.V1.t
-[@@deriving bin_io, sexp]
+    include T
+  end
+
+  module Latest = V1
+end
+
+type t = Stable.Latest.t =
+  | User_command of User_command.With_valid_signature.Stable.Latest.t
+  | Fee_transfer of Fee_transfer.Stable.Latest.t
+  | Coinbase of Coinbase.Stable.Latest.t
+[@@deriving sexp, compare, eq]
 
 let fee_excess = function
   | User_command t ->
--- src/lib/coda_networking/coda_networking.ml
+++ src/lib/coda_networking/coda_networking.ml
@@ -56,7 +56,7 @@ struct
         type response =
           ( Staged_ledger_aux.Stable.V1.t
           * Ledger_hash.Stable.V1.t
-          * Pending_coinbase.t )
+          * Pending_coinbase.Stable.V1.t )
           option
       end
 
@@ -81,12 +81,10 @@ struct
         type response =
           ( Staged_ledger_aux.Stable.V1.t
           * Ledger_hash.Stable.V1.t
-          * Pending_coinbase.t )
+          * Pending_coinbase.Stable.V1.t )
           option
-        [@@deriving bin_io]
+        [@@deriving bin_io, version {rpc}]
 
-        (* , version {rpc} *)
-        
         (* TODO : remove after uncommenting version{rpc} *)
         let version = 1
 
