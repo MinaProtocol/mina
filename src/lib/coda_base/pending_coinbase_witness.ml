f3d5c056958f56c3e36e350677a91a8b46ebb64c
John Wu <johnwu93@gmail.com>
2019-04-10 20:04:33 -0700

    Versioned pending coinbase witness

--- src/lib/coda_base/pending_coinbase_witness.ml
+++ src/lib/coda_base/pending_coinbase_witness.ml
@@ -1,6 +1,29 @@
 open Core_kernel
+open Module_version
 
-(* TODO: version *)
-type t =
-  {pending_coinbases: Pending_coinbase.Stable.Latest.t; is_new_stack: bool}
-[@@deriving bin_io, sexp]
+module Stable = struct
+  module V1 = struct
+    module T = struct
+      type t =
+        {pending_coinbases: Pending_coinbase.Stable.V1.t; is_new_stack: bool}
+      [@@deriving bin_io, sexp, version]
+    end
+
+    include T
+    include Registration.Make_latest_version (T)
+  end
+
+  module Latest = V1
+
+  module Module_decl = struct
+    let name = "pending_coinbase_witness"
+
+    type latest = Latest.t
+  end
+
+  module Registrar = Registration.Make (Module_decl)
+  module Registered_V1 = Registrar.Register (V1)
+end
+
+type t = Stable.Latest.t =
+  {pending_coinbases: Pending_coinbase.Stable.V1.t; is_new_stack: bool}
