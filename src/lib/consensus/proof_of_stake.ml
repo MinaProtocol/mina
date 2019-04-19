51b70b3ce57612f92207aad98f43de7d9b1c0753
Corey Richardson <cmr@o1labs.org>
2019-04-19 15:43:18 -0700

    no

--- src/lib/consensus/proof_of_stake.ml
+++ src/lib/consensus/proof_of_stake.ml
@@ -2331,8 +2331,8 @@ let next_proposal now (state : Consensus_state.Value.t) ~local_state ~keypair
     in
     if
       Epoch.equal curr_epoch state.curr_epoch
-      && Epoch.Slot.equal curr_slot state.curr_slot
-    then Epoch.incr (curr_epoch, curr_slot)
+      && Epoch.Slot.equal curr_slot state.curr_slot th
+    then (Logger.fatal logger "why did this happen?" ~module_:__MODULE__ ~location:__LOC__; Epoch.incr (curr_epoch, curr_slot))
     else (curr_epoch, curr_slot)
   in
   Logger.info logger ~module_:__MODULE__ ~location:__LOC__
