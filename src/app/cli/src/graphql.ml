c650a48d865bcd00d4643e9af1b99067eec83b1c
John Wu <johnwu93@gmail.com>
2019-04-23 13:15:13 -0700

    Removed scalar types

--- src/app/cli/src/graphql.ml
+++ src/app/cli/src/graphql.ml
@@ -11,75 +11,57 @@ module Make (Program : Coda_inputs.Main_intf) = struct
   open Inputs
 
   module Types = struct
-    module Scalars = struct
-      let public_key : (t, Public_key.Compressed.t option) typ =
-        scalar
-          ~doc:
-            "64-bit respresentation of public key that is compressed to make \
-             snark computation efficent"
-          "public_key"
-          ~coerce:
-            (Fn.compose Yojson.Safe.to_basic Public_key.Compressed.to_yojson)
+    module Stringable = struct
+      (** 64-bit respresentation of public key that is compressed to make snark computation efficent *)
+      let public_key = Public_key.Compressed.to_base64
 
-      let date : (t, Time.t option) typ =
-        scalar
-          ~doc:
-            "String representation of the Unix form of time, which is the \
-             number of milliseconds that elapsed from January 1, 1970"
-          "time" ~coerce:(fun time ->
-            let string_time =
-              Time.to_span_since_epoch time
-              |> Time.Span.to_ms |> Int64.to_string
-            in
-            `String string_time )
+      (** Unix form of time, which is the number of milliseconds that elapsed from January 1, 1970 *)
+      let date time =
+        Time.to_span_since_epoch time |> Time.Span.to_ms |> Int64.to_string
 
-      let uint64 : (t, Unsigned.UInt64.t option) typ =
-        scalar
-          ~doc:
-            "String representation of Unsigned Int64 (Javascript only has \
-             64-bit strings)"
-          "uint64" ~coerce:(fun uint64 ->
-            `String (Unsigned.UInt64.to_string uint64) )
+      (** Javascript only has 53-bit integers so we need to make them into strings  *)
+      let uint64 uint64 = Unsigned.UInt64.to_string uint64
     end
 
     (* TODO: include submitted_at (date) and included_at (date). These two fields are not exposed in the user_command *)
     let payment : (t, User_command.t option) typ =
-      let open Scalars in
       obj "payment" ~fields:(fun _ ->
           [ field "nonce" ~typ:(non_null int)
               ~args:Arg.[]
               ~resolve:(fun _ payment ->
                 User_command_payload.nonce @@ User_command.payload payment
                 |> Account.Nonce.to_int )
-          ; field "sender" ~typ:(non_null public_key)
+          ; field "sender" ~typ:(non_null string)
               ~args:Arg.[]
-              ~resolve:(fun _ payment -> User_command.sender payment)
-          ; field "receiver" ~typ:(non_null public_key)
+              ~resolve:(fun _ payment ->
+                User_command.sender payment |> Stringable.public_key )
+          ; field "receiver" ~typ:(non_null string)
               ~args:Arg.[]
               ~resolve:(fun _ payment ->
                 match
                   User_command_payload.body (User_command.payload payment)
                 with
                 | Payment {Payment_payload.Poly.receiver; _} ->
-                    receiver
+                    receiver |> Stringable.public_key
                 | Stake_delegation _ ->
                     failwith "Payment should not consist of a stake delegation"
                 )
-          ; field "amount" ~typ:(non_null uint64)
+          ; field "amount" ~typ:(non_null string)
               ~args:Arg.[]
               ~resolve:(fun _ payment ->
                 match
                   User_command_payload.body (User_command.payload payment)
                 with
                 | Payment {Payment_payload.Poly.amount; _} ->
-                    amount |> Currency.Amount.to_uint64
+                    amount |> Currency.Amount.to_uint64 |> Stringable.uint64
                 | Stake_delegation _ ->
                     failwith "Payment should not consist of a stake delegation"
                 )
-          ; field "fee" ~typ:(non_null uint64)
+          ; field "fee" ~typ:(non_null string)
               ~args:Arg.[]
               ~resolve:(fun _ payment ->
-                User_command.fee payment |> Currency.Fee.to_uint64 )
+                User_command.fee payment |> Currency.Fee.to_uint64
+                |> Stringable.uint64 )
           ; field "memo" ~typ:(non_null string)
               ~args:Arg.[]
               ~resolve:(fun _ payment ->
