a979201cdc493fcd39c2dbf37a44f1b31f147746
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2019-04-29 11:06:47 -0700

    let the waiting time to be an optional cmd arg

--- src/app/cli/src/coda_long_fork.ml
+++ src/app/cli/src/coda_long_fork.ml
@@ -6,7 +6,7 @@ open Coda_base
 
 let name = "coda-long-fork"
 
-let main n () =
+let main n waiting_time () =
   let logger = Logger.create () in
   let keypairs =
     List.map Genesis_ledger.accounts
@@ -21,14 +21,14 @@ let main n () =
     Coda_worker_testnet.test logger n Option.some snark_work_public_keys
       Protocols.Coda_pow.Work_selection.Seq ~max_concurrent_connections:None
   in
-  let epoche_duration =
+  let epoch_duration =
     Consensus.Constants.(block_window_duration_ms * 3 * c * k)
   in
   let%bind () =
     Coda_worker_testnet.Restarts.restart_node testnet ~logger ~node:1
-      ~duration:(Time.Span.of_ms (2 * epoche_duration |> Float.of_int))
+      ~duration:(Time.Span.of_ms (2 * epoch_duration |> Float.of_int))
   in
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after (Time.Span.of_min (waiting_time |> Float.of_int)) in
   Coda_worker_testnet.Api.teardown testnet
 
 let command =
@@ -37,5 +37,9 @@ let command =
     (let%map_open num_proposers =
        flag "num-proposers" ~doc:"NUM number of proposers to have"
          (required int)
+     and waiting_time =
+       flag "waiting-time"
+         ~doc:"the waiting time after the nodes coming back alive"
+         (optional_with_default 2 int)
      in
-     main num_proposers)
+     main num_proposers waiting_time)
