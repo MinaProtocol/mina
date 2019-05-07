7296f732573e3eabba4ddede5681ac6bcebe4a02
Avery Morin <avery.schmavery@gmail.com>
2019-05-06 17:13:43 -0700

    Fix small js linting issue

--- frontend/wallet/rollup.config.js
+++ frontend/wallet/rollup.config.js
@@ -1,11 +1,11 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
-  input: './lib/js/src/main/app.js',
+  input: "./lib/js/src/main/app.js",
   output: {
-    file: './app.js',
-    format: 'cjs'
+    file: "./app.js",
+    format: "cjs"
   },
   external: ["electron"],
   plugins: [
