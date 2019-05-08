5e047354326ae6bdc8dfcceed11a6384ce421768
John Wu <johnwu93@gmail.com>
2019-05-08 09:24:30 -0700

    Fixed comments

--- src/lib/transaction_database/intf.ml
+++ src/lib/transaction_database/intf.ml
@@ -2,9 +2,9 @@ open Coda_base
 open Signature_lib
 
 (** Transaction database is a database that stores transactions for a client.
-    It would query a set of transactions involving a peer with a certain public
-    key. The main purpose for this database is for a developer to store
-    transactions and perform pagination queries of transactions via GraphQL *)
+    It queries a set of transactions involving a peer with a certain public
+    key. This database allows a developer to store transactions and perform
+    pagination queries of transactions via GraphQL *)
 module type S = sig
   type t
 
@@ -16,18 +16,17 @@ module type S = sig
 
   val add : t -> Transaction.t -> time -> unit
 
-  (** [get_transactions t pk] would query all the transactions involving [pk]
-      as a participant. Specifically, it would query all the transactions [pk]
-      sent or received. *)
+  (** [get_transactions t pk] queries all the transactions involving [pk] as a
+      participant. Specifically, it queries all the transactions [pk] sent or
+      received. *)
   val get_transactions : t -> Public_key.Compressed.t -> Transaction.t list
 
-  (** [get_earlier_transactions t pk txn n] would query [n] transactions
-      involving peer, [pk], that were added before [txn], exclusively. It would
-      indicate if there are any earlier transactions that were added before the
-      earliest transaction in the query. It would also indicate any
-      transactions that occurred after [txn]. If there are no transactions
-      related to [pk] in the database, it would output an empty list of
-      transactions. *)
+  (** [get_earlier_transactions t pk txn n] queries [n] transactions involving
+      peer, [pk], added before [txn], exclusively. It indicates if there are
+      any earlier transactions added before the earliest transaction in the
+      query. It also indicates any transactions that occurred after [txn]. It
+      outputs an empty list of transactions if there are no transactions
+      related to [pk] in the database *)
   val get_earlier_transactions :
        t
     -> Public_key.Compressed.t
@@ -37,12 +36,12 @@ module type S = sig
        * [`Has_earlier_page of bool]
        * [`Has_later_page of bool]
 
-  (* [get_later_transactions t pk txn n] would query [n] transactions involving
-     peer, [pk], that were added after [txn], exclusively. It would indicate if
-     there are any later transactions that were added after the latest
-     transaction in the query. It would also indicate any transactions that
-     occurred before [txn]. If there are no transactions related to [pk] in the
-     database, it would output an empty list of transactions. *)
+  (** [get_later_transactions t pk txn n] queries [n] transactions involving
+      peer, [pk], added after [txn], exclusively. It would indicate if there
+      are any later transactions added after the latest transaction in the
+      query. It also indicates any transactions that occurred before [txn]. It
+      would output an empty list of transactions if there are no transactions
+      related to [pk] in the database *)
   val get_later_transactions :
        t
     -> Public_key.Compressed.t
