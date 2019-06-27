713868b50171a4e0d5fe2d5c1fdd624082650cee
Paul Steckler <steck@stecksoft.com>
2019-06-26 17:36:26 -0700

    dir for core files

--- scripts/gather-coredumps.sh
+++ scripts/gather-coredumps.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# gather core files for CI to help debug test failures
+
+CORE_DIR=core_dumps
+
+rm -rf $CORE_DIR
+mkdir $CORE_DIR
+
 for file in `find . -name "core.[0-9]*.*"` ;
-  do cp "$file" . ;
+  do cp "$file" $CORE_DIR/ ;
 done
