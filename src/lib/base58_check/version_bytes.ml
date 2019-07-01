72116c2707c22e9972c64fbfe58bcaeed2e7d2dd
Paul Steckler <psteckler@o1labs.org>
2019-07-01 15:18:36 -0700

    restore deleted version_byte for lite_precomputed

--- src/lib/base58_check/version_bytes.ml
+++ src/lib/base58_check/version_bytes.ml
@@ -30,6 +30,8 @@ let user_command_memo : t = '\xA2'
 
 let lite_params : t = '\x21'
 
+let lite_precomputed : t = '\xBC'
+
 let receipt_chain_hash : t = '\x9D'
 
 let secret_box_byteswr : t = '\x02'
