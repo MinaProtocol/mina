3425a2d284c7e4476c1c4f47e1dafd7ee027707a
Brandon Kase <bkase@o1labs.org>
2019-07-16 18:22:27 -0700

    Requested changes

--- src/lib/kademlia/membership.ml
+++ src/lib/kademlia/membership.ml
@@ -248,6 +248,7 @@ module Haskell_process = struct
           match prefix with
           | "DBUG" ->
               Logger.debug logger ~module_:__MODULE__ ~location:__LOC__ "%s"
+                ~metadata:[("kademlia_level", `String "DBUG")]
                 line_no_prefix ;
               None
           | "TRAC" ->
@@ -255,11 +256,13 @@ module Haskell_process = struct
               None
           | "EROR" ->
               Logger.error logger ~module_:__MODULE__ ~location:__LOC__ "%s"
+                ~metadata:[("kademlia_level", `String "EROR")]
                 line_no_prefix ;
               None
           | "DATA" ->
               (* Too noisy to put in info logs *)
               Logger.debug logger ~module_:__MODULE__ ~location:__LOC__ "%s"
+                ~metadata:[("kademlia_level", `String "DATA")]
                 line_no_prefix ;
               Some [line_no_prefix]
           | _ ->
