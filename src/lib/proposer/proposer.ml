6600f9c1c630ac530efc1ed67e7111e717f7b4f5
Paul Steckler <steck@stecksoft.com>
2019-07-17 15:25:34 -0700

    Wait to run transition router until genesis

--- src/lib/proposer/proposer.ml
+++ src/lib/proposer/proposer.ml
@@ -529,8 +529,8 @@ let run ~logger ~prover ~verifier ~trust_system ~get_completed_work
             [ ( "time_till_genesis"
               , `Int (Int64.to_int_exn (Time.Span.to_ms time_till_genesis)) )
             ]
-          "node started before genesis: waiting $time_till_genesis ms before \
-           proposing any blocks" ;
+          "Node started before genesis: waiting $time_till_genesis \
+           milliseconds before proposing any blocks" ;
         ignore
           (Time.Timeout.create time_controller time_till_genesis ~f:(fun _ ->
                start () )) )
