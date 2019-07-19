c44763ebaa368944637eb1d995baf478b6c282e8
Avery Morin <avery.schmavery@gmail.com>
2019-07-19 15:44:47 -0700

    Find keys using brew prefix

--- src/lib/cache_dir/cache_dir.ml
+++ src/lib/cache_dir/cache_dir.ml
@@ -1,10 +1,19 @@
 open Core
+open Async
 
 let autogen_path = Filename.temp_dir_name ^/ "coda_cache_dir"
 
 let manual_install_path = "/var/lib/coda"
 
-let brew_install_path = "/usr/local/var/coda"
+let brew_install_path =
+  match
+    Thread_safe.block_on_async_exn (fun () ->
+        Process.run ~prog:"brew" ~args:["--prefix"] () )
+  with
+  | Ok brew ->
+      brew ^ "/var"
+  | _ ->
+      "/usr/local/var"
 
 let possible_paths base =
   List.map [manual_install_path; brew_install_path; autogen_path] ~f:(fun d ->
