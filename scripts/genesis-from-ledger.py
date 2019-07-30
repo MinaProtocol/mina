0d1f273a44b49744da1240a79134807ed97f4eb1
Paul Steckler <psteckler@o1labs.org>
2019-07-29 18:19:42 -0700

    print prelude, postlude in loop

--- scripts/genesis-from-ledger.py
+++ scripts/genesis-from-ledger.py
@@ -11,15 +11,19 @@ from datetime import datetime
 output = subprocess.check_output(['coda', 'advanced', 'dump-ledger', '-json'])
 accounts=output.split('\n')
 
-count=0
+prelude = ['(* GENERATED GENESIS LEDGER AT: ' + str(datetime.now ()) + ' *)'
+,''
+,'open Core'
+,'open Coda_base'
+,'open Functor'
+,''
+,'let accounts = ['
+]
 
-print ('(* GENERATED GENESIS LEDGER AT: ' + str(datetime.now ())) + ' *)'
-print ('')
-print ('open Core')
-print ('open Coda_base')
-print ('open Functor')
-print ('')
-print ('let accounts = [')
+for line in prelude :
+    print(line)
+
+count=0
 for account in accounts :
     if not account == "" :
         if count > 0 :
@@ -28,12 +32,17 @@ for account in accounts :
             sys.stdout.write ('  ')
         print ('\"' + account.replace('\"','\\"') + '\"')
         count = count + 1
-print (']')
-print ('')
-print ('module Accounts = struct')
-print (' let accounts = List.map accounts ~f:(fun s ->')
-print ('   let account = Yojson.Safe.from_string s |> Account.Stable.Latest.of_yojson |> Result.ok_or_failwith in')
-print ('   (None,account))')
-print ('end')
-print ('')
-print ('include Make_from_base (Accounts)')
+
+postlude= [']'
+,''
+,'module Accounts = struct'
+,' let accounts = List.map accounts ~f:(fun s ->'
+,'   let account = Yojson.Safe.from_string s |> Account.Stable.Latest.of_yojson |> Result.ok_or_failwith in'
+,'   (None,account))'
+,'end'
+,''
+,'include Make_from_base (Accounts)'
+]
+
+for line in postlude :
+    print(line)
