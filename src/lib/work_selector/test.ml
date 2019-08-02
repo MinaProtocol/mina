45c0117e887dcea4b464687910a480cbf0804430
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2019-08-02 14:10:13 -0700

    Logger.null for tests

--- src/lib/work_selector/test.ml
+++ src/lib/work_selector/test.ml
@@ -22,7 +22,7 @@ struct
     let p = 50 in
     let snark_pool = T.Snark_pool.create () in
     let fee = Currency.Fee.zero in
-    let logger = Logger.create () in
+    let logger = Logger.null () in
     Quickcheck.test gen_staged_ledger ~trials:100 ~f:(fun sl ->
         Async.Thread_safe.block_on_async_exn (fun () ->
             let open Deferred.Let_syntax in
@@ -41,7 +41,7 @@ struct
     Backtrace.elide := false ;
     let snark_pool = T.Snark_pool.create () in
     let fee = Currency.Fee.zero in
-    let logger = Logger.create () in
+    let logger = Logger.null () in
     let send_work sl seen =
       let rec go seen all_work =
         let stuff, seen =
@@ -95,7 +95,7 @@ struct
     Backtrace.elide := false ;
     let my_fee = Currency.Fee.of_int 2 in
     let p = 50 in
-    let logger = Logger.create () in
+    let logger = Logger.null () in
     let g =
       let open Quickcheck.Generator.Let_syntax in
       let%bind sl = gen_staged_ledger in
