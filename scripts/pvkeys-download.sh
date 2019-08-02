71ae3cb889ec9c8f504f01b7af8ac3f6afe3c669
Joel Krauska <jkrauska@gmail.com>
2019-08-01 18:08:34 -0700

    add branch to pv key archive name

--- scripts/pvkeys-download.sh
+++ scripts/pvkeys-download.sh
@@ -4,15 +4,18 @@
 
 set -eo pipefail
 
-# Get fixed set of PV keys (which needs to be updated when snark changes)
-if [ -z "$JSON_GCLOUD_CREDENTIALS" ]; then
-    echo "WARNING: JSON_GCLOUD_CREDENTIALS not set, static PV keys not used"
-    exit 0
-fi
+# When running in CI
+if [ "$CI" = true ] ; then
+    # Get fixed set of PV keys (which needs to be updated when snark changes)
+    if [ -z "$JSON_GCLOUD_CREDENTIALS" ]; then
+        echo "WARNING: JSON_GCLOUD_CREDENTIALS not set, static PV keys not used"
+        exit 0
+    fi
 
-# GC credentials
-echo $JSON_GCLOUD_CREDENTIALS > google_creds.json
-/usr/bin/gcloud auth activate-service-account --key-file=google_creds.json
+    # GC credentials
+    echo $JSON_GCLOUD_CREDENTIALS > google_creds.json
+    /usr/bin/gcloud auth activate-service-account --key-file=google_creds.json
+fi
 
 # Debug output
 #set -x
@@ -20,9 +23,26 @@ echo $JSON_GCLOUD_CREDENTIALS > google_creds.json
 # Get cached keys
 echo "------------------------------------------------------------"
 echo "Downloading keys"
-PINNED_KEY_COMMIT=temporary_hack
-TARBALL="keys-${PINNED_KEY_COMMIT}-${DUNE_PROFILE}.tar.bz2"
-/usr/bin/gsutil cp gs://proving-keys-stable/$TARBALL /tmp/.
+
+set +e
+
+# Look for tar based on branch name
+if gsutil -q stat gs://proving-keys-stable/keys-${CIRCLE_BRANCH}-${DUNE_PROFILE}.tar.bz2
+then
+    TARBALL="keys-${CIRCLE_BRANCH}-${DUNE_PROFILE}.tar.bz2"
+# Fall back to old tar based on just DUNE_PROFILE
+elif gsutil -q stat gs://proving-keys-stable/keys-temporary_hack-${DUNE_PROFILE}.tar.bz2
+then
+    TARBALL="keys-temporary_hack-${DUNE_PROFILE}.tar.bz2"
+else
+    echo "PV Archive not found - Skipping"
+    exit 0
+fi
+
+echo "Found ${TARBALL}"
+
+URI="gs://proving-keys-stable/${TARBALL}"
+gsutil cp ${URI} /tmp/.
 
 # Unpack keys
 echo "------------------------------------------------------------"
