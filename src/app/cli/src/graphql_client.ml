979d0fc456c523040d22c6e0a762656523f6502d
Avery Morin <avery.schmavery@gmail.com>
2019-08-09 15:30:05 -0700

    Catch error connecting to daemon

--- src/app/cli/src/graphql_client.ml
+++ src/app/cli/src/graphql_client.ml
@@ -10,17 +10,31 @@ let query query_obj port =
       variables_string
   in
   let query_uri = Uri.of_string uri_string in
-  Cohttp_async.Client.post
-    ~headers:
-      (Cohttp.Header.add (Cohttp.Header.init ()) "Accept" "application/json")
-    ~body:(Cohttp_async.Body.of_string body_string)
-    query_uri
-  >>= fun (_, body) ->
-  Cohttp_async.Body.to_string body
-  >>| fun body ->
-  Yojson.Basic.from_string body
-  |> Yojson.Basic.Util.member "data"
-  |> query_obj#parse
+  let open Deferred.Let_syntax in
+  let get_result () =
+    let%bind _, body =
+      Cohttp_async.Client.post
+        ~headers:
+          (Cohttp.Header.add (Cohttp.Header.init ()) "Accept"
+             "application/json")
+        ~body:(Cohttp_async.Body.of_string body_string)
+        query_uri
+    in
+    let%map body = Cohttp_async.Body.to_string body in
+    Yojson.Basic.from_string body
+    |> Yojson.Basic.Util.member "data"
+    |> query_obj#parse
+  in
+  match%bind Deferred.Or_error.try_with ~extract_exn:true get_result with
+  | Ok e ->
+      return e
+  | Error e ->
+      eprintf
+        "Error connecting to daemon. You might need to start it, or specify a \
+         custom --rest-port if it's already started.\n\
+         Error message: %s\n"
+        (Error.to_string_hum e) ;
+      exit 17
 
 module Decoders = struct
   let public_key json =
