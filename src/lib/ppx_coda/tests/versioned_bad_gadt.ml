8fed2fab1df83dbea1296ac096c8fd215069ba48
Paul Steckler <steck@stecksoft.com>
2019-08-14 14:58:14 -0700

    check GADT result type parameters; improve comment

--- src/lib/ppx_coda/tests/versioned_bad_gadt.ml
+++ src/lib/ppx_coda/tests/versioned_bad_gadt.ml
@@ -1,6 +1,6 @@
 open Core_kernel
 
-(* a GADT type with unversioned input types; type parameters in result *)
+(* a GADT type with unversioned input types (List.t and Array.t are from Core_kernel, not versioned); type parameters in result *)
 module Stable = struct
   module V1 = struct
     module T = struct
