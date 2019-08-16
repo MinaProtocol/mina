6e8ed5421773e6d6a0f4704349eebdc782e8b13d
John Wu <johnwu93@gmail.com>
2019-08-16 14:27:26 -0700

    Asynchronously stop snark worker (#3219)

--- src/app/cli/src/tests/coda_process.ml
+++ src/app/cli/src/tests/coda_process.ml
@@ -142,9 +142,12 @@ let replace_snark_worker_key (conn, _proc, _) key =
   Coda_worker.Connection.run_exn conn
     ~f:Coda_worker.functions.replace_snark_worker_key ~arg:key
 
+let stop (conn, _proc, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.stop ~arg:()
+
 let disconnect ((conn, proc, _) as t) =
   (* This kills any strangling snark worker process *)
-  let%bind () = replace_snark_worker_key t None in
+  let%bind () = stop t in
   let%bind () = Coda_worker.Connection.close conn in
   let%map (_ : Unix.Exit_or_signal.t) = Process.wait proc in
   ()
