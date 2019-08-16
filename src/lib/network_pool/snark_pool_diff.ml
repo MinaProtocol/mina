ee1b78b12128c45e0936cf31b11c3d1340339157
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2019-08-16 10:34:11 -0700

    comments

--- src/lib/network_pool/snark_pool_diff.ml
+++ src/lib/network_pool/snark_pool_diff.ml
@@ -44,7 +44,11 @@ end)
               [ ("work", `Int (Work.hash work))
               ; ("fee", Currency.Fee.to_yojson fee)
               ; ("prover", Signature_lib.Public_key.Compressed.to_yojson prover)
-              ]
+                (*TODO: This should be 
+              Signature_lib.Public_key.Compressed.Stable.V1.to_yojson but for 
+              some reason both are returning two different strings and so using 
+              what mostly used in cli. Paul is looking into it*)
+               ]
     end
 
     module Latest = V1
