e573a3dfbee76e769e1758a6130f04ae7bff9dfe
Echo Nolan <echo@echonolan.net>
2019-08-19 11:30:02 -0700

    Increase offline timeout. Closes #3186.

--- src/lib/consensus/intf.ml
+++ src/lib/consensus/intf.ml
@@ -32,7 +32,7 @@ module type Constants_intf = sig
    * block. In sig, it's exactly 1 as blocks should be produced every slot. *)
   val c : int
 
-  val inactivity_secs : int
+  val inactivity_ms : int
 
   (** Number of slots in one epoch *)
   val slots_per_epoch : Unsigned.UInt32.t
