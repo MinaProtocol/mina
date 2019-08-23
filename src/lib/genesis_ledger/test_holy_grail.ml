6373d8ebee71365c1fe16132799e2035935f5f07
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2019-08-23 13:44:15 -0700

    tweak balances in accounts

--- src/lib/genesis_ledger/test_holy_grail.ml
+++ src/lib/genesis_ledger/test_holy_grail.ml
@@ -4,7 +4,7 @@ open Core
 include Make (struct
   let accounts =
     let keypairs = Lazy.force Coda_base.Sample_keypairs.keypairs in
-    let balances = [1_000_000; 1_000_000; 1_000_000; 10; 10; 10] in
+    let balances = [1_000_000; 1_000_000; 1_000_000; 1000; 1000; 1000] in
     List.mapi balances ~f:(fun i b ->
         {balance= b; pk= fst keypairs.(i); sk= snd keypairs.(i)} )
 end)
