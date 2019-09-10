c06ff7bab02bf479942c02b4627450caf36d5036
Paul Steckler <psteckler@o1labs.org>
2019-09-10 13:03:08 -0700

    fix test code

--- src/lib/transition_frontier_controller_tests/stubs.ml
+++ src/lib/transition_frontier_controller_tests/stubs.ml
@@ -159,7 +159,7 @@ struct
       in
       let next_blockchain_state =
         Blockchain_state.create_value
-          ~timestamp:(Block_time.now Block_time.Controller.basic)
+          ~timestamp:(Block_time.now Block_time.Controller.basic ~logger)
           ~snarked_ledger_hash:next_ledger_hash
           ~staged_ledger_hash:next_staged_ledger_hash
       in
