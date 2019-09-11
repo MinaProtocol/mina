2c004d07d23d6aaabbef7f7cb6952bce1b92b281
Izaak Meckler <ihmeckler@gmail.com>
2019-09-10 22:29:57 -0700

    rescue lib

---
+++ src/lib/rescue/rescue.ml
@@ -0,0 +1,74 @@
+open Core_kernel
+
+let rounds = 11
+
+module Params = struct
+  type 'a t = {mds: 'a array array; round_constants: 'a array array}
+  [@@deriving bin_io]
+
+  let map {mds; round_constants} ~f =
+    let f = Array.map ~f:(Array.map ~f) in
+    {mds= f mds; round_constants= f round_constants}
+
+  let create ~m ~random_elt =
+    let arr rows cols =
+      Array.init rows ~f:(fun _ -> Array.init cols ~f:(fun _ -> random_elt ()))
+    in
+    {mds= arr m m; round_constants= arr ((2 * rounds) + 1) m}
+end
+
+module Make (Inputs : Inputs.S) = struct
+  open Inputs
+
+  let add_block ~state block =
+    Array.iteri block ~f:(fun i bi -> state.(i) <- Field.( + ) state.(i) bi)
+
+  let sponge perm inputs state =
+    Array.fold ~init:state inputs ~f:(fun state block ->
+        add_block ~state block ; perm state )
+
+  let sbox0, sbox1 = (alphath_root, to_the_alpha)
+
+  let for_ n ~init ~f =
+    let rec go i acc = if Int.(i = n) then acc else go (i + 1) (f i acc) in
+    go 0 init
+
+  let apply matrix v =
+    let dotv row =
+      Array.reduce_exn (Array.map2_exn v row ~f:Field.( * )) ~f:Field.( + )
+    in
+    Array.map matrix ~f:dotv
+
+  let block_cipher state ~rounds ~round_constants ~mds =
+    add_block ~state round_constants.(0) ;
+    for_ (2 * rounds) ~init:state ~f:(fun r state ->
+        let sbox = if Int.(r mod 2 = 0) then sbox0 else sbox1 in
+        Array.map_inplace state ~f:sbox ;
+        let state = apply mds state in
+        add_block ~state round_constants.(r + 1) ;
+        state )
+
+  let chunk r a =
+    let n = Array.length a in
+    Array.init
+      ((n + r - 1) / r)
+      ~f:(fun i ->
+        Array.init r ~f:(fun j ->
+            let k = (r * i) + j in
+            if k < n then a.(k) else Field.zero ) )
+
+  let%test_unit "chunk" =
+    let z = Field.zero in
+    [%test_eq: unit array array]
+      (Array.map (chunk 2 [|z; z; z|]) ~f:(Array.map ~f:ignore))
+      [|[|(); ()|]; [|(); ()|]|]
+
+  let hash {Params.mds; round_constants} inputs =
+    let m = Array.length mds in
+    let r = m - 1 in
+    let perm = block_cipher ~rounds ~round_constants ~mds in
+    let final_state =
+      sponge perm (chunk r inputs) (Array.init m ~f:(fun _ -> Field.zero))
+    in
+    final_state.(0)
+end
