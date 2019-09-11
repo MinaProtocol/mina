5ce0cf144347a91e54576e72ba56ce99268b6bf8
Izaak Meckler <ihmeckler@gmail.com>
2019-09-11 09:59:55 -0700

    remove create function from mli

--- src/lib/rescue/rescue.mli
+++ src/lib/rescue/rescue.mli
@@ -3,8 +3,6 @@ module Params : sig
   [@@deriving bin_io]
 
   val map : 'a t -> f:('a -> 'b) -> 'b t
-
-  val create : m:int -> random_elt:(unit -> 'a) -> 'a t
 end
 
 module Make (Inputs : Inputs.S) : sig
