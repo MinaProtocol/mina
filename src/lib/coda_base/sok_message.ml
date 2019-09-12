67a5e5e761d7e8caa828462b0a83b1482d0a6760
Izaak Meckler <ihmeckler@gmail.com>
2019-09-12 09:47:21 -0700

    use blake2 in sok_message

--- src/lib/coda_base/sok_message.ml
+++ src/lib/coda_base/sok_message.ml
@@ -35,13 +35,34 @@ type t = Stable.Latest.t =
 let create ~fee ~prover = Stable.Latest.{fee; prover}
 
 module Digest = struct
+  let length_in_bytes = Blake2.digest_size_in_bytes
+
   module Stable = struct
     module V1 = struct
       module T = struct
-        include Random_oracle.Digest.Stable.V1
+        type t = string [@@deriving sexp, hash, compare, eq, yojson, version]
 
-        let fold, typ, length_in_triples =
-          Random_oracle.Digest.(fold, typ, length_in_triples)
+        include Binable.Of_binable
+                  (String)
+                  (struct
+                    type nonrec t = t
+
+                    let to_binable = Fn.id
+
+                    let of_binable s =
+                      assert (String.length s = length_in_bytes) ;
+                      s
+                  end)
+
+        let fold = Fold_lib.Fold.string_triples
+
+        let typ =
+          let open Snark_params.Tick in
+          Typ.array ~length:Blake2.digest_size_in_bits Boolean.typ
+          |> Typ.transport ~there:Blake2.string_to_bits
+               ~back:Blake2.bits_to_string
+
+        let length_in_triples = (Blake2.digest_size_in_bits + 2) / 3
       end
 
       include T
@@ -63,15 +84,22 @@ module Digest = struct
   (* bin_io omitted intentionally *)
   type t = Stable.Latest.t [@@deriving sexp, eq, yojson]
 
-  module Checked = Random_oracle.Digest.Checked
+  module Checked = struct
+    open Snark_params.Tick
+
+    type t = Boolean.var array
+
+    let to_triples x =
+      Fold_lib.Fold.(to_list (group3 ~default:Boolean.false_ (of_array x)))
+  end
 
   [%%define_locally
   Stable.Latest.(fold, typ, length_in_triples)]
 
-  let default =
-    let open Random_oracle.Digest in
-    of_string (String.init length_in_bytes ~f:(fun _ -> '\000'))
+  let default = String.init length_in_bytes ~f:(fun _ -> '\000')
 end
 
 let digest t =
-  Random_oracle.digest_string (Binable.to_string (module Stable.Latest.T) t)
+  Blake2.(
+    to_raw_string
+      (digest_string (Binable.to_string (module Stable.Latest.T) t)))
