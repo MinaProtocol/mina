171be4ad7f339af09bd582897b9dc97820fac041
Izaak Meckler <ihmeckler@gmail.com>
2019-09-19 15:15:32 -0700

    Poseidon hash function (#3455)
    After implementing Rescue, I benchmarked it and found it was really slow
    
    ```
    ┌─────────────────────────────────────────────┬────────────┬──────────┬──────────┬──────────┬────────────┐
    │ Name                                        │   Time/Run │  mWd/Run │ mjWd/Run │ Prom/Run │ Percentage │
    ├─────────────────────────────────────────────┼────────────┼──────────┼──────────┼──────────┼────────────┤
    │ [lib/snark_params/snark_params.ml] pedersen │   152.88us │  22.56kw │   1.32kw │   1.32kw │      5.10% │
    │ [lib/snark_params/snark_params.ml] rescue   │ 2_995.88us │ 120.05kw │ 118.56kw │ 118.56kw │    100.00% │
    │ [lib/snark_params/snark_params.ml] poseidon │   132.82us │   6.03kw │   4.71kw │   4.71kw │      4.43% │
    └─────────────────────────────────────────────┴────────────┴──────────┴──────────┴──────────┴────────────┘
    ```
    
    but as you can see from that table, the [Poseidon](https://eprint.iacr.org/2019/458.pdf) implementation in this PR is way faster! I'm not 100% sure about the number of rounds I chose. It may need to be a bit higher.

--- src/lib/sponge/sponge.mli
+++ src/lib/sponge/sponge.mli
@@ -1,9 +1,4 @@
-module Params : sig
-  type 'a t = {mds: 'a array array; round_constants: 'a array array}
-  [@@deriving bin_io]
-
-  val map : 'a t -> f:('a -> 'b) -> 'b t
-end
+module Params = Params
 
 module State : sig
   type 'a t = 'a array
@@ -11,8 +6,17 @@ module State : sig
   val map : 'a t -> f:('a -> 'b) -> 'b t
 end
 
-module Make (Inputs : Inputs.S) : sig
-  open Inputs
+module Rescue (Inputs : Intf.Inputs.Rescue) :
+  Intf.Permutation with module Field = Inputs.Field
+
+module Poseidon (Inputs : Intf.Inputs.Common) :
+  Intf.Permutation with module Field = Inputs.Field
+
+module Make_operations (Field : Intf.Field) :
+  Intf.Operations with module Field := Field
+
+module Make (P : Intf.Permutation) : sig
+  open P
 
   val update :
        Field.t Params.t
