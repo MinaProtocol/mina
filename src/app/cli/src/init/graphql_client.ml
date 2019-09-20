e919ce793883e194264067453fa0b8c7fe688dea
Avery Morin <avery.schmavery@gmail.com>
2019-09-20 01:38:18 -0700

    Improve gql client

--- src/app/cli/src/init/graphql_client.ml
+++ src/app/cli/src/init/graphql_client.ml
@@ -7,7 +7,10 @@ let query_or_error
       < parse: Yojson.Basic.json -> 'response
       ; query: string
       ; variables: Yojson.Basic.json
-      ; .. >) port : 'response Deferred.Or_error.t =
+      ; .. >) port :
+    ( 'response
+    , [`Failed_request of string | `Graphql_error of string] )
+    Deferred.Result.t =
   let uri_string = "http://localhost:" ^ string_of_int port ^ "/graphql" in
   let variables_string = Yojson.Basic.to_string query_obj#variables in
   let body_string =
@@ -15,39 +18,61 @@ let query_or_error
       variables_string
   in
   let query_uri = Uri.of_string uri_string in
-  let open Deferred.Let_syntax in
-  let get_result () =
-    let%bind _, body =
-      Cohttp_async.Client.post
-        ~headers:
-          (Cohttp.Header.add (Cohttp.Header.init ()) "Accept"
-             "application/json")
-        ~body:(Cohttp_async.Body.of_string body_string)
-        query_uri
-    in
-    let%map body = Cohttp_async.Body.to_string body in
-    Yojson.Basic.from_string body
-    |> Yojson.Basic.Util.member "data"
-    |> query_obj#parse
+  let open Deferred.Result.Let_syntax in
+  let%bind _, body =
+    Deferred.Or_error.try_with ~extract_exn:true (fun () ->
+        Cohttp_async.Client.post
+          ~headers:
+            (Cohttp.Header.add (Cohttp.Header.init ()) "Accept"
+               "application/json")
+          ~body:(Cohttp_async.Body.of_string body_string)
+          query_uri )
+    |> Deferred.Result.map_error ~f:(fun e ->
+           `Failed_request (Error.to_string_hum e) )
   in
-  Deferred.Or_error.try_with ~extract_exn:true get_result
+  let%bind body_str =
+    Cohttp_async.Body.to_string body |> Deferred.map ~f:Result.return
+  in
+  let body_json = Yojson.Basic.from_string body_str in
+  let open Yojson.Basic.Util in
+  ( match (member "errors" body_json, member "data" body_json) with
+  | `Null, `Null ->
+      Error (`Graphql_error "Empty response from graphql query")
+  | data, `Null ->
+      Error (`Graphql_error (Yojson.Basic.to_string data))
+  | _, data ->
+      Result.try_with (fun () -> query_obj#parse data)
+      |> Result.map_error ~f:(fun _ ->
+             `Graphql_error "Encountered problem parsing graphql response" ) )
+  |> Deferred.return
 
 let query query_obj port =
+  let open Deferred.Let_syntax in
   match%bind query_or_error query_obj port with
   | Ok r ->
       Deferred.return r
-  | Error e ->
+  | Error (`Failed_request e) ->
       eprintf
-        "Error connecting to daemon. You might need to start it, or specify a \
-         custom --rest-port if it's already started.\n\
-         Error message: %s\n"
-        (Error.to_string_hum e) ;
+        "❌ Error connecting to daemon. You might need to start it, or \
+         specify a custom --rest-port if it's already started.\n\
+         Error message: %s\n\
+         %!"
+        e ;
+      exit 17
+  | Error (`Graphql_error e) ->
+      eprintf "❌ Error: %s\n" e ;
       exit 17
 
 module Encoders = struct
   let optional = Option.value_map ~default:`Null
 
   let uint64 value = `String (Unsigned.UInt64.to_string value)
+
+  let amount value = `String (Currency.Amount.to_string value)
+
+  let fee value = `String (Currency.Fee.to_string value)
+
+  let nonce value = `String (Coda_base.Account.Nonce.to_string value)
 
   let uint32 value = `String (Unsigned.UInt32.to_string value)
 
@@ -65,106 +90,7 @@ module Decoders = struct
 
   let uint64 json =
     Yojson.Basic.Util.to_string json |> Unsigned.UInt64.of_string
+
+  let balance json =
+    Yojson.Basic.Util.to_string json |> Currency.Balance.of_string
 end
-
-module Get_wallets =
-[%graphql
-{|
-query {
-  ownedWallets {
-    public_key: publicKey @bsDecoder(fn: "Decoders.public_key")
-    locked
-    balance {
-      total @bsDecoder(fn: "Decoders.uint64")
-    }
-  }
-}
-|}]
-
-module Add_wallet =
-[%graphql
-{|
-mutation ($password: String) {
-  addWallet(input: {password: $password}) {
-    public_key: publicKey @bsDecoder(fn: "Decoders.public_key")
-  }
-}
-|}]
-
-module Unlock_wallet =
-[%graphql
-{|
-mutation ($password: String, $public_key: PublicKey) {
-  unlockWallet(input: {password: $password, publicKey: $public_key }) {
-    public_key: publicKey @bsDecoder(fn: "Decoders.public_key")
-  }
-}
-|}]
-
-module Lock_wallet =
-[%graphql
-{|
-mutation ($public_key: PublicKey) {
-  lockWallet(input: {publicKey: $public_key }) {
-    public_key: publicKey @bsDecoder(fn: "Decoders.public_key")
-  }
-}
-|}]
-
-module Reload_wallets = [%graphql {|
-mutation { reloadWallets { success } }
-|}]
-
-module Snark_pool =
-[%graphql
-{|
-query snarkPool {
-  snarkPool {
-  fee @bsDecoder(fn: "Decoders.uint64")
-  prover @bsDecoder(fn: "Decoders.public_key")
-  work_ids: workIds
-}
-}
-|}]
-
-module Set_snark_worker =
-[%graphql
-{|
-mutation ($wallet: PublicKey) {
-  setSnarkWorker (input : {wallet: $wallet}) {
-      lastSnarkWorker @bsDecoder(fn: "Decoders.optional_public_key")
-    }
-  }
-|}]
-
-module Set_snark_work_fee =
-[%graphql
-{|
-mutation ($fee: UInt64!) {
-  setSnarkWorkFee(input: {fee: $fee}) {
-    lastFee @bsDecoder(fn: "Decoders.uint64")
-    }
-}
-|}]
-
-module Cancel_user_command =
-[%graphql
-{|
-mutation ($from: PublicKey, $to_: PublicKey, $fee: UInt64!, $nonce: UInt32) {
-  sendPayment(input: {from: $from, to: $to_, amount: "0", fee: $fee, nonce: $nonce}) {
-    payment {
-      id
-    }
-  }
-}
-|}]
-
-module Get_inferred_nonce =
-[%graphql
-{|
-query nonce($public_key: PublicKey) {
-  wallet(publicKey: $public_key) {
-    inferredNonce
-  }
-}
-|}]
