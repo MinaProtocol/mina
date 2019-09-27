20493f3547cdae544adccf8dd81826f0c5b842db
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2019-09-26 17:14:42 -0700

    Revert "adding logs to help debugging"
    This reverts commit 73279f7635fc532bfa0a8d080de13252c142148e.

--- src/app/cli/src/tests/coda_txns_and_restart_non_proposers.ml
+++ src/app/cli/src/tests/coda_txns_and_restart_non_proposers.ml
@@ -5,8 +5,6 @@ open Coda_base
 let name = "coda-txns-and-restart-non-proposers"
 
 let main () =
-  Backtrace.elide := false ;
-  Async.Scheduler.set_record_backtraces true ;
   let logger = Logger.create () in
   let snark_work_public_keys =
     Fn.const
@@ -27,19 +25,26 @@ let main () =
     ~n:10
   |> don't_wait_for ;
   (* restart non-proposers *)
-
+  let random_non_proposer () = Random.int 2 + 3 in
   (* catchup *)
   let%bind () = after (Time.Span.of_sec 30.) in
   let%bind () =
-    Coda_worker_testnet.Restarts.trigger_catchup testnet ~logger ~node:3
+    Coda_worker_testnet.Restarts.trigger_catchup testnet ~logger
+      ~node:(random_non_proposer ())
   in
   let%bind () = after (Time.Span.of_min 1.) in
   (* bootstrap *)
   let%bind () =
-    Coda_worker_testnet.Restarts.trigger_bootstrap testnet ~logger ~node:4
+    Coda_worker_testnet.Restarts.trigger_bootstrap testnet ~logger
+      ~node:(random_non_proposer ())
   in
   (* random restart *)
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after (Time.Span.of_min 1.) in
+  let%bind () =
+    Coda_worker_testnet.Restarts.restart_node testnet ~logger
+      ~node:(random_non_proposer ())
+      ~duration:(Time.Span.of_min (Random.float 3. +. 1.))
+  in
   (* settle for a few more min *)
   let%bind () = after (Time.Span.of_min 1.) in
   Coda_worker_testnet.Api.teardown testnet
