5778de1b06959bc526297d3b1d654e0024c2700b
Paul Steckler <psteckler@o1labs.org>
2019-09-27 17:07:43 -0700

    provide delay and tps, calculate capacity

--- src/lib/snark_params/scan_state_constants.ml
+++ src/lib/snark_params/scan_state_constants.ml
@@ -3,20 +3,13 @@
 
 (* in the config files, we either have:
 
-    - transaction_capacity_log_2 and work_delay explicitly, or
-    - those same parameters, and a tps_goal
+    - a work_delay and transaction_capacity_log_2, or
+    - a work_delay and a tps_goal, allowing us to calculate transaction_capacity_log_2
 
    use the config boolean scan_state_with_tps_goal to distinguish those cases
-
-   If the tps_goal is given, then we can calculate a desired number of snark
-   workers.
-
 *)
 
 module type S = Scan_state_constants_intf.S
-
-[%%inject
-"transaction_capacity_log_2", scan_state_transaction_capacity_log_2]
 
 [%%inject
 "work_delay", scan_state_work_delay]
@@ -27,7 +20,19 @@ scan_state_with_tps_goal]
 [%%inject
 "tps_goal", scan_state_tps_goal]
 
-let number_of_snark_workers () =
-  Int.pow 2 transaction_capacity_log_2 / work_delay
+[%%inject
+"block_window_duration", block_window_duration]
+
+let max_coinbases = 2
+
+let max_user_commands_per_block = tps_goal * block_window_duration / 1000
+
+let transaction_capacity_log_2 =
+  1 + Int.ceil_log2 (max_user_commands_per_block + max_coinbases)
+
+[%%else]
+
+[%%inject
+"transaction_capacity_log_2", scan_state_transaction_capacity_log_2]
 
 [%%endif]
