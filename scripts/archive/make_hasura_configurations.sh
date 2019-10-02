3f47f08b7b91672d893d6acf1f08b6f69a43bdbf
John Wu <johnwu93@gmail.com>
2019-10-02 12:49:55 -0700

    Using docker-compose to spawn hasura and postgres in circleci

--- scripts/archive/make_hasura_configurations.sh
+++ scripts/archive/make_hasura_configurations.sh
@@ -1,6 +1,17 @@
 # Makes a database queryable through graphql
-
 opam config exec -- dune build scripts/archive/ocaml/stitch_introspection.exe
+
+for i in `seq 1 10`;
+do
+    # TODO: Install netcat to docker toolchain
+    nc -z 127.0.0.1 9000 && echo "Received successful connection to Hasura" && break
+    echo "Waiting for connection from Hasura"
+    sleep 1
+done
+if [ $i -eq 10 ]
+then
+    echo "Took to long for seeing Hasura is up. Did you run the Hasura docker container?" && exit 1
+fi
 
 curl -d'{"type":"replace_metadata", "args":'$(cat scripts/archive/metadata.json)'}' \
     http://localhost:9000/v1/query
