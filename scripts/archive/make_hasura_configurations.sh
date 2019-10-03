8437c7c9b18c6bd84b71b03cf5667acb11220c0b
John Wu <johnwu93@gmail.com>
2019-10-03 12:30:40 -0700

    stuff

--- scripts/archive/make_hasura_configurations.sh
+++ scripts/archive/make_hasura_configurations.sh
@@ -8,10 +8,10 @@ sleep 10
 curl -d'{"type":"replace_metadata", "args":'$(cat scripts/archive/metadata.json)'}' \
     http://localhost:9000/v1/query
 
-mkdir -p scripts/archive/output/
+mkdir -p output/
 # Generates the graphql query types for OCaml
 python3 scripts/introspection_query.py --port 9000 --uri /v1/graphql --headers X-Hasura-Role:user \
-    | _build/default/scripts/archive/ocaml/stitch_introspection.exe \
-    > scripts/archive/output/graphql_schema.json
+    | _build/default/scripts/archive/ocaml/sttch_introspection.exe \
+    > output/graphql_schema.json
 
 echo "Finished getting introspection data"
