caadc290b60a85e55156f0815a7a2c406b18589d
Brandon Kase <bkase@o1labs.org>
2019-10-09 17:58:17 -0700

    New coda-discovery.deb

--- scripts/rebuild-deb.sh
+++ scripts/rebuild-deb.sh
@@ -32,7 +32,7 @@ Version: ${VERSION}
 Section: base
 Priority: optional
 Architecture: amd64
-Depends: libssl1.1, libprocps6, libgmp10, libffi6, libgomp1, miniupnpc, coda-kademlia
+Depends: libssl1.1, libprocps6, libgmp10, libffi6, libgomp1, miniupnpc, coda-discover
 License: Apache-2.0
 Homepage: https://codaprotocol.com/
 Maintainer: o(1)Labs <build@o1labs.org>
@@ -116,7 +116,7 @@ Version: ${VERSION}
 Section: base
 Priority: optional
 Architecture: amd64
-Depends: libssl1.1, libprocps6, libgmp10, libffi6, libgomp1, miniupnpc, coda-kademlia
+Depends: libssl1.1, libprocps6, libgmp10, libffi6, libgomp1, miniupnpc, coda-discovery
 License: Apache-2.0
 Homepage: https://codaprotocol.com/
 Maintainer: o(1)Labs <build@o1labs.org>
