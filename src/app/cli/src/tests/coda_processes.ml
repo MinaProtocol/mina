b91df4c293ba64dede8bcf01d588e01ef45ed72b
Corey Richardson <cmr@o1labs.org>
2019-10-11 16:04:33 -0700

    Timeout to 10s

--- src/app/cli/src/tests/coda_processes.ml
+++ src/app/cli/src/tests/coda_processes.ml
@@ -66,7 +66,7 @@ let local_configs ?proposal_interval ?(proposers = Fn.const None)
   in
   configs
 
-let stabalize_and_start_or_timeout ?(timeout_ms = 5000.) nodes =
+let stabalize_and_start_or_timeout ?(timeout_ms = 10000.) nodes =
   let ready () =
     let check_ready node =
       let%map peers = Coda_process.peers_exn node in
