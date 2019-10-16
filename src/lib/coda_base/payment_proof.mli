a9254ee98d6d6dfe3b1aeb27c0ae24f2e0768205
John Wu <johnwu93@gmail.com>
2019-10-16 16:00:46 -0700

    Refactored receipt_chain_database - Rewrote logic such that it uses merkle_list_prover and merkle_list_verifier - Inverted the dependencies between coda_base and receipt_chain_database_lib. This resulted in having less functors

--- src/lib/coda_base/payment_proof.mli
+++
@@ -1,8 +0,0 @@
-open Receipt_chain_database_lib
-
-type t = (Receipt.Chain_hash.t, User_command.t) Payment_proof.t
-[@@deriving eq, sexp, bin_io, yojson]
-
-val initial_receipt : t -> Receipt.Chain_hash.t
-
-val payments : t -> User_command.t list
