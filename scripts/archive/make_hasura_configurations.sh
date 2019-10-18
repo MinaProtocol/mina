bb7c74259968fd071b5274f978af745429e92b19
John Wu <johnwu93@gmail.com>
2019-10-18 12:43:05 -0700

    scripts can now be run on different directories

--- scripts/archive/make_hasura_configurations.sh
+++ scripts/archive/make_hasura_configurations.sh
@@ -6,9 +6,8 @@ sleep 5
 curl -d'{"type":"replace_metadata", "args":'$(cat scripts/archive/metadata.json)'}' \
     http://localhost:$HASURA_PORT/v1/query
 
-mkdir -p output/
 # Generates the graphql query types for OCaml
-python3 /scripts/introspection_query.py --port $HASURA_PORT --uri /v1/graphql --headers X-Hasura-Role:user \
-    | python3 /scripts/archive/change_constraint.py > /scripts/archive/output/graphql_schema.json
+python3 $CODA_DIRECTORY_PATH/scripts/introspection_query.py --port $HASURA_PORT --uri /v1/graphql --headers X-Hasura-Role:user \
+    | python3 $CODA_DIRECTORY_PATH/scripts/archive/change_constraint.py > $CODA_DIRECTORY_PATH/scripts/archive/output/graphql_schema.json
 
 echo "Finished getting introspection data"
