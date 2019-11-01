083db68f611a811c6e389a9b1a23f3626c1e41de
Avery Morin <avery.schmavery@gmail.com>
2019-11-01 09:56:22 -0700

    Some cleanup

--- src/app/verifier_demo/verifier_demo.ml
+++ src/app/verifier_demo/verifier_demo.ml
@@ -1,38 +1,39 @@
 module Js = Js_of_ocaml.Js
 
 let consolelog data =
-  ignore @@ Js.Unsafe.fun_call (Js.Unsafe.js_expr "console.log")
-    [|(data |> Js.Unsafe.inject)|]
+  ignore
+  @@ Js.Unsafe.fun_call
+       (Js.Unsafe.js_expr "console.log")
+       [|data |> Js.Unsafe.inject|]
 
 let create_verification_key key_string =
   let key = Js.to_string key_string in
   let sexp = Core_kernel.Sexp.of_string key in
-  consolelog "deserialize key";
-  let key = Demo.Verification_key.t_of_sexp sexp
-  |> Demo.Verification_key.Processed.create in
-  consolelog key;
-  key
+  consolelog "deserialize key" ;
+  let key =
+    Demo.Verification_key.t_of_sexp sexp
+    |> Demo.Verification_key.Processed.create
+  in
+  consolelog key ; key
 
 let decode_g1 a =
   let open Snarkette.Mnt6753 in
   let open Core_kernel in
-  consolelog "deserialize g1";
-  let a = Js.to_string a |> Sexp.of_string |> [%of_sexp: Fq.t * Fq.t] |>
-  G1.of_affine in
-  consolelog a;
-  a
+  consolelog "deserialize g1" ;
+  let a =
+    Js.to_string a |> Sexp.of_string |> [%of_sexp: Fq.t * Fq.t] |> G1.of_affine
+  in
+  consolelog a ; a
 
 let decode_g2 a =
   let open Snarkette.Mnt6753 in
   let open Core_kernel in
-  consolelog "deserialize g2";
-  let a = 
-  Js.to_string a
-  |> Sexp.of_string
-  |> [%of_sexp: (Fq.t * Fq.t * Fq.t) * (Fq.t * Fq.t * Fq.t)] |>
-  G2.of_affine in
-  consolelog a;
-  a
+  consolelog "deserialize g2" ;
+  let a =
+    Js.to_string a |> Sexp.of_string
+    |> [%of_sexp: (Fq.t * Fq.t * Fq.t) * (Fq.t * Fq.t * Fq.t)] |> G2.of_affine
+  in
+  consolelog a ; a
 
 let construct_proof a b c delta_prime z =
   let a = decode_g1 a in
@@ -47,21 +48,35 @@ let bigint_of_string s = Snarkette.Nat.of_string (Js.to_string s)
 let bigint_to_string bi = Js.string (Snarkette.Nat.to_string bi)
 
 let verify_state_hash verification_key state_hash proof =
-  consolelog "deserialize state_hash";
+  consolelog "deserialize state_hash" ;
   let input_nat = bigint_of_string state_hash in
   let open Snarkette in
   (* ... *)
-  consolelog "run verify";
-  let v = Demo.verify verification_key [Nat.shift_left input_nat 1; Nat.of_int 1 ] proof in
-  consolelog v;
-  let v = Demo.verify verification_key [Nat.shift_left input_nat 1; Nat.of_int 1 ] proof in
-  consolelog v;
-  let v = Demo.verify verification_key [Nat.shift_right input_nat 1; Nat.of_int 0 ] proof in
-  consolelog v;
-  let v = Demo.verify verification_key [Nat.shift_right input_nat 1; Nat.of_int 0 ] proof in
-  consolelog v;
-  consolelog "ran verify";
-  v
+  consolelog "run verify" ;
+  let v =
+    Demo.verify verification_key
+      [Nat.shift_left input_nat 1; Nat.of_int 1]
+      proof
+  in
+  consolelog v ;
+  let v =
+    Demo.verify verification_key
+      [Nat.shift_left input_nat 1; Nat.of_int 1]
+      proof
+  in
+  consolelog v ;
+  let v =
+    Demo.verify verification_key
+      [Nat.shift_right input_nat 1; Nat.of_int 0]
+      proof
+  in
+  consolelog v ;
+  let v =
+    Demo.verify verification_key
+      [Nat.shift_right input_nat 1; Nat.of_int 0]
+      proof
+  in
+  consolelog v ; consolelog "ran verify" ; v
 
 let () =
   let window = Js.Unsafe.global in
