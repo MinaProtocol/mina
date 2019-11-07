823d1c27fce463c987c0b9e51bbd53e690192dbf
cmr <cmr@o1labs.org>
2019-11-07 14:22:35 -0800

    remove kademlia-haskell

--- src/lib/kademlia/caml_syslimits.c
+++
@@ -1,6 +0,0 @@
-#include <limits.h>
-#include <caml/mlvalues.h>
-
-CAMLprim value caml_syslimits_path_max() {
-  return Val_int(PATH_MAX);
-}
--- src/lib/kademlia/syslimits.ml
+++
@@ -1,1 +0,0 @@
-external path_max : unit -> int = "caml_syslimits_path_max"
