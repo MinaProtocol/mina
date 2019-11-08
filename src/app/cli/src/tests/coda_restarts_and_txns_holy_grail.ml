edfb3f0996bce3dc094816f95229f5f132ad2be0
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2019-11-08 13:53:34 -0800

    bump up the block_window_duration and do the changes requested by psteckler

--- src/app/cli/src/tests/coda_restarts_and_txns_holy_grail.ml
+++ src/app/cli/src/tests/coda_restarts_and_txns_holy_grail.ml
@@ -8,6 +8,7 @@ let _ =
   "make some change in order to create a PR so that I can detect CI failures"
 
 let main n () =
+  let wait_time = Time.Span.of_min 2. in
   assert (n > 1) ;
   let logger = Logger.create () in
   let snark_work_public_keys =
@@ -32,26 +33,26 @@ let main n () =
   |> don't_wait_for ;
   (* RESTART NODES *)
   (* catchup *)
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after wait_time in
   let%bind () =
     Coda_worker_testnet.Restarts.trigger_catchup testnet ~logger
       ~node:(random_non_proposer ())
   in
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after wait_time in
   (* bootstrap *)
   let%bind () =
     Coda_worker_testnet.Restarts.trigger_bootstrap testnet ~logger
       ~node:(random_non_proposer ())
   in
   (* random restart *)
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after wait_time in
   let%bind () =
     Coda_worker_testnet.Restarts.restart_node testnet ~logger
       ~node:(random_proposer ())
       ~duration:(Time.Span.of_min (Random.float 3.))
   in
   (* settle for a few more min *)
-  let%bind () = after (Time.Span.of_min 2.) in
+  let%bind () = after wait_time in
   Coda_worker_testnet.Api.teardown testnet ~logger
 
 let command =
