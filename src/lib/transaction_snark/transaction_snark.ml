46a10744ce73e642548ff9f0ccbefcbba157258e
Izaak Meckler <ihmeckler@gmail.com>
2019-11-20 13:59:36 -0800

    add expiration time check to transaction logic inside and outside snark

--- src/lib/transaction_snark/transaction_snark.ml
+++ src/lib/transaction_snark/transaction_snark.ml
@@ -4,6 +4,7 @@ open Coda_base
 open Snark_params
 open Currency
 open Fold_lib
+module Global_slot = Coda_numbers.Global_slot
 
 let state_hash_size_in_triples = Tick.Field.size_in_triples
 
@@ -306,6 +307,15 @@ module Base = struct
     let%bind payload_section = Schnorr.Message.var_of_payload payload in
     let%bind is_user_command =
       Transaction_union.Tag.Checked.is_user_command tag
+    in
+    let%bind () =
+      let current_global_slot =
+        Global_slot.(Checked.constant zero)
+        (* TODO: psteckler is working on passing through the consensus state to
+           here. This should be replaced with the real value when his PR lands. *)
+      in
+      Global_slot.Checked.(current_global_slot <= payload.common.valid_until)
+      >>= Boolean.Assert.is_true
     in
     let%bind () =
       check_signature shifted ~payload ~is_user_command ~sender ~signature
@@ -1545,6 +1555,7 @@ let%test_module "transaction_snark" =
       let receiver = wallets.(j) in
       let payload : User_command.Payload.t =
         User_command.Payload.create ~fee ~nonce ~memo
+          ~valid_until:Global_slot.max_value
           ~body:
             (Payment
                { receiver= receiver.account.public_key
