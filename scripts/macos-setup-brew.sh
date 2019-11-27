3ec4a5400b0a871edbe9efb35c10c508ad2edf85
Avery Morin <avery.schmavery@gmail.com>
2019-11-27 11:48:46 -0800

    "Fix" the bash script a little
    just to see if it works

--- scripts/macos-setup-brew.sh
+++ scripts/macos-setup-brew.sh
@@ -3,7 +3,7 @@ set -eu
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-NEEDED_PACKAGES="bash boost cmake gmp gpatch jemalloc libffi libomp libsodium opam openssl@1.1 pkg-config zlib"
+NEEDED_PACKAGES=" bash boost cmake gmp gpatch jemalloc libffi libomp libsodium opam openssl@1.1 pkg-config zlib "
 echo "Needed:  ${NEEDED_PACKAGES}"
 
 CURRENT_PACKAGES=$(brew list | xargs)
@@ -11,7 +11,7 @@ echo "Current: ${CURRENT_PACKAGES}"
 
 # Prune already installed packages from the todo list
 for p in $CURRENT_PACKAGES; do
-  NEEDED_PACKAGES=${NEEDED_PACKAGES//$p/}
+  NEEDED_PACKAGES=${NEEDED_PACKAGES// $p / }
 done;
 
 echo "Todo:    ${NEEDED_PACKAGES}"
