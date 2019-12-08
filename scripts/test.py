63aca7e32b1569a80993575c0243b91458963a74
cmr <cmr@o1labs.org>
2019-12-08 12:55:45 -0800

    more descriptive terms than blacklist/whitelist

--- scripts/test.py
+++ scripts/test.py
@@ -74,12 +74,12 @@ medium_curve_profiles_full = [
     'testnet_postake_many_proposers_medium_curves'
 ]
 
-ci_blacklist = [
+ci_excludes = [
     "ci/circleci: lint-opt",
 ]
 
-# of all the generated CI jobs, allow these specific ones to fail (extra blacklist on top of ci_blacklist)
-required_blacklist = [
+# of all the generated CI jobs, allow these specific ones to fail (extra excludes on top of ci_excludes)
+required_excludes = [
     'test_postake_five_even_snarkless:*',
     'test_postake_catchup:*',
     'test_postake_holy_grail:*',
@@ -128,25 +128,25 @@ def parse_filter(pattern_src):
 
 
 def filter_tests(tests,
-                 whitelist_filters,
-                 blacklist_filters,
+                 includes_filters,
+                 excludes_filters,
                  permutations=None):
     if permutations is None:
         permutations = tests
 
-    whitelist_patterns = list(map(parse_filter, whitelist_filters))
-    blacklist_patterns = list(map(parse_filter, blacklist_filters))
+    includes_patterns = list(map(parse_filter, includes_filters))
+    excludes_patterns = list(map(parse_filter, excludes_filters))
 
     def keep(profile, test):
-        whitelisted = all(
+        includesed = all(
             test_pattern(profile_pat, profile)
             and test_pattern(test_pat, test)
-            for (profile_pat, test_pat) in whitelist_patterns)
-        blacklisted = any(
+            for (profile_pat, test_pat) in includes_patterns)
+        excludesed = any(
             test_pattern(profile_pat, profile)
             and test_pattern(test_pat, test)
-            for (profile_pat, test_pat) in blacklist_patterns)
-        return whitelisted and not (blacklisted)
+            for (profile_pat, test_pat) in excludes_patterns)
+        return includesed and not (excludesed)
 
     result = collections.defaultdict(list)
     for (profile, tests) in permutations.items():
@@ -193,16 +193,16 @@ def run(args):
 
     all_tests = small_curves_tests
     all_tests.update(medium_curves_tests)
-    all_tests = filter_tests(all_tests, args.whitelist_patterns,
-                             args.blacklist_patterns)
+    all_tests = filter_tests(all_tests, args.includes_patterns,
+                             args.excludes_patterns)
     if len(all_tests) == 0:
         # TODO: support direct test dispatching
-        if args.whitelist_patterns != ['*']:
+        if args.includes_patterns != ['*']:
             fail(
-                'no tests were selected -- whitelist pattern did not match any known tests'
+                'no tests were selected -- includes pattern did not match any known tests'
             )
         else:
-            fail('no tests were selected -- blacklist is too restrictive')
+            fail('no tests were selected -- excludes is too restrictive')
 
     print('Preparing to run the following tests:')
     for (profile, tests) in all_tests.items():
@@ -251,9 +251,9 @@ def run(args):
 
 def get_required_status():
     tests = filter_tests(small_curves_tests, ['*'],
-                         required_blacklist,
+                         required_excludes,
                          permutations=filter_tests(small_curves_tests, ['*'],
-                                                   ci_blacklist))
+                                                   ci_excludes))
     return list(
         filter(
             lambda el: el not in not_required_status_checks,
@@ -277,7 +277,7 @@ def render(args):
     (output_file, ext) = os.path.splitext(args.circle_jinja_file)
     assert ext == '.jinja'
 
-    tests = filter_tests(small_curves_tests, ['*'], ci_blacklist)
+    tests = filter_tests(small_curves_tests, ['*'], ci_excludes)
 
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader(circle_ci_conf_dir), autoescape=False)
@@ -355,17 +355,17 @@ def main():
         help='Do not perform any side effects, only print what the program would do.'
     )
     run_parser.add_argument('-b',
-                            '--blacklist-pattern',
+                            '--excludes-pattern',
                             action='append',
                             type=str,
                             default=[],
-                            dest='blacklist_patterns',
+                            dest='excludes_patterns',
                             help='''
             Specify a pattern of tests to exclude from running. This flag can be
             provided multiple times to specify a series of patterns'
         ''')
     run_parser.add_argument(
-        'whitelist_patterns',
+        'includes_patterns',
         nargs='*',
         type=str,
         default=['*'],
