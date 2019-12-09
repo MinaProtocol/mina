53c431b0288136c57f7c0449a4077c4f08740263
John Wu <johnwu93@gmail.com>
2019-12-09 13:50:09 -0800

    Cleaned up consensus time in graphql (#3925)
    * Created epochDelegateAccount
    - Created staking_ledger method in coda_lib
    - Exposed Snapshot module in pos.ml
    
    * Delegate public key should reference epoch ledgers account
    
    * Cleaned up consensus time in graphql
    
    - Put the Slot and Epoch modules into it's own file
    - Removed the mli file for Consensus_stake_hooks because the
    
    * Refactored consensus_state type in graphql
    - This PR primarily makes some of the graphql types for coda a dependency for proof_of_stake.ml
        - This in encapsulates many of the fields for proof_of_stake and makes the proof_of_stake algorithm more general
        - Put some types that we use for graphql in graphql_client_lib
    - Renamed graphql_client_lib to graphql_lib so that it has more functionalities than holding client util functions base_types file.
    - Removed Stringable because it is not needed as much anymore
    - Replaced select_epoch_snapshot with get_epoch_ledger to encapsulate implementation details of POS
    
    * Updated graphql schema
    
    * Encapsulating the concept of consensus_time in POS.ml
    
    * Global_slot is now fully private within consensus
    
    * Fixed compiling errors
    
    * Addressed Avery's comments
    
    * Updated graphql schema
    
    * Deleted some code
    
    * updated schema

--- src/lib/transition_frontier/full_frontier/full_frontier.ml
+++ src/lib/transition_frontier/full_frontier/full_frontier.ml
@@ -461,8 +461,9 @@ let update_metrics_with_diff (type mutant) t
           |> Coda_numbers.Length.to_int )
       in
       let global_slot =
-        Consensus.Data.Consensus_state.global_slot consensus_state
-        |> Unsigned.UInt32.to_int |> Float.of_int
+        Consensus.Data.Consensus_state.consensus_time consensus_state
+        |> Consensus.Data.Consensus_time.to_uint32 |> Unsigned.UInt32.to_int
+        |> Float.of_int
       in
       Coda_metrics.(
         let num_breadcrumbs_removed =
