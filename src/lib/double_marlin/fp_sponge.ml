ec852aa715959cf63dd8101d0fb5ef46cd06c082
Izaak Meckler <ihmeckler@gmail.com>
2019-12-30 16:58:14 -0800

    unchecked fp sponge

---
+++ src/lib/double_marlin/fp_sponge.ml
@@ -0,0 +1,35 @@
+open Core_kernel
+
+module Rounds = struct
+  let rounds_full = 8
+
+  let rounds_partial = 55
+end
+
+module Inputs = struct
+  include Rounds
+  module Field = Snarky_bn382_backend.Fp
+
+  (* TODO: Square in place *)
+  let to_the_alpha x =
+    let open Field in
+    let x4 = square (square x) in
+    x4 *= x ; x4
+
+  module Operations = struct
+    let add_assign ~state i x = Field.(state.(i) += x)
+
+    let apply_affine_map (rows, c) v =
+      Array.mapi rows ~f:(fun j row ->
+          let open Field in
+          let res = zero + zero in
+          Array.iteri row ~f:(fun i r -> res += (r * v.(i))) ;
+          res += c.(j) ;
+          res )
+
+    let copy a = Array.map a ~f:(fun x -> Field.(x + zero))
+  end
+end
+
+module Field = Sponge.Make_sponge (Sponge.Poseidon (Inputs))
+module Bits = Sponge.Make_bit_sponge (Bool) (Inputs.Field) (Field)
