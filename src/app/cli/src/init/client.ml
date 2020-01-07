8954fd525c87b86c4f8eaa1138cac92cca45f669
Joel Krauska <jkrauska@gmail.com>
2020-01-07 11:29:38 -0800

    remove commented out fcn

--- src/app/cli/src/init/client.ml
+++ src/app/cli/src/init/client.ml
@@ -877,20 +877,6 @@ let dump_keypair =
       |> Public_key.Compressed.to_base58_check )
       (kp.private_key |> Private_key.to_base58_check))
 
-(* let generate_keypair =
-  Command.async ~summary:"Generate a new public-key/private-key pair"
-    (let open Command.Let_syntax in
-    let%map_open privkey_path = Cli_lib.Flag.privkey_write_path in
-    Cli_lib.Handle_exception_nicely.handle_exception_nicely
-    @@ fun () ->
-    let open Deferred.Let_syntax in
-    let kp = Keypair.create () in
-    let%bind () = Secrets.Keypair.Terminal_stdin.write_exn kp ~privkey_path in
-    printf "Keypair generated\nPublic key: %s\n"
-      ( kp.public_key |> Public_key.compress
-      |> Public_key.Compressed.to_base58_check ) ;
-    exit 0) *)
-
 let dump_ledger =
   let sl_hash_flag =
     Command.Param.(
