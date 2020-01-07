3be0978c0b6e44475c73304905e999f360cbca8f
Joel Krauska <jkrauska@gmail.com>
2020-01-07 11:26:49 -0800

    move shared functions to lib

---
+++ src/lib/cli_lib/handle_exception_nicely.ml
@@ -0,0 +1,11 @@
+open Core_kernel
+open Async
+
+let handle_exception_nicely (type a) (f : unit -> a Deferred.t) () :
+    a Deferred.t =
+  match%bind Deferred.Or_error.try_with ~extract_exn:true f with
+  | Ok e ->
+      return e
+  | Error e ->
+      eprintf "Error: %s" (Error.to_string_hum e) ;
+      exit 4
