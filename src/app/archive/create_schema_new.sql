14bf671ff28f0b5b9b681921e29deaee2d61779f
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2020-01-07 19:09:23 -0800

    doing the changes requested by nholland94

--- src/app/archive/create_schema_new.sql
+++ src/app/archive/create_schema_new.sql
@@ -1,10 +1,3 @@
-CREATE TABLE state_hashes
-( id    serial PRIMARY KEY
-, value text   NOT NULL UNIQUE
-);
-
-CREATE INDEX idx_state_hashes_value ON state_hashes(value);
-
 CREATE TABLE public_keys
 ( id    serial PRIMARY KEY
 , value text   NOT NULL UNIQUE
@@ -22,14 +15,15 @@ CREATE INDEX idx_snarked_ledger_hashes_value ON snarked_ledger_hashes(value);
 CREATE TYPE user_command_type AS ENUM ('payment', 'delegation');
 
 CREATE TABLE user_commands
-( id          serial            PRIMARY KEY
-, type        user_command_type NOT NULL
-, sender_id   int               NOT NULL REFERENCES public_keys(id)
-, receiver_id int               NOT NULL REFERENCES public_keys(id)
-, nonce       bigint            NOT NULL
-, amount      bigint            NOT NULL
-, fee         bigint            NOT NULL
-, memo        text              NOT NULL
+( id             serial            PRIMARY KEY
+, type           user_command_type NOT NULL
+, sender_id      int               NOT NULL REFERENCES public_keys(id)
+, receiver_id    int               NOT NULL REFERENCES public_keys(id)
+, nonce          bigint            NOT NULL
+, amount         bigint            NOT NULL
+, fee            bigint            NOT NULL
+, memo           text              NOT NULL
+, transaction_id int               NOT NULL UNIQUE
 );
 
 CREATE TYPE internal_command_type AS ENUM ('fee_transfer', 'coinbase');
@@ -39,6 +33,7 @@ CREATE TABLE internal_commands
 , type        internal_command_type NOT NULL
 , receiver_id int                   NOT NULL REFERENCES public_keys(id)
 , fee         bigint                NOT NULL
+, transaction_id      int    NOT NULL UNIQUE
 );
 
 CREATE TABLE transactions
@@ -50,10 +45,13 @@ CREATE TABLE transactions
 
 CREATE INDEX idx_transactions_hash ON transactions(hash);
 
+ALTER TABLE user_commands     ADD FOREIGN KEY (transaction_id) REFERENCES transactions(id);
+ALTER TABLE internal_commands ADD FOREIGN KEY (transaction_id) REFERENCES transactions(id);
+
 CREATE TABLE blocks
 ( id                     serial PRIMARY KEY
-, state_hash_id          int    NOT NULL UNIQUE REFERENCES state_hashes(id)
-, parent_hash_id         int    NOT NULL        REFERENCES state_hashes(id)
+, state_hash             text   NOT NULL UNIQUE
+, parent_id              int    NOT NULL        REFERENCES blocks(id)
 , creator_id             int    NOT NULL        REFERENCES public_keys(id)
 , snarked_ledger_hash_id int    NOT NULL        REFERENCES snarked_ledger_hashes(id)
 , ledger_hash            text   NOT NULL
@@ -62,9 +60,9 @@ CREATE TABLE blocks
 , coinbase_id            int    NOT NULL UNIQUE REFERENCES internal_commands(id)
 );
 
-CREATE INDEX idx_blocks_state_hash_id ON blocks(state_hash_id);
-CREATE INDEX idx_blocks_create_id     ON blocks(creator_id);
-CREATE INDEX idx_blocks_height        ON blocks(height);
+CREATE INDEX idx_blocks_state_hash ON blocks(state_hash);
+CREATE INDEX idx_blocks_create_id  ON blocks(creator_id);
+CREATE INDEX idx_blocks_height     ON blocks(height);
 
 CREATE TABLE blocks_transactions
 ( block_id       int NOT NULL REFERENCES blocks(id)
