fdbbff928097a0d932728e5b55284eae8489abc2
Carey Janecka <careyjanecka@gmail.com>
2020-01-28 17:54:50 -0800

    exp

--- src/lib/currency/currency.ml
+++ src/lib/currency/currency.ml
@@ -66,28 +66,29 @@ end = struct
   [%%define_locally
   Unsigned.(to_uint64, of_uint64, of_int, to_int, of_string, to_string)]
 
-  let precision = Unsigned.of_int 9
+  let precision = 9
+
+  let precision_exp = Unsigned.of_int 1_000_000_000
 
   let to_formatted_string amount =
-    let whole = Unsigned.div amount precision in
-    let remainder = Unsigned.rem amount precision in
+    let whole = Unsigned.div amount precision_exp in
+    let remainder = Unsigned.rem amount precision_exp in
     to_string whole ^ "." ^ to_string remainder
 
   let of_formatted_string input =
     let parts = String.split ~on:'.' input in
     match parts with
     | [whole] ->
-        of_string (whole ^ String.make (to_int precision) '0')
+        of_string (whole ^ String.make precision '0')
     | [whole; decimal] ->
         let decimal_length = String.length decimal in
-        let precision_int = to_int precision in
         if
           let open Int in
-          decimal_length > precision_int
-        then of_string (whole ^ String.sub decimal ~pos:0 ~len:precision_int)
+          decimal_length > precision
+        then of_string (whole ^ String.sub decimal ~pos:0 ~len:precision)
         else
           of_string
-            (whole ^ decimal ^ String.make (precision_int - decimal_length) '0')
+            (whole ^ decimal ^ String.make (precision - decimal_length) '0')
     | [] | _ ->
         failwith "Invalid currency input"
 
