e885bd790777a46943478d9109d91ad9ac7026f9
Ember Arlynx <ember@o1labs.org>
2020-02-07 15:25:11 -0800

    conflict new coda-daemon deb with old coda-discovery deb

--- scripts/rebuild-deb.sh
+++ scripts/rebuild-deb.sh
@@ -40,6 +40,7 @@ Section: base
 Priority: optional
 Architecture: amd64
 Depends: libffi6, libgmp10, libgomp1, libjemalloc1, libprocps6, libssl1.1, miniupnpc
+Conflicts: coda-discovery
 License: Apache-2.0
 Homepage: https://codaprotocol.com/
 Maintainer: o(1)Labs <build@o1labs.org>
