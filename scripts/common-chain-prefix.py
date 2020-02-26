9f7a653be954944196b23c4ae712093ff40bd574
Paul Steckler <psteckler@o1labs.org>
2020-02-25 17:14:50 -0800

    use namespace

--- scripts/common-chain-prefix.py
+++ scripts/common-chain-prefix.py
@@ -5,23 +5,35 @@
 import os
 import sys
 import json
+
+from kubernetes import client, config
+
 import CodaClient
 
-def main (ip_addrs) :
+def main (namespace) :
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    pods = v1.list_pod_for_all_namespaces(watch=False)
+
     common_prefix = None
-    for ip_addr in ip_addrs :
-        client = CodaClient.Client (graphql_host=ip_addr)
-        result = client._send_query (query="query bestChainQuery { bestChain { stateHash } }")
-        chain = result['data']['bestChain']
-        if common_prefix is None :
-            common_prefix = chain
-        else :
-            common_prefix = os.path.commonprefix ([chain,common_prefix])
+
+    for pod in pods.items :
+        if pod.metadata.namespace == namespace and 'block-producer' in pod.metadata.name :
+            print ('Trying query on ' + pod.metadata.name + ' at IP ' + pod.status.pod_ip)
+            coda = CodaClient.Client (graphql_host=pod.status.pod_ip)
+            result = coda._send_query (query="query bestChainQuery { bestChain { stateHash } }")
+            chain = result['data']['bestChain']
+            if common_prefix is None :
+                common_prefix = chain
+            else :
+                common_prefix = os.path.commonprefix ([chain,common_prefix])
+
     print (common_prefix)
         
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: %s ip_addr0 ..." % sys.argv[0], file=sys.stderr)
+    if len(sys.argv) != 2:
+        print("Usage: %s namespace" % sys.argv[0], file=sys.stderr)
         sys.exit(1)
 
-    main(sys.argv[1:])
+    main(sys.argv[1])
