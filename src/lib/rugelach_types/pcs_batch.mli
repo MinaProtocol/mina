2c40f287ddaf76d95a8a7cc88f5040dc651c0ffd
Izaak Meckler <ihmeckler@gmail.com>
2020-02-25 19:12:45 -0800

    expand pcs_batch mli

--- src/lib/rugelach_types/pcs_batch.mli
+++ src/lib/rugelach_types/pcs_batch.mli
@@ -1,27 +1,48 @@
-type ('n, 'm) t
+type ('a, 'n, 'm) t
+
+val map: ('a, 'n, 'm) t -> f:('a -> 'b) -> ('b, 'n, 'm) t
+
+val pow : one:'f
+  -> mul:('f -> 'f -> 'f)
+  -> add:('f -> 'f -> 'f)
+  -> 'f
+  -> int
+  -> 'f
 
 val create :
      without_degree_bound:'n Nat.t
-  -> with_degree_bound:(int, 'm) Vector.t
-  -> ('n, 'm) t
+  -> with_degree_bound:('a, 'm) Vector.t
+  -> ('a, 'n, 'm) t
 
 val combine_commitments :
-     ('n Nat.s, 'm) t
+     (int, 'n , 'm) t
   -> scale:('g -> 'f -> 'g)
   -> add:('g -> 'g -> 'g)
   -> xi:'f
-  -> ('g, 'n Nat.s) Vector.t
+  -> ('g, 'n ) Vector.t
   -> ('g * 'g, 'm) Vector.t
   -> 'g
 
 val combine_evaluations :
-     ('n Nat.s, 'm) t
+     (int, 'n , 'm) t
   -> crs_max_degree:int
   -> mul:('f -> 'f -> 'f)
   -> add:('f -> 'f -> 'f)
   -> one:'f
   -> evaluation_point:'f
   -> xi:'f
-  -> ('f, 'n Nat.s) Vector.t
+  -> ('f, 'n ) Vector.t
+  -> ('f, 'm) Vector.t
+  -> 'f
+
+val combine_evaluations' :
+     ('a, 'n , 'm) t
+  -> shifted_pow:('a -> 'f -> 'f)
+  -> mul:('f -> 'f -> 'f)
+  -> add:('f -> 'f -> 'f)
+  -> one:'f
+  -> evaluation_point:'f
+  -> xi:'f
+  -> ('f, 'n ) Vector.t
   -> ('f, 'm) Vector.t
   -> 'f
