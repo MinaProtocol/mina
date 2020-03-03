d11198240051cedc1346304c253bae3ffdfb4d29
Paul Steckler <psteckler@o1labs.org>
2020-03-03 13:57:51 -0800

    use to_int, valid use there

--- src/lib/coda_base/user_command.ml
+++ src/lib/coda_base/user_command.ml
@@ -80,7 +80,7 @@ let fee = Fn.compose Payload.fee payload
 let nonce = Fn.compose Payload.nonce payload
 
 (* for filtering *)
-let minimum_fee = Fee.of_string "2000000000"
+let minimum_fee = Fee.of_int 2_000_000_000
 
 let is_trivial t = Fee.(fee t < minimum_fee)
 
