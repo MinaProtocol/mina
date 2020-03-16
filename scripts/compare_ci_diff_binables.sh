75e303e9ab0f3d32b31f7102f62dec50a66d75b3
Paul Steckler <steck@stecksoft.com>
2020-03-16 10:06:56 -0700

    use dev profile

--- scripts/compare_ci_diff_binables.sh
+++ scripts/compare_ci_diff_binables.sh
@@ -16,5 +16,5 @@ declare -x CODA_PRINT_BINABLE_FUNCTORS=true
 
 # build run_ppx_coda, then run Python script to compare binable functors in a pull request
 source ~/.profile && \
-    (dune build --profile=print_binable_functors src/lib/ppx_coda/run_ppx_coda.exe) && \
+    (dune build --profile=dev src/lib/ppx_coda/run_ppx_coda.exe) && \
     ./scripts/compare_pr_diff_binables.py ${CIRCLE_PULL_REQUEST}
