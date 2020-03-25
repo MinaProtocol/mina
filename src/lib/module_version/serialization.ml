6490cd57713da3c72363c6d2783c122fd9714a0a
psteckler <steck@stecksoft.com>
2020-03-25 10:07:10 -0700

    Print hashes in check_serialization when they differ (#4575)
    * Flag to show hash in check_serialization
    
    * print hashes iff they differ

--- src/lib/module_version/serialization.ml
+++ src/lib/module_version/serialization.ml
@@ -22,5 +22,10 @@ let check_serialization (type t) (module M : Binable.S with type t = t) (t : t)
   let ctx0 = init () in
   let ctx1 = feed_string ctx0 s in
   let hash = get ctx1 |> to_raw_string in
-  (* print_hash hash ; *)
-  String.equal hash known_good_hash
+  let result = String.equal hash known_good_hash in
+  if not result then (
+    printf "Expected hash: " ;
+    print_hash known_good_hash ;
+    printf "Got hash:      " ;
+    print_hash hash ) ;
+  result
