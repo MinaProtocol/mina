76d22611431d4b4926d23e47d6a5ac430959ac8c
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2020-03-29 17:30:07 -0700

    refactor

--- src/lib/user_command_input/user_command_input.ml
+++ src/lib/user_command_input/user_command_input.ml
@@ -44,6 +44,10 @@ module Payload = struct
 
   let create ~fee ~nonce_opt ~valid_until ~memo ~body : t =
     {common= Common.create ~fee ~nonce_opt ~valid_until ~memo; body}
+
+  let to_user_command_payload ~fee ~nonce ~valid_until ~memo ~body :
+      User_command_payload.t =
+    User_command.Payload.create ~fee ~nonce ~valid_until ~memo ~body
 end
 
 module Sign_choice = struct
@@ -88,6 +92,20 @@ let create ?(nonce_opt = None) ~fee ~valid_until ~memo ~body ~sender
   let payload = Payload.create ~fee ~nonce_opt ~valid_until ~memo ~body in
   {payload; sender; signature= sign_choice}
 
+let sign ~sender ~(user_command_payload : User_command_payload.t) = function
+  | Sign_choice.Signature signature ->
+      Option.value_map
+        ~default:(Deferred.return (Error "Invalid_signature"))
+        (User_command.create_with_signature_checked signature sender
+           user_command_payload)
+        ~f:Deferred.Result.return
+  | Keypair sender_kp ->
+      Deferred.Result.return (User_command.sign sender_kp user_command_payload)
+  | Hd_index hd_index ->
+      Secrets.Hardware_wallets.sign ~hd_index
+        ~public_key:(Public_key.decompress_exn sender)
+        ~user_command_payload
+
 let to_user_command ~result_cb ~inferred_nonce ~logger uc_inputs :
     (User_command.t list * (_ Or_error.t -> unit)) option Deferred.t =
   let setup_user_command (client_input : t) :
@@ -95,52 +113,55 @@ let to_user_command ~result_cb ~inferred_nonce ~logger uc_inputs :
     let open Deferred.Or_error.Let_syntax in
     let nonce_opt = client_input.payload.common.nonce in
     let payload_common = client_input.payload.common in
+    let err str =
+      sprintf "Error creating user command: %s Error: %s"
+        (Yojson.Safe.to_string (to_yojson client_input))
+        str
+    in
     let opt_error ~error_string = function
       | None ->
-          Deferred.Or_error.errorf "Error creating user command: %s Error: %s"
-            (Yojson.Safe.to_string (to_yojson client_input))
-            error_string
+          Deferred.Or_error.error_string (err error_string)
       | Some v ->
           Deferred.Or_error.return v
     in
-    let create_user_command nonce =
+    let create_user_command nonce : User_command.t Deferred.Or_error.t =
       let user_command_payload =
-        User_command.Payload.create ~fee:payload_common.fee ~nonce
+        Payload.to_user_command_payload ~fee:payload_common.fee ~nonce
           ~valid_until:payload_common.valid_until ~memo:payload_common.memo
           ~body:client_input.payload.body
       in
-      (*Capture the errors*)
       let%map signed_user_command =
-        match client_input.signature with
-        | Signature signature ->
-            User_command.create_with_signature_checked signature
-              client_input.sender user_command_payload
-            |> opt_error ~error_string:"Invalid signature"
-        | Keypair sender_kp ->
-            Deferred.Or_error.return
-              (User_command.sign sender_kp user_command_payload)
-        | Hd_index hd_index ->
-            Deferred.map
-              (Secrets.Hardware_wallets.sign ~hd_index
-                 ~public_key:(Public_key.decompress_exn client_input.sender)
-                 ~user_command_payload)
-              ~f:(Result.map_error ~f:Error.of_string)
+        Deferred.map
+          (sign ~sender:client_input.sender ~user_command_payload
+             client_input.signature)
+          ~f:(Result.map_error ~f:(fun str -> Error.of_string (err str)))
       in
       User_command.forget_check signed_user_command
+    in
+    let%bind nonce_inferred =
+      Participating_state.active (inferred_nonce client_input.sender)
+      |> Option.bind ~f:Fn.id
+      |> opt_error
+           ~error_string:
+             "Couldn't infer nonce for transaction from specified `sender` \
+              since `sender` is not in the ledger or sent a transaction in \
+              transaction pool."
     in
     let%bind nonce =
       match nonce_opt with
       | Some nonce ->
-          Deferred.Or_error.return nonce
+          if Account_nonce.equal nonce nonce_inferred then
+            Deferred.Or_error.return nonce
+          else
+            opt_error
+              ~error_string:
+                (sprintf
+                   !"Input nonce %s different from inferred nonce %s"
+                   (Account_nonce.to_string nonce)
+                   (Account_nonce.to_string nonce_inferred))
+              None
       | None ->
-          (*get inferred nonce*)
-          Participating_state.active (inferred_nonce client_input.sender)
-          |> Option.bind ~f:Fn.id
-          |> opt_error
-               ~error_string:
-                 "Couldn't infer nonce for transaction from specified \
-                  `sender` since `sender` is not in the ledger or sent a \
-                  transaction in transaction pool."
+          Deferred.Or_error.return nonce_inferred
     in
     create_user_command nonce
   in
