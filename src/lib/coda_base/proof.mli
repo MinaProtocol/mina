76529fbaa6e02806ea92aba4e1d1a3494d3a9581
Paul Steckler <steck@stecksoft.com>
2020-04-01 10:49:17 -0700

    Proof, use %%versioned_binable

--- src/lib/coda_base/proof.mli
+++ src/lib/coda_base/proof.mli
@@ -4,8 +4,9 @@ type t = Tock.Proof.t [@@deriving sexp, yojson]
 
 val dummy : Tock.Proof.t
 
+[%%versioned:
 module Stable : sig
   module V1 : sig
-    type nonrec t = t [@@deriving bin_io, compare, sexp, version, yojson]
+    type nonrec t = t [@@deriving compare, sexp, yojson]
   end
-end
+end]
