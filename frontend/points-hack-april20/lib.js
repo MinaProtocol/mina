46536e249fbf9fd157358ed0ed677ef286e113d8
Brandon Kase <bkase@o1labs.org>
2020-04-07 01:48:20 -0400

    Works locally

--- frontend/points-hack-april20/lib.js
+++ frontend/points-hack-april20/lib.js
@@ -1,16 +1,46 @@
+// GraphQL stuff
+
+const ws = require('ws');
+const { gql } = require('apollo-server');
+const {ApolloClient } = require('apollo-client');
+const { ApolloLink } = require( 'apollo-link');
+const { InMemoryCache } = require('apollo-cache-inmemory');
+const { HttpLink } = require('apollo-link-http');
+const { WebSocketLink } = require('apollo-link-ws');
+const fetch = require('node-fetch');
+
+const httpLink = new HttpLink({ uri: 'http://localhost:3085/graphql', fetch: fetch });
+
+const cache = new InMemoryCache();
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:3085/graphql',
+  options: { reconnect: true, connectionParams: null },
+  webSocketImpl: ws
+});
+
+const apolloClient = new ApolloClient({
+  cache: cache,
+  link: ApolloLink.split(operation => {
+    const def = operation.query.definitions[0];
+    return def.kind == 'OperationDefinition' && def.operation == 'subscription'
+  }, wsLink, httpLink)
+});
+
+// Gcloud
+
 const {Storage} = require('@google-cloud/storage');
 const { Readable } = require('stream');
 
-const storage = new Storage();
+const storage = new Storage({keyFilename: 'o1labs-192920-7e170ede82f5.json'});
 
-function uploadFile() {
+function uploadFile(result) {
   const bucketName = "points-data-hack-april20";
-  const filename = "metric" + Date.now();
+  const filename = "block-" + Date.now() + ".json";
 
   const bucket = storage.bucket(bucketName);
-  const file = bucket.file("test/" + filename);
+  const file = bucket.file("32qa/" + filename);
 
-  const buffer = Buffer.from(JSON.stringify({x: 3, y: "hello"}), 'utf8');
+  const buffer = Buffer.from(JSON.stringify(result), 'utf8');
   const readable = new Readable();
   readable._read = () => {};
   readable.push(buffer);
@@ -25,12 +55,87 @@ function uploadFile() {
     }
   }))
   .on('error', function(err) {
-      console.error(err);
+      console.error("Failed uploading metric to google cloude", err);
   })
   .on('finish', function() {
       console.log(`Finished uploading ${filename} to ${bucketName}`);
   });
 }
 
-uploadFile()
+// Subscription
+
+const SUBSCRIPTION=`
+subscription Blocks {
+  newBlock {
+    protocolState {
+      previousStateHash
+      consensusState {
+        blockHeight
+        epoch
+        epochCount
+        hasAncestorInSameCheckpointWindow
+        lastVrfOutput
+        minWindowDensity
+        nextEpochData {
+          epochLength
+          ledger {
+            hash
+            totalCurrency
+          }
+          lockCheckpoint
+          seed
+          startCheckpoint
+        }
+        slot
+        totalCurrency
+      }
+      blockchainState {
+        date
+        snarkedLedgerHash
+        stagedLedgerHash
+      }
+    }
+    stateHash
+    stateHashField
+    snarkJobs {
+      fee
+      prover
+      workIds
+    }
+    transactions {
+      coinbase
+      feeTransfer {
+        fee
+        recipient
+      }
+      userCommands {
+        amount
+        fee
+        fromAccount {
+          publicKey
+        }
+        id
+        isDelegation
+        memo
+        nonce
+        toAccount {
+          publicKey
+        }
+      }
+    }
+  }
+}`
+
+// listen on subscription and take results and upload them
+console.log("Listening for new blocks...");
+apolloClient.subscribe({
+  query: gql(SUBSCRIPTION),
+}).subscribe({
+  next (data) {
+    console.log("Uploading result", data);
+    uploadFile(data);
+  },
+  error(err) { console.log(`Finished with error: ${ err }`) },
+  complete() { console.log('Finished') }
+});
 
