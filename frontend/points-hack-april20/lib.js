148b5e50ebf7a77dc2f2a487faca6297404445e9
Brandon Kase <bkase@o1labs.org>
2020-04-07 02:37:26 -0400

    Prettier on the js

--- frontend/points-hack-april20/lib.js
+++ frontend/points-hack-april20/lib.js
@@ -1,35 +1,44 @@
 // GraphQL stuff
 
-const ws = require('ws');
-const { gql } = require('apollo-server');
-const {ApolloClient } = require('apollo-client');
-const { ApolloLink } = require( 'apollo-link');
-const { InMemoryCache } = require('apollo-cache-inmemory');
-const { HttpLink } = require('apollo-link-http');
-const { WebSocketLink } = require('apollo-link-ws');
-const fetch = require('node-fetch');
+const ws = require("ws");
+const { gql } = require("apollo-server");
+const { ApolloClient } = require("apollo-client");
+const { ApolloLink } = require("apollo-link");
+const { InMemoryCache } = require("apollo-cache-inmemory");
+const { HttpLink } = require("apollo-link-http");
+const { WebSocketLink } = require("apollo-link-ws");
+const fetch = require("node-fetch");
 
-const httpLink = new HttpLink({ uri: 'http://localhost:3085/graphql', fetch: fetch });
+const httpLink = new HttpLink({
+  uri: "http://localhost:3085/graphql",
+  fetch: fetch,
+});
 
 const cache = new InMemoryCache();
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:3085/graphql',
+  uri: "ws://localhost:3085/graphql",
   options: { reconnect: true, connectionParams: null },
-  webSocketImpl: ws
+  webSocketImpl: ws,
 });
 
 const apolloClient = new ApolloClient({
   cache: cache,
-  link: ApolloLink.split(operation => {
-    const def = operation.query.definitions[0];
-    return def.kind == 'OperationDefinition' && def.operation == 'subscription'
-  }, wsLink, httpLink)
+  link: ApolloLink.split(
+    (operation) => {
+      const def = operation.query.definitions[0];
+      return (
+        def.kind == "OperationDefinition" && def.operation == "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  ),
 });
 
 // Gcloud
 
-const {Storage} = require('@google-cloud/storage');
-const { Readable } = require('stream');
+const { Storage } = require("@google-cloud/storage");
+const { Readable } = require("stream");
 
 const storage = new Storage();
 
@@ -40,31 +49,34 @@ function uploadFile(result) {
   const bucket = storage.bucket(bucketName);
   const file = bucket.file("32qa/" + filename);
 
-  const buffer = Buffer.from(JSON.stringify(result), 'utf8');
+  const buffer = Buffer.from(JSON.stringify(result), "utf8");
   const readable = new Readable();
   readable._read = () => {};
   readable.push(buffer);
   readable.push(null);
 
-  readable.pipe(file.createWriteStream({
-    metadata: {
-      contentType: 'application/json',
-      metadata: {
-        cacheControl: 'public, max-age=31536000',
-      }
-    }
-  }))
-  .on('error', function(err) {
+  readable
+    .pipe(
+      file.createWriteStream({
+        metadata: {
+          contentType: "application/json",
+          metadata: {
+            cacheControl: "public, max-age=31536000",
+          },
+        },
+      })
+    )
+    .on("error", function (err) {
       console.error("Failed uploading metric to google cloude", err);
-  })
-  .on('finish', function() {
+    })
+    .on("finish", function () {
       console.log(`Finished uploading ${filename} to ${bucketName}`);
-  });
+    });
 }
 
 // Subscription
 
-const SUBSCRIPTION=`
+const SUBSCRIPTION = `
 subscription Blocks {
   newBlock {
     protocolState {
@@ -124,18 +136,23 @@ subscription Blocks {
       }
     }
   }
-}`
+}`;
 
 // listen on subscription and take results and upload them
 console.log("Listening for new blocks...");
-apolloClient.subscribe({
-  query: gql(SUBSCRIPTION),
-}).subscribe({
-  next (data) {
-    console.log("Uploading result", data);
-    uploadFile(data);
-  },
-  error(err) { console.log(`Finished with error: ${ err }`) },
-  complete() { console.log('Finished') }
-});
-
+apolloClient
+  .subscribe({
+    query: gql(SUBSCRIPTION),
+  })
+  .subscribe({
+    next(data) {
+      console.log("Uploading result", data);
+      uploadFile(data);
+    },
+    error(err) {
+      console.log(`Finished with error: ${err}`);
+    },
+    complete() {
+      console.log("Finished");
+    },
+  });
