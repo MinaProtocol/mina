9bef3919581eea9950222ec2dee4f55d09a08477
Paul Steckler <steck@stecksoft.com>
2020-04-08 19:37:48 -0700

    reformat comments

--- src/lib/ppx_coda/bin_io_unversioned.ml
+++ src/lib/ppx_coda/bin_io_unversioned.ml
@@ -27,23 +27,24 @@ let bin_io_gens :
       let deriver = Option.value_exn (Ppx_derivers.lookup name) in
       (* deriver is an instance of Ppxlib.Deriving.Deriver.deriver
 
-           if instead we consider it to be an instance of Ppx_deriving.t, the 0th
-           field, which should be the name, but prints out as garbage
+         if instead we consider it to be an instance of Ppx_deriving.t,
+           the 0th field, which should be the name, but prints out as garbage
 
-           the 0th field of deriver is a string "Ppxlib__Deriving.Deriver.T", perhaps an artifact
-             of the += type extension
-           the 1st field of deriver appears to be of type Ppxlib.Deriving.Deriver.t, and the 0th field
-             of that gives us an Actual_deriver.t
+         the 0th field of deriver is a string "Ppxlib__Deriving.Deriver.T",
+           perhaps an artifact of the += type extension
+         the 1st field of deriver appears to be of type
+           Ppxlib.Deriving.Deriver.t, and the 0th field of that gives us
+           an Actual_deriver.t
 
-           the 0th field of the Actual_deriver.t should be the name, which prints out as
-             expected
+         the 0th field of the Actual_deriver.t should be the name, which
+           prints out as expected
 
-           the number of fields of the Actual_deriver.t is 8, which corresponds to the type
-             definition
+         the number of fields of the Actual_deriver.t is 8, which corresponds
+           to the type definition
 
-           from the Actual_deriver.t, we pull out the 1st field, the Generator.t with the label
-             str_type_decl; we then apply the generator
-        *)
+         from the Actual_deriver.t, we pull out the 1st field, the Generator.t
+           with the label str_type_decl; we then apply the generator
+      *)
       let actual_deriver =
         let constructor_argument_idx = 0 in
         let extension_constructor_argument_idx = 1 in
@@ -61,7 +62,8 @@ let bin_io_gens :
       in
       let generator :
           (structure, rec_flag * type_declaration list) Deriving.Generator.t =
-        (* Ppxlib.Deriving.Deriver.Actual_deriver.t, 1st field is str_type_decl *)
+        (* Ppxlib.Deriving.Deriver.Actual_deriver.t, 1st field is
+             str_type_decl *)
         let str_type_decl_idx = 1 in
         Option.value_exn
           (actual_deriver |> fun r -> Obj.(field r str_type_decl_idx |> obj))
