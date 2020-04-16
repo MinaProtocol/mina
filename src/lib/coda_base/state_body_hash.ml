7a924574dc693cd7c96a479ddb66cae77a003dc5
ember arlynx <cmr@o1labs.org>
2020-04-15 17:20:31 -0700

    data_hash: use b58 check for json (#4692)

--- src/lib/coda_base/state_body_hash.ml
+++ src/lib/coda_base/state_body_hash.ml
@@ -1,3 +1,14 @@
 include Data_hash.Make_full_size ()
 
+module Base58_check = Codable.Make_base58_check (struct
+  include Stable.Latest
+
+  let description = "State body hash"
+
+  let version_byte = Base58_check.Version_bytes.state_body_hash
+end)
+
+[%%define_locally
+Base58_check.(to_yojson, of_yojson)]
+
 let dummy = of_hash Outside_pedersen_image.t
