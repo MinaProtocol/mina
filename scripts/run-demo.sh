9babcbd5da1cdfd2a533c02c686212fc9a1c70a9
Brandon Kase <bkase@o1labs.org>
2020-04-19 18:32:34 -0400

    Adds back block_producer

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -8,4 +8,4 @@ echo '{"txpool_max_size":3000,"genesis_state_timestamp":"'$(date '+%Y-%m-%d %H:%
 
 export CODA_PRIVKEY_PASS=""
 
-exec coda daemon -seed -demo-mode -run-snark-worker 4vsRCVMNTrCx4NpN6kKTkFKLcFN4vXUP5RB9PqSZe1qsyDs4AW5XeNgAf16WUPRBCakaPiXcxjp6JUpGNQ6fdU977x5LntvxrSg11xrmK6ZDaGSMEGj12dkeEpyKcEpkzcKwYWZ2Yf2vpwQP -genesis-constants "$genesis_constants_file" -insecure-rest-server $@
+exec coda daemon -seed -demo-mode -block-producer-key /root/keys/demo-block-producer -run-snark-worker 4vsRCVMNTrCx4NpN6kKTkFKLcFN4vXUP5RB9PqSZe1qsyDs4AW5XeNgAf16WUPRBCakaPiXcxjp6JUpGNQ6fdU977x5LntvxrSg11xrmK6ZDaGSMEGj12dkeEpyKcEpkzcKwYWZ2Yf2vpwQP -genesis-constants "$genesis_constants_file" -insecure-rest-server $@
