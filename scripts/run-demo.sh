3fe124bc8ce61b71fa61bbf278e5b034141c1912
Brandon Kase <bkase@o1labs.org>
2020-04-19 14:53:38 -0400

    Rewrites run-demo.sh to use runtime genesis

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-genesis_time=$(date -d "$(coda advanced compile-time-constants | jq -r '.genesis_state_timestamp')" +%s)
-now_time=$(date +%s)
+genesis_constants_file=$(mktemp)
 
-export CODA_TIME_OFFSET=$(( $now_time - $genesis_time ))
+echo '{"txpool_max_size":3000,"genesis_state_timestamp":"'$(date '+%Y-%m-%d %H:%M:%S%z')'"}' > "$genesis_constants_file"
+
 export CODA_PRIVKEY_PASS=""
 
-exec coda daemon -seed -demo-mode -block-producer-key /root/keys/demo-block-producer -run-snark-worker 4vsRCVMNTrCx4NpN6kKTkFKLcFN4vXUP5RB9PqSZe1qsyDs4AW5XeNgAf16WUPRBCakaPiXcxjp6JUpGNQ6fdU977x5LntvxrSg11xrmK6ZDaGSMEGj12dkeEpyKcEpkzcKwYWZ2Yf2vpwQP -insecure-rest-server $@
+exec coda.exe daemon -seed -demo-mode -run-snark-worker 4vsRCVMNTrCx4NpN6kKTkFKLcFN4vXUP5RB9PqSZe1qsyDs4AW5XeNgAf16WUPRBCakaPiXcxjp6JUpGNQ6fdU977x5LntvxrSg11xrmK6ZDaGSMEGj12dkeEpyKcEpkzcKwYWZ2Yf2vpwQP -genesis-constants "$genesis_constants_file" -insecure-rest-server $@
