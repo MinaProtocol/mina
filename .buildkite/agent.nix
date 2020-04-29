4aab6a5f7c326a3c92a941ab308f3b58b91c132a
Brandon Kase <bkase@o1labs.org>
2020-04-29 19:54:46 -0400

    Docs on agent.nix

--- .buildkite/agent.nix
+++ .buildkite/agent.nix
@@ -1,9 +1,17 @@
-# minimal nix shell config for running local agents for testing purposes
+# Minimal nix shell config for running local agents for testing purposes
+#
+# Usage:
+#
+# nix-shell agent.nix
+# buildkite-agent start --tags size=small --token <TOKEN> --build-path ~/buildkite
+#
+# Replace `tags` with anything relevant and token with the buildkite token for O(1) Labs
 
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     buildInputs = [ pkgs.buildkite-agent pkgs.dhall pkgs.dhall-json ];
     shellHook = ''
+      mkdir -p ~/buildkite
       export GIT_SSL_CAINFO=/etc/ssl/certs/ca-certificates.crt
       export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
     '';
