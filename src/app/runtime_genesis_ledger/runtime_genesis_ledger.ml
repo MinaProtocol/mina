6dcb2fba43cff500eedd81ea0e24ae9ed9fab149
mrmr1993 <mr_1993@hotmail.co.uk>
2020-05-05 17:13:27 +0100

    Remove unused functions

--- src/app/runtime_genesis_ledger/runtime_genesis_ledger.ml
+++ src/app/runtime_genesis_ledger/runtime_genesis_ledger.ml
@@ -4,54 +4,12 @@ open Coda_base
 
 type t = Ledger.t
 
-let generate_base_proof ~ledger ~(genesis_constants : Genesis_constants.t) =
-  let%map ((module Keys) as keys) = Keys_lib.Keys.create () in
-  let protocol_state_with_hash =
-    Coda_state.Genesis_protocol_state.t
-      ~genesis_ledger:(Genesis_ledger.Packed.t ledger)
-      ~genesis_constants
-  in
-  let base_hash = Keys.Step.instance_hash protocol_state_with_hash.data in
-  let computed_values =
-    Genesis_proof.create_values ~keys
-      { genesis_ledger= ledger
-      ; protocol_state_with_hash
-      ; base_hash
-      ; genesis_constants }
-  in
-  (base_hash, computed_values.genesis_proof)
-
 let compiled_accounts_json () : Account_config.t =
   List.map (Lazy.force Test_genesis_ledger.accounts) ~f:(fun (sk_opt, acc) ->
       { Account_config.pk= acc.public_key
       ; sk= sk_opt
       ; balance= acc.balance
       ; delegate= Some acc.delegate } )
-
-let generate_ledger :
-    directory_name:string -> Account_config.t -> Genesis_ledger.Packed.t =
- fun ~directory_name accounts ->
-  let ledger = Ledger.create ~directory_name () in
-  let accounts =
-    List.map accounts ~f:(fun {pk; sk; balance; delegate} ->
-        let account =
-          let account_id = Account_id.create pk Token_id.default in
-          let base_acct = Account.create account_id balance in
-          {base_acct with delegate= Option.value ~default:pk delegate}
-        in
-        Ledger.create_new_account_exn ledger
-          (Account.identifier account)
-          account ;
-        (sk, account) )
-  in
-  Ledger.commit ledger ;
-  ( module struct
-    include Genesis_ledger.Make (struct
-      let accounts = lazy accounts
-    end)
-
-    let t = lazy ledger
-  end )
 
 let get_accounts accounts_json_file n =
   let open Deferred.Or_error.Let_syntax in
