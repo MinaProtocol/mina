4e047b2a69ddf08c909ad163458d1fcb62fdae65
Izaak Meckler <ihmeckler@gmail.com>
2020-05-06 10:16:08 -0700

    blockchain snark state without unit test

--- src/lib/blockchain_snark/blockchain_snark_state.ml
+++ src/lib/blockchain_snark/blockchain_snark_state.ml
@@ -315,52 +315,6 @@ struct
   module Proof = (val p)
 end
 
-let time lab f =
-  let start = Time.now () in
-  let x = f () in
-  let stop = Time.now () in
-  printf "%s: %s\n%!" lab (Time.Span.to_string_hum (Time.diff stop start)) ;
-  x
-
-let%test_unit "blockchain profiling" =
-  let module T = Transaction_snark.Make () in
-  let module B = Make (T) in
-  let protocol_state_with_hash =
-    Genesis_protocol_state.compile_time_genesis ()
-  in
-  let prev_state =
-    Protocol_state.negative_one ~genesis_ledger:Test_genesis_ledger.t
-  in
-  let curr = protocol_state_with_hash.data in
-  let dummy_txn_stmt : Transaction_snark.Statement.With_sok.t =
-    { sok_digest= Coda_base.Sok_message.Digest.default
-    ; source=
-        Blockchain_state.snarked_ledger_hash
-          (Protocol_state.blockchain_state prev_state)
-    ; target=
-        Blockchain_state.snarked_ledger_hash
-          (Protocol_state.blockchain_state curr)
-    ; supply_increase= Currency.Amount.zero
-    ; fee_excess= Currency.Amount.Signed.zero
-    ; pending_coinbase_stack_state=
-        { source= Coda_base.Pending_coinbase.Stack.empty
-        ; target= Coda_base.Pending_coinbase.Stack.empty } }
-  in
-  let dummy = Coda_base.Proof.dummy in
-  let _ =
-    time "step" (fun () ->
-        B.step
-          ~handler:
-            (Consensus.Data.Prover_state.precomputed_handler
-               ~genesis_ledger:Test_genesis_ledger.t)
-          { transition=
-              Snark_transition.genesis ~genesis_ledger:Test_genesis_ledger.t
-          ; prev_state }
-          [(prev_state, dummy); (dummy_txn_stmt, dummy)]
-          protocol_state_with_hash.data )
-  in
-  ()
-
 let constraint_system_digests () =
   let digest = Tick.R1CS_constraint_system.digest in
   [ ( "blockchain-step"
