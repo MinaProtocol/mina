13723c7777c291c4cfd44142f4800c8837817a3b
Izaak Meckler <ihmeckler@gmail.com>
2020-05-06 09:14:58 -0700

    pickles cache

---
+++ src/lib/pickles/cache.ml
@@ -0,0 +1,85 @@
+open Core
+
+module Step = struct
+  module Key = struct
+    module Proving = struct
+      type t =
+        Type_equal.Id.Uid.t
+        * int
+        * Snarky_bn382_backend.Pairing_based.R1CS_constraint_system.t
+
+      let to_string : t -> _ = function
+        | _id, _n, h ->
+            sprintf !"step-%s"
+              (Md5.to_hex
+                 (Snarky_bn382_backend.Pairing_based.R1CS_constraint_system
+                  .digest h))
+    end
+
+    module Verification = struct
+      type t = Type_equal.Id.Uid.t * int * Md5.t
+
+      let to_string : t -> _ = function
+        | _id, _n, h ->
+            sprintf !"vk-step-%s" (Md5.to_hex h)
+    end
+  end
+
+  let storable =
+    Key_cache.Disk_storable.simple Key.Proving.to_string
+      (fun (_, _, t) ~path ->
+        Snarky_bn382.Fp_index.read
+          (Snarky_bn382_backend.Pairing_based.Keypair.load_urs ())
+          t.m.a t.m.b t.m.c
+          (Unsigned.Size_t.of_int (1 + t.public_input_size))
+          path )
+      Snarky_bn382.Fp_index.write
+
+  let vk_storable =
+    Key_cache.Disk_storable.simple Key.Verification.to_string
+      (fun _ ~path -> Snarky_bn382.Fp_verifier_index.read path)
+      Snarky_bn382.Fp_verifier_index.write
+end
+
+module Wrap = struct
+  module Key = struct
+    module Verification = struct
+      type t = Type_equal.Id.Uid.t * Md5.t [@@deriving sexp]
+
+      let to_string : t -> _ = function
+        | _id, h ->
+            sprintf !"vk-wrap-%s" (Md5.to_hex h)
+    end
+
+    module Proving = struct
+      type t =
+        Type_equal.Id.Uid.t
+        * Snarky_bn382_backend.Dlog_based.R1CS_constraint_system.t
+
+      let to_string : t -> _ = function
+        | _id, h ->
+            sprintf !"wrap-%s"
+              (Md5.to_hex
+                 (Snarky_bn382_backend.Dlog_based.R1CS_constraint_system.digest
+                    h))
+    end
+  end
+
+  let storable =
+    Key_cache.Disk_storable.simple Key.Proving.to_string
+      (fun (_, t) ~path ->
+        Snarky_bn382.Fq_index.read
+          (Snarky_bn382_backend.Dlog_based.Keypair.load_urs ())
+          t.m.a t.m.b t.m.c
+          (Unsigned.Size_t.of_int (1 + t.public_input_size))
+          path )
+      Snarky_bn382.Fq_index.write
+
+  let vk_storable =
+    Key_cache.Disk_storable.simple Key.Verification.to_string
+      (fun _ ~path ->
+        Snarky_bn382.Fq_verifier_index.read
+          (Snarky_bn382_backend.Dlog_based.Keypair.load_urs ())
+          path )
+      Snarky_bn382.Fq_verifier_index.write
+end
