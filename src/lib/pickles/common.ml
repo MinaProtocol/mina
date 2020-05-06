99d19d7960076f0977222429f98d937944fba602
Izaak Meckler <ihmeckler@gmail.com>
2020-05-06 10:19:08 -0700

    pickles printing tweaks

--- src/lib/pickles/common.ml
+++ src/lib/pickles/common.ml
@@ -47,20 +47,23 @@ let split_last xs =
   go [] xs
 
 let when_profiling profiling default =
-  match Option.map (Sys.getenv_opt "PICKLES_PROFILING") ~f:String.lowercase with
-  | None
-  | Some ("0" | "false") -> default
-  | Some _ -> profiling
+  match
+    Option.map (Sys.getenv_opt "PICKLES_PROFILING") ~f:String.lowercase
+  with
+  | None | Some ("0" | "false") ->
+      default
+  | Some _ ->
+      profiling
 
 let time lab f =
-  when_profiling (fun () ->
-    let start = Time.now () in
-    let x = f () in
-    let stop = Time.now () in
-    printf "%s: %s\n%!" lab (Time.Span.to_string_hum (Time.diff stop start)) ;
-    x )
-    f
-    ()
+  when_profiling
+    (fun () ->
+      let start = Time.now () in
+      let x = f () in
+      let stop = Time.now () in
+      printf "%s: %s\n%!" lab (Time.Span.to_string_hum (Time.diff stop start)) ;
+      x )
+    f ()
 
 let bits_random_oracle =
   let h = Digestif.blake2s 32 in
