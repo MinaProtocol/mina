93e8ee1c92a1bebf3690f870d3c5c6e549910cad
Izaak Meckler <ihmeckler@gmail.com>
2020-05-06 09:36:23 -0700

    remove chunk table

--- src/lib/crypto_params/chunk_table/chunk_table.ml
+++
@@ -1,9 +0,0 @@
-(* chunk_table.ml -- wrapper around table of memoized curve points in Pedersen hash *)
-
-open Core_kernel
-module Group = Snarky_bn382_backend.G
-
-type t = {table_data: Group.t Array.t Array.t} [@@deriving bin_io]
-
-(** creates chunk table *)
-let create table_data = {table_data}
