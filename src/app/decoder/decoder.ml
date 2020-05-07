a898d0d79b396d8d55a41bcf4f45ddc2123f1561
mrmr1993 <mr_1993@hotmail.co.uk>
2020-05-07 14:52:59 +0100

    Reformat

--- src/app/decoder/decoder.ml
+++ src/app/decoder/decoder.ml
@@ -1,11 +1,8 @@
 open Coda_base
 
 let () =
-  if Array.length Sys.argv < 2 then
-    failwith "MISSING ARGUMENT"
+  if Array.length Sys.argv < 2 then failwith "MISSING ARGUMENT"
   else
     let state_hash_b58 = Sys.argv.(1) in
     State_hash.of_base58_check_exn state_hash_b58
-    |> State_hash.to_yojson
-    |> Yojson.Safe.to_string
-    |> print_endline
+    |> State_hash.to_yojson |> Yojson.Safe.to_string |> print_endline
