4d13f0feda433d325bf38bf48c1da6385dbe007d
Matthew Ryan <MR_1993@hotmail.co.uk>
2020-05-08 07:01:59 +0100

    Carry the ledger depth in the ledger (#4860)
    * Routing of ledger_depth
    
    * Expose depth from Genesis_ledger.Intf.S
    
    * Extract ledger_depth from precomputed_values wherever possible
    
    * Remove unused functor in Merkle_address
    
    * Fixup Empty_hashes
    
    * Typo
    
    * Add missing ledger_depth
    
    * Add depth arguments in Merkle_ledger_tests
    
    * Add missing depth argument in Transition_frontier.Full_frontier_tests
    
    * Syncable_ledger

--- src/lib/empty_hashes/empty_hashes.ml
+++ src/lib/empty_hashes/empty_hashes.ml
@@ -6,14 +6,30 @@ module type Hash_intf = sig
   val merge : height:int -> t -> t -> t
 end
 
-let cache (type hash) (module Hash : Hash_intf with type t = hash)
-    ~(init_hash : hash) depth =
-  let empty_hashes = Array.create ~len:(depth + 1) init_hash in
-  let rec loop last_hash height =
-    if height <= depth then (
-      let hash = Hash.merge ~height:(height - 1) last_hash last_hash in
-      empty_hashes.(height) <- hash ;
-      loop hash (height + 1) )
-  in
-  loop init_hash 1 ;
-  Immutable_array.of_array empty_hashes
+let merge_hash (type hash) (module Hash : Hash_intf with type t = hash) height
+    (last_hash : hash) =
+  Hash.merge ~height last_hash last_hash
+
+let cache hash_mod ~init_hash depth =
+  let last_hash = ref init_hash in
+  Immutable_array.of_array
+  @@ Array.init (depth + 1) ~f:(fun i ->
+         if Int.equal i 0 then !last_hash
+         else (
+           last_hash := merge_hash hash_mod (i - 1) !last_hash ;
+           !last_hash ) )
+
+let extensible_cache hash_mod ~init_hash =
+  let empty_hashes = ref [|init_hash|] in
+  fun i ->
+    let prev = !empty_hashes in
+    let height = Array.length prev - 1 in
+    let deficit = i - height in
+    ( if deficit > 0 then
+      let last_hash = ref (Array.last prev) in
+      empty_hashes :=
+        Array.append prev
+          (Array.init deficit ~f:(fun i ->
+               last_hash := merge_hash hash_mod (i + height) !last_hash ;
+               !last_hash )) ) ;
+    !empty_hashes.(i)
