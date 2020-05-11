52d4192eb248ba675c6e50d66c0629b4792b99d6
mrmr1993 <mr_1993@hotmail.co.uk>
2020-05-11 13:19:13 +0100

    Runtime ledger_depth in Merkle_ledger.Location.Make

--- src/lib/merkle_ledger_tests/test_mask.ml
+++ src/lib/merkle_ledger_tests/test_mask.ml
@@ -739,7 +739,7 @@ module Make_maskable_and_mask_with_depth (Depth : Depth_S) = struct
   let depth = Depth.depth
 
   module Location : Merkle_ledger.Location_intf.S =
-    Merkle_ledger.Location.Make (Depth)
+    Merkle_ledger.Location.T
 
   module Location_binable = struct
     module Arg = struct
@@ -766,7 +766,6 @@ module Make_maskable_and_mask_with_depth (Depth : Depth_S) = struct
     module Location_binable = Location_binable
     module Kvdb = In_memory_kvdb
     module Storage_locations = Storage_locations
-    module Depth = Depth
   end
 
   (* underlying Merkle tree *)
