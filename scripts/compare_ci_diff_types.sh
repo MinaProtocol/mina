267161e815ef4a162f7db1dccd09b0d279439a48
Paul Steckler <steck@stecksoft.com>
2020-05-15 15:08:08 -0700

    fix directory name

--- scripts/compare_ci_diff_types.sh
+++ scripts/compare_ci_diff_types.sh
@@ -13,5 +13,5 @@ rm -rf base
 # build print_versioned_types, then run Python script to compare versioned types in a pull request
 
 source ~/.profile && \
-    (dune build --profile=dev src/lib/ppx_coda/print_versioned_types.exe) && \
+    (dune build --profile=dev src/lib/ppx_version/print_versioned_types.exe) && \
     ./scripts/compare_pr_diff_types.py ${CIRCLE_PULL_REQUEST}
