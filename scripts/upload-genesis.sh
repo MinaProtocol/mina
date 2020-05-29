f87d3a04cedf30529407bb868046df7a330307f4
Brandon Kase <bkase@o1labs.org>
2020-05-28 22:19:41 -0400

    Upload the runtime genesis ledger after generation

---
+++ scripts/upload-genesis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GENESIS_DIR="/tmp/coda_cache_dir/"
+
+# Need AWS creds to upload
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+    echo "WARNING: AWS_ACCESS_KEY_ID is missing, not uploading files for genesis"
+else
+    ls -l ${GENESIS_DIR}genesis_* && \
+    aws s3 sync --exclude "*" --include "coda_genesis_*" --acl public-read ${GENESIS_DIR} s3://snark-keys.o1test.net/
+fi
