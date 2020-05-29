d76165f6763b4c22da4d46ae5480b576ee38e3ea
mrmr1993 <mr_1993@hotmail.co.uk>
2020-05-29 03:49:40 +0100

    Make it so that upload-genesis will not fail if genesis data was cached

--- scripts/upload-genesis.sh
+++ scripts/upload-genesis.sh
@@ -6,6 +6,7 @@ GENESIS_DIR="/tmp/coda_cache_dir/"
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
     echo "WARNING: AWS_ACCESS_KEY_ID is missing, not uploading files for genesis"
 else
-    ls -l ${GENESIS_DIR}coda_genesis_* && \
-    aws s3 sync --exclude "*" --include "coda_genesis_*" --acl public-read ${GENESIS_DIR} s3://snark-keys.o1test.net/
+    if ls -l ${GENESIS_DIR}coda_genesis_*; then
+        aws s3 sync --exclude "*" --include "coda_genesis_*" --acl public-read ${GENESIS_DIR} s3://snark-keys.o1test.net/
+    fi
 fi
