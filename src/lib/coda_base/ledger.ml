b59622d7184e37ed94a024d0f0bc800443a961f8
Matthew Ryan <MR_1993@hotmail.co.uk>
2020-06-03 20:28:13 +0100

    Include account layout information in genesis ledger filenames (#5002)
    * Cache digest of the empty account in Coda_base.Account, do not recompute
    
    * Include the empty account hash when identifying genesis ledgers
    
    * Consider account serialisation for ledger identifier
    
    * Consider record layout for hash-derived filename of genesis ledger
    
    * Reformat
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/lib/coda_base/ledger.ml
+++ src/lib/coda_base/ledger.ml
@@ -58,7 +58,7 @@ module Ledger_inner = struct
 
         let hash_account = Fn.compose Ledger_hash.of_digest Account.digest
 
-        let empty_account = hash_account Account.empty
+        let empty_account = Ledger_hash.of_digest Account.empty_digest
       end
     end]
 
