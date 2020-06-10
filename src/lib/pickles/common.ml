8eed22a1df91908cb2f1ae945a0f7dd529eaf4cf
Izaak Meckler <ihmeckler@gmail.com>
2020-06-10 16:35:08 -0700

    time functiong

--- src/lib/pickles/common.ml
+++ src/lib/pickles/common.ml
@@ -8,3 +8,14 @@ let when_profiling profiling default =
       default
   | Some _ ->
       profiling
+
+let time lab f =
+  when_profiling
+    (fun () ->
+      let start = Time.now () in
+      let x = f () in
+      let stop = Time.now () in
+      printf "%s: %s\n%!" lab (Time.Span.to_string_hum (Time.diff stop start)) ;
+      x )
+    f ()
+
