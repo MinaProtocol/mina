a82f5b3cf7ddd787208303ac4946718bd5256c53
Izaak Meckler <ihmeckler@gmail.com>
2020-06-12 16:26:55 -0700

    Pickles: "inputs" (#5091)
    * predecessor work
    
    * endo stuff
    
    * add ppx version
    
    * version domain
    
    * timer
    
    * "impls" and a few utility functions
    
    * time functiong
    
    * reformat
    
    * verification key
    
    * clean up
    
    * cache stuff
    
    * functionality related to the sponge
    
    * the "inputs" files
    
    * Remove repeated functor parameter
    
    * fix bug
    
    * bump snarky
    
    Co-authored-by: mrmr1993 <mr_1993@hotmail.co.uk>

--- src/lib/pickles/common.ml
+++ src/lib/pickles/common.ml
@@ -1,6 +1,9 @@
 open Core_kernel
 open Pickles_types
 module Rounds = Zexe_backend.Dlog_based.Rounds
+module Unshifted_acc =
+  Pairing_marlin_types.Accumulator.Degree_bound_checks.Unshifted_accumulators
+open Import
 
 let crs_max_degree = 1 lsl Nat.to_int Rounds.n
 
@@ -27,3 +30,36 @@ let time lab f =
       printf "%s: %s\n%!" lab (Time.Span.to_string_hum (Time.diff stop start)) ;
       x )
     f ()
+
+let bits_random_oracle =
+  let h = Digestif.blake2s 32 in
+  fun ?(length = 256) s ->
+    Digestif.digest_string h s |> Digestif.to_raw_string h |> String.to_list
+    |> List.concat_map ~f:(fun c ->
+           let c = Char.to_int c in
+           List.init 8 ~f:(fun i -> (c lsr i) land 1 = 1) )
+    |> fun a -> List.take a length
+
+let bits_to_bytes bits =
+  let byte_of_bits bs =
+    List.foldi bs ~init:0 ~f:(fun i acc b ->
+        if b then acc lor (1 lsl i) else acc )
+    |> Char.of_int_exn
+  in
+  List.map (List.groupi bits ~break:(fun i _ _ -> i mod 8 = 0)) ~f:byte_of_bits
+  |> String.of_char_list
+
+let group_map m ~a ~b =
+  let params = Group_map.Params.create m {a; b} in
+  stage (fun x -> Group_map.to_group m ~params x)
+
+let compute_challenge ~is_square x =
+  let open Zexe_backend in
+  let nonresidue = Fq.of_int 7 in
+  let x = Endo.Dlog.to_field x in
+  assert (is_square = Fq.is_square x) ;
+  Fq.sqrt (if is_square then x else Fq.(nonresidue * x))
+
+let compute_challenges chals =
+  Vector.map chals ~f:(fun {Bulletproof_challenge.prechallenge; is_square} ->
+      compute_challenge ~is_square prechallenge )
