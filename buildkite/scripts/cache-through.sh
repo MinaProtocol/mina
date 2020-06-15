192e098ed43870ea63c95df46a1a960cae02789a
Brandon Kase <bkase@o1labs.org>
2020-06-15 14:13:53 -0400

    Made requested changes

--- buildkite/scripts/cache-through.sh
+++ buildkite/scripts/cache-through.sh
@@ -12,16 +12,16 @@ fi
 # TODO: Bake this into the agents
 if [[ ! -f ./google-cloud-sdk/bin/gsutil ]]; then
   echo "Downloading gsutil because it doesn't exist"
-  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-296.0.1-linux-x86_64.tar.gz && tar -zxf google-cloud-sdk-296.0.1-linux-x86_64.tar.gz
+  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-296.0.1-linux-x86_64.tar.gz
 
-  tar -zxf google-cloud-sdk-296.0.1-linux-x86_64.tar.gz
+  tar -zxf google-cloud-sdk-296.0.1-linux-x86_64.tar.gz -C /usr/local/
 
   echo "$BUILDKITE_GS_APPLICATION_CREDENTIALS_JSON" > /tmp/gcp_creds.json
 
   export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcp_creds.json && ./google-cloud-sdk/bin/gcloud auth activate-service-account bk-large@o1labs-192920.iam.gserviceaccount.com --key-file /tmp/gcp_creds.json
 fi
 
-UPLOAD_BIN=./google-cloud-sdk/bin/gsutil
+UPLOAD_BIN=/usr/local/google-cloud-sdk/bin/gsutil
 # UPLOAD_BIN=echo
 FILE="$1"
 CMD="$2"
