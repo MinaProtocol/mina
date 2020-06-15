0b4654f680a7cee74a560afc1376bf337350748f
Brandon Kase <bkase@o1labs.org>
2020-06-15 14:32:11 -0400

    Makes thing an env var

--- buildkite/scripts/cache-through.sh
+++ buildkite/scripts/cache-through.sh
@@ -22,18 +22,19 @@ if [[ ! -f ./google-cloud-sdk/bin/gsutil ]]; then
 fi
 
 UPLOAD_BIN=/usr/local/google-cloud-sdk/bin/gsutil
+PREFIX=gs://buildkite_k8s/coda/shared
 # UPLOAD_BIN=echo
 FILE="$1"
 CMD="$2"
 
 set +e
-$UPLOAD_BIN cp gs://buildkite_k8s/coda/shared/${FILE} . ; echo $? > download_status.txt
+$UPLOAD_BIN cp ${PREFIX}/${FILE} . ; echo $? > download_status.txt
 set -e
 
 if ! ( exit $(cat download_status.txt) ); then
   echo "*** Cache miss -- executing step ***"
   bash -c "$CMD"
-  $UPLOAD_BIN cp ${FILE} gs://buildkite_k8s/coda/shared/${FILE}
+  $UPLOAD_BIN cp ${FILE} ${PREFIX}/${FILE}
 else
   echo "*** Cache Hit -- skipping step ***"
 fi
