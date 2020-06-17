8aded5db0944cedc2f20c83d15648f67e44ac733
Izaak Meckler <ihmeckler@gmail.com>
2020-06-17 14:39:57 -0700

    fix hashes

--- src/lib/non_zero_curve_point/non_zero_curve_point.ml
+++ src/lib/non_zero_curve_point/non_zero_curve_point.ml
@@ -91,7 +91,7 @@ module Compressed = struct
           Quickcheck.random_value
             ~seed:(`Deterministic "nonzero_curve_point_compressed-seed") V1.gen
         in
-        let known_good_digest ="96893614296cb3768fd7e389efe9e0eb" in
+        let known_good_digest = "96893614296cb3768fd7e389efe9e0eb" in
         Ppx_version.Serialization.check_serialization
           (module V1)
           point known_good_digest
