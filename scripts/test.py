cc9bc510a193c255baa1e113466a104fa5a11abc
mrmr1993 <mr_1993@hotmail.co.uk>
2020-06-24 03:17:55 +0100

    Use a common 'dev' build for config-agnostic integration tests

--- scripts/test.py
+++ scripts/test.py
@@ -33,34 +33,57 @@ simple_tests = [
 ]
 
 integration_tests = [
-    'coda-peers-test',
-    'coda-transitive-peers-test',
-    'coda-block-production-test',
-    'coda-shared-prefix-test -who-produces 0',
-    'coda-shared-prefix-test -who-produces 1',
-    'coda-change-snark-worker-test',
-    'coda-archive-node-test'
+    #'coda-peers-test',
+    #'coda-transitive-peers-test',
+    #'coda-block-production-test',
+    #'coda-shared-prefix-test -who-produces 0',
+    #'coda-shared-prefix-test -who-produces 1',
+    #'coda-change-snark-worker-test',
+    #'coda-archive-node-test'
 ]
 
 all_tests = simple_tests + integration_tests
+
+compile_config_agnostic_tests = [
+    'coda-archive-node-test',
+    # Disabled: manually run in CI
+    # 'coda-archive-processor-test',
+    'coda-batch-payment-test',
+    'coda-block-production-test',
+    'coda-bootstrap-test',
+    'coda-change-snark-worker-test',
+    'coda-delegation-test',
+    # Disabled
+    # 'coda-long-fork',
+    'coda-peers-test',
+    'coda-restart-node-test',
+    # Disabled
+    # 'coda-restarts-and-txns-holy-grail',
+    'coda-shared-prefix-test -who-produces 0',
+    'coda-shared-prefix-test -who-produces 1',
+    'coda-shared-state-test',
+    'coda-transitive-peers-test',
+    'coda-txns-and-restart-non-producers'
+]
+
+compile_config_agnostic_profiles = ['dev']
 
 # dictionary mapping configs to lists of tests
 small_curves_tests = {
     'fake_hash': ['full-test'],
     'test_postake_snarkless':
     simple_tests,
-    'test_postake_split_snarkless':
-    integration_tests,
+    # 'test_postake_split_snarkless':
+    # integration_tests,
     'test_postake_split':
     ['coda-shared-prefix-multiproducer-test -num-block-producers 2'],
     'test_postake':
     simple_tests,
-    'test_postake_catchup': ['coda-restart-node-test'],
-    'test_postake_bootstrap':
-    ['coda-bootstrap-test'],
-    'test_postake_three_producers': ['coda-txns-and-restart-non-producers'],
-    'test_postake_delegation': ['coda-delegation-test'],
-    'test_postake_txns': ['coda-shared-state-test', 'coda-batch-payment-test'],
+    # 'test_postake_catchup': ['coda-restart-node-test'],
+    # 'test_postake_bootstrap': ['coda-bootstrap-test'],
+    # 'test_postake_three_producers': ['coda-txns-and-restart-non-producers'],
+    # 'test_postake_delegation': ['coda-delegation-test'],
+    # 'test_postake_txns': ['coda-shared-state-test', 'coda-batch-payment-test'],
     'test_postake_five_even_txns':
     ['coda-shared-prefix-multiproducer-test -num-block-producers 5 -payments'],
 }
@@ -401,6 +424,7 @@ def run(args):
     all_tests = small_curves_tests
     all_tests.update(medium_curves_and_other_tests)
     all_tests.update(archive_processor_test)
+    all_tests.update({'dev': compile_config_agnostic_tests})
     all_tests = filter_tests(all_tests, args.includes_patterns, args.excludes_patterns)
     if len(all_tests) == 0:
         if args.includes_patterns != ['*']:
@@ -429,7 +453,8 @@ def run(args):
         build_log = os.path.join(out_dir.build_logs, build_log_name)
         executive.reserve_file(build_log)
         executive.register_artifact_collector(SingleArtifactCollector(out_dir.build_logs, 'build', build_log_name))
-        project.build(build_log, profile)
+        if not args.no_build:
+            project.build(build_log, profile)
 
         for test in all_tests[profile]:
             print('  - %s' % test)
@@ -483,7 +508,9 @@ def render(args):
         unit_test_profiles_medium_curves=unit_test_profiles_medium_curves,
         small_curves_tests=tests,
         medium_curves_and_other_tests=medium_curves_and_other_tests,
-        medium_curve_profiles=medium_curve_profiles_full)
+        medium_curve_profiles=medium_curve_profiles_full,
+        compile_config_agnostic_profiles=compile_config_agnostic_profiles,
+        compile_config_agnostic_tests=compile_config_agnostic_tests)
 
     if args.check:
         with open(output_file, 'r') as file:
@@ -590,6 +617,11 @@ def main():
             Specify a pattern of tests to exclude from running. This flag can be
             provided multiple times to specify a series of patterns'
         '''
+    )
+    run_parser.add_argument(
+        '--no-build',
+        action='store_true',
+        help='Run tests using an existing built binary.'
     )
     run_parser.add_argument(
         'includes_patterns',
