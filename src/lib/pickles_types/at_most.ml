034e276ea0bb8d193de4cfa4951e5c2f1bd66c67
Izaak Meckler <ihmeckler@gmail.com>
2020-06-29 22:48:39 -0700

    reformat

--- src/lib/pickles_types/at_most.ml
+++ src/lib/pickles_types/at_most.ml
@@ -1,25 +1,24 @@
 type ('a, _) t =
   | [] : ('a, 'n) t
-  | (::) : 'a * ('a, 'n) t -> ('a, 'n Nat.s) t
+  | ( :: ) : 'a * ('a, 'n) t -> ('a, 'n Nat.s) t
 
-let rec to_list : type a n. (a, n) t -> a list =
-  function
-  | [] -> []
-  | x :: xs -> x :: to_list xs
-
-let rec to_vector : type a n. (a, n) t -> a Vector.e =
-  function
-  | [] -> T []
+let rec to_list : type a n. (a, n) t -> a list = function
+  | [] ->
+      []
   | x :: xs ->
-    let T xs = to_vector xs in
-    T (x :: xs)
+      x :: to_list xs
 
-let rec of_vector : type a n m. (a, n) Vector.t -> (n, m) Nat.Lte.t -> (a, m) t =
-  fun v p ->
-  match v, p with
-  | [], _ -> []
-  | x :: xs, S p ->
-    x :: of_vector xs p
+let rec to_vector : type a n. (a, n) t -> a Vector.e = function
+  | [] ->
+      T []
+  | x :: xs ->
+      let (T xs) = to_vector xs in
+      T (x :: xs)
+
+let rec of_vector : type a n m. (a, n) Vector.t -> (n, m) Nat.Lte.t -> (a, m) t
+    =
+ fun v p ->
+  match (v, p) with [], _ -> [] | x :: xs, S p -> x :: of_vector xs p
 
 let rec of_list_and_length_exn : type a n. a list -> n Nat.t -> (a, n) t =
  fun xs n ->
@@ -33,8 +32,8 @@ let rec of_list_and_length_exn : type a n. a list -> n Nat.t -> (a, n) t =
 
 open Core_kernel
 
-module Yojson (N : Nat.Intf) : Vector.Yojson_intf1 with type 'a t := ('a, N.n) t =
-struct
+module Yojson (N : Nat.Intf) :
+  Vector.Yojson_intf1 with type 'a t := ('a, N.n) t = struct
   let to_yojson f t = Vector.L.to_yojson f (to_list t)
 
   let of_yojson f s =
@@ -42,19 +41,27 @@ struct
 end
 
 (* TODO: Change to check length before reading the whole list. *)
-module Binable (N : Nat.Intf) : Binable.S1 with type 'a t := ('a, N.n) t = 
-  Binable.Of_binable1(List)(struct
-  type nonrec 'a t = ('a, N.n) t
-  let to_binable = to_list
-  let of_binable xs = of_list_and_length_exn xs N.n
-  end)
+module Binable (N : Nat.Intf) : Binable.S1 with type 'a t := ('a, N.n) t =
+  Binable.Of_binable1
+    (List)
+    (struct
+      type nonrec 'a t = ('a, N.n) t
 
-module Sexpable (N : Nat.Intf) : Sexpable.S1 with type 'a t := ('a, N.n) t = 
-  Sexpable.Of_sexpable1(List)(struct
-  type nonrec 'a t = ('a, N.n) t
-  let to_sexpable = to_list
-  let of_sexpable xs = of_list_and_length_exn xs N.n
-end)
+      let to_binable = to_list
+
+      let of_binable xs = of_list_and_length_exn xs N.n
+    end)
+
+module Sexpable (N : Nat.Intf) : Sexpable.S1 with type 'a t := ('a, N.n) t =
+  Sexpable.Of_sexpable1
+    (List)
+    (struct
+      type nonrec 'a t = ('a, N.n) t
+
+      let to_sexpable = to_list
+
+      let of_sexpable xs = of_list_and_length_exn xs N.n
+    end)
 
 module With_length (N : Nat.Intf) = struct
   type nonrec 'a t = ('a, N.n) t
