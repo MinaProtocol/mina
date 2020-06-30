caf663c8fb0b723c2e9b3c14655aa6c45b4a25b1
Izaak Meckler <ihmeckler@gmail.com>
2020-06-29 22:19:58 -0700

    marli checks updates

--- src/lib/pickles/marlin_checks/domain.ml
+++ src/lib/pickles/marlin_checks/domain.ml
@@ -4,13 +4,14 @@ open Core_kernel
 module Stable = struct
   module V1 = struct
     type t = Pow_2_roots_of_unity of int
-    [@@deriving version, eq, bin_io, sexp]
+    [@@deriving version, eq, bin_io, sexp, compare]
 
     let to_latest = Fn.id
   end
 end]
 
-type t = Stable.Latest.t = Pow_2_roots_of_unity of int [@@deriving eq, sexp]
+type t = Stable.Latest.t = Pow_2_roots_of_unity of int
+[@@deriving eq, sexp, compare]
 
 let log2_size (Pow_2_roots_of_unity k) = k
 
