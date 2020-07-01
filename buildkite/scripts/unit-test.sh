028d6b36a91cf2532baef0e94051d2ae459803a6
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-07-01 10:27:36 -0400

    display cpu freq (mhz) in unit-test script

--- buildkite/scripts/unit-test.sh
+++ buildkite/scripts/unit-test.sh
@@ -9,8 +9,13 @@ fi
 profile=$1
 path=$2
 
+echo "--- CPU frequency info"
+lscpu | grep -i hz
+
 source ~/.profile
+
 echo "--- Make build"
 make build 2>&1 | tee /tmp/make-build.log
+
 echo "--- Run unit tests"
 dune runtest "${path}" --profile="${profile}" -j8 || (./scripts/link-coredumps.sh && false) 2>&1 | tee /tmp/unit-test.log
