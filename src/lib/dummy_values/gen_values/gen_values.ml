67adc03ed4390a3052f53dc6cfff32a6f156b7c7
Izaak Meckler <ihmeckler@gmail.com>
2020-07-02 14:33:38 -0700

    txn snark dummy proof

--- src/lib/dummy_values/gen_values/gen_values.ml
+++ src/lib/dummy_values/gen_values/gen_values.ml
@@ -4,30 +4,39 @@ open Parsetree
 open Longident
 open Core
 open Async
+open Pickles_types
 
-let proof_string =
-  let open Pickles_types in
+let proof_string prev_width =
   let module Proof = Pickles.Proof.Make (Nat.N2) (Nat.N2) in
-  let dummy = Pickles.Proof.dummy Nat.N2.n Nat.N2.n Nat.N2.n in
+  let dummy = Pickles.Proof.dummy Nat.N2.n Nat.N2.n prev_width in
   Binable.to_string (module Proof) dummy
 
-let proof ~loc =
+let blockchain_proof_string = proof_string Nat.N2.n
+
+let transaction_proof_string = proof_string Nat.N0.n
+
+let str ~loc =
   let module E = Ppxlib.Ast_builder.Make (struct
     let loc = loc
   end) in
   let open E in
-  [%stri
-    let proof =
+  [%str
+    let blockchain_proof, transaction_proof =
       let open Pickles_types in
       let module Proof = Pickles.Proof.Make (Nat.N2) (Nat.N2) in
-      Core_kernel.Binable.of_string (module Proof) [%e estring proof_string]]
+      ( Core_kernel.Binable.of_string
+          (module Proof)
+          [%e estring blockchain_proof_string]
+      , Core_kernel.Binable.of_string
+          (module Proof)
+          [%e estring transaction_proof_string] )]
 
 let main () =
   let fmt =
     Format.formatter_of_out_channel (Out_channel.create "dummy_values.ml")
   in
   let loc = Ppxlib.Location.none in
-  Pprintast.top_phrase fmt (Ptop_def [proof ~loc]) ;
+  Pprintast.top_phrase fmt (Ptop_def (str ~loc)) ;
   exit 0
 
 let () =
