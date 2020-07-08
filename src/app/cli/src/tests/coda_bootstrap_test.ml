1e4e875e1ad180cc227e20a0183bcfcfe790d67b
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-08 23:28:39 +0100

    Inline config in bootstrap test

--- src/app/cli/src/tests/coda_bootstrap_test.ml
+++ src/app/cli/src/tests/coda_bootstrap_test.ml
@@ -3,9 +3,38 @@ open Async
 
 let name = "coda-bootstrap-test"
 
+let runtime_config =
+  lazy
+    ( (* test_postake_bootstrap *)
+      {json|
+  { "daemon":
+      { "txpool_max_size": 3000 }
+  , "genesis":
+      { "k": 6
+      , "delta": 3
+      , "genesis_state_timestamp": "2019-01-30 12:00:00-08:00" }
+  , "proof":
+      { "level": "none"
+      , "c": 8
+      , "ledger_depth": 6
+      , "work_delay": 2
+      , "block_window_duration_ms": 1500
+      , "transaction_capacity": {"2_to_the": 3}
+      , "coinbase_amount": "20"
+      , "account_creation_fee": "1" }
+  , "ledger": { "name": "test", "add_genesis_winner": false } }
+      |json}
+    |> Yojson.Safe.from_string |> Runtime_config.of_yojson
+    |> Result.ok_or_failwith )
+
 let main () =
   let logger = Logger.create () in
-  let precomputed_values = Lazy.force Precomputed_values.compiled in
+  let%bind precomputed_values, _runtime_config =
+    Genesis_ledger_helper.init_from_config_file ~logger ~may_generate:false
+      ~proof_level:None
+      (Lazy.force runtime_config)
+    >>| Or_error.ok_exn
+  in
   let n = 2 in
   let block_production_keys i = Some i in
   let snark_work_public_keys i =
