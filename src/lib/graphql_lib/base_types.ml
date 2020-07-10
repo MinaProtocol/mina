cac2917dde632afa7889bbc9946f87c3590429be
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-10 18:42:04 +0100

    Unify capitalisation of TokenId

--- src/lib/graphql_lib/base_types.ml
+++ src/lib/graphql_lib/base_types.ml
@@ -25,5 +25,5 @@ let uint64 () =
   unsigned_scalar_scalar ~to_string:Unsigned.UInt64.to_string "UInt64"
 
 let token_id () =
-  scalar "TokenID" ~doc:"String representation of a token's UInt64 identifier"
+  scalar "TokenId" ~doc:"String representation of a token's UInt64 identifier"
     ~coerce:(fun tid -> `String (Coda_base.Token_id.to_string tid))
