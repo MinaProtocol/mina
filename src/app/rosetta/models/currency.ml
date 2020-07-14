dfe71891dfee25828288a710a270b47d0ba8bb6e
Brandon Kase <bkase@o1labs.org>
2020-07-13 22:01:10 -0400

    Regens rosetta models (#5362)
    Regens models to reflect 1.4.0 and updates code to support it. Also tweaks README to be more honest.
    
    Review commit by commit to ignore generated garbage. Note: `enums.ml` was manually added, so please review that file.
    
    Tested via the `./network_status.sh` endpoint to ensure SQL query is typesafe.

--- src/app/rosetta/models/currency.ml
+++ src/app/rosetta/models/currency.ml
@@ -9,9 +9,9 @@
 type t =
   { (* Canonical symbol associated with a currency. *)
     symbol: string
-  ; (* Number of decimal places in the standard unit representation of the amount.  For example, BTC has 8 decimals. Note that it is not possible to represent the value of some currency in atomic units that is not base 10. *)
+  ; (* Number of decimal places in the standard unit representation of the amount. For example, BTC has 8 decimals. Note that it is not possible to represent the value of some currency in atomic units that is not base 10. *)
     decimals: int32
-  ; (* Any additional information related to the currency itself.  For example, it would be useful to populate this object with the contract address of an ERC-20 token. *)
+  ; (* Any additional information related to the currency itself. For example, it would be useful to populate this object with the contract address of an ERC-20 token. *)
     metadata: Yojson.Safe.t option [@default None] }
 [@@deriving yojson {strict= false}, show]
 
--- src/app/rosetta/models/transaction.ml
+++ src/app/rosetta/models/transaction.ml
@@ -9,7 +9,7 @@
 type t =
   { transaction_identifier: Transaction_identifier.t
   ; operations: Operation.t list
-  ; (* Transactions that are related to other transactions (like a cross-shard transactioin) should include the tranaction_identifier of these transactions in the metadata. *)
+  ; (* Transactions that are related to other transactions (like a cross-shard transaction) should include the tranaction_identifier of these transactions in the metadata. *)
     metadata: Yojson.Safe.t option [@default None] }
 [@@deriving yojson {strict= false}, show]
 
