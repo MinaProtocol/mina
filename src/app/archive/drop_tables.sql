bbc2030d41febd7ddca92299c5c5de0a0663e694
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-14 03:43:07 +0100

    Make drop_tables.sql match create_schema.sql

--- src/app/archive/drop_tables.sql
+++ src/app/archive/drop_tables.sql
@@ -1,13 +1,27 @@
-drop table blocks_transactions;
+drop table blocks_internal_commands;
 
-drop table fee_transfers;
+drop table blocks_user_commands;
 
-drop table blocks_snark_jobs;
+drop index idx_blocks_height;
+
+drop index idx_blocks_creator_id;
+
+drop index idx_blocks_state_hash;
 
 drop table blocks;
 
-drop table snark_jobs;
+drop table internal_commands;
 
-drop table receipt_chain_hashes;
+drop type internal_command_type;
 
 drop table user_commands;
+
+drop table user_command_type;
+
+drop index idx_snarked_ledger_hashes_value;
+
+drop table snarked_ledger_hashes;
+
+drop index idx_public_keys_value;
+
+drop table public_keys;
