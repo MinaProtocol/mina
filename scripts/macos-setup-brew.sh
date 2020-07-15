32a540640a4c415ea023a2b461b3018be119d633
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-15 02:12:43 +0100

    Use a Brewfile for Mac CI setup

--- scripts/macos-setup-brew.sh
+++ scripts/macos-setup-brew.sh
@@ -6,36 +6,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 # re-reads /usr/local/Homebrew if a cache has been put in place
 brew update-reset
 
-NEEDED_PACKAGES=" bash boost cmake gmp gpatch jemalloc libffi libomp libsodium opam openssl@1.1 pkg-config zlib libpq postgresql"
-echo "Needed:  ${NEEDED_PACKAGES}"
-
-CURRENT_PACKAGES=$(brew list | xargs)
-echo "Current: ${CURRENT_PACKAGES}"
-
-# Prune already installed packages from the todo list
-for p in $CURRENT_PACKAGES; do
-  NEEDED_PACKAGES=${NEEDED_PACKAGES// $p / }
-done;
-
-echo "Todo:    ${NEEDED_PACKAGES}"
-
-# Remove old python (uses force to always return true)
-# https://discourse.brew.sh/t/python-2-eol-2020/4647
-brew uninstall --force python@2
-
-# Remove old openssl.
-brew uninstall openssl
-
-# only run if there's work to do
-if [[ $NEEDED_PACKAGES = *[![:space:]]* ]]; then
-  yes | brew install $NEEDED_PACKAGES
-  brew update
-
-  # Upgrade to newer python3, using openssl1.1
-  brew upgrade python3
-else
-  echo 'All required brew packages have already been installed.'
-fi
+brew bundle install --file=scripts/Brewfile
 
 echo 'export PKG_CONFIG_PATH=$(brew --prefix openssl)/lib/pkgconfig' >> /Users/distiller/.bash_profile
 echo 'export PATH="$(brew --prefix openssl)/bin:$PATH"' >> /Users/distiller/.bash_profile
