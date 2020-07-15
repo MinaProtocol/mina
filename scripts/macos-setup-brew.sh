ec691095cab206e4e4746a40de9f24001a16cb84
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-15 02:39:05 +0100

    Uninstall python2, add yarn to dependencies

--- scripts/macos-setup-brew.sh
+++ scripts/macos-setup-brew.sh
@@ -8,5 +8,7 @@ brew update-reset
 
 brew bundle install --file=scripts/Brewfile
 
+brew uninstall python2 || true
+
 echo 'export PKG_CONFIG_PATH=$(brew --prefix openssl)/lib/pkgconfig' >> /Users/distiller/.bash_profile
 echo 'export PATH="$(brew --prefix openssl)/bin:$PATH"' >> /Users/distiller/.bash_profile
