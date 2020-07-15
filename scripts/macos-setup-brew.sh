8d0142fa3bea04213c107c74a073a2271a77b890
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-15 02:49:49 +0100

    Uninstall python2 first

--- scripts/macos-setup-brew.sh
+++ scripts/macos-setup-brew.sh
@@ -6,9 +6,9 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 # re-reads /usr/local/Homebrew if a cache has been put in place
 brew update-reset
 
-brew bundle install --file=scripts/Brewfile
-
 brew uninstall python2 || true
+
+brew bundle install --file=scripts/Brewfile
 
 echo 'export PKG_CONFIG_PATH=$(brew --prefix openssl)/lib/pkgconfig' >> /Users/distiller/.bash_profile
 echo 'export PATH="$(brew --prefix openssl)/bin:$PATH"' >> /Users/distiller/.bash_profile
