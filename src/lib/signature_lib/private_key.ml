1107332199de40acac858ecc6e74c2175a8db794
Paul Steckler <steck@stecksoft.com>
2020-07-20 18:14:24 -0700

    Fix another lib

--- src/lib/signature_lib/private_key.ml
+++ src/lib/signature_lib/private_key.ml
@@ -65,7 +65,7 @@ module Stable = struct
           V1.gen
       in
       let known_good_digest = "65c75a7d10b6ce193f0c0e296611a935" in
-      Ppx_version.Serialization.check_serialization
+      Ppx_version_runtime.Serialization.check_serialization
         (module V1)
         pk known_good_digest
 
