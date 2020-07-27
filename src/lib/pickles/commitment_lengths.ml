c7255e40a5c01924343bbe7c91c37a82b81cfd7e
Izaak Meckler <ihmeckler@gmail.com>
2020-07-27 15:02:42 -0700

    more generic commitment lengths function

--- src/lib/pickles/commitment_lengths.ml
+++ src/lib/pickles/commitment_lengths.ml
@@ -5,6 +5,29 @@ open Import
 (* Compute the number of group elements required to represent each commitment.
    This is simply ceil(degree of commitment / max degree natively supported by the PCS) *)
 
+let generic' ~h ~k ~sub:( - ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
+    _ Dlog_marlin_types.Evals.t =
+  let two_h = ceil_div_max_degree (of_int 2 * h#size) in
+  let three_k_minus_three =
+    ceil_div_max_degree ((of_int 3 * k#size) - of_int 3)
+  in
+  let h = ceil_div_max_degree h#size in
+  let k = ceil_div_max_degree k#size in
+  let index : _ Abc.t = {a= k; b= k; c= k} in
+  { w_hat= h
+  ; z_hat_a= h
+  ; z_hat_b= h
+  ; h_1= two_h
+  ; h_2= h
+  ; h_3= three_k_minus_three
+  ; row= index
+  ; col= index
+  ; value= index
+  ; rc= index
+  ; g_1= h
+  ; g_2= h
+  ; g_3= k }
+
 let generic map ~h ~k ~max_degree : _ Dlog_marlin_types.Evals.t =
   let index : _ Abc.t = {a= k; b= k; c= k} in
   Dlog_marlin_types.Evals.map
