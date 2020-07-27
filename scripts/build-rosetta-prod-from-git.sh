dfa897d62f9feeb9f6d83cffa34d79adf5d1dbe6
Andrew Trainor <andrew@o1labs.org>
2020-07-27 14:28:14 -0400

    rosetta/dockerfile Add scripts for building dockerfile in stages

---
+++ scripts/build-rosetta-prod-from-git.sh
@@ -0,0 +1,5 @@
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --target production \
+  -t gcr.io/o1labs-192920/coda-rosetta:latest \
+  --cache-from gcr.io/o1labs-192920/coda-rosetta-build-deps:latest \
+  --build-arg "CODA_BRANCH=rosetta/dockerfile" -
