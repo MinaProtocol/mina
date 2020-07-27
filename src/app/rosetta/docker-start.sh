029501de0e251f816bbfc49bca3742e7e2fe101d
Andrew Trainor <andrew@o1labs.org>
2020-07-27 15:39:56 -0400

    rosetta/dockerfile Update docker-start.sh with a default usable peer

--- src/app/rosetta/docker-start.sh
+++ src/app/rosetta/docker-start.sh
@@ -40,7 +40,7 @@ now_time=$(date '+%s')
 export CODA_PRIVKEY_PASS=""
 export CODA_TIMEOFFSET=$(( $now_time - $genesis_time ))
 export CODA_CONFIG_FILE=${CODA_CONFIG_FILE:=/data/config.json}
-export PEER_ID=${PEER_ID:=$PEER_ID}
+export PEER_ID=${PEER_ID:=/ip4/35.229.37.63/tcp/10001/ipfs/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr/}
 DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug -external-port 101010"
 export CODA_FLAGS=${CODA_FLAGS:=$DEFAULT_FLAGS}
 export CODA_LIBP2P_HELPER_PATH=/coda-bin/libp2p_helper
