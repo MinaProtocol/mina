aec56313dd0efa6872d0d1beaee2e25064660997
Andrew Trainor <andrew@o1labs.org>
2020-07-27 18:21:03 -0400

    rosetta/dockerfile Fix bind address for rosetta daemon

--- src/app/rosetta/lib/rosetta.ml
+++ src/app/rosetta/lib/rosetta.ml
@@ -74,7 +74,7 @@ let command =
     | Ok db ->
         let%bind server =
           Cohttp_async.Server.create ~on_handler_error:`Raise
-            (Async.Tcp.Where_to_listen.bind_to Localhost (On_port port))
+            (Async.Tcp.Where_to_listen.bind_to All_addresses (On_port port))
             (server_handler ~db ~graphql_uri ~logger)
         in
         Logger.info logger ~module_:__MODULE__ ~location:__LOC__
