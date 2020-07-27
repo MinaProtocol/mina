1051b79fae9a9c22511f2f46f92b78b03018f916
Andrew Trainor <andrew@o1labs.org>
2020-07-27 19:31:37 -0400

    rosetta/dockerfile Fix default peer and config for new testnet

--- src/app/rosetta/docker-start.sh
+++ src/app/rosetta/docker-start.sh
@@ -36,7 +36,7 @@ sleep 3
 
 export CODA_PRIVKEY_PASS=""
 export CODA_CONFIG_FILE=${CODA_CONFIG_FILE:=/data/config.json}
-export PEER_ID=${PEER_ID:=/ip4/35.229.37.63/tcp/10001/ipfs/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr/}
+export PEER_ID=${PEER_ID:=/ip4/34.74.175.158/tcp/10001/ipfs/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr/}
 DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug -external-port 10101"
 export CODA_FLAGS=${CODA_FLAGS:=$DEFAULT_FLAGS}
 export CODA_LIBP2P_HELPER_PATH=/coda-bin/libp2p_helper
