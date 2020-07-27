1c9b024a3f3257884cdee3c348500110e5df22b3
mrmr1993 <mr_1993@hotmail.co.uk>
2020-07-27 19:15:04 +0100

    Plugins behind command-line flag, enabled only for dev* profiles

--- src/lib/coda_plugins/examples/do_nothing/plugin_do_nothing.ml
+++ src/lib/coda_plugins/examples/do_nothing/plugin_do_nothing.ml
@@ -3,4 +3,5 @@ let coda = Coda_plugins.get_coda_lib ()
 let () =
   let config = Coda_lib.config coda in
   Logger.info config.logger "Hi from do-nothing plugin!" ~module_:__MODULE__
-    ~location:__LOC__
+    ~location:__LOC__ ;
+  if true then assert false
