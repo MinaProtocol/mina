e3896d7bf148ac69add6a5be2fbdeb98ab3cf6a6
Andrew Trainor <andrew@o1labs.org>
2020-07-27 15:46:05 -0400

    rosetta/dockerfile Use a real port

--- src/app/rosetta/docker-start.sh
+++ src/app/rosetta/docker-start.sh
@@ -41,7 +41,7 @@ export CODA_PRIVKEY_PASS=""
 export CODA_TIMEOFFSET=$(( $now_time - $genesis_time ))
 export CODA_CONFIG_FILE=${CODA_CONFIG_FILE:=/data/config.json}
 export PEER_ID=${PEER_ID:=/ip4/35.229.37.63/tcp/10001/ipfs/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr/}
-DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug -external-port 101010"
+DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug -external-port 10101"
 export CODA_FLAGS=${CODA_FLAGS:=$DEFAULT_FLAGS}
 export CODA_LIBP2P_HELPER_PATH=/coda-bin/libp2p_helper
 PK=${CODA_PK:=ZsMSUuKL9zLAF7sMn951oakTFRCCDw9rDfJgqJ55VMtPXaPa5vPwntQRFJzsHyeh8R8}
