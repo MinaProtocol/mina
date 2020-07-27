fdec8919f7adce267e1ca2a989259ac8e1d3c4fd
Andrew Trainor <andrew@o1labs.org>
2020-07-27 14:52:59 -0400

    rosetta/dockerfile Fix flags to match using a random testnet

--- src/app/rosetta/docker-start.sh
+++ src/app/rosetta/docker-start.sh
@@ -40,7 +40,8 @@ now_time=$(date '+%s')
 export CODA_PRIVKEY_PASS=""
 export CODA_TIMEOFFSET=$(( $now_time - $genesis_time ))
 export CODA_CONFIG_FILE=${CODA_CONFIG_FILE:=/data/config.json}
-DEFAULT_FLAGS="-seed -demo-mode -insecure-rest-server -archive-address 0.0.0.0:3086 -log-level debug -run-snark-worker $PK "
+export PEER_ID=${PEER_ID:=$PEER_ID}
+DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug"
 export CODA_FLAGS=${CODA_FLAGS:=$DEFAULT_FLAGS}
 export CODA_LIBP2P_HELPER_PATH=/coda-bin/libp2p_helper
 PK=${CODA_PK:=ZsMSUuKL9zLAF7sMn951oakTFRCCDw9rDfJgqJ55VMtPXaPa5vPwntQRFJzsHyeh8R8}
