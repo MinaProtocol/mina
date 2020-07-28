6d3b4dbc76c59dce0de2f37056efcbbe7b1576a1
Izaak Meckler <ihmeckler@gmail.com>
2020-07-28 13:52:08 -0700

    data record in types map for side loaded keys, updated tag type

--- src/lib/pickles/tag.mli
+++ src/lib/pickles/tag.mli
@@ -8,7 +8,4 @@ type kind = Side_loaded | Compiled
 type ('var, 'value, 'n1, 'n2) t = {kind: kind; id: ('var, 'value, 'n1, 'n2) tag}
 [@@deriving fields]
 
-let create ~name =
-  {kind= Compiled; id= Type_equal.Id.create ~name sexp_of_opaque}
-
-let side_loaded id = {kind= Side_loaded; id}
+val create : name:string -> ('var, 'value, 'n1, 'n2) t
