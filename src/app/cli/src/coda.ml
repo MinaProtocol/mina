9a7c99f6fb3c2fb105d36a45a09727ccfe95ddd6
Ember Arlynx <ember@lunar.town>
2020-07-29 10:15:28 -0700

    unused variable warning makes ci sad :(

--- src/app/cli/src/coda.ml
+++ src/app/cli/src/coda.ml
@@ -206,7 +206,7 @@ let daemon logger =
             generate-libp2p-keypair`) to use with libp2p discovery (default: \
             generate per-run temporary keypair)"
      and is_seed = flag "seed" ~doc:"Start the node as a seed node" no_arg
-     and enable_flooding =
+     and _enable_flooding =
        flag "enable-flooding" ~doc:"true|false Deprecated and unused" no_arg
      and libp2p_peers_raw =
        flag "peer"
