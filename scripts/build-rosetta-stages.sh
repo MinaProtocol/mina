c9b9243b0bb993562d4bc3277f0b0ffeb650ca03
Andrew Trainor <andrew@o1labs.org>
2020-07-31 16:31:53 -0400

    rosetta/dockerfile Update build scripts so they use the branch you are running as opposed to rosetta/dockerfile

--- scripts/build-rosetta-stages.sh
+++ scripts/build-rosetta-stages.sh
@@ -1,19 +1,19 @@
+GITBRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 time cat dockerfiles/Dockerfile-rosetta | docker build \
   --target build-deps \
-  -t gcr.io/o1labs-192920/coda-rosetta-build-deps:latest \
-  --build-arg "CODA_BRANCH=rosetta/dockerfile" -
+  -t gcr.io/o1labs-192920/coda-rosetta-build-deps:latest -
 time cat dockerfiles/Dockerfile-rosetta | docker build \
   --target opam-deps \
    -t gcr.io/o1labs-192920/coda-rosetta-opam-deps:latest \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-build-deps:latest \
-  --build-arg "CODA_BRANCH=rosetta/dockerfile" -
+  --build-arg "CODA_BRANCH=${GITBRANCH}" -
 time cat dockerfiles/Dockerfile-rosetta | docker build \
   --target builder \
   -t gcr.io/o1labs-192920/coda-rosetta-builder:latest \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:latest \
-  --build-arg "CODA_BRANCH=rosetta/dockerfile" -
+  --build-arg "CODA_BRANCH=${GITBRANCH}" -
 time cat dockerfiles/Dockerfile-rosetta | docker build \
   --target production \
   -t gcr.io/o1labs-192920/coda-rosetta:latest \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-builder:latest \
-  --build-arg "CODA_BRANCH=rosetta/dockerfile" -
+  --build-arg "CODA_BRANCH=${GITBRANCH}" -
