fc9e106588d4fc53afccf4dc2c13361935b3b5e2
Brandon Kase <bkase@o1labs.org>
2020-08-05 17:32:52 -0700

    Parameterized over custom PK

--- src/app/rosetta/docker-test-start.sh
+++ src/app/rosetta/docker-test-start.sh
@@ -37,7 +37,7 @@ sleep 3
 sleep 3
 
 # Setup and run demo-node
-PK=B62qrPN5Y5yq8kGE3FbVKbGTdTAJNdtNtB5sNVpxyRwWGcDEhpMzc8g
+PK=${PK:-B62qrPN5Y5yq8kGE3FbVKbGTdTAJNdtNtB5sNVpxyRwWGcDEhpMzc8g}
 genesis_time=$(date -d '2019-01-30 20:00:00.000000Z' '+%s')
 now_time=$(date +%s)
 export CODA_TIME_OFFSET=$(( $now_time - $genesis_time ))
