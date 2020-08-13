9804d2495440935d280087b420f0b65cd340fce9
Andrew Trainor <andrew@o1labs.org>
2020-08-13 01:42:24 +0000

    run-docker-build-from-artifacts Quote version and service just in case

--- buildkite/scripts/docker-artifact.sh
+++ buildkite/scripts/docker-artifact.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 source DOCKER_DEPLOY_ENV
 
 echo "--- Build/Release docker artifact"
-scripts/release-docker.sh --service $CODA_SERVICE --version $CODA_VERSION \
+scripts/release-docker.sh --service "${CODA_SERVICE}" --version "${CODA_VERSION}" \
 --extra-args "--build-arg coda_version=${CODA_DEB_VERSION} --build-arg deb_repo=${CODA_DEB_REPO}"
 
