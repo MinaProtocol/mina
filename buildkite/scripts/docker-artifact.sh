543cb2a4fcfa58e9b854d9ff068e19d83ff20217
Andrew Trainor <andrew@o1labs.org>
2020-08-13 04:50:38 +0000

    rosetta/test-build-rosetta-dockerfile-in-ci Dont escape buildkite_branch in rebuild-deb

--- buildkite/scripts/docker-artifact.sh
+++ buildkite/scripts/docker-artifact.sh
@@ -8,7 +8,7 @@ echo "--- Build/Release docker artifact for coda-daemon"
 scripts/release-docker.sh --service "${CODA_SERVICE}" --version "${CODA_VERSION}"\
  --extra-args "--build-arg coda_version=${CODA_DEB_VERSION} --build-arg deb_repo=${CODA_DEB_REPO}"
 
-if [[ $CODA_BUILD_ROSETTA ]]; then
+if [[ -n $CODA_BUILD_ROSETTA ]]; then
   echo "--- Build/Release coda-rosetta to docker hub"
   scripts/release-docker.sh --service "coda-rosetta" --version "${CODA_VERSION}"\
     --extra-args "--build-arg DUNE_PROFILE=dev --build-arg CODA_BRANCH=${CODA_GIT_BRANCH}"
