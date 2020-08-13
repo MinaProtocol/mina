292e0fd03c7f20ec4a38b7901e5c4ceda646515c
Andrew Trainor <andrew@o1labs.org>
2020-08-13 01:35:25 +0000

    run-docker-build-from-artifacts Simplify extra-args

--- buildkite/scripts/docker-artifact.sh
+++ buildkite/scripts/docker-artifact.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 source DOCKER_DEPLOY_ENV
 
-extra_args="--build-arg coda_version=${CODA_DEB_VERSION} --build-arg deb_repo=${CODA_DEB_REPO}"
-
 echo "--- Build/Release docker artifact"
-scripts/release-docker.sh --service $CODA_SERVICE --version $CODA_VERSION --extra-args "${extra_args}"
+scripts/release-docker.sh --service $CODA_SERVICE --version $CODA_VERSION \
+--extra-args "--build-arg coda_version=${CODA_DEB_VERSION} --build-arg deb_repo=${CODA_DEB_REPO}"
+
--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -4,7 +4,8 @@
 # we have to trustlist and configure image builds individually because each one is going to be slightly different. 
 # This is needed as opposed to trusting the structure of the each project to be consistent for every deployable. 
 
-set -e+x
+set -e
+set +x
 
 CLEAR='\033[0m'
 RED='\033[0;31m'
