de5561c36df5f082a36fbe0cb56e32fc08ddd290
Paul Steckler <steck@stecksoft.com>
2020-08-14 10:32:46 -0700

    Fix linter warnings in Dyn_array

--- src/lib/dyn_array/dyn_array.ml
+++ src/lib/dyn_array/dyn_array.ml
@@ -1,16 +1,31 @@
-open Core
-include DynArray
+open Core_kernel
 
 let sexp_of_t sexp_of_a t = [%sexp_of: a list] (DynArray.to_list t)
 
 let t_of_sexp a_of_sexp ls = DynArray.of_list ([%of_sexp: a list] ls)
 
-include Binable.Of_binable1
-          (Array)
-          (struct
-            type 'a t = 'a DynArray.t
+module Arr = struct
+  [%%versioned
+  module Stable = struct
+    module V1 = struct
+      type 'a t = 'a array
+    end
+  end]
+end
 
-            let to_binable = DynArray.to_array
+[%%versioned_binable
+module Stable = struct
+  module V1 = struct
+    type 'a t = 'a DynArray.t
 
-            let of_binable = DynArray.of_array
-          end)
+    include Binable.Of_binable1
+              (Arr.Stable.V1)
+              (struct
+                type 'a t = 'a DynArray.t
+
+                let to_binable = DynArray.to_array
+
+                let of_binable = DynArray.of_array
+              end)
+  end
+end]
