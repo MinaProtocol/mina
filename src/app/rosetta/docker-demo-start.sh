f95f1bdab382111b31ba38cb9a60d567c7305a43
Andrew Trainor <andrew@o1labs.org>
2020-08-14 19:26:39 +0000

    rosetta/add-docker-demo-start Add docker-demo-start.sh script for running in demo mode without the test agent and with less-verbose logging

---
+++ src/app/rosetta/docker-demo-start.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -eou pipefail
+
+function cleanup
+{
+  CODE=${1:-0}
+  echo "Killing archive.exe"
+  kill $(ps aux | egrep '_build/default/src/app/.*archive.exe' | grep -v grep | awk '{ print $2 }') || true
+  echo "Killing coda.exe"
+  kill $(ps aux | egrep '_build/default/src/app/.*coda.exe'    | grep -v grep | awk '{ print $2 }') || true
+  echo "Killing rosetta.exe"
+  kill $(ps aux | egrep '_build/default/src/app/rosetta'       | grep -v grep | awk '{ print $2 }') || true
+  exit $CODE
+}
+
+trap cleanup TERM
+trap cleanup INT
+
+PG_CONN=postgres://$USER:$USER@localhost:5432/archiver
+
+# Start postgres
+echo "========================= STARTING POSTGRESQL ==========================="
+pg_ctlcluster 11 main start
+
+# wait for it to settle
+sleep 2
+
+# archive
+echo "========================= STARTING ARCHIVE PROCESS ==========================="
+/coda-bin/archive/archive.exe run \
+  -postgres-uri $PG_CONN \
+  -server-port 3086 \
+  -log-level fatal &
+
+# wait for it to settle
+sleep 2
+
+# Setup and run demo-node
+PK=${PK:-B62qrPN5Y5yq8kGE3FbVKbGTdTAJNdtNtB5sNVpxyRwWGcDEhpMzc8g}
+genesis_time=$(date -d '2019-01-30 20:00:00.000000Z' '+%s')
+now_time=$(date +%s)
+export CODA_TIME_OFFSET=$(( $now_time - $genesis_time ))
+export CODA_PRIVKEY_PASS=""
+export CODA_LIBP2P_HELPER_PATH=/coda-bin/libp2p_helper
+CODA_CONFIG_DIR=/root/.coda-config
+
+# CODA_CONFIG_DIR is exposed by the dockerfile and contains demo mode essentials
+echo "========================= STARTING DAEMON ==========================="
+/coda-bin/cli/src/coda.exe daemon \
+  -seed \
+  -demo-mode \
+  -block-producer-key "$CODA_CONFIG_DIR/wallets/store/$PK" \
+  -run-snark-worker $PK \
+  -config-file "$CODA_CONFIG_DIR/daemon.json" \
+  -config-dir "$CODA_CONFIG_DIR" \
+  -insecure-rest-server \
+  -external-ip 127.0.0.1 \
+  -archive-address 3086 \
+  -log-level fatal &
+
+# wait for it to settle
+sleep 15
+
+# rosetta
+echo "========================= STARTING ROSETTA API on PORT 3087 ==========================="
+/coda-bin/rosetta/rosetta.exe \
+  -archive-uri $PG_CONN \
+  -graphql-uri http://localhost:3085/graphql \
+  -log-level fatal \
+  -port 3087 &
+
+sleep infinity
