acb984ce601799466c7376bc477b42da623b8f39
Helena Li <helena.li0x1@gmail.com>
2020-08-19 15:55:55 -0700

    finished writing the tests

--- src/lib/integration_test_lib/error_monad.ml
+++ src/lib/integration_test_lib/error_monad.ml
@@ -1,26 +1,29 @@
-(* type 'a result =
-  | Success of string option * 'a
-  | HardError of string
-  | SoftError of string * 'a
-
-type ('a, 'b) err_accumulator =
-  {log: string list; res: 'a result; already_hit_hard: bool} *)
-
 open Core_kernel
-
-(* open Error *)
 open Async_kernel
 
-(* open Result *)
-(* open Base *)
-(* open Stdio *)
-
 module Hard_fail = struct
-  type t = {hard_error: Error.t; soft_errors: Error.t list}
+  type t =
+    { hard_error: Error.t
+          [@equal
+            fun a b ->
+              String.equal (Error.to_string_hum a) (Error.to_string_hum b)]
+    ; soft_errors: Error.t list
+          [@equal
+            List.equal (fun a b ->
+                String.equal (Error.to_string_hum a) (Error.to_string_hum b) )]
+    }
+  [@@deriving eq, sexp_of, compare]
 end
 
 module Accumulator = struct
-  type 'a t = {computation_result: 'a; soft_errors: Error.t list}
+  type 'a t =
+    { computation_result: 'a
+    ; soft_errors: Error.t list
+          [@equal
+            List.equal (fun a b ->
+                String.equal (Error.to_string_hum a) (Error.to_string_hum b) )]
+    }
+  [@@deriving eq, sexp_of, compare]
 end
 
 module ErrorReporting = Monad.Make (struct
@@ -41,10 +44,12 @@ module ErrorReporting = Monad.Make (struct
                  { Accumulator.computation_result= comp
                  ; Accumulator.soft_errors= List.append err_list next_list })
         | Error
-            {Hard_fail.hard_error= hard_err; Hard_fail.soft_errors= err_list}
+            {Hard_fail.hard_error= hard_err; Hard_fail.soft_errors= next_list}
           ->
             Deferred.return
-              (Error {Hard_fail.hard_error= hard_err; soft_errors= err_list}) )
+              (Error
+                 { Hard_fail.hard_error= hard_err
+                 ; soft_errors= List.append err_list next_list }) )
     | Error {Hard_fail.hard_error= hard_err; Hard_fail.soft_errors= err_list}
       ->
         Deferred.return
@@ -54,75 +59,109 @@ module ErrorReporting = Monad.Make (struct
   let map = `Define_using_bind
 end)
 
-let%test_unit "error_monad_unit_test" =
-  let open ErrorReporting.Let_syntax in
-  let test1 =
-    let f nm =
-      let%bind n = nm in
-      Deferred.return
-        (Ok {Accumulator.computation_result= n + 1; soft_errors= []})
-    in
-    f (f (f (f (f (return 0)))))
-  in
-  assert (
-    test1
-    = Deferred.return (Ok {Accumulator.computation_result= 5; soft_errors= []})
+let%test_unit "error_monad_unit_test_1" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test1 =
+        let open ErrorReporting.Let_syntax in
+        let f nm =
+          let%bind n = nm in
+          Deferred.return
+            (Ok {Accumulator.computation_result= n + 1; soft_errors= []})
+        in
+        f (f (f (f (f (return 0)))))
+      in
+      [%test_eq: (int Accumulator.t, Hard_fail.t) Result.t] test1
+        (Ok {Accumulator.computation_result= 5; soft_errors= []}) )
+
+let%test_unit "error_monad_unit_test_2" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test2 =
+        let open ErrorReporting.Let_syntax in
+        let%bind () =
+          Deferred.return
+            (Ok {Accumulator.computation_result= (); soft_errors= []})
+        in
+        Deferred.return
+          (Ok {Accumulator.computation_result= "123"; soft_errors= []})
+      in
+      [%test_eq: (string Accumulator.t, Hard_fail.t) Result.t] test2
+        (Ok {Accumulator.computation_result= "123"; soft_errors= []}) )
+
+let%test_unit "error_monad_unit_test_3" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test3 =
+        let open ErrorReporting.Let_syntax in
+        let%bind () =
+          Deferred.return
+            (Ok
+               { Accumulator.computation_result= ()
+               ; soft_errors= [Error.of_string "a"] })
+        in
+        Deferred.return
+          (Ok
+             { Accumulator.computation_result= "123"
+             ; soft_errors= [Error.of_string "b"] })
+      in
+      [%test_eq: (string Accumulator.t, Hard_fail.t) Result.t] test3
+        (Ok
+           { Accumulator.computation_result= "123"
+           ; soft_errors= List.map ["a"; "b"] ~f:Error.of_string }) )
+
+let%test_unit "error_monad_unit_test_4" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test4 =
+        let open ErrorReporting.Let_syntax in
+        let%bind () =
+          Deferred.return
+            (Ok {Accumulator.computation_result= (); soft_errors= []})
+        in
+        Deferred.return
+          (Error {Hard_fail.hard_error= Error.of_string "xyz"; soft_errors= []})
+      in
+      [%test_eq: (string Accumulator.t, Hard_fail.t) Result.t] test4
+        (Error {Hard_fail.hard_error= Error.of_string "xyz"; soft_errors= []})
   )
 
-(* 
-let test2 =
-  let%bind () = Ok {result= (); soft_errors= []} in
-  Ok {result= "123"; soft_errors= []}
+let%test_unit "error_monad_unit_test_5" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test5 =
+        let open ErrorReporting.Let_syntax in
+        let%bind () =
+          Deferred.return
+            (Ok
+               { Accumulator.computation_result= ()
+               ; soft_errors= [Error.of_string "a"] })
+        in
+        Deferred.return
+          (Error {Hard_fail.hard_error= Error.of_string "xyz"; soft_errors= []})
+      in
+      [%test_eq: (string Accumulator.t, Hard_fail.t) Result.t] test5
+        (Error
+           { Hard_fail.hard_error= Error.of_string "xyz"
+           ; soft_errors= [Error.of_string "a"] }) )
 
-let () = assert (test2 = Ok {result= "123"; soft_errors= []})
-
-let test3 =
-  let%bind () = Ok {result= (); soft_errors= [Error.of_string "a"]} in
-  Ok {result= "123"; soft_errors= [Error.of_string "b"]}
-
-let () =
-  assert (
-    match test3 with
-    | Ok {result= "123"; soft_errors} ->
-        ["a"; "b"] = List.map soft_errors ~f:Error.to_string_hum
-    | _ ->
-        false )
-
-let test4 =
-  let%bind () = Ok {result= (); soft_errors= []} in
-  Error {hard_error= Error.of_string "xyz"; soft_errors= []}
-
-let () =
-  assert (
-    match test4 with
-    | Error {hard_error; soft_errors= []} ->
-        "xyz" = Error.to_string_hum hard_error
-    | _ ->
-        false )
-
-let test5 =
-  let%bind () = Ok {result= (); soft_errors= [Error.to_string_hum "a"]} in
-  Error {hard_error= Error.of_string "xyz"; soft_errors= []}
-
-let () =
-  assert (
-    match test5 with
-    | Error {hard_error; soft_errors= [soft_error]} ->
-        "xyz" = Error.to_string_hum hard_error
-        && "a" = Error.to_string_hum soft_error
-    | _ ->
-        false )
-
-let test6 =
-  let%bind () = Ok {result= (); soft_errors= [Error.to_string_hum "a"]} in
-  Error
-    {hard_error= Error.of_string "xyz"; soft_errors= [Error.to_string_hum "b"]}
-
-let () =
-  assert (
-    match test6 with
-    | Error {hard_error; soft_errors} ->
-        "xyz" = Error.to_string_hum hard_error
-        && ["a"; "b"] = List.map soft_errors ~f:Error.to_string_hum
-    | _ ->
-        false ) *)
+let%test_unit "error_monad_unit_test_6" =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Deferred.Let_syntax in
+      let%map test6 =
+        let open ErrorReporting.Let_syntax in
+        let%bind () =
+          Deferred.return
+            (Ok
+               { Accumulator.computation_result= ()
+               ; soft_errors= [Error.of_string "a"] })
+        in
+        Deferred.return
+          (Error
+             { Hard_fail.hard_error= Error.of_string "xyz"
+             ; soft_errors= [Error.of_string "b"] })
+      in
+      [%test_eq: (string Accumulator.t, Hard_fail.t) Result.t] test6
+        (Error
+           { Hard_fail.hard_error= Error.of_string "xyz"
+           ; soft_errors= [Error.of_string "a"; Error.of_string "b"] }) )
