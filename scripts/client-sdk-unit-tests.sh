065fb49d541eb4238555f11c479951c250382eeb
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-08-24 14:29:56 -0400

    update PATH within client sdk unit test script

--- scripts/client-sdk-unit-tests.sh
+++ scripts/client-sdk-unit-tests.sh
@@ -4,6 +4,9 @@ set -e
 
 # run client SDK tests in node
 
+export PATH=/home/opam/.cargo/bin:/usr/lib/go/bin:$PATH
+export GO=/usr/lib/go/bin/go
+
 echo "Building client SDK..."
 make client_sdk
 echo "Running tests"
