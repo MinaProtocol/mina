476b22e7e3aa684b95d8d98647fb90ba001067db
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-08-24 15:36:47 -0400

    source (opam) user profile for sdk unit test runs

--- scripts/client-sdk-unit-tests.sh
+++ scripts/client-sdk-unit-tests.sh
@@ -5,13 +5,11 @@ set -eo pipefail
 # run client SDK tests in node
 
 export PATH=/home/opam/.cargo/bin:/usr/lib/go/bin:$PATH
-export GO=/usr/lib/go/bin/go
 
 echo "Building client SDK..."
-
-opam install dune
-
+source ~/.profile
 make client_sdk
+
 echo "Running tests"
 nodejs src/app/client_sdk/tests/run_unit_tests.js
 
