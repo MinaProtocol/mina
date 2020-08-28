53e34e098146c13061c7a1620a565a2230bc403f
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-08-28 12:05:38 -0400

    test block production integration test on develop

---
+++ buildkite/scripts/run-test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+eval $(opam env)
+
+# Waiting for the coda-daemon image to be built by CircleCI (this is redundent once build-artifacts works in buildkite)
+
+git_tag=$(git describe --abbrev=0)
+git_branch=$(echo $BUILDKITE_BRANCH | sed 's!/!-!; s!_!-!g')
+# git_branch=$(git rev-parse --symbolic-full-name --abbrev-ref  HEAD  | sed 's!/!-!; s!_!-!g')
+git_hash=$(git rev-parse --short=7 HEAD)
+
+coda_daemon_image="codaprotocol/coda-daemon:${git_tag}-${git_branch}-${git_hash}"
+export TF_LOG=DEBUG
+
+echo "image: ${coda_daemon_image}"
+
+# echo "Waiting for coda-daemon image to appear in docker registry"
+# for i in $(seq 60); do
+#     docker pull "$coda_daemon_image" && break
+#     [ "$i" != 30 ] || (echo "coda-daemon image never appeared in docker registry" && exit 1)
+#     sleep 60
+# done
+
+echo "--- Building test-executive"
+
+dune build --profile=testnet_postake_medium_curves src/app/test_executive/test_executive.exe
+
+echo "--- Clone coda-automation repo (this would be changed in future. Nathan is suggesting linking coda-automation in coda repo)"
+
+git clone https://github.com/CodaProtocol/coda-automation.git
+
+echo "--- Set the credential for gcloud"
+
+echo $BUILDKITE_GS_APPLICATION_CREDENTIALS_JSON > /tmp/credential.json
+
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/credential.json"
+
+echo "--- Set default region for AWS"
+
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+echo "--- Run test executive"
+
+./_build/default/src/app/test_executive/test_executive.exe --coda-image "$coda_daemon_image" block-production
---
+++ buildkite/src/Jobs/BlockProductionTest.dhall
@@ -0,0 +1,42 @@
+let Pipeline = ../Pipeline/Dsl.dhall
+let JobSpec = ../Pipeline/JobSpec.dhall
+let Command = ../Command/Base.dhall
+let OpamInit = ../Command/Base.dhall
+let Docker = ../Command/Docker/Type.dhall
+let Size =  ../Command/Size.dhall
+let Cmd = ../Lib/Cmds.dhall
+let S  = ../Lib/SelectFiles.dhall
+let DockerArtifact = ../Command/DockerArtifact.dhall
+let OpamInit = ../Command/OpamInit.dhall
+in
+let name = "BlockProductionTest"
+let runTestExecutive =
+  Command.build
+    Command.Config::
+      { commands = OpamInit.andThenRunInDocker
+          ([ "BUILDKITE_GS_APPLICATION_CREDENTIALS_JSON"
+           , "AWS_ACCESS_KEY_ID"
+           , "AWS_SECRET_ACCESS_KEY"
+           , "AWS_REGION"
+           ]) "bash buildkite/scripts/run-test.sh"
+      , label = "Build and run test-executive"
+      , key = "run-test-executive"
+      , target = Size.Large
+      , docker = None Docker.Type
+      , depends_on = [ { name = "Artifact", key = "docker-artifact" } ]
+      }
+
+in
+
+Pipeline.build
+  Pipeline.Config::
+    { spec =
+      JobSpec::
+        { dirtyWhen =
+          [ S.strictlyStart (S.contains "buildkite/src/Jobs/BlockProductionTest")
+          , S.strictlyStart (S.contains "buildkite/scripts/run-test.sh")
+          , S.strictlyStart (S.contains "src/lib") ]
+        , name = name
+        }
+    , steps = [ runTestExecutive ]
+    }
