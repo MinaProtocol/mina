65f6880ac511a7f5fcffa2ffa9c6888c6d3b0092
Brandon Kase <bkase@o1labs.org>
2020-09-01 13:54:52 +0300

    Supports full reconciliation for pay + delegation
    * Moves fee_payer_dec to fee_transfer when in a block to be more
    faithful to protocol's understanding of operations; mempool still
    returns fee_payer_dec from user_command as an approximation
    * Fixes JOIN clause on SQL queries utilizing the join table for blocks
    and transactions to properly query for them.
    
    `rosetta-cli check:data` now works against everything our test-agent
    throws at it (which at the moment are payments and delegations):
    
    ```
    +--------------------+--------------------------------+--------+
    | Response Assertion | All responses are correctly    | PASSED |
    |                    | formatted                      |        |
    +--------------------+--------------------------------+--------+
    | Block Syncing      | Blocks are connected into a    | PASSED |
    |                    | single canonical chain         |        |
    +--------------------+--------------------------------+--------+
    | Balance Tracking   | Account balances did not go    | PASSED |
    |                    | negative                       |        |
    +--------------------+--------------------------------+--------+
    | Reconciliation     | No balance discrepencies were  | PASSED |
    |                    | found between computed and     |        |
    |                    | live balances                  |        |
    +--------------------+--------------------------------+--------+
    ```

--- src/app/rosetta/lib/block.ml
+++ src/app/rosetta/lib/block.ml
@@ -76,80 +76,54 @@ module Internal_command_info = struct
     ; hash: string }
   [@@deriving to_yojson]
 
-  module T (M : Monad_fail.S) = struct
-    module Op_build = Op.T (M)
-
-    let to_operations ~coinbase_receiver ~coinbase (t : t) :
-        (Operation.t list, Errors.t) M.t =
-      (* We choose to represent the dec-side of fee transfers from txns from the
-       * canonical user command that created them so we are able consistently
-       * produce more balance changing operations in the mempool or a block.
-       * *)
-      let plan : 'a Op.t list =
-        match t.kind with
-        | `Coinbase ->
-            (* The coinbase transaction is really incrementing by the coinbase
-           * amount  *)
-            [{Op.label= `Coinbase_inc; related_to= None}]
-        | `Fee_transfer ->
-            [{Op.label= `Fee_receiver_inc; related_to= None}]
-        | `Fee_transfer_via_coinbase ->
-            [ {Op.label= `Fee_receiver_inc; related_to= None}
-            ; {Op.label= `Fee_payer_dec; related_to= Some `Fee_receiver_inc} ]
-      in
-      Op_build.build
-        ~a_eq:[%eq: [`Coinbase_inc | `Fee_payer_dec | `Fee_receiver_inc]] ~plan
-        ~f:(fun ~related_operations ~operation_identifier op ->
-          (* All internal commands succeed if they're in blocks *)
-          let status = Operation_statuses.name `Success in
-          match op.label with
-          | `Coinbase_inc ->
-              M.return
-                { Operation.operation_identifier
-                ; related_operations
-                ; status
-                ; account=
-                    Some (account_id t.receiver Amount_of.Token_id.default)
-                ; _type= Operation_types.name `Coinbase_inc
-                ; amount= Some (Amount_of.coda coinbase)
-                ; coin_change= None
-                ; metadata= None }
-          | `Fee_receiver_inc ->
-              M.return
-                { Operation.operation_identifier
-                ; related_operations
-                ; status
-                ; account= Some (account_id t.receiver t.token)
-                ; _type= Operation_types.name `Fee_receiver_inc
-                ; amount= Some (Amount_of.token t.token t.fee)
-                ; coin_change= None
-                ; metadata= None }
-          | `Fee_payer_dec ->
-              let open M.Let_syntax in
-              let%map coinbase_receiver =
-                match coinbase_receiver with
-                | Some r ->
-                    M.return r
-                | None ->
-                    M.fail
-                      (Errors.create
-                         ~context:
-                           "This operation existing (fee payer dec within \
-                            Internal_command) demands a coinbase receiver to \
-                            exist. Please report this bug."
-                         `Invariant_violation)
-              in
-              { Operation.operation_identifier
-              ; related_operations
-              ; status
-              ; account=
-                  Some
-                    (account_id coinbase_receiver Amount_of.Token_id.default)
-              ; _type= Operation_types.name `Fee_payer_dec
-              ; amount= Some Amount_of.(negated (coda t.fee))
-              ; coin_change= None
-              ; metadata= None } )
-  end
+  let to_operations ~coinbase (t : t) : Operation.t list =
+    (* We choose to represent the dec-side of fee transfers from txns from the
+     * canonical user command that created them so we are able consistently
+     * produce more balance changing operations in the mempool or a block.
+     * *)
+    let plan : 'a Op.t list =
+      match t.kind with
+      | `Coinbase ->
+          (* The coinbase transaction is really incrementing by the coinbase
+         * amount  *)
+          [{Op.label= `Coinbase_inc; related_to= None}]
+      | `Fee_transfer ->
+          [ {Op.label= `Fee_receiver_inc; related_to= None}
+            (* we use the fee_payer_dec from here instead of the user command when we have internal commands (ie in a block) for more accuracy w.r.t. the protocol *)
+          ; {Op.label= `Fee_payer_dec; related_to= Some `Fee_receiver_inc} ]
+    in
+    Op.build ~a_eq:[%eq: [`Coinbase_inc | `Fee_payer_dec | `Fee_receiver_inc]]
+      ~plan ~f:(fun ~related_operations ~operation_identifier op ->
+        (* All internal commands succeed if they're in blocks *)
+        let status = Operation_statuses.name `Success in
+        match op.label with
+        | `Coinbase_inc ->
+            { Operation.operation_identifier
+            ; related_operations
+            ; status
+            ; account= Some (account_id t.receiver Amount_of.Token_id.default)
+            ; _type= Operation_types.name `Coinbase_inc
+            ; amount= Some (Amount_of.coda coinbase)
+            ; coin_change= None
+            ; metadata= None }
+        | `Fee_receiver_inc ->
+            { Operation.operation_identifier
+            ; related_operations
+            ; status
+            ; account= Some (account_id t.receiver t.token)
+            ; _type= Operation_types.name `Fee_receiver_inc
+            ; amount= Some (Amount_of.token t.token t.fee)
+            ; coin_change= None
+            ; metadata= None }
+        | `Fee_payer_dec ->
+            { Operation.operation_identifier
+            ; related_operations
+            ; status
+            ; account= Some (account_id t.receiver Amount_of.Token_id.default)
+            ; _type= Operation_types.name `Fee_payer_dec
+            ; amount= Some Amount_of.(negated (coda t.fee))
+            ; coin_change= None
+            ; metadata= None } )
 
   let dummies =
     [ { kind= `Coinbase
