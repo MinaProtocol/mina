45ccacfcc2a2a26c64ae027a54df8556ac188ddc
Izaak Meckler <ihmeckler@gmail.com>
2020-09-08 10:29:04 -0700

    update transaction type

--- src/lib/coda_base/transaction.ml
+++ src/lib/coda_base/transaction.ml
@@ -1,65 +1,87 @@
 open Core
 
+module Poly = struct
+  [%%versioned
+  module Stable = struct
+    module V1 = struct
+      type 'command t =
+        | Command of 'command
+        | Fee_transfer of Fee_transfer.Stable.V1.t
+        | Coinbase of Coinbase.Stable.V1.t
+      [@@deriving sexp, compare, eq, hash, yojson]
+
+      let to_latest = Fn.id
+    end
+  end]
+end
+
+module Valid = struct
+  [%%versioned
+  module Stable = struct
+    module V1 = struct
+      type t = Command_transaction.Valid.Stable.V1.t Poly.Stable.V1.t
+      [@@deriving sexp, compare, eq, hash, yojson]
+
+      let to_latest = Fn.id
+    end
+  end]
+
+  include Hashable.Make (Stable.Latest)
+  include Comparable.Make (Stable.Latest)
+end
+
 [%%versioned
 module Stable = struct
-  [@@@no_toplevel_latest_type]
-
   module V1 = struct
-    type t =
-      | User_command of User_command.With_valid_signature.Stable.V1.t
-      | Fee_transfer of Fee_transfer.Stable.V1.t
-      | Coinbase of Coinbase.Stable.V1.t
+    type t = Command_transaction.Stable.V1.t Poly.Stable.V1.t
     [@@deriving sexp, compare, eq, hash, yojson]
 
     let to_latest = Fn.id
   end
 end]
 
-module T = struct
-  module T0 = struct
-    type t = Stable.Latest.t =
-      | User_command of User_command.With_valid_signature.t
-      | Fee_transfer of Fee_transfer.t
-      | Coinbase of Coinbase.t
-    [@@deriving sexp, compare, eq, hash, yojson]
-  end
+include Hashable.Make (Stable.Latest)
+include Comparable.Make (Stable.Latest)
 
-  include Hashable.Make (T0)
-  include Comparable.Make (T0)
-  include T0
-end
+type 'command t_ = 'command Poly.t =
+  | Command of 'command
+  | Fee_transfer of Fee_transfer.t
+  | Coinbase of Coinbase.t
 
-include T
+let forget : Valid.t -> t = fun x -> (x :> t)
 
 let fee_excess : t -> Fee_excess.t Or_error.t = function
-  | User_command t ->
-      Ok (User_command.fee_excess (User_command.forget_check t))
+  | Command (User_command t) ->
+      Ok (User_command.fee_excess t)
+  | Command (Snapp_command t) ->
+      Snapp_command.(fee_excess (t :> t))
   | Fee_transfer t ->
       Fee_transfer.fee_excess t
   | Coinbase t ->
       Coinbase.fee_excess t
 
 let supply_increase = function
-  | User_command _ | Fee_transfer _ ->
+  | Command _ | Fee_transfer _ ->
       Ok Currency.Amount.zero
   | Coinbase t ->
       Coinbase.supply_increase t
 
-let accounts_accessed ~next_available_token = function
-  | User_command cmd ->
-      User_command.accounts_accessed ~next_available_token
-        (User_command.forget_check cmd)
+let accounts_accessed ~next_available_token : t -> _ = function
+  | Command (User_command cmd) ->
+      User_command.accounts_accessed ~next_available_token cmd
+  | Command (Snapp_command t) ->
+      Snapp_command.(accounts_accessed (t :> t))
   | Fee_transfer ft ->
       Fee_transfer.receivers ft
   | Coinbase cb ->
       Coinbase.accounts_accessed cb
 
-let next_available_token t next_available_token =
+let next_available_token (t : t) next_available_token =
   match t with
-  | User_command cmd ->
-      User_command.next_available_token
-        (User_command.forget_check cmd)
-        next_available_token
+  | Command (User_command cmd) ->
+      User_command.next_available_token cmd next_available_token
+  | Command (Snapp_command t) ->
+      Snapp_command.next_available_token t next_available_token
   | Fee_transfer _ ->
       next_available_token
   | Coinbase _ ->
