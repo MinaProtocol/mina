9fc04ada4f4f2b3b208fd627fad63326c40c9cb5
Andrew Trainor <andrew@o1labs.org>
2020-09-14 23:10:17 -0400

    rosetta/upgrade-rosetta-caching Fix another script typo

--- scripts/build-rosetta-stages.sh
+++ scripts/build-rosetta-stages.sh
@@ -22,7 +22,8 @@ cat dockerfiles/Dockerfile-rosetta | docker build \
   --target builder \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
   --build-arg "DUNE_PROFILE=dev" \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
+  --build-arg "CODA_BRANCH=${GITBRANCH}" \
+  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
   -t gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG -
 
 cat dockerfiles/Dockerfile-rosetta | docker build \
@@ -30,22 +31,17 @@ cat dockerfiles/Dockerfile-rosetta | docker build \
   --target production \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG \
   --build-arg "DUNE_PROFILE=dev" \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
+  --build-arg "CODA_BRANCH=${GITBRANCH}" \
   --build-arg "CODA_COMMIT=${GITCOMMIT}" \
   -t gcr.io/o1labs-192920/coda-rosetta:dev-$TAG -
 
 # Also build with the default dune profile
-time cat dockerfiles/Dockerfile-rosetta | docker build \
-  --target builder \
-  -t gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG \
-  --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
-
 cat dockerfiles/Dockerfile-rosetta | docker build \
   --force-rm \
+  --target builder \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
-  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
   --build-arg "CODA_BRANCH=${GITBRANCH}" \
+  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
   -t gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG -
 
 cat dockerfiles/Dockerfile-rosetta | docker build \
@@ -53,6 +49,7 @@ cat dockerfiles/Dockerfile-rosetta | docker build \
   --target production \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG \
   --build-arg "CODA_BRANCH=${GITBRANCH}" \
+  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
   -t gcr.io/o1labs-192920/coda-rosetta:medium-curves-$TAG -
 
 docker push gcr.io/o1labs-192920/coda-rosetta-build-deps:develop
