1f8d9577d77e90fe4401ed120703049edf4957e5
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2020-09-14 17:19:39 -0700

    adding sequence no to the sql schema

--- src/app/archive/create_schema.sql
+++ src/app/archive/create_schema.sql
@@ -66,11 +66,14 @@ CREATE INDEX idx_blocks_height     ON blocks(height);
 CREATE TABLE blocks_user_commands
 ( block_id        int NOT NULL REFERENCES blocks(id) ON DELETE CASCADE
 , user_command_id int NOT NULL REFERENCES user_commands(id) ON DELETE CASCADE
+, sequence_no     int NOT NULL
 , PRIMARY KEY (block_id, user_command_id)
 );
 
 CREATE TABLE blocks_internal_commands
-( block_id            int NOT NULL REFERENCES blocks(id) ON DELETE CASCADE
-, internal_command_id int NOT NULL REFERENCES internal_commands(id) ON DELETE CASCADE
+( block_id              int NOT NULL REFERENCES blocks(id) ON DELETE CASCADE
+, internal_command_id   int NOT NULL REFERENCES internal_commands(id) ON DELETE CASCADE
+, sequence_no           int NOT NULL
+, secondary_sequence_no int
 , PRIMARY KEY (block_id, internal_command_id)
 );
