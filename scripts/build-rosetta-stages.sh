ed4227f349efb5161830a6ca900295e9665fa2a2
Andrew Trainor <andrew@o1labs.org>
2020-09-14 22:58:16 -0400

    rosetta/upgrade-rosetta-caching Upgrade rosetta docker caching

--- scripts/build-rosetta-stages.sh
+++ scripts/build-rosetta-stages.sh
@@ -3,31 +3,36 @@
 set -eou pipefail
 
 GITBRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+GITCOMMIT=$(git log -1 --pretty=format:%h)
 TAG=$(echo ${GITBRANCH} | sed 's!/!-!; s!_!-!g')
-time cat dockerfiles/Dockerfile-rosetta | docker build \
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
   --target build-deps \
-  -t gcr.io/o1labs-192920/coda-rosetta-build-deps:$TAG -
-docker push gcr.io/o1labs-192920/coda-rosetta-build-deps:$TAG
-time cat dockerfiles/Dockerfile-rosetta | docker build \
+  -t gcr.io/o1labs-192920/coda-rosetta-build-deps:develop -
+
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
   --target opam-deps \
-   -t gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
-  --cache-from gcr.io/o1labs-192920/coda-rosetta-build-deps:$TAG \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
-docker push gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG
-time cat dockerfiles/Dockerfile-rosetta | docker build \
+  --cache-from gcr.io/o1labs-192920/coda-rosetta-build-deps:develop \
+   -t gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG -
+
+# Build with the dev profile
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
   --target builder \
-  -t gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG \
-  --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
+  --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps$TAG \
   --build-arg "DUNE_PROFILE=dev" \
   --build-arg "CODA_BRANCH=${GITBRANCH}" -
-docker push gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG
-time cat dockerfiles/Dockerfile-rosetta | docker build \
+  -t gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG -
+
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
   --target production \
-  -t gcr.io/o1labs-192920/coda-rosetta:dev-$TAG \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG \
   --build-arg "DUNE_PROFILE=dev" \
   --build-arg "CODA_BRANCH=${GITBRANCH}" -
-docker push gcr.io/o1labs-192920/coda-rosetta:dev-$TAG
+  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
+  -t gcr.io/o1labs-192920/coda-rosetta:dev-$TAG -
 
 # Also build with the default dune profile
 time cat dockerfiles/Dockerfile-rosetta | docker build \
@@ -35,10 +40,27 @@ time cat dockerfiles/Dockerfile-rosetta | docker build \
   -t gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
   --build-arg "CODA_BRANCH=${GITBRANCH}" -
-docker push gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG
-time cat dockerfiles/Dockerfile-rosetta | docker build \
+
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
+  --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:$TAG \
+  --build-arg "CODA_COMMIT=${GITCOMMIT}" \
+  --build-arg "CODA_BRANCH=${GITBRANCH}" \
+  -t gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG -
+
+cat dockerfiles/Dockerfile-rosetta | docker build \
+  --force-rm \
   --target production \
-  -t gcr.io/o1labs-192920/coda-rosetta:medium-curves-$TAG \
   --cache-from gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
-docker push gcr.io/o1labs-192920/coda-rosetta:medium-curves-$TAG
+  --build-arg "CODA_BRANCH=${GITBRANCH}" \
+  -t gcr.io/o1labs-192920/coda-rosetta:medium-curves-$TAG -
+
+#docker push gcr.io/o1labs-192920/coda-rosetta-build-deps:develop
+docker push gcr.io/o1labs-192920/coda-rosetta-opam-deps:master
+# docker push gcr.io/o1labs-192920/coda-rosetta-builder:medium-curves-$TAG
+# docker push gcr.io/o1labs-192920/coda-rosetta-builder:dev-$TAG
+
+if [[ $TAG == develop ]]; then
+  docker push gcr.io/o1labs-192920/coda-rosetta:dev-$TAG
+  docker push gcr.io/o1labs-192920/coda-rosetta:medium-curves-$TAG
+fi
