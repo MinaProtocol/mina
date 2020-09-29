9ac14e2d5f47c0d88e461846d5fbfc4f517d1046
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-09-29 13:43:49 -0400

    update incremental build EnvVar name

--- buildkite/scripts/generate-diff.sh
+++ buildkite/scripts/generate-diff.sh
@@ -11,15 +11,16 @@ COMMIT=$(diff -u <(git rev-list --first-parent HEAD) \
 
 if [[ $COMMIT != "" ]]; then
   # Get the files that have changed since that shared commit
+  echo "--- Generating diff based on shared commit: ${COMMIT}"
   git diff $COMMIT --name-only
 else
-  if [ -n "${INCREMENTAL_BUILD+x}" ]; then
+  if [ -n "${BUILDKITE_INCREMENTAL+x}" ]; then
     # base DIFF on last successful Buildkite `develop` RUN
     ci_recent_pass_commit=$(
       curl https://graphql.buildkite.com/v1 -H "Authorization: Bearer $TOKEN" -d'{"query": "query { pipeline(slug: \"o-1-labs-2/coda\") { builds(first: 1 branch: \"develop\" state: PASSED) { edges { node { commit } } } } }"}' | jq '.data.pipeline.builds.edges[0].node.commit' | tr -d '"'
     )
 
-    #echo "--- Generating diff against: ${ci_recent_pass_commit}"
+    echo "--- Generating incremental diff against: ${ci_recent_pass_commit}"
     git diff "${ci_recent_pass_commit}" --name-only
   else
     # TODO: Dump commits as artifacts when build succeeds so we can diff against
