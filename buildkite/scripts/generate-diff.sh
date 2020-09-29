7f5c7c352ac2ef7d9731d9c5bf1fb9723a98f8e8
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-09-29 14:05:23 -0400

    add infra provisioned BUILDKITE_API_TOKEN as primary for graphql call

--- buildkite/scripts/generate-diff.sh
+++ buildkite/scripts/generate-diff.sh
@@ -8,7 +8,6 @@ BASE=${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-origin/develop}
 COMMIT=$(diff -u <(git rev-list --first-parent HEAD) \
         <(git rev-list --first-parent $BASE) | \
         sed -ne 's/^ //p' | head -1)
-COMMIT=""
 
 if [[ $COMMIT != "" ]]; then
   # Get the files that have changed since that shared commit
@@ -18,7 +17,7 @@ else
   if [ -n "${BUILDKITE_INCREMENTAL+x}" ]; then
     # base DIFF on last successful Buildkite `develop` RUN
     ci_recent_pass_commit=$(
-      curl https://graphql.buildkite.com/v1 -H "Authorization: Bearer $TOKEN" \
+      curl https://graphql.buildkite.com/v1 -H "Authorization: Bearer ${BUILDKITE_API_TOKEN:-$TOKEN}" \
         -d'{"query": "query { pipeline(slug: \"o-1-labs-2/coda\") { builds(first: 1 branch: \"develop\" state: PASSED) { edges { node { commit } } } } }"}' \
       | jq '.data.pipeline.builds.edges[0].node.commit' | tr -d '"'
     )
