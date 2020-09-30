bffd19341a133e59d883b54da8793220a5ac55d3
Andrew Trainor <andrew@o1labs.org>
2020-09-30 01:55:28 -0400

    rosetta/add-docker-build-instructions update build-and-test script

--- scripts/build-and-test-rosetta-from-git.sh
+++ scripts/build-and-test-rosetta-from-git.sh
@@ -1,15 +1,16 @@
 GITBRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 TAG=$(echo ${GITBRANCH} | sed 's!/!-!; s!_!-!g')
 
-docker pull gcr.io/o1labs-192920/coda-rosetta-opam-deps:${TAG}
+docker pull gcr.io/o1labs-192920/mina-rosetta-opam-deps:${TAG}
 
 cat dockerfiles/Dockerfile-rosetta | docker build \
   --target production \
-  -t gcr.io/o1labs-192920/coda-rosetta:${TAG} \
-  --cache-from gcr.io/o1labs-192920/coda-rosetta-opam-deps:${TAG} \
+  -t gcr.io/o1labs-192920/mina-rosetta:${TAG} \
+  --no-cache \
   --build-arg "DUNE_PROFILE=dev" \
-  --build-arg "CODA_BRANCH=${GITBRANCH}" -
+  --build-arg "MINA_BRANCH=${GITBRANCH}" -
+  #--cache-from gcr.io/o1labs-192920/mina-rosetta-opam-deps:${TAG} \
 
-docker run -it --entrypoint=./docker-test-start.sh gcr.io/o1labs-192920/coda-rosetta:${TAG}
+docker run -it --entrypoint=./docker-test-start.sh gcr.io/o1labs-192920/minaa-rosetta:${TAG}
 
-[[ $? -eq 0 ]] && docker push gcr.io/o1labs-192920/coda-rosetta:${TAG} || echo "Tests failed, not pushing"
+[[ $? -eq 0 ]] && docker push gcr.io/o1labs-192920/mina-rosetta:${TAG} || echo "Tests failed, not pushing"
