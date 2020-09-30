c8a576b1a9712d0ec8adccc059e7355adcc612cb
Andrew Trainor <andrew@o1labs.org>
2020-09-30 16:09:18 -0400

    rosetta/add-docker-build-instructions Update flags

--- scripts/build-and-test-rosetta-from-git.sh
+++ scripts/build-and-test-rosetta-from-git.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 GITBRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 TAG=$(echo ${GITBRANCH} | sed 's!/!-!; s!_!-!g')
 
@@ -5,10 +7,12 @@ docker pull gcr.io/o1labs-192920/mina-rosetta-opam-deps:${TAG}
 
 cat dockerfiles/Dockerfile-rosetta | docker build \
   --target production \
-  -t gcr.io/o1labs-192920/mina-rosetta:${TAG} \
   --no-cache \
+  --force-rm \
+  -t gcr.io/o1labs-192920/mina-rosetta:${TAG} \
   --build-arg "DUNE_PROFILE=dev" \
   --build-arg "MINA_BRANCH=${GITBRANCH}" -
+
   #--cache-from gcr.io/o1labs-192920/mina-rosetta-opam-deps:${TAG} \
 
 docker run -it --entrypoint=./docker-test-start.sh gcr.io/o1labs-192920/mina-rosetta:${TAG}
