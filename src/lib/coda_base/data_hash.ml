4cdce3bd67b0d5a4c4f6cea75fc262a5d93384c1
Paul Steckler <steck@stecksoft.com>
2020-10-07 16:41:12 -0700

    Fix builds for nonconsensus

--- src/lib/coda_base/data_hash.ml
+++ src/lib/coda_base/data_hash.ml
@@ -1,1 +1,15 @@
+(* data_hash.ml *)
+
+[%%import
+"/src/config.mlh"]
+
+[%%ifdef
+consensus_mechanism]
+
 include Data_hash_lib.Data_hash
+
+[%%else]
+
+include Data_hash_lib_nonconsensus.Data_hash
+
+[%%endif]
--- src/lib/coda_base/state_hash.ml
+++ src/lib/coda_base/state_hash.ml
@@ -1,1 +1,15 @@
+(* state_hash.ml *)
+
+[%%import
+"/src/config.mlh"]
+
+[%%ifdef
+consensus_mechanism]
+
 include Data_hash_lib.State_hash
+
+[%%else]
+
+include Data_hash_lib_nonconsensus.State_hash
+
+[%%endif]
