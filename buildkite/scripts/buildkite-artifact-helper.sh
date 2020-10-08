d87e09f43fcdf8cd3c2ea82fa56ea10932cb5479
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-10-08 10:47:05 -0400

    add exec mods to buildkite artifact helper script

--- buildkite/scripts/buildkite-artifact-helper.sh
+++ buildkite/scripts/buildkite-artifact-helper.sh
@@ -8,5 +8,5 @@ if [[ $# -ne 1 ]]; then
 fi
 
 # Move to build checkout root to allow for artifact paths to be expressed relative to root
-cd "${BUILDKITE_BUILD_CHECKOUT_PATH:-"../.."}
+cd "${BUILDKITE_BUILD_CHECKOUT_PATH:-'../..'}"
 BUILDKITE_ARTIFACT_UPLOAD_DESTINATION="gs://buildkite_k8s/coda/shared/${BUILDKITE_JOB_ID}" buildkite-agent artifact upload "${1}"
