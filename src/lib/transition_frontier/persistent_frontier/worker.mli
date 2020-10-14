c50a6d9f308635bbf6a7d99b512a2d15cf18bea9
Izaak Meckler <ihmeckler@gmail.com>
2020-10-14 12:56:06 -0700

    fix mli

--- src/lib/transition_frontier/persistent_frontier/worker.mli
+++ src/lib/transition_frontier/persistent_frontier/worker.mli
@@ -4,8 +4,6 @@ type create_args = {db: Database.t; logger: Logger.t}
 
 type input = Diff.Lite.E.t list
 
-val oc : Core.Out_channel.t
-
 include
   Otp_lib.Worker_supervisor.S
   with type create_args := create_args
