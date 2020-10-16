503f74d87be887c3152cb25cf9add20dd5499cd3
Izaak Meckler <ihmeckler@gmail.com>
2020-10-16 11:28:56 -0700

    pickles base changes

--- src/lib/pickles_base/domains.ml
+++ src/lib/pickles_base/domains.ml
@@ -3,8 +3,7 @@ open Core_kernel
 [%%versioned
 module Stable = struct
   module V1 = struct
-    type t =
-      {h: Domain.Stable.V1.t; k: Domain.Stable.V1.t; x: Domain.Stable.V1.t}
+    type t = {h: Domain.Stable.V1.t; x: Domain.Stable.V1.t}
     [@@deriving fields, sexp, compare]
 
     let to_latest = Fn.id
