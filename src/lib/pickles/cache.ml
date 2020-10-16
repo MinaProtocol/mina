2fe2c9ec0f51da49a8e7e8c41ddc48d9227e647b
Izaak Meckler <ihmeckler@gmail.com>
2020-10-16 11:58:54 -0700

    update caching

--- src/lib/pickles/cache.ml
+++ src/lib/pickles/cache.ml
@@ -4,49 +4,44 @@ module Step = struct
   module Key = struct
     module Proving = struct
       type t =
-        Type_equal.Id.Uid.t * int * Backend.Tick.R1CS_constraint_system.t
+        Type_equal.Id.Uid.t
+        * string
+        * int
+        * Backend.Tick.R1CS_constraint_system.t
 
       let to_string : t -> _ = function
-        | _id, _n, h ->
-            sprintf !"step-%s"
+        | _id, s, n, h ->
+            sprintf !"step-%s-%d-%s" s n
               (Md5.to_hex (Backend.Tick.R1CS_constraint_system.digest h))
     end
 
     module Verification = struct
-      type t = Type_equal.Id.Uid.t * int * Md5.t [@@deriving sexp]
+      type t = Type_equal.Id.Uid.t * string * int * Md5.t [@@deriving sexp]
 
       let to_string : t -> _ = function
-        | _id, _n, h ->
-            sprintf !"vk-step-%s" (Md5.to_hex h)
+        | _id, s, n, h ->
+            sprintf !"vk-step-%s-%d-%s" s n (Md5.to_hex h)
     end
   end
 
   let storable =
     Key_cache.Sync.Disk_storable.simple Key.Proving.to_string
-      (fun (_, _, t) ~path ->
-        let t =
-          Snarky_bn382.Tweedle.Dum.Field_index.read
+      (fun (_, _, _, cs) ~path ->
+        let index =
+          Snarky_bn382.Tweedle.Dum.Plonk.Field_index.read
             (Backend.Tick.Keypair.load_urs ())
-            t.m.a t.m.b t.m.c
-            (Unsigned.Size_t.of_int (1 + t.public_input_size))
             path
         in
-        Caml.Gc.finalise Snarky_bn382.Tweedle.Dum.Field_index.delete t ;
-        t )
-      Snarky_bn382.Tweedle.Dum.Field_index.write
-
-  let vk_storable =
-    Key_cache.Sync.Disk_storable.simple Key.Verification.to_string
-      (fun _ ~path -> Snarky_bn382.Fp_verifier_index.read path)
-      Snarky_bn382.Fp_verifier_index.write
+        {Tweedle.Dum_based_plonk.Keypair.index; cs} )
+      (fun t -> Snarky_bn382.Tweedle.Dum.Plonk.Field_index.write t.index)
 
   let vk_storable =
     Key_cache.Sync.Disk_storable.simple Key.Verification.to_string
       (fun _ ~path ->
-        Snarky_bn382.Tweedle.Dum.Field_verifier_index.read
+        Snarky_bn382.Tweedle.Dum.Plonk.Field_verifier_index.read
           (Backend.Tick.Keypair.load_urs ())
           path )
-      Snarky_bn382.Tweedle.Dum.Field_verifier_index.write
+      (fun x s -> Snarky_bn382.Tweedle.Dum.Plonk.Field_verifier_index.write x s)
 
   let read_or_generate cache k_p k_v typ main =
     let s_p = storable in
@@ -62,8 +57,10 @@ module Step = struct
             Common.time "step keypair create" (fun () ->
                 (Keypair.create ~pk ~vk:(Backend.Tick.Keypair.vk pk), dirty) )
         | Error _e ->
-            Timer.clock __LOC__ ;
-            let r = generate_keypair ~exposing:[typ] main in
+            let r =
+              Common.time "stepkeygen" (fun () ->
+                  generate_keypair ~exposing:[typ] main )
+            in
             Timer.clock __LOC__ ;
             let _ =
               Key_cache.Sync.write cache s_p (Lazy.force k_p) (Keypair.pk r)
@@ -91,46 +88,37 @@ end
 module Wrap = struct
   module Key = struct
     module Verification = struct
-      type t = Type_equal.Id.Uid.t * Md5.t [@@deriving sexp]
+      type t = Type_equal.Id.Uid.t * string * Md5.t [@@deriving sexp]
 
-      let equal (_, x1) (_, x2) = Md5.equal x1 x2
+      let equal ((_, x1, y1) : t) ((_, x2, y2) : t) =
+        [%eq: string * Md5.t] (x1, y1) (x2, y2)
 
       let to_string : t -> _ = function
-        | _id, h ->
-            sprintf !"vk-wrap-%s" (Md5.to_hex h)
+        | _id, s, h ->
+            sprintf !"vk-wrap-%s-%s" s (Md5.to_hex h)
     end
 
     module Proving = struct
-      type t = Type_equal.Id.Uid.t * Backend.Tock.R1CS_constraint_system.t
+      type t =
+        Type_equal.Id.Uid.t * string * Backend.Tock.R1CS_constraint_system.t
 
       let to_string : t -> _ = function
-        | _id, h ->
-            sprintf !"wrap-%s"
+        | _id, s, h ->
+            sprintf !"wrap-%s-%s" s
               (Md5.to_hex (Backend.Tock.R1CS_constraint_system.digest h))
     end
   end
 
   let storable =
     Key_cache.Sync.Disk_storable.simple Key.Proving.to_string
-      (fun (_, t) ~path ->
-        let t =
-          Snarky_bn382.Tweedle.Dee.Field_index.read
+      (fun (_, _, cs) ~path ->
+        let index =
+          Snarky_bn382.Tweedle.Dee.Plonk.Field_index.read
             (Backend.Tock.Keypair.load_urs ())
-            t.m.a t.m.b t.m.c
-            (Unsigned.Size_t.of_int (1 + t.public_input_size))
             path
         in
-        Caml.Gc.finalise Snarky_bn382.Tweedle.Dee.Field_index.delete t ;
-        t )
-      Snarky_bn382.Tweedle.Dee.Field_index.write
-
-  let vk_storable =
-    Key_cache.Sync.Disk_storable.simple Key.Verification.to_string
-      (fun _ ~path ->
-        Snarky_bn382.Tweedle.Dee.Field_verifier_index.read
-          (Backend.Tock.Keypair.load_urs ())
-          path )
-      Snarky_bn382.Tweedle.Dee.Field_verifier_index.write
+        {Tweedle.Dee_based_plonk.Keypair.index; cs} )
+      (fun t -> Snarky_bn382.Tweedle.Dee.Plonk.Field_index.write t.index)
 
   let read_or_generate step_domains cache k_p k_v typ main =
     let module Vk = Verification_key in
@@ -139,11 +127,17 @@ module Wrap = struct
     let pk =
       lazy
         (let k = Lazy.force k_p in
-         match Key_cache.Sync.read cache s_p k with
+         match
+           Common.time "wrap key read" (fun () ->
+               Key_cache.Sync.read cache s_p k )
+         with
          | Ok (pk, d) ->
              (Keypair.create ~pk ~vk:(Backend.Tock.Keypair.vk pk), d)
          | Error _e ->
-             let r = generate_keypair ~exposing:[typ] main in
+             let r =
+               Common.time "wrapkeygen" (fun () ->
+                   generate_keypair ~exposing:[typ] main )
+             in
              let _ = Key_cache.Sync.write cache s_p k (Keypair.pk r) in
              (r, `Generated_something))
     in
@@ -155,9 +149,9 @@ module Wrap = struct
              (module Vk)
          in
          match Key_cache.Sync.read cache s_v k_v with
-         | Ok (vk, _) ->
-             vk
-         | Error _e ->
+         | Ok (vk, d) ->
+             (vk, d)
+         | Error e ->
              let kp, _dirty = Lazy.force pk in
              let vk = Keypair.vk kp in
              let pk = Keypair.pk kp in
@@ -166,17 +160,13 @@ module Wrap = struct
                ; commitments= Backend.Tock.Keypair.vk_commitments vk
                ; step_domains
                ; data=
-                   (let open Snarky_bn382.Tweedle.Dee.Field_index in
-                   let n = Unsigned.Size_t.to_int in
-                   let variables = n (num_variables pk) in
-                   { public_inputs= n (public_inputs pk)
-                   ; variables
-                   ; constraints= variables
-                   ; nonzero_entries= n (nonzero_entries pk)
-                   ; max_degree= n (max_degree pk) }) }
+                   (let open Snarky_bn382.Tweedle.Dee.Plonk.Field_index in
+                   { constraints=
+                       Unsigned.Size_t.to_int (domain_d1_size pk.index) }) }
              in
              let _ = Key_cache.Sync.write cache s_v k_v vk in
-             vk)
+             let _vk = Key_cache.Sync.read cache s_v k_v in
+             (vk, `Generated_something))
     in
     (pk, vk)
 end
