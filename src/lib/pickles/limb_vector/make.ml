24bd61767a9e481408f5fc679fb31f1a4cd4f3c7
Izaak Meckler <ihmeckler@gmail.com>
2020-10-16 11:34:42 -0700

    limb vector updates

--- src/lib/pickles/limb_vector/make.ml
+++ src/lib/pickles/limb_vector/make.ml
@@ -13,9 +13,15 @@ struct
 
   module Constant = Constant.Make (N)
 
-  let typ : (t, Constant.t) Typ.t =
-    Typ.list ~length Boolean.typ
+  let typ' bool : (t, Constant.t) Typ.t =
+    Typ.list ~length bool
     |> Typ.transport ~there:Constant.to_bits ~back:Constant.of_bits
+
+  let typ : (t, Constant.t) Typ.t = typ' Boolean.typ
+
+  let typ_unchecked : (t, Constant.t) Typ.t = typ' Boolean.typ_unchecked
+
+  let typ' = function `Constrained -> typ | `Unconstrained -> typ_unchecked
 
   let packed_typ : (Field.t, Constant.t) Typ.t =
     Typ.field
