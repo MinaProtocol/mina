307968b79774050adf69d24850c74461d9962c05
Izaak Meckler <ihmeckler@gmail.com>
2020-10-16 10:27:03 -0700

    remove old types

--- src/lib/zexe_backend/tweedle/tweedle.ml
+++ src/lib/zexe_backend/tweedle/tweedle.ml
@@ -1,3 +1,4 @@
 include Basic
-module Dee_based = Dee_based
-module Dum_based = Dum_based
+module Dee_based_plonk = Dee_based_plonk
+module Dum_based_plonk = Dum_based_plonk
+module Precomputed = Precomputed
