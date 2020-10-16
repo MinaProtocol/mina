388c7f7ee20cef76be008d54239178857d420bbe
Izaak Meckler <ihmeckler@gmail.com>
2020-10-16 11:57:46 -0700

    update commitment lengths computation

--- src/lib/pickles/commitment_lengths.ml
+++ src/lib/pickles/commitment_lengths.ml
@@ -5,54 +5,44 @@ open Import
 (* Compute the number of group elements required to represent each commitment.
    This is simply ceil(degree of commitment / max degree natively supported by the PCS) *)
 
-let generic' ~h ~k ~sub:( - ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
-    _ Dlog_marlin_types.Evals.t =
-  let two_h = ceil_div_max_degree (of_int 2 * h#size) in
-  let three_k_minus_three =
-    ceil_div_max_degree ((of_int 3 * k#size) - of_int 3)
-  in
-  let h = ceil_div_max_degree h#size in
-  let k = ceil_div_max_degree k#size in
-  let index : _ Abc.t = {a= k; b= k; c= k} in
-  { w_hat= h
-  ; z_hat_a= h
-  ; z_hat_b= h
-  ; h_1= two_h
-  ; h_2= h
-  ; h_3= three_k_minus_three
-  ; row= index
-  ; col= index
-  ; value= index
-  ; rc= index
-  ; g_1= h
-  ; g_2= h
-  ; g_3= k }
+let generic' ~h ~add:( + ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
+    _ Dlog_plonk_types.Evals.t =
+  let n = h#size in
+  let n_plus_2 = ceil_div_max_degree (of_int 2 + n) in
+  let n_plus_3 = ceil_div_max_degree (of_int 3 + n) in
+  let five_n_plus_1 = ceil_div_max_degree (of_int 5 * (n + of_int 1)) in
+  let h = ceil_div_max_degree n in
+  { l= n_plus_2
+  ; r= n_plus_2
+  ; o= n_plus_2
+  ; z= n_plus_3
+  ; t= five_n_plus_1
+  ; f= n_plus_3
+  ; sigma1= h
+  ; sigma2= h }
 
-let generic map ~h ~k ~max_degree : _ Dlog_marlin_types.Evals.t =
-  let index : _ Abc.t = {a= k; b= k; c= k} in
-  Dlog_marlin_types.Evals.map
+let generic map ~h ~max_degree : _ Dlog_plonk_types.Evals.t =
+  let n_plus_2 = map h ~f:(fun h -> h + 2) in
+  let n_plus_3 = map h ~f:(fun h -> h + 3) in
+  let five_n_plus_1 = map h ~f:(fun h -> 5 * (h + 1)) in
+  Dlog_plonk_types.Evals.map
     ~f:(fun v ->
       map v ~f:(fun x -> Int.round_up x ~to_multiple_of:max_degree / max_degree)
       )
-    { w_hat= h
-    ; z_hat_a= h
-    ; z_hat_b= h
-    ; h_1= map h ~f:(( * ) 2)
-    ; h_2= h
-    ; h_3= map k ~f:(fun k -> (3 * k) - 3)
-    ; row= index
-    ; col= index
-    ; value= index
-    ; rc= index
-    ; g_1= h
-    ; g_2= h
-    ; g_3= k }
+    { l= n_plus_2
+    ; r= n_plus_2
+    ; o= n_plus_2
+    ; z= n_plus_3
+    ; t= five_n_plus_1
+    ; f= n_plus_3
+    ; sigma1= h
+    ; sigma2= h }
 
-let of_domains {Domains.h; k} ~max_degree : int Dlog_marlin_types.Evals.t =
-  let h, k = Domain.(size h, size k) in
-  generic ~max_degree (fun x ~f -> f x) ~h ~k
+let of_domains {Domains.h} ~max_degree : int Dlog_plonk_types.Evals.t =
+  let h = Domain.size h in
+  generic ~max_degree (fun x ~f -> f x) ~h
 
 let of_domains_vector domainses =
   let open Vector in
   let f field = map domainses ~f:(Fn.compose Domain.size field) in
-  Vector.(generic map ~h:(f Domains.h) ~k:(f Domains.k))
+  Vector.(generic map ~h:(f Domains.h))
