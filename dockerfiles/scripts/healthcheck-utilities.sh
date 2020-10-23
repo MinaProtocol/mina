cb450ab19c2876e7759890bc87168f9bc9bb04df
Ahmad Wilson <zer0ne.io.x@gmail.com>
2020-10-23 15:26:22 -0400

    update healthcheck utilities and add to daemon image

--- dockerfiles/scripts/healthcheck-utilities.sh
+++ dockerfiles/scripts/healthcheck-utilities.sh
@@ -1,10 +1,7 @@
-set +x
-
-function is_daemon_synced() {
-    #
-    # determine whether a local daemon is SYNCed with its network
-    #
-
+#
+# Determine whether a local daemon is SYNCed with its network
+#
+function isDaemonSynced() {
     status=$(
         curl -H "Content-Type:application/json" -d'{ "query": "query { syncStatus } " }' localhost:3085/graphql | \
             jq '.data.syncStatus'
@@ -13,11 +10,10 @@ function is_daemon_synced() {
     [[ status == \"SYNCED\" ]] && return 0 || (echo "Daemon is out of sync with status: ${status}" && return 1) 
 }
 
-function is_chain_length_highest_received() {
-    #
-    # determine whether a local daemon is has processed the highest observed block
-    #
-
+#
+# Determine whether a local daemon is has processed the highest observed block
+#
+function isChainlengthHighestReceived() {
     chainLength=$(
         curl -H "Content-Type:application/json" -d'{ "query": "query { daemonStatus { blockchainLength } }" }' localhost:3085/graphql | \
             jq '.data.daemonStatus.blockchainLength'
@@ -32,13 +28,15 @@ function is_chain_length_highest_received() {
         (echo "Daemon chain length[${chainLength}] is not at highest received[${highestReceived}]." && return 1) 
 }
 
-function peer_count_greater_than() {
-    #
-    # determine whether a local daemon has a peer count greater than some threshold
-    #
-
+#
+# Determine whether a local daemon has a peer count greater than some threshold
+#
+function peerCountGreaterThan() {
     peerCountMinThreshold=$1
-    peerCount=$(curl localhost:3085/graphql -d'{ query { daemonStatus { peers } } }')
+    peerCount=$(
+        curl -H "Content-Type:application/json" -d'{ "query": "query { daemonStatus { peers } }" }' localhost:3085/graphql | \
+            jq '.data.daemonStatus.peers | length'
+    )
     
     [[ $peerCount -gt $peerCountMinThreshold ]] && return 0 ||
         (echo "Peer count[${peerCount}] is not greater than mininum threshold[${peerCountMinThreshold}]." && return 1) 
