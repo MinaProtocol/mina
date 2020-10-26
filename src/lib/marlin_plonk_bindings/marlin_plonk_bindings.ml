46a7356caadef1dfc79fc083439f25e1e1826f4a
mrmr1993 <mr_1993@hotmail.co.uk>
2020-10-26 23:51:13 +0000

    Add bindings for tweedle_fq

--- src/lib/marlin_plonk_bindings/marlin_plonk_bindings.ml
+++ src/lib/marlin_plonk_bindings/marlin_plonk_bindings.ml
@@ -6,3 +6,4 @@ module Bigint_384 = Marlin_plonk_bindings_bigint_384
 (** Finite fields *)
 
 module Tweedle_fp = Marlin_plonk_bindings_tweedle_fp
+module Tweedle_fq = Marlin_plonk_bindings_tweedle_fq
--- src/lib/marlin_plonk_bindings/stubs/src/lib.rs
+++ src/lib/marlin_plonk_bindings/stubs/src/lib.rs
@@ -3,3 +3,4 @@ extern crate libc;
 pub mod bigint_256;
 pub mod bigint_384;
 pub mod tweedle_fp;
+pub mod tweedle_fq;
