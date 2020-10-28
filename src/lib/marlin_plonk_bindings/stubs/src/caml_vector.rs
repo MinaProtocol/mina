dcf622850cfde2c43917cc8b1221a64710a5834b
mrmr1993 <mr_1993@hotmail.co.uk>
2020-10-28 22:28:38 +0000

    Rework caml_vector to be simpler, easier to use

--- src/lib/marlin_plonk_bindings/stubs/src/caml_vector.rs
+++ src/lib/marlin_plonk_bindings/stubs/src/caml_vector.rs
@@ -1,74 +1,53 @@
-pub fn to_array<Caml: ocaml::ToValue, T: Into<Caml>>(x: Vec<T>) -> ocaml::Array<ocaml::Value> {
-    let len = x.len();
+pub fn to_array_<T, F: Fn(T) -> ocaml::Value>(v: Vec<T>, f: F) -> ocaml::Array<ocaml::Value> {
+    let len = v.len();
     // Manually allocate an OCaml array of the right size
     let mut array = ocaml::Array::alloc(len);
-    let mut i = 0;
-    for c in x.into_iter() {
+    for (i, x) in v.into_iter().enumerate() {
         unsafe {
-            // Construct the OCaml value for each element, then place it in the correct position in
-            // the array.
-            // Bounds checks are skipped because we know statically that the indices are in range.
-            array.set_unchecked(i, c.into().to_value());
+            array.set_unchecked(i, f(x));
         }
-        i = i + 1;
     }
     array
 }
 
-pub fn ref_to_array<'a, Caml: ocaml::ToValue + From<&'a T>, T: 'a>(
-    x: &'a Vec<T>,
-) -> ocaml::Array<ocaml::Value> {
-    let len = x.len();
+pub fn ref_to_array<T, F: Fn(&T) -> ocaml::Value>(v: &Vec<T>, f: F) -> ocaml::Array<ocaml::Value> {
+    let len = v.len();
     // Manually allocate an OCaml array of the right size
     let mut array = ocaml::Array::alloc(len);
-    let mut i = 0;
-    for c in x.into_iter() {
+    for (i, x) in v.into_iter().enumerate() {
         unsafe {
-            // Construct the OCaml value for each element, then place it in the correct position in
-            // the array.
-            // Bounds checks are skipped because we know statically that the indices are in range.
-            array.set_unchecked(i, Caml::from(&c).to_value());
+            array.set_unchecked(i, f(x));
         }
-        i = i + 1;
     }
     array
+}
+
+pub fn to_array<Caml: ocaml::ToValue, T: Into<Caml>>(v: Vec<T>) -> ocaml::Array<ocaml::Value> {
+    to_array_(v, |x| Into::<Caml>::into(x).to_value())
+}
+
+pub fn from_array_<A: ocaml::ToValue + ocaml::FromValue, T, F: Fn(A) -> T>(
+    array: ocaml::Array<A>,
+    f: F,
+) -> Vec<T> {
+    let len = array.len();
+    let mut vec: Vec<T> = Vec::with_capacity(len);
+    for i in 0..len {
+        unsafe {
+            vec.push(f(array.get_unchecked(i)));
+        }
+    }
+    vec
 }
 
 pub fn from_array<Caml: ocaml::ToValue + ocaml::FromValue, T: From<Caml>>(
     array: ocaml::Array<Caml>,
 ) -> Vec<T> {
-    let len = array.len();
-    let mut vec: Vec<T> = Vec::with_capacity(len);
-    for i in 0..len {
-        unsafe {
-            vec.push(array.get_unchecked(i).into());
-        }
-    }
-    vec
+    from_array_(array, T::from)
 }
 
 pub fn from_value_array<Caml: ocaml::FromValue, T: From<Caml>>(
     array: ocaml::Array<ocaml::Value>,
 ) -> Vec<T> {
-    let len = array.len();
-    let mut vec: Vec<T> = Vec::with_capacity(len);
-    for i in 0..len {
-        unsafe {
-            vec.push(Caml::from_value(array.get_unchecked(i)).into());
-        }
-    }
-    vec
-}
-
-pub fn from_pointer_array<Caml, T: From<ocaml::Pointer<Caml>>>(
-    array: ocaml::Array<ocaml::Pointer<Caml>>,
-) -> Vec<T> {
-    let len = array.len();
-    let mut vec: Vec<T> = Vec::with_capacity(len);
-    for i in 0..len {
-        unsafe {
-            vec.push(array.get_unchecked(i).into());
-        }
-    }
-    vec
+    from_array_(array, |x| Caml::from_value(x).into())
 }
