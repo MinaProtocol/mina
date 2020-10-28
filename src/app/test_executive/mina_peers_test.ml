f960e3cff5af3b5feccefe6760bbc9ca4c680e7f
Helena Li <helena.li0x1@gmail.com>
2020-10-27 17:54:24 -0700

    resolve pauls comments

--- src/app/test_executive/mina_peers_test.ml
+++ src/app/test_executive/mina_peers_test.ml
@@ -45,26 +45,22 @@ module Make (Engine : Engine_intf) = struct
       ~metadata:
       [("namespace", `String t.namespace); ("pod_id", `String t.pod_id)] ; *)
     let get_peer_id_partial = Node.get_peer_id ~logger in
-    let%bind query_result =
+    let%bind query_results =
       Malleable_error.List.map peer_list ~f:get_peer_id_partial
     in
-    [%log info] "mina_peers_test: successfully made graphql querry" ;
-    (* query_result is of type (string * string sexp_list) sexp_list *)
+    [%log info] "mina_peers_test: successfully made graphql query" ;
+    (* query_results is of type (string * string sexp_list) sexp_list *)
     (* each element represents the data of a single node relevant to this test. ( peer_id of node * [list of peer_ids of node's peers] ) *)
-    let expected_peers, _ = List.unzip query_result in
+    let expected_peers, _ = List.unzip query_results in
     let test_compare_func (node_peer_id, visible_peers_of_node) =
       let expected_peers_of_node : string list =
-        List.filter
-          ~f:(fun p -> if String.equal p node_peer_id then false else true)
-          expected_peers
+        List.filter ~f:(String.equal node_peer_id) expected_peers
         (* expected_peers_of_node is just expected_peers but with the peer_id of the given node removed from the list *)
       in
       List.iter visible_peers_of_node ~f:(fun p ->
-          assert (
-            List.exists expected_peers_of_node ~f:(fun x ->
-                if String.equal x p then true else false ) ) )
+          assert (List.exists expected_peers_of_node ~f:(String.equal p)) )
       (* loop through visible_peers_of_node and make sure everything in that list is also in expected_peers_of_node *)
     in
     [%log info] "mina_peers_test: making assertions" ;
-    return (List.iter query_result ~f:test_compare_func)
+    return (List.iter query_results ~f:test_compare_func)
 end
