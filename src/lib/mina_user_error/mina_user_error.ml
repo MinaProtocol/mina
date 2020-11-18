cde97204428ec8fb597c047c806b6e645183594e
mrmr1993 <mr_1993@hotmail.co.uk>
2020-11-18 00:08:05 +0000

    Add pretty-printer for uncaught `Mina_user_error`s

--- src/lib/mina_user_error/mina_user_error.ml
+++ src/lib/mina_user_error/mina_user_error.ml
@@ -4,3 +4,26 @@ let raisef ?where =
   Format.ksprintf (fun message -> raise (Mina_user_error {message; where}))
 
 let raise ?where message = raise (Mina_user_error {message; where})
+
+let () =
+  Stdlib.Printexc.register_printer (fun exn ->
+      match exn with
+      | Mina_user_error {message; where} ->
+          let error =
+            match where with
+            | None ->
+                "encountered a configuration error"
+            | Some where ->
+                Printf.sprintf "encountered a configuration error %s" where
+          in
+          Some
+            (Printf.sprintf {err|
+FATAL ERROR
+
+  ☠  Coda %s.
+
+  %s
+%!|err}
+               error message)
+      | _ ->
+          None )
