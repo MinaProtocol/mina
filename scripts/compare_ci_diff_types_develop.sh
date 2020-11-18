3d435665bbda55ad9bf4486ae7812b2cfc944adc
mrmr1993 <mr_1993@hotmail.co.uk>
2020-11-18 00:54:16 +0000

    Add exports to allow Buildkite to run the CircleCI scripts

--- scripts/compare_ci_diff_types_develop.sh
+++ scripts/compare_ci_diff_types_develop.sh
@@ -6,5 +6,9 @@ if [ ! "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" = "develop" ]; then
   exit 0
 fi
 
+# Exports to let Buildkite run the CircleCI scripts
+export CI=true
+export BASE_BRANCH_NAME="$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
+
 ./scripts/compare_ci_diff_types.sh
 ./scripts/compare_ci_diff_binables.sh
