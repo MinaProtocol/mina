dfd22be017df6bc386f1f5e0a119b1ea9ee3fbb1
mrmr1993 <mr_1993@hotmail.co.uk>
2020-11-18 19:13:42 +0000

    Add script for connecting to running testnet

---
+++ buildkite/scripts/connect-to-testnet-on-develop.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ ! "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" = "develop" ]; then
+  exit 0
+fi
+
+source buildkite/scripts/export-git-env-vars.sh
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+echo "deb [trusted=yes] http://packages.o1test.net unstable main" | sudo tee /etc/apt/sources.list.d/coda.list
+sudo apt-get update
+sudo apt-get install --allow-downgrades -y curl ${PROJECT}_${VERSION}.deb
+
+TESTNET_NAME="turbo-pickles"
+
+# Generate genesis proof and then crash due to no peers
+coda daemon \
+  -config-file ./coda-automation/terraform/testnets/$TESTNET_NAME/genesis_ledger.txt \
+  -generate-genesis-proof true \
+|| true
+
+# Restart in the background
+coda daemon \
+  -peer-list-file coda-automation/terraform/testnets/$TESTNET_NAME/peers.txt \
+  -config-file ./coda-automation/terraform/testnets/$TESTNET_NAME/genesis_ledger.txt \
+  -generate-genesis-proof true \
+  -background
+
+# Check that the GraphQL client is up within 30s
+sleep 30s
+coda client status
+
+# Check that the daemon has connected to peers and is still up after 2 mins
+sleep 2m
+coda client status
+if [ $(coda client get-peers | wc -l) -gt 0 ]; then
+    echo "Found some peers"
+else
+    echo "No peers found"
+    exit 1
+fi
