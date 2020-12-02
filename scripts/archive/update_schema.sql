124c1278a8f89619ceeb5abb280ec6f54a2f82cc
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2020-12-01 19:23:55 -0800

    add block_winner to update-schema.sql script

--- scripts/archive/update_schema.sql
+++ scripts/archive/update_schema.sql
@@ -12,4 +12,13 @@ alter table blocks alter column global_slot_since_genesis set not null;
 --check if global_slot_since_genesis = global_slot; this should return 0 rows
 select * from blocks as b1 inner join blocks as b2 on b1.id = b2.id where b1.global_slot_since_genesis <> b2.global_slot
 
+-- for block winner
+--add the column without the not null constraint first
+alter table blocks add column block_winner_id int;
 
+--update the values of the new column. Making winner = creator because there is no way to get this information
+update blocks as b1 set block_winner_id = (select creator_id from blocks b2 where b1.id = b2.id);
+
+--add the not null, foreign key constraint
+alter table blocks alter column block_winner_id set not null;
+alter table blocks add constraint block_winner_id_fkey foreign key (block_winner_id) references public_keys(id)
