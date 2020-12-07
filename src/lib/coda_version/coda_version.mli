7a2d3fe5fc6714648222a1a5372d00673791d6e3
mrmr1993 <mr_1993@hotmail.co.uk>
2020-12-07 22:42:24 +0000

    Update marlin to use zexe as an indirect submodule

--- src/lib/coda_version/coda_version.mli
+++ src/lib/coda_version/coda_version.mli
@@ -11,9 +11,3 @@ val marlin_commit_id : string
 val marlin_commit_id_short : string
 
 val marlin_commit_date : string
-
-val zexe_commit_id : string
-
-val zexe_commit_id_short : string
-
-val zexe_commit_date : string
