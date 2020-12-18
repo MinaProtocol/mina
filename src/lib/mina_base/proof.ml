0423512821561611a771734905e82a17ae106697
Izaak Meckler <ihmeckler@gmail.com>
2020-12-18 15:54:22 -0800

    fix unit tests

--- src/lib/mina_base/proof.ml
+++ src/lib/mina_base/proof.ml
@@ -33,7 +33,7 @@ let%test_module "proof-tests" =
 
     let%test "proof serialization v1" =
       let proof = blockchain_dummy in
-      let known_good_digest = "48907559d134ea1a0f88edbd9e1e046a" in
+      let known_good_digest = "efccc2eb25063349f6f728b32ff4e658" in
       Ppx_version_runtime.Serialization.check_serialization
         (module Stable.V1)
         proof known_good_digest
--- src/lib/pickles/step_main.ml
+++ src/lib/pickles/step_main.ml
@@ -312,7 +312,7 @@ let step_main
                             ~messages ~wrap_verification_key:d.wrap_key
                             statement unfinalized )
                     in
-                    if true then
+                    if debug then
                       as_prover
                         As_prover.(
                           fun () ->
