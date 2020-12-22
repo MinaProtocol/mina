96326afec85be9f0a33a038edac98f91635e3bdb
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-12-22 00:10:41 -0600

    reformat coda_version_defaults.ml

--- src/lib/coda_version/coda_version_defaults.ml
+++ src/lib/coda_version/coda_version_defaults.ml
@@ -1,12 +1,19 @@
 let commit_id = "0000000000000000000000000000000000000000"
+
 let commit_id_short = "0000000"
+
 let branch = "MINA_NUL"
+
 let commit_date = "00-00-00"
 
 let marlin_commit_id = "0000000000000000000000000000000000000000"
+
 let marlin_commit_id_short = "0000000"
+
 let marlin_commit_date = "00-00-00"
 
 let zexe_commit_id = "0000000000000000000000000000000000000000"
+
 let zexe_commit_id_short = "0000000"
+
 let zexe_commit_date = "00-00-00"
