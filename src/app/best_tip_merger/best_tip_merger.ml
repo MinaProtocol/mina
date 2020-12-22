e216523edf769a9332d18ac0b4961907bd179759
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-12-21 22:49:04 -0600

    bazel app: best_tip_merger. includes src code change
    * will not build under bazel without the change to best_tip_merger.ml

--- src/app/best_tip_merger/best_tip_merger.ml
+++ src/app/best_tip_merger/best_tip_merger.ml
@@ -337,7 +337,7 @@ let main ~input_dir ~output_dir ~output_format ~min_peers () =
     ~transport:
       (Logger.Transport.File_system.dumb_logrotate ~directory:output_dir
          ~log_filename:"mina-best-tip-merger.log" ~max_size:logrotate_max_size
-         ~num_rotate) ;
+         ~num_rotate:logrotate_num_rotate) ;
   let logger = Logger.create () in
   let t' =
     List.fold ~init:t files ~f:(fun t log_file ->
