81f01e80d51a12131dd805d34c3c48aee951b95c
Aryeh Alexander <aryeh.alexander@gmail.com>
2020-12-23 10:57:36 -0500

    Pass in built docker images as command line variables to terraform

--- buildkite/scripts/deploy-nightly.sh
+++ buildkite/scripts/deploy-nightly.sh
@@ -2,8 +2,13 @@
 
 # TODO: make sure terraform installed
 
+source DOCKER_DEPLOY_ENV
+DAEMON_TAG="gcr.io/o1labs-192920/coda-daemon:${CODA_VERSION}-${CODA_GIT_HASH}"
+
+ARCHIVE_TAG="gcr.io/o1labs-192920/coda-archive:${CODA_VERSION}-${CODA_GIT_HASH}"
+
 cd coda-automation/terraform/testnets/nightly
 terraform init
 terraform destroy -auto-approve
-terraform apply -auto-approve
+terraform apply -var="coda_image=${DAEMON_TAG}" -var="coda_archive_image=${ARCHIVE_TAG}" -auto-approve
 
