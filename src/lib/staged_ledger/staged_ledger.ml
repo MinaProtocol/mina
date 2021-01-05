8f93ec735ef5f1bbd5a724e8811ea8e8c1a09b22
Izaak Meckler <ihmeckler@gmail.com>
2021-01-04 17:08:03 -0800

    add finer grained argument for skipping staged ledger verification

--- src/lib/staged_ledger/staged_ledger.ml
+++ src/lib/staged_ledger/staged_ledger.ml
@@ -961,6 +961,9 @@ module T = struct
       ((a : Transaction.Valid.t With_status.t list), b, c, d) =
     ((a :> Transaction.t With_status.t list), b, c, d)
 
+  [%%if
+  feature_snapps]
+
   let check_commands ledger ~verifier (cs : User_command.t list) =
     match
       Or_error.try_with (fun () ->
@@ -988,6 +991,35 @@ module T = struct
                   (Verifier.Failure.Verification_failed
                      (Error.of_string "batch verification failed")) ))
 
+  [%%else]
+
+  (* imeckler: added this version because the call to the verifier was
+   causing super catchup to proceed more slowly than it could have otherwise.
+
+   The reason is as follows: catchup would have, say 100 blocks in the "to verify"
+   queue and 20 in the "already verified, to apply" queue. Those 20 would be
+   processed very slowly because each one would have to call the verifier, which
+   the other queue was trying to call as well. *)
+  let check_commands _ledger ~verifier:_ (cs : User_command.t list) :
+      (User_command.Valid.t list, _) result Deferred.Or_error.t =
+    Result.all
+      (List.map cs ~f:(function
+        | Snapp_command _ ->
+            Error
+              (Verifier.Failure.Verification_failed
+                 (Error.of_string "check_commands: snapp commands disabled"))
+        | Signed_command c -> (
+          match Signed_command.check c with
+          | Some c ->
+              Ok (User_command.Signed_command c)
+          | None ->
+              Error
+                (Verifier.Failure.Verification_failed
+                   (Error.of_string "signature failed to verify")) ) ))
+    |> Deferred.Or_error.return
+
+  [%%endif]
+
   let apply ?skip_verification ~constraint_constants t
       (witness : Staged_ledger_diff.t) ~logger ~verifier ~current_state_view
       ~state_and_body_hash ~coinbase_receiver ~supercharge_coinbase =
@@ -996,9 +1028,9 @@ module T = struct
     let%bind () =
       time ~logger "check_completed_works" (fun () ->
           match skip_verification with
-          | Some true ->
+          | Some `All | Some `Proofs ->
               return ()
-          | Some false | None ->
+          | None ->
               check_completed_works ~logger ~verifier t.scan_state work )
     in
     let%bind prediff =
@@ -1012,7 +1044,9 @@ module T = struct
     in
     let apply_diff_start_time = Core.Time.now () in
     let%map ((_, _, `Staged_ledger new_staged_ledger, _) as res) =
-      apply_diff ?skip_verification ~constraint_constants t
+      apply_diff
+        ~skip_verification:(skip_verification = Some `All)
+        ~constraint_constants t
         (forget_prediff_info prediff)
         ~logger ~current_state_view ~state_and_body_hash
         ~log_prefix:"apply_diff"
