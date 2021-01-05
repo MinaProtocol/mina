b99aa15dbd16bbe6f946dfdab6221e73866010b2
Aryeh Alexander <aryeh.alexander@gmail.com>
2021-01-05 07:42:22 -0500

    Change to correct name of automation folder

--- buildkite/scripts/deploy-nightly.sh
+++ buildkite/scripts/deploy-nightly.sh
@@ -7,7 +7,7 @@ DAEMON_TAG="gcr.io/o1labs-192920/coda-daemon:${CODA_VERSION}-${CODA_GIT_HASH}"
 
 ARCHIVE_TAG="gcr.io/o1labs-192920/coda-archive:${CODA_VERSION}-${CODA_GIT_HASH}"
 
-cd coda-automation/terraform/testnets/nightly
+cd automation/terraform/testnets/nightly
 terraform init
 terraform destroy -auto-approve
 terraform apply -var="coda_image=${DAEMON_TAG}" -var="coda_archive_image=${ARCHIVE_TAG}" -auto-approve
