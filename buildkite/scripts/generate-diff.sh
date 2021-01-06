c01fcb3560b9fea32eb76389e0d097201437e3c9
Aryeh Alexander <aryeh.alexander@gmail.com>
2021-01-06 13:45:05 -0500

    Revert "Merge pull request #7229 from MinaProtocol/aryeh/nightlies"
    This reverts commit fcda24db7ce9d433ed8ae86d1d8c944d348f7137, reversing
    changes made to 6b66f9db55b625ab37a223942f548d3e540d4853.

--- buildkite/scripts/generate-diff.sh
+++ buildkite/scripts/generate-diff.sh
@@ -21,10 +21,7 @@ if [[ $BASECOMMIT != $COMMIT ]]; then
   # Get the files that have diverged from $BASE
   git diff $BASECOMMIT --name-only
 else
-
-  if [ -n $NIGHTLY ]; then
-    echo "buildkite/nightly.txt"
-  elif [ -n "${BUILDKITE_INCREMENTAL+x}" ]; then
+  if [ -n "${BUILDKITE_INCREMENTAL+x}" ]; then
     # TODO: remove (temporarily install network tooling)
     apt-get install --yes curl jq
 
--- buildkite/src/Jobs/Release/ArchiveNodeArtifact.dhall
+++ buildkite/src/Jobs/Release/ArchiveNodeArtifact.dhall
@@ -28,7 +28,6 @@ Pipeline.build
       JobSpec::{
         dirtyWhen = [
           S.strictlyStart (S.contains "buildkite/src/Jobs/Release/ArchiveNodeArtifact"),
-          S.exactly "buildkite/nightly.txt" "sh",
           S.strictlyStart (S.contains "src/app/archive"),
           S.strictlyStart (S.contains "scripts/archive")
         ],
