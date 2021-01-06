b9ffa7d0219c157a492af6bcd119c58a2834f1a2
Paul Steckler <steck@stecksoft.com>
2021-01-06 11:56:50 -0800

    Fix type error in Rosetta

--- src/app/rosetta/lib/block.ml
+++ src/app/rosetta/lib/block.ml
@@ -411,7 +411,7 @@ WITH RECURSIVE chain AS (
           in
           { Internal_command_info.kind
           ; receiver= Internal_commands.Extras.receiver extras
-          ; fee= Unsigned.UInt64.of_int ic.fee
+          ; fee= Unsigned.UInt64.of_int64 ic.fee
           ; token= Unsigned.UInt64.of_int64 ic.token
           ; hash= ic.hash } )
     in
