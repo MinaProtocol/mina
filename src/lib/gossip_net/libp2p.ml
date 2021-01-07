9335dcdff84b35de408da5a9dc7de740da032291
Izaak Meckler <ihmeckler@gmail.com>
2021-01-06 21:27:43 -0800

    rename Mina_net2 back to Coda_net2

--- src/lib/gossip_net/libp2p.ml
+++ src/lib/gossip_net/libp2p.ml
@@ -62,7 +62,8 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
           (Message.msg Envelope.Incoming.t * Coda_net2.Validation_callback.t)
           Strict_pipe.Reader.t
       ; subscription:
-          Message.msg Coda_net2.Pubsub.Subscription.t Deferred.t ref }
+          Message.msg Coda_net2.Pubsub.Subscription.t Deferred.t ref
+      ; restart_helper: unit -> unit }
 
     let create_rpc_implementations
         (Rpc_handler {rpc; f= handler; cost; budget}) =
@@ -416,6 +417,10 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
       in
       let net2_ref = ref (Deferred.never ()) in
       let subscription_ref = ref (Deferred.never ()) in
+      let restarts_r, restarts_w =
+        Strict_pipe.create ~name:"libp2p-restarts"
+          (Strict_pipe.Buffered (`Capacity 0, `Overflow Strict_pipe.Drop_head))
+      in
       let%bind () =
         let rec on_libp2p_create res =
           net2_ref :=
@@ -464,6 +469,12 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
             high_connectivity_ivar ~on_unexpected_termination
         in
         on_libp2p_create res ;
+        don't_wait_for
+          (Strict_pipe.Reader.iter restarts_r ~f:(fun () ->
+               let%bind n = !net2_ref in
+               let%bind () = Coda_net2.shutdown n in
+               let%bind () = on_unexpected_termination () in
+               !net2_ref >>| ignore )) ;
         let%map _ = res in
         ()
       in
@@ -513,7 +524,8 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
       ; high_connectivity_ivar
       ; subscription= subscription_ref
       ; message_reader
-      ; ban_reader }
+      ; ban_reader
+      ; restart_helper= (fun () -> Strict_pipe.Writer.write restarts_w ()) }
 
     let peers t = !(t.net2) >>= Coda_net2.peers
 
@@ -681,10 +693,10 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
         (peer_id : Peer.Id.t) (rpc : (q, r) rpc) (qs : q list) =
       let%bind net2 = !(t.net2) in
       match%bind
-        Mina_net2.open_stream net2 ~protocol:rpc_transport_proto peer_id
+        Coda_net2.open_stream net2 ~protocol:rpc_transport_proto peer_id
       with
       | Ok stream ->
-          let peer = Mina_net2.Stream.remote_peer stream in
+          let peer = Coda_net2.Stream.remote_peer stream in
           let transport = prepare_stream_transport stream in
           let (module Impl) = implementation_of_rpc rpc in
           try_call_rpc_with_dispatch ?heartbeat_timeout ?timeout
@@ -731,6 +743,8 @@ module Make (Rpc_intf : Mina_base.Rpc_intf.Rpc_interface_intf) :
     let set_connection_gating t config =
       let%bind net2 = !(t.net2) in
       Coda_net2.set_connection_gating_config net2 config
+
+    let restart_helper t = t.restart_helper ()
   end
 
   include T
