f55e1a82925f6f55e1a2ff8ef6366e3b6e680546
mrmr1993 <mr_1993@hotmail.co.uk>
2021-01-20 18:13:57 +0000

    Add a CI script to check PRs on compatible for conflicts with develop

---
+++ buildkite/scripts/merges-cleanly-to-develop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" != "compatible" ]; then exit 0; fi
+
+# Adapted from this stackoverflow answer: https://stackoverflow.com/a/10856937
+# The git merge-tree command shows the content of a 3-way merge without
+# touching the index, which we can then search for conflict markers.
+git merge-tree `git merge-base origin/develop HEAD` origin/develop HEAD | grep "^<<<<<<<"
+
+RET=$?
+
+if [ $RET -eq 0 ]; then
+  exit 0
+else
+  exit 1;
+fi
