8956483fa39c778ac82e691c0ad6ac30de3fb36e
mrmr1993 <mr_1993@hotmail.co.uk>
2021-01-25 18:44:24 +0000

    Use reference counted pointers for CamlPointer

--- src/lib/marlin_plonk_bindings/stubs/src/caml_pointer.rs
+++ src/lib/marlin_plonk_bindings/stubs/src/caml_pointer.rs
@@ -1,13 +1,14 @@
 use std::ops::{Deref, DerefMut};
+use std::rc::Rc;
 
-pub struct CamlPointer<T>(pub *mut T);
+#[derive(std::fmt::Debug, Clone)]
+pub struct CamlPointer<T>(pub Rc<T>);
 
 impl<T> CamlPointer<T> {
     extern "C" fn caml_pointer_finalize(v: ocaml::Value) {
         let mut v: ocaml::Pointer<CamlPointer<T>> = ocaml::FromValue::from_value(v);
         unsafe {
-            // Memory is freed when the variable goes out of scope
-            let _box = Box::from_raw(v.as_mut().0);
+            v.drop_in_place();
         }
     }
 
@@ -26,24 +27,24 @@ ocaml::custom!(CamlPointer<T> {
 unsafe impl<T> ocaml::FromValue for CamlPointer<T> {
     fn from_value(x: ocaml::Value) -> Self {
         let x = ocaml::Pointer::<Self>::from_value(x);
-        CamlPointer(x.as_ref().0)
+        CamlPointer(x.as_ref().0.clone())
     }
 }
 
 pub fn create<T>(x: T) -> CamlPointer<T> {
-    CamlPointer(Box::into_raw(Box::new(x)))
+    CamlPointer(Rc::new(x))
 }
 
 impl<T> Deref for CamlPointer<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        unsafe { &*self.0 }
+        &*self.0
     }
 }
 
 impl<T> DerefMut for CamlPointer<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        unsafe { &mut *self.0 }
+        Rc::get_mut(&mut self.0).unwrap()
     }
 }
