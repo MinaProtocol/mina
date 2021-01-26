caead29427af0ef77fe25e09acd8ef8451f42bea
Omer Zach <omer.zach@gmail.com>
2021-01-26 12:20:48 -0500

    Add new balances table to archiver
    The balances table will contain (id, public_key_id, balance) tuples.
    We'll soon add foreign keys from blocks_user_commands and
    blocks_internal_commands pointing into the balances table with
    corresponding balance rows.

--- src/app/archive/create_schema.sql
+++ src/app/archive/create_schema.sql
@@ -85,6 +85,12 @@ CREATE INDEX idx_blocks_state_hash ON blocks(state_hash);
 CREATE INDEX idx_blocks_creator_id ON blocks(creator_id);
 CREATE INDEX idx_blocks_height     ON blocks(height);
 
+CREATE TABLE balances
+( id            serial PRIMARY KEY
+, public_key_id int    NOT NULL REFERENCES public_keys(id)
+, balance       bigint NOT NULL
+);
+
 CREATE TABLE blocks_user_commands
 ( block_id        int NOT NULL REFERENCES blocks(id) ON DELETE CASCADE
 , user_command_id int NOT NULL REFERENCES user_commands(id) ON DELETE CASCADE
