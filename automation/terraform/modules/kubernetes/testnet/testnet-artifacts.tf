b96bf8a85aa4285c730c217cb83d53d66116b0e8
Ahmad Wilson <zer0ne.io.x@gmail.com>
2021-01-26 22:29:13 -0500

    explicitly set ${path.module} root for testnet-artifact key script execs

--- automation/terraform/modules/kubernetes/testnet/testnet-artifacts.tf
+++ automation/terraform/modules/kubernetes/testnet/testnet-artifacts.tf
@@ -1,14 +1,14 @@
 resource "null_resource" "block_producer_key_generation" {
   count = var.generate_and_upload_artifacts ? 1 : 0
   provisioner "local-exec" {
-    command = "../../../scripts/generate-keys-and-ledger.sh --testnet=${var.testnet_name} --wc=${var.whale_count} --fc=${var.fish_count} --reset=false"
+    command = "${path.module}/../../../../scripts/generate-keys-and-ledger.sh --testnet=${var.testnet_name} --wc=${var.whale_count} --fc=${var.fish_count} --reset=false"
   }
 }
 
 resource "null_resource" "block_producer_uploads" {
   count = var.generate_and_upload_artifacts ? 1 : 0
   provisioner "local-exec" {
-    command = "../../../scripts/upload-keys-k8s.sh ${var.testnet_name}"
+    command = "${path.module}/../../../../scripts/upload-keys-k8s.sh ${var.testnet_name}"
   }
   depends_on = [
     kubernetes_namespace.testnet_namespace,
