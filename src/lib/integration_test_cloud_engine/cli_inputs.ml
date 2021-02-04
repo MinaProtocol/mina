40ea88d08af09054831b0bb93befbcf30430df4c
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2021-02-04 15:36:59 -0500

    change CODA_AUTOMATION_LOCATION to MINA_AUTOMATION_LOCATION

--- src/lib/integration_test_cloud_engine/cli_inputs.ml
+++ src/lib/integration_test_cloud_engine/cli_inputs.ml
@@ -8,12 +8,12 @@ let term =
       "Location of the coda automation repository to use when deploying the \
        network."
     in
-    let env = Arg.env_var "CODA_AUTOMATION_LOCATION" ~doc in
+    let env = Arg.env_var "MINA_AUTOMATION_LOCATION" ~doc in
     Arg.(
       value & opt string "./automation"
       & info
-          ["coda-automation-location"]
-          ~env ~docv:"CODA_AUTOMATION_LOCATION" ~doc)
+          ["mina-automation-location"]
+          ~env ~docv:"MINA_AUTOMATION_LOCATION" ~doc)
   in
   let cons_inputs coda_automation_location = {coda_automation_location} in
   Term.(const cons_inputs $ coda_automation_location)
