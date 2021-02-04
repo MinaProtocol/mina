e6ca7068c1f3d6c3a5efaf39662c45867a9e82cc
Paul Steckler <steck@stecksoft.com>
2021-02-04 15:33:43 -0800

    Key validation improvements

--- src/lib/cli_lib/commands.ml
+++ src/lib/cli_lib/commands.ml
@@ -27,6 +27,37 @@ let validate_keypair =
     let%map_open privkey_path = Flag.privkey_write_path in
     Exceptions.handle_nicely
     @@ fun () ->
+    let read_pk () =
+      let pubkey_path = privkey_path ^ ".pub" in
+      try
+        In_channel.with_file pubkey_path ~f:(fun in_channel ->
+            match In_channel.input_line in_channel with
+            | Some line -> (
+              try Public_key.Compressed.of_base58_check_exn line
+              with _exn ->
+                eprintf
+                  "Could not create public key in file %s from text: %s\n!"
+                  pubkey_path line ;
+                exit 1 )
+            | None ->
+                eprintf "No public key found in file %s\n!" pubkey_path ;
+                exit 1 )
+      with exn ->
+        eprintf "Could not read public key file %s, error: %s\n!" pubkey_path
+          (Exn.to_string exn) ;
+        exit 1
+    in
+    let compare_public_keys ~pk_from_disk ~pk_from_keypair =
+      if Public_key.Compressed.equal pk_from_disk pk_from_keypair then
+        printf "Public key on-disk is derivable from private key\n"
+      else (
+        eprintf
+          "Public key read from disk %s different than public key %s derived \
+           from private key\n"
+          (Public_key.Compressed.to_base58_check pk_from_disk)
+          (Public_key.Compressed.to_base58_check pk_from_keypair) ;
+        exit 1 )
+    in
     let validate_transaction keypair =
       let dummy_payload = Mina_base.Signed_command_payload.dummy in
       let signature =
@@ -53,6 +84,9 @@ let validate_keypair =
       in
       match%map Secrets.Keypair.read ~privkey_path ~password with
       | Ok keypair ->
+          let pk_from_disk = read_pk () in
+          compare_public_keys ~pk_from_disk
+            ~pk_from_keypair:(keypair.public_key |> Public_key.compress) ;
           validate_transaction keypair
       | Error err ->
           eprintf "Could not read the specified keypair: %s\n"
