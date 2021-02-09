3b5824aa71b3bed033bd62dfe8cbc60a97184e57
Andrew Trainor <andrew@o1labs.org>
2021-02-09 14:07:06 -0500

    encore-peers Update patch

--- automation/terraform/testnets/encore/patch.sh
+++ automation/terraform/testnets/encore/patch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # archive-node-{1..3}
+# fish-block-producer-{3..5}
 
-for whale in fish-block-producer-{1..5} whale-block-producer-{1..15}; do
-  kubectl patch deploy/${whale} -p "$(cat patch.yaml)"
+for node in whale-block-producer-{10..15}; do
+  kubectl patch deploy/${node} -p "$(cat patch.yaml)"
   ready=""
   while [[ -z $ready ]]; do
-    ready=$(kubectl get pods -l app=${whale} | grep -P '\s+([1-9]+)\/\1\s+')
-    kubectl get pods -l app=${whale}
+    ready=$(kubectl get pods -l app=${node} | grep -P '\s+([1-9]+)\/\1\s+')
+    kubectl get pods -l app=${node}
     sleep 30
   done
 done
