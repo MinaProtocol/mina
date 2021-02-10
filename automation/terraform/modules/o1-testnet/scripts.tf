f1f30e6a7ad1666e2efc3ea190d39cd50cfb7848
Nathan Holland <nholland94@gmail.com>
2021-02-10 15:26:59 -0800

    Run terraform scripts from automation root

--- automation/terraform/modules/o1-testnet/scripts.tf
+++ automation/terraform/modules/o1-testnet/scripts.tf
@@ -1,12 +1,14 @@
 resource "null_resource" "block_producer_key_generation" {
   provisioner "local-exec" {
-    command = "${path.module}/../../../scripts/generate-keys-and-ledger.sh --testnet=${var.testnet_name} --wc=${var.whale_count} --fc=${var.fish_count} --reset=false --artifact-path=${var.artifact_path}"
+    working_dir = "${path.module}/../../.."
+    command = "./scripts/generate-keys-and-ledger.sh --testnet=${var.testnet_name} --wc=${var.whale_count} --fc=${var.fish_count} --reset=false --artifact-path=${var.artifact_path}"
   }
 }
 
 resource "null_resource" "block_producer_uploads" {
   provisioner "local-exec" {
-    command = "${path.module}/../../../scripts/upload-keys-k8s.sh ${var.testnet_name}"
+    working_dir = "${path.module}/../../.."
+    command = "./scripts/upload-keys-k8s.sh ${var.testnet_name}"
     environment = {
       CLUSTER = var.k8s_context
     }
