9a9dadde2a9a4c722a5cb29283a998421f99c676
Andrew Trainor <andrew@o1labs.org>
2021-02-11 03:39:01 -0500

    encore-peers Update peers.txt with a handful of loadbalanced peers

--- automation/terraform/testnets/encore/patch.sh
+++ automation/terraform/testnets/encore/patch.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # archive-node-{1..3}
-# fish-block-producer-{3..5}
+# fish-block-producer-{1..5}
+# whale-block-producer-{1..15}
 
-for node in whale-block-producer-12 archive-node-{1..3} fish-block-producer-4; do
+for node in fish-block-producer-{1..3}; do
   kubectl patch deploy/${node} -p "$(cat patch.yaml)"
   ready=""
   while [[ -z $ready ]]; do
     ready=$(kubectl get pods -l app=${node} | grep -P '\s+([1-9]+)\/\1\s+')
     kubectl get pods -l app=${node}
     sleep 30
+    # Uncomment to short-cut the readiness check for slow-to-ready containers
+    # ready="true"
   done
 done
