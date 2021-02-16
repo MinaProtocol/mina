8becd3457aa172847401a88cfd222b1df0f095b1
mrmr1993 <mr_1993@hotmail.co.uk>
2021-02-16 22:40:43 +0000

    Migrate required circleci tests to buildkite

---
+++ buildkite/scripts/old_integration_tests/test_postake_split.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/home/opam/.cargo/bin:/usr/lib/go/bin:$PATH
+export GO=/usr/lib/go/bin/go
+
+make libp2p_helper
+./scripts/test.py run --non-interactive --collect-artifacts --yes "test_postake_split:coda-shared-prefix-multiproducer-test -num-block-producers 2"
---
+++ buildkite/scripts/old_integration_tests/test_shared_state.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/home/opam/.cargo/bin:/usr/lib/go/bin:$PATH
+export GO=/usr/lib/go/bin/go
+
+make libp2p_helper
+./scripts/test.py run --no-build --non-interactive --collect-artifacts --yes "dev:coda-shared-state-test"
