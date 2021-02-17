63f4b4ca0eb651e0851c737cea835a9f981c66c2
Ahmad Wilson <zer0ne.io.x@gmail.com>
2021-02-17 10:09:01 -0500

    modify integration-test-framework required terraform version

--- src/lib/integration_test_cloud_engine/coda_automation.ml
+++ src/lib/integration_test_cloud_engine/coda_automation.ml
@@ -246,7 +246,7 @@ module Network_config = struct
   let to_terraform network_config =
     let open Terraform in
     [ Block.Terraform
-        { Block.Terraform.required_version= "~> 0.14.5"
+        { Block.Terraform.required_version= ">= 0.12.0"
         ; backend=
             Backend.S3
               { Backend.S3.key=
