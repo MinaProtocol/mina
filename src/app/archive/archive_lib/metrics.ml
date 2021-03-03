031c1efc8897d4b19f671d3ed720a434ff7ffaf9
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2021-03-02 19:07:17 -0800

    trying to fix count(*) shown as comments

--- src/app/archive/archive_lib/metrics.ml
+++ src/app/archive/archive_lib/metrics.ml
@@ -21,7 +21,7 @@ module Missing_blocks = struct
     Caqti_request.find Caqti_type.unit Caqti_type.int
       (Core_kernel.sprintf
          {sql| 
-        SELECT count(*) 
+        SELECT count( * )
         FROM (SELECT h::int FROM generate_series(GREATEST(1, (select max(height) from blocks)-2000) , (select max(height) from blocks)) h
         LEFT JOIN blocks b 
         ON h = b.height where b.height is null) as v
@@ -42,7 +42,7 @@ module Unparented_blocks = struct
   let query =
     Caqti_request.find Caqti_type.unit Caqti_type.int
       {sql|
-           SELECT count(*) FROM blocks
+           SELECT count( * ) FROM blocks
            WHERE parent_id IS NULL
       |sql}
 
