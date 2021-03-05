4f011681553b4b01eb26a2b76c9fc85073ba5cf5
Paul Steckler <steck@stecksoft.com>
2021-03-05 15:39:49 -0800

    Remove pending key check, dummy keys

--- src/app/genesis_ledger_from_tsv/genesis_ledger_from_tsv.ml
+++ src/app/genesis_ledger_from_tsv/genesis_ledger_from_tsv.ml
@@ -28,9 +28,6 @@ let add_account pk =
 
 let valid_pk pk = Or_error.is_ok @@ Public_key.Compressed.of_base58_check pk
 
-let is_pending pk =
-  List.mem ["pending"; "finoa"; "topsecret"] pk ~equal:String.Caseless.equal
-
 let no_delegatee pk = String.is_empty pk || String.equal pk "0"
 
 let slot_duration_ms =
@@ -84,22 +81,10 @@ let generate_missing_delegate_accounts ~logger =
 
 let runtime_config_account ~logger ~wallet_pk ~amount ~initial_min_balance
     ~cliff_time_months ~cliff_amount ~unlock_frequency ~unlock_amount
-    ~delegatee_pk ~dummy_pks =
+    ~delegatee_pk =
   [%log info] "Processing record for $wallet_pk"
     ~metadata:[("wallet_pk", `String wallet_pk)] ;
-  let pk, dummy_pks' =
-    if is_pending wallet_pk then
-      match dummy_pks with
-      | pk :: tl ->
-          (* make sure dummy key isn't same as any real key *)
-          let pk_str = Public_key.Compressed.to_base58_check pk in
-          if String.Table.mem accounts_tbl pk_str then
-            failwith "Dummy key found among real keys" ;
-          (Some pk_str, tl)
-      | _ ->
-          failwith "Ran out of dummy public keys"
-    else (Some wallet_pk, dummy_pks)
-  in
+  let pk = Some wallet_pk in
   let balance = Currency.Balance.of_formatted_string amount in
   let initial_minimum_balance =
     (* if omitted in the TSV, use balance *)
@@ -133,14 +118,13 @@ let runtime_config_account ~logger ~wallet_pk ~amount ~initial_min_balance
     (* 0 or empty string denotes "no delegation" *)
     if no_delegatee delegatee_pk then None else Some delegatee_pk
   in
-  ( { Runtime_config.Json_layout.Accounts.Single.default with
-      pk
-    ; balance
-    ; timing
-    ; delegate }
-  , dummy_pks' )
+  { Runtime_config.Json_layout.Accounts.Single.default with
+    pk
+  ; balance
+  ; timing
+  ; delegate }
 
-let account_of_tsv ~logger tsv dummy_pks =
+let account_of_tsv ~logger tsv =
   match String.split tsv ~on:'\t' with
   | "skip" :: _ ->
       None
@@ -155,7 +139,7 @@ let account_of_tsv ~logger tsv dummy_pks =
       Some
         (runtime_config_account ~logger ~wallet_pk ~amount ~initial_min_balance
            ~cliff_time_months ~cliff_amount ~unlock_frequency ~unlock_amount
-           ~delegatee_pk ~dummy_pks)
+           ~delegatee_pk)
   | _ ->
       (* should not occur, we've already validated the record *)
       failwithf "TSV line does not contain expected number of fields: %s" tsv
@@ -163,9 +147,8 @@ let account_of_tsv ~logger tsv dummy_pks =
 
 let validate_fields ~wallet_pk ~amount ~initial_min_balance ~cliff_time_months
     ~cliff_amount ~unlock_frequency ~unlock_amount ~delegatee_pk =
-  let pending_wallet_pk = is_pending wallet_pk in
-  let valid_wallet_pk = pending_wallet_pk || valid_pk wallet_pk in
-  let not_duplicate_wallet_pk = pending_wallet_pk || add_account wallet_pk in
+  let valid_wallet_pk = valid_pk wallet_pk in
+  let not_duplicate_wallet_pk = add_account wallet_pk in
   let valid_amount = valid_mina_amount amount in
   let valid_init_min_balance =
     String.is_empty initial_min_balance
@@ -267,29 +250,21 @@ let main ~tsv_file ~output_file () =
     In_channel.with_file tsv_file ~f:(fun in_channel ->
         [%log info] "Opened TSV file $tsv_file for translation"
           ~metadata:[("tsv_file", `String tsv_file)] ;
-        let rec go accounts num_accounts dummy_pks =
+        let rec go accounts num_accounts =
           match In_channel.input_line in_channel with
           | Some line -> (
               let underscored_line = remove_commas line in
-              match account_of_tsv ~logger underscored_line dummy_pks with
-              | Some (account, dummy_pks') ->
-                  go (account :: accounts) (num_accounts + 1) dummy_pks'
+              match account_of_tsv ~logger underscored_line with
+              | Some account ->
+                  go (account :: accounts) (num_accounts + 1)
               | None ->
-                  go accounts num_accounts dummy_pks )
+                  go accounts num_accounts )
           | None ->
               (List.rev accounts, num_accounts)
         in
         (* skip first line *)
         let _headers = In_channel.input_line in_channel in
-        let num_dummy_pks = 2000 in
-        let dummy_pks =
-          Quickcheck.random_value
-            (Quickcheck.Generator.list_with_length num_dummy_pks
-               Public_key.Compressed.gen)
-        in
-        if List.contains_dup dummy_pks ~compare:Public_key.Compressed.compare
-        then failwith "Dummy keys contains a duplicate" ;
-        go [] 0 dummy_pks )
+        go [] 0 )
   in
   [%log info] "Processed %d records" num_accounts ;
   let generated_accounts, num_generated =
