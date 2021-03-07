b32959b947587f5a121596ad50cedc9fbde23280
Helena Li <helena.li0x1@gmail.com>
2021-03-06 19:35:14 -0800

    remove accidental comments

--- src/lib/integration_test_cloud_engine/kubernetes_network.ml
+++ src/lib/integration_test_cloud_engine/kubernetes_network.ml
@@ -6,32 +6,6 @@ open Integration_test_lib
 [@@@coverage exclude_file]
 
 module Node = struct
-  (* 
-  type network_keypair =
-    { keypair: Keypair.t
-    ; secret_name: string
-    ; public_key_file: string
-    ; private_key_file: string }
-  [@@deriving to_yojson]
-
-
-  let create_network_keypair ~keypair ~secret_name =
-    let open Keypair in
-    let public_key_file =
-      Public_key.Compressed.to_base58_check
-        (Public_key.compress keypair.public_key)
-      ^ "\n"
-    in
-    let private_key_file =
-      let plaintext =
-        Bigstring.to_bytes (Private_key.to_bigstring keypair.private_key)
-      in
-      let password = Bytes.of_string "naughty blue worm" in
-      Secrets.Secret_box.encrypt ~plaintext ~password
-      |> Secrets.Secret_box.to_yojson |> Yojson.Safe.to_string
-    in
-    {keypair; secret_name; public_key_file; private_key_file} *)
-
   type t =
     { testnet_name: string
     ; cluster: string
