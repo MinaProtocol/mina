5ebc77c677ef0a5a406b3b80174210f67bb614dc
mrmr1993 <mr_1993@hotmail.co.uk>
2021-03-09 00:51:35 +0000

    Fix up client sdk compilation

--- src/lib/rosetta_lib/transaction.ml
+++ src/lib/rosetta_lib/transaction.ml
@@ -14,6 +14,7 @@ module Field = Snark_params_nonconsensus.Field
 module Mina_base = Mina_base_nonconsensus
 module Hex = Hex_nonconsensus.Hex
 module Unsigned_extended = Unsigned_extended_nonconsensus.Unsigned_extended
+module Signature_lib = Signature_lib_nonconsensus
 
 [%%endif]
 
