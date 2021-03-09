a29d4207d1bac6faea8ed4811d2386ce260fe1e8
Evan Shapiro <evanshapiro@MacBook-Pro-2.local>
2021-03-08 20:08:46 -1000

    switch to new new tfstate

--- automation/terraform/testnets/devnet2/main.tf
+++ automation/terraform/testnets/devnet2/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.14.0"
   backend "s3" {
-    key     = "terraform-devnet2.tfstate"
+    key     = "terraform-devnet2-2.tfstate"
     encrypt = true
     region  = "us-west-2"
     bucket  = "o1labs-terraform-state"
