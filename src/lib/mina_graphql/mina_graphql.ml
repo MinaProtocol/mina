d1410a9e47769a45cfb76f3f21c0cab1911d0e1c
mrmr1993 <mr_1993@hotmail.co.uk>
2021-03-10 19:05:32 +0000

    Add GraphQL command to convert rosetta-format transactions for GraphQL

--- src/lib/mina_graphql/mina_graphql.ml
+++ src/lib/mina_graphql/mina_graphql.ml
@@ -3263,6 +3263,20 @@ module Queries = struct
         in
         Signed_command.check_signature user_command )
 
+  let convert_rosetta_transaction =
+    io_field "convertRosettaTransaction"
+      ~doc:"Convert a transaction from rosetta format"
+      ~typ:(non_null Types.Payload.send_rosetta_transaction)
+      ~args:Arg.[arg "input" ~typ:(non_null Types.Input.rosetta_transaction)]
+      ~resolve:(fun _ () signed_command ->
+        return
+          (Ok
+             (Types.UserCommand.mk_user_command
+                { With_hash.data= signed_command
+                ; hash=
+                    Transaction_hash.hash_command
+                      (User_command.Signed_command signed_command) })) )
+
   let commands =
     [ sync_state
     ; daemon_status
@@ -3289,7 +3303,8 @@ module Queries = struct
     ; genesis_constants
     ; time_offset
     ; next_available_token
-    ; validate_payment ]
+    ; validate_payment
+    ; convert_rosetta_transaction ]
 end
 
 let schema =
