2088df29f5e6f78fe269e366847bec72ad4ad87b
Nathan Holland <nholland94@gmail.com>
2021-03-18 15:24:12 -0700

    Address review comments

--- src/app/libp2p_helper/src/codanet.go
+++ src/app/libp2p_helper/src/codanet.go
@@ -501,7 +501,11 @@ func (h *Helper) handlePxStreams(s network.Stream) {
 	}
 
 	for _, p := range peers {
-		h.pxDiscoveries <- p
+		select {
+		case h.pxDiscoveries <- p:
+		default:
+			logger.Debugf("peer discoveries channel full; dropping peer %v", p)
+		}
 	}
 }
 
@@ -652,7 +656,7 @@ func MakeHelper(ctx context.Context, listenOn []ma.Multiaddr, externalAddr ma.Mu
 		return h, nil
 	}
 
-	h.pxDiscoveries = make(chan peer.AddrInfo)
+	h.pxDiscoveries = make(chan peer.AddrInfo, maxConnections * 3)
 	for w := 0; w < numPxConnectionWorkers; w++ {
 		go h.pxConnectionWorker()
 	}
