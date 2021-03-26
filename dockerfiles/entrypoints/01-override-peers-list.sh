e4ee7ee4b4851524dc57c9d6bc90fb24a4a19646
Ahmad Wilson <zer0ne.io.x@gmail.com>
2021-03-26 17:17:12 -0400

    move peer list override file to tmp DIR

--- dockerfiles/entrypoints/01-override-peers-list.sh
+++ dockerfiles/entrypoints/01-override-peers-list.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 if [[ -n ${PEERS_LIST_OVERRIDE} ]]; then
     echo "Peers list override, ${PEERS_LIST_OVERRIDE}, provided. Overriding peers list..."
 
-    curl ${PEERS_LIST_OVERRIDE} -o /root/peers.txt
+    curl ${PEERS_LIST_OVERRIDE} -o /tmp/peers.txt
 fi
