263682f9942a1f4d79aa5f5ef1a6c436a79c4d4c
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2021-04-01 17:06:31 -0400

    use the old context for create-stream

--- src/app/libp2p_helper/src/libp2p_helper/main.go
+++ src/app/libp2p_helper/src/libp2p_helper/main.go
@@ -1234,7 +1234,7 @@ func (m *getPeerNodeStatusMsg) run(app *app) (interface{}, error) {
 
 	// Open a "get node status" stream on m.PeerID,
 	// block until you can read the response, return that.
-	s, err := app.P2p.Host.NewStream(ctx, addrInfo.ID, codanet.NodeStatusProtocolID)
+	s, err := app.P2p.Host.NewStream(app.Ctx, addrInfo.ID, codanet.NodeStatusProtocolID)
 	ctx, _ := context.WithTimeout(app.Ctx, codanet.NodeStatusTimeout)
 	if err != nil {
 		app.P2p.Logger.Error("failed to open stream: ", err)
