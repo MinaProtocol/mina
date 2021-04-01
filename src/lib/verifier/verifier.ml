e72ffd09fc89745490947f1870c728d27dd2802b
mrmr1993 <mr_1993@hotmail.co.uk>
2021-04-01 02:06:13 +0100

    Use Base.am_testing for more accurate inline test detection

--- src/lib/verifier/verifier.ml
+++ src/lib/verifier/verifier.ml
@@ -3,12 +3,12 @@ module Prod = Prod
 module Dummy = Dummy
 
 let m =
-  if Core_kernel.am_running_inline_test then
+  if Base__Import.am_testing then
     (* Spawning a process using [Rpc_parallel] calls the current binary with a
        particular set of arguments. Unfortunately, unit tests use the inline
-       test binary, which doesn't support these arguments, and so we're not
-       able to use these [Rpc_parallel] calls. Instead, we detect this and call
-       out to the dummy verifier instead.
+       test binary -- which doesn't support these arguments -- and so we're not
+       able to use these [Rpc_parallel] calls. Here we detect this and call out
+       to the dummy verifier instead.
        The implementation of dummy is equivalent to the one with
        [proof_level <> Full], so this should make no difference. Inline tests
        shouldn't be run with [proof_level = Full].
