bf4d1c38b5ed1bd5b3c5e198b3bfe8ee1b2f12bb
Paul Steckler <steck@stecksoft.com>
2021-04-01 17:55:38 -0700

    Swap balances for combined fee transfers

--- src/app/archive/archive_lib/processor.ml
+++ src/app/archive/archive_lib/processor.ml
@@ -1174,6 +1174,10 @@ module Block = struct
                 let fee_transfers =
                   Mina_base.Fee_transfer.to_numbered_list fee_transfer_bundled
                 in
+                (* balances.receiver1_balance is for receiver of head of fee_transfers
+                   balances.receiver2_balance, if it exists, is for receiver of
+                     next element of fee_transfers
+                *)
                 let%bind fee_transfer_infos =
                   deferred_result_list_fold fee_transfers ~init:[]
                     ~f:(fun acc (secondary_sequence_no, fee_transfer) ->
@@ -1185,11 +1189,12 @@ module Block = struct
                       (id, secondary_sequence_no, fee_transfer.receiver_pk)
                       :: acc )
                 in
-                let fee_transfer_infos =
+                (* the fold reverses the order of the infos from the fee transfers *)
+                let fee_transfer_infos_with_balances =
                   match fee_transfer_infos with
                   | [id] ->
                       [(id, balances.receiver1_balance)]
-                  | [id1; id2] ->
+                  | [id2; id1] ->
                       [ (id1, balances.receiver1_balance)
                       ; (id2, Option.value_exn balances.receiver2_balance) ]
                   | _ ->
@@ -1198,7 +1203,8 @@ module Block = struct
                          transfer transaction"
                 in
                 let%map () =
-                  deferred_result_list_fold fee_transfer_infos ~init:()
+                  deferred_result_list_fold fee_transfer_infos_with_balances
+                    ~init:()
                     ~f:(fun ()
                        ( (fee_transfer_id, secondary_sequence_no, receiver_pk)
                        , balance )
