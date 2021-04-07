e999d78458571da003fcbad9dac05e391a4ee81c
Paul Steckler <steck@stecksoft.com>
2021-04-07 13:40:47 -0700

    Add replayer, swapper to archive artifact

--- buildkite/scripts/ci-archive-release.sh
+++ buildkite/scripts/ci-archive-release.sh
@@ -6,3 +6,5 @@ artifact-cache-helper.sh _build/default/src/app/archive/archive.exe
 artifact-cache-helper.sh _build/default/src/app/archive_blocks/archive_blocks.exe
 artifact-cache-helper.sh _build/default/src/app/extract_blocks/extract_blocks.exe
 artifact-cache-helper.sh _build/default/src/app/missing_blocks_auditor/missing_blocks_auditor.exe
+artifact-cache-helper.sh _build/default/src/app/replayer/replayer.exe
+artifact-cache-helper.sh _build/default/src/app/replayer/swap_bad_balances.exe
