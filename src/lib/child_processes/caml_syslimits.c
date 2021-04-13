0e20b408bec02dcac2cbd30cd572e3cfd1a212a8
Paul Steckler <steck@stecksoft.com>
2021-04-12 17:15:43 -0700

    C comments, not OCaml

--- src/lib/child_processes/caml_syslimits.c
+++ src/lib/child_processes/caml_syslimits.c
@@ -1,9 +1,9 @@
-(* caml_syslimits.c *)
+/* caml_syslimits.c */
 
 #include <limits.h>
 #include <caml/mlvalues.h>
 
-(* maximum length of a file path in the OCaml compiler *)
+/* maximum length of a file path in the OCaml compiler */
 CAMLprim value caml_syslimits_path_max() {
   return Val_int(PATH_MAX);
 }
