cbea19329e6e3ac5c794780fad2325a3185b0a0c
Izaak Meckler <ihmeckler@gmail.coom>
2021-04-21 16:03:35 -0700

    updated transaction hash type

--- src/lib/mina_base/transaction_hash.mli
+++ src/lib/mina_base/transaction_hash.mli
@@ -32,6 +32,13 @@ module User_command_with_valid_signature : sig
   module Stable : sig
     [@@@no_toplevel_latest_type]
 
+    module V2 : sig
+      type t =
+        private
+        (User_command.Valid.Stable.V2.t, hash) With_hash.Stable.V1.t
+      [@@deriving sexp, compare, hash, to_yojson]
+    end
+
     module V1 : sig
       type t =
         private
@@ -63,6 +70,11 @@ module User_command : sig
   [%%versioned:
   module Stable : sig
     [@@@no_toplevel_latest_type]
+
+    module V2 : sig
+      type t = private (User_command.Stable.V2.t, hash) With_hash.Stable.V1.t
+      [@@deriving sexp, compare, hash, to_yojson]
+    end
 
     module V1 : sig
       type t = private (User_command.Stable.V1.t, hash) With_hash.Stable.V1.t
