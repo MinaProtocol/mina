254b72b83ba182366edc0c3f61088b0116064904
Nathan Holland <nholland94@gmail.com>
2021-04-28 12:03:31 -0700

    Make handleStreamReads compatible with daemon

--- src/app/libp2p_helper/src/libp2p_helper/main.go
+++ src/app/libp2p_helper/src/libp2p_helper/main.go
@@ -687,7 +687,11 @@ func handleStreamReads(app *app, stream net.Stream, idx int) {
 		for {
 			length, err := readLEB128ToUint64(r)
 			if err == io.EOF {
-				continue
+				app.writeMsg(streamReadCompleteUpcall{
+					Upcall:    "streamReadComplete",
+					StreamIdx: idx,
+				})
+				return
 			} else if err != nil {
 				app.writeMsg(streamLostUpcall{
 					Upcall:    "streamLost",
@@ -698,6 +702,7 @@ func handleStreamReads(app *app, stream net.Stream, idx int) {
 			}
 
 			if length == 0 {
+				// should this be an error? we could be skipping null bytes like this?
 				continue
 			}
 
@@ -715,10 +720,6 @@ func handleStreamReads(app *app, stream net.Stream, idx int) {
 			app.writeMsg(incomingMsgUpcall{
 				Upcall:    "incomingStreamMsg",
 				Data:      codaEncode(msgBytes),
-				StreamIdx: idx,
-			})
-			app.writeMsg(streamReadCompleteUpcall{
-				Upcall:    "streamReadComplete",
 				StreamIdx: idx,
 			})
 		}
