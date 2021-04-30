69963ed5f913d6339522ed9507f85276245c7a24
Helena Li <helena.li0x1@gmail.com>
2021-04-29 18:23:04 -0700

    what is bash

--- dockerfiles/scripts/cron_job_dump_ledger.sh
+++ dockerfiles/scripts/cron_job_dump_ledger.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/root/init_mina.sh daemon --generate-genesis-proof true --peer-list-url https://storage.googleapis.com/mina-seed-lists/mainnet_seeds.txt --background
+mina daemon --generate-genesis-proof true --peer-list-url https://storage.googleapis.com/mina-seed-lists/mainnet_seeds.txt --background
 
-sleep 480 #wait 8 minutes for the node to, hopefully, come online anbd fully sync
+sleep 480 #wait 8 minutes for the node to, hopefully, come online and fully sync
 
 while true; do
   mina ledger export staking-epoch-ledger > staking_epoch_ledger.txt
@@ -18,15 +18,15 @@ done
 
 STAKING_LEDGER_HASH = mina ledger hash --ledger-file staking_epoch_ledger.txt
 
-DATE = "$(date +'%F_%H%M')"
-mv ./staking_epoch_ledger.txt ./$(DATE)_staking_epoch_ledger_$(staking_ledger_hash).txt
+DATE = "$(date +%F_%H%M)"
+mv ./staking_epoch_ledger.txt ./"$DATE"_staking_epoch_ledger_"$STAKING_LEDGER_HASH".txt
 
-# echo STAKING_LEDGER > $(DATE)_staking_epoch_ledger_$(STAKING_LEDGER_HASH).txt
+# echo STAKING_LEDGER > "$DATE"_staking_epoch_ledger_$(STAKING_LEDGER_HASH).txt
 
 mina ledger export next-epoch-ledger > next_epoch_ledger.txt
 NEXT_LEDGER_HASH = mina ledger hash --ledger-file next_epoch_ledger.txt
-mv ./next_epoch_ledger.txt ./$(DATE)_next_epoch_ledger_$(next_ledger_hash).txt
-# echo NEXT_LEDGER > $(DATE)_next_epoch_ledger_$(STAKING_LEDGER_HASH).txt
+mv ./next_epoch_ledger.txt ./"$DATE"_next_epoch_ledger_"$NEXT_LEDGER_HASH".txt
+# echo NEXT_LEDGER > "$DATE"_next_epoch_ledger_$(STAKING_LEDGER_HASH).txt
 
 
 
