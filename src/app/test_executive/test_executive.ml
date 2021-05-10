ad81b2f0b64307496efeef7ff63d008476626177
Nathan Holland <nholland94@gmail.com>
2021-05-10 10:18:56 -0700

    Consolidate more integration tests

--- src/app/test_executive/test_executive.ml
+++ src/app/test_executive/test_executive.ml
@@ -40,9 +40,7 @@ let engines : engine list =
 
 let tests : test list =
   [ ("reliability", (module Reliability_test.Make : Intf.Test.Functor_intf))
-  ; ("send-payment", (module Send_payment_test.Make : Intf.Test.Functor_intf))
-  ; ( "pmt-timed-accts"
-    , (module Payments_timed_accounts_test.Make : Intf.Test.Functor_intf) )
+  ; ("payments", (module Payments_test.Make : Intf.Test.Functor_intf))
     (*
   ; ( "bp-timed-accts"
     , (module Block_production_timed_accounts_test.Make : Intf.Test.Functor_intf) )
