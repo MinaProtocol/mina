5c0c9ce56f33bcf50a5d9f80d1beea08e7b087ed
Andrew Trainor <andrew@o1labs.org>
2021-05-16 19:15:48 -0400

    enable-alpha-builds Update deb-builder stage to more closely match rebuild-deb.sh

---
+++ scripts/build-deb-builder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eou pipefail
+
+BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+DOCKER_BRANCH=$(echo "${BRANCH}" | sed 's!/!-!; s!_!-!g')
+CODENAME="${CODENAME:=buster}"
+TAG="${CODENAME}-${DOCKER_BRANCH}"
+echo "--- Building and pushing gcr.io/o1labs-192920/mina-deb-builder:${TAG}"
+
+time cat dockerfiles/Dockerfile-deb-builder | docker build \
+  --target builder \
+  --build-args CODENAME=${CODENAME} \
+  --build-args OPAM_BRANCH=${BRANCH} \
+  --build-args MINA_BRANCH=${BRANCH} \
+  -t gcr.io/o1labs-192920/mina-deb-builder:${TAG} -
+
+docker push gcr.io/o1labs-192920/mina-deb-builder:${TAG}
