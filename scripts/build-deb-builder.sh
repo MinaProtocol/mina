a2a9db108cd680abf148abbe5fdaf029fb0835a0
Andrew Trainor <andrew@o1labs.org>
2021-05-16 20:28:18 -0400

    enable-alpha-builds swap codename and release

--- scripts/build-deb-builder.sh
+++ scripts/build-deb-builder.sh
@@ -12,7 +12,8 @@ time cat dockerfiles/Dockerfile-deb-builder | docker build \
   --target builder \
   --build-arg CODENAME=${CODENAME} \
   --build-arg OPAM_BRANCH=${BRANCH} \
-  --build-arg MINA_BRANCH=$(git rev-parse HEAD) \
+  --build-arg MINA_BRANCH=${BRANCH} \
+  --build-arg MINA_COMMIT=$(git rev-parse HEAD) \
   -t gcr.io/o1labs-192920/mina-deb-builder:${TAG} -
 
 docker push gcr.io/o1labs-192920/mina-deb-builder:${TAG}
