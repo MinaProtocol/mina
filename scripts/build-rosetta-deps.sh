f7f77ca04a94e70e4ae3e6cc09c329e8e2e52250
Andrew Trainor <andrew@o1labs.org>
2021-05-18 19:44:05 -0400

    enable-alpha-builds Build rosetta in stretch also

--- scripts/build-rosetta-deps.sh
+++ scripts/build-rosetta-deps.sh
@@ -8,6 +8,7 @@ echo "--- Building and pushing gcr.io/o1labs-192920/mina-rosetta-build-deps:$TAG
 
 time cat dockerfiles/Dockerfile-rosetta | docker build \
   --target build-deps \
+  --no-cache \
   -t gcr.io/o1labs-192920/mina-rosetta-build-deps:$TAG -
 
 docker push gcr.io/o1labs-192920/mina-rosetta-build-deps:$TAG
