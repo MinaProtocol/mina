5398c4cd70d7807d169bcc2c3640fd24a8a29af9
Andrew Trainor <andrew@o1labs.org>
2021-05-19 03:49:45 -0400

    enable-alpha-builds Fix ArchiveNodeUnitTest so that opam is found and postgresql installs

--- buildkite/scripts/setup-database-for-archive-node.sh
+++ buildkite/scripts/setup-database-for-archive-node.sh
@@ -6,7 +6,11 @@ user=$1
 password=$2
 db=$3
 
-sudo apt-get update -y && sudo apt-get install -y postgresql
+
+# Workaround terrible postgresql package requirements with man
+mkdir /usr/share/man/man7 /usr/share/man/man1
+
+sudo apt-get update -y && sudo apt-get install -y apt-utils man postgresql
 sudo service postgresql start
 sudo -u postgres psql -c "CREATE USER ${user} WITH SUPERUSER PASSWORD '${password}';"
 sudo -u postgres createdb -O $user $db
