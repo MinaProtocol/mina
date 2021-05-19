7db49c35fbcbcf0096cbfda84c876bc4bf44730d
Andrew Trainor <andrew@o1labs.org>
2021-05-19 04:09:20 -0400

    enable-alpha-builds Move postgres setup into toolchain build

--- buildkite/scripts/setup-database-for-archive-node.sh
+++ buildkite/scripts/setup-database-for-archive-node.sh
@@ -7,10 +7,6 @@ password=$2
 db=$3
 
 
-# Workaround terrible postgresql package requirements with man
-mkdir /usr/share/man/man7 /usr/share/man/man1
-
-sudo apt-get update -y && sudo apt-get install -y apt-utils man postgresql
 sudo service postgresql start
 sudo -u postgres psql -c "CREATE USER ${user} WITH SUPERUSER PASSWORD '${password}';"
 sudo -u postgres createdb -O $user $db
