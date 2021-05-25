97b0ebd3cb5f63bb779a29e4e470810f57b3026c
Izaak Meckler <ihmeckler@gmail.coom>
2021-05-25 09:42:35 -0700

    remove Both control constructor

--- src/lib/mina_base/control.ml
+++ src/lib/mina_base/control.ml
@@ -13,9 +13,6 @@ module Stable = struct
     type t =
       | Proof of Pickles.Side_loaded.Proof.Stable.V1.t
       | Signature of Signature.Stable.V1.t
-      | Both of
-          { signature: Signature.Stable.V1.t
-          ; proof: Pickles.Side_loaded.Proof.Stable.V1.t }
       | None_given
     [@@deriving sexp, eq, yojson, hash, compare]
 
@@ -31,7 +28,6 @@ module Stable = struct
     type t =
       | Proof of unit
       | Signature of Signature.Stable.V1.t
-      | Both of {signature: Signature.Stable.V1.t; proof: unit}
       | None_given
     [@@deriving sexp, eq, yojson, hash, compare]
 
@@ -42,16 +38,13 @@ end]
 [%%endif]
 
 module Tag = struct
-  type t = Proof | Signature | Both | None_given
-  [@@deriving eq, compare, sexp]
+  type t = Proof | Signature | None_given [@@deriving eq, compare, sexp]
 end
 
 let tag : t -> Tag.t = function
   | Proof _ ->
       Proof
   | Signature _ ->
-      Both
-  | Both _ ->
-      Both
+      Signature
   | None_given ->
       None_given
