3b33450f9e8b5ad47e1b7490b461951e40f30091
Izaak Meckler <ihmeckler@gmail.coom>
2021-05-30 13:39:48 -0400

    reformat

--- src/lib/verifier/prod.ml
+++ src/lib/verifier/prod.ml
@@ -46,8 +46,8 @@ module Worker_state = struct
           (let bc_vk = Precomputed_values.blockchain_verification ()
            and tx_vk = Precomputed_values.transaction_verification () in
            let module M = struct
-             let verify_commands (cs : User_command.Verifiable.t list) : _ list Deferred.t
-                 =
+             let verify_commands (cs : User_command.Verifiable.t list) :
+                 _ list Deferred.t =
                let cs = List.map cs ~f:Common.check in
                let to_verify =
                  List.concat_map cs ~f:(function
