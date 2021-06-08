826a70b784e363905f71fda80e3d411ea51f3b84
mrmr1993 <mr_1993@hotmail.co.uk>
2021-06-08 19:41:37 +0100

    Remove unused bn382 curve bindings

--- src/lib/marlin_plonk_bindings/marlin_plonk_bindings.ml
+++ src/lib/marlin_plonk_bindings/marlin_plonk_bindings.ml
@@ -3,7 +3,6 @@ module Types = Marlin_plonk_bindings_types
 (** Biginteger types *)
 
 module Bigint_256 = Marlin_plonk_bindings_bigint_256
-module Bigint_384 = Marlin_plonk_bindings_bigint_384
 
 (** Finite fields *)
 
@@ -11,8 +10,6 @@ module Pasta_fp = Marlin_plonk_bindings_pasta_fp
 module Pasta_fq = Marlin_plonk_bindings_pasta_fq
 module Tweedle_fp = Marlin_plonk_bindings_tweedle_fp
 module Tweedle_fq = Marlin_plonk_bindings_tweedle_fq
-module Bn_382_fp = Marlin_plonk_bindings_bn_382_fp
-module Bn_382_fq = Marlin_plonk_bindings_bn_382_fq
 
 (* Finite field vectors *)
 
@@ -20,8 +17,6 @@ module Pasta_fp_vector = Marlin_plonk_bindings_pasta_fp_vector
 module Pasta_fq_vector = Marlin_plonk_bindings_pasta_fq_vector
 module Tweedle_fp_vector = Marlin_plonk_bindings_tweedle_fp_vector
 module Tweedle_fq_vector = Marlin_plonk_bindings_tweedle_fq_vector
-module Bn_382_fp_vector = Marlin_plonk_bindings_bn_382_fp_vector
-module Bn_382_fq_vector = Marlin_plonk_bindings_bn_382_fq_vector
 
 (* Groups *)
 
--- src/lib/marlin_plonk_bindings/stubs/src/lib.rs
+++ src/lib/marlin_plonk_bindings/stubs/src/lib.rs
@@ -4,17 +4,12 @@ extern crate libc;
 pub mod caml_pointer;
 /* Bigints */
 pub mod bigint_256;
-pub mod bigint_384;
 /* Fields */
-pub mod bn_382_fp;
-pub mod bn_382_fq;
 pub mod tweedle_fp;
 pub mod tweedle_fq;
 pub mod pasta_fp;
 pub mod pasta_fq;
 /* Field vectors */
-pub mod bn_382_fp_vector;
-pub mod bn_382_fq_vector;
 pub mod tweedle_fp_vector;
 pub mod tweedle_fq_vector;
 pub mod pasta_fp_vector;
