efe362779333f39a199d5ebff725e57ee9412edf
Joseph Spadavecchia <joseph@redtrie.com>
2021-06-15 16:06:55 +0100

    Handle errors in more secure way

--- src/lib/marlin_plonk_bindings/stubs/src/bigint_256.rs
+++ src/lib/marlin_plonk_bindings/stubs/src/bigint_256.rs
@@ -114,7 +114,7 @@ pub fn caml_bigint_256_to_bytes(x: ocaml::Pointer<BigInteger256>) -> ocaml::Valu
     let x_ptr: *const BigInteger256 = x.as_ref();
     unsafe {
         let mut input_bytes = vec![];
-        (*x_ptr).serialize(&mut input_bytes).expect("serialize failed");
+        (*x_ptr).serialize(&mut input_bytes).expect("caml_bigint_256_to_bytes, serialize failed");
         core::ptr::copy_nonoverlapping(input_bytes.as_ptr(), ocaml::sys::string_val(str), input_bytes.len());
     }
     ocaml::Value(str)
@@ -126,8 +126,10 @@ pub fn caml_bigint_256_of_bytes(x: &[u8]) -> Result<BigInteger256, ocaml::Error>
     if x.len() != len {
         ocaml::Error::failwith("caml_bigint_256_of_bytes")?;
     };
-    let x = BigInteger256::deserialize(&mut &x[..]).expect("deserialize failed");
-    Ok(x)
+    match BigInteger256::deserialize(&mut &x[..]) {
+        Ok(b) => Ok(b),
+        Err(_) => Err(ocaml::Error::invalid_argument("caml_bigint_256_of_bytes, deserialize failed").err().unwrap())
+    }
 }
 
 #[ocaml::func]
