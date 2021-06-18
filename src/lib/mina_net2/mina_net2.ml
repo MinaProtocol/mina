de5fe2e2911ccbeeeffc9ce53198c69024b49a73
Paul Steckler <steck@stecksoft.com>
2021-06-18 15:20:08 -0700

    Use MINA_LIBP2P_HELPER_PATH

--- src/lib/mina_net2/mina_net2.ml
+++ src/lib/mina_net2/mina_net2.ml
@@ -1688,8 +1688,8 @@ let create ~all_peers_seen_metric ~on_unexpected_termination ~logger ~pids
         (Error.tag e
            ~tag:
              "Could not start libp2p_helper. If you are a dev, did you forget \
-              to `make libp2p_helper` and set CODA_LIBP2P_HELPER_PATH? Try \
-              CODA_LIBP2P_HELPER_PATH=$PWD/src/app/libp2p_helper/result/bin/libp2p_helper.")
+              to `make libp2p_helper` and set MINA_LIBP2P_HELPER_PATH? Try \
+              MINA_LIBP2P_HELPER_PATH=$PWD/src/app/libp2p_helper/result/bin/libp2p_helper.")
   | Ok subprocess ->
       Child_processes.register_process ~termination_expected:true pids
         subprocess Libp2p_helper ;
