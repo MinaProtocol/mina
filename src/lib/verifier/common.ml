4835c70ad80b1b945a8815475ab5e9e72ff2bf71
Izaak Meckler <ihmeckler@gmail.coom>
2021-07-16 13:31:05 -0700

    update verifier

--- src/lib/verifier/common.ml
+++ src/lib/verifier/common.ml
@@ -12,79 +12,67 @@ let check :
           `Invalid
       | Some c ->
           `Valid (User_command.Signed_command c) )
-  | Snapp_command (cmd, vks) ->
+  | Parties { fee_payer; other_parties; protocol_state } ->
       with_return (fun { return } ->
-          let payload =
-            lazy
-              Snapp_command.(
-                Payload.(Digested.digest (digested (to_payload cmd))))
-          in
-          let check_signature s pk =
-            if
-              not
-                (Signature_lib.Schnorr.verify s
-                   (Backend.Tick.Inner_curve.of_affine
-                      (Signature_lib.Public_key.decompress_exn pk))
-                   (Random_oracle_input.field (Lazy.force payload)))
-            then return `Invalid
-            else ()
-          in
-          (* TODO: Unify this computation of statement with the code Snapp_statement.of_payload. *)
-          let statement_to_check
-              ( vk
-              , (p : Snapp_command.Party.Authorized.Proved.t)
-              , (other : Snapp_command.Party.Body.t) ) =
-            let statement : Snapp_statement.t =
-              { predicate = p.data.predicate
-              ; body1 = p.data.body
-              ; body2 = other
-              }
+          let commitment =
+            let other_parties_hash =
+              match other_parties with
+              | [] ->
+                  Parties.With_hashes.empty
+              | (_, h) :: _ ->
+                  h
             in
-            match p.authorization with
-            | Signature s ->
-                check_signature s p.data.body.pk ;
-                None
-            | Both { signature; proof } ->
-                check_signature signature p.data.body.pk ;
-                Some (vk, statement, proof)
-            | Proof p ->
-                Some (vk, statement, p)
-            | None_given ->
-                (* TODO: This should probably be an error. *)
-                None
+            Parties.Transaction_commitment.create ~other_parties_hash
+              ~protocol_state_predicate_hash:
+                (Snapp_predicate.Protocol_state.digest protocol_state)
           in
-          let statements_to_check : _ list =
-            List.filter_map ~f:statement_to_check
-              ( match (cmd, vks) with
-              | Proved_proved r, `Two (vk1, vk2) ->
-                  [ (vk1, r.one, r.two.data.body)
-                  ; (vk2, r.two, r.one.data.body)
-                  ]
-              | Proved_signed r, `One vk1 ->
-                  check_signature r.two.authorization r.two.data.body.pk ;
-                  [ (vk1, r.one, r.two.data.body) ]
-              | Proved_empty r, `One vk1 ->
-                  let two =
-                    Option.value_map r.two
-                      ~default:Snapp_command.Party.Body.dummy ~f:(fun two ->
-                        two.data.body)
-                  in
-                  [ (vk1, r.one, two) ]
-              | Signed_signed r, `Zero ->
-                  check_signature r.one.authorization r.one.data.body.pk ;
-                  check_signature r.two.authorization r.two.data.body.pk ;
-                  []
-              | Signed_empty r, `Zero ->
-                  check_signature r.one.authorization r.one.data.body.pk ;
-                  []
-              | Proved_proved _, (`Zero | `One _)
-              | (Proved_signed _ | Proved_empty _), (`Zero | `Two _)
-              | (Signed_signed _ | Signed_empty _), (`One _ | `Two _) ->
-                  failwith "Wrong number of vks" )
+          let check_signature s pk msg =
+            match Signature_lib.Public_key.decompress pk with
+            | None ->
+                return `Invalid
+            | Some pk ->
+                if
+                  not
+                    (Signature_lib.Schnorr.verify s
+                       (Backend.Tick.Inner_curve.of_affine pk)
+                       (Random_oracle_input.field msg))
+                then return `Invalid
+                else ()
           in
-          let v = User_command.Snapp_command cmd in
-          match statements_to_check with
+          check_signature fee_payer.authorization fee_payer.data.body.pk
+            (Parties.Transaction_commitment.with_fee_payer commitment
+               ~fee_payer_hash:
+                 (Party.Predicated.digest
+                    (Party.Predicated.of_signed fee_payer.data))) ;
+          let valid_assuming =
+            List.filter_map other_parties ~f:(fun ((p, vk_opt), at_party) ->
+                match p.authorization with
+                | Signature s ->
+                    check_signature s p.data.body.pk commitment ;
+                    None
+                | None_given ->
+                    None
+                | Proof pi -> (
+                    match vk_opt with
+                    | None ->
+                        return `Invalid
+                    | Some vk ->
+                        Some
+                          ( vk
+                          , { Snapp_statement.Poly.transaction = commitment
+                            ; at_party
+                            }
+                          , pi ) ))
+          in
+          let v =
+            User_command.Poly.Parties
+              { Parties.fee_payer
+              ; other_parties = List.map other_parties ~f:(fun ((p, _), _) -> p)
+              ; protocol_state
+              }
+          in
+          match valid_assuming with
           | [] ->
               `Valid v
           | _ :: _ ->
-              `Valid_assuming (v, statements_to_check))
+              `Valid_assuming (v, valid_assuming))
