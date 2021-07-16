5772ea729111fb5cb821f6731e573ae2010aed12
Izaak Meckler <ihmeckler@gmail.coom>
2021-07-16 15:08:30 -0700

    transaction snark work

--- src/lib/transaction_snark_work/transaction_snark_work.ml
+++ src/lib/transaction_snark_work/transaction_snark_work.ml
@@ -6,11 +6,19 @@ module Statement = struct
   module Arg = struct
     [%%versioned
     module Stable = struct
+      module V2 = struct
+        type t = Transaction_snark.Statement.Stable.V2.t One_or_two.Stable.V1.t
+        [@@deriving hash, sexp, compare]
+
+        let to_latest = Fn.id
+      end
+
       module V1 = struct
         type t = Transaction_snark.Statement.Stable.V1.t One_or_two.Stable.V1.t
         [@@deriving hash, sexp, compare]
 
-        let to_latest = Fn.id
+        let to_latest (t : t) : V2.t =
+          One_or_two.map t ~f:Transaction_snark.Statement.Stable.V1.to_latest
       end
     end]
   end
@@ -19,11 +27,22 @@ module Statement = struct
   module Stable = struct
     [@@@no_toplevel_latest_type]
 
+    module V2 = struct
+      type t = Transaction_snark.Statement.Stable.V2.t One_or_two.Stable.V1.t
+      [@@deriving equal, compare, hash, sexp, yojson]
+
+      let to_latest = Fn.id
+
+      type _unused = unit constraint t = Arg.Stable.V2.t
+
+      include Hashable.Make_binable (Arg.Stable.V2)
+    end
+
     module V1 = struct
       type t = Transaction_snark.Statement.Stable.V1.t One_or_two.Stable.V1.t
       [@@deriving equal, compare, hash, sexp, yojson]
 
-      let to_latest = Fn.id
+      let to_latest = Arg.Stable.V1.to_latest
 
       type _unused = unit constraint t = Arg.Stable.V1.t
 
@@ -51,6 +70,18 @@ module Info = struct
   module Stable = struct
     [@@@no_toplevel_latest_type]
 
+    module V2 = struct
+      type t =
+        { statements : Statement.Stable.V2.t
+        ; work_ids : int One_or_two.Stable.V1.t
+        ; fee : Fee.Stable.V1.t
+        ; prover : Public_key.Compressed.Stable.V1.t
+        }
+      [@@deriving compare, sexp, to_yojson]
+
+      let to_latest = Fn.id
+    end
+
     module V1 = struct
       type t =
         { statements : Statement.Stable.V1.t
@@ -60,7 +91,12 @@ module Info = struct
         }
       [@@deriving compare, sexp, to_yojson]
 
-      let to_latest = Fn.id
+      let to_latest (t : t) : V2.t =
+        { statements = Statement.Stable.V1.to_latest t.statements
+        ; work_ids = t.work_ids
+        ; fee = t.fee
+        ; prover = t.prover
+        }
     end
   end]
 
@@ -78,6 +114,17 @@ module T = struct
   module Stable = struct
     [@@@no_toplevel_latest_type]
 
+    module V2 = struct
+      type t =
+        { fee : Fee.Stable.V1.t
+        ; proofs : Ledger_proof.Stable.V2.t One_or_two.Stable.V1.t
+        ; prover : Public_key.Compressed.Stable.V1.t
+        }
+      [@@deriving compare, sexp, yojson]
+
+      let to_latest = Fn.id
+    end
+
     module V1 = struct
       type t =
         { fee : Fee.Stable.V1.t
@@ -86,7 +133,11 @@ module T = struct
         }
       [@@deriving compare, sexp, yojson]
 
-      let to_latest = Fn.id
+      let to_latest (t : t) : V2.t =
+        { fee = t.fee
+        ; proofs = One_or_two.map t.proofs ~f:Ledger_proof.Stable.V1.to_latest
+        ; prover = t.prover
+        }
     end
   end]
 
