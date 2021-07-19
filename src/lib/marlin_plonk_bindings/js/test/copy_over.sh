3c544e9d8e1b69fe270274b45a7f94b4d3d44be0
Brandon Kase <brandon.kase@gmail.com>
2021-07-19 18:47:21 +0200

    Copy over with env package

--- src/lib/marlin_plonk_bindings/js/test/copy_over.sh
+++ src/lib/marlin_plonk_bindings/js/test/copy_over.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 chmod -R +w _build/default/src/lib/marlin_plonk_bindings/js/test
 cp -t _build/default/src/lib/marlin_plonk_bindings/js/test _build/default/src/lib/marlin_plonk_bindings/js/node_js/plonk_wasm*
-nodejs --experimental-wasm-modules --experimental-modules --experimental-wasm-threads -i -r ./_build/default/src/lib/marlin_plonk_bindings/js/test/bindings_js_test.bc.js -e "var bindings = require('./_build/default/src/lib/marlin_plonk_bindings/js/test/bindings_js_test.bc.js'); console.log('Bindings attached to global variable \\'bindings\\'')"
+node --version
+echo '{ "dependencies": { "env": "^0.0.2" } }' > ./_build/default/package.json
+node --experimental-wasm-modules --experimental-modules --experimental-wasm-threads -i -r ./_build/default/src/lib/marlin_plonk_bindings/js/test/bindings_js_test.bc.js -e "var bindings = require('./_build/default/src/lib/marlin_plonk_bindings/js/test/bindings_js_test.bc.js'); console.log('Bindings attached to global variable \\'bindings\\'')"
