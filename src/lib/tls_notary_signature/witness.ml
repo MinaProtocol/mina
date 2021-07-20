c09a965136aa6962825c019fd3120b90c5f64325
Joseph Spadavecchia <joseph@redtrie.com>
2021-07-20 20:08:25 +0100

    Add witness request handler and create unit tests

--- src/lib/tls_notary_signature/witness.ml
+++ src/lib/tls_notary_signature/witness.ml
@@ -1,10 +1,46 @@
+(*
+ * httpsnapps prover witness structure
+ *)
+
 open Core
-open Snarky_backendless
-module Field = Marlin_plonk_bindings.Pasta_fp
+
+(* Import snarky primaitves on top of pasta/vesta plonk plookup backend *)
+module Impl =
+  Snarky.Snark.Run.Make
+    (Zexe_backend.Pasta.Vesta_based_plonk_plookup)
+    (Core.Unit)
+open Impl
+
+(*
+ * Useful shorthands
+ *)
+
+module Field = Impl.Field.Constant
+
+module Bytes = struct
+  type t = int array
+end
+
+module Bits = struct
+  type t = bool list
+end
 
 module Signature = struct
   type t = (Field.t * Field.t) * Field.t
 end
+
+(*
+ * Witness structure
+ *)
+
+module Witness = struct
+  type t =
+    {ciphertext: Bytes.t; key: Bytes.t; iv: Bytes.t; signature: Signature.t}
+end
+
+(*
+ * Interface to access witness data
+ *)
 
 type _ Snarky_backendless.Request.t +=
   | Get_ciphertext : Bytes.t Snarky_backendless.Request.t
@@ -18,14 +54,8 @@ type _ Snarky_backendless.Request.t +=
 type _ Snarky_backendless.Request.t +=
   | Get_signature : Signature.t Snarky_backendless.Request.t
 
-module Witness = struct
-  type t =
-    {ciphertext: Bytes.t; key: Bytes.t; iv: Bytes.t; signature: Signature.t}
-end
-
-let witness_handler (witness : Witness.t) :
-    Snarky_backendless.Request.request -> _ =
- fun (With {request; respond} as r) ->
+let witness_handler (witness : Witness.t) : Impl.request -> _ =
+ fun (With {request; respond}) ->
   match request with
   | Get_ciphertext ->
       respond (Provide witness.ciphertext)
@@ -38,8 +68,11 @@ let witness_handler (witness : Witness.t) :
   | _ ->
       respond Unhandled
 
-let bits_to_bytes (bits : bool list) : int array =
-  let byte_of_bits (bs : bool list) : int =
+(*
+ * Convert list of bits from prover byte-order to array of big-endian bytes
+ *)
+let bits_to_bytes (bits : Bits.t) : Bytes.t =
+  let byte_of_bits (bs : Bits.t) : int =
     List.foldi bs ~init:0 ~f:(fun i acc bit ->
         if bit then acc lor (1 lsl i) else acc )
   in
@@ -47,19 +80,135 @@ let bits_to_bytes (bits : bool list) : int array =
     (List.map ~f:byte_of_bits
        (List.groupi bits ~break:(fun i _ _ -> i mod 8 = 0)))
 
-let bytes_to_bits (bytes : int array) : bool list =
+(*
+ * Convert array of big-endian bytes to list of bits in prover byte-order
+ *)
+let bytes_to_bits (bytes : Bytes.t) : Bits.t =
   List.init
     (8 * Array.length bytes)
     ~f:(fun i ->
-      if bytes.(i / 8) land (1 lsl (7 - (i % 8))) <> 0 then true else false )
+      if bytes.(i / 8) land (1 lsl (i % 8)) <> 0 then true else false )
 
-let%test_module "httpsnapps witness tests" =
-  ( module struct
-    ;;
-    let bits = [false; false; true; true; true; true; true; true] in
-    let bytes : int array = bits_to_bytes bits in
-    Printf.printf "byte = %d\n" bytes.(0) ;
-    let bytes = [|0x01; 0x02; 0x03|] in
-    let bits : bool list = bytes_to_bits bytes in
-    Printf.printf "httpsnapps witness tests\n"
-  end )
+let bytes_transporter num_bytes =
+  Typ.transport ~there:bytes_to_bits ~back:bits_to_bytes
+    (Typ.list ~length:(8 * num_bytes) Boolean.typ)
+
+(*
+ * Unit tests
+*)
+
+let%test "bits_to_bytes test 1" =
+  let bytes : Bytes.t =
+    bits_to_bytes [false; false; true; true; true; true; true; true]
+  in
+  bytes = [|0xfc|]
+
+let%test "bits_to_bytes test 2" =
+  let bytes : Bytes.t =
+    bits_to_bytes
+      [ false
+      ; false
+      ; true
+      ; true
+      ; true
+      ; true
+      ; true
+      ; true
+      ; false
+      ; false
+      ; false
+      ; true
+      ; false
+      ; true
+      ; true
+      ; true
+      ; true
+      ; true
+      ; true
+      ; true
+      ; false
+      ; true
+      ; true
+      ; false ]
+  in
+  bytes = [|0xfc; 0xe8; 0x6f|]
+
+let%test "bytes_to_bits test 1" =
+  let bits : Bits.t = bytes_to_bits [|0xfc|] in
+  bits = [false; false; true; true; true; true; true; true]
+
+let%test "bytes_to_bits test 2" =
+  let bits : Bits.t = bytes_to_bits [|0x01; 0x02; 0x03|] in
+  bits
+  = [ true
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; true
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; true
+    ; true
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false
+    ; false ]
+
+let%test "bytes_transporter 1" =
+  let fe = Field.of_int 1 in
+  let witness : Witness.t =
+    { ciphertext= [|0x01; 0x02; 0x03|]
+    ; key= [|0x01; 0x02; 0x03|]
+    ; iv= [|0x01; 0x02; 0x03|]
+    ; signature= ((fe, fe), fe) }
+  in
+  let get_ciphertext () =
+    (* bytes to prover bits *)
+    Impl.exists (bytes_transporter 3) ~request:(fun () -> Get_ciphertext)
+  in
+  let state =
+    Impl.run_and_check (fun () ->
+        let ct = handle get_ciphertext (witness_handler witness) in
+        fun () ->
+          (* Testing postscript (run as test prover) *)
+          ( As_prover.read (bytes_transporter 3) ct
+          , As_prover.read (Typ.list Boolean.typ ~length:(3 * 8)) ct ) )
+  in
+  let (), (bytes, bits) = Or_error.ok_exn @@ state () in
+  bytes = [|0x01; 0x02; 0x03|]
+  && bits
+     = [ true
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; true
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; true
+       ; true
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false
+       ; false ]
