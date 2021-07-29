dc1e38e5da38778979b861a99122c7405e52daf8
Andrew Trainor <andrew@o1labs.org>
2021-07-29 03:59:29 -0400

    lk86/fix-deb-s3 Revert previous change due to danger

--- buildkite/scripts/publish-deb.sh
+++ buildkite/scripts/publish-deb.sh
@@ -12,6 +12,7 @@ DEBS3='deb-s3 upload '\
 '--s3-region=us-west-2 '\
 '--bucket packages.o1test.net '\
 '--lock '\
+'--preserve-versions '\
 '--cache-control=max-age=120 '
 
 DEBS='_build/mina-*.deb'
