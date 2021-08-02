6dec0d9a4e282b5962a6b3f1c16c0f49d72428ba
Martin Minkov <minkovlmartin@gmail.com>
2021-08-02 10:41:40 -0700

    feat: Basic implementation of http-snapp-server
    Added a basic implementation of the http-snapp-server. This is meant to
    work in conjunction with the `httpsnapp` library which will proxy
    requests from the user to this server.
    
    The server will make a request on behalf of the proxy and sign the
    message with the Mina client-sdk. If there is an error, we simply
    propogate it back to the `httpsnapp` caller.

---
+++ frontend/snapps/http-snapp-server/src/index.js
@@ -0,0 +1,64 @@
+import MinaSDK from '@o1labs/client-sdk';
+import polka from 'polka';
+import fetch from 'node-fetch';
+import send from '@polka/send-type';
+
+const PORT = process.env | 3000;
+const keys = MinaSDK.genKeys();
+
+polka()
+  .get('/', async (req, res) => {
+    // Bad encoding of query
+    if (!'url' in req.query) {
+      send(res, 401);
+    }
+
+    // Parse out URL and format query string
+    const { url, ...params } = req.query;
+    const qs = Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join('&');
+
+    try {
+      const fetchUrl = qs.length === 0 ? url : `${url}?${qs}`;
+      const data = await fetch(fetchUrl, { method: 'GET' });
+      const jsonData = await data.json();
+      const signedData = MinaSDK.signMessage(JSON.stringify(jsonData), keys);
+      send(res, data.status, signedData);
+    } catch (err) {
+      const errorCode = 500;
+      const error = {
+        error: errorCode,
+        payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
+      };
+      send(res, errorCode, error);
+    }
+  })
+  .post('/', async (req, res) => {
+    // Bad encoding of query
+    if (!'url' in req.query) {
+      send(res, 401);
+    }
+
+    const { url, ...params } = req.query;
+
+    try {
+      const data = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(params),
+      });
+      const jsonData = await data.json();
+      const signedData = MinaSDK.signMessage(JSON.stringify(jsonData), keys);
+      send(res, data.status, signedData);
+    } catch (err) {
+      const errorCode = 500;
+      const error = {
+        error: errorCode,
+        payload: JSON.stringify(err, Object.getOwnPropertyNames(err)),
+      };
+      send(res, errorCode, error);
+    }
+  })
+  .listen(PORT, () => {
+    console.log(`> Running on localhost:${PORT}`);
+  });
