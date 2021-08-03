27e9b8d44eef522f4b24e2d90509cbc899ad89b6
Izaak Meckler <ihmeckler@gmail.coom>
2021-08-03 15:10:24 -0700

    update test

--- frontend/snapps/snarkyjs/test/simple.test.ts
+++ frontend/snapps/snarkyjs/test/simple.test.ts
@@ -1,7 +1,20 @@
 import { five } from '../src';
+import { Main, main } from '../src/examples/schnorr_sign';
+
+const timeout = (ms : number) => {
+  return new Promise((resolve, _) => {
+    let wait = setTimeout(() => {
+      clearTimeout(wait);
+      resolve('');
+    }, ms)
+  })
+};
 
 describe('five', () => {
-  it('is five', () => {
+  console.log(Main);
+  it('is five', async () => {
+    await timeout(1000);
+    main();
     expect(five).toEqual(5);
   });
 });
