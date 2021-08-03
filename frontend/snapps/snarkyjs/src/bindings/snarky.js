bffdf556d16cf90a36e66eff5a962c9a55f37a37
Izaak Meckler <ihmeckler@gmail.coom>
2021-08-03 15:33:57 -0700

    rm snarky.js hack

--- frontend/snapps/snarkyjs/src/bindings/snarky.js
+++
@@ -1,8 +0,0 @@
-import * as snarky from './snarky_raw';
-
-export const Field = snarky.Field;
-export const Bool = snarky.Bool;
-export const Scalar = snarky.Scalar;
-export const Group = snarky.Group;
-export const Poseidon = snarky.Poseidon;
-export const Circuit = snarky.Circuit;
