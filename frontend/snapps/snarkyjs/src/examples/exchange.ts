aec1ad0466b3dad743736c1e669b4645ef76f956
Izaak Meckler <ihmeckler@gmail.coom>
2021-08-06 15:56:12 -0700

    refactor exchange example

--- frontend/snapps/snarkyjs/src/examples/exchange.ts
+++ frontend/snapps/snarkyjs/src/examples/exchange.ts
@@ -1,67 +1,19 @@
 // import { MerkleCollection, MerkleProof } from '../mina.js';
-import { Circuit as C, Group, Field, Bool, AsField } from '../bindings/snarky';
+import { Circuit as C, Field, Bool, AsField } from '../bindings/snarky';
 import { public_, circuitMain, prop, CircuitValue } from '../circuit_value';
-import { Signature } from '../signature';
+import { HTTPSAttestation, Bytes } from './exchange_lib';
 
 // Proof of bought low sold high for bragging rights
 // 
 // Prove I did a trade that did "X%" increase
 
-type TradeObject = { timestamp: Field, price: Field, quantity: Field, isBuy: Bool };
-
-class Bytes extends CircuitValue {
-  value: Array<TradeObject>
-
-  toFieldElements(this:Bytes): Field[] { return [] }
-
-  constructor(value: Array<TradeObject>) {
-    super();
-    this.value = value;
-  }
-
-  static ofString(_ : string): Bytes {
-    return new Bytes([]);
-  }
-
-  static readAll<A>(ctor: { read: (bs:Bytes) => null | [Bytes, A] }, bs: Bytes): Array<A> {
-    let xs = [];
-    let res;
-    while (true) {
-      res = ctor.read(bs);
-      if (res === null) {
-        break;
-      } else {
-        let [ bsPrime, x ] = res;
-        bs = bsPrime;
-        xs.push(x);
-      }
-    }
-    return xs;
-  }
-}
-
-class HTTPSAttestation extends CircuitValue {
-  @prop response: Bytes;
-  @prop signature: Signature;
-
-  constructor(resp: Bytes, sig: Signature) {
-    super();
-    this.response = resp;
-    this.signature = sig;
-  }
-
-  verify(request: Bytes) {
-    const O1PUB: Group = Group.generator;
-    this.signature.verify(O1PUB, request.toFieldElements().concat(this.response.toFieldElements()))
-  }
-}
-
-class Witness {
+class Witness extends CircuitValue {
   @prop buyIndex: Field;
   @prop sellIndex: Field;
   @prop attestation: HTTPSAttestation
 
   constructor(buy: Field, sell: Field, a: HTTPSAttestation) {
+    super();
     this.buyIndex = buy;
     this.sellIndex = sell;
     this.attestation = a;
@@ -91,16 +43,6 @@ class Trade {
   }
 }
 
-function getElt<A>(xs: Array<A>, i_ : AsField) : A {
-  let i = new Field(i_);
-  let [ x, found ] = xs.reduce(([acc, found], x, j) => {
-    let eltHere = i.equals(j);
-    return [ C.if(eltHere, x, acc), found.or(eltHere) ];
-  }, [ xs[0], new Bool(false)]);
-  found.assertEquals(true);
-  return x;
-}
-
 export class Main extends C {
   @circuitMain
   // percentGain is an integer in basis points
@@ -124,4 +66,14 @@ export class Main extends C {
     sellTotal.mul(FULL_BASIS.add(percentChange)).assertGte(
       buyTotal.mul(FULL_BASIS));
   }
+}
+
+function getElt<A>(xs: Array<A>, i_ : AsField) : A {
+  let i = new Field(i_);
+  let [ x, found ] = xs.reduce(([acc, found], x, j) => {
+    let eltHere = i.equals(j);
+    return [ C.if(eltHere, x, acc), found.or(eltHere) ];
+  }, [ xs[0], new Bool(false)]);
+  found.assertEquals(true);
+  return x;
 }
---
+++ frontend/snapps/snarkyjs/src/examples/exchange_lib.ts
@@ -0,0 +1,55 @@
+// import { MerkleCollection, MerkleProof } from '../mina.js';
+import { Group, Bool, Field } from '../bindings/snarky';
+import { prop, CircuitValue } from '../circuit_value';
+import { Signature } from '../signature';
+
+type TradeObject = { timestamp: Field, price: Field, quantity: Field, isBuy: Bool };
+
+export class Bytes extends CircuitValue {
+  value: Array<TradeObject>
+
+  toFieldElements(this:Bytes): Field[] { return [] }
+
+  constructor(value: Array<TradeObject>) {
+    super();
+    this.value = value;
+  }
+
+  static ofString(_ : string): Bytes {
+    return new Bytes([]);
+  }
+
+  static readAll<A>(ctor: { read: (bs:Bytes) => null | [Bytes, A] }, bs: Bytes): Array<A> {
+    let xs = [];
+    let res;
+    while (true) {
+      res = ctor.read(bs);
+      if (res === null) {
+        break;
+      } else {
+        let [ bsPrime, x ] = res;
+        bs = bsPrime;
+        xs.push(x);
+      }
+    }
+    return xs;
+  }
+}
+
+export class HTTPSAttestation extends CircuitValue {
+  @prop response: Bytes;
+  @prop signature: Signature;
+
+  constructor(resp: Bytes, sig: Signature) {
+    super();
+    this.response = resp;
+    this.signature = sig;
+  }
+
+  verify(request: Bytes) {
+    const O1PUB: Group = Group.generator;
+    this.signature.verify(O1PUB, request.toFieldElements().concat(this.response.toFieldElements()))
+  }
+}
+
+
