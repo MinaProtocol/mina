d0a954cf832e8d551beb795cb43cd3153345a1ce
Izaak Meckler <ihmeckler@gmail.coom>
2021-08-06 16:34:58 -0700

    use signature library

--- frontend/snapps/snarkyjs/src/examples/schnorr_sign.ts
+++ frontend/snapps/snarkyjs/src/examples/schnorr_sign.ts
@@ -1,37 +1,6 @@
-import { Poseidon, Circuit as C, Group, Field, Bool, Scalar } from '../bindings/snarky';
+import { Circuit as C, Group, Field, Scalar } from '../bindings/snarky';
 import { public_, circuitMain, prop, CircuitValue } from '../circuit_value';
-
-type PrivateKey = Scalar;
-
-class Signature extends CircuitValue {
-  @prop r: Field;
-  @prop s: Scalar;
-
-  constructor(r: Field, s: Scalar) {
-    super();
-    this.r = r;
-    this.s = s;
-  }
-
-  static create(d: PrivateKey, msg: Field[]) : Signature {
-    const publicKey = Group.generator.scale(d);
-    const kPrime = Scalar.random();
-    let { x: r, y: ry } = Group.generator.scale(kPrime);
-    const k = ry.toBits()[0].toBoolean() ? kPrime.neg() : kPrime;
-    const e = Scalar.ofBits(Poseidon.hash(msg.concat([publicKey.x, publicKey.y, r])).toBits());
-    const s = e.mul(d).add(k);
-    return new Signature(r, s)
-  }
-
-  verify(this: this, pubKey: Group, msg: Field[]): Bool {
-    let e = Scalar.ofBits(Poseidon.hash(msg.concat([pubKey.x, pubKey.y, this.r])).toBits());
-    let r = pubKey.scale(e).neg().add(Group.generator).scale(this.s);
-    return Bool.and(
-      r.x.equals(this.r),
-      r.y.toBits()[0].equals(false)
-    )
-  }
-};
+import { Signature } from '../signature';
 
 class Witness extends CircuitValue {
   @prop signature: Signature;
