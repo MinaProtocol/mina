cdf58759920b2fba64032959e50abe939dd997ca
Izaak Meckler <ihmeckler@gmail.coom>
2021-08-10 11:23:51 -0700

    examples for workshop

---
+++ frontend/snapps/snarkyjs/src/examples/ex01_small_preimage.ts
@@ -0,0 +1,24 @@
+import { Poseidon, Field, Circuit } from '../bindings/snarky';
+import { circuitMain, public_ } from '../circuit_value';
+
+/* Exercise 1:
+
+Public input: a hash value h
+Prove:
+  I know a value x < 2^32 such that hash(x) = h 
+*/
+
+class Main extends Circuit {
+  @circuitMain
+  static main(preimage: Field, @public_ hash: Field) {
+    preimage.toBits(32);
+    Poseidon.hash([preimage]).assertEquals(hash);
+  }
+}
+
+const kp = Main.generateKeypair();
+
+const preimage = Field.ofBits(Field.random().toBits().slice(0, 32));
+const hash = Poseidon.hash([preimage]);
+const pi = Main.prove([preimage], [hash], kp);
+console.log('proof', pi);
---
+++ frontend/snapps/snarkyjs/src/examples/ex03_matrix.ts
@@ -0,0 +1,35 @@
+import { Field } from '../bindings/snarky';
+
+/* Exercise 3:
+
+Implement a function for matrix multiplication.
+*/
+
+type Matrix<A> = A[][];
+
+/* Input: 
+    m1: m x n matrix
+    m2: n x k matrix
+   Output: m x k matrix
+*/
+export function matMul(m1: Matrix<Field>, m2: Matrix<Field>) {
+  console.assert(m1[0].length === m2.length);
+  const m = m1.length;
+  const n = m2.length;
+  const k = m2[0].length;
+
+  let res = [];
+  for (let rowIndex = 0; rowIndex < m; ++rowIndex) {
+    let row = [];
+    for (let colIndex = 0; colIndex < k; ++colIndex) {
+      let v = Field.zero;
+      for (let i = 0; i < n; ++i) {
+        v = v.add(m1[rowIndex][i].mul(m2[i][colIndex]))
+      }
+      row.push(v);
+    }
+    res.push(row);
+  }
+
+  return res;
+}
