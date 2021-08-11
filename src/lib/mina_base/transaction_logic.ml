5e669ce50e6c17461e52060c973e95973a8c57a1
mrmr1993 <mr_1993@hotmail.co.uk>
2021-08-11 12:51:49 +0100

    Use a new version instead of changing Control.Stable.V1.t

--- src/lib/mina_base/transaction_logic.ml
+++ src/lib/mina_base/transaction_logic.ml
@@ -88,6 +88,17 @@ module Transaction_applied = struct
   module Snapp_command_applied = struct
     [%%versioned
     module Stable = struct
+      module V2 = struct
+        type t =
+          { accounts :
+              (Account_id.Stable.V1.t * Account.Stable.V1.t option) list
+          ; command : Snapp_command.Stable.V2.t With_status.Stable.V1.t
+          }
+        [@@deriving sexp]
+
+        let to_latest = Fn.id
+      end
+
       module V1 = struct
         type t =
           { accounts :
@@ -96,7 +107,11 @@ module Transaction_applied = struct
           }
         [@@deriving sexp]
 
-        let to_latest = Fn.id
+        let to_latest (t : t) : V2.t =
+          { accounts = t.accounts
+          ; command =
+              With_status.map ~f:Snapp_command.Stable.V1.to_latest t.command
+          }
       end
     end]
   end
