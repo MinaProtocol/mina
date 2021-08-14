4d5e1febb93dd3bd487f86ecec18970a233a4719
Andrew Trainor <andrew@o1labs.org>
2021-08-14 00:37:19 -0400

    lk86/pipeline-refactor Copy over changes from pauls testing script to the main docker-start.sh script

--- src/app/rosetta/docker-start.sh
+++ src/app/rosetta/docker-start.sh
@@ -20,8 +20,10 @@ trap cleanup INT
 
 PG_CONN=postgres://$USER:$USER@localhost:5432/archiver
 
+POSTGRES_VERSION=$(psql -V | cut -d " " -f 3 | sed 's/.[[:digit:]]*$//g')
+
 # Start postgres
-pg_ctlcluster 11 main start
+pg_ctlcluster ${POSTGRES_VERSION} main start
 
 # wait for it to settle
 sleep 3
@@ -29,10 +31,12 @@ sleep 3
 export MINA_PRIVKEY_PASS=""
 export MINA_LIBP2P_HELPER_PATH=/mina-bin/libp2p_helper
 
-export MINA_CONFIG_FILE=${MINA_CONFIG_FILE:=/data/config.json}
-export PEER_ID=${PEER_ID:=/ip4/34.74.175.158/tcp/10001/ipfs/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr/}
-export MINA_PORT=${MINA_DAEMON_PORT:=10101}
-DEFAULT_FLAGS="-generate-genesis-proof true -peer ${PEER_ID} -archive-address 0.0.0.0:3086 -insecure-rest-server -log-level debug -external-port ${MINA_PORT}"
+export MINA_CONFIG_FILE=${MINA_CONFIG_FILE:=/data/genesis_ledgers/devnet.json}
+export PEER_LIST_URL=${PEER_LIST_URL:=https://storage.googleapis.com/seed-lists/devnet_seeds.txt}
+export MINA_DAEMON_PORT=${MINA_DAEMON_PORT:=10101}
+export MINA_ARCHIVE_PORT=${MINA_ARCHIVE_PORT:=3086}
+export MINA_ROSETTA_PORT=${MINA_ROSETTA_PORT:=3087}
+DEFAULT_FLAGS="-peer-list-url ${PEER_LIST_URL} -archive-address 0.0.0.0:${MINA_ARCHIVE_PORT} -insecure-rest-server -log-level debug -external-port ${MINA_DAEMON_PORT}"
 export MINA_FLAGS=${MINA_FLAGS:=$DEFAULT_FLAGS}
 PK=${MINA_PK:=ZsMSUuKL9zLAF7sMn951oakTFRCCDw9rDfJgqJ55VMtPXaPa5vPwntQRFJzsHyeh8R8}
 
@@ -42,12 +46,13 @@ echo "MINA Flags: $MINA_FLAGS -config-file ${MINA_CONFIG_FILE}"
 /mina-bin/archive/archive.exe run \
   -postgres-uri $PG_CONN \
   -config-file ${MINA_CONFIG_FILE} \
-  -server-port 3086 &
+  -server-port ${MINA_ARCHIVE_PORT} &
 
 # wait for it to settle
 sleep 3
 
-# Daemon w/ mounted config file, initial file is phase 3 config.json
+# Daemon w/ config file
+# Use MINA_CONFIG_FILE=/data/genesis_ledgers/mainnet.json to run on mainnet
 /mina-bin/cli/src/mina.exe daemon \
     -config-file ${MINA_CONFIG_FILE} \
     ${MINA_FLAGS} $@ &
@@ -60,7 +65,7 @@ sleep 3
   -archive-uri $PG_CONN \
   -graphql-uri http://localhost:3085/graphql \
   -log-level debug \
-  -port 3087 &
+  -port ${MINA_ROSETTA_PORT} &
 
 # wait for a signal
 sleep infinity
