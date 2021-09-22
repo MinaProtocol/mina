b10c0e3db9112a2a8aebc3eec7c6d2570fcc4044
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2021-09-21 17:35:44 -0700

    reformat

--- src/lib/mina_base/transaction_logic.ml
+++ src/lib/mina_base/transaction_logic.ml
@@ -413,8 +413,7 @@ module Make (L : Ledger_intf) : S with type ledger := L.t = struct
       | Some account ->
           Ok (`Existing location, account)
       | None ->
-          Ok (`New, Account.create account_id Balance.zero)
-      )
+          Ok (`New, Account.create account_id Balance.zero) )
     | None ->
         Ok (`New, Account.create account_id Balance.zero)
 
