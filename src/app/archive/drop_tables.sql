cce990a9a238081686e9ae031917b714bb852169
Paul Steckler <steck@stecksoft.com>
2021-09-30 11:54:18 -0700

    capitalize DROP TABLE, to match other SQL keywords

--- src/app/archive/drop_tables.sql
+++ src/app/archive/drop_tables.sql
@@ -4,81 +4,81 @@
     containing the indexed column
 */
 
-drop table blocks_internal_commands;
+DROP TABLE blocks_internal_commands;
 
-drop table blocks_user_commands;
+DROP TABLE blocks_user_commands;
 
-drop table blocks_snapp_commands;
+DROP TABLE blocks_snapp_commands;
 
-drop table snapp_party_balances;
+DROP TABLE snapp_party_balances;
 
-drop table balances;
+DROP TABLE balances;
 
-drop table blocks;
+DROP TABLE blocks;
 
-drop table epoch_data;
+DROP TABLE epoch_data;
 
-drop table internal_commands;
+DROP TABLE internal_commands;
 
 drop type internal_command_type;
 
-drop table user_commands;
+DROP TABLE user_commands;
 
 drop type user_command_type;
 
 drop type user_command_status;
 
-drop table snapp_commands;
+DROP TABLE snapp_commands;
 
-drop table snapp_other_parties;
+DROP TABLE snapp_other_parties;
 
-drop table snapp_party;
+DROP TABLE snapp_party;
 
-drop table snapp_fee_payers;
+DROP TABLE snapp_fee_payers;
 
-drop table snapp_party_predicated;
+DROP TABLE snapp_party_predicated;
 
-drop table snapp_party_body;
+DROP TABLE snapp_party_body;
 
-drop table snapp_updates;
+DROP TABLE snapp_updates;
 
-drop table snapp_protocol_states;
+DROP TABLE snapp_protocol_states;
 
-drop table snapp_predicate;
+DROP TABLE snapp_predicate;
 
-drop table snapp_predicate_account;
+DROP TABLE snapp_predicate_account;
 
-drop table snapp_epoch_data;
+DROP TABLE snapp_epoch_data;
 
-drop table snapp_epoch_ledger;
+DROP TABLE snapp_epoch_ledger;
 
-drop table snapp_events;
+DROP TABLE snapp_events;
 
-drop table snapp_permissions;
+DROP TABLE snapp_permissions;
 
-drop table snapp_state_array;
+DROP TABLE snapp_state_array;
 
-drop table snapp_states;
+DROP TABLE snapp_states;
 
-drop table snapp_state_data;
+DROP TABLE snapp_state_data;
 
-drop table snapp_timing_info;
+DROP TABLE snapp_timing_info;
 
-drop table snapp_verification_keys;
+DROP TABLE snapp_verification_keys;
 
-drop table snapp_bounded_amount;
+DROP TABLE snapp_bounded_amount;
 
-drop table snapp_bounded_balance;
+DROP TABLE snapp_bounded_balance;
 
-drop table snapp_bounded_blockchain_length;
+DROP TABLE snapp_bounded_blockchain_length;
 
-drop table snapp_bounded_global_slot;
+DROP TABLE snapp_bounded_global_slot;
 
-drop table snapp_bounded_nonce;
+DROP TABLE snapp_bounded_nonce;
 
-drop table snapp_bounded_timestamp;
+DROP TABLE snapp_bounded_timestamp;
 
-drop table snapp_bounded_token_id;
+DROP TABLE snapp_bounded_token_id;
 
 drop type snapp_auth_required_type;
 
@@ -86,8 +86,8 @@ drop type snapp_authorization_kind_type;
 
 drop type snapp_predicate_type;
 
-drop table snarked_ledger_hashes;
+DROP TABLE snarked_ledger_hashes;
 
-drop table timing_info;
+DROP TABLE timing_info;
 
-drop table public_keys;
+DROP TABLE public_keys;
