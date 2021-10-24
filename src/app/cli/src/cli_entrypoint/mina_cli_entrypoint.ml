0b6a4763fa296d945ae8d77ca973f8ff6252db72
Gareth Davies <hello@garethtdavies.com>
2021-10-24 02:49:40 +0000

    Update coda for mina in log message

--- src/app/cli/src/cli_entrypoint/mina_cli_entrypoint.ml
+++ src/app/cli/src/cli_entrypoint/mina_cli_entrypoint.ml
@@ -428,7 +428,7 @@ let setup_daemon logger =
       ]
     in
     [%log info]
-      "Coda daemon is booting up; built with commit $commit on branch $branch"
+      "Mina daemon is booting up; built with commit $commit on branch $branch"
       ~metadata:version_metadata ;
     let%bind () = Mina_lib.Conf_dir.check_and_set_lockfile ~logger conf_dir in
     if not @@ String.equal daemon_expiry "never" then (
