d4b71bb9e2e46d44eacb2d133580834ddac7fd3b
mrmr1993 <mr_1993@hotmail.co.uk>
2021-10-24 12:37:20 +0100

    Reformat

--- src/lib/cache_dir/fake/cache_dir.ml
+++ src/lib/cache_dir/fake/cache_dir.ml
@@ -1,5 +1,6 @@
 open Core_kernel
 open Async_kernel
+
 let autogen_path = "/tmp/coda_cache_dir"
 
 let s3_install_path = "/tmp/s3_cache_dir"
@@ -25,4 +26,4 @@ let possible_paths base =
     ] ~f:(fun d -> d ^ "/" ^ base)
 
 let load_from_s3 _s3_bucket_prefix _s3_install_path ~logger:_ =
-  Deferred.Or_error.fail (Error.createf "Cannot load files from S3" )
+  Deferred.Or_error.fail (Error.createf "Cannot load files from S3")
