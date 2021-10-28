dd4d5132567dedc124e7634b390cc8a97200f448
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2021-10-28 21:13:19 +0800

    add delegation-verify as valid service

--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -10,7 +10,7 @@ set +x
 CLEAR='\033[0m'
 RED='\033[0;31m'
 # Array of valid service names
-VALID_SERVICES=('mina-archive', 'mina-daemon' 'mina-rosetta' 'mina-toolchain' 'bot' 'leaderboard' 'delegation-backend' 'delegation-backend-toolchain' 'mina-test-executive')
+VALID_SERVICES=('mina-archive', 'mina-daemon' 'mina-rosetta' 'mina-toolchain' 'bot' 'leaderboard' 'delegation-backend' 'delegation-backend-toolchain' 'mina-test-executive' 'delegation-verify' )
 
 function usage() {
   if [[ -n "$1" ]]; then
