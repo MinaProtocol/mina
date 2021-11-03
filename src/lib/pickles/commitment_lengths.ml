MinaProtocol/mina

commit df12992198627f0f115a617dd4197ee5322d6d93

update to work with zero knowledge backend

src/lib/pickles/commitment_lengths.ml
@@ -5,38 +5,22 @@ open Import
 (* Compute the number of group elements required to represent each commitment.
    This is simply ceil(degree of commitment / max degree natively supported by the PCS) *)
 
-let generic' ~h ~add:( + ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
+let generic' ~h ~sub ~add:( + ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
     _ Dlog_plonk_types.Evals.t =
   let n = h#size in
-  let n_plus_2 = ceil_div_max_degree (of_int 2 + n) in
-  let n_plus_3 = ceil_div_max_degree (of_int 3 + n) in
-  let five_n_plus_1 = ceil_div_max_degree (of_int 5 * (n + of_int 1)) in
+  let t_bound =
+    ceil_div_max_degree (Common.max_quot_size ~of_int ~mul:( * ) ~sub n)
+  in
   let h = ceil_div_max_degree n in
-  { l= n_plus_2
-  ; r= n_plus_2
-  ; o= n_plus_2
-  ; z= n_plus_3
-  ; t= five_n_plus_1
-  ; f= n_plus_3
-  ; sigma1= h
-  ; sigma2= h }
+  {l= n; r= n; o= n; z= n; t= t_bound; f= n; sigma1= h; sigma2= h}
 
 let generic map ~h ~max_degree : _ Dlog_plonk_types.Evals.t =
-  let n_plus_2 = map h ~f:(fun h -> h + 2) in
-  let n_plus_3 = map h ~f:(fun h -> h + 3) in
-  let five_n_plus_1 = map h ~f:(fun h -> 5 * (h + 1)) in
+  let t_bound = map h ~f:(fun h -> Common.max_quot_size_int h) in
   Dlog_plonk_types.Evals.map
     ~f:(fun v ->
       map v ~f:(fun x -> Int.round_up x ~to_multiple_of:max_degree / max_degree)
       )
-    { l= n_plus_2
-    ; r= n_plus_2
-    ; o= n_plus_2
-    ; z= n_plus_3
-    ; t= five_n_plus_1
-    ; f= n_plus_3
-    ; sigma1= h
-    ; sigma2= h }
+    {l= h; r= h; o= h; z= h; t= t_bound; f= h; sigma1= h; sigma2= h}
 
 let of_domains {Domains.h; _} ~max_degree : int Dlog_plonk_types.Evals.t =
   let h = Domain.size h in

src/lib/pickles/common.ml
@@ -226,3 +226,8 @@ let index_commitment_length k ~max_degree =
   1
 
 let max_log2_degree = Pickles_base.Side_loaded_verification_key.max_log2_degree
+
+let max_quot_size ~of_int ~mul:( * ) ~sub:( - ) domain_size =
+  of_int 5 * (domain_size - of_int 1)
+
+let max_quot_size_int = max_quot_size ~of_int:Fn.id ~mul:( * ) ~sub:( - )

src/lib/pickles/dlog_main.ml
@@ -114,9 +114,10 @@ struct
       as_prover
         As_prover.(
           fun () ->
-            Core.printf "in-snark %s: %s\n%!" lab
-              (Field.Constant.to_string
-                 (Field.Constant.project (List.map ~f:(read Boolean.typ) x))))
+            Core.printf "in-snark %s:%!" lab ;
+            Field.Constant.print
+              (Field.Constant.project (List.map ~f:(read Boolean.typ) x)) ;
+            Core.printf "\n%!")
 
   let print_bool lab x =
     if debug then
@@ -599,13 +600,13 @@ struct
           in
           let poseidon =
             let ( * ) = Fn.flip Scalar_challenge.endo in
-            (* alpha^3 rcm_comm[0] + alpha^4 rcm_comm[1] + alpha^5 rcm_comm[2]
+            (* alpha^2 rcm_comm[0] + alpha^3 rcm_comm[1] + alpha^4 rcm_comm[2]
                 =
-                alpha^3 (rcm_comm[0] + alpha (rcm_comm[1] + alpha rcm_comm[2]))
+                alpha^2 (rcm_comm[0] + alpha (rcm_comm[1] + alpha rcm_comm[2]))
             *)
             let a = alpha in
             m.rcm_comm_0 + (a * (m.rcm_comm_1 + (a * m.rcm_comm_2)))
-            |> ( * ) a |> ( * ) a |> ( * ) a
+            |> ( * ) a |> ( * ) a
           in
           let g =
             List.reduce_exn ~f:( + )

src/lib/pickles/pairing_main.ml
@@ -383,14 +383,14 @@ struct
             + m.qc_comm
           in
           let poseidon =
-            (* alpha^3 rcm_comm[0] + alpha^4 rcm_comm[1] + alpha^5 rcm_comm[2]
+            (* alpha^2 rcm_comm[0] + alpha^3 rcm_comm[1] + alpha^4 rcm_comm[2]
                  =
-                 alpha^3 (rcm_comm[0] + alpha (rcm_comm[1] + alpha rcm_comm[2]))
+                 alpha^2 (rcm_comm[0] + alpha (rcm_comm[1] + alpha rcm_comm[2]))
               *)
             let a = alpha in
             let ( * ) = Fn.flip Scalar_challenge.endo in
             m.rcm_comm_0 + (a * (m.rcm_comm_1 + (a * m.rcm_comm_2)))
-            |> ( * ) a |> ( * ) a |> ( * ) a
+            |> ( * ) a |> ( * ) a
           in
           let g =
             List.reduce_exn ~f:( + )
@@ -436,7 +436,8 @@ struct
               check_bulletproof
                 ~pcs_batch:
                   (Common.dlog_pcs_batch (Branching.add Nat.N8.n)
-                     ~max_quot_size:((5 * (Domain.size domain + 2)) - 5))
+                     ~max_quot_size:
+                       (Common.max_quot_size_int (Domain.size domain)))
                 ~sponge:sponge_before_evaluations ~xi ~combined_inner_product
                 ~advice ~openings_proof
                 ~polynomials:(without_degree_bound, [t_comm]) )
@@ -772,7 +773,7 @@ struct
               Side_loaded_verification_key.(Domain.log2_size max_domains.h)
         ; x= Pow_2_roots_of_unity 0 }
     in
-    Commitment_lengths.generic' ~h ~add:Field.add ~mul:Field.mul
+    Commitment_lengths.generic' ~h ~add:Field.add ~mul:Field.mul ~sub:Field.sub
       ~of_int:Field.of_int
       ~ceil_div_max_degree:
         (let k = Nat.to_int Backend.Tick.Rounds.n in
@@ -912,10 +913,13 @@ struct
               `Known
                 ( which_branch
                 , Vector.map step_domains ~f:(fun x ->
-                      (5 * (Domain.size x.Domains.h + 2)) - 5 ) )
+                      Common.max_quot_size_int (Domain.size x.Domains.h) ) )
           | `Side_loaded domains ->
               let conv domain =
-                let deg = (of_int 5 * domain#size) + of_int 5 in
+                let deg =
+                  Common.max_quot_size ~of_int ~mul:( * ) ~sub:( - )
+                    domain#size
+                in
                 let d = Split_evaluations.mod_max_degree deg in
                 Number.(
                   to_bits (constant (Field.Constant.of_int Max_degree.step) - d))

src/lib/pickles/side_loaded_verification_key.ml
@@ -151,7 +151,9 @@ include Make
               in
               let t =
                 let d = Common.wrap_domains.h in
-                let max_quot_size = (5 * (Import.Domain.size d + 2)) - 5 in
+                let max_quot_size =
+                  Common.max_quot_size_int (Import.Domain.size d)
+                in
                 let {Snarky_bn382_bindings.Shifts.r; o} =
                   Common.tock_shifts
                     ~log2_size:(Pickles_base.Domain.log2_size d)

src/lib/pickles/step.ml
@@ -148,6 +148,7 @@ struct
             Tick.Field.(
               zeta * domain_generator ~log2_size:(Domain.log2_size domain))
           in
+          time "plonk_checks" (fun () ->
           Plonk_checks.derive_plonk
             (module Tick.Field)
             ~endo:Endo.Dee.base ~shift:Shifts.tick
@@ -163,7 +164,7 @@ struct
             ; gamma= Challenge.Constant.to_tick_field plonk0.gamma }
             (Plonk_checks.evals_of_split_evals
                (module Tick.Field)
-               t.prev_evals ~rounds:(Nat.to_int Tick.Rounds.n) ~zeta ~zetaw)
+               t.prev_evals ~rounds:(Nat.to_int Tick.Rounds.n) ~zeta ~zetaw) )
         in
         let data = Types_map.lookup_basic tag in
         let (module Local_max_branching) = data.max_branching in
@@ -310,7 +311,8 @@ struct
             Pcs_batch.combine_split_evaluations
               (Common.dlog_pcs_batch
                  (Local_max_branching.add Nat.N8.n)
-                 ~max_quot_size:Int.((5 * Domain.size domains.h) + 5))
+                 ~max_quot_size:
+                   (Common.max_quot_size_int (Domain.size domains.h)))
               ~xi ~init:Fn.id ~mul ~last:Array.last
               ~mul_and_add:(fun ~acc ~xi fx -> fx + (xi * acc))
               ~evaluation_point:pt

src/lib/pickles/verification_key.ml
@@ -53,7 +53,7 @@ module Stable = struct
         let {Snarky_bn382_bindings.Shifts.r; o} =
           Common.tock_shifts ~log2_size
         in
-        let max_quot_size = (5 * (Domain.size d + 2)) - 5 in
+        let max_quot_size = Common.max_quot_size_int (Domain.size d) in
         Snarky_bn382.Tweedle.Dee.Plonk.Field_verifier_index.make
           ~max_poly_size:(u (1 lsl Nat.to_int Rounds.Wrap.n))
           ~max_quot_size:(u max_quot_size) ~urs ~sigma_comm0:(g c.sigma_comm_0)

src/lib/pickles/wrap.ml
@@ -42,7 +42,8 @@ let combined_inner_product (type actual_branching)
     let open Tick.Field in
     Pcs_batch.combine_split_evaluations
       (Common.dlog_pcs_batch (AB.add Nat.N8.n)
-         ~max_quot_size:Int.((5 * (Domain.size step_branch_domains.h + 2)) - 5))
+         ~max_quot_size:
+           (Common.max_quot_size_int (Domain.size step_branch_domains.h)))
       ~xi ~init:Fn.id ~mul
       ~mul_and_add:(fun ~acc ~xi fx -> fx + (xi * acc))
       ~last:Array.last ~evaluation_point:pt

commit de1fcf2e6bc260dd9872c66809bb591af433406c

Fix commitment lengths for side-loaded verification keys
This error was introduced in df12992198627f0f115a617dd4197ee5322d6d93,
where the `generic` version was updated correctly to use `h` everywhere,
but the `generic'` version incorrectly used `n` instead of `h`. Since we
weren't using side-loaded keys anywhere at the time, this was also
missed during testing.

src/lib/pickles/commitment_lengths.ml
@@ -12,7 +12,7 @@ let generic' ~h ~sub ~add:( + ) ~mul:( * ) ~of_int ~ceil_div_max_degree :
     ceil_div_max_degree (Common.max_quot_size ~of_int ~mul:( * ) ~sub n)
   in
   let h = ceil_div_max_degree n in
-  { l = n; r = n; o = n; z = n; t = t_bound; f = n; sigma1 = h; sigma2 = h }
+  { l = h; r = h; o = h; z = h; t = t_bound; f = h; sigma1 = h; sigma2 = h }
 
 let generic map ~h ~max_degree : _ Dlog_plonk_types.Evals.t =
   let t_bound = map h ~f:(fun h -> Common.max_quot_size_int h) in