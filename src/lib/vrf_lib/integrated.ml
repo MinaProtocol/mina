7ec74f232609494b035923ec261fff83477b0cda
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-04 12:47:44 -0700

    update vrf

--- src/lib/vrf_lib/integrated.ml
+++ src/lib/vrf_lib/integrated.ml
@@ -13,28 +13,13 @@ module Make
       val scale : value -> Scalar.value -> value
 
       module Checked : sig
-        module Shifted : sig
-          module type S =
-            Snarky_curves.Shifted_intf
-              with type ('a, 'b) checked := ('a, 'b) Checked.t
-               and type boolean_var := Boolean.var
-               and type curve_var := var
-
-          type 'a m = (module S with type t = 'a)
+        module Assert : sig
+          val equal : var -> var -> (unit, _) Checked.t
         end
 
-        val scale :
-             (module Shifted.S with type t = 'shifted)
-          -> var
-          -> Scalar.var
-          -> init:'shifted
-          -> ('shifted, _) Checked.t
+        val scale_random : var -> Scalar.var -> (var, _) Checked.t
 
-        val scale_generator :
-             (module Shifted.S with type t = 'shifted)
-          -> Scalar.var
-          -> init:'shifted
-          -> ('shifted, _) Checked.t
+        val scale_generator : Scalar.var -> (var, _) Checked.t
       end
     end) (Message : sig
       type value
@@ -72,14 +57,12 @@ module Make
 
   module Checked : sig
     val eval :
-         'shifted Group.Checked.Shifted.m
-      -> private_key:Scalar.var
+         private_key:Scalar.var
       -> Message.var
       -> (Output_hash.var, _) Impl.Checked.t
 
     val eval_and_check_public_key :
-         'shifted Group.Checked.Shifted.m
-      -> private_key:Scalar.var
+         private_key:Scalar.var
       -> public_key:Group.var
       -> Message.var
       -> (Output_hash.var, _) Impl.Checked.t
@@ -95,28 +78,27 @@ end = struct
   module Checked = struct
     open Let_syntax
 
-    let eval (type shifted) ((module Shifted) : shifted Group.Checked.Shifted.m)
-        ~private_key m =
+    let eval ~private_key m =
       let%bind h = Message.Checked.hash_to_group m in
       let%bind u =
-        (* This use of unshift_nonzero is acceptable since if h^private_key = 0 then
-           the prover had a bad private key *)
-        Group.Checked.scale (module Shifted) h private_key ~init:Shifted.zero
-        >>= Shifted.unshift_nonzero
+        (* If [private_key] is not sampled honestly (i.e., at random),
+           then the in-snark value of the VRF will not match the out of SNARK
+           value. That's fine since this SNARK is run only by a block producer,
+           so they may screw themselves over so to speak, but it will not cause
+           SNARK workers to choke or anything like that.
+
+           Also, it doesn't give any advantage in evaluating the VRF since they
+           still only get one chance. *)
+        Group.Checked.scale_random h private_key
       in
       Output_hash.Checked.hash m u
 
-    let eval_and_check_public_key (type shifted)
-        ((module Shifted) : shifted Group.Checked.Shifted.m) ~private_key
-        ~public_key message =
+    let eval_and_check_public_key ~private_key ~public_key message =
       let%bind () =
         with_label __LOC__
-          (let%bind public_key_shifted = Shifted.(add zero public_key) in
-           Group.Checked.scale_generator
-             (module Shifted)
-             private_key ~init:Shifted.zero
-           >>= Shifted.Assert.equal public_key_shifted)
+          ( Group.Checked.scale_generator private_key
+          >>= Group.Checked.Assert.equal public_key )
       in
-      eval (module Shifted) ~private_key message
+      eval ~private_key message
   end
 end
