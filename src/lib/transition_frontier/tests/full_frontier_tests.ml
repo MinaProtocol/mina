2d20d2db3a74a8e206737a9ea763dcc916e54bab
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2021-11-05 23:01:34 +0800

    fix type error in full_frontier_test

--- src/lib/transition_frontier/tests/full_frontier_tests.ml
+++ src/lib/transition_frontier/tests/full_frontier_tests.ml
@@ -209,9 +209,10 @@ let%test_module "Full_frontier tests" =
                   ignore (
                   List.iter (State_hash.Set.to_list required_state_hashes)
                     ~f:(fun hash ->
+                      ignore (
                       Full_frontier.For_tests.find_protocol_state_exn frontier
                         hash
-                      ) : Protocol_state.value option list ) );
+                      ) : Mina_state.Protocol_state.value ) )) ;
               clean_up_persistent_root ~frontier ) )
 
     let%test_unit "The length of the longest branch should never be greater \
