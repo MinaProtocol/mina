976abda22e0b83663d560283693168603094f386
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-08 14:17:35 -0800

    add a little test to make sure vectors are working

--- src/lib/crypto/kimchi_backend/tests.ml
+++ src/lib/crypto/kimchi_backend/tests.ml
@@ -9,6 +9,32 @@ let main x () =
   in
   assert_r1cs y y x
 
+let%test_unit "of_affine" =
+  let one = Kimchi.Pallas.one () in
+  let x, y =
+    match Kimchi.Pallas.to_affine one with
+    | Finite (x, y) ->
+        (x, y)
+    | Infinity ->
+        assert false
+  in
+  Kimchi.Foundations.Fp.print x ;
+  Kimchi.Foundations.Fp.print y ;
+  Kimchi.Pallas.(ignore (of_affine_coordinates x y : t))
+
+let%test_unit "vector test" =
+  let elt1 = Kimchi.Foundations.Fp.of_int 1231 in
+  let elt2 = Kimchi.Foundations.Fp.of_int 893234 in
+  let v = Kimchi.FieldVectors.Fp.create () in
+  Kimchi.FieldVectors.Fp.emplace_back v elt1 ;
+  Kimchi.FieldVectors.Fp.emplace_back v elt2 ;
+  Kimchi.FieldVectors.Fp.emplace_back v elt1 ;
+  let x0 = Kimchi.FieldVectors.Fp.get v 0 in
+  Kimchi.Foundations.Fp.mut_mul x0 x0 ;
+  assert (Kimchi.Foundations.Fp.equal elt1 (Kimchi.FieldVectors.Fp.get v 0)) ;
+  assert (Kimchi.Foundations.Fp.equal elt2 (Kimchi.FieldVectors.Fp.get v 1)) ;
+  assert (Kimchi.Foundations.Fp.equal elt1 (Kimchi.FieldVectors.Fp.get v 2))
+
 let%test_unit "thing" =
   Kimchi_pasta.Pallas_based_plonk.Keypair.set_urs_info [] ;
   let _kp = Impl.generate_keypair ~exposing:[ Field.typ ] main in
