b887d80aee9270092ba73d29186b697f32576a5e
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-08 13:46:24 -0800

    bindings for expr related types

--- src/lib/crypto/kimchi_bindings/stubs/binding_generation/src/main.rs
+++ src/lib/crypto/kimchi_bindings/stubs/binding_generation/src/main.rs
@@ -15,6 +15,10 @@ use wires_15_stubs::{
     plonk_verifier_index::{CamlPlonkDomain, CamlPlonkVerificationEvals, CamlPlonkVerifierIndex},
     projective::{pallas::*, vesta::*},
     srs::{fp::*, fq::*},
+    CamlColumn,
+    CamlVariable,
+    CamlPolishToken,
+    CamlLinearization,
     CamlCircuitGate,
     CamlLookupEvaluations,
     CamlOpeningProof,
@@ -26,6 +30,7 @@ use wires_15_stubs::{
     CamlScalarChallenge,
     CamlWire,
     GateType,
+    CurrOrNext,
 };
 
 fn main() {
@@ -243,6 +248,12 @@ fn generate_bindings(mut w: impl std::io::Write) {
         decl_type!(w, env, CamlWire => "wire");
         decl_type!(w, env, GateType => "gate_type");
         decl_type!(w, env, CamlCircuitGate<T1> => "circuit_gate");
+
+        decl_type!(w, env, CurrOrNext => "curr_or_next");
+        decl_type!(w, env, CamlColumn => "column");
+        decl_type!(w, env, CamlVariable => "variable");
+        decl_type!(w, env, CamlPolishToken<T1> => "polish_token");
+        decl_type!(w, env, CamlLinearization<T1> => "linearization");
 
         decl_type!(w, env, CamlOracles<T1> => "oracles");
 
