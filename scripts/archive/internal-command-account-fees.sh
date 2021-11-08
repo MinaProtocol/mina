de2a22de9df18a4228a14c6f470e472d30203be0
Paul Steckler <steck@stecksoft.com>
2021-11-08 12:21:52 -0800

    Bash variable for creation fee in patch script

--- scripts/archive/internal-command-account-fees.sh
+++ scripts/archive/internal-command-account-fees.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# add account creation fee of 1 Mina to blocks_internal_commands for internal commands, where
-# the balance is the command amount minus 1 Mina
+# add account creation fee to blocks_internal_commands for internal commands, where
+# the balance is the command amount minus the creation fee
 
 ARCHIVE=archive
 TMPFILE=$(mktemp -t internal-cmd-acct-fee.XXXXX)
+CREATION_FEE=1000000000
 
 psql $ARCHIVE <<EOF
 ALTER TABLE blocks_internal_commands
@@ -19,11 +20,11 @@ INNER JOIN internal_commands as ic
 ON bic.internal_command_id = ic.id
 INNER JOIN balances
 ON bic.receiver_balance = balances.id
-WHERE balances.balance = ic.fee - 1000000000
+WHERE balances.balance = ic.fee - $CREATION_FEE
 EOF
 
 # if a coinbase has an associated fee transfer, add account creation fee if
-# the balance is coinbase amount minus 1 Mina, minus the amount of the fee transfer
+# the balance is coinbase amount minus the creation fee, minus the amount of the fee transfer
 # an associated fee transfer is in the same block with the same sequence number as the coinbase
 psql $ARCHIVE <<EOF | awk -F \| '(NR > 2){print $1 $2 $3 $4}' | head -n -2 >> $TMPFILE
 SELECT bic_coinbase.block_id, bic_coinbase.internal_command_id, bic_coinbase.sequence_no, bic_coinbase.secondary_sequence_no
@@ -39,12 +40,12 @@ INNER JOIN balances
 ON bic_coinbase.receiver_balance = balances.id
 WHERE ic_coinbase.type = 'coinbase'
 AND ic_fee_transfer.type = 'fee_transfer_via_coinbase'
-AND balances.balance = ic_coinbase.fee - 1000000000 - ic_fee_transfer.fee
+AND balances.balance = ic_coinbase.fee - $CREATION_FEE - ic_fee_transfer.fee
 EOF
 
 while read -r block_id internal_command_id sequence_no secondary_sequence_no; do
     psql $ARCHIVE <<EOF
-    UPDATE blocks_internal_commands SET receiver_account_creation_fee_paid = 1000000000
+    UPDATE blocks_internal_commands SET receiver_account_creation_fee_paid = $CREATION_FEE
     WHERE block_id=$block_id
     AND internal_command_id=$internal_command_id
     AND sequence_no=$sequence_no
