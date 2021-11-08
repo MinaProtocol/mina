6c94ce5da3b70389d8884faa68597751e2ad89c8
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-08 14:16:43 -0800

    use run_in_thread

--- src/lib/crypto/kimchi_backend/pasta/vesta_based_plonk.ml
+++ src/lib/crypto/kimchi_backend/pasta/vesta_based_plonk.ml
@@ -100,7 +100,7 @@ module Proof = Plonk_dlog_proof.Make (struct
 
     let batch_verify =
       with_lagranges (fun lgrs vks ts ->
-          Async.In_thread.run (fun () -> batch_verify lgrs vks ts))
+          Run_in_thread.run_in_thread (fun () -> batch_verify lgrs vks ts))
 
     let create_aux ~f:create (pk : Keypair.t) primary auxiliary prev_chals
         prev_comms =
@@ -132,7 +132,7 @@ module Proof = Plonk_dlog_proof.Make (struct
     let create_async (pk : Keypair.t) primary auxiliary prev_chals prev_comms =
       create_aux pk primary auxiliary prev_chals prev_comms
         ~f:(fun pk auxiliary_input prev_challenges prev_sgs ->
-          Async.In_thread.run (fun () ->
+          Run_in_thread.run_in_thread (fun () ->
               create pk auxiliary_input prev_challenges prev_sgs))
 
     let create (pk : Keypair.t) primary auxiliary prev_chals prev_comms =
