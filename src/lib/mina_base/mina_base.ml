0a9e0031349e8f0181e3e6901ec0987c733825bb
Izaak Meckler <ihmeckler@gmail.coom>
2021-11-18 16:43:42 -0800

    supporting files for mina_base

---
+++ src/lib/mina_base/mina_base.ml
@@ -0,0 +1,11 @@
+module Sparse_ledger0 = Sparse_ledger
+module Ledger_transfer = Ledger_transfer
+module Snapp_generators = Snapp_generators
+module Rpc_intf = Rpc_intf
+module Transaction_validator = Transaction_validator
+module Ledger = Ledger
+module Sync_ledger = Sync_ledger
+
+include Mina_base_kernel
+
+module Sparse_ledger = Sparse_ledger0
