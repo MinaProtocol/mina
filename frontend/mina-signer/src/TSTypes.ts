33c8988c6814c267bc04fd64151c66aeea0d3be0
Martin Minkov <minkovlmartin@gmail.com>
2021-11-24 08:19:18 -0800

    feat: Remove unused declares

--- frontend/mina-signer/src/TSTypes.ts
+++ frontend/mina-signer/src/TSTypes.ts
@@ -1,32 +1,32 @@
 export type uint32 = number | bigint | string;
 export type uint64 = number | bigint | string;
 
-export declare type publicKey = string;
-export declare type privateKey = string;
-export declare type network = "mainnet" | "testnet";
-export declare type signableData = message | stakeDelegation | payment;
+export type publicKey = string;
+export type privateKey = string;
+export type network = "mainnet" | "testnet";
+export type signableData = message | stakeDelegation | payment;
 
-export declare type keypair = {
+export type keypair = {
   readonly privateKey: privateKey;
   readonly publicKey: publicKey;
 };
 
-export declare type message = {
+export type message = {
   publicKey: publicKey;
   message: string;
 };
 
-export declare type signature = {
+export type signature = {
   readonly field: string;
   readonly scalar: string;
 };
 
-export declare type signed<signableData> = {
+export type signed<signableData> = {
   readonly signature: signature;
   readonly data: signableData;
 };
 
-export declare type stakeDelegation = {
+export type stakeDelegation = {
   readonly to: publicKey;
   readonly from: publicKey;
   readonly fee: uint64;
@@ -35,7 +35,7 @@ export declare type stakeDelegation = {
   readonly validUntil?: uint32;
 };
 
-export declare type payment = {
+export type payment = {
   readonly to: publicKey;
   readonly from: publicKey;
   readonly fee: uint64;
