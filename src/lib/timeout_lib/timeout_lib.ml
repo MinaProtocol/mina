6ee240d7a251896e59b0142a3f76ee4bff543fbb
Izaak Meckler <ihmeckler@gmail.coom>
2021-12-02 10:23:03 -0800

    remove logger dep in timeout lib

--- src/lib/timeout_lib/timeout_lib.ml
+++ src/lib/timeout_lib/timeout_lib.ml
@@ -85,8 +85,7 @@ module Make (Time : Time_intf) : Timeout_intf(Time).S = struct
       Deferred.create (fun ivar ->
           ignore
             ( create time_controller timeout_duration ~f:(fun x ->
-                  if Ivar.is_full ivar then
-                    [%log' error (Logger.create ())] "Ivar.fill bug is here!" ;
+                  if Ivar.is_full ivar then eprintf "Ivar.fill bug is here!" ;
                   Ivar.fill_if_empty ivar x)
               : unit t ))
     in
