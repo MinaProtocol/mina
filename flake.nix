9b73686c2c2b9a4359ebfa9805f97038ed4338e8
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-12-13 21:49:25 +0100

    nix: add lint-codeowners, lint-rfcs steps

--- flake.nix
+++ flake.nix
@@ -30,9 +30,31 @@
       in {
 
         # todo: Fast
-        # - codeowners + rfcs + snarky + preprocessor deps
-        # - compare ci diff types
-        # - binable
+        checks.lint-codeowners = pkgs.stdenv.mkDerivation {
+          # todo: filter source
+          name = "lint-codeowners";
+          src = ./.;
+          # todo: submodules :(
+          buildPhase = ''
+            mkdir -p src/lib/snarky
+            bash ./scripts/lint_codeowners.sh
+          '';
+          installPhase = "touch $out";
+        };
+        # todo: this check succeeds with 0 rfcs
+        checks.lint-rfcs = pkgs.runCommand "lint-rfcs" {} ''
+          ln -s ${./rfcs} ./rfcs
+          bash ${./scripts/lint_rfcs.sh}
+          touch $out
+        '';
+        # todo: ./scripts/check-snarky-submodule.sh # submodule issue
+        checks.lint-preprocessor-deps = pkgs.runCommand "lint-preprocessor-deps" {} ''
+          ln -s ${./src} ./src
+          bash ${./scripts/lint_preprocessor_deps.sh}
+          touch $out
+        '';
+        # - compare ci diff_types
+        # - compare_ci_diff_binables
 
         # todo: helmchart
         # todo: merges cleanly into develop -- wait why
