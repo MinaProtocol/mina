c17824ee9335459041c3efd1a1566c3543e52367
mrmr1993 <mr_1993@hotmail.co.uk>
2021-12-16 22:16:33 +0000

    Test pasta and vesta snarky instances

--- src/lib/crypto/kimchi_backend/tests.ml
+++ src/lib/crypto/kimchi_backend/tests.ml
@@ -1,18 +1,39 @@
-module Impl =
-  Snarky_backendless.Snark.Run.Make (Kimchi_pasta.Pallas_based_plonk) (Unit)
-open Impl
+module Setup_test (Backend : Snarky_backendless.Backend_intf.S) = struct
+  module Impl = Snarky_backendless.Snark.Run.Make (Backend) (Unit)
+  open Impl
 
-let main x () =
-  let y =
-    exists Field.typ ~compute:(fun () ->
-        Field.Constant.sqrt (As_prover.read_var x))
-  in
-  assert_r1cs y y x
+  let main x () =
+    let y =
+      exists Field.typ ~compute:(fun () ->
+          Field.Constant.sqrt (As_prover.read_var x))
+    in
+    assert_r1cs y y x
+end
 
-let%test_unit "thing" =
-  Kimchi_pasta.Pallas_based_plonk.Keypair.set_urs_info [] ;
-  let _kp = Impl.generate_keypair ~exposing:[ Field.typ ] main in
-  let _witness =
-    Impl.generate_witness [ Field.typ ] main () (Field.Constant.of_int 4)
-  in
-  ()
+let%test_module "pallas" =
+  ( module struct
+    include Setup_test (Kimchi_pasta.Pallas_based_plonk)
+    open Impl
+
+    let%test_unit "test snarky instance" =
+      Kimchi_pasta.Pallas_based_plonk.Keypair.set_urs_info [] ;
+      let _kp = Impl.generate_keypair ~exposing:[ Field.typ ] main in
+      let _witness =
+        Impl.generate_witness [ Field.typ ] main () (Field.Constant.of_int 4)
+      in
+      ()
+  end )
+
+let%test_module "vesta" =
+  ( module struct
+    include Setup_test (Kimchi_pasta.Vesta_based_plonk)
+    open Impl
+
+    let%test_unit "test snarky instance" =
+      Kimchi_pasta.Pallas_based_plonk.Keypair.set_urs_info [] ;
+      let _kp = Impl.generate_keypair ~exposing:[ Field.typ ] main in
+      let _witness =
+        Impl.generate_witness [ Field.typ ] main () (Field.Constant.of_int 4)
+      in
+      ()
+  end )
