3f2c3b1b74bd03cbb7aa1db0ff6d3655906fb40b
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-12-24 17:38:48 +0100

    mina.nix: fix marlin_plonk_bindings/stubs

--- overlay.nix
+++ overlay.nix
@@ -27,6 +27,20 @@ in {
     '';
   };
 
+  marlin_plonk_bindings_stubs = pkgs.rustPlatform.buildRustPackage {
+    pname = "marlin_plonk_bindings_stubs";
+    version = "0.1.0";
+    srcs = [./src/lib/marlin_plonk_bindings/stubs ./src/lib/marlin];
+    nativeBuildInputs = [pkgs.ocaml-ng.ocamlPackages_4_11.ocaml];
+    sourceRoot = "stubs";
+    postUnpack = ''
+      mkdir -p marlin_plonk_bindings
+      mv stubs marlin_plonk_bindings
+      export sourceRoot=marlin_plonk_bindings/stubs
+    '';
+    cargoLock.lockFile = ./src/lib/marlin_plonk_bindings/stubs/Cargo.lock;
+  };
+
   # Jobs/Test/Libp2pUnitTest
   libp2p_helper = pkgs.buildGoModule {
     pname = "libp2p_helper";
