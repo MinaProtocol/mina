09da74e62b2e513631738fb528ef52fe5a109270
Yorick van Pelt <yorick@yorickvanpelt.nl>
2021-12-24 17:38:48 +0100

    mina.nix: fix marlin_plonk_bindings/stubs

--- mina.nix
+++ mina.nix
@@ -73,6 +73,8 @@ let
           sed 's/mina_version.normal/mina_version.dummy/' -i src/lib/mina_version/dune
           sed 's,/usr/local/lib/librocksdb_coda.a,${pkgs.rocksdb}/lib/librocksdb.a,' -i src/external/ocaml-rocksdb/dune
           sed 's,make ,make GO_CAPNP_STD=${pkgs.go-capnproto2.src}/std ,' -i src/libp2p_ipc/dune
+          sed 's,cargo build --release,mkdir target,' -i src/lib/marlin_plonk_bindings/stubs/dune
+          sed 's,target/release,${pkgs.marlin_plonk_bindings_stubs}/lib,' -i src/lib/marlin_plonk_bindings/stubs/dune
           dune build src/app/logproc/logproc.exe src/app/cli/src/mina.exe
         '';
         installPhase = ''
