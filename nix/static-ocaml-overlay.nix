84e77d05c06031771e3abb87aef8e442b69cc34b
Alexander Bantyev <balsoft@balsoft.ru>
2021-12-27 18:20:02 +0300

    Fix ocaml-sodium static build

--- nix/static-ocaml-overlay.nix
+++ nix/static-ocaml-overlay.nix
@@ -1,13 +1,32 @@
-self: super: {
-  "conf-g++" = super."conf-g++".overrideAttrs (oa: {
-    nativeBuildInputs = oa.nativeBuildInputs
-      ++ [ (self.nixpkgs.writeShellScriptBin "g++" ''$CXX "$@"'') ];
+self: super:
+let
+
+  fake-cxx = self.nixpkgs.writeShellScriptBin "g++" ''$CXX "$@"'';
+  fake-cc = self.nixpkgs.writeShellScriptBin "cc" ''$CC "$@"'';
+
+in {
+  "conf-g++" = super."conf-g++".overrideAttrs
+    (oa: { nativeBuildInputs = oa.nativeBuildInputs ++ [ fake-cxx ]; });
+
+  ppx_deriving = self.nixpkgs.pkgsBuildBuild.ocamlPackages.ppx_deriving;
+
+  sodium = super.sodium.overrideAttrs (oa: {
+    buildInputs = oa.buildInputs ++ [ self.nixpkgs.sodium-static ];
+    nativeBuildInputs = oa.nativeBuildInputs ++ [ fake-cc ];
+    buildPhase = ''
+      ocamlbuild lib/sodium.cma lib/sodium.cmxa
+    '';
+    installPhase = ''
+	  ocamlfind install sodium lib/META \
+		  $(addprefix _build/lib/,sodium.mli sodium.cmi sodium.cmti \
+			  sodium.cma sodium.cmx sodium.cmxa sodium.cmxs \
+		                        sodium$(EXT_LIB) \
+					libsodium_stubs$(EXT_LIB))
+    '';
   });
 
-  conf-gmp = super.conf-gmp.overrideAttrs (oa: {
-    nativeBuildInputs = oa.nativeBuildInputs
-      ++ [ (self.nixpkgs.writeShellScriptBin "cc" ''$CC "$@"'') ];
-  });
+  conf-gmp = super.conf-gmp.overrideAttrs
+    (oa: { nativeBuildInputs = oa.nativeBuildInputs ++ [ fake-cc ]; });
 
   base58 = super.base58.overrideAttrs (oa: {
     buildPhase = ''
@@ -33,7 +52,7 @@ self: super: {
       make build-byte build-native
     '';
     installPhase = ''
-      make PREFIX=$out LIBDIR=$OCAMLFIND_DESTDIR install-common install-native
+      make PREFIX=$out LIBDIR=$OCAMLFIND_DESTDIR/cmdliner install-common install-native
     '';
   });
 
