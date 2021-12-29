cf41ba602b32a011140afaef35c97a581d36d641
Brandon Kase <brandon.kase@gmail.com>
2021-12-28 22:32:07 -0600

    Attempts to implement compressed coding

--- src/lib/rosetta_coding/coding.ml
+++ src/lib/rosetta_coding/coding.ml
@@ -125,42 +125,86 @@ let to_public_key raw =
   (to_field raw1, to_field raw2)
 
 module Public_key_compressed_direct = struct
+  (*
+    Go encode:
+
+    x := p1.x.Bytes()
+    x[31] |= (p1.y.Bytes()[0] & 1) << 7
+    return x[:]
+
+    Go decode:
+
+    sign := (input[31] >> 7) & 1
+    input[31] &= 0x7F
+    x = input
+  *)
+  (* break of the bits byte by byte *)
+  let bits_by_n n bits =
+    let rec go bits acc =
+      if List.is_empty bits then List.rev acc
+      else
+        let bitsn, rest = List.split_n bits n in
+        go rest (bitsn :: acc)
+    in
+    go bits []
+
   (* direct hex encoding and decoding of bits in compressed public key
      [encode] differs from [of_public_key_compressed], which converts to an uncompressed key first
   *)
   let encode (pk_compressed : Public_key.Compressed.t) =
     (* of_unpackable assumes a 255-bit value, but we want  *)
     let { Public_key.Compressed.Poly.x; is_odd } = pk_compressed in
-    let field_bits = Field.unpack x |> List.rev in
-    (* insert parity bit as high bit in last byte *)
-    let before_bits, after_bits = List.split_n field_bits 248 in
-    let bits = before_bits @ (is_odd :: after_bits) in
-    let bits_by_4s =
-      let rec go bits acc =
-        if List.is_empty bits then List.rev acc
-        else
-          let bits4, rest = List.split_n bits 4 in
-          go rest (bits4 :: acc)
-      in
-      go bits []
-    in
+    let field_bits = Field.unpack x in
+    (* Our Field representation has zero-th bit first so reverse *)
+    let bits = List.rev field_bits in
+    (* Insert a parity bit at the highest bit of the highest byte *)
+    let bits_parity = is_odd :: bits in
+    (* Convert to bytes *)
+    let bits_by_8s = bits_by_n 8 bits_parity in
+    (* All bytes should have 8 bits now *)
+    List.iter bits_by_8s ~f:(fun byte -> [%test_eq: int] (List.length byte) 8) ;
+    (* Encoding wants highest byte at the end *)
+    let final_bits_by_8s = List.rev bits_by_8s in
+    (* We need by 4s to encode in hex *)
+    let bits_by_4s = List.concat final_bits_by_8s |> bits_by_n 4 in
     let cs = List.map bits_by_4s ~f:bits4_to_hex_char in
     let result = String.of_char_list cs in
+    assert (String.length result = 64) ;
     result
 
   let decode raw : Public_key.Compressed.t =
     assert (String.length raw = 64) ;
-    let bits =
+    (* get the raw bits *)
+    let raw_bits =
       String.to_list raw |> List.map ~f:hex_char_to_bits4 |> List.concat
     in
-    (* high bit of high byte *)
-    let is_odd = Bool.equal (List.nth_exn bits 248) true in
-    (* 0 parity bit, remove padding bit *)
-    let field_bits =
-      List.sub bits ~pos:1 ~len:248 @ (false :: List.sub bits ~pos:249 ~len:7)
+    (* break of the bits byte by byte *)
+    let bits_by_8s = bits_by_n 8 raw_bits in
+    (* the highest byte is at the end and the lowest at the beginning,
+       so we reverse here *)
+    let bits_by_8s_good_order = List.rev bits_by_8s in
+    (* Now the high byte has the parity so get it out *)
+    let high_byte = List.hd_exn bits_by_8s_good_order in
+    let high_bit = List.hd_exn high_byte in
+    (* is_odd if this is true *)
+    let is_odd = Bool.equal high_bit true in
+    (* ungroup the bits *)
+    let bits =
+      (* drop the parity bit *)
+      List.concat bits_by_8s_good_order |> List.tl_exn
     in
+    (* Our Field representation has zero-th bit first so reverse *)
+    let field_bits = List.rev bits in
     let x = Field.project field_bits in
     { Public_key.Compressed.Poly.x; is_odd }
+
+  (*
+    TODO:
+      1. Decode to bytes
+      2. Print out every byte here (and every byte on Go)
+      3. re-arrange to be consistent
+      4. Convert back to bits and then "project"
+    *)
 end
 
 let to_public_key_compressed raw =
@@ -195,32 +239,16 @@ let pk_roundtrip_test () =
   let field0', field1' = to_public_key hex in
   Public_key.equal (field0, field1) (field0', field1')
 
-let hex_key = "834f83195c9480aa1bde8e8b2e24a2cd9186a3825d6ca967ffecc9f9befe1c20"
+let hex_key_odd =
+  "fad1d3e31aede102793fb2cce62b4f1e71a214c94ce18ad5756eba67ef398390"
 
-let test_pk =
-  Public_key.Compressed.of_base58_check
-    "B62qovbUAE4SAq8bT88PZUAyoZDFh65NELvgRf519pHPP3mu7rhrx3z"
+let hex_key_even =
+  "7e406ca640115a8c44ece6ef5d0c56af343b1a993d8c871648ab7980ecaf8230"
 
-let pk_compressed_roundtrip_test () =
-  let field0 = Field.of_int 123123 in
-  let field1 = Field.of_int 234234 in
-  let pk_compressed = Public_key.compress (field0, field1) in
-  let hex = Public_key_compressed_direct.encode pk_compressed in
-  (* do not use of_public_key_compressed *)
-  let pk_compressed' = Public_key_compressed_direct.decode hex in
-  Format.eprintf "ORIG@." ;
-  Format.eprintf "IS_ODD: %B@." pk_compressed.is_odd ;
-  Format.eprintf "FIELD:@." ;
-  List.iter (Field.unpack pk_compressed.x) ~f:(fun b -> Format.eprintf "%B " b) ;
-  Format.eprintf "@." ;
-
-  Format.eprintf "NEW@." ;
-  Format.eprintf "IS_ODD: %B@." pk_compressed'.is_odd ;
-  Format.eprintf "FIELD:@." ;
-  List.iter (Field.unpack pk_compressed'.x) ~f:(fun b -> Format.eprintf "%B " b) ;
-  Format.eprintf "@." ;
-
-  Public_key.Compressed.equal pk_compressed pk_compressed'
+let pk_compressed_roundtrip_test hex_key () =
+  let pk = Public_key_compressed_direct.decode hex_key in
+  let hex' = Public_key_compressed_direct.encode pk in
+  String.equal (String.lowercase hex_key) (String.lowercase hex')
 
 let%test "field_example" = field_example_test ()
 
@@ -228,7 +256,11 @@ let%test "field_hex round-trip" = field_hex_roundtrip_test ()
 
 let%test "public key round-trip" = pk_roundtrip_test ()
 
-let%test "public key compressed roundtrip" = pk_compressed_roundtrip_test ()
+let%test "public key compressed roundtrip odd" =
+  pk_compressed_roundtrip_test hex_key_odd ()
+
+let%test "public key compressed roundtrip even" =
+  pk_compressed_roundtrip_test hex_key_even ()
 
 [%%ifndef consensus_mechanism]
 
@@ -238,7 +270,10 @@ let unit_tests =
   [ ("field example", field_example_test)
   ; ("field-hex round-trip", field_hex_roundtrip_test)
   ; ("public key round-trip", pk_roundtrip_test)
-  ; ("public key compressed round-trip", pk_compressed_roundtrip_test)
+  ; ( "public key compressed round-trip odd"
+    , pk_compressed_roundtrip_test hex_key_odd )
+  ; ( "public key compressed round-trip even"
+    , pk_compressed_roundtrip_test hex_key_even )
   ]
 
 let run_unit_tests () =
