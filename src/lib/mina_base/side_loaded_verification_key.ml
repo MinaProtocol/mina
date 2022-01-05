e3937feb259dbbc9fbfe59cab988e0a32437d932
mrmr1993 <mr_1993@hotmail.co.uk>
2022-01-05 16:21:39 +0000

    Fixup compilation of dummy side_loaded_verification_key for client SDK

--- src/lib/mina_base/side_loaded_verification_key.ml
+++ src/lib/mina_base/side_loaded_verification_key.ml
@@ -54,10 +54,10 @@ module Stable = struct
 
                 let to_binable
                     { Poly.step_data; max_width; wrap_index; wrap_vk = _ } =
-                  { Repr.Stable.V1.step_data; max_width; wrap_index }
+                  { Repr.Stable.V2.step_data; max_width; wrap_index }
 
                 let of_binable
-                    { Repr.Stable.V1.step_data; max_width; wrap_index = c } =
+                    { Repr.Stable.V2.step_data; max_width; wrap_index = c } =
                   { Poly.step_data
                   ; max_width
                   ; wrap_index = c
@@ -74,25 +74,16 @@ let dummy : t =
   { step_data = At_most.[]
   ; max_width = Pickles_base.Side_loaded_verification_key.Width.zero
   ; wrap_index =
-      (let g = [ Snarkette.Pasta.Pallas.(to_affine_exn one) ] in
-       { sigma_comm_0 = g
-       ; sigma_comm_1 = g
-       ; sigma_comm_2 = g
-       ; ql_comm = g
-       ; qr_comm = g
-       ; qo_comm = g
-       ; qm_comm = g
-       ; qc_comm = g
-       ; rcm_comm_0 = g
-       ; rcm_comm_1 = g
-       ; rcm_comm_2 = g
+      (let g = Snarkette.Pasta.Pallas.(to_affine_exn one) in
+       { sigma_comm = Vector.init Dlog_plonk_types.Permuts.n ~f:(fun _ -> g)
+       ; coefficients_comm =
+           Vector.init Dlog_plonk_types.Columns.n ~f:(fun _ -> g)
+       ; generic_comm = g
        ; psm_comm = g
-       ; add_comm = g
-       ; mul1_comm = g
-       ; mul2_comm = g
-       ; emul1_comm = g
-       ; emul2_comm = g
-       ; emul3_comm = g
+       ; complete_add_comm = g
+       ; mul_comm = g
+       ; emul_comm = g
+       ; endomul_scalar_comm = g
        })
   ; wrap_vk = None
   }
