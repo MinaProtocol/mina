532588af567b7e9a6c157163a8bacc3b2491160f
Gregor <gregor.mitscha-baude@gmx.at>
2022-01-06 18:35:59 +0100

    scripts

--- build_chrome_and_copy.sh
+++ build_chrome_and_copy.sh
@@ -1,10 +1,10 @@
 dune b src/lib/snarky_js_bindings/snarky_js_chrome.bc.js
-rm -r ~/repos/snarkyjs/src/chrome_bindings/snippets
-cp -r _build/default/src/lib/crypto/kimchi_bindings/js/chrome/snippets ~/repos/snarkyjs/src/chrome_bindings/snippets
-cp _build/default/src/lib/crypto/kimchi_bindings/js/chrome/plonk_wasm* ~/repos/snarkyjs/src/chrome_bindings/
-cp _build/default/src/lib/snarky_js_bindings/*.js ~/repos/snarkyjs/src/chrome_bindings/
+#rm -r ~/repos/snarkyjs/src/chrome_bindings/snippets
+#cp -r _build/default/src/lib/crypto/kimchi_bindings/js/chrome/snippets ~/repos/snarkyjs/src/chrome_bindings/snippets
+cp _build/default/src/lib/crypto/kimchi_bindings/js/chrome/plonk_wasm* ../snarkyjs/src/chrome_bindings/
+cp _build/default/src/lib/snarky_js_bindings/snarky_js_chrome*.js ../snarkyjs/src/chrome_bindings/
 
-pushd ~/repos/snarkyjs/src/chrome_bindings
-  wasm-opt -O4 plonk_wasm_bg.wasm -o plonk_wasm_bg.wasm.opt
+pushd ../snarkyjs/src/chrome_bindings
+  wasm-opt --detect-features --enable-mutable-globals -O4 plonk_wasm_bg.wasm -o plonk_wasm_bg.wasm.opt
   mv plonk_wasm_bg.wasm.opt plonk_wasm_bg.wasm
 popd
