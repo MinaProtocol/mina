10cbefe46a3ff8ef9b0874204a99c4c80348f938
Helena Li <helena.li0x1@gmail.com>
2022-01-06 16:28:49 -0800

    reproducing peer graph check

--- src/app/test_executive/peers_reliability_test.ml
+++ src/app/test_executive/peers_reliability_test.ml
@@ -27,25 +27,53 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
     ; num_snark_workers = 0
     }
 
+  open Graph_algorithms
+
+  module X = struct
+    include String
+
+    type display = string [@@deriving yojson]
+
+    let display = Fn.id
+
+    let name = Fn.id
+  end
+
+  module G = Visualization.Make_ocamlgraph (X)
+
+  let graph_of_adjacency_list adj =
+    List.fold adj ~init:G.empty ~f:(fun acc (x, xs) ->
+        let acc = G.add_vertex acc x in
+        List.fold xs ~init:acc ~f:(fun acc y ->
+            let acc = G.add_vertex acc y in
+            G.add_edge acc x y))
+
   let run network t =
     let open Network in
     let open Malleable_error.Let_syntax in
     let logger = Logger.create () in
-    (* TEMP: until we fix the seed graphql port, we will only check peers for block producers *)
-    (* let all_nodes = Network.all_nodes network in *)
-    let all_nodes = Network.block_producers network in
+    let all_nodes = Network.all_nodes network in
+    (* let all_nodes = Network.block_producers network in *)
     let[@warning "-8"] [ node_a; node_b; node_c ] =
       Network.block_producers network
     in
+    [%log info] "peers_list"
+      ~metadata:
+        [ ("peers", `List (List.map all_nodes ~f:(fun n -> `String (Node.id n))))
+        ] ;
     (* TODO: let%bind () = wait_for t (Wait_condition.nodes_to_initialize [node_a; node_b; node_c]) in *)
     let%bind () =
-      Malleable_error.List.iter [ node_a; node_b; node_c ]
+      Malleable_error.List.iter all_nodes
         ~f:(Fn.compose (wait_for t) Wait_condition.node_to_initialize)
     in
     let%bind () =
       section "network is fully connected upon initialization"
         (Util.check_peers ~logger all_nodes)
     in
+    (* let () =
+         Out_channel.with_file "/tmp/network-graph.dot" ~f:(fun c ->
+             G.output_graph c (graph_of_adjacency_list query_results) )
+       in *)
     let%bind _ =
       section "blocks are produced"
         (wait_for t (Wait_condition.blocks_to_be_produced 1))
