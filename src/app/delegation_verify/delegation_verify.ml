b600f7ab6b57cf380ac0d614087715d999b9e0df
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2022-01-25 19:44:14 +0800

    adding an option "no-checks" to bypass all the checks

--- src/app/delegation_verify/delegation_verify.ml
+++ src/app/delegation_verify/delegation_verify.ml
@@ -77,7 +77,7 @@ let verify_snark_work ~proof ~message =
   let%map result = Verifier.verify_transaction_snarks [ (proof, message) ] in
   if result then Ok () else Error `Invalid_snark_work
 
-let validate_submission ~block_dir ~metadata_path =
+let validate_submission ~block_dir ~metadata_path ~no_checks =
   let open Deferred.Result.Let_syntax in
   let%bind () = Deferred.return @@ check_path metadata_path in
   let%bind metadata_str = Deferred.return @@ load_metadata metadata_path in
@@ -86,34 +86,40 @@ let validate_submission ~block_dir ~metadata_path =
   in
   let%bind block_str = Deferred.return @@ load_block ~block_dir ~block_hash in
   let%bind block = Deferred.return @@ decode_block block_str in
-  let%bind () = verify_block ~block in
+  let%bind () = if no_checks then return () else verify_block ~block in
   let%map () =
-    match snark_work_opt with
-    | None ->
-        Deferred.Result.return ()
-    | Some snark_work_str ->
-        let%bind Uptime_service.Proof_data.
-                   { proof; proof_time = _; snark_work_fee } =
-          Deferred.return @@ decode_snark_work snark_work_str
-        in
-        let message =
-          Mina_base.Sok_message.create ~fee:snark_work_fee ~prover:submitter
-        in
-        verify_snark_work ~proof ~message
+    if no_checks then return ()
+    else
+      match snark_work_opt with
+      | None ->
+          Deferred.Result.return ()
+      | Some snark_work_str ->
+          let%bind Uptime_service.Proof_data.
+                     { proof; proof_time = _; snark_work_fee } =
+            Deferred.return @@ decode_snark_work snark_work_str
+          in
+          let message =
+            Mina_base.Sok_message.create ~fee:snark_work_fee ~prover:submitter
+          in
+          verify_snark_work ~proof ~message
   in
   ( External_transition.state_hash block
+  , External_transition.parent block
   , External_transition.blockchain_length block
   , External_transition.global_slot block )
 
 type valid_payload =
   { state_hash : State_hash.t
+  ; parent : State_hash.t
   ; height : Unsigned.uint32
   ; slot : Unsigned.uint32
   }
 
-let valid_payload_to_yojson { state_hash; height; slot } : Yojson.Safe.t =
+let valid_payload_to_yojson { state_hash; parent; height; slot } : Yojson.Safe.t
+    =
   `Assoc
     [ ("state_hash", State_hash.to_yojson state_hash)
+    ; ("parent", State_hash.to_yojson parent)
     ; ("height", `Int (Unsigned.UInt32.to_int height))
     ; ("slot", `Int (Unsigned.UInt32.to_int slot))
     ]
@@ -133,14 +139,22 @@ let command =
         flag "--block-dir" ~aliases:[ "-block-dir" ]
           ~doc:"the path to the directory containing blocks for the submission"
           (required Filename.arg_type)
-      and inputs = anon (sequence ("filename" %: Filename.arg_type)) in
+      and inputs = anon (sequence ("filename" %: Filename.arg_type))
+      and no_checks =
+        flag "--no-checks" ~aliases:[ "-no-checks" ]
+          ~doc:
+            "disable all the checks, just extract the info from the submissions"
+          no_arg
+      in
       fun () ->
         let open Deferred.Let_syntax in
         let metadata_pathes = get_filenames inputs in
         Deferred.List.iter metadata_pathes ~f:(fun metadata_path ->
-            match%bind validate_submission ~block_dir ~metadata_path with
-            | Ok (state_hash, height, slot) ->
-                display { state_hash; height; slot } ;
+            match%bind
+              validate_submission ~block_dir ~metadata_path ~no_checks
+            with
+            | Ok (state_hash, parent, height, slot) ->
+                display { state_hash; parent; height; slot } ;
                 Deferred.unit
             | Error `Path_is_invalid ->
                 display_error "path for metadata is invalid" ;
