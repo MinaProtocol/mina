73325455cff3515c79150d1f2a8a4cfeea8da804
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2022-01-26 23:39:08 +0800

    delay the creation of the verifier

--- src/app/delegation_verify/verifier.ml
+++ src/app/delegation_verify/verifier.ml
@@ -14,10 +14,30 @@ module B = Blockchain_snark.Blockchain_snark_state.Make (struct
   let proof_level = Genesis_constants.Proof_level.compiled
 end)
 
-let verify_blockchain_snarks (chains : Blockchain_snark.Blockchain.t list) =
+let verify_blockchain_snarks_ (chains : Blockchain_snark.Blockchain.t list) =
   B.Proof.verify
   @@ List.map chains ~f:(fun snark ->
          ( Blockchain_snark.Blockchain.state snark
          , Blockchain_snark.Blockchain.proof snark ))
 
-let verify_transaction_snarks ts = T.verify ts
+let verify_blockchain_snarks =
+  lazy
+    (let module B = Blockchain_snark.Blockchain_snark_state.Make (struct
+       let tag = T.tag
+
+       let constraint_constants =
+         Genesis_constants.Constraint_constants.compiled
+
+       let proof_level = Genesis_constants.Proof_level.compiled
+     end) in
+    B.Proof.verify)
+
+let verify_transaction_snarks =
+  lazy
+    (let module T = Transaction_snark.Make (struct
+       let constraint_constants =
+         Genesis_constants.Constraint_constants.compiled
+
+       let proof_level = Genesis_constants.Proof_level.compiled
+     end) in
+    T.verify)
