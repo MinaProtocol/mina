975949c281c0e17ccbf124c9cad48cd3ff8b8ca5
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2022-01-27 00:04:23 +0800

    wrap verification in a pair of functions

--- src/app/delegation_verify/verifier.ml
+++ src/app/delegation_verify/verifier.ml
@@ -1,38 +1,4 @@
-open Core
-
-module T = Transaction_snark.Make (struct
-  let constraint_constants = Genesis_constants.Constraint_constants.compiled
-
-  let proof_level = Genesis_constants.Proof_level.compiled
-end)
-
-module B = Blockchain_snark.Blockchain_snark_state.Make (struct
-  let tag = T.tag
-
-  let constraint_constants = Genesis_constants.Constraint_constants.compiled
-
-  let proof_level = Genesis_constants.Proof_level.compiled
-end)
-
-let verify_blockchain_snarks_ (chains : Blockchain_snark.Blockchain.t list) =
-  B.Proof.verify
-  @@ List.map chains ~f:(fun snark ->
-         ( Blockchain_snark.Blockchain.state snark
-         , Blockchain_snark.Blockchain.proof snark ))
-
-let verify_blockchain_snarks =
-  lazy
-    (let module B = Blockchain_snark.Blockchain_snark_state.Make (struct
-       let tag = T.tag
-
-       let constraint_constants =
-         Genesis_constants.Constraint_constants.compiled
-
-       let proof_level = Genesis_constants.Proof_level.compiled
-     end) in
-    B.Proof.verify)
-
-let verify_transaction_snarks =
+let verify_functions =
   lazy
     (let module T = Transaction_snark.Make (struct
        let constraint_constants =
@@ -40,4 +6,11 @@ let verify_transaction_snarks =
 
        let proof_level = Genesis_constants.Proof_level.compiled
      end) in
-    T.verify)
+    let module B = Blockchain_snark.Blockchain_snark_state.Make (struct
+      let tag = T.tag
+
+      let constraint_constants = Genesis_constants.Constraint_constants.compiled
+
+      let proof_level = Genesis_constants.Proof_level.compiled
+    end) in
+    (B.Proof.verify, T.verify))
