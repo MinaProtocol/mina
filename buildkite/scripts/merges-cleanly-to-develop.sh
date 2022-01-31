90e3ee9ff241f13a82f6c1ca1a1b553d6001ebaf
Andrew Trainor <andrew@o1labs.org>
2022-01-31 18:49:19 -0500

    lk86/fix-merges-cleanly-job Quick and dirty PoC

--- buildkite/scripts/merges-cleanly-to-develop.sh
+++ buildkite/scripts/merges-cleanly-to-develop.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" != "compatible" ]; then exit 0; fi
-
 # Adapted from this stackoverflow answer: https://stackoverflow.com/a/10856937
 # The git merge-tree command shows the content of a 3-way merge without
 # touching the index, which we can then search for conflict markers.
-git merge-tree `git merge-base origin/develop HEAD` origin/develop HEAD | grep "^<<<<<<<"
 
-RET=$?
+for BRANCH in compatible develop feature/snapps-protocol; do
+  git merge-tree `git merge-base origin/$BRANCH HEAD` origin/$BRANCH HEAD | grep "^<<<<<<<"
+  RET=$?
 
-if [ $RET -eq 0 ]; then
-  # Found a conflict
-  exit 1
-else
-  # No conflicts were found
-  exit 0
-fi
+  if [ $RET -eq 0 ]; then
+    # Found a conflict
+    echo "This pull request conflicts with $BRANCH , please open a new PR https://github.com/MinaProtocol/mina/compare/${BRANCH}...${BUILDKITE_BRANCH}"
+    exit 1
+  else
+    # No conflicts were found
+    exit 0
+  fi
+done
+
