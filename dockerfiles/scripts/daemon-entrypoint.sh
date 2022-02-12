42b0bcd5e378840ddfff8506f0ee760a97bf725c
Andrew Trainor <andrew@o1labs.org>
2022-02-12 18:19:27 -0500

    lk86/improve-docker-entrypoint Update daemon entrypoint to stop printing all commands and to support environment varibles similar to mina.service

--- dockerfiles/scripts/daemon-entrypoint.sh
+++ dockerfiles/scripts/daemon-entrypoint.sh
@@ -6,16 +6,15 @@ set -euo pipefail
 shopt -s nullglob
 
 
-set -x
-
 INPUT_ARGS="$@"
 
 # These arrays can be overwritten or extended in scripts to adjust verbosity
-# Example: LOG_FILES+=("${VERBOSE_LOG_FILES[@]}")
-declare -a LOG_FILES=('mina.log')
+# Example:
+declare -a LOG_FILES=('mina.log' 'mina-errors.log')
 declare -a VERBOSE_LOG_FILES=('.mina-config/mina-prover.log' '.mina-config/mina-verifier.log' '.mina-config/mina-best-tip.log')
 
 # Attempt to execute or source custom entrypoint scripts accordingly
+# Example: mount a mina-env file with variable evironment variables to source and pass to the daemon
 for script in /entrypoint.d/*; do
   if [ -x "$script" ]; then
     "$script" $INPUT_ARGS
@@ -24,16 +23,30 @@ for script in /entrypoint.d/*; do
   fi
 done
 
+# If VERBOSE=true then also append other log files
+if [[ ${VERBOSE} ]]; then
+  LOG_FILES+=("${VERBOSE_LOG_FILES[@]}")
+fi
+# Support flags from .mina-env on debian
+if [[ ${PEER_LIST_URL} ]]; then
+  EXTRA_FLAGS+=" --peer-list-url ${PEER_LIST_URL}"
+fi
+if [[ ${LOG_LEVEL} ]]; then
+  EXTRA_FLAGS+=" --log-level ${LOG_LEVEL}"
+fi
+if [[ ${FILE_LOG_LEVEL} ]]; then
+  EXTRA_FLAGS+=" --file-log-level ${FILE_LOG_LEVEL}"
+fi
+
 # Mina daemon initialization
 mkdir -p .mina-config
-touch .mina-config/mina-prover.log
-touch .mina-config/mina-verifier.log
-touch .mina-config/mina-best-tip.log
+# Create all of the log files that we will tail later
+touch "${LOG_FILES[@]}"
 
-set +ex # Allow wait and kill commands to fail in this loop, don't print these commands
+set +e # Allow wait and kill commands to fail in this loop
 while true; do
   rm -f .mina-config/.mina-lock
-  mina $INPUT_ARGS 2>&1 >mina.log &
+  mina $INPUT_ARGS $EXTRA_ARGS 2>mina-errors.log 1>mina.log &
   mina_pid=$!
 
   tail -q -f "${LOG_FILES[@]}" &
