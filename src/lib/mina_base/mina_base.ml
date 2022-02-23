6c7176397076350bbd2eb31121158cc381833743
Gregor <gregor.mitscha-baude@gmx.at>
2022-02-23 12:42:39 +0100

    remove from mina_base.ml

--- src/lib/mina_base/mina_base.ml
+++ src/lib/mina_base/mina_base.ml
@@ -1,7 +1,5 @@
 module Sparse_ledger0 = Sparse_ledger
 module Ledger_transfer = Ledger_transfer
-module Snapp_generators = Snapp_generators
-module User_command_generators = User_command_generators
 module Rpc_intf = Rpc_intf
 module Transaction_validator = Transaction_validator
 module Ledger = Ledger
