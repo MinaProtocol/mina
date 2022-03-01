ef62a9af385b4d3b759853b76bab284ea25819a0
gregor <gregor.mitscha-baude@gmx.at>
2022-03-01 19:05:46 +0100

    wip js tests

--- src/lib/snarky_js_bindings/tests/snarkyjs.js
+++ src/lib/snarky_js_bindings/tests/snarkyjs.js
@@ -1,22 +1,22 @@
+/* eslint-env node */
 let fs = require("fs");
 let path = require("path");
 
 let dir = path.dirname(__filename);
-let wasmPath = path.resolve(
-  dir,
-  "../../../../_build/default/src/lib/crypto/kimchi_bindings/js/node_js"
+let resolve = (file) => path.resolve(dir, file);
+let build = "../../../../_build/default";
+let wasmPath = resolve(`${build}/src/lib/crypto/kimchi_bindings/js/node_js`);
+let snarkyPath = resolve(
+  `${build}/src/lib/snarky_js_bindings/snarky_js_node.bc.js`
 );
-let snarkyPath = path.resolve(
-  dir,
-  "../../../../_build/default/src/lib/snarky_js_bindings/snarky_js_node.bc.js"
-);
+let nodeModules = resolve("node_modules");
 
 // set up node_modules
-if (!fs.existsSync("node_modules")) {
-  fs.mkdirSync("node_modules");
-  fs.mkdirSync("node_modules/env");
-  fs.writeFileSync("node_modules/env/index.js", "module.exports = {};");
-  fs.mkdirSync("node_modules/snarkyjs");
+if (!fs.existsSync(nodeModules)) {
+  fs.mkdirSync(nodeModules);
+  fs.mkdirSync(`${nodeModules}/env`);
+  fs.writeFileSync(`${nodeModules}/env/index.js`, "module.exports = {};");
+  fs.mkdirSync(`${nodeModules}/snarkyjs`);
 }
 // copy over js artifacts
 try {
@@ -30,9 +30,7 @@ try {
 fs.readdirSync(wasmPath)
   .filter((f) => f.startsWith("plonk_wasm"))
   .forEach((file) => {
-    fs.cpSync(`${wasmPath}/${file}`, `node_modules/snarkyjs/${file}`);
+    fs.cpSync(`${wasmPath}/${file}`, `${nodeModules}/snarkyjs/${file}`);
   });
 
-let snarkyjs = require("snarkyjs");
-
-module.exports = snarkyjs;
+module.exports = require("snarkyjs");
