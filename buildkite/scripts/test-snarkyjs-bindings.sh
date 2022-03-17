5f79da0fe3270507a9eeb85e8bc270bb1a1ab252
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-17 17:21:36 +0100

    get rust toolchain before running snarkyjs test

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -4,6 +4,10 @@ set -eo pipefail
 
 # run snarkyjs tests in node
 
+echo "Installing Rust dependencies..."
+rustup toolchain install nightly-2021-11-16
+rustup component add rust-src --toolchain nightly-2021-11-16
+
 echo "Building SnarkyJS.."
 source ~/.profile
 dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js
