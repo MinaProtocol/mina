fd396a851df2e0b7a6aa9a20a4a2e55d1aca9c28
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-17 15:37:40 +0100

    [snarkyjs] refactor JS test

--- src/lib/snarky_js_bindings/tests/tests.mjs
+++ src/lib/snarky_js_bindings/tests/tests.mjs
@@ -1,35 +1,44 @@
-import snarkyjs from "./snarkyjs.js";
-await snarkyjs.snarky_ready;
-
-let { Poseidon, Circuit, Field } = snarkyjs;
-
-let FieldTyp = {
-  sizeInFields: () => 1,
-  toFields: (f) => f,
-  ofFields: (f) => f,
-};
-
 // test proving a basic circuit
 
+import snarkyjs from "./snarkyjs.js";
+await snarkyjs.snarky_ready;
+let { Poseidon, Circuit, Field } = snarkyjs;
+
+export { basicCircuitTest };
 class Main extends Circuit {
-  static snarkyMain([preimage], [hash]) {
-    Poseidon.hash([preimage]).assertEquals(hash);
+  static snarkyMain(preimage, [hash]) {
+    Poseidon.hash(preimage).assertEquals(hash);
   }
-  static snarkyWitnessTyp = FieldTyp;
-  static snarkyPublicTyp = FieldTyp;
+  static snarkyWitnessTyp = FieldTyp(4);
+  static snarkyPublicTyp = FieldTyp(1);
 }
 
-let preimage = Field.one;
-let hash = Poseidon.hash([preimage]);
+function basicCircuitTest() {
+  let name = "basic circuit test";
+  console.log(name);
+  let preimage = [
+    Field.random(),
+    Field.random(),
+    Field.random(),
+    Field.random(),
+  ];
+  let hash = Poseidon.hash(preimage);
 
-console.log("generating keypair...");
-let kp = Main.generateKeypair();
+  console.log("generating keypair...");
+  let kp = Main.generateKeypair();
 
-console.log("prove...");
-let proof = Main.prove([preimage], [hash], kp);
+  console.log("prove...");
+  let proof = Main.prove(preimage, [hash], kp);
 
-console.log("verify...");
-let ok = Main.verify([hash], kp.verificationKey(), proof);
+  console.log("verify...");
+  let ok = Main.verify([hash], kp.verificationKey(), proof);
 
-console.log("ok?", ok);
-snarkyjs.shutdown();
+  console.log("ok?", ok);
+  if (!ok) throw Error(`${name} failed`);
+}
+
+let FieldTyp = (size) => ({
+  sizeInFields: () => size,
+  toFields: (f) => f,
+  ofFields: (f) => f,
+});
