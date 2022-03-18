495f522a0a8bdaa26c19c508700ffb4dd3b39772
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-18 17:27:37 +0100

    [snarkyjs] set ulimit -s unlimited before CI build

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -6,6 +6,7 @@ set -eo pipefail
 
 echo "Building SnarkyJS.."
 source ~/.profile
+ulimit -s unlimited
 dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js --profile=dev
 
 echo "Running tests in Javascript"
