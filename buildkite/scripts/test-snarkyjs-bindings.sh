0e46ee64c20ca3ecf69d6267c88f8545f3b2e1ab
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-18 14:33:56 +0100

    [snarkyjs] fix test

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 # run snarkyjs tests in node
 
 echo "Building SnarkyJS.."
+source ~/.profile
 dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js || dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js
 
 echo "Running tests in Javascript"
