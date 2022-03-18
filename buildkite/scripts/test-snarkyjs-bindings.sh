d973d64b7b003fbdeba45dbdc149cdc8264b7566
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-18 14:29:22 +0100

    [snarkyjs] change test script

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -4,12 +4,8 @@ set -eo pipefail
 
 # run snarkyjs tests in node
 
-echo "Installing Rust dependencies..."
-rustup toolchain install nightly-2021-11-16
-rustup component add rust-src --toolchain nightly-2021-11-16
-
 echo "Building SnarkyJS.."
-source ~/.profile
-dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js
+dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js || dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js
+
 echo "Running tests in Javascript"
 node src/lib/snarky_js_bindings/tests/run-tests.mjs
