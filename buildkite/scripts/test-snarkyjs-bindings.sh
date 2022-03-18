9b87146235a2c589a7b7d336c48c0507616be6cc
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-18 17:30:54 +0100

    [snarkyjs] try to add files which CI fails on

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -7,6 +7,7 @@ set -eo pipefail
 echo "Building SnarkyJS.."
 source ~/.profile
 ulimit -s unlimited
+touch src/lib/crypto/kimchi_bindings/js/node_js/node_backend.ml src/lib/crypto/kimchi_bindings/js/node_js/node_backend.mli
 dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js --profile=dev
 
 echo "Running tests in Javascript"
