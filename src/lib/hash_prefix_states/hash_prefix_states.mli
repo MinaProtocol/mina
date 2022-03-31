81a48307ab04a6fe9899c1f048a67b6ada5badfe
Izaak Meckler <ihmeckler@gmail.coom>
2022-03-30 20:20:08 -0700

    new hash prefixes

--- src/lib/hash_prefix_states/hash_prefix_states.mli
+++ src/lib/hash_prefix_states/hash_prefix_states.mli
@@ -57,6 +57,8 @@ val party_node : Field.t State.t
 
 val party_stack_frame : Field.t State.t
 
+val party_stack_frame_cons : Field.t State.t
+
 val party_with_protocol_state_predicate : Field.t State.t
 
 val receipt_chain_user_command : Field.t State.t
