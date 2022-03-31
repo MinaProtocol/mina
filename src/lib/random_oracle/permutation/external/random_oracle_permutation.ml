5cd560a87bb41dffef0a994ba4a2f98a1ec5d6e3
Gregor <gregor.mitscha-baude@gmx.at>
2022-03-31 16:11:50 +0200

    fix kimchi dependencies

--- src/lib/random_oracle/permutation/external/random_oracle_permutation.ml
+++ src/lib/random_oracle/permutation/external/random_oracle_permutation.ml
@@ -1,9 +1,6 @@
 open Core_kernel
-
 module Inputs = Pickles.Tick_field_sponge.Inputs
-
 module Ocaml_permutation = Sponge.Poseidon (Inputs)
-
 module Field = Kimchi_backend.Pasta.Basic.Fp
 
 let add_assign = Ocaml_permutation.add_assign
@@ -13,13 +10,15 @@ let copy = Ocaml_permutation.copy
 let params = Kimchi_pasta_fp_poseidon.create ()
 
 let block_cipher _params (s : Field.t array) =
-  let v = Kimchi.FieldVectors.Fp.create () in
-  Array.iter s ~f:(Kimchi.FieldVectors.Fp.emplace_back v) ;
+  let v = Kimchi_bindings.FieldVectors.Fp.create () in
+  Array.iter s ~f:(Kimchi_bindings.FieldVectors.Fp.emplace_back v) ;
   Kimchi_pasta_fp_poseidon.block_cipher params v ;
-  Array.init (Array.length s) ~f:(Kimchi.FieldVectors.Fp.get v)
+  Array.init (Array.length s) ~f:(Kimchi_bindings.FieldVectors.Fp.get v)
 
 let%test_unit "check rust implementation of block-cipher" =
-  let params' : Field.t Sponge.Params.t = Sponge.Params.(map pasta_p_kimchi ~f:Field.of_string) in
+  let params' : Field.t Sponge.Params.t =
+    Sponge.Params.(map pasta_p_kimchi ~f:Field.of_string)
+  in
   let open Pickles.Impls.Step in
   let module T = Internal_Basic in
   Quickcheck.test (Quickcheck.Generator.list_with_length 3 T.Field.gen)
