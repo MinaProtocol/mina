1c6fc07f744d2ce902ad19d6c381672650c2eff9
Izaak Meckler <ihmeckler@gmail.coom>
2022-03-30 20:21:59 -0700

    call_stack digest

---
+++ src/lib/mina_base/call_stack_digest.mli
@@ -0,0 +1,19 @@
+open Core_kernel
+
+include Digest_intf.S
+
+val cons : Stack_frame.Digest.t -> t -> t
+
+val empty : t
+
+val gen : t Quickcheck.Generator.t
+
+open Pickles.Impls.Step
+
+module Checked : sig
+  include Digest_intf.S_checked
+
+  val cons : Stack_frame.Digest.Checked.t -> t -> t
+end
+
+include Digest_intf.S_aux with type t := t and type checked := Checked.t
