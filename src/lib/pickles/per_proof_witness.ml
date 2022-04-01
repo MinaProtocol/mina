5a4867eeed7f7b878f1b76c352aa6933acb615b0
Izaak Meckler <ihmeckler@gmail.coom>
2022-03-31 18:40:26 -0700

    comment explaining the prev challenges

--- src/lib/pickles/per_proof_witness.ml
+++ src/lib/pickles/per_proof_witness.ml
@@ -4,6 +4,42 @@ open Import
 module Impl = Impls.Step
 module One_hot_vector = One_hot_vector.Make (Impl)
 
+(* Let F, K be the two fields (either (Fp, Fq) or (Fq, Fp)).
+   Each proof over F has an accumulator state which contains
+   - a set of IPA challenges c_0, ..., c_{k-1}, which can be interpreted as F elements.
+   - a polynomial commitment challenge_polynomial_commitment, which has coordinates in K.
+
+   This part of the accumulator state is finalized by checking that challenge_polynomial_commitment
+   is a commitment to the polynomial
+
+   f_c := prod_{i = 0}^{k-1} (1 + c_i x^{2^{k-1 - i}})
+
+   When incrementally-verifying such a proof in a K-circuit (since we need K-arithmetic to perform
+   the group operations on the F-polynomial-commitments that constitute the proof),
+   instead of checking this, we get an evaluation E_c at a random point zeta and check
+   that challenge_polynomial_commitment opens to E_c at zeta. Then we will need to check that
+   E_c = f_c(zeta) = prod_{i = 0}^{k-1} (1 + c_i zeta^{2^{k-1 - i}}).
+
+   However, because we are then in a K-circuit, we cannot actually compute f_c(zeta), which requires
+   F-arithmetic.
+
+   Therefore, after incrementally verifying the F-proof, the challenges (c_0, ..., c_{k-1}) which are
+   part of the accumulator of the F-proof we just incrementally verified, must remain part of
+   the accumulator of the K-proof, along with E_c (or at least some kind of commitment to it, which is
+   what we actually do).
+
+   Then, when we incrementally verify that K-proof in an F-circuit, we will check that E_c was
+   computed correctly using c_0, ..., c_{k-1} and they along with E_c can be discarded.
+
+   So, to summarize, the accumulator state for each proof P (corresponding to an F-circuit)
+   contains
+   - a set of IPA challenges (thought of as K-elements) for every proof that it incrementally verified inside its circuit
+   - a challenge_polynomial_commitment (coordinates in F) for every proof that it incrementally verified inside its circuit
+   - a challenge_polynomial_commitment (coordinates in K) corresponding to P's own inner-product argument
+   - a set of IPA challenges (thought of as F-elements) corresponding to P's own inner-product argument
+*)
+
+
 (** Represents a proof (along with its accumulation state) which wraps a
     "step" proof S on the other curve.
 
