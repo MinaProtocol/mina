372f4d76829f068eb90eecf45934df7639e82cc7
Gregor <gregor.mitscha-baude@gmx.at>
2022-04-01 16:33:04 +0200

    test mina-signer in snarkyjs unit tests

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -8,6 +8,12 @@ echo "Building SnarkyJS.."
 source ~/.profile
 make snarkyjs
 
-echo "Running tests in Javascript"
+echo "Running SnarkyJS unit tests"
 node --version
 node src/lib/snarky_js_bindings/tests/run-tests.mjs
+
+echo "Building MinaSigner"
+make mina_signer
+
+echo "Running MinaSigner unit tests"
+npm --prefix=frontend/mina-signer test
