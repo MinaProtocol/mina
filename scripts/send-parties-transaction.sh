0d93ed7e3a6676f37b416703245c6fe01d89b155
Brandon Kase <brandon.kase@gmail.com>
2022-04-02 16:30:44 +1100

    Fixes errors with script

--- scripts/send-parties-transaction.sh
+++ scripts/send-parties-transaction.sh
@@ -9,4 +9,4 @@ set -x
 curl \
   -X POST \
   -H "Content-Type: application/json" \
-  -d "$(printf '{ "query": "mutation($parties: SendSnappInput!) { sendSnapp(input: $parties) { %s } }" , "variables": { "parties": %s } }' "$1" "$2")" $3
+  -d "$(printf '{ "query": "mutation($input: SendSnappInput!) { sendSnapp(input: $input) %s }" , "variables": { "input": { "parties": %s } } }' "$1" "$2")" $3
