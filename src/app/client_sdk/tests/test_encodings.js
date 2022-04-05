bac0b15b08e7a52a0be52adc74548b585cf9cdc8
Gregor <gregor.mitscha-baude@gmx.at>
2022-04-05 11:36:13 +0200

    revert using js_backend in client sdk, mina-signer

--- src/app/client_sdk/tests/test_encodings.js
+++ src/app/client_sdk/tests/test_encodings.js
@@ -1,7 +1,6 @@
 // test_encodings.js -- print Rosetta encodings of a couple of public keys
 
-var mina =
-  require("../../../../_build/default/src/app/client_sdk/client_sdk.bc.js").minaSDK;
+var mina = require("./client_sdk.js").minaSDK;
 
 var pk1 = "B62qrcFstkpqXww1EkSGrqMCwCNho86kuqBd4FrAAUsPxNKdiPzAUsy";
 var pk2 = "B62qkfHpLpELqpMK6ZvUTJ5wRqKDRF3UHyJ4Kv3FU79Sgs4qpBnx5RR";
@@ -11,3 +10,5 @@ var enc2 = mina.rawPublicKeyOfPublicKey(pk2);
 
 console.log(enc1);
 console.log(enc2);
+
+mina.shutdown();
