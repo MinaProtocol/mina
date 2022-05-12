88f0bc16a4507137458d2583fba94aa39266f17d
Alexander Bantyev <alexander.bantyev@tweag.io>
2022-05-12 11:49:10 +0300

    Build docker images using cloud build

---
+++ dockerfiles/cloudbuild-schema.dhall
@@ -0,0 +1,89 @@
+let Map =
+      https://raw.githubusercontent.com/dhall-lang/dhall-lang/master/Prelude/Map/package.dhall
+
+let Volume
+    : Type
+    = { name : Text, path : Text }
+
+let Artifacts
+    : Type
+    = { objects : { location : Text, paths : List Text } }
+
+let Step =
+      { Type =
+          { name : Text
+          , args : Optional (List Text)
+          , env : Optional (List Text)
+          , dir : Optional Text
+          , timeout : Optional Text
+          , script : Optional Text
+          , entrypoint : Optional Text
+          , id : Optional Text
+          , secretEnv : Optional (List Text)
+          , volumes : Optional (List Volume)
+          }
+      , default =
+        { args = None (List Text)
+        , env = None (List Text)
+        , dir = None Text
+        , timeout = None Text
+        , script = None Text
+        , entrypoint = None Text
+        , id = None Text
+        , secretEnv = None (List Text)
+        , volumes = None (List Volume)
+        }
+      }
+
+let Options =
+      { Type =
+          { env : Optional (List Text)
+          , secretEnv : Optional Text
+          , volumes : Optional (List Volume)
+          , sourceProvenanceHash : Optional Text
+          , machineType : Optional Text
+          , diskSizeGb : Optional Text
+          , dynamicSubstitutions : Optional Bool
+          , logStreamingOption : Optional Text
+          , logging : Optional Text
+          }
+      , default =
+        { env = None (List Text)
+        , secretEnv = None Text
+        , volumes = None (List Volume)
+        , sourceProvenanceHash = None Text
+        , machineType = None Text
+        , diskSizeGb = None Text
+        , dynamicSubstitutions = None Bool
+        , logStreamingOption = None Text
+        , logging = None Text
+        }
+      }
+
+let Cloudbuild =
+      { Type =
+          { steps : List Step.Type
+          , timeout : Optional Text
+          , queueTtl : Optional Text
+          , logsBucket : Optional Text
+          , options : Optional Options.Type
+          , substitutions : Optional (Map.Type Text Text)
+          , tags : Optional (List Text)
+          , serviceAccount : Optional Text
+          , artifacts : Optional Artifacts
+          , images : Optional (List Text)
+          }
+      , default =
+        { timeout = None Text
+        , queueTtl = None Text
+        , logsBucket = None Text
+        , options = None Options.Type
+        , substitutions = None (Map.Type Text Text)
+        , tags = None (List Text)
+        , serviceAccount = None Text
+        , artifacts = None Artifacts
+        , images = None (List Text)
+        }
+      }
+
+in  { Volume, Artifacts, Step, Options, Cloudbuild }
--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -12,6 +12,8 @@ RED='\033[0;31m'
 # Array of valid service names
 VALID_SERVICES=('mina-archive', 'mina-daemon' 'mina-rosetta' 'mina-toolchain' 'bot' 'leaderboard' 'delegation-backend' 'delegation-backend-toolchain')
 
+ROOT="$(cd -- "$(dirname "$( dirname -- "${BASH_SOURCE[0]}" )")" &> /dev/null && pwd )"
+
 function usage() {
   if [[ -n "$1" ]]; then
     echo -e "${RED}☞  $1${CLEAR}\n";
@@ -30,30 +32,22 @@ function usage() {
   exit 1
 }
 
+FUNCTION=cloudBuild
+
 while [[ "$#" -gt 0 ]]; do case $1 in
   --no-upload) NOUPLOAD=1;;
   -s|--service) SERVICE="$2"; shift;;
   -v|--version) VERSION="$2"; shift;;
-  -n|--network) NETWORK="--build-arg network=$2"; shift;;
-  -b|--branch) BRANCH="--build-arg MINA_BRANCH=$2"; shift;;
-  -c|--cache-from) CACHE="--cache-from $2"; shift;;
-  --deb-codename) DEB_CODENAME="--build-arg deb_codename=$2"; shift;;
-  --deb-release) DEB_RELEASE="--build-arg deb_release=$2"; shift;;
-  --deb-version) DEB_VERSION="--build-arg deb_version=$2"; shift;;
-  --extra-args) EXTRA=${@:2}; shift $((${#}-1));;
+  -n|--network) NETWORK=", network = Some \"$2\""; shift;;
+  -b|--branch) BRANCH=", branch = Some \"$2\""; shift;;
+  -c|--cache-from) CACHE=", cache = Some \"$2\""; shift;;
+  --deb-codename) DEB_CODENAME=", debCodename = Some cb.DebCodename.$2"; shift;;
+  --deb-release) DEB_RELEASE=", debRelease = Some \"$2\""; shift;;
+  --deb-version) DEB_VERSION=", debVersion = Some \"$2\""; shift;;
+  --local) FUNCTION=dockerBuild ;;
+  --extra-args) EXTRA=", extraArgs=[\"${@:2}\"]"; shift $((${#}-1));;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
-
-# Determine the proper image for ubuntu or debian
-case "${DEB_CODENAME##*=}" in
-  bionic|focal|impish|jammy)
-    IMAGE="ubuntu:${DEB_CODENAME##*=}"
-  ;;
-  stretch|buster|bullseye|sid)
-    IMAGE="debian:${DEB_CODENAME##*=}-slim"
-  ;;
-esac
-IMAGE="--build-arg image=${IMAGE}"
 
 # Debug prints for visability
 # Substring removal to cut the --build-arg arguments on the = so that the output is exactly the input flags https://wiki.bash-hackers.org/syntax/pe#substring_removal
@@ -64,65 +58,37 @@ echo "docker image: ${IMAGE}"
 # Verify Required Parameters are Present
 if [[ -z "$SERVICE" ]]; then usage "Service is not set!"; fi;
 if [[ -z "$VERSION" ]]; then usage "Version is not set!"; fi;
-if [[ -z "$EXTRA" ]]; then EXTRA=""; fi;
-if [[ $(echo ${VALID_SERVICES[@]} | grep -o "$SERVICE" - | wc -w) -eq 0 ]]; then usage "Invalid service!"; fi
 
-case "${SERVICE}" in
-mina-archive)
-  DOCKERFILE_PATH="dockerfiles/Dockerfile-mina-archive"
-  DOCKER_CONTEXT="dockerfiles/"
-  ;;
-bot)
-  DOCKERFILE_PATH="frontend/bot/Dockerfile"
-  DOCKER_CONTEXT="frontend/bot"
-  ;;
-mina-daemon)
-  DOCKERFILE_PATH="dockerfiles/Dockerfile-mina-daemon"
-  DOCKER_CONTEXT="dockerfiles/"
-  VERSION="${VERSION}-${NETWORK##*=}"
-  ;;
-mina-toolchain)
-  DOCKERFILE_PATH="dockerfiles/stages/1-build-deps dockerfiles/stages/2-opam-deps dockerfiles/stages/3-toolchain"
-  ;;
-mina-rosetta)
-  DOCKERFILE_PATH="dockerfiles/stages/1-build-deps dockerfiles/stages/2-opam-deps dockerfiles/stages/3-builder dockerfiles/stages/4-production"
-  ;;
-leaderboard)
-  DOCKERFILE_PATH="frontend/leaderboard/Dockerfile"
-  DOCKER_CONTEXT="frontend/leaderboard"
-  ;;
-delegation-backend)
-  DOCKERFILE_PATH="dockerfiles/Dockerfile-delegation-backend"
-  DOCKER_CONTEXT="src/app/delegation_backend"
-  ;;
-delegation-backend-toolchain)
-  DOCKERFILE_PATH="dockerfiles/Dockerfile-delegation-backend-toolchain"
-  DOCKER_CONTEXT="src/app/delegation_backend"
-  ;;
-esac
-
-
-REPO="--build-arg MINA_REPO=${BUILDKITE_PULL_REQUEST_REPO}"
-if [[ -z "${BUILDKITE_PULL_REQUEST_REPO}" ]]; then
-  REPO="--build-arg MINA_REPO=https://github.com/MinaProtocol/mina"
+if ! [[ -z "${BUILDKITE_PULL_REQUEST_REPO}" ]]; then
+  REPO=", repo=\"${BUILDKITE_PULL_REQUEST_REPO}\""
 fi
 
-TAG="minaprotocol/$SERVICE:$VERSION"
+pushd "$ROOT"
 
-# If DOCKER_CONTEXT is not specified, assume none and just pipe the dockerfile into docker build
-extra_build_args=$(echo ${EXTRA} | tr -d '"')
-if [[ -z "${DOCKER_CONTEXT}" ]]; then
-  cat $DOCKERFILE_PATH | docker build $CACHE $NETWORK $IMAGE $DEB_CODENAME $DEB_RELEASE $DEB_VERSION $BRANCH $REPO $extra_build_args -t "$TAG" -
+dhall="
+  let cb = ./dockerfiles/cloudbuild.dhall
+  in cb.$FUNCTION cb.$SERVICE cb.ServiceDescription::
+       { version = \"$VERSION\"
+       $NETWORK
+       $BRANCH
+       $CACHE
+       $DEB_CODENAME
+       $DEB_RELEASE
+       $DEB_VERSION
+       $EXTRA
+       }
+"
+
+
+if [[ $FUNCTION == cloudBuild ]]; then
+    dhall-to-yaml <<< "$dhall" > "cloudbuild.yaml"
+    gcloud builds submit
+elif [[ $FUNCTION == dockerBuild ]]; then
+    eval "$(dhall-to-bash --declare args <<< "$dhall")"
+    docker "${args[@]}"
 else
-  docker build $CACHE $NETWORK $IMAGE $DEB_CODENAME $DEB_RELEASE $DEB_VERSION $BRANCH $REPO $extra_build_args $DOCKER_CONTEXT -t "$TAG" -f $DOCKERFILE_PATH
+    echo "Unknown function: $FUNCTION" > /dev/stderr
+    exit 1
 fi
 
-tag-and-push() {
-  docker tag "${TAG}" "$1"
-  docker push "$1"
-}
-
-if [ -z "$NOUPLOAD" ] || [ "$NOUPLOAD" -eq 0 ]; then
-  docker push "${TAG}"
-  tag-and-push "gcr.io/o1labs-192920/$SERVICE:$VERSION"
-fi
+popd
