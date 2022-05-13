60b2b5f68cb540a0f90440cd3d209addf82570fe
Yves-Stan Le Cornec <ylecornec@users.noreply.github.com>
2022-05-13 17:27:37 +0200

    interface and functor for GraphQL modules for basic scalar types #10925

--- src/lib/graphql_utils/gql_types.ml
+++ src/lib/graphql_utils/gql_types.ml
@@ -1,125 +1,154 @@
-open Graphql_async.Schema
-
 (** A module providing the typ value used to build the graphql schema as well as
     well as utility functions to build queries and parse responses. *)
 
-module type MAYBE_NULLABLE_TYP = sig
-  type 'a final_option_modifier
+module Make (Schema : Graphql_intf.Schema) = struct
+  open Schema
 
-  type 'a modifier
+  module type SCALAR = sig
+    type 'a final_option_modifier
 
-  type out_before_modifiers
+    type 'a modifier
 
-  (** [typ] value for the graphql schema *)
-  val typ :
-    unit -> (unit, out_before_modifiers modifier final_option_modifier) typ
+    type out_before_modifiers
 
-  type 'a query
+    type out = out_before_modifiers modifier final_option_modifier
 
-  (* type 'a res *)
+    val typ :
+      unit -> (unit, out_before_modifiers modifier final_option_modifier) typ
 
-  val response_of_json :
-    'a query -> Yojson.Basic.t -> 'a modifier final_option_modifier
+    val response_of_json : Yojson.Basic.t -> out
+  end
 
-  val mk_query : 'a query -> string
+  module type NON_NULLABLE_SCALAR = sig
+    include SCALAR with type 'a final_option_modifier = 'a
+
+    val typ_nullable : unit -> (unit, out_before_modifiers modifier option) typ
+  end
+
+  module type MAYBE_NULLABLE_TYP = sig
+    type 'a final_option_modifier
+
+    type 'a modifier
+
+    type out_before_modifiers
+
+    type out = out_before_modifiers modifier final_option_modifier
+
+    (** [typ] value for the graphql schema *)
+    val typ :
+      unit -> (unit, out_before_modifiers modifier final_option_modifier) typ
+
+    type 'a query
+
+    val response_of_json :
+      'a query -> Yojson.Basic.t -> 'a modifier final_option_modifier
+
+    val mk_query : 'a query -> string
+  end
+
+  module type NOT_NULLABLE_TYP = sig
+    include MAYBE_NULLABLE_TYP
+
+    val typ_nullable : unit -> (unit, out_before_modifiers modifier option) typ
+  end
+
+  module NullableScalar (Input : NON_NULLABLE_SCALAR) = struct
+    type 'a final_option_modifier = 'a option
+
+    type 'a modifier = 'a Input.modifier
+
+    type out_before_modifiers = Input.out_before_modifiers
+
+    type out = out_before_modifiers modifier final_option_modifier
+
+    let response_of_json = function
+      | `Null ->
+          None
+      | json ->
+          Some (Input.response_of_json json)
+
+    let typ = Input.typ_nullable
+  end
+
+  module Nullable
+      (Input : NOT_NULLABLE_TYP with type 'a final_option_modifier = 'a) =
+  struct
+    type 'a final_option_modifier = 'a option
+
+    type 'a modifier = 'a Input.modifier
+
+    type out_before_modifiers =
+      Input.out_before_modifiers Input.modifier Input.final_option_modifier
+
+    type 'a query = 'a Input.query
+
+    let response_of_json query json =
+      Json.nullable Input.response_of_json query json
+
+    let typ = Input.typ_nullable
+
+    let mk_query = Input.mk_query
+  end
+
+  module List (Input : MAYBE_NULLABLE_TYP) = struct
+    type 'a final_option_modifier = 'a
+
+    type 'a modifier = 'a Input.modifier Input.final_option_modifier list
+
+    type out_before_modifiers = Input.out_before_modifiers
+
+    type out = out_before_modifiers modifier final_option_modifier
+
+    type 'a query = 'a Input.query
+
+    type 'a res = 'a Input.modifier Input.final_option_modifier list
+
+    let response_of_json query json =
+      Json.non_null_list_of_json Input.response_of_json query json
+
+    let typ_nullable () = list (Input.typ ())
+
+    let typ () = non_null @@ typ_nullable ()
+
+    let mk_query = Input.mk_query
+  end
+
+  module Make_non_null_scalar (Input : sig
+    type t
+
+    val typ_nullable : unit -> ('a, t option) typ
+
+    val of_json : Yojson.Basic.t -> t
+  end) =
+  struct
+    type 'a final_option_modifier = 'a
+
+    type 'a modifier = 'a
+
+    type out_before_modifiers = Input.t
+
+    type out = out_before_modifiers modifier final_option_modifier
+
+    let typ_nullable () = Input.typ_nullable ()
+
+    let typ () = non_null (typ_nullable ())
+
+    let response_of_json = Input.of_json
+  end
+
+  module Gql_int = Make_non_null_scalar (struct
+    type t = int
+
+    let typ_nullable () = int
+
+    let of_json = Json.get_int
+  end)
+
+  module Gql_string = Make_non_null_scalar (struct
+    type t = string
+
+    let typ_nullable () = string
+
+    let of_json = Json.get_string
+  end)
 end
-
-module type TYP = sig
-  include MAYBE_NULLABLE_TYP
-
-  val typ_nullable : unit -> (unit, out_before_modifiers modifier option) typ
-end
-
-(* module Non_null (Aux : sig *)
-(*   type new_out *)
-
-(*   type 'a new_res *)
-(* end) *)
-(* (Input : TYP *)
-(*            with type out = Aux.new_out option *)
-(*             and type 'a res = 'a Aux.new_res option) : *)
-(*   TYP *)
-(*     with type out = Aux.new_out *)
-(*      and type 'a query = 'a Input.query *)
-(*      and type 'a res = 'a Aux.new_res = struct *)
-(*   type out = Aux.new_out *)
-
-(*   type 'a res = 'a Aux.new_res *)
-
-(*   type 'a query = 'a Input.query *)
-
-(*   let typ () = non_null @@ Input.typ () *)
-
-(*   let response_of_json query json = *)
-(*     match Input.response_of_json query json with *)
-(*     | None -> *)
-(*         failwith @@ "Non nullable value should not return None " ^ __LOC__ *)
-(*     | Some v -> *)
-(*         v *)
-
-(*   let mk_query = Input.mk_query *)
-(* end *)
-
-module Nullable (Input : TYP with type 'a final_option_modifier = 'a) = struct
-  type 'a final_option_modifier = 'a option
-
-  type 'a modifier = 'a Input.modifier
-
-  type out_before_modifiers =
-    Input.out_before_modifiers Input.modifier Input.final_option_modifier
-
-  type 'a query = 'a Input.query
-
-  let response_of_json query json =
-    Json.nullable Input.response_of_json query json
-
-  let typ = Input.typ_nullable
-
-  let mk_query = Input.mk_query
-end
-
-module List (Input : MAYBE_NULLABLE_TYP) = struct
-  type 'a final_option_modifier = 'a
-
-  type 'a modifier = 'a Input.modifier Input.final_option_modifier list
-
-  type out_before_modifiers = Input.out_before_modifiers
-
-  type 'a query = 'a Input.query
-
-  type 'a res = 'a Input.modifier Input.final_option_modifier list
-
-  let response_of_json query json =
-    Json.non_null_list_of_json Input.response_of_json query json
-
-  let typ_nullable () = list (Input.typ ())
-
-  let typ () = non_null @@ typ_nullable ()
-
-  let mk_query = Input.mk_query
-end
-
-(* module ListNullable (Input : TYP) : *)
-(*   TYP *)
-(*     with type out = Input.out list option *)
-(*      and type 'a query = 'a Input.query *)
-(*      and type 'a res = 'a Input.res list = struct *)
-(*   type 'a res = 'a Input.res list *)
-
-(*   type 'a query = 'a Input.query *)
-
-(*   let response_of_json query json = *)
-(*     match json with *)
-(*     | `List l -> *)
-(*         List.map (Input.response_of_json query) l *)
-(*     | _ -> *)
-(*         failwith *)
-(*         @@ Format.asprintf "expecting a json list (%s): but got\n%a\n " __LOC__ *)
-(*              Yojson.Basic.pp json *)
-
-(*   type out = Input.out list option *)
-
-(*   let typ () = list @@ Input.typ () *)
-
-(*   let mk_query = Input.mk_query *)
-(* end *)
