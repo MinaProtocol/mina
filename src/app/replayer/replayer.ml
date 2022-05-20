640751b4bbb400ee1b58abb456e1b32b469e7be6
Paul Steckler <steck@stecksoft.com>
2022-05-20 15:22:04 -0700

    Use text columns for OCaml unsigned 64-bit values

--- src/app/replayer/replayer.ml
+++ src/app/replayer/replayer.ml
@@ -543,7 +543,7 @@ module Zkapp_helpers = struct
           let snarked_ledger_hash =
             Frozen_ledger_hash.of_base58_check_exn snarked_ledger_hash_str
           in
-          let timestamp = parent_block.timestamp |> Block_time.of_int64 in
+          let timestamp = Block_time.of_string_exn parent_block.timestamp in
           let blockchain_length =
             parent_block.height |> Unsigned.UInt32.of_int64
             |> Mina_numbers.Length.of_uint32
@@ -555,8 +555,7 @@ module Zkapp_helpers = struct
           (* TODO : this will change *)
           let last_vrf_output = () in
           let total_currency =
-            parent_block.total_currency |> Unsigned.UInt64.of_int64
-            |> Currency.Amount.of_uint64
+            Currency.Amount.of_string parent_block.total_currency
           in
           let global_slot_since_hard_fork =
             parent_block.global_slot_since_hard_fork |> Unsigned.UInt32.of_int64
@@ -575,8 +574,7 @@ module Zkapp_helpers = struct
             in
             let hash = Frozen_ledger_hash.of_base58_check_exn hash_str in
             let total_currency =
-              raw_epoch_data.total_currency |> Unsigned.UInt64.of_int64
-              |> Currency.Amount.of_uint64
+              Currency.Amount.of_string raw_epoch_data.total_currency
             in
             let ledger = { Mina_base.Epoch_ledger.Poly.hash; total_currency } in
             let seed = raw_epoch_data.seed |> Epoch_seed.of_base58_check_exn in
