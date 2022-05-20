82f5ba9401bef99bb5556281830718f74497b46f
Brandon Kase <brandon.kase@gmail.com>
2022-05-20 21:46:44 +0300

    Adjusts account merklePath API
    Adds merklePath and leafHash to `account` so you can get the merkle path
    of any account (and the extra hash) s.t. you can compute the implied
    root from the blockchain state.

--- src/lib/mina_graphql/mina_graphql.ml
+++ src/lib/mina_graphql/mina_graphql.ml
@@ -160,6 +160,13 @@ module Reflection = struct
   end
 end
 
+let get_ledger_and_breadcrumb coda =
+  coda |> Mina_lib.best_tip |> Participating_state.active
+  |> Option.map ~f:(fun tip ->
+         ( Transition_frontier.Breadcrumb.staged_ledger tip
+           |> Staged_ledger.ledger
+         , tip ) )
+
 module Types = struct
   open Schema
   open Graphql_lib.Base_types
@@ -324,14 +331,22 @@ module Types = struct
   let merkle_path_element :
       (_, [ `Left of Zkapp_basic.F.t | `Right of Zkapp_basic.F.t ] option) typ =
     obj "MerklePathElement" ~fields:(fun _ ->
-        [ field "isRightBranch" ~typ:(non_null bool)
+        [ field "left" ~typ:string
             ~args:Arg.[]
             ~resolve:(fun _ x ->
-              match x with `Left _ -> false | `Right _ -> true )
-        ; field "otherHash" ~typ:(non_null string)
+              match x with
+              | `Left h ->
+                  Some (Zkapp_basic.F.to_string h)
+              | `Right _ ->
+                  None )
+        ; field "right" ~typ:string
             ~args:Arg.[]
             ~resolve:(fun _ x ->
-              match x with `Left h | `Right h -> Zkapp_basic.F.to_string h )
+              match x with
+              | `Left _ ->
+                  None
+              | `Right h ->
+                  Some (Zkapp_basic.F.to_string h) )
         ] )
 
   module DaemonStatus = struct
@@ -852,23 +867,19 @@ module Types = struct
           ; zkapp_uri
           } =
         let open Option.Let_syntax in
-        let%bind public_key = public_key in
         let%bind token_permissions = token_permissions in
         let%bind token_symbol = token_symbol in
         let%bind nonce = nonce in
         let%bind receipt_chain_hash = receipt_chain_hash in
-        let%bind delegate = delegate in
         let%bind voting_for = voting_for in
-        let%bind timing = timing in
         let%bind permissions = permissions in
-        let%bind zkapp = zkapp in
         let%map zkapp_uri = zkapp_uri in
         { Account.Poly.public_key
         ; token_id
         ; token_permissions
         ; token_symbol
         ; nonce
-        ; balance
+        ; balance = balance.AnnotatedBalance.total
         ; receipt_chain_hash
         ; delegate
         ; voting_for
@@ -1369,6 +1380,31 @@ module Types = struct
                        List.map ~f:Snark_params.Tick.Field.to_string
                          (Pickles_types.Vector.to_list
                             zkapp_account.sequence_state ) ) )
+             ; field "leafHash"
+                 ~doc:
+                   "The base58Check-encoded hash of this account to bootstrap \
+                    the merklePath"
+                 ~typ:string
+                 ~args:Arg.[]
+                 ~resolve:(fun _ { account; _ } ->
+                   let open Option.Let_syntax in
+                   let%map account = Partial_account.to_full_account account in
+                   Zkapp_basic.F.to_string
+                     (Ledger_hash.of_digest (Account.digest account)) )
+             ; field "merklePath"
+                 ~doc:
+                   "Merkle path is a list of path elements that are either the \
+                    left or right hashes up to the root"
+                 ~typ:(list (non_null merkle_path_element))
+                 ~args:Arg.[]
+                 ~resolve:(fun { ctx = mina; _ } { index; _ } ->
+                   let open Option.Let_syntax in
+                   let%bind ledger, _breadcrumb =
+                     get_ledger_and_breadcrumb mina
+                   in
+                   let%bind index = index in
+                   Option.try_with (fun () ->
+                       Ledger.merkle_path_at_index_exn ledger index ) )
              ] ) )
 
     let account = Lazy.force account
@@ -3823,13 +3859,6 @@ module Queries = struct
               ~typ:(non_null Types.Input.public_key_arg)
           ]
       ~resolve:account_resolver
-
-  let get_ledger_and_breadcrumb coda =
-    coda |> Mina_lib.best_tip |> Participating_state.active
-    |> Option.map ~f:(fun tip ->
-           ( Transition_frontier.Breadcrumb.staged_ledger tip
-             |> Staged_ledger.ledger
-           , tip ) )
 
   let account =
     field "account" ~doc:"Find any account via a public key and token"
