8cb73037b8e1095edfb7bf37d42b1bcba459df50
Gregor <gregor.mitscha-baude@gmx.at>
2022-05-23 10:48:59 +0200

    [snarkyjs] do not export balance change type

--- src/lib/snarky_js_bindings/snarky_js_types.ml
+++ src/lib/snarky_js_bindings/snarky_js_types.ml
@@ -4,9 +4,6 @@ let () =
   let layout = Fields_derivers_zkapps.js_layout in
   let js_layout =
     `Assoc
-      [ ("Parties", layout Parties.deriver)
-      ; ("BalanceChange", layout Fields_derivers_zkapps.Derivers.balance_change)
-      ; ("Party", layout Party.deriver)
-      ]
+      [ ("Parties", layout Parties.deriver); ("Party", layout Party.deriver) ]
   in
   print_endline (js_layout |> Yojson.Safe.pretty_to_string)
