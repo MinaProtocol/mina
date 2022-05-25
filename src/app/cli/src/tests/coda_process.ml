8f9d21f7cab7cc5dd6848e82c4d72565d07cf02f
Helena Li <helena.li0x1@gmail.com>
2022-05-25 13:48:44 -0700

    accidentally deleted these 2 files

---
+++ src/app/cli/src/tests/coda_process.ml
@@ -0,0 +1,190 @@
+[%%import "/src/config.mlh"]
+
+open Core
+open Async
+open Pipe_lib
+
+type t = Coda_worker.Connection.t * Process.t * Coda_worker.Input.t
+
+let spawn_exn (config : Coda_worker.Input.t) =
+  let%bind conn, process =
+    Coda_worker.spawn_in_foreground_exn ~env:config.env ~on_failure:Error.raise
+      ~cd:config.program_dir ~shutdown_on:Connection_closed
+      ~connection_state_init_arg:() ~connection_timeout:(Time.Span.of_sec 15.)
+      config
+  in
+  File_system.dup_stdout process ;
+  File_system.dup_stderr process ;
+  return (conn, process, config)
+
+let local_config ?block_production_interval:_ ~is_seed ~peers ~addrs_and_ports
+    ~chain_id ~libp2p_keypair
+    ~net_configs:(addrs_and_ports_list, all_peers_list) ~acceptable_delay
+    ~program_dir ~block_production_key ~snark_worker_key ~work_selection_method
+    ~offset ~trace_dir ~max_concurrent_connections ~is_archive_rocksdb
+    ~archive_process_location ~runtime_config () =
+  let conf_dir =
+    match Sys.getenv "MINA_INTEGRATION_TEST_DIR" with
+    | Some dir ->
+        dir
+        ^/ Network_peer.Peer.Id.to_string
+             (Mina_net2.Keypair.to_peer_id libp2p_keypair)
+    | None ->
+        Filename.temp_dir_name
+        ^/ String.init 16 ~f:(fun _ -> (Int.to_string (Random.int 10)).[0])
+  in
+  if
+    not
+      ([%equal: [ `Yes | `No | `Unknown ]] (Core.Sys.file_exists conf_dir) `No)
+  then
+    failwithf
+      "cannot configure Mina process because directory already exists: %s"
+      conf_dir () ;
+  let config =
+    { Coda_worker.Input.addrs_and_ports
+    ; libp2p_keypair
+    ; net_configs =
+        ( List.map
+            ~f:(fun (na, kp) -> (Node_addrs_and_ports.to_display na, kp))
+            addrs_and_ports_list
+        , List.map
+            ~f:(List.map ~f:Node_addrs_and_ports.to_display)
+            all_peers_list )
+    ; env =
+        ( "MINA_TIME_OFFSET"
+        , Time.Span.to_int63_seconds_round_down_exn offset
+          |> Int63.to_int
+          |> Option.value_exn ?here:None ?message:None ?error:None
+          |> Int.to_string )
+        :: ( Core.Unix.environment () |> Array.to_list
+           |> List.filter_map
+                ~f:
+                  (Fn.compose
+                     (function [ a; b ] -> Some (a, b) | _ -> None)
+                     (String.split ~on:'=') ) )
+    ; block_production_key
+    ; snark_worker_key
+    ; work_selection_method
+    ; conf_dir
+    ; chain_id
+    ; peers
+    ; trace_dir
+    ; program_dir
+    ; acceptable_delay
+    ; is_archive_rocksdb
+    ; is_seed
+    ; archive_process_location
+    ; max_concurrent_connections
+    ; runtime_config
+    }
+  in
+  config
+
+let peers_exn (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.peers ~arg:()
+
+let get_balance_exn (conn, _, _) pk =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.get_balance
+    ~arg:pk
+
+let get_nonce_exn (conn, _, _) pk =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.get_nonce ~arg:pk
+
+let root_length_exn (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.root_length
+    ~arg:()
+
+let send_user_command_exn (conn, _, _) sk pk amount fee memo =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.send_user_command
+    ~arg:(sk, pk, amount, fee, memo)
+
+let process_user_command_exn (conn, _, _) cmd =
+  Coda_worker.Connection.run_exn conn
+    ~f:Coda_worker.functions.process_user_command ~arg:cmd
+
+let sync_status_exn (conn, _, _) =
+  let%map r =
+    Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.sync_status
+      ~arg:()
+  in
+  Linear_pipe.wrap_reader r
+
+let verified_transitions_exn (conn, _, _) =
+  let%map r =
+    Coda_worker.Connection.run_exn conn
+      ~f:Coda_worker.functions.verified_transitions ~arg:()
+  in
+  Linear_pipe.wrap_reader r
+
+(* TODO: 2836 delete once transition_frontier extensions refactoring gets in *)
+let validated_transitions_keyswaptest_exn (conn, _, _) =
+  let%map r =
+    Coda_worker.Connection.run_exn conn
+      ~f:Coda_worker.functions.validated_transitions_keyswaptest ~arg:()
+  in
+  Linear_pipe.wrap_reader r
+
+let new_block_exn (conn, _, _) key =
+  let%map r =
+    Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.new_block
+      ~arg:key
+  in
+  Linear_pipe.wrap_reader r
+
+let root_diff_exn (conn, _, _) =
+  let%map r =
+    Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.root_diff
+      ~arg:()
+  in
+  Linear_pipe.wrap_reader r
+
+let initialization_finish_signal_exn (conn, _, _) =
+  let%map p =
+    Coda_worker.Connection.run_exn conn
+      ~f:Coda_worker.functions.initialization_finish_signal ~arg:()
+  in
+  Linear_pipe.wrap_reader p
+
+let start_exn (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.start ~arg:()
+
+let new_user_command_exn (conn, _, _) pk =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.new_user_command
+    ~arg:pk
+
+let dump_tf (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.dump_tf ~arg:()
+
+let best_path (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.best_path ~arg:()
+
+let replace_snark_worker_key (conn, _, _) key =
+  Coda_worker.Connection.run_exn conn
+    ~f:Coda_worker.functions.replace_snark_worker_key ~arg:key
+
+let stop_snark_worker (conn, _, _) =
+  Coda_worker.Connection.run_exn conn ~f:Coda_worker.functions.stop_snark_worker
+    ~arg:()
+
+let disconnect ((conn, proc, _) as t) ~logger =
+  Child_processes.Termination.wait_for_process_log_errors ~logger proc
+    ~module_:__MODULE__ ~location:__LOC__ ~here:[%here] ;
+  (* This kills any straggling snark worker process *)
+  let%bind () =
+    match%map
+      Monitor.try_with ~here:[%here] (fun () -> stop_snark_worker t)
+    with
+    | Ok () ->
+        ()
+    | Error exn ->
+        [%log info] "Harmless error when stopping snark worker: $exn"
+          ~metadata:[ ("exn", `String (Exn.to_string exn)) ]
+  in
+  let%bind () = Coda_worker.Connection.close conn in
+  match%map Monitor.try_with ~here:[%here] (fun () -> Process.wait proc) with
+  | Ok _ ->
+      ()
+  | Error e ->
+      [%log info]
+        ~metadata:[ ("e", `String (Exn.to_string e)) ]
+        "Harmless error when stopping test node: $exn"
---
+++ src/app/cli/src/tests/coda_processes.ml
@@ -0,0 +1,139 @@
+[%%import "/src/config.mlh"]
+
+open Core
+open Async
+
+let init () = Parallel.init_master ()
+
+type ports =
+  { communication_port : int; discovery_port : int; libp2p_port : int }
+
+let net_configs n =
+  File_system.with_temp_dir "coda-processes-generate-keys" ~f:(fun tmpd ->
+      let%bind net =
+        Mina_net2.create
+          ~pids:(Child_processes.Termination.create_pid_table ())
+          ~logger:(Logger.create ()) ~conf_dir:tmpd ~all_peers_seen_metric:false
+          ~on_peer_connected:ignore ~on_peer_disconnected:ignore
+      in
+      let net = Or_error.ok_exn net in
+      let ips =
+        List.init n ~f:(fun i ->
+            Unix.Inet_addr.of_string @@ sprintf "127.0.0.1%i" i )
+      in
+      let%bind addrs_and_ports_list =
+        Deferred.List.mapi ips ~f:(fun i ip ->
+            let%map key = Mina_net2.generate_random_keypair net in
+            let base = 23000 + (i * 2) in
+            let libp2p_port = base in
+            let client_port = base + 1 in
+            ( { Node_addrs_and_ports.external_ip = ip
+              ; bind_ip = ip
+              ; peer =
+                  Some
+                    (Network_peer.Peer.create ip ~libp2p_port
+                       ~peer_id:(Mina_net2.Keypair.to_peer_id key) )
+              ; libp2p_port
+              ; client_port
+              }
+            , key ) )
+      in
+      let all_peers = addrs_and_ports_list in
+      let peers =
+        List.init n ~f:(fun i ->
+            List.take all_peers i @ List.drop all_peers (i + 1) )
+      in
+      let%map () = Mina_net2.shutdown net in
+      (addrs_and_ports_list, List.map ~f:(List.map ~f:fst) peers) )
+
+let offset (consensus_constants : Consensus.Constants.t) =
+  Core.Time.(
+    diff (now ())
+      (Block_time.to_time consensus_constants.genesis_state_timestamp))
+
+let local_configs ?block_production_interval
+    ?(block_production_keys = Fn.const None)
+    ?(is_archive_rocksdb = Fn.const false)
+    ?(archive_process_location = Fn.const None) n ~acceptable_delay ~chain_id
+    ~program_dir ~snark_worker_public_keys ~work_selection_method ~trace_dir
+    ~max_concurrent_connections ~runtime_config =
+  let%map net_configs = net_configs n in
+  let addrs_and_ports_list, peers = net_configs in
+  let peers = [] :: List.drop peers 1 in
+  let args = List.zip_exn addrs_and_ports_list peers in
+  let offset =
+    let genesis_state_timestamp =
+      match
+        Option.bind runtime_config.Runtime_config.genesis
+          ~f:(fun { genesis_state_timestamp = ts; _ } -> ts)
+      with
+      | Some timestamp ->
+          Genesis_constants.genesis_timestamp_of_string timestamp
+      | None ->
+          (Lazy.force Precomputed_values.compiled_inputs).consensus_constants
+            .genesis_state_timestamp |> Block_time.to_time
+    in
+    Core.Time.(diff (now ())) genesis_state_timestamp
+  in
+  let configs =
+    List.mapi args ~f:(fun i ((addrs_and_ports, libp2p_keypair), peers) ->
+        let public_key =
+          Option.bind snark_worker_public_keys ~f:(fun keys ->
+              List.nth_exn keys i )
+        in
+        let addrs_and_ports = Node_addrs_and_ports.to_display addrs_and_ports in
+        let peers = List.map ~f:Node_addrs_and_ports.to_multiaddr_exn peers in
+        Coda_process.local_config ?block_production_interval ~is_seed:true
+          ~addrs_and_ports ~libp2p_keypair ~net_configs ~peers
+          ~snark_worker_key:public_key ~program_dir ~acceptable_delay ~chain_id
+          ~block_production_key:(block_production_keys i) ~work_selection_method
+          ~trace_dir ~is_archive_rocksdb:(is_archive_rocksdb i)
+          ~archive_process_location:(archive_process_location i)
+          ~offset ~max_concurrent_connections ~runtime_config () )
+  in
+  configs
+
+let stabalize_and_start_or_timeout ?(timeout_ms = 240000.) nodes =
+  let ready () =
+    let check_ready node =
+      let%map peers = Coda_process.peers_exn node in
+      List.length peers >= List.length nodes - 1
+    in
+    let rec go () =
+      if%bind Deferred.List.for_all nodes ~f:check_ready then return ()
+      else after (Time.Span.of_ms 100.) >>= go
+    in
+    go ()
+  in
+  let before_time = Time.now () in
+  match%bind
+    Deferred.any
+      [ (after (Time.Span.of_ms timeout_ms) >>= fun () -> return `Timeout)
+      ; (ready () >>= fun () -> return `Ready)
+      ]
+  with
+  | `Timeout ->
+      failwith @@ sprintf "Nodes couldn't initialize within %f ms" timeout_ms
+  | `Ready ->
+      let after_time = Time.now () in
+      [%log' info (Logger.create ())]
+        "Initialized nodes in $time_span_ms ms"
+        ~metadata:
+          [ ( "time_span_ms"
+            , `Float (Time.Span.to_ms (Time.abs_diff before_time after_time)) )
+          ] ;
+      Deferred.List.iter nodes ~f:(fun node -> Coda_process.start_exn node)
+
+let spawn_local_processes_exn ?(first_delay = 0.0) configs =
+  match configs with
+  | [] ->
+      failwith "Configs should be non-empty"
+  | first :: rest ->
+      let%bind first_created = Coda_process.spawn_exn first in
+      let%bind () = after (Time.Span.of_sec first_delay) in
+      let%bind rest_created =
+        Deferred.List.all (List.map rest ~f:(fun c -> Coda_process.spawn_exn c))
+      in
+      let all_created = first_created :: rest_created in
+      let%map () = stabalize_and_start_or_timeout all_created in
+      all_created
