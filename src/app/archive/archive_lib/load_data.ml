e14bec98c4d59eba09f14cc9db7f70ebfe0c86dd
mrmr1993 <mr_1993@hotmail.co.uk>
2022-05-26 02:58:08 +0100

    Fixup other compilation errors

--- src/app/archive/archive_lib/load_data.ml
+++ src/app/archive/archive_lib/load_data.ml
@@ -824,8 +824,6 @@ let get_account_accessed ~pool (account : Processor.Accounts_accessed.t) :
   let account =
     ( { public_key
       ; token_id
-      ; token_permissions =
-          Token_permissions.Not_owned { account_disabled = false }
       ; token_symbol
       ; balance
       ; nonce
