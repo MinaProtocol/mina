17b95d518bcdec8aebe01ee21fb23c99bb5ee605
Alexander Bantyev <balsoft@balsoft.ru>
2022-06-08 13:44:37 +0400

    Remove some submodules in favour of opam pins

--- scripts/pin-external-packages.sh
+++ scripts/pin-external-packages.sh
@@ -2,7 +2,7 @@
 
 # update and pin packages, used by CI
 
-PACKAGES="ocaml-sodium capnp-ocaml rpc_parallel async_kernel coda_base58"
+PACKAGES="ocaml-sodium coda_base58"
 
 git submodule sync && git submodule update --init --recursive
 
