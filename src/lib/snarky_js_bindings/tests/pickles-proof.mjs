972f83f944d55da1e0bd81a52dab27808a517e38
Gregor <gregor.mitscha-baude@gmx.at>
2022-06-16 15:19:10 +0200

    [snarkyjs] fix and clean up tests

--- src/lib/snarky_js_bindings/tests/pickles-proof.mjs
+++ src/lib/snarky_js_bindings/tests/pickles-proof.mjs
@@ -25,20 +25,18 @@ async function picklesProof() {
 
   let factorsRule = createDummyRule("factors", factors, [FieldTyp, FieldTyp]);
   let hashRule = createDummyRule("hash", hash, [FieldTyp]);
-  let methods = [factors, hash];
   let rules = [factorsRule, hashRule];
   let witnesses = [trueFactors, [truePrehash]];
 
   console.log("compile (proof system with two branches)...");
   let start = Date.now();
-  let compiled = compile(rules);
-  let { provers, verify } = compiled;
+  let { provers, verify } = compile(rules);
   let time = Date.now() - start;
   console.log(`compiled proof system in ${(time / 1000).toFixed(2)} sec`);
 
   console.log("prove (first rule)...");
   start = Date.now();
-  let { statement, proof } = await prove(provers[0], methods[0], witnesses[0]);
+  let { statement, proof } = await prove(provers[0], witnesses[0]);
   time = Date.now() - start;
   console.log(`created recursive proof in ${(time / 1000).toFixed(2)} sec`);
 
@@ -53,35 +51,24 @@ let mainContext = undefined;
 
 function compile(rules) {
   mainContext = {};
-  let output = Pickles.compile(rules);
+  let output = Pickles.compile(rules, 1);
   mainContext = undefined;
   return output;
 }
 
-async function prove(prover, func, args) {
-  // run rule to get the statement
-  let statement = Circuit.runAndCheck(() => {
-    mainContext = {};
-    func(...args);
-    mainContext = undefined;
-    // TODO this is a dummy statement
-    let statementVar = { transaction: Field.one, atParty: Field.one };
-    return {
-      transaction: statementVar.transaction.toConstant(),
-      atParty: statementVar.atParty.toConstant(),
-    };
-  });
+async function prove(prover, args) {
   // prove
   mainContext = {
     witnesses: args,
   };
-  let proof = await prover(statement);
+  let statement = [Field.one];
+  let proof = await prover(statement, []);
   mainContext = undefined;
   return { proof, statement };
 }
 
 function createDummyRule(name, func, witnessTypes) {
-  function main(statement) {
+  function main([statement]) {
     // get the private inputs from current context and call the function with them
     let { witnesses } = mainContext;
     witnesses = witnessTypes.map(
@@ -90,16 +77,13 @@ function createDummyRule(name, func, witnessTypes) {
         : emptyWitness
     );
     func(...witnesses);
-    // dummy constraint
-    let { transaction } = statement;
-    transaction.assertEquals(transaction);
+    statement.assertEquals(1);
+    return [];
   }
-  // return (name, main) in the format OCaml expects
-  return [0, name, main];
+  return { identifier: name, main, proofsToVerify: [] };
 }
 
 function emptyWitness(typ) {
-  // return typ.ofFields(Array(typ.sizeInFields()).fill(Field.zero));
   return Circuit.witness(typ, () =>
     typ.ofFields(Array(typ.sizeInFields()).fill(Field.zero))
   );
@@ -109,4 +93,5 @@ let FieldTyp = {
   sizeInFields: () => 1,
   toFields: (f) => [f],
   ofFields: ([f]) => f,
+  check: () => {},
 };
