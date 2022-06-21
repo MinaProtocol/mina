e47376b1283dbe10629d02c3db84bb20a5e13725
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2022-06-21 14:35:04 -0700

    include create command in replayer database dump

--- src/app/replayer/test/archive_db.sql
+++ src/app/replayer/test/archive_db.sql
@@ -17,6 +17,25 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: archiver; Type: DATABASE; Schema: -; Owner: o1labs
+--
+
+CREATE DATABASE archiver WITH TEMPLATE = template0 ENCODING = 'UTF8'  LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+\connect archiver
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: call_type_type; Type: TYPE; Schema: public; Owner: o1labs
 --
 
