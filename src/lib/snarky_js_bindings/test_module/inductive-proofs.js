bbe0e3d6e13984ee2263dd225352d0e1802ce987
Gregor <gregor.mitscha-baude@gmx.at>
2022-06-21 16:45:55 +0200

    [snarkyjs] add test for recursive proofs

---
+++ src/lib/snarky_js_bindings/test_module/inductive-proofs.js
@@ -0,0 +1,47 @@
+import { SelfProof, Field, ZkProgram, isReady, shutdown } from "snarkyjs";
+import { tic, toc } from "./tictoc.js";
+
+await isReady;
+
+let MyProgram = ZkProgram({
+  publicInput: Field,
+
+  methods: {
+    baseCase: {
+      privateInputs: [],
+
+      method(publicInput) {
+        publicInput.assertEquals(Field.zero);
+      },
+    },
+
+    inductiveCase: {
+      privateInputs: [SelfProof],
+
+      method(publicInput, earlierProof) {
+        earlierProof.verify();
+        earlierProof.publicInput.add(1).assertEquals(publicInput);
+      },
+    },
+  },
+});
+
+tic("compiling MyProgram");
+await MyProgram.compile();
+toc();
+
+tic("proving base case");
+let proof = await MyProgram.baseCase(Field.zero);
+toc();
+
+tic("proving step 1");
+proof = await MyProgram.inductiveCase(Field.one, proof);
+toc();
+
+tic("proving step 2");
+proof = await MyProgram.inductiveCase(Field(2), proof);
+toc();
+
+console.log("ok?", proof.publicInput.toString() === "2");
+
+shutdown();
