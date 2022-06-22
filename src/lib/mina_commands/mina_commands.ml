2120f5d8b72af183992f30f5779500623e911aeb
Tang Jiawei <ghost.not.in.the.shell@gmail.com>
2022-06-22 19:56:54 +0800

    add vk and prover

--- src/lib/mina_commands/mina_commands.ml
+++ src/lib/mina_commands/mina_commands.ml
@@ -10,6 +10,12 @@ let txn_count = ref 0
 let generate_random_zkapps t
     ((kps, num_of_parties, parties_size) : Keypair.t list * int * int option) =
   let open Participating_state.Let_syntax in
+  let config = Mina_lib.config t in
+  let `VK vk, `Prover prover =
+    Transaction_snark.For_tests.create_trivial_snapp
+      ~constraint_constants:config.precomputed_values.constraint_constants ()
+  in
+
   let%bind ledger = Mina_lib.best_ledger t in
   let%map protocol_state = Mina_lib.best_protocol_state t in
   let protocol_state_view =
@@ -20,14 +26,14 @@ let generate_random_zkapps t
         (Public_key.compress public_key, private_key) )
     |> Public_key.Compressed.Map.of_alist_exn
   in
-
   let account_state_tbl = Account_id.Table.create () in
   let rec go n acc : Parties.t list Quickcheck.Generator.t =
     let open Quickcheck.Generator.Let_syntax in
     if n > 0 then
       let%bind parties =
         Mina_generators.Parties_generators.gen_parties_with_limited_keys ~keymap
-          ~ledger ~protocol_state_view ~account_state_tbl ?parties_size ()
+          ~ledger ~protocol_state_view ~account_state_tbl ?parties_size ~vk
+          ~prover ()
       in
       go (n - 1) (parties :: acc)
     else return (List.rev acc)
