cf6429f13a47921c02864794abd23dff8f90603e
Andrew Trainor <andrew@o1labs.org>
2022-06-27 15:43:15 -0400

    lk86/port-no-install-changes Remove rustup from build.sh files and install the wasm32-unknown-unknown target

--- src/lib/crypto/kimchi_bindings/js/chrome/build.sh
+++ src/lib/crypto/kimchi_bindings/js/chrome/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_WEB-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    rustup run nightly-2021-11-16 wasm-pack build --target web --out-dir ../js/chrome ../../wasm -- -Z build-std=panic_abort,std
+    wasm-pack build --target web --out-dir ../js/chrome ../../wasm -- -Z build-std=panic_abort,std
 else
     cp "$PLONK_WASM_WEB"/* -R .
 fi
--- src/lib/crypto/kimchi_bindings/js/node_js/build.sh
+++ src/lib/crypto/kimchi_bindings/js/node_js/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_NODEJS-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    rustup run nightly-2021-11-16 wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Z build-std=panic_abort,std --features nodejs
+    wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Z build-std=panic_abort,std --features nodejs
 else
     cp "$PLONK_WASM_NODEJS"/* -R .
 fi
