6b0c0e81ff471aef81f09c89900e33728914aa5d
Martin Minkov <minkovlmartin@gmail.com>
2022-07-05 13:58:01 -0700

    [snarkyjs] - Add tokens test script

---
+++ src/lib/snarky_js_bindings/test_module/simple-zkapp-token.js
@@ -0,0 +1,182 @@
+import {
+  Field,
+  declareState,
+  declareMethods,
+  State,
+  PublicKey,
+  PrivateKey,
+  SmartContract,
+  isReady,
+  shutdown,
+  Mina,
+  Permissions,
+  Party,
+  UInt64,
+  partiesToJson,
+  Token,
+  getDefaultTokenId,
+} from "snarkyjs";
+
+function sendTransaction(tx) {
+  //console.log("DEBUG -- TXN\n", JSON.stringify(partiesToJson(tx.transaction)));
+  tx.send();
+}
+
+await isReady;
+
+// declare the zkapp
+class SimpleZkapp extends SmartContract {
+  constructor(address) {
+    super(address);
+    this.x = State();
+  }
+
+  deploy(args) {
+    super.deploy(args);
+    this.setPermissions({
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+    });
+    this.balance.addInPlace(UInt64.fromNumber(initialBalance));
+    this.x.set(initialState);
+  }
+
+  update(y) {
+    let x = this.x.get();
+    this.x.set(x.add(y));
+  }
+
+  initialize() {
+    this.x.set(initialState);
+  }
+
+  mint(receiverAddress) {
+    let amount = 1_000_000_000;
+    this.token().mint({
+      address: receiverAddress,
+      amount,
+    });
+    console.log(`Minting ${amount} to ${receiverAddress.toBase58()}`);
+  }
+
+  burn(receiverAddress) {
+    let amount = 1_000_000;
+    this.token().burn({
+      address: receiverAddress,
+      amount,
+    });
+    console.log(`Burning ${amount} to ${receiverAddress.toBase58()}`);
+  }
+
+  send(senderAddress, receiverAddress) {
+    let amount = 1_000_000;
+    this.token().send({
+      from: senderAddress,
+      to: receiverAddress,
+      amount,
+    });
+    console.log(`Sending ${amount} to ${receiverAddress.toBase58()}`);
+  }
+}
+// note: this is our non-typescript way of doing what our decorators do
+declareState(SimpleZkapp, { x: Field });
+declareMethods(SimpleZkapp, {
+  initialize: [],
+  update: [Field],
+  send: [PublicKey, PublicKey],
+  mint: [PublicKey],
+  burn: [PublicKey],
+});
+
+let Local = Mina.LocalBlockchain();
+Mina.setActiveInstance(Local);
+
+// a test account that pays all the fees, and puts additional funds into the zkapp
+let feePayer = Local.testAccounts[0].privateKey;
+
+// the zkapp account
+let zkappKey = PrivateKey.fromBase58(
+  "EKEfEZpMctKoyon4nxhqFBiKyUsCyyZReF9fbs21nDrrTgGMTcok"
+);
+let zkappAddress = zkappKey.toPublicKey();
+
+let tokenAccount1Key = Local.testAccounts[1].privateKey;
+let tokenAccount1 = tokenAccount1Key.toPublicKey();
+
+let tokenAccount2Key = Local.testAccounts[2].privateKey;
+let tokenAccount2 = tokenAccount2Key.toPublicKey();
+
+let initialBalance = 10_000_000_000;
+let initialState = Field(1);
+let zkapp = new SimpleZkapp(zkappAddress);
+
+console.log("deploy");
+let tx = await Local.transaction(feePayer, () => {
+  Party.fundNewAccount(feePayer, { initialBalance });
+  zkapp.deploy({ zkappKey });
+});
+tx.send();
+
+console.log(`initial balance: ${zkapp.account.balance.get().div(1e9)} MINA`);
+
+// Log custom token info
+const customToken = new Token({ tokenOwner: zkappAddress });
+console.log("---FEE PAYER", feePayer.toPublicKey().toBase58());
+console.log("---TOKEN OWNER", zkappAddress.toBase58());
+console.log("---CUSTOM TOKEN", customToken.id);
+console.log("---TOKEN ACCOUNT1", tokenAccount1.toBase58());
+console.log("---TOKEN ACCOUNT2", tokenAccount2.toBase58());
+
+console.log("----------token minting----------");
+tx = await Local.transaction(feePayer, () => {
+  Party.fundNewAccount(feePayer);
+  zkapp.mint(tokenAccount1);
+  zkapp.sign(zkappKey);
+});
+sendTransaction(tx);
+
+console.log(
+  `tokenAccount1 balance: ${Mina.getBalance({
+    publicKey: tokenAccount1,
+    tokenId: customToken.id,
+  })} custom tokens`
+);
+
+console.log("----------token burning----------");
+tx = await Local.transaction(feePayer, () => {
+  zkapp.burn(tokenAccount1);
+  zkapp.sign(zkappKey);
+});
+tx = tx.sign([tokenAccount1Key]);
+sendTransaction(tx);
+
+console.log(
+  `tokenAccount1 balance: ${Mina.getBalance({
+    publicKey: tokenAccount1,
+    tokenId: customToken.id,
+  })} custom tokens`
+);
+
+console.log("----------token transfer----------");
+tx = await Local.transaction(feePayer, () => {
+  Party.fundNewAccount(feePayer);
+  zkapp.send(tokenAccount1, tokenAccount2);
+  zkapp.sign(zkappKey);
+});
+tx = tx.sign([tokenAccount1Key, tokenAccount2Key]);
+sendTransaction(tx);
+
+console.log(
+  `tokenAccount1 balance: ${Mina.getBalance({
+    publicKey: tokenAccount1,
+    tokenId: customToken.id,
+  })} custom tokens`
+);
+console.log(
+  `tokenAccount2 balance: ${Mina.getBalance({
+    publicKey: tokenAccount2,
+    tokenId: customToken.id,
+  })} custom tokens`
+);
+
+shutdown();
