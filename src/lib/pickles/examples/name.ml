b24c2ba89af4301c027e5826197a683709396df9
mrmr1993 <mr_1993@hotmail.co.uk>
2022-07-15 02:32:06 +0100

    Idiotic type errors

--- src/lib/pickles/examples/name.ml
+++ src/lib/pickles/examples/name.ml
@@ -45,8 +45,7 @@ let%test_module "can we wordle" =
         end
 
         let tag, _, p, [ step ] =
-          Async.Thread_safe.block_on_async_exn
-          @@ Pickles.compile
+          Pickles.compile
                (module Statement)
                (module Statement)
                ~public_input:
@@ -91,15 +90,15 @@ let%test_module "can we wordle" =
                              ~request:(fun () -> In_place)
                          in
                          let res =
-                           Array.iter2 guess soln ~f:(fun guess soln ->
+                           Array.map2_exn guess soln ~f:(fun guess soln ->
                                Field.equal guess soln )
                          in
                          let res_out_of_order =
-                           Array.iter2 res in_place ~f:(fun res in_place ->
+                           Array.iter2_exn res in_place ~f:(fun res in_place ->
                                Boolean.(res && in_place) )
                          in
                          let res_in_order =
-                           Array.iter2 res in_place ~f:(fun res in_place ->
+                           Array.iter2_exn res in_place ~f:(fun res in_place ->
                                Boolean.(res && not in_place) )
                          in
                          { previous_proof_statements = []
