062049b3d0ad1f8e9793c2a826ae48f36b95fa5d
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2022-07-19 22:57:38 +0100

    ran ocamlformat

--- src/lib/ppx_version/versioned_module.ml
+++ src/lib/ppx_version/versioned_module.ml
@@ -1097,8 +1097,8 @@ let version_module ~loc ~path:_ ~version_option modname modbody =
     in
     let empty_binding =
       [%str
-         let (_ : _) =
-           (module Stable.Latest : Ppx_version_runtime.Util.Empty_signature)]
+        let (_ : _) =
+          (module Stable.Latest : Ppx_version_runtime.Util.Empty_signature)]
     in
     Str.include_ ~loc
       (Incl.mk ~loc
