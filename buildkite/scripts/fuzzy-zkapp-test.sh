c237ce2bf5169e6490a7e958d137a0cd60de10b2
Tang Jiawei <tang@o1labs.org>
2022-08-22 22:33:37 +0800

    fix fuzzy zkapp script so that it only compiles and runs if nightly is true

--- buildkite/scripts/fuzzy-zkapp-test.sh
+++ buildkite/scripts/fuzzy-zkapp-test.sh
@@ -11,17 +11,20 @@ profile=$1
 path=$2
 trials=$3
 
-source ~/.profile
+if [ $NIGHTLY ]
+then
+  source ~/.profile
 
-echo "--- Make build"
-export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
+  echo "--- Make build"
+  export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
 
-# Note: By attempting a re-run on failure here, we can avoid rebuilding and
-# skip running all of the tests that have already succeeded, since dune will
-# only retry those tests that failed.
-echo "--- Run fuzzy zkapp tests"
-time dune exec "${path}" --profile="${profile}" -j16 -- --trials "${trials}" || \
-(./scripts/link-coredumps.sh && \
- echo "--- Retrying failed unit tests" && \
- time dune exec "${path}" --profile="${profile}" -j16 -- --trials "${trials}" || \
- (./scripts/link-coredumps.sh && false))
+  # Note: By attempting a re-run on failure here, we can avoid rebuilding and
+  # skip running all of the tests that have already succeeded, since dune will
+  # only retry those tests that failed.
+  echo "--- Run fuzzy zkapp tests"
+  time dune exec "${path}" --profile="${profile}" -j16 -- --trials "${trials}" || \
+  (./scripts/link-coredumps.sh && \
+   echo "--- Retrying failed unit tests" && \
+   time dune exec "${path}" --profile="${profile}" -j16 -- --trials "${trials}" || \
+   (./scripts/link-coredumps.sh && false))
+fi
