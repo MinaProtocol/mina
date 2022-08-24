7b52748ac072394e6c7359562f35997c21de9416
Paul Steckler <steck@stecksoft.com>
2022-08-23 18:08:03 -0700

    temp

---
+++ scripts/compare_ci_type_shapes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def docker_of_branch(branch):
+  
+-- TODO
+-- pull dockers for release, base branch, PR branch
+-- populate shape tables for each
+-- do comparison
+
+if __name__ == "__main__":
+  pull_dockers ()
