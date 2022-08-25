f14807ddfd328b61ba36c74e355c9be04da61cae
Virgile Robles <virgile.robles@protonmail.ch>
2022-08-25 13:33:51 +0200

    Add with_hash and some zkapp types

--- src/lib/mina_wire_types/mina_base/mina_base.ml
+++ src/lib/mina_wire_types/mina_base/mina_base.ml
@@ -10,3 +10,5 @@ module Signed_command_memo = Mina_base_signed_command_memo
 module Token_id = Mina_base_token_id
 module Party = Mina_base_party
 module Account_id = Mina_base_account_id
+module Zkapp_basic = Mina_base_zkapp_basic
+module Zkapp_state = Mina_base_zkapp_state
