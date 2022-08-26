a58a9d3570d5f1d8331d0bfcca2e9811e1e0936f
mrmr1993 <mr_1993@hotmail.co.uk>
2022-08-26 12:43:11 +0100

    Fixup compilation of best_tip_merger

--- src/app/best_tip_merger/best_tip_merger.ml
+++ src/app/best_tip_merger/best_tip_merger.ml
@@ -177,7 +177,6 @@ module Output = struct
         root_node :: acc_trees )
 end
 
-
 module Display = struct
   type state =
     | Root of State_hash.t
@@ -334,7 +333,7 @@ let main ~input_dir ~output_dir ~output_format ~min_peers () =
     ~transport:
       (Logger_file_system.dumb_logrotate ~directory:output_dir
          ~log_filename:"mina-best-tip-merger.log" ~max_size:logrotate_max_size
-         ~num_rotate ) ;
+         ~num_rotate:logrotate_num_rotate ) ;
   let logger = Logger.create () in
   let t' =
     List.fold ~init:t files ~f:(fun t log_file ->
