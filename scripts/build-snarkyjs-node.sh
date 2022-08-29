4c162974780b9dc79b1643767fd90b963a8389f3
Gregor <gregor.mitscha-baude@gmx.at>
2022-08-29 15:48:36 +0200

    [snarkyjs] build script marks jsoo output as cjs

--- scripts/build-snarkyjs-node.sh
+++ scripts/build-snarkyjs-node.sh
@@ -11,20 +11,23 @@ dune b src/lib/crypto/kimchi_bindings/js/node_js \
 && dune b src/lib/snarky_js_bindings/snarky_js_node.bc.js \
 && dune b src/lib/snarky_js_bindings/snarkyjs/src/snarky/gen/js-layout.ts || exit 1
 
-BINDINGS_PATH="$SNARKY_JS_PATH"/dist/server/node_bindings/
+BINDINGS_PATH="$SNARKY_JS_PATH"/dist/server/_node_bindings/
 mkdir -p "$BINDINGS_PATH"
 cp _build/default/src/lib/crypto/kimchi_bindings/js/node_js/plonk_wasm* "$BINDINGS_PATH"
+mv $BINDINGS_PATH/plonk_wasm.js $BINDINGS_PATH/plonk_wasm.cjs
 cp _build/default/src/lib/snarky_js_bindings/snarky_js_node*.js "$BINDINGS_PATH"
+mv $BINDINGS_PATH/snarky_js_node.bc.js $BINDINGS_PATH/snarky_js_node.bc.cjs
+sed -i 's/plonk_wasm.js/plonk_wasm.cjs/' $BINDINGS_PATH/snarky_js_node.bc.cjs
 chmod -R 777 "$BINDINGS_PATH"
 
 # better error messages
 # TODO: find a less hacky way to make adjustments to jsoo compiler output
 # `s` is the jsoo representation of the error message string, and `s.c` is the actual JS string
-sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.js
-sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.js
-sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.js
+sed -i 's/function failwith(s){throw \[0,Failure,s\]/function failwith(s){throw joo_global_object.Error(s.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.cjs
+sed -i 's/function invalid_arg(s){throw \[0,Invalid_argument,s\]/function invalid_arg(s){throw joo_global_object.Error(s.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.cjs
+sed -i 's/return \[0,Exn,t\]/return joo_global_object.Error(t.c)/' "$BINDINGS_PATH"/snarky_js_node.bc.cjs
 # TODO: this doesn't cover all cases, maybe should rewrite to_exn instead
-sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "some error")}/' "$BINDINGS_PATH"/snarky_js_node.bc.js
+sed -i 's/function raise(t){throw caml_call1(to_exn$0,t)}/function raise(t){throw Error(t?.[1]?.c ?? "some error")}/' "$BINDINGS_PATH"/snarky_js_node.bc.cjs
 
 
-npm run --prefix="$SNARKY_JS_PATH" build -- --bindings=./dist/server/node_bindings/
+npm run --prefix="$SNARKY_JS_PATH" dev
