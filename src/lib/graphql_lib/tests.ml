fa13da233ac7b372df527baa92263dc7781313b4
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2022-09-01 09:19:34 +0100

    Include other half of unsigned integers to be tested

--- src/lib/graphql_lib/tests.ml
+++ src/lib/graphql_lib/tests.ml
@@ -64,7 +64,7 @@ module UInt32_gen = struct
   include Unsigned.UInt32
 
   let gen =
-    Int32.gen_incl 0l Int32.max_value
+    Int32.quickcheck_generator
     |> Quickcheck.Generator.map ~f:Unsigned.UInt32.of_int32
 
   let sexp_of_t = Fn.compose Int32.sexp_of_t Unsigned.UInt32.to_int32
@@ -74,7 +74,7 @@ module UInt64_gen = struct
   include Unsigned.UInt64
 
   let gen =
-    Int64.gen_incl 0L Int64.max_value
+    Int64.quickcheck_generator
     |> Quickcheck.Generator.map ~f:Unsigned.UInt64.of_int64
 
   let sexp_of_t = Fn.compose Int64.sexp_of_t Unsigned.UInt64.to_int64
@@ -84,28 +84,6 @@ module String_gen = struct
   include String
 
   let gen = gen_nonempty
-end
-
-module Time_gen = struct
-  type t = Core_kernel.Time.t
-
-  (* The following generator function is copied from version 0.15.0 of the core library, and only generates values that can be serialized.
-     https://github.com/janestreet/core/blob/35941320a3eab628786ae3853e5f753a3ab357c2/core/src/span_float.ml#L742-L754.
-     See issue https://github.com/MinaProtocol/mina/issues/11310.
-     Once the core library is updated to >= 0.15.0, [Core.Time.quickcheck_generator] should be used instead work.*)
-
-  let gen =
-    let span_gen =
-      let open Core_kernel_private.Span_float in
-      let millenium = of_day (Float.round_up (365.2425 *. 1000.)) in
-      Quickcheck.Generator.filter quickcheck_generator ~f:(fun t ->
-          neg millenium <= t && t <= millenium )
-    in
-    Quickcheck.Generator.map span_gen ~f:Core_kernel.Time.of_span_since_epoch
-
-  let sexp_of_t = Core.Time.sexp_of_t
-
-  let compare x y = Core_kernel.Time.robustly_compare x y
 end
 
 module Span_gen = struct
@@ -118,13 +96,34 @@ module Span_gen = struct
         neg millenium <= t && t <= millenium )
 
   let compare x y =
-    (* Core_kernel.Time.Span.robustly_compare x y *)
     (* https://github.com/janestreet/core_kernel/blob/v0.14.1/src/float.ml#L61 *)
+    (* Note: We have to use a different tolerance than 
+       `Core_kernel.Time.Span.robustly_compare` does 
+       because spans are rounded to the millisecond when 
+       serialized through GraphQL. See the implementation 
+       of Span in the `Graphql_basic_scalars` module. *)
     let tolerance = 1E-3 in
     let diff = x - y in
     if diff < of_sec ~-.tolerance then -1
     else if diff > of_sec tolerance then 1
     else 0
+end
+
+module Time_gen = struct
+  type t = Core_kernel.Time.t
+
+  (* The following generator function is copied from version 0.15.0 of the core library, and only generates values that can be serialized.
+     https://github.com/janestreet/core/blob/35941320a3eab628786ae3853e5f753a3ab357c2/core/src/span_float.ml#L742-L754.
+     See issue https://github.com/MinaProtocol/mina/issues/11310.
+     Once the core library is updated to >= 0.15.0, [Core.Time.quickcheck_generator] should be used instead work.*)
+
+  let gen =
+    Quickcheck.Generator.map Span_gen.gen
+      ~f:Core_kernel.Time.of_span_since_epoch
+
+  let sexp_of_t = Core.Time.sexp_of_t
+
+  let compare x y = Core_kernel.Time.robustly_compare x y
 end
 
 module Slot_gen = struct
