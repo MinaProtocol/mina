9db4d97d1e67ecab1ed29456d6592338a34054e7
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2022-09-08 07:24:07 +0100

    Basic skipping mechanism

--- scripts/compare_versioned_types.py
+++ scripts/compare_versioned_types.py
@@ -10,10 +10,22 @@ import sys
 
 from compare_versioned_items import run_comparison
 
+# This is the list of file while has never been changed before
+# and they haven't run through the CI before, and then fail 
+# when modified.
+skip_list = ["src/nonconsensus/snark_params/tick.ml"]
+
+def skip(original, modified):
+    for skip_item in skip_list:
+        if skip_item in original:
+            return True
+    return False
+
 if __name__ == "__main__":
     if len(sys.argv) != 3 or sys.argv[1] == sys.argv[2] :
         print("Usage: %s path1-to-file.ml path2-to-file.ml" % sys.argv[0], file=sys.stderr)
         print("The .ml files must have the same name, with different paths")
         sys.exit(1)
 
-    run_comparison('_build/default/src/lib/ppx_version/tools/print_versioned_types.exe','Versioned types',sys.argv[1],sys.argv[2])
+    if not (skip (sys.argv[1], sys.argv[2])):  
+        run_comparison('_build/default/src/lib/ppx_version/tools/print_versioned_types.exe','Versioned types',sys.argv[1],sys.argv[2])
