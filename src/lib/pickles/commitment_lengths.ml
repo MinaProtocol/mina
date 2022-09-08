fe18d1b25849d97e33c14494425101e66e86a072
Izaak Meckler <ihmeckler@gmail.coom>
2022-09-08 09:13:01 -0700

    commitment length comments

--- src/lib/pickles/commitment_lengths.ml
+++ src/lib/pickles/commitment_lengths.ml
@@ -3,6 +3,13 @@ open Pickles_types
 open Import
 open Plonk_types
 
+(* Specifies the number of group elements in each message in the protocol, which
+   are polynomials committed to as "split commitments". I.e., we fix an SRS length N,
+   and then a polynomial of degree d is committed to by chunking it up into ceil(d / N)
+   polynomials of degree N, and committing to each individually.
+
+   All commitments have length 1, except the quotient polynomial commitment
+   which has length 7. *)
 let create (type a) ~(of_int : int -> a) :
     (a Columns_vec.t, a, a) Messages.Poly.t =
   let one = of_int 1 in
