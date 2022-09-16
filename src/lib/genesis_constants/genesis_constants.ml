db096702a26c973246c3c39d776cb252950d39f8
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2022-09-16 14:49:24 -0700

    zkapp cost in runtime config

--- src/lib/genesis_constants/genesis_constants.ml
+++ src/lib/genesis_constants/genesis_constants.ml
@@ -325,8 +325,10 @@ module T = struct
     ; txpool_max_size : int
     ; num_accounts : int option
     ; transaction_expiry_hr : int
-    ; max_proof_zkapp_command : int
-    ; max_zkapp_command : int
+    ; zkapp_proof_update_cost : float
+    ; zkapp_signed_single_update_cost : float
+    ; zkapp_signed_pair_update_cost : float
+    ; zkapp_transaction_cost_limit : float
     ; max_event_elements : int
     ; max_sequence_event_elements : int
     }
@@ -381,8 +383,13 @@ let compiled : t =
   ; txpool_max_size = pool_max_size
   ; num_accounts = None
   ; transaction_expiry_hr = Mina_compile_config.transaction_expiry_hr
-  ; max_proof_zkapp_command = Mina_compile_config.max_proof_zkapp_command
-  ; max_zkapp_command = Mina_compile_config.max_zkapp_command
+  ; zkapp_proof_update_cost = Mina_compile_config.zkapp_proof_update_cost
+  ; zkapp_signed_single_update_cost =
+      Mina_compile_config.zkapp_signed_single_update_cost
+  ; zkapp_signed_pair_update_cost =
+      Mina_compile_config.zkapp_signed_pair_update_cost
+  ; zkapp_transaction_cost_limit =
+      Mina_compile_config.zkapp_transaction_cost_limit
   ; max_event_elements = Mina_compile_config.max_event_elements
   ; max_sequence_event_elements =
       Mina_compile_config.max_sequence_event_elements
