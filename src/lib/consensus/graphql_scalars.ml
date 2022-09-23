03a40aed3de209c492a36b7cfb0d37e9a49d7b2d
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2022-09-23 15:46:40 +0100

    Refactor consensus GraphQL scalars

--- src/lib/consensus/graphql_scalars.ml
+++ src/lib/consensus/graphql_scalars.ml
@@ -18,22 +18,6 @@ module Epoch_scalar =
       let doc = "epoch"
     end)
 
-(* TESTS *)
-module Slot_gen = struct
-  include Slot
-
-  let gen =
-    Core_kernel.Quickcheck.Generator.map ~f:Slot.of_uint32
-      (Constants.for_unit_tests |> Lazy.force |> gen)
-end
-
-let%test_module "Epoch" = (module Make_test (Epoch_scalar) (Epoch))
-
-let%test_module "Slot" = (module Make_test (Slot_scalar) (Slot_gen))
-
-module Slot = Slot_scalar
-module Epoch = Epoch_scalar
-
 module VrfScalar =
   Make_scalar_using_to_string
     (Consensus_vrf.Scalar)
@@ -43,12 +27,6 @@ module VrfScalar =
       let doc = "consensus vrf scalar"
     end)
 
-module VrfScalar_gen = struct
-  include Snark_params.Tick.Inner_curve.Scalar
-end
-
-let%test_module "VrfScalar" = (module Make_test (VrfScalar) (VrfScalar_gen))
-
 module VrfOutputTruncated =
   Make_scalar_using_base58_check
     (Consensus_vrf.Output.Truncated)
@@ -57,15 +35,6 @@ module VrfOutputTruncated =
 
       let doc = "truncated vrf output"
     end)
-
-module VrfOutputTruncated_gen = struct
-  include Consensus_vrf.Output.Truncated
-
-  let gen = Core_kernel.Quickcheck.Generator.return dummy
-end
-
-let%test_module "VrfOutputTruncated" =
-  (module Make_test (VrfOutputTruncated) (VrfOutputTruncated_gen))
 
 module BodyReference = struct
   open Body_reference
@@ -83,6 +52,37 @@ module BodyReference = struct
          as a hex-encoded string"
       ~coerce:serialize
 end
+  
+(* TESTS *)
+module Slot_gen = struct
+  include Slot
+
+  let gen =
+    Core_kernel.Quickcheck.Generator.map ~f:Slot.of_uint32
+      (Constants.for_unit_tests |> Lazy.force |> gen)
+end
+
+let%test_module "Epoch" = (module Make_test (Epoch_scalar) (Epoch))
+
+let%test_module "Slot" = (module Make_test (Slot_scalar) (Slot_gen))
+
+module Slot = Slot_scalar
+module Epoch = Epoch_scalar
+
+module VrfScalar_gen = struct
+  include Snark_params.Tick.Inner_curve.Scalar
+end
+
+let%test_module "VrfScalar" = (module Make_test (VrfScalar) (VrfScalar_gen))
+
+module VrfOutputTruncated_gen = struct
+  include Consensus_vrf.Output.Truncated
+
+  let gen = Core_kernel.Quickcheck.Generator.return dummy
+end
+
+let%test_module "VrfOutputTruncated" =
+  (module Make_test (VrfOutputTruncated) (VrfOutputTruncated_gen))
 
 module BodyReference_gen = struct
   include Body_reference
