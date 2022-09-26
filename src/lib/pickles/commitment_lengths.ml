e7af57524c53de248e10549d876757fb95421780
Richard Bonichon <richard.bonichon@gmail.com>
2022-09-26 14:51:49 +0200

    Pickles: remove global opens in Commitment_lengths

--- src/lib/pickles/commitment_lengths.ml
+++ src/lib/pickles/commitment_lengths.ml
@@ -1,12 +1,6 @@
-open Core_kernel
-open Pickles_types
-open Import
-open Plonk_types
-
-let create (type a) ~(of_int : int -> a) :
-    (a Columns_vec.t, a, a) Messages.Poly.t =
+let create ~of_int =
   let one = of_int 1 in
-  { w = Vector.init Plonk_types.Columns.n ~f:(fun _ -> one)
-  ; z = one
-  ; t = of_int 7
-  }
+  let t = of_int 7 in
+  let z = one in
+  let w = Pickles_types.(Vector.init Plonk_types.Columns.n ~f:(fun _ -> one)) in
+  Pickles_types.Plonk_types.Messages.Poly.{ w; z; t }
