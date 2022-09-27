b75a5d3434218d17b83b6c9c6dd9be2a6bbc154b
mrmr1993 <mr_1993@hotmail.co.uk>
2022-09-27 16:25:10 +0100

    Revert "REVERT ME: Run the replayer with --continue-on-error"
    This reverts commit 6f38fb243a948d96c019f1477beff8fe25e88ca3.

--- buildkite/scripts/replayer-test.sh
+++ buildkite/scripts/replayer-test.sh
@@ -38,4 +38,4 @@ cd /workdir
 
 echo "Running replayer"
 mina-replayer --archive-uri postgres://postgres:$PGPASSWORD@localhost:5432/archive \
-	      --input-file $TEST_DIR/input.json --output-file /dev/null --continue-on-error
+	      --input-file $TEST_DIR/input.json --output-file /dev/null
