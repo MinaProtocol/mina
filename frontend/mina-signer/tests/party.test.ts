4f66d5a2c3344742bd2cfb68292b3742d45b0e79
Gregor <gregor.mitscha-baude@gmx.at>
2022-09-28 09:15:13 +0200

    fix mina-signer party test

--- frontend/mina-signer/tests/party.test.ts
+++ frontend/mina-signer/tests/party.test.ts
@@ -90,6 +90,7 @@ let mockedZkappCommand = {
 
         useFullCommitment: true,
         caller: "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf",
+        authorizationKind: "Signature",
       },
       authorization: {
         proof: null,
