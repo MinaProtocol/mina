318dcc6f6217480d8c4580ad81348bb3be09414a
Yves-Stan Le Cornec <yves.stan.lecornec@tweag.io>
2022-09-29 15:55:54 +0200

    Mina_base.Frozen_ledger_hash0.V1.t

--- src/lib/mina_wire_types/mina_base/mina_base.ml
+++ src/lib/mina_wire_types/mina_base/mina_base.ml
@@ -36,3 +36,4 @@ module Stack_frame = Mina_base_stack_frame
 module Sok_message = Mina_base_sok_message
 module Fee_with_prover = Mina_base_fee_with_prover
 module State_body_hash = Mina_base_state_body_hash
+module Frozen_ledger_hash0 = Mina_base_frozen_ledger_hash0
