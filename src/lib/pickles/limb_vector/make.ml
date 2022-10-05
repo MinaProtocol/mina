92453905e25c1de3f633ce50d60dde4f617a5e68
Richard Bonichon <richard.bonichon@gmail.com>
2022-10-05 11:22:52 +0200

    Pickles/Limb_vector: remove open in Make

--- src/lib/pickles/limb_vector/make.ml
+++ src/lib/pickles/limb_vector/make.ml
@@ -1,13 +1,12 @@
-open Core_kernel
-open Pickles_types
-
-module T (Impl : Snarky_backendless.Snark_intf.Run) (N : Vector.Nat_intf) =
+module T
+    (Impl : Snarky_backendless.Snark_intf.Run)
+    (N : Pickles_types.Vector.Nat_intf) =
 struct
   open Impl
 
   type t = Field.t
 
-  let length = 64 * Nat.to_int N.n
+  let length = 64 * Pickles_types.Nat.to_int N.n
 
   module Constant = Constant.Make (N)
 
@@ -16,5 +15,6 @@ struct
     |> Typ.transport
          ~there:(fun x -> Field.Constant.project (Constant.to_bits x))
          ~back:(fun x ->
-           Constant.of_bits (List.take (Field.Constant.unpack x) length) )
+           Constant.of_bits
+             (Core_kernel.List.take (Field.Constant.unpack x) length) )
 end
