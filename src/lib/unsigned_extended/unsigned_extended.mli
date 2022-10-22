1bd4a3fc16c8ef89c66c6192498a9681fefca91b
Paul Steckler <steck@stecksoft.com>
2022-10-21 18:40:21 -0700

    more annotations

--- src/lib/unsigned_extended/unsigned_extended.mli
+++ src/lib/unsigned_extended/unsigned_extended.mli
@@ -9,16 +9,17 @@ module type S = S
 module Extend : F
 
 module UInt64 : sig
+  [%%versioned:
   module Stable : sig
+    [@@@no_toplevel_latest_type]
+
     module V1 : sig
+      [@@@with_all_version_tags]
+
       type t = Unsigned.UInt64.t
       [@@deriving bin_io, sexp, hash, compare, equal, yojson, version]
-
-      module With_all_version_tags : Bin_prot.Binable.S with type t = t
     end
-
-    module Latest = V1
-  end
+  end]
 
   include S with type t = Stable.Latest.t
 
@@ -30,16 +31,17 @@ module UInt64 : sig
 end
 
 module UInt32 : sig
+  [%%versioned:
   module Stable : sig
+    [@@@no_toplevel_latest_type]
+
     module V1 : sig
+      [@@@with_all_version_tags]
+
       type t = Unsigned.UInt32.t
       [@@deriving bin_io, sexp, hash, compare, equal, yojson, version]
-
-      module With_all_version_tags : Bin_prot.Binable.S with type t = t
     end
-
-    module Latest = V1
-  end
+  end]
 
   include S with type t = Stable.Latest.t
 
