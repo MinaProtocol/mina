1f50b15bfdcddf9f48e42a7febd88545a56e84ab
David Wong <davidwong.crypto@gmail.com>
2022-11-09 14:35:55 -0800

    [crypto] added more tests to snarky

---
+++ src/lib/crypto/snarky_tests/examples/ternary.asm
@@ -0,0 +1,9 @@
+row0.pub.Generic<1,0,0,0,0>
+.l1 -> row1.r2
+
+row1.Generic<1,0,-1,0,1><-1,0,0,1,0>
+.l1 -> row0.l1, .o1 -> row2.l1
+.l2 -> row1.l1, .r2 -> row1.l2
+
+row2.Generic<1,0,0,0,-2>
+.l1 -> row1.o1
