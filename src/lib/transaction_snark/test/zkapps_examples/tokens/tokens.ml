476acbaa655eff5bd73bb7a8c41723147dc295c9
mrmr1993 <mr_1993@hotmail.co.uk>
2022-11-16 17:05:22 +0000

    Add initialize_ledger, finalize_ledger arguments

--- src/lib/transaction_snark/test/zkapps_examples/tokens/tokens.ml
+++ src/lib/transaction_snark/test/zkapps_examples/tokens/tokens.ml
@@ -46,7 +46,8 @@ let%test_module "Tokens test" =
         account_update
     end
 
-    let test_zkapp_command ?expected_failure zkapp_command =
+    let test_zkapp_command ?expected_failure ~initialize_ledger ~finalize_ledger
+        zkapp_command =
       let memo = Signed_command_memo.empty in
       let fee_payer : Account_update.Fee_payer.t =
         { body =
@@ -58,31 +59,37 @@ let%test_module "Tokens test" =
         }
       in
       let zkapp_command : Zkapp_command.t =
-          { fee_payer; account_updates = zkapp_command; memo }
+        { fee_payer; account_updates = zkapp_command; memo }
         |> Zkapps_examples.insert_signatures pk_compressed sk
       in
       Ledger.with_ledger ~depth:ledger_depth ~f:(fun ledger ->
-          let account =
-            Account.create account_id
-              Currency.Balance.(
-                Option.value_exn
-                  (add_amount zero (Currency.Amount.of_nanomina_int_exn 500)))
-          in
-          let _, loc =
-            Ledger.get_or_create_account ledger account_id account
-            |> Or_error.ok_exn
-          in
+          let aux = initialize_ledger ledger in
           Async.Thread_safe.block_on_async_exn (fun () ->
               check_zkapp_command_with_merges_exn ?expected_failure ledger
                 [ zkapp_command ] ) ;
-          Ledger.get ledger loc )
+          finalize_ledger aux ledger )
+
+    let initialize_ledger ledger =
+      let account =
+        Account.create account_id
+          Currency.Balance.(
+            Option.value_exn
+              (add_amount zero (Currency.Amount.of_nanomina_int_exn 500)))
+      in
+      let _, loc =
+        Ledger.get_or_create_account ledger account_id account
+        |> Or_error.ok_exn
+      in
+      loc
+
+    let finalize_ledger loc ledger = Ledger.get ledger loc
 
     let%test_unit "Initialize" =
       let account =
         []
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
       in
       let zkapp_state =
         (Option.value_exn (Option.value_exn account).zkapp).app_state
@@ -97,7 +104,7 @@ let%test_module "Tokens test" =
         |> Zkapp_command.Call_forest.cons_tree Account_updates.update_state
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
       in
       let zkapp_state =
         (Option.value_exn (Option.value_exn account).zkapp).app_state
@@ -113,7 +120,7 @@ let%test_module "Tokens test" =
         |> Zkapp_command.Call_forest.cons_tree Account_updates.update_state
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
       in
       let zkapp_state =
         (Option.value_exn (Option.value_exn account).zkapp).app_state
@@ -127,7 +134,7 @@ let%test_module "Tokens test" =
         []
         |> Zkapp_command.Call_forest.cons_tree Account_updates.update_state
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
              ~expected_failure:Account_proved_state_precondition_unsatisfied
       in
       assert (Option.is_none (Option.value_exn account).zkapp)
@@ -138,7 +145,7 @@ let%test_module "Tokens test" =
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
              ~expected_failure:Account_proved_state_precondition_unsatisfied
       in
       assert (Option.is_none (Option.value_exn account).zkapp)
@@ -150,7 +157,7 @@ let%test_module "Tokens test" =
         |> Zkapp_command.Call_forest.cons_tree Account_updates.update_state
         |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
         |> Zkapp_command.Call_forest.cons Account_updates.deploy
-        |> test_zkapp_command
+        |> test_zkapp_command ~initialize_ledger ~finalize_ledger
              ~expected_failure:Account_proved_state_precondition_unsatisfied
       in
       assert (Option.is_none (Option.value_exn account).zkapp)
@@ -164,7 +171,7 @@ let%test_module "Tokens test" =
             []
             |> Zkapp_command.Call_forest.cons_tree Account_updates.update_state
             |> Zkapp_command.Call_forest.cons_tree Account_updates.initialize
-            |> test_zkapp_command )
+            |> test_zkapp_command ~initialize_ledger ~finalize_ledger )
       in
       assert (Or_error.is_error account)
   end )
