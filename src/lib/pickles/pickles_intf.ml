f36af4503aa072755c991c49e2ebbacf83ab73a8
mrmr1993 <mr_1993@hotmail.co.uk>
2022-11-17 23:27:14 +0000

    Update test to be more representative of the underlying error

--- src/lib/pickles/pickles_intf.ml
+++ src/lib/pickles/pickles_intf.ml
@@ -306,10 +306,6 @@ module type S = sig
       module Max_branches : Nat.Add.Intf
 
       module Max_width = Nat.N2
-
-      module For_tests : sig
-        val forget_wrap_vk : t -> t
-      end
     end
 
     module Proof : sig
--- src/lib/transaction_snark/test/verify-simple-test/transaction_snark_tests_verify_simple_test.ml
+++ src/lib/transaction_snark/test/verify-simple-test/transaction_snark_tests_verify_simple_test.ml
@@ -1,10 +1,16 @@
+open Core_kernel
 open Mina_base
 
-let `VK {With_hash.data= vk; hash= _}, `Prover p =
+let `VK { With_hash.data = vk; hash = _ }, `Prover p =
   Transaction_snark.For_tests.create_trivial_snapp
     ~constraint_constants:Genesis_constants.Constraint_constants.compiled ()
 
-let vk = Pickles.Side_loaded.Verification_key.For_tests.forget_wrap_vk vk
+let vk =
+  vk
+  |> Binable.to_string
+       (module Pickles.Side_loaded.Verification_key.Stable.Latest)
+  |> Binable.of_string
+       (module Pickles.Side_loaded.Verification_key.Stable.Latest)
 
 let stmt : Zkapp_statement.t =
   { Zkapp_statement.Poly.account_update = Snark_params.Tick.Field.one
@@ -19,4 +25,12 @@ let to_verify = [ (vk, stmt, proof) ]
 
 let verify () = Pickles.Side_loaded.verify ~typ:Zkapp_statement.typ to_verify
 
-let%test "Verifies" = Async.Thread_safe.block_on_async_exn (fun () -> verify ())
+let%test_module "Simple verifies test" =
+  ( module struct
+    let () = Backtrace.elide := false
+
+    let () = Pickles.Side_loaded.srs_precomputation ()
+
+    let%test "Verifies" =
+      Async.Thread_safe.block_on_async_exn (fun () -> verify ())
+  end )
