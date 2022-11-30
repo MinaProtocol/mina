b465451939a7d7f0a86204e4b2347ba0ccdd7f41
Tang Jiawei <tang@o1labs.org>
2022-11-30 23:12:23 +0800

    restore transaction_pool from develop

--- src/lib/mina_base/zkapp_precondition.ml
+++ src/lib/mina_base/zkapp_precondition.ml
@@ -1315,13 +1315,13 @@ module Protocol_state = struct
 
   let valid_until time : t =
     { snarked_ledger_hash = Ignore
-    ; timestamp = Check time
+    ; timestamp = Ignore
     ; blockchain_length = Ignore
     ; min_window_density = Ignore
     ; last_vrf_output = ()
     ; total_currency = Ignore
     ; global_slot_since_hard_fork = Ignore
-    ; global_slot_since_genesis = Ignore
+    ; global_slot_since_genesis = Check time
     ; staking_epoch_data = epoch_data
     ; next_epoch_data = epoch_data
     }
