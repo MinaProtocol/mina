d0da49acf3403f9252800cd336f3541bb2cc5de8
Martin Minkov <minkovlmartin@gmail.com>
2022-12-01 15:33:26 -0800

    test-executive: Add padding payments and refactor zkapp payments to use fish kps

--- src/app/test_executive/zkapps_precondition_nonce_test.ml
+++ src/app/test_executive/zkapps_precondition_nonce_test.ml
@@ -65,13 +65,40 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
     incr transactions_sent ;
     send_zkapp ~logger node zkapp_command
 
+  (* Call [f] [n] times in sequence *)
+  let repeat_seq ~n ~f =
+    let open Malleable_error.Let_syntax in
+    let rec go n =
+      if n = 0 then return ()
+      else
+        let%bind () = f () in
+        go (n - 1)
+    in
+    go n
+
+  let send_padding_transactions ~fee ~logger ~n nodes =
+    let sender = List.nth_exn nodes 0 in
+    let receiver = List.nth_exn nodes 1 in
+    let open Malleable_error.Let_syntax in
+    let%bind sender_pub_key = Util.pub_key_of_node sender in
+    let%bind receiver_pub_key = Util.pub_key_of_node receiver in
+    repeat_seq ~n ~f:(fun () ->
+        Network.Node.must_send_payment ~logger sender ~sender_pub_key
+          ~receiver_pub_key ~amount:Currency.Amount.one ~fee
+        >>| ignore )
+
   let run network t =
     let open Malleable_error.Let_syntax in
     (* Setup *)
+    (* Use genesis accounts instead -- zkapps test*)
     let logger = Logger.create () in
     let block_producer_nodes = Network.block_producers network in
     let node = List.hd_exn block_producer_nodes in
-    let%bind fee_payer_pk = Util.pub_key_of_node node in
+    let[@warning "-8"] [ fish1_kp; fish2_kp ] =
+      Network.extra_genesis_keypairs network
+    in
+    let fish1_pk = Signature_lib.Public_key.compress fish1_kp.public_key in
+    let fish2_pk = Signature_lib.Public_key.compress fish2_kp.public_key in
     let fee = Currency.Fee.of_nanomina_int_exn 10_000_000 in
     let memo = Signed_command_memo.create_from_string_exn "Zkapp update all" in
     let with_timeout =
@@ -101,7 +128,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
       Zkapp_command.of_simple
         { fee_payer =
             { body =
-                { public_key = fee_payer_pk
+                { public_key = fish1_pk
                 ; fee
                 ; valid_until = None
                 ; nonce = Account.Nonce.of_int 0
@@ -110,7 +137,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
             }
         ; account_updates =
             [ { body =
-                  { public_key = fee_payer_pk
+                  { public_key = fish1_pk
                   ; update = Account_update.Update.noop
                   ; token_id = Token_id.default
                   ; balance_change = Currency.Amount.Signed.zero
@@ -138,7 +165,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
       Zkapp_command.of_simple
         { fee_payer =
             { body =
-                { public_key = fee_payer_pk
+                { public_key = fish1_pk
                 ; fee
                 ; valid_until = None
                 ; nonce = Account.Nonce.of_int 1
@@ -147,7 +174,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
             }
         ; account_updates =
             [ { body =
-                  { public_key = fee_payer_pk
+                  { public_key = fish1_pk
                   ; update = Account_update.Update.noop
                   ; token_id = Token_id.default
                   ; balance_change = Currency.Amount.Signed.zero
@@ -175,16 +202,16 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
       Zkapp_command.of_simple
         { fee_payer =
             { body =
-                { public_key = fee_payer_pk
+                { public_key = fish2_pk
                 ; fee
                 ; valid_until = None
-                ; nonce = Account.Nonce.of_int 3
+                ; nonce = Account.Nonce.of_int 0
                 }
             ; authorization = Signature.dummy
             }
         ; account_updates =
             [ { body =
-                  { public_key = fee_payer_pk
+                  { public_key = fish2_pk
                   ; update = Account_update.Update.noop
                   ; token_id = Token_id.default
                   ; balance_change = Currency.Amount.Signed.zero
@@ -196,7 +223,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
                   ; preconditions =
                       { Account_update.Preconditions.network =
                           Zkapp_precondition.Protocol_state.accept
-                      ; account = Nonce (Account.Nonce.of_int 4)
+                      ; account = Nonce (Account.Nonce.of_int 1)
                       }
                   ; use_full_commitment = false
                   ; caller = Call
@@ -208,11 +235,11 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
         ; memo
         }
     in
+    (* Submit transactions*)
     let%bind () =
       section "Send a zkApp transaction with an invalid account_update nonce"
         (send_zkapp ~logger node invalid_nonce_transaction)
     in
-    (* Submit transactions*)
     let%bind () =
       section
         "Send a zkApp transaction that has it's nonce properly incremented \
@@ -220,10 +247,7 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
         (send_zkapp ~logger node valid_nonce_transaction)
     in
     let%bind () =
-      section
-        "Send a zkApp transaction that has it's nonce properly incremented \
-         after the second transaction"
-        (send_zkapp ~logger node t1)
+      section "Send a zkApp transaction  " (send_zkapp ~logger node t1)
     in
     (* Check transactions*)
     let%bind () =
@@ -239,14 +263,27 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
         (wait_for_zkapp ~has_failures:false valid_nonce_transaction)
     in
     let%bind () =
-      section
-        "Wait for second zkApp transaction with valid nonce to be accepted by \
-         transition frontier"
+      section "Wait for zkApp transaction to be accepted by transition frontier"
         (wait_for_zkapp ~has_failures:false t1)
+    in
+    let%bind () =
+      let padding_payments =
+        (* for work_delay=1 and transaction_capacity=4 per block*)
+        let needed = 36 in
+        if !transactions_sent >= needed then 0 else needed - !transactions_sent
+      in
+      let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
+      send_padding_transactions block_producer_nodes ~fee ~logger
+        ~n:padding_payments
     in
     let%bind () =
       section_hard "wait for 1 block to be produced"
         (wait_for t (Wait_condition.blocks_to_be_produced 1))
+    in
+    let%bind () =
+      section_hard "Wait for proof to be emitted"
+        (wait_for t
+           (Wait_condition.ledger_proofs_emitted_since_genesis ~num_proofs:2) )
     in
     (* End test *)
     section_hard "Running replayer"
