ca179262668da8391d34145360a87cb9cbe1dab9
mrmr1993 <mr_1993@hotmail.co.uk>
2022-12-06 19:33:18 +0000

    Add a doc-comment

--- src/lib/crypto/kimchi_bindings/wasm/src/wasm_ocaml_serde/mod.rs
+++ src/lib/crypto/kimchi_bindings/wasm/src/wasm_ocaml_serde/mod.rs
@@ -1,3 +1,17 @@
+//! This module constructs a serde serializer (and deserializer) to convert Rust structures to (and from) Js types expected by js-of-ocaml.
+//! js-of-ocaml expects arrays of values instead of objects, so a Rust structure like:
+//!
+//! ```ignore
+//! { a: F, b: Vec<F>, c: SomeType }
+//! ```
+//!
+//! must be converted to an array that looks like this:
+//!
+//! ```ignore
+//! // notice the leading 0, which is an artifact of OCaml's memory layout and how js-of-ocaml is implemented.
+//! [0, a, b, c]
+//! ```
+
 use wasm_bindgen::prelude::*;
 
 pub mod de;
