ac23938c01712eeab2c71cb0bba06b8fe377b691
David Lehuby <david.lehuby@minaprotocol.com>
2022-12-07 11:05:14 +1100

    12318 - Specify OFFLINE in rosetta docker-standalone-start.sh

--- src/app/rosetta/docker-standalone-start.sh
+++ src/app/rosetta/docker-standalone-start.sh
@@ -3,13 +3,13 @@
 set -eou pipefail
 
 # Allows configuring the port that each service runs on.
-# To interact with rosetta, use MINA_ROSETTA_PORT.
-MINA_ROSETTA_PORT=${MINA_ROSETTA_PORT:=3087}
+# To interact with rosetta, use MINA_ROSETTA_OFFLINE_PORT.
+MINA_ROSETTA_OFFLINE_PORT=${MINA_ROSETTA_OFFLINE_PORT:=3087}
 # This script does not run a daemon or archive node at all, so the remaining MINA_*_PORTs from other scripts are not used here
 LOG_LEVEL=${LOG_LEVEL:=Debug}
 
 # rosetta
-echo "========================= STARTING ROSETTA API on PORT ${MINA_ROSETTA_PORT} ==========================="
+echo "========================= STARTING ROSETTA API on PORT ${MINA_ROSETTA_OFFLINE_PORT} ==========================="
 mina-rosetta \
   -log-level "${LOG_LEVEL}" \
-  -port "${MINA_ROSETTA_PORT}" $@
+  -port "${MINA_ROSETTA_OFFLINE_PORT}" $@
