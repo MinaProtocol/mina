cf0304a48089d9f9482c4e24e800ca946fb2312d
Gregor Mitscha-Baude <gregor.mitscha-baude@gmx.at>
2022-12-20 00:35:49 +0100

    fix mina-signer test

--- frontend/mina-signer/tests/party.test.ts
+++ frontend/mina-signer/tests/party.test.ts
@@ -85,7 +85,11 @@ let mockedZkappCommand = {
 
         useFullCommitment: true,
         caller: "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf",
-        authorizationKind: "Signature",
+        authorizationKind: {
+          isSigned: true,
+          isProved: false,
+          verificationKeyHash: "0",
+        },
       },
       authorization: {
         proof: null,
