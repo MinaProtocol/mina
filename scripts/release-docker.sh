b68c2550dd14b788c6dfcac1311562d8d237f0d2
Helena Li <helena.li0x1@gmail.com>
2022-12-20 12:42:29 -0800

    add valid service to release-docker.sh

--- scripts/release-docker.sh
+++ scripts/release-docker.sh
@@ -10,7 +10,7 @@ set +x
 CLEAR='\033[0m'
 RED='\033[0;31m'
 # Array of valid service names
-VALID_SERVICES=('mina-archive', 'mina-daemon' 'mina-rosetta' 'mina-toolchain' 'bot' 'leaderboard' 'delegation-backend' 'delegation-backend-toolchain')
+VALID_SERVICES=('mina-archive', 'mina-daemon' 'mina-rosetta' 'mina-toolchain' 'bot' 'leaderboard' 'delegation-backend' 'delegation-backend-toolchain' 'mina-test-executive')
 
 function usage() {
   if [[ -n "$1" ]]; then
