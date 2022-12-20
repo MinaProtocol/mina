f1c349002128a85dd0ba513a7a6ad831b420b933
Florian <florian@technotro.com>
2022-12-20 12:37:16 +0100

    add json roundtrip

--- src/lib/snarky_js_bindings/test_module/inductive-proofs.js
+++ src/lib/snarky_js_bindings/test_module/inductive-proofs.js
@@ -86,11 +86,14 @@ await testRecursion(MaxProofsVerifiedTwo, 2);
 async function testRecursion(Program, maxProofsVerified) {
   console.log(`testing maxProofsVerified = ${maxProofsVerified}`);
 
+  let ProofClass = Experimental.ZkProgram.Proof(Program);
+
   tic("executing base case..");
   let initialProof = await Program.baseCase(Field(0));
+  toc();
+  initialProof = testJsonRoundtrip(ProofClass, initialProof);
   initialProof.verify();
   initialProof.publicInput.assertEquals(Field(0));
-  toc();
 
   if (initialProof.maxProofsVerified != maxProofsVerified) {
     throw Error(
@@ -103,6 +106,8 @@ async function testRecursion(Program, maxProofsVerified) {
 
   tic("executing mergeOne..");
   p1 = await Program.mergeOne(Field(1), initialProof);
+  toc();
+  p1 = testJsonRoundtrip(ProofClass, p1);
   p1.verify();
   p1.publicInput.assertEquals(Field(1));
   if (p1.maxProofsVerified != maxProofsVerified) {
@@ -110,11 +115,12 @@ async function testRecursion(Program, maxProofsVerified) {
       `Expected p1 to have maxProofsVerified = ${maxProofsVerified} but has ${p1.maxProofsVerified}`
     );
   }
-  toc();
 
   if (initialProof.maxProofsVerified == 1) return;
   tic("executing mergeTwo..");
   p2 = await Program.mergeTwo(Field(2), initialProof, p1);
+  toc();
+  p2 = testJsonRoundtrip(ProofClass, p2);
   p2.verify();
   p2.publicInput.assertEquals(Field(2));
   if (p2.maxProofsVerified != maxProofsVerified) {
@@ -122,7 +128,15 @@ async function testRecursion(Program, maxProofsVerified) {
       `Expected p2 to have maxProofsVerified = ${maxProofsVerified} but has ${p2.maxProofsVerified}`
     );
   }
-  toc();
+}
+
+function testJsonRoundtrip(ProofClass, proof) {
+  let jsonProof = proof.toJSON();
+  console.log(
+    "json roundtrip",
+    JSON.stringify({ ...jsonProof, proof: jsonProof.proof.slice(0, 10) + ".." })
+  );
+  return ProofClass.fromJSON(jsonProof);
 }
 
 shutdown();
