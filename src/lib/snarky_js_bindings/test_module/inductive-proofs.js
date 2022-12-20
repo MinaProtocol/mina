ae69abb5647eb891e1f0f18bb1fb16c604804dd1
Florian <florian@technotro.com>
2022-12-20 12:29:00 +0100

    add integration tests

--- src/lib/snarky_js_bindings/test_module/inductive-proofs.js
+++ src/lib/snarky_js_bindings/test_module/inductive-proofs.js
@@ -3,7 +3,21 @@ import { tic, toc } from "./tictoc.js";
 
 await isReady;
 
-let MyProgram = Experimental.ZkProgram({
+let MaxProofsVerifiedZero = Experimental.ZkProgram({
+  publicInput: Field,
+
+  methods: {
+    baseCase: {
+      privateInputs: [],
+
+      method(publicInput) {
+        publicInput.assertEquals(Field.zero);
+      },
+    },
+  },
+});
+
+let MaxProofsVerifiedOne = Experimental.ZkProgram({
   publicInput: Field,
 
   methods: {
@@ -15,7 +29,7 @@ let MyProgram = Experimental.ZkProgram({
       },
     },
 
-    inductiveCase: {
+    mergeOne: {
       privateInputs: [SelfProof],
 
       method(publicInput, earlierProof) {
@@ -26,22 +40,89 @@ let MyProgram = Experimental.ZkProgram({
   },
 });
 
-tic("compiling MyProgram");
-await MyProgram.compile();
+let MaxProofsVerifiedTwo = Experimental.ZkProgram({
+  publicInput: Field,
+
+  methods: {
+    baseCase: {
+      privateInputs: [],
+
+      method(publicInput) {
+        publicInput.assertEquals(Field.zero);
+      },
+    },
+
+    mergeOne: {
+      privateInputs: [SelfProof],
+
+      method(publicInput, earlierProof) {
+        earlierProof.verify();
+        earlierProof.publicInput.add(1).assertEquals(publicInput);
+      },
+    },
+
+    mergeTwo: {
+      privateInputs: [SelfProof, SelfProof],
+
+      method(publicInput, p1, p2) {
+        p1.verify();
+        p1.publicInput.add(1).assertEquals(p2.publicInput);
+        p2.verify();
+        p2.publicInput.add(1).assertEquals(publicInput);
+      },
+    },
+  },
+});
+tic("compiling three programs..");
+await MaxProofsVerifiedZero.compile();
+await MaxProofsVerifiedOne.compile();
+await MaxProofsVerifiedTwo.compile();
 toc();
 
-tic("proving base case");
-let proof = await MyProgram.baseCase(Field.zero);
-toc();
+await testRecursion(MaxProofsVerifiedZero, 0);
+await testRecursion(MaxProofsVerifiedOne, 1);
+await testRecursion(MaxProofsVerifiedTwo, 2);
 
-tic("proving step 1");
-proof = await MyProgram.inductiveCase(Field.one, proof);
-toc();
+async function testRecursion(Program, maxProofsVerified) {
+  console.log(`testing maxProofsVerified = ${maxProofsVerified}`);
 
-tic("proving step 2");
-proof = await MyProgram.inductiveCase(Field(2), proof);
-toc();
+  tic("executing base case..");
+  let initialProof = await Program.baseCase(Field(0));
+  initialProof.verify();
+  initialProof.publicInput.assertEquals(Field(0));
+  toc();
 
-console.log("ok?", proof.publicInput.toString() === "2");
+  if (initialProof.maxProofsVerified != maxProofsVerified) {
+    throw Error(
+      `Expected initialProof to have maxProofsVerified = ${maxProofsVerified} but has ${initialProof.maxProofsVerified}`
+    );
+  }
+
+  let p1, p2;
+  if (initialProof.maxProofsVerified == 0) return;
+
+  tic("executing mergeOne..");
+  p1 = await Program.mergeOne(Field(1), initialProof);
+  p1.verify();
+  p1.publicInput.assertEquals(Field(1));
+  if (p1.maxProofsVerified != maxProofsVerified) {
+    throw Error(
+      `Expected p1 to have maxProofsVerified = ${maxProofsVerified} but has ${p1.maxProofsVerified}`
+    );
+  }
+  toc();
+
+  if (initialProof.maxProofsVerified == 1) return;
+  tic("executing mergeTwo..");
+  p2 = await Program.mergeTwo(Field(2), initialProof, p1);
+  p2.verify();
+  p2.publicInput.assertEquals(Field(2));
+  if (p2.maxProofsVerified != maxProofsVerified) {
+    throw Error(
+      `Expected p2 to have maxProofsVerified = ${maxProofsVerified} but has ${p2.maxProofsVerified}`
+    );
+  }
+  toc();
+}
 
 shutdown();
