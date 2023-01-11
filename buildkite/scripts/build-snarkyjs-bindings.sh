5b000af0f47ff787c09a9bfd1ea44e043088245b
João Santos Reis <joaosreis@outlook.pt>
2023-01-11 13:22:23 +0000

    Install snarkyjs npm dependencies

--- buildkite/scripts/build-snarkyjs-bindings.sh
+++ buildkite/scripts/build-snarkyjs-bindings.sh
@@ -3,5 +3,10 @@
 set -eo pipefail
 source ~/.profile
 
+echo "Install NPM dependencies..."
+cd src/lib/snarky_js_bindings/snarkyjs
+npm install --no-progress
+cd -
+
 echo "Build SnarkyJS..."
 ./scripts/update-snarkyjs-bindings.sh
