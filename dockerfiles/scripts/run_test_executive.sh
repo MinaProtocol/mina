e4a8ce8e0f062720352c063ab032d297dec8abe8
Helena Li <helena.li0x1@gmail.com>
2023-01-10 21:33:44 -0800

    debugging the entrypoint script and env vars

--- dockerfiles/scripts/run_test_executive.sh
+++ dockerfiles/scripts/run_test_executive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcloud auth activate-service-account --key-file=$GOOGLE_CLOUD_KEYFILE_JSON_PATH
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 gcloud config set project o1labs-192920
 gcloud container clusters get-credentials --region us-west1 mina-integration-west1
 kubectl config use-context gke_o1labs-192920_us-west1_mina-integration-west1
