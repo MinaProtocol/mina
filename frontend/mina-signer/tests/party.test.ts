41cf7f17da5aad15fded06c0da9358682a45d681
mrmr1993 <mr_1993@hotmail.co.uk>
2023-01-14 17:42:52 +0000

    And this is why we should stop rewriting the world in JS

--- frontend/mina-signer/tests/party.test.ts
+++ frontend/mina-signer/tests/party.test.ts
@@ -85,7 +85,7 @@ let mockedZkappCommand = {
         },
 
         useFullCommitment: true,
-        caller: "wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf",
+        callType: "call",
         authorizationKind: "Signature",
       },
       authorization: {
