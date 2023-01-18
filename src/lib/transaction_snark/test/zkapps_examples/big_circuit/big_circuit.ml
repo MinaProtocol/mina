14335c4e88af6a0e784f7e323311e6f84899bbe8
David Wong <davidwong.crypto@gmail.com>
2023-01-17 16:29:08 -0800

    fixup! [zkapps] add new test for large circuits

--- src/lib/transaction_snark/test/zkapps_examples/big_circuit/big_circuit.ml
+++ src/lib/transaction_snark/test/zkapps_examples/big_circuit/big_circuit.ml
@@ -8,15 +8,6 @@ module Nat = Pickles_types.Nat
 module Local_state = Mina_state.Local_state
 module Zkapp_command_segment = Transaction_snark.Zkapp_command_segment
 
-(* Hacky way to detect exceptions being raised. *)
-module MustRaise = struct
-  exception WontCatch
-
-  let check (fn : unit -> unit) =
-    let wrapper _ = fn () ; raise WontCatch in
-    try wrapper () with e when Poly.(e <> WontCatch) -> ()
-end
-
 let sk = Private_key.create ()
 
 let pk = Public_key.of_private_key_exn sk
@@ -31,128 +22,18 @@ let num_constraints = 1 lsl 16
 let expected_err =
   "polynomial segment size has to be not smaller than that of the circuit!"
 
-let tag, _cache, _p_module, Pickles.Provers.[ prover ] =
-  Zkapps_examples.compile () ~cache:Cache_dir.cache ~auxiliary_typ:Impl.Typ.unit
-    ~branches:(module Nat.N1)
-    ~max_proofs_verified:(module Nat.N0)
-    ~name:"big_circuit"
-    ~constraint_constants:
-      (Genesis_constants.Constraint_constants.to_snark_keys_header
-         constraint_constants )
-    ~choices:(fun ~self:_ ->
-      [ Zkapps_big_circuit.rule ~num_constraints pk_compressed ] )
-
-let vk : Side_loaded_verification_key.t =
-  Pickles.Side_loaded.Verification_key.of_compiled tag
-
-let account_update, () = Async.Thread_safe.block_on_async_exn prover
-
-let deploy_account_update_body : Account_update.Body.t =
-  { Account_update.Body.dummy with
-    public_key = pk_compressed
-  ; update =
-      { Account_update.Update.dummy with
-        verification_key = Set { data = vk; hash = Zkapp_account.digest_vk vk }
-      }
-  ; preconditions =
-      { Account_update.Preconditions.network =
-          Zkapp_precondition.Protocol_state.accept
-      ; account = Accept
-      }
-  ; caller = Token_id.default
-  ; use_full_commitment = true
-  ; authorization_kind = Signature
-  }
-
-let deploy_account_update : Account_update.t =
-  { body = deploy_account_update_body
-  ; authorization = Signature Signature.dummy
-  }
-
-let account_updates =
-  []
-  |> Zkapp_command.Call_forest.cons_tree account_update
-  |> Zkapp_command.Call_forest.cons deploy_account_update
-
-let memo = Signed_command_memo.empty
-
-let transaction_commitment : Zkapp_command.Transaction_commitment.t =
-  let account_updates_hash = Zkapp_command.Call_forest.hash account_updates in
-  Zkapp_command.Transaction_commitment.create ~account_updates_hash
-
-let fee_payer =
-  { Account_update.Fee_payer.body =
-      { Account_update.Body.Fee_payer.dummy with
-        public_key = pk_compressed
-      ; fee = Currency.Fee.(of_nanomina_int_exn 100)
-      }
-  ; authorization = Signature.dummy
-  }
-
-let full_commitment =
-  Zkapp_command.Transaction_commitment.create_complete transaction_commitment
-    ~memo_hash:(Signed_command_memo.hash memo)
-    ~fee_payer_hash:
-      (Zkapp_command.Digest.Account_update.create
-         (Account_update.of_fee_payer fee_payer) )
-
-let sign_all ({ fee_payer; account_updates; memo } : Zkapp_command.t) :
-    Zkapp_command.t =
-  let fee_payer =
-    match fee_payer with
-    | { body = { public_key; _ }; _ }
-      when Public_key.Compressed.equal public_key pk_compressed ->
-        { fee_payer with
-          authorization =
-            Schnorr.Chunked.sign sk
-              (Random_oracle.Input.Chunked.field full_commitment)
-        }
-    | fee_payer ->
-        fee_payer
-  in
-  let account_updates =
-    Zkapp_command.Call_forest.map account_updates ~f:(function
-      | ({ body = { public_key; use_full_commitment; _ }
-         ; authorization = Signature _
-         } as account_update :
-          Account_update.t )
-        when Public_key.Compressed.equal public_key pk_compressed ->
-          let commitment =
-            if use_full_commitment then full_commitment
-            else transaction_commitment
-          in
-          { account_update with
-            authorization =
-              Control.Signature
-                (Schnorr.Chunked.sign sk
-                   (Random_oracle.Input.Chunked.field commitment) )
-          }
-      | account_update ->
-          account_update )
-  in
-  { fee_payer; account_updates; memo }
-
-let zkapp_command : Zkapp_command.t =
-  sign_all
-    { fee_payer = { body = fee_payer.body; authorization = Signature.dummy }
-    ; account_updates
-    ; memo
-    }
-
-let expected_err =
-  "polynomial segment size has to be not smaller than that of the circuit!"
-
 let () =
   let f _ =
-    Ledger.with_ledger ~depth:ledger_depth ~f:(fun ledger ->
-        let (_ : _) =
-          Ledger.get_or_create_account ledger account_id
-            (Account.create account_id
-               Currency.Balance.(
-                 Option.value_exn
-                   (add_amount zero (Currency.Amount.of_nanomina_int_exn 500))) )
-        in
-        ignore (apply_zkapp_command ledger [ zkapp_command ] : Sparse_ledger.t) )
+    Zkapps_examples.compile () ~cache:Cache_dir.cache
+      ~auxiliary_typ:Impl.Typ.unit
+      ~branches:(module Nat.N1)
+      ~max_proofs_verified:(module Nat.N0)
+      ~name:"big_circuit"
+      ~constraint_constants:
+        (Genesis_constants.Constraint_constants.to_snark_keys_header
+           constraint_constants )
+      ~choices:(fun ~self:_ ->
+        [ Zkapps_big_circuit.rule ~num_constraints pk_compressed ] )
   in
   match f with
   | exception Failure err when String.(err = expected_err) ->
