a74e82d173eea252481153211651dbfac03f822c
Helena Li <helena.li0x1@gmail.com>
2023-01-23 11:46:45 -0800

    remove comments

--- src/lib/integration_test_cloud_engine/mina_automation.ml
+++ src/lib/integration_test_cloud_engine/mina_automation.ml
@@ -301,25 +301,8 @@ module Network_config = struct
               { path =
                   "terraform-" ^ network_config.terraform.testnet_name
                   ^ ".tfstate"
-                  (* bucket = "lucy-tf-state"
-                     ; prefix = "terraform/state" *)
-
-                  (* Backend.GCS.key =
-                         "terraform-" ^ network_config.terraform.testnet_name
-                         ^ ".tfstate"
-                     ; encrypt = true
-                     ; region = aws_region
-                     ; bucket = "o1labs-terraform-state"
-                     ; acl = "bucket-owner-full-control"*)
               }
         }
-      (* ; Block.Provider
-          { Block.Provider.provider = "aws"
-          ; region = aws_region
-          ; zone = None
-          ; project = None
-          ; alias = None
-          } *)
     ; Block.Provider
         { Block.Provider.provider = "google"
         ; region = cluster_region
