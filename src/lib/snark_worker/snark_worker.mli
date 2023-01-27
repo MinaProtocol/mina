6c06e8998924761e7ace3ed6cb6d868fb32ba4e6
mrmr1993 <mr_1993@hotmail.co.uk>
2023-01-27 16:07:26 +0000

    See whether we actually get the failure logs if we don't log the stmt

--- src/lib/snark_worker/snark_worker.mli
+++ src/lib/snark_worker/snark_worker.mli
@@ -5,9 +5,5 @@ module Intf : module type of Intf
 include Intf.S with type ledger_proof := Ledger_proof.t
 
 type Structured_log_events.t +=
-  | Generating_snark_work_failed of
-      { error : Yojson.Safe.t
-      ; work_spec : Work.Spec.t
-      ; prover_public_key : Signature_lib.Public_key.Compressed.t
-      }
+  | Generating_snark_work_failed of { error : Yojson.Safe.t }
   [@@deriving register_event]
