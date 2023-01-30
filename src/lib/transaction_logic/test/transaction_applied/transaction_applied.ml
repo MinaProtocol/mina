b1e81c9a48ebd81a627febbf7876d665fd5ba103
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-01-30 16:28:38 +0000

    Continue with generator

--- src/lib/transaction_logic/test/transaction_applied/transaction_applied.ml
+++ src/lib/transaction_logic/test/transaction_applied/transaction_applied.ml
@@ -6,35 +6,38 @@ let%test_module "transaction_applied" =
   ( module struct
     let stable_type_generator : Stable.V2.t =
       let open Signed_command_payload.Poly.Stable.V1 in
-      let payload =
+      let open Snark_params.Tick0 in
+      let payload : Signed_command_payload.t =
         { common =
-            { Signed_command_payload.Common.Poly.Stable.V2.fee = 1
+            { Signed_command_payload.Common.Poly.Stable.V2.fee =
+                Currency.Fee.one
             ; fee_payer_pk =
-                { Signature_lib.Public_key.Compressed.Poly.x = 1
+                { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
                 ; is_odd = false
                 }
             ; nonce = 1
-            ; valid_until = 1
-            ; memo = "memo"
+            ; valid_until = Mina_transaction_logic.Global_slot.of_int 1
+            ; memo = Signed_command_memo.empty
             }
         ; body =
             Signed_command_payload.Body.Payment
               { source_pk =
-                  { Signature_lib.Public_key.Compressed.Poly.x = 1
+                  { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
                   ; is_odd = false
                   }
               ; receiver_pk =
-                  { Signature_lib.Public_key.Compressed.Poly.x = 1
+                  { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
                   ; is_odd = false
                   }
               ; amount = Currency.Amount.zero
               }
         }
-      in
-      let data_input =
+      and signer : Import.Public_key.t = Field.of_int 1 * Field.of_int 1
+      and signature : Signature.t = 0 in
+      let data_input : Signed_command.Stable.V2.t =
         { Signed_command.Poly.Stable.V1.payload
-        ; Signed_command.Poly.Stable.V1.signer = 0
-        ; Signed_command.Poly.Stable.V1.signature = 0
+        ; Signed_command.Poly.Stable.V1.signer
+        ; Signed_command.Poly.Stable.V1.signature
         }
       in
       let user_command_input :
@@ -50,8 +53,6 @@ let%test_module "transaction_applied" =
         }
       in
       let command_input = Signed_command signed_command_input in
-      (* let zkapp_input = {accounts= []} in
-         let command_input = Zkapp_command zkapp_input in *)
       let varying_input = Command command_input and previous_hash_input = 0 in
       { varying = varying_input; previous_hash = previous_hash_input }
 
