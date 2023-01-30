0451b435b17a6646e9b4e93882d5bc577c518fe5
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-01-30 17:10:01 +0000

    Finished Load Generator

--- src/lib/transaction_logic/test/transaction_applied/transaction_applied.ml
+++ src/lib/transaction_logic/test/transaction_applied/transaction_applied.ml
@@ -4,18 +4,16 @@ open Mina_base
 
 let%test_module "transaction_applied" =
   ( module struct
-    let stable_type_generator : Stable.V2.t =
-      let open Signed_command_payload.Poly.Stable.V1 in
+    let stable_type_generator : t =
       let open Snark_params.Tick0 in
       let payload : Signed_command_payload.t =
         { common =
-            { Signed_command_payload.Common.Poly.Stable.V2.fee =
-                Currency.Fee.one
+            { Signed_command_payload.Common.Poly.fee = Currency.Fee.one
             ; fee_payer_pk =
                 { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
                 ; is_odd = false
                 }
-            ; nonce = 1
+            ; nonce = Unsigned.UInt32.of_int 1
             ; valid_until = Mina_transaction_logic.Global_slot.of_int 1
             ; memo = Signed_command_memo.empty
             }
@@ -32,30 +30,30 @@ let%test_module "transaction_applied" =
               ; amount = Currency.Amount.zero
               }
         }
-      and signer : Import.Public_key.t = Field.of_int 1 * Field.of_int 1
-      and signature : Signature.t = 0 in
+      and signer : Import.Public_key.t = (Field.of_int 1, Field.of_int 1)
+      and signature : Signature.t =
+        (Field.of_int 1, Pasta_bindings.Fq.of_int 1)
+      in
       let data_input : Signed_command.Stable.V2.t =
-        { Signed_command.Poly.Stable.V1.payload
-        ; Signed_command.Poly.Stable.V1.signer
-        ; Signed_command.Poly.Stable.V1.signature
+        { Signed_command.Poly.payload
+        ; Signed_command.Poly.signer
+        ; Signed_command.Poly.signature
         }
       in
-      let user_command_input :
-          Signed_command.Stable.V2.t With_status.Stable.V2.t =
-        { data = data_input; status = Transaction_status.Stable.V2.Applied }
+      let user_command_input : Signed_command.t With_status.t =
+        { data = data_input; status = Transaction_status.Applied }
       in
-      let common_input : Signed_command_applied.t =
+      let common_input : Signed_command_applied.Common.t =
         { user_command = user_command_input }
       in
-      let signed_command_input =
-        { common = common_input
-        ; body = Signed_command_applied.Body.Stable.V2.Failed
-        }
+      let signed_command_input : Signed_command_applied.t =
+        { common = common_input; body = Signed_command_applied.Body.Failed }
       in
-      let command_input = Signed_command signed_command_input in
-      let varying_input = Command command_input and previous_hash_input = 0 in
+      let command_input = Command_applied.Signed_command signed_command_input in
+      let varying_input = Varying.Command command_input
+      and previous_hash_input = Field.of_int 0 in
       { varying = varying_input; previous_hash = previous_hash_input }
 
-    let%test "burned_tokens" = true
+    let%test "supply_increase" = true
     (* Currency.Amount.zero == burned_tokens stable_type_generator *)
   end )
