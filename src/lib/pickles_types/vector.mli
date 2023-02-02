12add91b873e3e3d21d5d182d7a5d87cee3d0d84
David Wong <davidwong.crypto@gmail.com>
2023-02-01 17:08:44 -0800

    [pickles] increase SRS size from 16 to 17

--- src/lib/pickles_types/vector.mli
+++ src/lib/pickles_types/vector.mli
@@ -78,6 +78,8 @@ module Vector_15 : VECTOR with type 'a t = ('a, Nat.N15.n) vec
 (** Vector of size 16 *)
 module Vector_16 : VECTOR with type 'a t = ('a, Nat.N16.n) vec
 
+module Vector_17 : VECTOR with type 'a t = ('a, Nat.N17.n) vec
+
 (** Vector of size 8 *)
 module Vector_8 : VECTOR with type 'a t = ('a, Nat.N8.n) vec
 
