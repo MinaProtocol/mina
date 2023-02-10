2c6b5b10a38d1243fe858c9da951fe1876e05d22
Bruno Deferrari <bruno.deferrari@viablesystems.io>
2023-02-10 09:51:57 -0300

    Measure SNARK RPC-handler times

--- src/app/cli/src/init/mina_run.ml
+++ src/app/cli/src/init/mina_run.ml
@@ -481,27 +481,45 @@ let setup_local_server ?(client_trustlist = []) ?rest_server_port
   in
   let snark_worker_impls =
     [ implement Snark_worker.Rpcs_versioned.Get_work.Latest.rpc (fun () () ->
-          Deferred.return
-            (let open Option.Let_syntax in
+          let received_time = Unix.gettimeofday () in
+          let request_work_start_time = ref (-1.0) in
+          let request_work_end_time = ref (-1.0) in
+          let result =
+            let open Option.Let_syntax in
             let%bind key =
               Option.merge
                 (Mina_lib.snark_worker_key mina)
                 (Mina_lib.snark_coordinator_key mina)
                 ~f:Fn.const
             in
+            request_work_start_time := Unix.gettimeofday () ;
             let%map r = Mina_lib.request_work mina in
+            request_work_end_time := Unix.gettimeofday () ;
             [%log trace]
               ~metadata:[ ("work_spec", Snark_worker.Work.Spec.to_yojson r) ]
               "responding to a Get_work request with some new work" ;
             Mina_metrics.(Counter.inc_one Snark_work.snark_work_assigned_rpc) ;
-            (r, key)) )
+            (r, key)
+          in
+          let respond_time = Unix.gettimeofday () in
+          let times =
+            ( received_time
+            , !request_work_start_time
+            , !request_work_end_time
+            , respond_time )
+          in
+          Deferred.return (result, times) )
     ; implement Snark_worker.Rpcs_versioned.Submit_work.Latest.rpc
         (fun () (work : Snark_worker.Work.Result.t) ->
+          let received_time = Unix.gettimeofday () in
           [%log trace] "received completed work from a snark worker"
             ~metadata:
               [ ("work_spec", Snark_worker.Work.Spec.to_yojson work.spec) ] ;
           log_snark_work_metrics work ;
-          Deferred.return @@ Mina_lib.add_work mina work )
+          let add_work_start_time = Unix.gettimeofday () in
+          Mina_lib.add_work mina work ;
+          let add_work_end_time = Unix.gettimeofday () in
+          Deferred.return (received_time, add_work_start_time, add_work_end_time) )
     ; implement Snark_worker.Rpcs_versioned.Failed_to_generate_snark.Latest.rpc
         (fun
           ()
