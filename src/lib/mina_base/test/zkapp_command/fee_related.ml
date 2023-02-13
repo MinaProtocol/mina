8ee63e31592394d2f03af13e61f0eec1f48fc980
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-02-13 16:51:53 +0000

    Rewrote tests to use existing wire generators

--- src/lib/mina_base/test/zkapp_command/fee_related.ml
+++ src/lib/mina_base/test/zkapp_command/fee_related.ml
@@ -6,57 +6,32 @@ open Snark_params.Tick
 
 let%test_module "fee_related_tests" =
   ( module struct
-    let feepayer_body_generator :
-        Mina_wire_types.Mina_base.Account_update.Body.Fee_payer.V1.t
-        Quickcheck.Generator.t =
-      let open Quickcheck.Generator.Let_syntax in
-      let%bind public_key = Int.quickcheck_generator in
-      let%bind is_odd = Bool.quickcheck_generator in
-      let%bind currency = Currency.Fee.gen in
-      let%map nonce = Int.quickcheck_generator in
-      { Mina_wire_types.Mina_base.Account_update.Body.Fee_payer.V1.public_key =
-          { x = Field.of_int public_key; is_odd }
-      ; fee = currency
-      ; valid_until = Some (Mina_numbers.Global_slot.random ())
-      ; nonce = Unsigned.UInt32.of_int nonce
-      }
+    let feepayer_body_generator = Zkapp_command.T.Stable.Latest.Wire.gen
 
-    let body_maker input : T.t =
-      let fee_payer : Account_update.Fee_payer.Stable.V1.t =
-        { body = input; authorization = Signature.dummy }
-      and account_updates = []
-      and memo = Signed_command_memo.empty in
-      { fee_payer; account_updates; memo }
-
-    (* Tests for fee *)
     let%test_unit "fee" =
-      Quickcheck.test feepayer_body_generator ~f:(fun x ->
-          [%test_eq: Currency.Fee.t] x.fee (fee @@ body_maker x) )
+      Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
+          [%test_eq: Currency.Fee.t] x.fee_payer.body.fee
+            (fee @@ Zkapp_command.T.Stable.Latest.of_wire x) )
 
-    (* Tests for fee_payer_account_update *)
     let%test_unit "fee_payer_account_update" =
-      Quickcheck.test feepayer_body_generator ~f:(fun x ->
-          [%test_eq: Account_update.Fee_payer.Stable.V1.t]
-            (body_maker x).fee_payer
-            (fee_payer_account_update @@ body_maker x) )
+      Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
+          [%test_eq: Account_update.Fee_payer.Stable.V1.t] x.fee_payer
+            (fee_payer_account_update @@ Zkapp_command.T.Stable.Latest.of_wire x) )
 
-    (* Tests for fee_payer_pk *)
     let%test_unit "fee_payer_pk" =
-      Quickcheck.test feepayer_body_generator ~f:(fun x ->
+      Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
           [%test_eq: Signature_lib.Public_key.Compressed.Stable.V1.t]
-            x.public_key
-            (fee_payer_pk @@ body_maker x) )
+            x.fee_payer.body.public_key
+            (fee_payer_pk @@ Zkapp_command.T.Stable.Latest.of_wire x) )
 
-    (* Tests for fee_excess *)
     let%test_unit "fee_excess" =
-      Quickcheck.test feepayer_body_generator ~f:(fun x ->
+      Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
           [%test_eq: (Token_id.t, Currency.Fee.Signed.t) Fee_excess.Poly.t]
             { fee_token_l = Token_id.default
             ; fee_excess_l =
-                Currency.Fee.Signed.of_unsigned
-                @@ (body_maker x).fee_payer.body.fee
+                Currency.Fee.Signed.of_unsigned @@ x.fee_payer.body.fee
             ; fee_token_r = Token_id.default
             ; fee_excess_r = Currency.Fee.Signed.zero
             }
-            (fee_excess @@ body_maker x) )
+            (fee_excess @@ Zkapp_command.T.Stable.Latest.of_wire x) )
   end )
