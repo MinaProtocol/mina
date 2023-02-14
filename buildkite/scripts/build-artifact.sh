026b969b367c412a1be3bbaa76bd1ed1a7a07d13
Andrew Trainor <andrew@o1labs.org>
2023-02-14 18:52:47 -0500

    merge-release-develop Remove mainnet and devnet builds from dhall, build-artifact, and rebuild-deb. Depend on berkeley images everywhere.

--- buildkite/scripts/build-artifact.sh
+++ buildkite/scripts/build-artifact.sh
@@ -15,6 +15,9 @@ MINA_COMMIT_SHA1=$(git rev-parse HEAD)
 echo "--- Build libp2p_helper TODO: use the previously uploaded build artifact"
 make -C src/app/libp2p_helper
 
+MAINNET_TARGETS=""
+[[ ${MINA_BUILD_MAINNET} ]] && MAINNET_TARGETS="src/app/cli/src/mina_mainnet_signatures.exe src/app/rosetta/rosetta_mainnet_signatures.exe "
+
 echo "--- Build all major tagets required for packaging"
 echo "Building from Commit SHA: ${MINA_COMMIT_SHA1}"
 echo "Rust Version: $(rustc --version)"
@@ -24,22 +27,19 @@ dune build "--profile=${DUNE_PROFILE}" \
   src/app/generate_keypair/generate_keypair.exe \
   src/app/validate_keypair/validate_keypair.exe \
   src/app/cli/src/mina_testnet_signatures.exe \
-  src/app/cli/src/mina_mainnet_signatures.exe \
   src/app/archive/archive.exe \
-  src/app/archive/archive_testnet_signatures.exe \
-  src/app/archive/archive_mainnet_signatures.exe \
-  src/app/extract_blocks/extract_blocks.exe \
-  src/app/missing_blocks_auditor/missing_blocks_auditor.exe \
-  src/app/archive_blocks/archive_blocks.exe \
   src/app/replayer/replayer.exe \
+  src/app/extract_blocks/extract_blocks.exe \
+  src/app/archive_blocks/archive_blocks.exe \
+  src/app/missing_blocks_auditor/missing_blocks_auditor.exe \
   src/app/swap_bad_balances/swap_bad_balances.exe \
   src/app/zkapp_test_transaction/zkapp_test_transaction.exe \
   src/app/rosetta/rosetta_mainnet_signatures.exe \
-  src/app/rosetta/rosetta_testnet_signatures.exe \
   src/app/test_executive/test_executive.exe # 2>&1 | tee /tmp/buildocaml.log
 
 echo "--- Bundle all packages for Debian ${MINA_DEB_CODENAME}"
-echo " Includes mina daemon, archive-node, rosetta, generate keypair for mainnet and devnet"
+echo " Includes mina daemon, archive-node, rosetta, generate keypair for berkeley"
+[[ ${MINA_BUILD_MAINNET} ]] && echo " MINA_BUILD_MAINNET is true so this includes the mainnet and devnet packages for mina-daemon as well"
 make deb
 
 make test_executive_deb
