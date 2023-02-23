1b2459347cb5c8bd5572fdd942ef4be6ea379968
Marcin Pastudzki <marcin.pastudzki@gmail.com>
2023-02-23 15:38:45 +0100

    [transaction_logic] Test that fee payer must pay the fee.

--- src/lib/transaction_logic/test/transaction_logic.ml
+++ src/lib/transaction_logic/test/transaction_logic.ml
@@ -21,7 +21,7 @@ let constraint_constants =
 type zk_cmd_result =
   Transaction_logic.Transaction_applied.Zkapp_command_applied.t
   * Amount.Signed.t
-      [@@deriving sexp]
+[@@deriving sexp]
 
 let balance_to_fee = Fn.compose Amount.to_fee Balance.to_amount
 
@@ -48,18 +48,22 @@ let%test_module "Test transaction logic." =
     let%test_unit "Many transactions between distinct accounts." =
       Quickcheck.test
         (let open Quickcheck in
-         let open Quickcheck.Generator.Let_syntax in
-         let%bind accs_and_txns =
-           Generator.list_non_empty gen_account_pair_and_txn
-           (* Generating too many transactions makes this test take too much time. *)
-           |> Generator.filter ~f:(fun l -> List.length l < 4)
-         in
-         let (account_pairs, txns) = List.unzip accs_and_txns in
-         let accounts = List.concat_map account_pairs ~f:(fun (a, b) -> [a; b]) in
-         (* Select a receiver to pay the fee. *)
-         let%bind fee_payer = Generator.of_list @@ List.map ~f:snd account_pairs in
-         let%map fee = Fee.(gen_incl zero @@ balance_to_fee fee_payer.balance) in
-         (fee_payer.pk, fee, accounts, txns))
+        let open Quickcheck.Generator.Let_syntax in
+        let%bind accs_and_txns =
+          Generator.list_non_empty gen_account_pair_and_txn
+          (* Generating too many transactions makes this test take too much time. *)
+          |> Generator.filter ~f:(fun l -> List.length l < 4)
+        in
+        let account_pairs, txns = List.unzip accs_and_txns in
+        let accounts =
+          List.concat_map account_pairs ~f:(fun (a, b) -> [ a; b ])
+        in
+        (* Select a receiver to pay the fee. *)
+        let%bind fee_payer =
+          Generator.of_list @@ List.map ~f:snd account_pairs
+        in
+        let%map fee = Fee.(gen_incl zero @@ balance_to_fee fee_payer.balance) in
+        (fee_payer.pk, fee, accounts, txns))
         ~f:(fun (fee_payer, fee, accounts, txns) ->
           [%test_pred: zk_cmd_result Or_error.t]
             (function
@@ -68,5 +72,39 @@ let%test_module "Test transaction logic." =
               | Error _ ->
                   false )
             (run_zkapp_cmd ~fee_payer ~fee ~accounts txns) )
-    end )
 
+    let%test_unit "Fee payer must be able to pay the fee." =
+      Quickcheck.test
+        (let open Quickcheck in
+        let open Generator.Let_syntax in
+        let%bind (sender, receiver), txn =
+          gen_account_pair_and_txn
+          |> Generator.filter
+               ~f:Balance.(fun ((a, _), _) -> a.balance < max_int)
+        in
+        let accounts = [ sender; receiver ] in
+        (* Make sure that fee is too high. *)
+        let min_fee =
+          Balance.(sender.balance - txn.amount)
+          |> Option.value_map ~default:Fee.zero ~f:balance_to_fee
+          |> Fee.(fun f -> f + of_nanomina_int_exn 1)
+          |> Option.value ~default:Fee.max_int
+        in
+        let max_fee =
+          Fee.(min_fee + of_mina_int_exn 10)
+          |> Option.value ~default:Fee.max_int
+        in
+        let%map fee = Fee.(gen_incl min_fee max_fee) in
+        (sender.pk, fee, accounts, [ txn ]))
+        ~f:(fun (fee_payer, fee, accounts, txns) ->
+          [%test_pred: zk_cmd_result Or_error.t]
+            (function
+              | Ok (txn, _) ->
+                  Transaction_status.(
+                    equal txn.command.status
+                      (Failed [ []; [ Overflow ]; [ Cancelled ] ]))
+              | Error e ->
+                  String.is_substring (Error.to_string_hum e)
+                    ~substring:"Overflow" )
+            (run_zkapp_cmd ~fee_payer ~fee ~accounts txns) )
+  end )
