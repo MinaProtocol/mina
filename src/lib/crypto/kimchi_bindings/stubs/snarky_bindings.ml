d447e6a382d2b485f0088dce7767b34fdaafdf23
David Wong <davidwong.crypto@gmail.com>
2023-02-28 12:09:42 +0100

    [crypto][bindings] generate the new snarky bindings by modifying the dune rules

---
+++ src/lib/crypto/kimchi_bindings/stubs/snarky_bindings.ml
@@ -0,0 +1,29 @@
+(* This file is generated automatically with ocaml_gen. *)
+
+module Fp = struct
+  module Cvar = struct
+    type nonrec t
+  end
+
+  module ConstraintSystem = struct
+    type nonrec t
+  end
+
+  module State = struct
+    type nonrec t
+  end
+end
+
+module Fq = struct
+  module Cvar = struct
+    type nonrec t
+  end
+
+  module ConstraintSystem = struct
+    type nonrec t
+  end
+
+  module State = struct
+    type nonrec t
+  end
+end
