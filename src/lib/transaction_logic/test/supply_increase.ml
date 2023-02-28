933d60be0e0cdc5b34476f1d074b31d4f594626f
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-02-28 11:24:47 +0000

    Remove comments

--- src/lib/transaction_logic/test/supply_increase.ml
+++ src/lib/transaction_logic/test/supply_increase.ml
@@ -47,59 +47,6 @@ let%test_module "supply_increase" =
       and previous_hash_input = return @@ Field.of_int prev_hash_input_gen in
       { varying = varying_input; previous_hash = previous_hash_input }
 
-    (* let stable_type_generator =
-       let open Snark_params.Tick in
-       let payload : Signed_command_payload.t =
-         { common =
-             { Signed_command_payload.Common.Poly.fee = Currency.Fee.one
-             ; fee_payer_pk =
-                 { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
-                 ; is_odd = false
-                 }
-             ; nonce = Unsigned.UInt32.of_int 1
-             ; valid_until = Mina_transaction_logic.Global_slot.of_int 1
-             ; memo = Signed_command_memo.empty
-             }
-         ; body =
-             Signed_command_payload.Body.Payment
-               { source_pk =
-                   { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
-                   ; is_odd = false
-                   }
-               ; receiver_pk =
-                   { Signature_lib.Public_key.Compressed.Poly.x = Field.of_int 1
-                   ; is_odd = false
-                   }
-               ; amount = Currency.Amount.of_mina_int_exn 100
-               }
-         }
-       and signer : Mina_base_import.Public_key.t =
-         (Field.of_int 1, Field.of_int 1)
-       and signature : Signature.t =
-         (Field.of_int 1, Pasta_bindings.Fq.of_int 1)
-       in
-       let data_input : Signed_command.Stable.V2.t =
-         { Signed_command.Poly.payload
-         ; Signed_command.Poly.signer
-         ; Signed_command.Poly.signature
-         }
-       in
-       let user_command_input : Signed_command.t With_status.t =
-         { data = data_input; status = Transaction_status.Applied }
-       in
-       let common_input : Signed_command_applied.Common.t =
-         { user_command = user_command_input }
-       in
-       let signed_command_input : Signed_command_applied.t =
-         { common = common_input
-         ; body = Signed_command_applied.Body.Payment { new_accounts = [] }
-         }
-       in
-       let command_input = Command_applied.Signed_command signed_command_input in
-       let varying_input = Varying.Command command_input
-       and previous_hash_input = Field.of_int 1 in
-       { varying = varying_input; previous_hash = previous_hash_input } *)
-
     type signed_amount = Currency.Amount.Signed.t
     [@@deriving equal, sexp, compare]
 
