6c770ae12a41b00143ef5c6358d5a11ddd35c603
querolita <anais.querol@gmail.com>
2023-02-28 17:30:20 +0100

    first attempt of mul

---
+++ src/lib/crypto/kimchi_backend/gadgets/generic.ml
@@ -0,0 +1,31 @@
+open Core_kernel
+
+open Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint
+
+let mul (type f)
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (left_input : Circuit.Field.t) (right_input : Circuit.Field.t) :
+    Circuit.Field.t =
+  (* Witness computation *)
+  let prod =
+    exists Field.typ ~compute:(fun () ->
+        let left_input = As_prover.read Field.typ left_input in
+        let right_input = As_prover.read Field.typ right_input in
+        Field.Constant.mul left_input right_input )
+  in
+
+  (* Set up generic gate *)
+  with_label "generic_mul" (fun () ->
+      assert_
+        { annotation = Some __LOC__
+        ; basic =
+            Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
+              (Basic
+                 { l = (Field.Constant.zero, left_input)
+                 ; r = (Field.Constant.zero, right_input)
+                 ; o = (negate Field.Constant.one, prod)
+                 ; m = Field.Constant.one
+                 ; c = Field.Constant.zero
+                 } )
+        } ;
+      prod )
