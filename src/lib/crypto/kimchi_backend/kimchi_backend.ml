e18b4cde13120e25d4b11961c98ba427b0813523
David Wong <davidwong.crypto@gmail.com>
2023-03-01 17:53:52 +0100

    [crypto][kimchi_backend] instantiate snarky in the backend to fail early

--- src/lib/crypto/kimchi_backend/kimchi_backend.ml
+++ src/lib/crypto/kimchi_backend/kimchi_backend.ml
@@ -11,3 +11,22 @@ module Pasta = struct
   module Pasta = Kimchi_pasta.Pasta
   module Vesta_based_plonk = Kimchi_pasta.Vesta_based_plonk
 end
+
+(* The following is mostly unused, but we do this to test that we can instantiate snarky with the backend here (instead of failing later) *)
+
+module Tests = struct
+  module Tick = struct
+    include Pasta.Vesta_based_plonk
+    module Inner_curve = Pasta.Pasta.Pallas
+  end
+
+  module Tock = struct
+    include Pasta.Pallas_based_plonk
+    module Inner_curve = Pasta.Pasta.Vesta
+  end
+
+  module Step_impl_monad = Snarky_backendless.Snark.Make (Tick)
+  module Wrap_impl_monad = Snarky_backendless.Snark.Make (Tock)
+  module Step_impl = Snarky_backendless.Snark.Run.Make (Tick)
+  module Wrap_impl = Snarky_backendless.Snark.Run.Make (Tock)
+end
