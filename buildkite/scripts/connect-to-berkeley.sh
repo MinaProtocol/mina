c2efa874b5eed4fb128e35b710a923593067f606
Andrew Trainor <andrew@o1labs.org>
2023-02-28 19:03:54 -0500

    lk86/cloudbuild-develop Fix bash loop

--- buildkite/scripts/connect-to-berkeley.sh
+++ buildkite/scripts/connect-to-berkeley.sh
@@ -26,18 +26,18 @@ mina daemon \
 # Attempt to connect to the GraphQL client every 10s for up to 4 minutes
 status_retry_iterator=0
 num_status_retries=24
-while [ $status_retry_iterator -lt $num_status_retries ] ; do
+while [ $status_retry_iterator -le $num_status_retries ] ; do
   sleep 10s
   set +e
   mina client status
   status_exit_code=$?
   set -e
+  true $((status_retry_iterator=status_retry_iterator+1))
   if [ $status_exit_code -eq 0 ]; then
     break
-  elif [ $i -eq $num_status_retries ]; then
+  elif [ $status_retry_iterator -eq $num_status_retries ]; then
     exit $status_exit_code
   fi
-  true $((i=i+1))
 done
 
 # Check that the daemon has connected to peers and is still up after 2 mins
