f8c2edcff81eb17b84fa0857010951f055a61a6c
Greg Trost <gltrost2@gmail.com>
2023-03-08 16:46:59 -0500

    Add tests for Stack

--- src/lib/transaction_logic/mina_transaction_logic.ml
+++ src/lib/transaction_logic/mina_transaction_logic.ml
@@ -546,7 +546,17 @@ module type S = sig
     -> ledger
     -> bool Or_error.t
 
-  module For_tests : sig
+    module For_tests : sig
+      module Stack (Elt : sig type t end) : sig
+        type t = Elt.t list
+        val if_ : bool -> then_:'a -> else_:'a -> 'a
+        val empty : unit -> 'a list
+        val is_empty : 'a list -> bool
+        val pop_exn : t -> Elt.t * t
+        val pop : t -> (Elt.t * t) option
+        val push : Elt.t -> onto:Elt.t list -> t
+      end
+
     val validate_timing_with_min_balance :
          account:Account.t
       -> txn_amount:Amount.t
@@ -2548,6 +2558,9 @@ module Make (L : Ledger_intf.S) :
     >>= Mina_stdlib.Result.List.map ~f:(apply_transaction_second_pass ledger)
 
   module For_tests = struct
+
+    module Stack = Inputs.Stack
+
     let validate_timing_with_min_balance = validate_timing_with_min_balance
 
     let validate_timing = validate_timing
