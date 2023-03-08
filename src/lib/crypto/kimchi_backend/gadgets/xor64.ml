621dd84ab8b2fbf08e893cb148b5f7ea85f528ad
querolita <anais.querol@gmail.com>
2023-03-08 18:00:26 +0100

    update tests

--- src/lib/crypto/kimchi_backend/gadgets/xor64.ml
+++ src/lib/crypto/kimchi_backend/gadgets/xor64.ml
@@ -4,6 +4,12 @@ open Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint
 
 (* XOR *)
 
+let pad_upto ~length ~value list = 
+  let len = List.length list in 
+  assert (len <= length);
+  let padding = List.init (length - len) ~f:(fun _ -> value)
+  list @ padding
+
 (* Xor of length bits *)
 let xor (type f) 
 (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
@@ -19,12 +25,8 @@ let open Circuit in
    assert (List.length input2_bits <= length) ;
  
    (* Pad with zeros in MSB until reaching same length *)
-  for _ = List.length input1_bits to length do
-    input1_bits <- input1_bits :: Circuit.Boolean.false_
-  done
-  for _ = List.length input2_bits to length do
-    input2_bits <- input2_bits :: Circuit.Boolean.false_
-  done
+  let input1_bits = pad_upto length Circuit.Boolean.false_ input1_bits in
+  let input2_bits = pad_upto length Circuit.Boolean.false_ input2_bits in
 
   (* Pad with more zeros until the length is a multiple of 16 *)
   let pad_length = length in
@@ -129,16 +131,14 @@ let rec xor_rec (type f)
                    } )
           })
           (* Remove least significant 4 nibbles *)
-          let next_in1 = (* i would like to remove the 16 leading bits of the list *) in 
-          let next_in2 = (in2 - in2_0  - in2_1 * 2^4 - in2_2 * 2^8 - in2_3 * 2^12) / 2^16 in
-          let next_out = (out - out_0  - out_1 * 2^4 - out_2 * 2^8 - out_3 * 2^12) / 2^16 in
+          let next_in1 = List.drop input1 16 in 
+          let next_in2 = List.drop input2 16 in
+          let next_out = List.drop output 16 in
           (* Next length is 4*4 less bits *)
           let next_length = length - 16 in
 
         (* Recursively call xor on the next nibble *)
         xor_rec (module Circuit) next_input1 next_input2 next_output next_length
-
-
 
 
 let%test_unit "xor16 gadget" =
@@ -151,7 +151,7 @@ let%test_unit "xor16 gadget" =
    *   Inputs operands and expected output: left_input xor right_input
    *   Returns true if constraints are satisfied, false otherwise.
    *)
-  let test_xor16 left_input right_input output =
+  let test_xor left_input right_input output length =
     try
       let _proof_keypair, _proof =
         Runner.generate_and_verify_proof (fun () ->
@@ -169,7 +169,7 @@ let%test_unit "xor16 gadget" =
               exists Field.typ ~compute:(fun () -> Field.Constant.of_int output)
             in
             (* Use the xor16 gate gadget *)
-            let result = xor16 (module Runner.Impl) left_input right_input in
+            let result = xor (module Runner.Impl) left_input right_input length in
             Field.Assert.equal output result ;
             (* Pad with a "dummy" constraint b/c Kimchi requires at least 2 *)
             Boolean.Assert.is_true (Field.equal Field.Constant.zero Field.Constant.zero) )
@@ -178,10 +178,24 @@ let%test_unit "xor16 gadget" =
     with _ -> false
   in
 
+  let%test_unit "xor gadget" =
+  (* Import the gadget test runner *)
+  let open Kimchi_gadgets_test_runner in
+  (* Initialize the SRS cache. *)
+  let () = Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] in
+
   (* Positive tests *)
-  assert (Bool.equal (test_xor16 0 0 0) true) ;
-  assert (Bool.equal (test_xor16 43210 56789 29983) true) ;
+  let zero = Field.zero in
+  let one = Field.one in
+  assert (Bool.equal (test_xor zero zero zero 16) true) ;
+  assert (Bool.equal (test_xor zero one one 8) true) ;
+  assert (Bool.equal (test_xor zero zero zero 1) true) ;
+  assert (Bool.equal (test_xor zero zero zero 4) true) ;
+  assert (Bool.equal (test_xor Field.of_int 43210 Field.of_int 56789 Field.of_int 29983 16) true) ;
+  assert (Bool.equal (test_xor Field.of_int 767430 Field.of_int 974317 Field.of_int 354347  20) true) ;
+  (* 0x5A5A5A5A5A5A5A5A xor 0xA5A5A5A5A5A5A5A5 = 0xFFFFFFFFFFFFFF*)
+  assert (Bool.equal (test_xor Common.field_from_base10 "0750096245555160156" Common.field_from_base10 "54015628281582163911" Common.field_from_base10 "51615590737044764481"  64) true) ;
   (* Negatve tests *)
-  assert (Bool.equal (test_xor16 1 0 0) false) ;
-  assert (Bool.equal (test_xor16 1111 2222 0) false) ;
+  assert (Bool.equal (test_xor 1 0 0 1) false) ;
+  assert (Bool.equal (test_xor 1111 2222 0 16) false) ;
   ()
