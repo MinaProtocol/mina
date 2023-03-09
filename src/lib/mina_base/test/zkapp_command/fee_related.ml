3a51a9eabe352cc88477a71703ed2acddfb0c1b9
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-03-09 08:13:41 +0000

    Code Review Changes

--- src/lib/mina_base/test/zkapp_command/fee_related.ml
+++ src/lib/mina_base/test/zkapp_command/fee_related.ml
@@ -11,27 +11,27 @@ let%test_module "fee_related_tests" =
     let%test_unit "fee" =
       Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
           [%test_eq: Currency.Fee.t] x.fee_payer.body.fee
-            (fee @@ Zkapp_command.T.Stable.Latest.of_wire x) )
+            (fee @@ Zkapp_command.of_wire x) )
 
     let%test_unit "fee_payer_account_update" =
       Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
-          [%test_eq: Account_update.Fee_payer.Stable.V1.t] x.fee_payer
-            (fee_payer_account_update @@ Zkapp_command.T.Stable.Latest.of_wire x) )
+          [%test_eq: Account_update.Fee_payer.t] x.fee_payer
+            (fee_payer_account_update @@ Zkapp_command.of_wire x) )
 
     let%test_unit "fee_payer_pk" =
       Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
-          [%test_eq: Signature_lib.Public_key.Compressed.Stable.V1.t]
+          [%test_eq: Signature_lib.Public_key.Compressed.t]
             x.fee_payer.body.public_key
-            (fee_payer_pk @@ Zkapp_command.T.Stable.Latest.of_wire x) )
+            (fee_payer_pk @@ Zkapp_command.of_wire x) )
 
     let%test_unit "fee_excess" =
       Quickcheck.test ~trials:50 feepayer_body_generator ~f:(fun x ->
-          [%test_eq: (Token_id.t, Currency.Fee.Signed.t) Fee_excess.Poly.t]
+          [%test_eq: Fee_excess.t]
             { fee_token_l = Token_id.default
             ; fee_excess_l =
                 Currency.Fee.Signed.of_unsigned @@ x.fee_payer.body.fee
             ; fee_token_r = Token_id.default
             ; fee_excess_r = Currency.Fee.Signed.zero
             }
-            (fee_excess @@ Zkapp_command.T.Stable.Latest.of_wire x) )
+            (fee_excess @@ Zkapp_command.of_wire x) )
   end )
