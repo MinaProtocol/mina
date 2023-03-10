f671bd769eda6ce3063b0d30013a9cccce99eaba
Richard Bonichon <richard.bonichon@gmail.com>
2023-03-10 11:24:26 +0100

    Tests for scalar challenge

--- src/lib/pickles/test/main.ml
+++ src/lib/pickles/test/main.ml
@@ -1,1 +1,3 @@
-let () = Alcotest.run "Pickles" (Test_wrap_hack.tests @ Test_opt_sponge.tests)
+let () =
+  Alcotest.run "Pickles"
+    (Test_wrap_hack.tests @ Test_opt_sponge.tests @ Test_scalar_challenge.tests)
