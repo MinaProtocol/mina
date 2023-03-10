b4c94648ea5a305c89c084955051babf4dc648c5
Richard Bonichon <richard.bonichon@gmail.com>
2023-03-10 16:43:49 +0100

    Move tests from Step_main_inputs to test directory

--- src/lib/pickles/test/main.ml
+++ src/lib/pickles/test/main.ml
@@ -2,4 +2,4 @@ let () =
   Alcotest.run "Pickles"
     ( Test_wrap_hack.tests @ Test_opt_sponge.tests @ Test_scalar_challenge.tests
     @ Test_side_loaded_verification_key.tests @ Test_impls.tests
-    @ Test_sponge.tests )
+    @ Test_sponge.tests @ Test_step.tests )
