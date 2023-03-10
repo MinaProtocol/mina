30155ceae7925a6a3b63c5e152b9c7e59e4b6fc8
Richard Bonichon <richard.bonichon@gmail.com>
2023-03-10 11:29:08 +0100

    Remove unit tests for scalar challenge

--- src/lib/pickles/step_verifier.ml
+++ src/lib/pickles/step_verifier.ml
@@ -828,9 +828,6 @@ struct
     Shifted_value.Type2.Shift.(
       map ~f:Field.constant (create (module Field.Constant)))
 
-  let%test_unit "endo scalar" =
-    SC.test (module Impl) ~endo:Endo.Wrap_inner_curve.scalar
-
   module Plonk = Types.Wrap.Proof_state.Deferred_values.Plonk
 
   module Plonk_checks = struct
