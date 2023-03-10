8c2c599af67bd917c59638a830fde14de1bf177b
Richard Bonichon <richard.bonichon@gmail.com>
2023-03-10 15:48:25 +0100

    Tests for side_loaded_verification_key

---
+++ src/lib/pickles/test/test_side_loaded_verification_key.ml
@@ -0,0 +1,37 @@
+module SLV_key = Pickles__Side_loaded_verification_key
+open Pickles_types
+
+let input_size ~of_int ~add ~mul w =
+  (* This should be an affine function in [a]. *)
+  let size proofs_verified =
+    let (T (Typ typ, _conv, _conv_inv)) =
+      Impls.Step.input ~proofs_verified ~wrap_rounds:Backend.Tock.Rounds.n
+        ~feature_flags:Plonk_types.Features.none
+    in
+    typ.size_in_field_elements
+  in
+  let f0 = size Nat.N0.n in
+  let slope = size Nat.N1.n - f0 in
+  add (of_int f0) (mul (of_int slope) w)
+
+let test_input_size () =
+  List.iter
+    (List.range 0
+       (Nat.to_int SLV_key.Width.Max.n)
+       ~stop:`inclusive ~start:`inclusive )
+    ~f:(fun n ->
+      [%test_eq: int]
+        (input_size ~of_int:Fn.id ~add:( + ) ~mul:( * ) n)
+        (let (T a) = Pickles_types.Nat.of_int n in
+         let (T (Typ typ, _conv, _conv_inv)) =
+           Impls.Step.input ~proofs_verified:a
+             ~wrap_rounds:Backend.Tock.Rounds.n
+             ~feature_flags:Plonk_types.Features.none
+         in
+         typ.size_in_field_elements ) )
+
+let tests =
+  let open Alcotest in
+  [ ( "Side_loaded_verification_key"
+    , [ test_case "test_input_size" `Quick test_input_size ] )
+  ]
