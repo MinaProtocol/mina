48f514cb46160afa1df11a7a82cee43a51b157d0
Richard Bonichon <richard.bonichon@gmail.com>
2023-03-13 16:32:41 +0100

    Start migration test from Pickles to test directory

--- src/lib/pickles/test/main.ml
+++ src/lib/pickles/test/main.ml
@@ -3,4 +3,4 @@ let () =
     ( Test_wrap_hack.tests @ Test_opt_sponge.tests @ Test_scalar_challenge.tests
     @ Test_side_loaded_verification_key.tests @ Test_impls.tests
     @ Test_sponge.tests @ Test_step.tests @ Test_plonk_curve_ops.tests
-    @ Test_wrap.tests @ Test_step_verifier.tests )
+    @ Test_wrap.tests @ Test_step_verifier.tests @ Test_pickles.tests )
