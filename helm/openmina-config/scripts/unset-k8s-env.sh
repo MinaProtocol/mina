89d6bab116be7d951b395ab84d087edc2d44f8d3
Alexander Koptelov <alexandre.koptelov@gmail.com>
2023-03-14 18:48:30 +0300

    Openmina testnet files

---
+++ helm/openmina-config/scripts/unset-k8s-env.sh
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env sh
+
+for VAR in $(printenv | sed -e 's/=.*//' | grep -E -e "(SEED|PROD|SNARK|NODE|FRONTEND).*_(SERVICE|PORT).*"); do
+    eval "unset $VAR"
+done
+
+echo mina "$@"
+exec mina "$@"
