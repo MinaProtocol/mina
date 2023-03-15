bd9c0119508f0e9a2876156e74118d8bc34ebd03
Joseph Spadavecchia <joseph@redtrie.com>
2023-03-15 08:52:30 +0000

    Solidify range-check gadget interface with an mli file

---
+++ src/lib/crypto/kimchi_backend/gadgets/range_check.mli
@@ -0,0 +1,28 @@
+(* range-check gadget - checks v0 \in [0, 2^88) *)
+val range_check :
+     (module Snarky_backendless.Snark_intf.Run with type field = 'f)
+  -> 'f Snarky_backendless.Cvar.t
+  -> unit
+
+(* 64-bit range-check gadget - checks v0 \in [0, 2^64) *)
+val range_check64 :
+     (module Snarky_backendless.Snark_intf.Run with type field = 'f)
+  -> 'f Snarky_backendless.Cvar.t
+  -> unit
+
+(* multi-range-check gadget - checks v0,v1,v2 \in [0, 2^88) *)
+val multi_range_check :
+     (module Snarky_backendless.Snark_intf.Run with type field = 'f)
+  -> 'f Snarky_backendless.Cvar.t
+  -> 'f Snarky_backendless.Cvar.t
+  -> 'f Snarky_backendless.Cvar.t
+  -> unit
+
+(* compact multi-range-check gadget - checks
+ *     - v0,v1,v2 \in [0, 2^88)
+ *     - v01 = v0 + 2^88 * v1 *)
+val compact_multi_range_check :
+     (module Snarky_backendless.Snark_intf.Run with type field = 'f)
+  -> 'f Snarky_backendless.Cvar.t
+  -> 'f Snarky_backendless.Cvar.t
+  -> unit
