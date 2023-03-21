5f154d2c4a2958e096b6fcf9ca6e46f8f08d1014
Joseph Spadavecchia <joseph@redtrie.com>
2023-03-21 22:45:16 +0000

    Work on foreign field multiplication snarky gaget
        * Wire it up
        * Create test helper function
        * Add a few tests

--- src/lib/crypto/kimchi_backend/gadgets/foreign_field.ml
+++ src/lib/crypto/kimchi_backend/gadgets/foreign_field.ml
@@ -75,6 +75,33 @@ let bignum_bigint_to_field_compact_limbs (type f)
   ( Common.bignum_bigint_to_field (module Circuit) l01
   , Common.bignum_bigint_to_field (module Circuit) l2 )
 
+(* Convert field standard_limbs to Bignum_bigint.t standard_limbs *)
+let field_standard_limbs_to_bignum_bigint_standard_limbs (type f)
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (field_limbs : f standard_limbs) : Bignum_bigint.t standard_limbs =
+  let l0, l1, l2 = field_limbs in
+  ( Common.field_to_bignum_bigint (module Circuit) l0
+  , Common.field_to_bignum_bigint (module Circuit) l1
+  , Common.field_to_bignum_bigint (module Circuit) l2 )
+
+(* Convert field standard_limbs to Bignum_bigint.t *)
+let field_standard_limbs_to_bignum_bigint (type f)
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (field_limbs : f standard_limbs) : Bignum_bigint.t =
+  let l0, l1, l2 =
+    field_standard_limbs_to_bignum_bigint_standard_limbs
+      (module Circuit)
+      field_limbs
+  in
+  Bignum_bigint.(l0 + (two_to_limb * l1) + (two_to_2limb * l2))
+
+(* Convert string standard_limbs to field standard_limbs *)
+let string_to_field_standard_limbs (type f)
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (value : string) : f standard_limbs =
+  let bigint = Bignum_bigint.of_string value in
+  bignum_bigint_to_field_standard_limbs (module Circuit) bigint
+
 (* Foreign field element base type - not used directly *)
 module type Foreign_field_element_base = sig
   type 'field t
@@ -83,6 +110,18 @@ module type Foreign_field_element_base = sig
 
   (* Create foreign field element from Cvar limbs *)
   val of_limbs : 'field Cvar.t limbs -> 'field t
+
+  (* Create foreign field element from Bignum_bigint.t *)
+  val of_bignum_bigint :
+    'field.
+       (module Snark_intf.Run with type field = 'field)
+    -> Bignum_bigint.t
+    -> 'field t
+
+  (* Create foreign field element from string *)
+  val of_string :
+    'field.
+    (module Snark_intf.Run with type field = 'field) -> string -> 'field t
 
   (* Convert foreign field element into Cvar limbs *)
   val to_limbs : 'field t -> 'field Cvar.t limbs
@@ -116,6 +155,14 @@ module Foreign_field_element_base = struct
   type 'field t = 'field Cvar.t limbs
 
   let of_limbs x = x
+
+  let of_bignum_bigint (type field)
+      (module Circuit : Snark_intf.Run with type field = field) _x : field t =
+    failwith "not implemented"
+
+  let of_string (type field)
+      (module Circuit : Snark_intf.Run with type field = field) _x : field t =
+    failwith "not implemented"
 
   let to_limbs x = x
 
@@ -175,6 +222,12 @@ module Foreign_field_element : sig
   (* Create foreign field element from standard_limbs *)
   val of_limbs : 'field Cvar.t standard_limbs -> 'field t
 
+  (* Create foreign field element from Bignum_bigint.t *)
+  (* of_bignum_bigint included from Foreign_field_element_base *)
+
+  (* Create foreign field element from string *)
+  (* of_string included from Foreign_field_element_base *)
+
   (* Convert a foreign field element into tuple of 3 field standard_limbs *)
   val to_limbs : 'field t -> 'field Cvar.t standard_limbs
 
@@ -196,14 +249,33 @@ module Foreign_field_element : sig
     -> Bignum_bigint.t standard_limbs
 
   (* Convert foreign field element into a bignum_bigint *)
-  (* Included from Foreign_field_element_base *)
+  (* to_bignum_bigint included from Foreign_field_element_base *)
 end = struct
   module Cvar = Snarky_backendless.Cvar
 
   type 'field t = 'field Cvar.t standard_limbs
 
-  let of_limbs x =
-    to_standard @@ Foreign_field_element_base.of_limbs (Standard x)
+  let of_limbs x = x
+
+  let of_bignum_bigint (type field)
+      (module Circuit : Snark_intf.Run with type field = field) x : field t =
+    let open Circuit in
+    let l12, l0 = Common.bignum_bigint_div_rem x two_to_limb in
+    let l2, l1 = Common.bignum_bigint_div_rem l12 two_to_limb in
+    let l0 =
+      Field.constant @@ Common.bignum_bigint_to_field (module Circuit) l0
+    in
+    let l1 =
+      Field.constant @@ Common.bignum_bigint_to_field (module Circuit) l1
+    in
+    let l2 =
+      Field.constant @@ Common.bignum_bigint_to_field (module Circuit) l2
+    in
+    of_limbs (l0, l1, l2)
+
+  let of_string (type field)
+      (module Circuit : Snark_intf.Run with type field = field) x : field t =
+    of_bignum_bigint (module Circuit) @@ Bignum_bigint.of_string x
 
   let to_limbs x = Foreign_field_element_base.to_limbs x
 
@@ -304,7 +376,6 @@ let compute_witness_variables (type f)
     (module Circuit : Snark_intf.Run with type field = f)
     (products : Bignum_bigint.t standard_limbs)
     (remainder : Bignum_bigint.t standard_limbs) : f * f * f * f * f * f =
-  let open Circuit in
   let products0, products1, products2 = products in
   let remainder0, remainder1, remainder2 = remainder in
 
@@ -367,47 +438,119 @@ let compute_bound_witness_carry (type f)
   in
   Common.bignum_bigint_to_field (module Circuit) quotient_bound_carry
 
-let foreign_field_mul (type f)
-    (module Circuit : Snark_intf.Run with type field = f)
+let array24_to_tuple24 array =
+  match array with
+  | [| a1
+     ; a2
+     ; a3
+     ; a4
+     ; a5
+     ; a6
+     ; a7
+     ; a8
+     ; a9
+     ; a10
+     ; a11
+     ; a12
+     ; a13
+     ; a14
+     ; a15
+     ; a16
+     ; a17
+     ; a18
+     ; a19
+     ; a20
+     ; a21
+     ; a22
+     ; a23
+     ; a24
+    |] ->
+      ( a1
+      , a2
+      , a3
+      , a4
+      , a5
+      , a6
+      , a7
+      , a8
+      , a9
+      , a10
+      , a11
+      , a12
+      , a13
+      , a14
+      , a15
+      , a16
+      , a17
+      , a18
+      , a19
+      , a20
+      , a21
+      , a22
+      , a23
+      , a24 )
+  | _ ->
+      assert false
+
+let mul (type f) (module Circuit : Snark_intf.Run with type field = f)
     (left_input : f Foreign_field_element.t)
     (right_input : f Foreign_field_element.t)
-    (foreign_field_modulus : f Foreign_field_element.t) =
+    (foreign_field_modulus : f standard_limbs) : f Foreign_field_element.t =
+  (* * (f External_checks.t) *)
   let open Circuit in
+  (* Compute gate coefficients
+   *   This happen when circuit is created / not part of witness (e.g. exists, As_prover code)
+   *)
+  let foreign_field_modulus0, foreign_field_modulus1, foreign_field_modulus2 =
+    foreign_field_modulus
+  in
+  let ( neg_foreign_field_modulus
+      , ( neg_foreign_field_modulus0
+        , neg_foreign_field_modulus1
+        , neg_foreign_field_modulus2 ) ) =
+    let foreign_field_modulus =
+      field_standard_limbs_to_bignum_bigint
+        (module Circuit)
+        foreign_field_modulus
+    in
+    (* Compute negated foreign field modulus f' = 2^t - f public parameter *)
+    let neg_foreign_field_modulus =
+      Bignum_bigint.(binary_modulus - foreign_field_modulus)
+    in
+    ( neg_foreign_field_modulus
+    , bignum_bigint_to_field_standard_limbs
+        (module Circuit)
+        neg_foreign_field_modulus )
+  in
+
   (* Compute witness values *)
-  let [| left_input0
-       ; left_input1
-       ; left_input2
-       ; right_input0
-       ; right_input1
-       ; right_input2
-       ; carry1_lo
-       ; carry1_hi
-       ; product1_hi_1
-       ; carry0
-       ; quotient0
-       ; quotient1
-       ; quotient2
-       ; quotient_bound_carry
-       ; remainder0
-       ; remainder1
-       ; remainder2
-       ; quotient_bound01
-       ; quotient_bound2
-       ; remainder_bound0
-       ; remainder_bound1
-       ; remainder_bound2
-       ; product1_lo
-       ; product1_hi_0
-       ; foreign_field_modulus0
-       ; foreign_field_modulus1
-       ; foreign_field_modulus2
-       ; neg_foreign_field_modulus0
-       ; neg_foreign_field_modulus1
-       ; neg_foreign_field_modulus2
-      |] =
-    exists (Typ.array ~length:30 Field.typ) ~compute:(fun () ->
+  let ( left_input0
+      , left_input1
+      , left_input2
+      , right_input0
+      , right_input1
+      , right_input2
+      , carry1_lo
+      , carry1_hi
+      , product1_hi_1
+      , carry0
+      , quotient0
+      , quotient1
+      , quotient2
+      , quotient_bound_carry
+      , remainder0
+      , remainder1
+      , remainder2
+      , quotient_bound01
+      , quotient_bound2
+      , _remainder_bound0
+      , _remainder_bound1
+      , _remainder_bound2
+      , product1_lo
+      , product1_hi_0 ) =
+    exists (Typ.array ~length:24 Field.typ) ~compute:(fun () ->
         (* Compute quotient remainder and negative foreign field modulus *)
-        let quotient, remainder, neg_foreign_field_modulus =
+        let quotient, remainder =
           (* Bignum_bigint computations *)
           let left_input =
             Foreign_field_element.to_bignum_bigint (module Circuit) left_input
@@ -416,7 +559,7 @@ let foreign_field_mul (type f)
             Foreign_field_element.to_bignum_bigint (module Circuit) right_input
           in
           let foreign_field_modulus =
-            Foreign_field_element.to_bignum_bigint
+            field_standard_limbs_to_bignum_bigint
               (module Circuit)
               foreign_field_modulus
           in
@@ -426,12 +569,7 @@ let foreign_field_mul (type f)
               Bignum_bigint.(left_input * right_input)
               foreign_field_modulus
           in
-
-          (* Compute negated foreign field modulus f' = 2^t - f public parameter *)
-          let neg_foreign_field_modulus =
-            Bignum_bigint.(binary_modulus - foreign_field_modulus)
-          in
-          (quotient, remainder, neg_foreign_field_modulus)
+          (quotient, remainder)
         in
 
         (* Compute the intermediate products *)
@@ -521,22 +659,6 @@ let foreign_field_mul (type f)
           bignum_bigint_to_field_standard_limbs (module Circuit) remainder_bound
         in
 
-        (* Compute gate coefficients *)
-        let ( foreign_field_modulus0
-            , foreign_field_modulus1
-            , foreign_field_modulus2 ) =
-          Foreign_field_element.to_field_limbs
-            (module Circuit)
-            foreign_field_modulus
-        in
-        let ( neg_foreign_field_modulus0
-            , neg_foreign_field_modulus1
-            , neg_foreign_field_modulus2 ) =
-          bignum_bigint_to_field_standard_limbs
-            (module Circuit)
-            neg_foreign_field_modulus
-        in
-
         [| left_input0
          ; left_input1
          ; left_input2
@@ -561,14 +683,26 @@ let foreign_field_mul (type f)
          ; remainder_bound2
          ; product1_lo
          ; product1_hi_0
-         ; foreign_field_modulus0
-         ; foreign_field_modulus1
-         ; foreign_field_modulus2
-         ; neg_foreign_field_modulus0
-         ; neg_foreign_field_modulus1
-         ; neg_foreign_field_modulus2
         |] )
+    |> array24_to_tuple24
   in
+
+  (* TODO: Witness hist module
+
+     [@@deriving hlist]
+     Circuit.Typ.of_hlistable
+     module Witness_vars: sig
+       type 'a t
+       val convert : 'a t -> 'a ....
+     end = struct
+       type 'a t = 'a array (* make it abstract *)
+
+       let typ ty = Typ.array ~length:27 Field.typ
+     end *)
+
+  (* TODO: external checks module *)
+
+  (* TODO: refactor Circuit into module template function / functor *)
 
   (* Create ForeignFieldMul gate *)
   with_label "foreign_field_mul" (fun () ->
@@ -606,4 +740,83 @@ let foreign_field_mul (type f)
                  ; neg_foreign_field_modulus2
                  } )
         } ) ;
+  Foreign_field_element.of_limbs (remainder0, remainder1, remainder2)
+
+(*********)
+(* Tests *)
+(*********)
+
+let%test_unit "foreign_field_mul gadget" =
+  (* Import the gadget test runner *)
+  let open Kimchi_gadgets_test_runner in
+  (* Initialize the SRS cache. *)
+  let () =
+    try Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] with _ -> ()
+  in
+
+  (* Helper to test foreign_field_mul gadget
+   *   Inputs:
+   *     - left_input
+   *     - right_input
+   *     - foreign_field_modulus
+   *     - expected product
+   *)
+  let test_mul (left_input : string) (right_input : string)
+      (foreign_field_modulus : string) (expected : string) : unit =
+    Printf.printf "test_mul\n" ;
+    let _proof_keypair, _proof =
+      Runner.generate_and_verify_proof (fun () ->
+          let open Runner.Impl in
+          (* Prepare test inputs *)
+          let foreign_field_modulus =
+            string_to_field_standard_limbs
+              (module Runner.Impl)
+              foreign_field_modulus
+          in
+          let left_input =
+            Foreign_field_element.of_string (module Runner.Impl) left_input
+          in
+          let right_input =
+            Foreign_field_element.of_string (module Runner.Impl) right_input
+          in
+          let result =
+            Foreign_field_element.of_string (module Runner.Impl) expected
+          in
+          (* Create the gadget *)
+          let product =
+            mul
+              (module Runner.Impl)
+              left_input right_input foreign_field_modulus
+          in
+          (* Check product matches expected result *)
+          as_prover (fun () ->
+              let expected =
+                Foreign_field_element.to_field_limbs (module Runner.Impl) result
+              in
+              let product =
+                Foreign_field_element.to_field_limbs
+                  (module Runner.Impl)
+                  product
+              in
+              assert (expected = product) ) ;
+          () )
+    in
+    ()
+  in
+
+  (* Positive tests *)
+  let secp256k1_modulus =
+    "115792089237316195423570985008687907853269984665640564039457584007908834671663"
+  in
+  let secp256k1_max =
+    "115792089237316195423570985008687907853269984665640564039457584007908834671662"
+  in
+  (* 0 * 0 == 0 *)
+  test_mul "0" "0" secp256k1_modulus "0" ;
+  (* max * 1 == max *)
+  test_mul secp256k1_max "1" secp256k1_modulus secp256k1_max ;
+  (* Negative tests *)
+  (* 0 * 0 == 1 *)
+  assert (Common.is_error (fun () -> test_mul "0" "0" secp256k1_modulus "1")) ;
+
   ()
