0fddcead9097336d4b76cd8d0502cfbc45a9ed55
querolita <anais.querol@gmail.com>
2023-03-22 11:59:50 +0100

    fixed syntax (apparently) except for recursive call

--- src/lib/crypto/kimchi_backend/gadgets/xor.ml
+++ src/lib/crypto/kimchi_backend/gadgets/xor.ml
@@ -135,144 +135,133 @@ let rec bxor_rec (type f)
     let next_length = length - (4 * len_xor) in
 
     (* Recursively call xor on the next nibble *)
-    bxor_rec
-      (module Circuit)
-      next_in1 next_in2 next_out next_length len_xor
-      (*
+    bxor_rec (Circuit) next_in1 next_in2 next_out next_length len_xor ;
+    ()
+
 (* Boolean Xor of length bits *)
 let bxor (type f)
-(module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
-(input1 : Circuit.Field.t) (input2 : Circuit.Field.t) (length : int)
-(len_xor : int) : Circuit.Field.t =
-let open Circuit in
-(* Check that the length is permitted *)
-assert (length < Field.size_in_bits) ;
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) (length : int)
+    (len_xor : int) : Circuit.Field.t =
+  let open Circuit in
+  (* Check that the length is permitted *)
+  assert (length < Field.size_in_bits) ;
 
-let output = 
-exists Field.typ
-~compute:(fun () ->
-  (* Read inputs *)
-  let input1_field = As_prover.read Field.typ input1 in
-  let input2_field = As_prover.read Field.typ input2 in
+  let output_xor =
+    exists Field.typ ~compute:(fun () ->
+        (* Read inputs *)
+        let input1_field = As_prover.read Field.typ input1 in
+        let input2_field = As_prover.read Field.typ input2 in
 
-  (* Convert inputs field elements to list of bits *)
-  let input1_bits = Field.Constant.unpack @@ input1_field in
-  let input2_bits = Field.Constant.unpack @@ input2_field in
+        (* Convert inputs field elements to list of bits *)
+        let input1_bits = Field.Constant.unpack @@ input1_field in
+        let input2_bits = Field.Constant.unpack @@ input2_field in
 
-  (* Check real lengths are at most the desired length *)
-  assert (List.length input1_bits <= length) ;
-  assert (List.length input2_bits <= length) ;
+        (* Check real lengths are at most the desired length *)
+        assert (List.length input1_bits <= length) ;
+        assert (List.length input2_bits <= length) ;
 
-  (* Pad with zeros in MSB until reaching same length *)
-  let input1_bits = Common.pad_upto ~length ~value:false input1_bits in
-  let input2_bits = Common.pad_upto ~length ~value:false input2_bits in
+        (* Pad with zeros in MSB until reaching same length *)
+        let input1_bits = Common.pad_upto ~length ~value:false input1_bits in
+        let input2_bits = Common.pad_upto ~length ~value:false input2_bits in
 
-  (* Pad with more zeros until the length is a multiple of 4*n for n-bit length lookup table *)
-  let pad_length =
-    if length mod 4 * len_xor <> 0 then
-      length + ((4 * len_xor) - (length mod 4 * len_xor))
-    else length
+        (* Pad with more zeros until the length is a multiple of 4*n for n-bit length lookup table *)
+        let pad_length =
+          if length mod 4 * len_xor <> 0 then
+            length + ((4 * len_xor) - (length mod 4 * len_xor))
+          else length
+        in
+        let input1_bits =
+          Common.pad_upto ~length:pad_length ~value:false input1_bits
+        in
+        let input2_bits =
+          Common.pad_upto ~length:pad_length ~value:false input2_bits
+        in
+
+        (* Xor list of bits to obtain output of the xor *)
+        let output_bits =
+          List.map2_exn input1_bits input2_bits ~f:(fun b1 b2 ->
+              Bool.(not (equal b1 b2)) )
+        in
+
+        (* Recursively build Xor gadget *)
+        bxor_rec
+          (module Circuit)
+          input1_bits input2_bits output_bits pad_length len_xor ;
+        (* Convert back to field *)
+        Field.Constant.project output_bits )
   in
-  let input1_bits =
-    Common.pad_upto ~length:pad_length ~value:false input1_bits
-  in
-  let input2_bits =
-    Common.pad_upto ~length:pad_length ~value:false input2_bits
-  in
-
-  (* Xor list of bits to obtain output of the xor *)
-  let output_bits =
-    List.map2_exn input1_bits input2_bits ~f:(fun b1 b2 ->
-        Bool.(not (equal b1 b2)) )
-  in
-
-  (* Recursively build Xor gadget *)
-  bxor_rec (module Circuit) input1_bits input2_bits output_bits pad_length len_xor;
-(* Convert back to field *)
-Field.Constant.project output_bits ) in
-
+  output_xor
 
 (* Xor of 16 bits *)
 let bxor16 (type f)
-(module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
-(input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
-let open Circuit in
-bxor (module Circuit) input1 input2 16 4
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
+  let open Circuit in
+  bxor (module Circuit) input1 input2 16 4
 
 (* Xor of 64 bits *)
 let bxor64 (type f)
-(module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
-(input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
-let open Circuit in
-bxor (module Circuit) input1 input2 64 4
-
+    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
+    (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
+  let open Circuit in
+  bxor (module Circuit) input1 input2 64 4
 
 let%test_unit "xor gadget" =
-(* Import the gadget test runner *)
-let open Kimchi_gadgets_test_runner in
-(* Initialize the SRS cache. *)
-let () = Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] in
+  (* Import the gadget test runner *)
+  let open Kimchi_gadgets_test_runner in
+  (* Initialize the SRS cache. *)
+  let () = Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] in
 
-(* Helper to test Xor gadget
+  (* Helper to test Xor gadget
      *   Inputs operands and expected output: left_input xor right_input
      *   Returns true if constraints are satisfied, false otherwise.
-*)
-let test_xor left_input right_input output length =
-try
-  let _proof_keypair, _proof =
-    Runner.generate_and_verify_proof (fun () ->
-        let open Runner.Impl in
-        (* Set up snarky variables for inputs and output *)
-        let left_input =
-          exists Field.typ ~compute:(fun () ->
-              Field.Constant.of_int left_input )
-        in
-        let right_input =
-          exists Field.typ ~compute:(fun () ->
-              Field.Constant.of_int right_input )
-        in
-        let output =
-          exists Field.typ ~compute:(fun () -> Field.Constant.of_int output)
-        in
-        (* Use the xor gate gadget *)
-        let result =
-          bxor (module Runner.Impl) left_input right_input length 4
-        in
-        Field.Assert.equal output result ;
-        (* Pad with a "dummy" constraint b/c Kimchi requires at least 2 *)
-        Boolean.Assert.is_true
-          (Field.equal Field.Constant.zero Field.Constant.zero) )
+  *)
+  let test_xor left_input right_input output_xor length =
+    try
+      let _proof_keypair, _proof =
+        Runner.generate_and_verify_proof (fun () ->
+            let open Runner.Impl in
+            (* Set up snarky variables for inputs and output *)
+            let left_input =
+              exists Field.typ ~compute:(fun () ->
+                  Field.Constant.of_string left_input )
+            in
+            let right_input =
+              exists Field.typ ~compute:(fun () ->
+                  Field.Constant.of_string right_input )
+            in
+            let output_xor =
+              exists Field.typ ~compute:(fun () ->
+                  Field.Constant.of_string output_xor )
+            in
+            (* Use the xor gate gadget *)
+            let result =
+              bxor (module Runner.Impl) left_input right_input length 4
+            in
+            Field.Assert.equal output_xor result ;
+            (* Pad with a "dummy" constraint b/c Kimchi requires at least 2 *)
+            Boolean.Assert.is_true (Field.equal result result) )
+      in
+      true
+    with _ -> false
   in
-  true
-with _ -> false
-in
 
-(* Positive tests *)
-let zero = Field.zero in
-let one = Field.one in
-assert (Bool.equal (test_xor zero zero zero 16) true) ;
-assert (Bool.equal (test_xor zero one one 8) true) ;
-assert (Bool.equal (test_xor zero zero zero 1) true) ;
-assert (Bool.equal (test_xor zero zero zero 4) true) ;
-assert (
-Bool.equal
-  (test_xor Field.of_int 43210 Field.of_int 56789 Field.of_int 29983 16)
-  true ) ;
-assert (
-Bool.equal
-  (test_xor Field.of_int 767430 Field.of_int 974317 Field.of_int 354347 20)
-  true ) ;
-(* 0x5A5A5A5A5A5A5A5A xor 0xA5A5A5A5A5A5A5A5 = 0xFFFFFFFFFFFFFFFF*)
-assert (
-Bool.equal
-  (test_xor Common.field_from_base10 "6510615555426900570"
-     Common.field_from_base10 "18446744073709551615"
-     Common.field_from_base10 "72057594037927935" 64 )
-  true ) ;
-(* Negatve tests *)
-assert (Bool.equal (test_xor 1 0 0 1) false) ;
-assert (Bool.equal (test_xor 1111 2222 0 16) false) ;
+  (* Positive tests *)
+  assert (Bool.equal (test_xor "0" "0" "0" 16) true) ;
+  assert (Bool.equal (test_xor "0" "0" "1" 8) true) ;
+  assert (Bool.equal (test_xor "0" "0" "0" 1) true) ;
+  assert (Bool.equal (test_xor "0" "0" "0" 4) true) ;
+  assert (Bool.equal (test_xor "43210" "56789" "29983" 16) true) ;
+  assert (Bool.equal (test_xor "767430" "974317" "354347" 20) true) ;
+  (* 0x5A5A5A5A5A5A5A5A xor 0xA5A5A5A5A5A5A5A5 = 0xFFFFFFFFFFFFFFFF*)
+  assert (
+    Bool.equal
+      (test_xor "6510615555426900570" "18446744073709551615" "72057594037927935"
+         64 )
+      true ) ;
+  (* Negatve tests *)
+  assert (Bool.equal (test_xor "1" "0" "0" 1) false) ;
+  assert (Bool.equal (test_xor "1111" "2222" "0" 16) false) ;
 
-()
-     *)
-      ()
+  ()
