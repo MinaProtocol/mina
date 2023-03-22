f653f42e695ea3b54bb6add67aa216d00d4165f0
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-03-22 13:38:06 +0000

    Added test for nonce-increments

--- src/lib/mina_base/test/zkapp_command/nonce_related.ml
+++ src/lib/mina_base/test/zkapp_command/nonce_related.ml
@@ -3,6 +3,7 @@ open Mina_base
 open Snarky_backendless
 open Snark_params.Tick
 open Zkapp_command
+open Signature_lib
 
 let filter ((x, y) : Stable.V1.Wire.t * int) : bool =
   Unsigned.UInt32.to_int x.fee_payer.body.nonce
@@ -30,6 +31,7 @@ let%test_unit "target_nonce_on_success" =
         (fun (a, b) -> Unsigned.UInt32.to_int a < Unsigned.UInt32.to_int b)
         ((of_wire x).fee_payer.body.nonce, target_nonce_on_success @@ of_wire x) )
 
+(* target_nonce_on_success *)
 (* Check that the nonce after the function has been applied is lower when there is overflow*)
 let%test_unit "target_nonce_on_success_overflow" =
   Quickcheck.test ~trials:50 filtered_overflow ~f:(fun (x, y) ->
@@ -38,11 +40,17 @@ let%test_unit "target_nonce_on_success_overflow" =
         ((of_wire x).fee_payer.body.nonce, target_nonce_on_success @@ of_wire x) )
 
 (* nonce_increments *)
-(* let%test_unit "nonce_increments" =
-   Quickcheck.test ~trials:50 T.Stable.Latest.Wire.gen ~f:(fun x ->
-       [%test_pred: Account.Nonce.t * Account.Nonce.t]
-         (fun (a, b) -> Unsigned.UInt32.to_int a <= Unsigned.UInt32.to_int b)
-         ((of_wire x).fee_payer.body.nonce, target_nonce_on_success @@ of_wire x) ) *)
+(* Check that the total in the Map is less than or equal to the total number of elements in the account_updates*)
+let%test_unit "nonce_increments" =
+  Quickcheck.test ~trials:50 T.Stable.Latest.Wire.gen ~f:(fun x ->
+      let total_nonce (input : int Public_key.Compressed.Map.t) : int =
+        Public_key.Compressed.Map.fold input ~init:0 ~f:(fun ~key ~data accum ->
+            data + accum )
+      in
+      [%test_pred: int * int]
+        (fun (a, b) -> a >= b)
+        ( List.length (Call_forest.to_list (of_wire x).account_updates)
+        , total_nonce (nonce_increments @@ of_wire x) - 1 ) )
 
 (* check_authorization *)
 (* let%test_unit "check_authorisation" =
