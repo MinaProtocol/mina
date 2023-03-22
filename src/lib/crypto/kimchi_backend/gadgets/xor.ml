d59edaee8246a45f51b32d392c308057e151cbe4
querolita <anais.querol@gmail.com>
2023-03-22 13:14:33 +0100

    move rec inside main xor

--- src/lib/crypto/kimchi_backend/gadgets/xor.ml
+++ src/lib/crypto/kimchi_backend/gadgets/xor.ml
@@ -4,145 +4,151 @@ open Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint
 
 (* XOR *)
 
-(* Recursively builds Xor *)
-let rec bxor_rec (type f)
-    (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
-    (input1_bits : bool list) (input2_bits : bool list)
-    (output_bits : bool list) (length : int) (len_xor : int) =
-  let open Circuit in
-  (* Transform to field elements *)
-  let input1 = Field.Constant.project input1_bits in
-  let input2 = Field.Constant.project input2_bits in
-  let output = Field.Constant.project output_bits in
-  (* If inputs are zero and length is zero, add the zero check *)
-  if length = 0 then (
-    assert (Field.Constant.(equal input1 zero)) ;
-    assert (Field.Constant.(equal input2 zero)) ;
-    assert (Field.Constant.(equal output zero)) ;
-    with_label "zero_check" (fun () ->
-        assert_
-          { annotation = Some __LOC__
-          ; basic =
-              Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
-                (Basic
-                   { l = (Field.Constant.zero, Field.one)
-                   ; r = (Field.Constant.zero, Field.zero)
-                   ; o = (Option.value_exn Field.(to_constant zero), Field.zero)
-                   ; m = Field.Constant.zero
-                   ; c = Field.Constant.one
-                   } )
-          } ) )
-  else
-    (* Nibbles *)
-    let in1 = Field.constant input1 in
-    let in2 = Field.constant input2 in
-    let out = Field.constant output in
-    let in1_0 = Common.field_bits_le_to_field (module Circuit) in1 0 len_xor in
-    let in1_1 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in1 len_xor
-        Int.(2 * len_xor)
-    in
-    let in1_2 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in1
-        Int.(2 * len_xor)
-        Int.(3 * len_xor)
-    in
-    let in1_3 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in1
-        Int.(3 * len_xor)
-        Int.(4 * len_xor)
-    in
-    let in2_0 = Common.field_bits_le_to_field (module Circuit) in2 0 len_xor in
-    let in2_1 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in2 len_xor
-        Int.(2 * len_xor)
-    in
-    let in2_2 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in2
-        Int.(2 * len_xor)
-        Int.(3 * len_xor)
-    in
-    let in2_3 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        in2
-        Int.(3 * len_xor)
-        Int.(4 * len_xor)
-    in
-    let out_0 = Common.field_bits_le_to_field (module Circuit) out 0 len_xor in
-    let out_1 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        out len_xor
-        Int.(2 * len_xor)
-    in
-    let out_2 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        out
-        Int.(2 * len_xor)
-        Int.(3 * len_xor)
-    in
-    let out_3 =
-      Common.field_bits_le_to_field
-        (module Circuit)
-        out
-        Int.(3 * len_xor)
-        Int.(4 * len_xor)
-    in
-
-    (* If length is more than 0, add the Xor gate *)
-    with_label "xor16_gate" (fun () ->
-        (* Set up Xor gate *)
-        assert_
-          { annotation = Some __LOC__
-          ; basic =
-              Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
-                (Xor
-                   { in1
-                   ; in2
-                   ; out
-                   ; in1_0
-                   ; in1_1
-                   ; in1_2
-                   ; in1_3
-                   ; in2_0
-                   ; in2_1
-                   ; in2_2
-                   ; in2_3
-                   ; out_0
-                   ; out_1
-                   ; out_2
-                   ; out_3
-                   } )
-          } ) ;
-
-    (* Remove least significant 4 nibbles *)
-    let next_in1 = List.drop input1_bits (4 * len_xor) in
-    let next_in2 = List.drop input2_bits (4 * len_xor) in
-    let next_out = List.drop output_bits (4 * len_xor) in
-    (* Next length is 4*n less bits *)
-    let next_length = length - (4 * len_xor) in
-
-    (* Recursively call xor on the next nibble *)
-    bxor_rec (module Circuit) next_in1 next_in2 next_out next_length len_xor ;
-    ()
-
 (* Boolean Xor of length bits *)
 let bxor (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) (length : int)
     (len_xor : int) : Circuit.Field.t =
+  (* Recursively builds Xor *)
+  let rec bxor_rec (input1_bits : bool list) (input2_bits : bool list)
+      (output_bits : bool list) (length : int) (len_xor : int) =
+    let open Circuit in
+    (* Transform to field elements *)
+    let input1 = Field.Constant.project input1_bits in
+    let input2 = Field.Constant.project input2_bits in
+    let output = Field.Constant.project output_bits in
+    (* If inputs are zero and length is zero, add the zero check *)
+    if length = 0 then (
+      assert (Field.Constant.(equal input1 zero)) ;
+      assert (Field.Constant.(equal input2 zero)) ;
+      assert (Field.Constant.(equal output zero)) ;
+      with_label "zero_check" (fun () ->
+          assert_
+            { annotation = Some __LOC__
+            ; basic =
+                Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
+                  (Basic
+                     { l = (Field.Constant.zero, Field.one)
+                     ; r = (Field.Constant.zero, Field.zero)
+                     ; o =
+                         (Option.value_exn Field.(to_constant zero), Field.zero)
+                     ; m = Field.Constant.zero
+                     ; c = Field.Constant.one
+                     } )
+            } ) )
+    else
+      (* Nibbles *)
+      let in1 = Field.constant input1 in
+      let in2 = Field.constant input2 in
+      let out = Field.constant output in
+      let in1_0 =
+        Common.field_bits_le_to_field (module Circuit) in1 0 len_xor
+      in
+      let in1_1 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in1 len_xor
+          Int.(2 * len_xor)
+      in
+      let in1_2 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in1
+          Int.(2 * len_xor)
+          Int.(3 * len_xor)
+      in
+      let in1_3 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in1
+          Int.(3 * len_xor)
+          Int.(4 * len_xor)
+      in
+      let in2_0 =
+        Common.field_bits_le_to_field (module Circuit) in2 0 len_xor
+      in
+      let in2_1 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in2 len_xor
+          Int.(2 * len_xor)
+      in
+      let in2_2 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in2
+          Int.(2 * len_xor)
+          Int.(3 * len_xor)
+      in
+      let in2_3 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          in2
+          Int.(3 * len_xor)
+          Int.(4 * len_xor)
+      in
+      let out_0 =
+        Common.field_bits_le_to_field (module Circuit) out 0 len_xor
+      in
+      let out_1 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          out len_xor
+          Int.(2 * len_xor)
+      in
+      let out_2 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          out
+          Int.(2 * len_xor)
+          Int.(3 * len_xor)
+      in
+      let out_3 =
+        Common.field_bits_le_to_field
+          (module Circuit)
+          out
+          Int.(3 * len_xor)
+          Int.(4 * len_xor)
+      in
+
+      (* If length is more than 0, add the Xor gate *)
+      with_label "xor_gate" (fun () ->
+          (* Set up Xor gate *)
+          assert_
+            { annotation = Some __LOC__
+            ; basic =
+                Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
+                  (Xor
+                     { in1
+                     ; in2
+                     ; out
+                     ; in1_0
+                     ; in1_1
+                     ; in1_2
+                     ; in1_3
+                     ; in2_0
+                     ; in2_1
+                     ; in2_2
+                     ; in2_3
+                     ; out_0
+                     ; out_1
+                     ; out_2
+                     ; out_3
+                     } )
+            } ) ;
+
+      (* Remove least significant 4 nibbles *)
+      let next_in1 = List.drop input1_bits (4 * len_xor) in
+      let next_in2 = List.drop input2_bits (4 * len_xor) in
+      let next_out = List.drop output_bits (4 * len_xor) in
+      (* Next length is 4*n less bits *)
+      let next_length = length - (4 * len_xor) in
+
+      (* Recursively call xor on the next nibble *)
+      bxor_rec next_in1 next_in2 next_out next_length len_xor ;
+      ()
+  in
+
   let open Circuit in
   (* Check that the length is permitted *)
   assert (length < Field.size_in_bits) ;
@@ -185,9 +191,7 @@ let bxor (type f)
         in
 
         (* Recursively build Xor gadget *)
-        bxor_rec
-          (module Circuit)
-          input1_bits input2_bits output_bits pad_length len_xor ;
+        bxor_rec input1_bits input2_bits output_bits pad_length len_xor ;
         (* Convert back to field *)
         Field.Constant.project output_bits )
   in
@@ -197,22 +201,21 @@ let bxor (type f)
 let bxor16 (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
-  let open Circuit in
   bxor (module Circuit) input1 input2 16 4
 
 (* Xor of 64 bits *)
 let bxor64 (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input1 : Circuit.Field.t) (input2 : Circuit.Field.t) : Circuit.Field.t =
-  let open Circuit in
   bxor (module Circuit) input1 input2 64 4
 
 let%test_unit "xor gadget" =
   (* Import the gadget test runner *)
   let open Kimchi_gadgets_test_runner in
   (* Initialize the SRS cache. *)
-  let () = Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] in
-
+  let () =
+    try Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] with _ -> ()
+  in
   (* Helper to test Xor gadget
      *   Inputs operands and expected output: left_input xor right_input
      *   Returns true if constraints are satisfied, false otherwise.
