5b41036fe51aca85e9079370a12b35fddd7629f7
querolita <anais.querol@gmail.com>
2023-03-24 15:08:50 +0100

    not works :)

--- src/lib/crypto/kimchi_backend/gadgets/not.ml
+++ src/lib/crypto/kimchi_backend/gadgets/not.ml
@@ -2,121 +2,118 @@ open Core_kernel
 
 open Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint
 
+module Bignum_bigint = Snarky_backendless.Backend_extended.Bignum_bigint
+
 (* NOT *)
 
 (* Boolean Not of length bits for checked length (uses Xor gadgets inside) *)
 let bnot_checked (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
-    (input : Circuit.Field.t) (length : int) : Circuit.Field.t =
+    (input : Circuit.Field.t) (length : int) (len_xor : int) : Circuit.Field.t =
   let open Circuit in
-  let all_ones = Field.of_int (Int.pow 2 length - 1) in
+  let all_ones = Bignum_bigint.(pow (of_int 2) (of_int length) - one) in
+  let all_ones = Common.bignum_bigint_to_field (module Circuit) all_ones in
+  let all_ones = Field.constant all_ones in
 
   (* Negating is equivalent to XORing with all one word *)
-  Xor.xor input all_ones length
+  Xor.bxor (module Circuit) input all_ones length len_xor
 
 (* Boolean Not of length bits for unchecked length (uses Generic subtractions inside) *)
-let bnot_checked (type f)
+let bnot_unchecked (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input : Circuit.Field.t) (length : int) : Circuit.Field.t =
   let open Circuit in
-  let all_ones = Field.of_int (Int.pow 2 length - 1) in
-
+  let all_ones = Bignum_bigint.(pow (of_int 2) (of_int length) - one) in
+  let all_ones = Common.bignum_bigint_to_field (module Circuit) all_ones in
+  let all_ones = Field.constant all_ones in
+  let all_ones_var =
+    exists Field.typ ~compute:(fun () ->
+        Common.cvar_field_to_field_as_prover (module Circuit) all_ones )
+  in
+  Field.Assert.equal all_ones all_ones_var ;
   (* Negating is equivalent to subtracting with all one word *)
+  (* [2^len - 1] - input = not (input) *)
   with_label "not_subtraction" (fun () ->
       assert_
         { annotation = Some __LOC__
         ; basic =
             Kimchi_backend_common.Plonk_constraint_system.Plonk_constraint.T
               (Basic
-                 { l = (Field.Constant.one, all_ones)
-                 ; r = (Field.Constant.Field.(to_constant (negate one)), input)
+                 { l = (Field.Constant.one, all_ones_var)
+                 ; r = (Option.value_exn Field.(to_constant (negate one)), input)
                  ; o =
                      ( Option.value_exn Field.(to_constant (negate one))
-                     , all_ones - input )
+                     , Field.(all_ones_var - input) )
                  ; m = Field.Constant.zero
                  ; c = Field.Constant.zero
                  } )
-        } )
+        } ) ;
+  Field.(all_ones_var - input)
 
 (* Not of 64 bits checked *)
 let bnot64_checked (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input : Circuit.Field.t) : Circuit.Field.t =
-  let open Circuit in
-  bnot_checked input 64
+  bnot_checked (module Circuit) input 64 4
 
 (* Not of 64 bits unchecked *)
 let bnot64_unchecked (type f)
     (module Circuit : Snarky_backendless.Snark_intf.Run with type field = f)
     (input : Circuit.Field.t) : Circuit.Field.t =
-  let open Circuit in
-  bnot_unchecked input 64
+  bnot_unchecked (module Circuit) input 64
 
 let%test_unit "not gadget" =
   (* Import the gadget test runner *)
   let open Kimchi_gadgets_test_runner in
   (* Initialize the SRS cache. *)
-  let () = Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] in
-
+  let () =
+    try Kimchi_pasta.Vesta_based_plonk.Keypair.set_urs_info [] with _ -> ()
+  in
   (* Helper to test not gadget with both checked and unchecked length procedures
      *   Input and expected output and desired length : not(input) = output
      *   Returns true if constraints are satisfied, false otherwise.
   *)
-  let test_not left output length =
-    try
-      let _proof_keypair, _proof =
-        Runner.generate_and_verify_proof (fun () ->
-            let open Runner.Impl in
-            (* Set up snarky variables for input and output *)
-            let input =
-              exists Field.typ ~compute:(fun () -> Field.Constant.of_int input)
-            in
-            let output =
-              exists Field.typ ~compute:(fun () -> Field.Constant.of_int output)
-            in
-            (* Use the not gate gadget *)
-            let result_checked =
-              bnot_checked (module Runner.Impl) input length
-            in
-            let result_unchecked =
-              bnot_unchecked (module Runner.Impl) input length
-            in
-            (* Check that the result is correct *)
-            Field.Assert.equal output result_checked ;
-            Field.Assert.equal output result_unchecked )
-      in
-      true
-    with _ -> false
+  let test_not input output length =
+    let _proof_keypair, _proof =
+      Runner.generate_and_verify_proof (fun () ->
+          let open Runner.Impl in
+          (* Set up snarky variables for input and output *)
+          let input =
+            Common.as_prover_cvar_field_of_base10 (module Runner.Impl) input
+          in
+          let output =
+            Common.as_prover_cvar_field_of_base10 (module Runner.Impl) output
+          in
+          (* Use the not gate gadget *)
+          let result_checked =
+            bnot_checked (module Runner.Impl) input length 4
+          in
+          let result_unchecked =
+            bnot_unchecked (module Runner.Impl) input length
+          in
+          (* Check that the result is correct *)
+          Field.Assert.equal output result_checked ;
+          Field.Assert.equal output result_unchecked )
+    in
+    ()
   in
 
   (* Positive tests *)
-  let zero = Field.zero in
-  let one = Field.one in
-  assert (Bool.equal (test_not zero one 1) true) ;
-  assert (Bool.equal (test_not zero Field.of_int 15 4) true) ;
-  assert (Bool.equal (test_not zero Field.of_int 255 8) true) ;
-  assert (Bool.equal (test_not zero Field.of_int 2047 11) true) ;
-  assert (Bool.equal (test_not zero Field.of_int 65535 16) true) ;
-  assert (Bool.equal (test_not Field.of_int 43210 Field.of_int 22325 16) true) ;
-  assert (Bool.equal (test_not Field.of_int 767430 Field.of_int 281145 20) true) ;
+  test_not "0" "1" 1 ;
+  test_not "0" "15" 4 ;
+  test_not "0" "255" 8 ;
+  test_not "0" "2047" 11 ;
+  test_not "0" "65535" 16 ;
+  test_not "43210" "22325" 16 ;
+  test_not "767430" "281145" 20 ;
   (* not 0xA5A5A5A5A5A5A5A5 = 0x5A5A5A5A5A5A5A5A*)
-  assert (
-    Bool.equal
-      (test_not Common.field_from_base10 "6510615555426900570"
-         Common.field_from_base10 "18446744073709551615" 64 )
-      true ) ;
+  test_not "11936128518282651045" "6510615555426900570" 64 ;
   (* not 0x5A5A5A5A5A5A5A5A = 0xA5A5A5A5A5A5A5A5 *)
-  assert (
-    Bool.equal
-      (test_not Common.field_from_base10 "18446744073709551615"
-         Common.field_from_base10 "6510615555426900570" 64 )
-      true ) ;
+  test_not "6510615555426900570" "11936128518282651045" 64 ;
   (* not 0xFFFFFFFFFFFFFFFF = 0 *)
-  assert (
-    Bool.equal
-      (test_not Common.field_from_base10 "72057594037927935" zero 64)
-      true ) ;
+  test_not "18446744073709551615" "0" 64 ;
 
   (* Negatve tests *)
-  assert (Bool.equal (test_not zero zero 1) false) ;
+  assert (Common.is_error (fun () -> test_not "0" "0" 1)) ;
+  assert (Common.is_error (fun () -> test_not "255" "0" 4)) ;
   ()
