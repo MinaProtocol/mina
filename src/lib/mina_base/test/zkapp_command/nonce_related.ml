59dac70e0349c1a772f9254627ce5e4361462ab5
ejMina226 <118474890+ejMina226@users.noreply.github.com>
2023-03-29 16:43:26 +0100

    Add in Alcotest

--- src/lib/mina_base/test/zkapp_command/nonce_related.ml
+++ src/lib/mina_base/test/zkapp_command/nonce_related.ml
@@ -18,14 +18,16 @@ let filter_overflow ((x, y) : Stable.V1.Wire.t * int) : bool =
   > Unsigned.UInt32.to_int Unsigned.UInt32.max_int
 
 let filtered =
-  Base_quickcheck.Generator.filter Valid_size.zkapp_type_gen ~f:filter
+  Base_quickcheck.Generator.filter Zkapp_command_test.Valid_size.zkapp_type_gen
+    ~f:filter
 
 let filtered_overflow =
-  Base_quickcheck.Generator.filter Valid_size.zkapp_type_gen ~f:filter_overflow
+  Base_quickcheck.Generator.filter Zkapp_command_test.Valid_size.zkapp_type_gen
+    ~f:filter_overflow
 
 (* target_nonce_on_success *)
 (* Check that the nonce after the function has been applied is higher*)
-let%test_unit "target_nonce_on_success" =
+let target_nonce_on_success_test () =
   Quickcheck.test ~trials:50 filtered ~f:(fun (x, y) ->
       [%test_pred: Account.Nonce.t * Account.Nonce.t]
         (fun (a, b) -> Unsigned.UInt32.to_int a < Unsigned.UInt32.to_int b)
@@ -33,7 +35,7 @@ let%test_unit "target_nonce_on_success" =
 
 (* target_nonce_on_success *)
 (* Check that the nonce after the function has been applied is lower when there is overflow*)
-let%test_unit "target_nonce_on_success_overflow" =
+let target_nonce_on_success_overflow () =
   Quickcheck.test ~trials:50 filtered_overflow ~f:(fun (x, y) ->
       [%test_pred: Account.Nonce.t * Account.Nonce.t]
         (fun (a, b) -> Unsigned.UInt32.to_int a > Unsigned.UInt32.to_int b)
@@ -41,7 +43,7 @@ let%test_unit "target_nonce_on_success_overflow" =
 
 (* nonce_increments *)
 (* Check that the total in the Map is less than or equal to the total number of elements in the account_updates*)
-let%test_unit "nonce_increments" =
+let nonce_increments_test () =
   Quickcheck.test ~trials:50 T.Stable.Latest.Wire.gen ~f:(fun x ->
       let total_nonce (input : int Public_key.Compressed.Map.t) : int =
         Public_key.Compressed.Map.fold input ~init:0 ~f:(fun ~key ~data accum ->
@@ -52,9 +54,21 @@ let%test_unit "nonce_increments" =
         ( List.length (Call_forest.to_list (of_wire x).account_updates)
         , total_nonce (nonce_increments @@ of_wire x) - 1 ) )
 
-(* check_authorization *)
-(* let%test_unit "check_authorisation" =
-   Quickcheck.test ~trials:50 T.Stable.Latest.Wire.gen ~f:(fun x ->
-       [%test_pred: Account.Nonce.t * Account.Nonce.t]
-         (fun (a, b) -> Unsigned.UInt32.to_int a <= Unsigned.UInt32.to_int b)
-         ((of_wire x).fee_payer.body.nonce, target_nonce_on_success @@ of_wire x) ) *)
+let () =
+  let open Alcotest in
+  run "Test nonce_related."
+    [ ( "nonce_related"
+      , [ test_case
+            "Check that the nonce after the function has been applied is \
+             higher."
+            `Quick target_nonce_on_success_test
+        ; test_case
+            "Check that the nonce after the function has been applied is lower \
+             when there is overflow."
+            `Quick target_nonce_on_success_overflow
+        ; test_case
+            "Check that the total in the Map is less than or equal to the \
+             total number of elements in the account_updates."
+            `Quick nonce_increments_test
+        ] )
+    ]
