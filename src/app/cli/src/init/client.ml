9282da08129d65ca21d89cefc515b23df9845266
Paul Steckler <steck@stecksoft.com>
2023-04-04 18:25:25 -0700

    batch zkApps

--- src/app/cli/src/init/client.ml
+++ src/app/cli/src/init/client.ml
@@ -2169,8 +2169,6 @@ let thread_graph =
                   (humanize_graphql_error ~graphql_endpoint e) ) ;
              exit 1 ) )
 
-[@@@warning "-26-27"]
-
 let itn_create_accounts =
   Command.async ~summary:"Fund new accounts for incentivized testnet"
     (let open Command.Param in
@@ -2196,12 +2194,26 @@ let itn_create_accounts =
     let args = Args.zip5 privkey_path key_prefix num_accounts fee amount in
     Cli_lib.Background_daemon.rpc_init args
       ~f:(fun port (privkey_path, key_prefix, num_accounts, fee, amount) ->
+        let keys_per_zkapp = 8 in
+        let zkapps_per_block = 10 in
+        let pk_check_wait = Time.Span.of_sec 60. in
+        let pk_check_timeout = Time.Span.of_min 12. in
         let min_fee =
           Currency.Fee.to_nanomina_int
             Mina_compile_config.minimum_user_command_fee
         in
+        let account_creation_fee_int =
+          let constraint_constants =
+            Genesis_constants.Constraint_constants.compiled
+          in
+          Currency.Fee.to_nanomina_int constraint_constants.account_creation_fee
+        in
         if fee < min_fee then (
           Format.eprintf "Minimum fee is %d@." min_fee ;
+          Core.exit 1 ) ;
+        if not @@ Option.is_some @@ Sys.getenv Secrets.Keypair.env then (
+          Format.eprintf "Please set environment variable %s"
+            Secrets.Keypair.env ;
           Core.exit 1 ) ;
         let%bind fee_payer_keypair =
           Secrets.Keypair.Terminal_stdin.read_exn
@@ -2252,9 +2264,19 @@ let itn_create_accounts =
           |> Public_key.Compressed.to_base58_check )
           (Account.Nonce.to_int fee_payer_initial_nonce) ;
         let fee_payer_current_nonce = ref fee_payer_initial_nonce in
-        let keys_per_zkapp = 8 in
         let keypairs =
           List.init num_accounts ~f:(fun _n -> Signature_lib.Keypair.create ())
+        in
+        let%bind () =
+          Deferred.List.iteri keypairs ~f:(fun i kp ->
+              let privkey_path = sprintf "%s-%d" key_prefix i in
+              let pk =
+                Signature_lib.Public_key.compress kp.public_key
+                |> Signature_lib.Public_key.Compressed.to_base58_check
+              in
+              Format.printf "Writing key file %s for public key %s@."
+                privkey_path pk ;
+              Secrets.Keypair.Terminal_stdin.write_exn kp ~privkey_path )
         in
         let keypair_chunks = List.chunks_of keypairs ~length:keys_per_zkapp in
         let num_chunks = List.length keypair_chunks in
@@ -2284,102 +2306,192 @@ let itn_create_accounts =
         let chunk_total = Array.fold chunk_amounts ~init:0 ~f:( + ) in
         top_off_chunks 0 chunk_total ;
         assert (Array.fold chunk_amounts ~init:0 ~f:( + ) = amount) ;
-        Deferred.List.iteri keypair_chunks ~f:(fun i kps ->
-            let chunk_amount = chunk_amounts.(i) - fee in
-            if Int.is_negative chunk_amount then (
-              Format.eprintf
-                "Calculated negative amount for chunk of account updates; \
-                 increase amount or lower fee@." ;
-              Core.exit 1 ) ;
-            let num_updates = List.length kps in
-            let amount_per_update = chunk_amount / num_updates in
-            let update_rem = chunk_amount mod num_updates in
-            let update_amounts =
-              Array.init num_updates ~f:(fun i ->
-                  if i < update_rem then amount_per_update + 1
-                  else amount_per_update )
-            in
-            assert (Array.fold update_amounts ~init:0 ~f:( + ) = chunk_amount) ;
-            let memo_str = sprintf "ITN account funder, chunk %d" i in
-            let memo = Signed_command_memo.create_from_string_exn memo_str in
-            let multispec :
-                Transaction_snark.For_tests.Multiple_transfers_spec.t =
-              let fee = Currency.Fee.of_nanomina_int_exn fee in
-              let sender = (fee_payer_keypair, !fee_payer_current_nonce) in
-              let fee_payer = None in
-              let receivers =
-                List.mapi kps ~f:(fun j kp ->
+        let zkapps =
+          List.mapi keypair_chunks ~f:(fun i kps ->
+              let num_updates = List.length kps in
+              let chunk_amount = chunk_amounts.(i) in
+              if Int.is_negative chunk_amount then (
+                Format.eprintf
+                  "Calculated negative amount for chunk of account updates; \
+                   increase amount or lower fee@." ;
+                Core.exit 1 ) ;
+              let amount_per_update =
+                (chunk_amount / num_updates) - account_creation_fee_int
+              in
+              let update_rem = chunk_amount mod num_updates in
+              let update_amounts =
+                Array.init num_updates ~f:(fun i ->
+                    if i < update_rem then amount_per_update + 1
+                    else amount_per_update )
+              in
+              assert (
+                Array.fold update_amounts ~init:0 ~f:( + )
+                = chunk_amount - (num_updates * account_creation_fee_int) ) ;
+              let memo_str = sprintf "ITN account funder, chunk %d" i in
+              let memo = Signed_command_memo.create_from_string_exn memo_str in
+              let multispec :
+                  Transaction_snark.For_tests.Multiple_transfers_spec.t =
+                let fee = Currency.Fee.of_nanomina_int_exn fee in
+                let sender = (fee_payer_keypair, !fee_payer_current_nonce) in
+                let fee_payer = None in
+                let receivers =
+                  List.mapi kps ~f:(fun j kp ->
+                      let pk =
+                        kp.public_key |> Signature_lib.Public_key.compress
+                      in
+                      let amount =
+                        update_amounts.(j)
+                        |> Currency.Amount.of_nanomina_int_exn
+                      in
+                      (pk, amount) )
+                in
+                let amount =
+                  chunk_amount |> Currency.Amount.of_nanomina_int_exn
+                in
+                let zkapp_account_keypairs = [] in
+                let new_zkapp_account = false in
+                let snapp_update = Account_update.Update.dummy in
+                let actions = [] in
+                let events = [] in
+                let call_data = Snark_params.Tick.Field.zero in
+                let preconditions = None in
+                { fee
+                ; sender
+                ; fee_payer
+                ; receivers
+                ; amount
+                ; zkapp_account_keypairs
+                ; memo
+                ; new_zkapp_account
+                ; snapp_update
+                ; actions
+                ; events
+                ; call_data
+                ; preconditions
+                }
+              in
+              fee_payer_current_nonce :=
+                Account.Nonce.succ !fee_payer_current_nonce ;
+              Transaction_snark.For_tests.multiple_transfers multispec )
+        in
+        let zkapps_batches = List.chunks_of zkapps ~length:zkapps_per_block in
+        Deferred.List.iter zkapps_batches ~f:(fun zkapps_batch ->
+            Format.printf "Processing batch of %d zkApps@."
+              (List.length zkapps_batch) ;
+            List.iteri zkapps_batch ~f:(fun i zkapp ->
+                let txn_hash =
+                  Transaction_hash.hash_command (Zkapp_command zkapp)
+                  |> Transaction_hash.to_base58_check
+                in
+                Format.printf " zkApp %d, transaction hash: %s@." i txn_hash ;
+                Format.printf " Fee payer: %s, nonce: %d@."
+                  (Signature_lib.Public_key.Compressed.to_base58_check
+                     zkapp.fee_payer.body.public_key )
+                  (Account.Nonce.to_int zkapp.fee_payer.body.nonce) ;
+                Format.printf " Account updates@." ;
+                Zkapp_command.Call_forest.iteri zkapp.account_updates
+                  ~f:(fun _i acct_update ->
                     let pk =
-                      kp.public_key |> Signature_lib.Public_key.compress
+                      Signature_lib.Public_key.Compressed.to_base58_check
+                        acct_update.body.public_key
                     in
-                    let amount =
-                      update_amounts.(j) |> Currency.Amount.of_nanomina_int_exn
+                    let balance_change = acct_update.body.balance_change in
+                    let sgn =
+                      if Currency.Amount.Signed.is_negative balance_change then
+                        "-"
+                      else ""
                     in
-                    (pk, amount) )
-              in
-              let amount =
-                chunk_amount |> Currency.Amount.of_nanomina_int_exn
-              in
-              let zkapp_account_keypairs = [] in
-              let new_zkapp_account = false in
-              let snapp_update = Account_update.Update.dummy in
-              let actions = [] in
-              let events = [] in
-              let call_data = Snark_params.Tick.Field.zero in
-              let preconditions = None in
-              { fee
-              ; sender
-              ; fee_payer
-              ; receivers
-              ; amount
-              ; zkapp_account_keypairs
-              ; memo
-              ; new_zkapp_account
-              ; snapp_update
-              ; actions
-              ; events
-              ; call_data
-              ; preconditions
-              }
-            in
-            fee_payer_current_nonce :=
-              Account.Nonce.succ !fee_payer_current_nonce ;
-            let zkapp_command =
-              Transaction_snark.For_tests.multiple_transfers multispec
-            in
+                    let balance_change_str =
+                      match
+                        Currency.Amount.to_yojson
+                        @@ Currency.Amount.Signed.magnitude balance_change
+                      with
+                      | `String s ->
+                          s
+                      | _ ->
+                          failwith "Expected string"
+                    in
+                    Format.printf "  Public key: %s  Balance change: %s%s@." pk
+                      sgn balance_change_str ) ) ;
             let%bind res =
               Daemon_rpcs.Client.dispatch Daemon_rpcs.Send_zkapp_commands.rpc
-                [ zkapp_command ] port
-            in
-            let txn_hash =
-              Transaction_hash.hash_command (Zkapp_command zkapp_command)
-              |> Transaction_hash.to_base58_check
+                zkapps_batch port
             in
             ( match res with
             | Ok res_inner -> (
                 match res_inner with
-                | Ok [ _ ] ->
-                    Format.printf "Sent zkApp [%s], transaction hash: %s@."
-                      memo_str txn_hash ;
+                | Ok zkapps ->
                     Format.printf
-                      "The zkApp will create accounts with these public keys \
-                       and balances:@." ;
-                    List.iter multispec.receivers ~f:(fun (pk, amt) ->
-                        Format.printf "  %s : %d@."
-                          (Public_key.Compressed.to_base58_check pk)
-                          (Currency.Amount.to_nanomina_int amt) )
-                | Ok _ ->
-                    failwith "Got unexpected number of zkApps sent"
+                      "Successfully sent %d zkApps to transaction pool@."
+                      (List.length zkapps)
                 | Error err ->
-                    Format.eprintf
-                      "When sending zkApp [%s], transaction hash: %s, got \
-                       error: %s@."
-                      memo_str txn_hash (Error.to_string_hum err) )
+                    Format.eprintf "When sending zkApps, got error: %s@."
+                      (Error.to_string_hum err) ;
+                    Core.exit 1 )
             | Error err ->
+                Format.printf "Failed to send zkApps, error: %s@."
+                  (Error.to_string_hum err) ;
+                Core.exit 1 ) ;
+            let batch_pks =
+              List.map zkapps_batch ~f:(fun zkapp ->
+                  let acct_update_pks =
+                    List.map
+                      (Zkapp_command.Call_forest.to_list zkapp.account_updates)
+                      ~f:(fun acct_update -> acct_update.body.public_key)
+                  in
+                  zkapp.fee_payer.body.public_key :: acct_update_pks )
+              |> List.concat
+              |> List.dedup_and_sort
+                   ~compare:Signature_lib.Public_key.Compressed.compare
+            in
+            (* check ledger at intervals for presence of all pks *)
+            let rec check_for_pks () =
+              let%bind res =
+                Daemon_rpcs.Client.dispatch Daemon_rpcs.Get_ledger.rpc None port
+              in
+              match res with
+              | Ok (Ok accounts) ->
+                  Format.printf "Succesfully downloaded daemon ledger@." ;
+                  let key_set =
+                    Signature_lib.Public_key.Compressed.Hash_set.of_list
+                      (List.map accounts ~f:(fun acct -> acct.public_key))
+                  in
+                  Deferred.return
+                  @@ List.for_all batch_pks ~f:(fun pk ->
+                         Hash_set.mem key_set pk )
+              | Ok (Error err) ->
+                  Format.eprintf "Error in getting daemon ledger: %s@."
+                    (Error.to_string_hum err) ;
+                  let%bind () = after (Time.Span.of_sec 10.) in
+                  check_for_pks ()
+              | Error err ->
+                  Format.eprintf "Error in getting daemon ledger: %s@."
+                    (Error.to_string_hum err) ;
+                  let%bind () = after (Time.Span.of_sec 10.) in
+                  check_for_pks ()
+            in
+            let rec check_loop () =
+              let%bind got_pks = check_for_pks () in
+              if got_pks then (
+                Format.printf "Found all batch public keys in daemon ledger@." ;
+                Deferred.unit )
+              else (
                 Format.printf
-                  "Failed to send zkApp [%s], transaction hash: %s@." memo_str
-                  txn_hash ) ;
-            Deferred.unit ) ))
+                  "Did not find all batch public keys in daemon ledger, will \
+                   retry@." ;
+                let%bind () = after pk_check_wait in
+                check_loop () )
+            in
+            Format.printf "Checking daemon ledger for batch public keys ...@." ;
+            match%bind Async.with_timeout pk_check_timeout (check_loop ()) with
+            | `Result _ ->
+                Deferred.unit
+            | `Timeout ->
+                Format.eprintf
+                  "Timed out (%s) waiting to find batch public keys in daemon \
+                   ledger@."
+                  (Time.Span.to_string pk_check_timeout) ;
+                exit 1 ) ))
 
 module Visualization = struct
   let create_command (type rpc_response) ~name ~f
