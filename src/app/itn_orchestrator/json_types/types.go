ba6279dc82788a8e8b20f12c0f309e60395b45f9
georgeee <george.agapov@gmail.com>
2023-04-06 16:49:40 +0200

    ITN orchestrator: structured JSON format

--- src/app/itn_orchestrator/json_types/types.go
+++ src/app/itn_orchestrator/json_types/types.go
@@ -1,9 +1,14 @@
 package itn_json_types
 
 import (
+	"crypto/ed25519"
+	"encoding/base64"
 	"errors"
 	"strconv"
 )
+
+type MinaPublicKey string
+type MinaPrivateKey string
 
 func UnmarshalUint64(data []byte, v *uint64) error {
 	if data[0] == '"' && data[len(data)-1] == '"' {
@@ -25,38 +30,40 @@ func MarshalUint64(v *uint64) ([]byte, error) {
 	return encloseInQuotes(strconv.FormatUint(*v, 10)), nil
 }
 
-// func UnmarshalBase64(data []byte, v *[]byte) error {
-// 	if data[0] == '"' && data[len(data)-1] == '"' {
-// 		res, err := base64.StdEncoding.DecodeString(string(data[1 : len(data)-1]))
-// 		if err != nil {
-// 			return err
-// 		}
-// 		*v = res
-// 		return nil
-// 	}
-// 	return errors.New("not a string token")
-// }
+func UnmarshalBase64(data []byte, v *[]byte) error {
+	if data[0] == '"' && data[len(data)-1] == '"' {
+		res, err := base64.StdEncoding.DecodeString(string(data[1 : len(data)-1]))
+		if err != nil {
+			return err
+		}
+		*v = res
+		return nil
+	}
+	return errors.New("not a string token")
+}
 
-// func MarshalBase64(v *[]byte) ([]byte, error) {
-// 	return encloseInQuotes(base64.StdEncoding.EncodeToString(*v)), nil
-// }
+func MarshalBase64(v *[]byte) ([]byte, error) {
+	return encloseInQuotes(base64.StdEncoding.EncodeToString(*v)), nil
+}
 
-// func UnmarshalPrivateKey(data []byte, v *ed25519.PrivateKey) error {
-// 	var b []byte
-// 	if err := UnmarshalBase64(data, &b); err != nil {
-// 		return err
-// 	}
-// 	if len(b) != ed25519.SeedSize {
-// 		return errors.New("wrong size of Private key")
-// 	}
-// 	*v = ed25519.NewKeyFromSeed(b)
-// 	return nil
-// }
+type Ed25519Privkey ed25519.PrivateKey
 
-// func MarshalPrivateKey(v *ed25519.PrivateKey) ([]byte, error) {
-// 	seed := v.Seed()
-// 	return MarshalBase64(&seed)
-// }
+func (v *Ed25519Privkey) UnmarshalJSON(data []byte) error {
+	var b []byte
+	if err := UnmarshalBase64(data, &b); err != nil {
+		return err
+	}
+	if len(b) != ed25519.SeedSize {
+		return errors.New("wrong size of Private key")
+	}
+	*v = Ed25519Privkey(ed25519.NewKeyFromSeed(b))
+	return nil
+}
+
+func (v *Ed25519Privkey) MarshalJSON() ([]byte, error) {
+	seed := ed25519.PrivateKey(*v).Seed()
+	return MarshalBase64(&seed)
+}
 
 // func UnmarshalPublicKey(data []byte, v *ed25519.PublicKey) error {
 // 	var b []byte
