ac16fb23869c64b0a6a60d83e1e56695e976e2fe
Bruno Deferrari <bruno.deferrari@viablesystems.io>
2023-04-11 17:13:53 -0300

    Update coordinated snark work scheduler for the new coordinator

--- src/lib/work_selector/coordinated.ml
+++ src/lib/work_selector/coordinated.ml
@@ -4,19 +4,19 @@ let coordinator_url =
   Sys.getenv_opt "MINA_SNARK_COORDINATOR_URL"
   |> Option.value ~default:"http://localhost:8080"
 
-let query_coordinator identifier =
-  let url =
-    String.concat ~sep:"/" [ coordinator_url; "lock-job"; identifier ]
-  in
-  match Ezcurl.put ~url ~content:(`String "") () with
+let query_coordinator identifiers =
+  let url = String.concat ~sep:"/" [ coordinator_url; "get-job" ] in
+  match
+    Ezcurl.put ~url ~content:(`String (String.concat ~sep:"\n" identifiers)) ()
+  with
   | Ok response ->
       Stdlib.Printf.printf "+++ querying coordinator: %s -> %d \n%!" url
         response.Ezcurl.code ;
-      response.Ezcurl.code = 201
+      if response.Ezcurl.code = 201 then Some response.Ezcurl.body else None
   | Error (_curl_code, msg) ->
       Stdlib.Printf.printf "+++ querying coordinator: %s -> failed: %s \n%!" url
         msg ;
-      false
+      None
 
 module Make
     (Inputs : Intf.Inputs_intf)
@@ -24,14 +24,24 @@ module Make
 struct
   let spec_hashes spec =
     let statement = Snark_work_lib.Work.Single.Spec.statement spec in
-    (* TODO: take into account all pass hashes here  *)
-    let source =
-      Mina_base.Frozen_ledger_hash.to_base58_check statement.source.first_pass_ledger
+    let source_first =
+      Mina_base.Frozen_ledger_hash.to_base58_check
+        statement.source.first_pass_ledger
     in
-    let target =
-      Mina_base.Frozen_ledger_hash.to_base58_check statement.target.second_pass_ledger
+    let source_second =
+      Mina_base.Frozen_ledger_hash.to_base58_check
+        statement.source.second_pass_ledger
     in
-    source ^ ":" ^ target
+    let target_first =
+      Mina_base.Frozen_ledger_hash.to_base58_check
+        statement.target.first_pass_ledger
+    in
+    let target_second =
+      Mina_base.Frozen_ledger_hash.to_base58_check
+        statement.target.second_pass_ledger
+    in
+    source_first ^ "->" ^ target_first ^ ":" ^ source_second ^ "->"
+    ^ target_second
 
   let work_identifier
       (work :
@@ -47,12 +57,17 @@ struct
     | _ ->
         assert false
 
-  let rec get_next_coordinated_job = function
-    | [] ->
+  let rec find_matching_job ~logger wanted_identifier identifiers jobs =
+    match (identifiers, jobs) with
+    | identifier :: _, job :: _ when String.equal identifier wanted_identifier
+      ->
+        Some job
+    | _ :: identifiers, _ :: jobs ->
+        find_matching_job ~logger wanted_identifier identifiers jobs
+    | _, _ ->
+        [%log error] "Could not find job for $identifier"
+          ~metadata:[ ("identifier", `String wanted_identifier) ] ;
         None
-    | work :: rest ->
-        if query_coordinator (work_identifier work) then Some work
-        else get_next_coordinated_job rest
 
   let work ~snark_pool ~fee ~logger (state : Lib.State.t) =
     Lib.State.remove_old_assignments state ~logger ;
@@ -61,8 +76,11 @@ struct
     | [] ->
         None
     | expensive_work ->
-        Option.map (get_next_coordinated_job expensive_work) ~f:(fun x ->
-            Lib.State.set state x ; x )
+        let identifiers = List.map ~f:work_identifier expensive_work in
+        query_coordinator identifiers
+        |> Option.bind ~f:(fun identifier ->
+               find_matching_job ~logger identifier identifiers expensive_work )
+        |> Option.map ~f:(fun x -> Lib.State.set state x ; x)
 
   let remove = Lib.State.remove
 
