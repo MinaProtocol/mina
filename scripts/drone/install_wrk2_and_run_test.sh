dd016721507058fdfe011d270a2f2ad1e968dbbc
Adrian Nagy <adrian.nagy@viablesystems.io>
2023-04-13 14:02:19 +0200

    CI: Add graphql timeout test pipeline

---
+++ scripts/drone/install_wrk2_and_run_test.sh
@@ -0,0 +1,5 @@
+rm -rf wrk2 && \
+git clone https://github.com/giltene/wrk2.git && \
+cd wrk2 && \
+make && \
+./wrk -t1 -c1 -d120s -R1 --latency --timeout 2s -s /tmp/version-post.lua http://localhost:3085/graphql > /tmp/wrk_output.txt
