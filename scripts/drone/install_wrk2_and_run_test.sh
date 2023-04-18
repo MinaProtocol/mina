e491b31a9dc42d8a9d724651a78d8bcf46fc792b
Adrian Nagy <adrian.nagy@viablesystems.io>
2023-04-18 13:27:25 +0200

    CI: use 127.0.0.1 instead of localhost

--- scripts/drone/install_wrk2_and_run_test.sh
+++ scripts/drone/install_wrk2_and_run_test.sh
@@ -2,4 +2,4 @@ rm -rf wrk2 && \
 git clone https://github.com/giltene/wrk2.git && \
 cd wrk2 && \
 make && \
-./wrk -t1 -c1 -d120s -R1 --latency --timeout 2s -s /tmp/version-post.lua http://localhost:3085/graphql > /tmp/wrk_output.txt
+./wrk -t1 -c1 -d120s -R1 --latency --timeout 2s -s /tmp/version-post.lua http://127.0.0.1:3085/graphql > /tmp/wrk_output.txt
