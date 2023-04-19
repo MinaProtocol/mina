6a46f1ed4fce8a89d65f9c22ff2c1a86280c654b
Sventimir <svantimir@gmail.com>
2023-04-19 14:56:01 +0200

    [mina_base] Migrate Zkapp_basic tests to Alcotest,

---
+++ src/lib/mina_base/test/zkapp_basic_test.ml
@@ -0,0 +1,13 @@
+open Core_kernel
+open Mina_base
+open Zkapp_basic
+open Signature_lib
+
+let int_to_bits_roundtrip () =
+  Quickcheck.test ~trials:100 Int.(gen_incl min_value max_value)
+    ~f:(fun i ->
+      [%test_eq: int] i (int_of_bits @@ int_to_bits ~length:64 i))
+
+let invalid_public_key_is_invalid () =
+  [%test_eq: Public_key.t option] None
+    (Public_key.decompress invalid_public_key)
