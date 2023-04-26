5195f4c4e3f0c46c698e2a50438a0c2965822e07
dkijania <dariusz@o1labs.org>
2023-04-26 13:10:51 +0200

    Added dryrun mode

--- automation/scripts/github_branch_autosync/github_autosync/__main__.py
+++ automation/scripts/github_branch_autosync/github_autosync/__main__.py
@@ -4,16 +4,19 @@ import json
 import argparse
 import os
 import sys
-from gcloud_entrypoint import handle_incoming_commit_push_json,config
+from gcloud_entrypoint import handle_incoming_commit_push_json,config,verify_signature
 
 parser = argparse.ArgumentParser()
-parser.add_argument('test_json', help='test file from github webhook push event')
+parser.add_argument('payload', help='test file from github webhook push event')
+parser.add_argument('secret', help='secret for calculating signature')
+parser.add_argument('incoming_signature', help='payload signature')
 
 args = parser.parse_args()
 
-if not os.path.isfile(args.test_json):
-    sys.exit('cannot find test file :',args.test_json)
+if not os.path.isfile(args.payload):
+    sys.exit('cannot find test file :',args.payload)
 
-with open(args.test_json,encoding="utf-8") as file:
+with open(args.payload,encoding="utf-8") as file:
     data = json.load(file)
-    handle_incoming_commit_push_json(data,config=config)
+    json_payload = json.dumps(data)
+    verify_signature(json_payload, args.secret, "sha=" + args.incoming_signature)
--- automation/scripts/github_branch_autosync/github_autosync/gcloud_entrypoint/__init__.py
+++ automation/scripts/github_branch_autosync/github_autosync/gcloud_entrypoint/__init__.py
@@ -1,2 +1,2 @@
 """Entrypoint init"""
-from .main import handle_incoming_commit_push_json,config
+from .main import handle_incoming_commit_push_json,config,verify_signature
