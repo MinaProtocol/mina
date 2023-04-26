7d425a3c705fcdf0876316f738e0556eeec233f8
Gregor <gregor.mitscha-baude@gmx.at>
2023-04-26 16:09:39 +0200

    fix snarkyjs unit tests

--- buildkite/scripts/test-snarkyjs-bindings.sh
+++ buildkite/scripts/test-snarkyjs-bindings.sh
@@ -14,8 +14,6 @@ make snarkyjs
 echo "Run SnarkyJS unit tests..."
 cd src/lib/snarkyjs
 npm run test:unit
-cd ../../../..
 
 echo "Run additional SnarkyJS tests..."
-node src/lib/snarkyjs/tests/integration/simple-zkapp-mock-apply.js
-node src/lib/snarkyjs/tests/integration/inductive-proofs.js
+./run-mina-integration-tests.sh
