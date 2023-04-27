bd063f2001ccb9f4417a8bebd651c17cdefd1976
Andrew Trainor <andrew@o1labs.org>
2023-04-27 17:56:49 -0400

    lk86/keygen-script-with-random-passwords Update script for more complete functionality

--- scripts/generate-community-keys.sh
+++ scripts/generate-community-keys.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -u
-# Set the prefix to the node name (like "seed" or "block-producer")
-# And set count as the number of keys of this type to generate (e.g. "./keygen.sh seed 6" produces 6 keys, "seed-1" through "seed-6")
+
+# This script depends on the mina daemon package, zip, and pwgen
+#   to generate unique passwords for each key, and output a zip file containing:
+#   the password along with the public and private keypairs
+# For convenience, the script finally zips together all of the individual zip files,
+#   plus a txt file containing just the public keys
+# Set the prefix to the node name (like "community", "seed" or "block-producer")
+#   and set count as the number of keys of this type to generate (e.g. "./generate-community-keys.sh bp 5" produces 3 keys, "bp-1" through "bp-5")
 PREFIX=$1
 COUNT=$2
+
+mkdir "${PREFIX}"
+cd "${PREFIX}"
 
 for i in $(seq 1 ${COUNT}); do
 
@@ -32,7 +41,13 @@ for i in $(seq 1 ${COUNT}); do
         rm -rf ${NODE}
 done
 
-echo "All keys generated successfully!"
 echo "Combining .pub files into one ${PREFIX}-keys.txt and cleaning up"
 cat ${PREFIX}-*.pub > ${PREFIX}-keys.txt
+cp ${PREFIX}-keys.txt ../
 rm -rf ${PREFIX}-*.pub
+
+cd ..
+echo "All keys generated successfully! Combining into one zip file"
+zip -r "${PREFIX}.zip" "${PREFIX}"
+
+rm -rf "${PREFIX}"
