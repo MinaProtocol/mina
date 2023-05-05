b10eefca6c694ca470ecf6887a26ec69f89862ee
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2023-05-05 17:26:32 -0600

    updated terraform deployment step

---
+++ buildkite/pipelines/hardfork/scripts/terraform_deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# this script deploys a pre-configured mina network using terraform
+
+set -euo pipefail
+
+echo "--- Cloning Mina repository"
+git clone https://github.com/MinaProtocol/mina.git
+
+##########################################################
+# Terraform network deployment
+##########################################################
+
+echo "--- Initializing terraform configuration"
+
+cd ./mina/automations/terraform/testnets/testworld-2-0/
+terraform init
+
+echo "--- Deploying hardfork network"
+
+
+echo "--- Waiting for mina nodes to come online"
