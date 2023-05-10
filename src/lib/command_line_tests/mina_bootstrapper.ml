a054d6bc7643f7ed7fea6d3b22eabbe8a64f5289
dkijania <dariusz@o1labs.org>
2023-05-10 10:40:25 +0200

    Revived command-line-tests:
    - migrated to alcotest
    - maintained two tests
    - extracted modules for future development

---
+++ src/lib/command_line_tests/mina_bootstrapper.ml
@@ -0,0 +1,79 @@
+open Async
+open Core
+open Config
+open Mina_cli
+
+(* executable location relative to src/default/lib/command_line_tests
+
+      dune won't allow running it via "dune exec", because it's outside its
+      workspace, so we invoke the executable directly
+
+      the mina.exe executable must have been built before running the test
+      here, else it will fail
+*)
+let default_mina_exe = "_build/default/src/app/cli/src/mina.exe"
+
+module MinaBootstrapper = struct
+  type t =
+    { config : Config.t
+    ; client_delay : float
+    ; retry_delay : float
+    ; retry_attempts : int
+    }
+
+  let create config =
+    { config; client_delay = 40.; retry_delay = 30.; retry_attempts = 5 }
+
+  let get_args t working_dir =
+    [ "daemon"
+    ; "-seed"
+    ; "--demo-mode"
+    ; "-background"
+    ; "-working-dir"
+    ; working_dir
+    ; "-client-port"
+    ; sprintf "%d" t.config.port
+    ; "-config-directory"
+    ; t.config.dirs.conf
+    ; "-genesis-ledger-dir"
+    ; t.config.dirs.genesis
+    ; "-current-protocol-version"
+    ; "0.0.0"
+    ; "-external-ip"
+    ; "0.0.0.0"
+    ; "-libp2p-keypair"
+    ; ConfigDirs.libp2p_keypair_folder t.config.dirs
+    ]
+
+  let prepare t =
+    let working_dir = Sys.getcwd () in
+    let args = get_args t working_dir in
+    Async.printf "Starting daemon inside %s\n" working_dir ;
+    Async.printf "Starting command: %s %s\n" t.config.mina_exe
+      (String.concat ~sep:" " args) ;
+    return (t.config.mina_exe, args)
+
+  let start t =
+    let%bind prog, args = prepare t in
+    Process.create ~prog ~args ()
+
+  let wait_for_bootstrap t =
+    let mina_cli = MinaCli.create t.config.port t.config.mina_exe in
+    Async.printf "Waiting initial %d s. before connecting\n"
+      (int_of_float t.client_delay) ;
+    let%bind _ =
+      Deferred.map (after @@ Time.Span.of_sec t.client_delay) ~f:Or_error.return
+    in
+    let rec go retries_remaining =
+      match%bind MinaCli.daemon_status mina_cli with
+      | Error _ when retries_remaining > 0 ->
+          Async.printf "Daemon not responding.. retrying (%i/%i)\n"
+            (t.retry_attempts - retries_remaining)
+            t.retry_attempts ;
+          let%bind () = after @@ Time.Span.of_sec t.retry_delay in
+          go (retries_remaining - 1)
+      | ret ->
+          return ret
+    in
+    go t.retry_attempts
+end
---
+++ src/lib/command_line_tests/runner.ml
@@ -0,0 +1,39 @@
+open Core
+open Async
+open Config
+
+module TestRunner = struct
+  let () =
+    Backtrace.elide := false ;
+    Async.Scheduler.set_record_backtraces true
+
+  let remove_config_dirs dirs =
+    Deferred.List.iter dirs ~f:(fun dir ->
+        Deferred.ignore_m @@ Process.run_exn ~prog:"rm" ~args:[ "-rf"; dir ] () )
+
+  let run test_case =
+    let test_failed = ref false in
+    let clean_up = false in
+    let open Deferred.Let_syntax in
+    let config = Config.default 1337 in
+    let%bind () = ConfigDirs.generate_keys config.dirs in
+    Monitor.protect
+      ~finally:(fun () ->
+        ( if !test_failed then
+          let contents =
+            Core.In_channel.(
+              with_file (ConfigDirs.mina_log config.dirs) ~f:input_all)
+          in
+          printf
+            !"**** DAEMON CRASHED (OUTPUT BELOW) ****\n%s\n************\n%!"
+            contents ) ;
+        if clean_up then remove_config_dirs (ConfigDirs.dirs config.dirs)
+        else Deferred.unit )
+      (fun () ->
+        match%map test_case config with
+        | Ok () ->
+            ()
+        | Error err ->
+            test_failed := true ;
+            Error.raise err )
+end
