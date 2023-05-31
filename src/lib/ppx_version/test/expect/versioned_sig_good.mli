5eef9990923681a590a0b2bb2ab5627b60c54d94
João Santos Reis <joaosreis@outlook.pt>
2023-05-31 13:41:07 +0100

    More reformats

--- src/lib/ppx_version/test/expect/versioned_sig_good.mli
+++ src/lib/ppx_version/test/expect/versioned_sig_good.mli
@@ -1,39 +1,47 @@
 open Core_kernel
-module Good :
-sig
-  include
-    sig
-      module Stable :
-      sig
-        module V2 :
-        sig
-          type t = int[@@deriving (bin_io, version)]
-          include
-            sig
-              [@@@ocaml.warning "-32"]
-              include Bin_prot.Binable.S with type  t :=  t
-              val __versioned__ : unit
-            end[@@ocaml.doc "@inline"]
+
+module Good : sig
+  include sig
+    module Stable : sig
+      module V2 : sig
+        type t = int [@@deriving bin_io, version]
+
+        include sig
+          [@@@ocaml.warning "-32"]
+
+          include Bin_prot.Binable.S with type t := t
+
+          val __versioned__ : unit
         end
-        module Latest = V2
-        module V1 :
-        sig
-          type t = string[@@deriving (bin_io, version)]
-          include
-            sig
-              [@@@ocaml.warning "-32"]
-              include Bin_prot.Binable.S with type  t :=  t
-              val __versioned__ : unit
-            end[@@ocaml.doc "@inline"]
-          val to_latest : t -> V2.t
-        end
-        val versions :
-          (int * (Core_kernel.Bigstring.t -> pos_ref:int ref -> Latest.t))
-            array
-        val bin_read_to_latest_opt :
-          Bin_prot.Common.buf -> pos_ref:int ref -> Latest.t option
+        [@@ocaml.doc "@inline"]
       end
-      type t = Stable.Latest.t
+
+      module Latest = V2
+
+      module V1 : sig
+        type t = string [@@deriving bin_io, version]
+
+        include sig
+          [@@@ocaml.warning "-32"]
+
+          include Bin_prot.Binable.S with type t := t
+
+          val __versioned__ : unit
+        end
+        [@@ocaml.doc "@inline"]
+
+        val to_latest : t -> V2.t
+      end
+
+      val versions :
+        (int * (Core_kernel.Bigstring.t -> pos_ref:int ref -> Latest.t)) array
+
+      val bin_read_to_latest_opt :
+        Bin_prot.Common.buf -> pos_ref:int ref -> Latest.t option
     end
+
+    type t = Stable.Latest.t
+  end
+
   val is_42 : t -> bool
 end
