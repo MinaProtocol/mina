e2b43b8f6f64ac65b60b021f14beb35cf451d0b7
Danny Willems <be.danny.willems@gmail.com>
2023-06-12 17:26:01 +0200

    Pickles: comment {tick|tock}_field_sponge

--- src/lib/pickles/tock_field_sponge.mli
+++ src/lib/pickles/tock_field_sponge.mli
@@ -1,3 +1,8 @@
+(** Implement a Sponge for the field Tock *)
+
 include module type of Make_sponge.Make (Backend.Tock.Field)
 
+(** Parameters for the permutation. It can be generated using the {{
+    https://github.com/o1-labs/proof-systems/tree/master/poseidon } SAGE
+    script} *)
 val params : Backend.Tock.Field.t Sponge.Params.t
