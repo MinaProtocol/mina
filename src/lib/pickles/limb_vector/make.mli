196a6a3add643fb4dd594652cba0de8b29e7e089
Danny Willems <be.danny.willems@gmail.com>
2023-06-16 07:39:35 +0200

    Pickles: comment Limb_vector.Make

--- src/lib/pickles/limb_vector/make.mli
+++ src/lib/pickles/limb_vector/make.mli
@@ -1,8 +1,8 @@
-module T
-    (Impl : Snarky_backendless.Snark_intf.Run)
-    (N : Pickles_types.Nat.Intf) : sig
+(** Type representation of a vector of [N.t] limbs *)
+module T (Impl : Snarky_backendless.Snark_intf.Run) (N : Pickles_types.Nat.Intf) : sig
   type t = Impl.Field.t
 
+  (** Returns the length of the vector as a runtime integer *)
   val length : int
 
   module Constant : module type of Constant.Make (N)
