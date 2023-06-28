397e46b688635e1ae44785440822d355564612f3
georgeee <george.agapov@gmail.com>
2023-06-28 11:50:34 +0200

    Remove Ledger_table extension

--- src/lib/transition_frontier/extensions/extensions.ml
+++ src/lib/transition_frontier/extensions/extensions.ml
@@ -6,14 +6,12 @@ module Root_history = Root_history
 module Snark_pool_refcount = Snark_pool_refcount
 module Transition_registry = Transition_registry
 module New_breadcrumbs = New_breadcrumbs
-module Ledger_table = Ledger_table
 
 type t =
   { root_history : Root_history.Broadcasted.t
   ; snark_pool_refcount : Snark_pool_refcount.Broadcasted.t
   ; best_tip_diff : Best_tip_diff.Broadcasted.t
   ; transition_registry : Transition_registry.Broadcasted.t
-  ; ledger_table : Ledger_table.Broadcasted.t
   ; new_breadcrumbs : New_breadcrumbs.Broadcasted.t
   }
 [@@deriving fields]
@@ -35,15 +33,11 @@ let create ~logger frontier : t Deferred.t =
   let%bind new_breadcrumbs =
     New_breadcrumbs.(Broadcasted.create (create ~logger frontier))
   in
-  let%bind ledger_table =
-    Ledger_table.(Broadcasted.create (create ~logger frontier))
-  in
   return
     { root_history
     ; snark_pool_refcount
     ; best_tip_diff
     ; transition_registry
-    ; ledger_table
     ; new_breadcrumbs
     }
 
@@ -60,7 +54,6 @@ let close t : unit =
     ~best_tip_diff:(close_extension (module Best_tip_diff.Broadcasted))
     ~transition_registry:
       (close_extension (module Transition_registry.Broadcasted))
-    ~ledger_table:(close_extension (module Ledger_table.Broadcasted))
     ~new_breadcrumbs:(close_extension (module New_breadcrumbs.Broadcasted))
 
 let notify (t : t) ~logger ~frontier ~diffs_with_mutants =
@@ -82,7 +75,6 @@ let notify (t : t) ~logger ~frontier ~diffs_with_mutants =
        ~snark_pool_refcount:(update (module Snark_pool_refcount.Broadcasted))
        ~best_tip_diff:(update (module Best_tip_diff.Broadcasted))
        ~transition_registry:(update (module Transition_registry.Broadcasted))
-       ~ledger_table:(update (module Ledger_table.Broadcasted))
        ~new_breadcrumbs:(update (module New_breadcrumbs.Broadcasted)) )
 
 type ('ext, 'view) access =
@@ -92,7 +84,6 @@ type ('ext, 'view) access =
   | Best_tip_diff : (Best_tip_diff.t, Best_tip_diff.view) access
   | Transition_registry
       : (Transition_registry.t, Transition_registry.view) access
-  | Ledger_table : (Ledger_table.t, Ledger_table.view) access
   | New_breadcrumbs : (New_breadcrumbs.t, New_breadcrumbs.view) access
 
 type ('ext, 'view) broadcasted_extension =
@@ -111,7 +102,6 @@ let get :
      ; snark_pool_refcount
      ; best_tip_diff
      ; transition_registry
-     ; ledger_table
      ; new_breadcrumbs
      } -> function
   | Root_history ->
@@ -124,8 +114,6 @@ let get :
   | Transition_registry ->
       Broadcasted_extension
         ((module Transition_registry.Broadcasted), transition_registry)
-  | Ledger_table ->
-      Broadcasted_extension ((module Ledger_table.Broadcasted), ledger_table)
   | New_breadcrumbs ->
       Broadcasted_extension
         ((module New_breadcrumbs.Broadcasted), new_breadcrumbs)
