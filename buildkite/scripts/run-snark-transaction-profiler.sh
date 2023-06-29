595a8290d6106d089dda1da10d9c31aa03874297
dkijania <dariusz@o1labs.org>
2023-06-29 09:02:36 +0200

    added testnet to mina apt get install

--- buildkite/scripts/run-snark-transaction-profiler.sh
+++ buildkite/scripts/run-snark-transaction-profiler.sh
@@ -17,7 +17,7 @@ source buildkite/scripts/export-git-env-vars.sh
 echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
 apt-get update
-apt-get install --allow-downgrades -y "mina=${MINA_DEB_VERSION}"
+apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
 
 K=1
 MAX_NUM_UPDATES=2
