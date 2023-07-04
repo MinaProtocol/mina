0835da5159e6e41ce7b317c7bd87b5b92d75d05e
dkijania <dariusz@o1labs.org>
2023-07-04 08:26:08 +0200

    added job_id run env when uploading test result to buildkite

--- buildkite/scripts/upload-test-results.sh
+++ buildkite/scripts/upload-test-results.sh
@@ -16,8 +16,9 @@ curl -X POST https://analytics-api.buildkite.com/v1/uploads -sv \
   -F "format=junit" \
   -F "run_env[CI]=buildkite" \
   -F "run_env[key]=$BUILDKITE_BUILD_ID" \
-  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
   -F "run_env[branch]=$BUILDKITE_BRANCH" \
   -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
-  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
   -F "run_env[message]=$BUILDKITE_MESSAGE"
