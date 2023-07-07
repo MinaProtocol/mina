06b3c02181419b0a73b2a8e1cf56730ea9704960
dkijania <dariusz@o1labs.org>
2023-07-07 16:33:33 +0200

    added mapping for branches

--- scripts/check-proof-systems-submodule.sh
+++ scripts/check-proof-systems-submodule.sh
@@ -10,6 +10,13 @@ git config http.sslVerify false
 git fetch origin
 git config http.sslVerify true
 
+declare -A BRANCH_MAPPING=(
+  ["rampup"]="compatible" 
+  ["berkeley"]="berkeley"
+  ["develop"]="develop"
+  ["izmir"]="master"
+)
+
 function in_branch {
   if git rev-list origin/$1 | grep -q $CURR; then
     echo "Proof systems submodule commit is an ancestor of $1"
@@ -19,8 +26,9 @@ function in_branch {
   fi
 }
 
-if (! in_branch "berkeley"); then
-  echo "Proof-systems submodule commit is NOT an ancestor of berkeley branch"
+BRANCH="${BRANCH_MAPPING[${BUILDKITE_PULL_REQUEST_BASE_BRANCH}]}"
+
+if (! in_branch ${BRANCH}); then
+  echo "Proof-systems submodule commit is NOT an ancestor of ${BRANCH} branch"
   exit 1
 fi
-
