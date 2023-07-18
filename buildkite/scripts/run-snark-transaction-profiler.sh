ff6ad22e241dd4f975feb9f51b17c0d354ac4c2f
dkijania <dariusz@o1labs.org>
2023-07-18 12:57:04 +0200

    added case for mina deb suffix based on base branch

--- buildkite/scripts/run-snark-transaction-profiler.sh
+++ buildkite/scripts/run-snark-transaction-profiler.sh
@@ -8,7 +8,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y git apt-transport-https ca-certificates tzdata curl python3
 
-TESTNET_NAME="berkeley"
+case "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" in
+  rampup|berkeley|release/2.0.0|develop)
+    TESTNET_NAME="berkeley"
+  ;;
+  *)
+    TESTNET_NAME="mainnet"
+esac
 
 git config --global --add safe.directory /workdir
 
