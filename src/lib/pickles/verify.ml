077045a0f2481c55ae6822e12f97c2452655188b
Richard Bonichon <richard.bonichon@gmail.com>
2023-07-19 16:54:43 +0200

    Pickles verify: avoid full iteration with local exception

--- src/lib/pickles/verify.ml
+++ src/lib/pickles/verify.ml
@@ -62,14 +62,19 @@ let verify_heterogenous (ts : Instance.t list) =
          ->
         Timer.start __LOC__ ;
         let non_chunking =
-          let single_chunk = ref true in
-          let _unit_evals =
+          let exception Is_chunked in
+          match
             Pickles_types.Plonk_types.Evals.map evals.evals.evals
               ~f:(fun (x, y) ->
-                single_chunk :=
-                  !single_chunk && Array.length x = 1 && Array.length y = 1 )
-          in
-          !single_chunk
+                if Array.length x > 1 || Array.length y > 1 then
+                  raise Is_chunked )
+          with
+          | exception Is_chunked ->
+              false
+          | _unit_evals ->
+              (* we do not care about _unit_evals, if we reached this point, we
+                 know all evals have length 1 for they cannot have length 0 *)
+              true
         in
         check (lazy "only uses single chunks", non_chunking) ;
         check
