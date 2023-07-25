60fb565981430c3c6d0a69d492db4cdd3fa57dd6
Tang Jiawei <tang@o1labs.org>
2023-07-26 04:41:27 +0800

    add mli file to epoch_ledger_medium_bootstrap and add sections to it

--- src/app/test_executive/epoch_ledger_medium_bootstrap.ml
+++ src/app/test_executive/epoch_ledger_medium_bootstrap.ml
@@ -17,13 +17,12 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
   type dsl = Dsl.t
 
   let config =
+    let k = 2 in
 
-    let k=2 in
-    
     let open Test_config in
     { default with
       k
-    ; slots_per_epoch = 3 *  8  * k 
+    ; slots_per_epoch = 3 * 8 * k
     ; requires_graphql = true
     ; genesis_ledger =
         [ { account_name = "node-a-key"; balance = "1000"; timing = Untimed }
@@ -35,39 +34,43 @@ module Make (Inputs : Intf.Test.Inputs_intf) = struct
         ; { node_name = "node-b"; account_name = "node-b-key" }
         ; { node_name = "node-c"; account_name = "node-c-key" }
         ]
-     ;  proof_config =
+    ; proof_config =
         { proof_config_default with
           work_delay = Some 1
         ; transaction_capacity =
             Some Runtime_config.Proof_keys.Transaction_capacity.medium
         }
     }
-    
-  (*
-     There are 3 cases of bootstrap that we need to test:
 
-     1: short bootstrap-- bootstrap where node has been down for less than 2k+1 blocks
-     2: medium bootstrap-- bootstrap where node has been down for more than 2k+1 blocks, OR equivalently when the blockchain is longer than 2k+1 blocks and a node goes down and resets to a fresh state, thereby resetting at the genesis block, before reconnecting to the network
-     3: long bootstrap-- bootstrap where node has been down for more than 42k slots (2 epochs) where each epoch emitted at least 1 parallel scan state proof
-  *)
+  let transactions_sent = ref 0
 
-let send_padding_transactions ~fee ~logger ~n nodes =
+  let num_proofs = 2
+
+  let padding_payments () =
+    let needed_for_padding =
+      Test_config.transactions_needed_for_ledger_proofs config ~num_proofs
+    in
+    if !transactions_sent >= needed_for_padding then 0
+    else needed_for_padding - !transactions_sent
+
+  let send_padding_transactions ~fee ~logger ~n nodes =
     let sender = List.nth_exn nodes 0 in
     let receiver = List.nth_exn nodes 1 in
     let open Malleable_error.Let_syntax in
     let%bind sender_pub_key = pub_key_of_node sender in
     let%bind receiver_pub_key = pub_key_of_node receiver in
     repeat_seq ~n ~f:(fun () ->
-        Network.Node.must_send_payment ~logger sender ~sender_pub_key
-          ~receiver_pub_key ~amount:Currency.Amount.one ~fee
+        Graphql_requests.must_send_online_payment ~logger
+          (Network.Node.get_ingress_uri sender)
+          ~sender_pub_key ~receiver_pub_key ~amount:Currency.Amount.one ~fee
         >>| ignore )
 
-  (* this test is the medium bootstrap test *)
-
   let run network t =
-    let slot_ms = Option.value_exn config.proof_config.block_window_duration_ms in
+    let slot_ms =
+      Option.value_exn config.proof_config.block_window_duration_ms
+    in
     (* time for 1 epoch *)
-    let epoch_ms = config.slots_per_epoch * slot_ms |> Float.of_int in 
+    let epoch_ms = config.slots_per_epoch * slot_ms |> Float.of_int in
     let open Network in
     let open Malleable_error.Let_syntax in
     let logger = Logger.create () in
@@ -88,50 +91,45 @@ let send_padding_transactions ~fee ~logger ~n nodes =
     let node_c =
       Core.String.Map.find_exn (Network.block_producers network) "node-c"
     in
-    
+
     (*let%bind () =
-      let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
-      send_padding_transactions block_producer_nodes ~fee ~logger
-        ~n:1000
-    in
-     *) 
+        let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
+        send_padding_transactions block_producer_nodes ~fee ~logger
+          ~n:1000
+      in
+    *)
     let%bind () =
       section "blocks are produced"
         (wait_for t (Wait_condition.blocks_to_be_produced 1))
     in
     let%bind () =
-      section "restart node after 2k+1, ie 5, blocks"
-        (let%bind () = Node.stop node_c in
-        let%bind () =
-          let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
-          send_padding_transactions block_producer_nodes ~fee ~logger
-          ~n:36 (* sending 36 txn *)
-        in
-         [%log info] "%s stopped, will now wait for 1 epoch"
-           (Node.id node_c) ;
-        (* let%bind () = wait_for t (Wait_condition.blocks_to_be_produced 5) in *)
-         let%bind () =  Malleable_error.lift (Async.after (Time.Span.of_ms epoch_ms)) in
-        (*
-          Wait for staking epoch ledger
-                let%bind () = wait_for t (Wait_condition.epoch_ledger_being_updated ~ledger:`staking)
-        in
-        *)
-         let%bind () =
-          let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
-          send_padding_transactions block_producer_nodes ~fee ~logger
-          ~n:36 (* sending 36 txn *)
-        in
-         [%log info] "%s waiting 1 epoch to complete"
-           (Node.id node_c) ;
-        let%bind () =  Malleable_error.lift (Async.after (Time.Span.of_ms epoch_ms)) in
-        (*
-          Once Jiawei checks in this code will work
-                let%bind () = wait_for t (Wait_condition.epoch_ledger_being_updated ~ledger:`Next)
-        in
-        *)
-         [%log info] "  epoch completed start the node %s"
-          (Node.id node_c) ;
-         let%bind () = Node.start ~fresh_state:true node_c in
+      section "send out padding transactions"
+        (let fee = Currency.Fee.of_nanomina_int_exn 1_000_000 in
+         send_padding_transactions block_producer_nodes ~fee ~logger
+           ~n:(padding_payments ()) )
+    in
+    let%bind () = section "stop the node" (Node.stop node_c) in
+    let%bind () =
+      section
+        "Wait for one epoch, and make sure the next epoch ledger has been \
+         updated"
+        (let%bind () =
+           Malleable_error.lift (Async.after (Time.Span.of_ms epoch_ms))
+         in
+         wait_for t (Wait_condition.next_epoch_ledger_updated ()) )
+    in
+    let%bind () =
+      section
+        "Wait for another epoch, and make sure that the staking epoch ledger \
+         has been updated"
+        (let%bind () =
+           Malleable_error.lift (Async.after (Time.Span.of_ms epoch_ms))
+         in
+         wait_for t (Wait_condition.staking_epoch_ledger_updated ()) )
+    in
+    let%bind () =
+      section "restart node after 2 epochs"
+        (let%bind () = Node.start ~fresh_state:true node_c in
          [%log info]
            "%s started again, will now wait for this node to initialize"
            (Node.id node_c) ;
---
+++ src/app/test_executive/epoch_ledger_medium_bootstrap.mli
@@ -0,0 +1,1 @@
+module Make : Integration_test_lib.Intf.Test.Functor_intf
