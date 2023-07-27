c56c1cd030337043cee22d70b9a95c547c0567df
georgeee <george.agapov@gmail.com>
2023-07-27 20:59:49 +0200

    Implement usability for skipping local snark work
    1. Make snark workers report their digests when using the GetWork RPC
       to the coordinator
    2. Add a flag to Mina node to enforce local snark work verification

--- src/app/cli/src/cli_entrypoint/mina_cli_entrypoint.ml
+++ src/app/cli/src/cli_entrypoint/mina_cli_entrypoint.ml
@@ -477,6 +477,11 @@ let setup_daemon logger =
          for the associated private key that is being tracked by this daemon. \
          You cannot provide both `uptime-submitter-key` and \
          `uptime-submitter-pubkey`."
+  and verify_local_snark_works =
+    flag "--verify_local_snark_works" no_arg
+      ~doc:
+        "Verify snark works coming from workers connected to this snark \
+         coordinator"
   in
   let to_pubsub_topic_mode_option =
     let open Gossip_net.Libp2p in
@@ -1355,6 +1360,8 @@ Pass one of -peer, -peer-list-file, -seed, -peer-list-url.|} ;
                    }
                  ~snark_coordinator_key:run_snark_coordinator_flag
                  ~snark_pool_disk_location:(conf_dir ^/ "snark_pool")
+                 ~skip_local_snark_work_verification:
+                   (not verify_local_snark_works)
                  ~wallets_disk_location:(conf_dir ^/ "wallets")
                  ~persistent_root_location:(conf_dir ^/ "root")
                  ~persistent_frontier_location:(conf_dir ^/ "frontier")
