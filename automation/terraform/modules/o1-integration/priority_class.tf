78e63e62cfdb35d0ca839f27c64bcabf40c69b2c
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2023-07-31 17:15:31 -0400

    .

--- automation/terraform/modules/o1-integration/priority_class.tf
+++ automation/terraform/modules/o1-integration/priority_class.tf
@@ -1,9 +1,10 @@
 resource "kubernetes_priority_class" "testnet_priority_class" {
   metadata {
-    name = "${var.testnet_name}-nonpreemptible-priority-class"
+    name      = "${var.testnet_name}-nonpreemptible-priority-class"
+    namespace = kubernetes_namespace.testnet_namespace.metadata[0].name
   }
 
-  value = var.pod_priority
+  value             = var.pod_priority
   preemption_policy = "Never"
-  global_default = false
+  global_default    = false
 }
