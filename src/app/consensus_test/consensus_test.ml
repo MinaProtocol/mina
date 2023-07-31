63837084ecd42fff8dce5a4a85faec8dd0f8e638
Martin Minkov <minkovlmartin@gmail.com>
2023-07-31 12:17:31 -0700

    refactor(consensus_test.ml): extract logic into smaller functions for readability and maintainability feat(consensus_test.ml): add logging for better tracking and debugging fix(consensus_test.ml): handle case when candidate block has lower blockchain length than existing block

--- src/app/consensus_test/consensus_test.ml
+++ src/app/consensus_test/consensus_test.ml
@@ -9,6 +9,8 @@ module BlockFileOutput = struct
   [@@deriving to_yojson]
 end
 
+type select_outcome = CandidateLonger | EqualLength | CandidateShorter
+
 module type CONTEXT = sig
   val logger : Logger.t
 
@@ -32,8 +34,6 @@ let context (logger : Logger.t) : (module CONTEXT) =
 
     let constraint_constants = precomputed_values.constraint_constants
   end )
-
-let current_chain : Mina_block.Precomputed.t list ref = ref []
 
 let read_directory dir_name =
   let extract_height_from_filename fname =
@@ -72,10 +72,6 @@ let read_block_file blocks_filename =
       In_channel.close blocks_file ;
       failwithf "File %s is empty" blocks_filename ()
 
-let print_block_info block =
-  Mina_block.Precomputed.to_yojson block
-  |> Yojson.Safe.to_string |> print_endline
-
 let precomputed_block_to_block_file_output (block : Mina_block.Precomputed.t) :
     BlockFileOutput.t =
   let open Yojson.Safe.Util in
@@ -98,9 +94,27 @@ let precomputed_block_to_block_file_output (block : Mina_block.Precomputed.t) :
   ; BlockFileOutput.previous_state_hash = parent_state_hash
   }
 
+let compare_lengths candidate_length existing_length =
+  if candidate_length > existing_length then CandidateLonger
+  else if candidate_length = existing_length then EqualLength
+  else CandidateShorter
+
+let update_chain ~current_chain ~candidate_block ~select_outcome =
+  match select_outcome with
+  | CandidateLonger ->
+      candidate_block :: !current_chain
+  | EqualLength -> (
+      match !current_chain with
+      | _ :: rest_of_list ->
+          candidate_block :: rest_of_list
+      | [] ->
+          !current_chain )
+  | CandidateShorter ->
+      !current_chain
+
 let run_select ~context:(module Context : CONTEXT)
+    (existing_block : Mina_block.Precomputed.t)
     (candidate_block : Mina_block.Precomputed.t) =
-  let existing_block = List.hd_exn !current_chain in
   let existing_consensus_state_with_hashes =
     { With_hash.hash =
         Mina_state.Protocol_state.hashes existing_block.protocol_state
@@ -132,31 +146,46 @@ let run_select ~context:(module Context : CONTEXT)
         |> Consensus.Data.Consensus_state.blockchain_length
         |> Unsigned.UInt32.to_int
       in
-      if candidate_length > existing_length then
-        current_chain := candidate_block :: !current_chain
-      else if candidate_length = existing_length then
-        match !current_chain with
-        | _ :: rest_of_list ->
-            current_chain := candidate_block :: rest_of_list
-        | [] ->
-            failwith "current_chain is empty"
-      else
-        failwith
-          "Candidate block has lower blockchain length than existing block" ;
-      return ()
+      compare_lengths candidate_length existing_length
   | `Keep ->
-      return ()
+      CandidateShorter
 
-let process_precomputed_blocks ~context blocks =
+let process_precomputed_blocks ~context ~current_chain blocks =
   let%bind () =
-    Deferred.List.iter blocks ~f:(fun block ->
-        let%bind () = run_select ~context block in
+    Deferred.List.iter blocks ~f:(fun candidate_block ->
+        let existing_block = List.hd_exn !current_chain in
+        let select_outcome =
+          run_select ~context existing_block candidate_block
+        in
+        current_chain :=
+          update_chain ~current_chain ~candidate_block ~select_outcome ;
+        return () )
+  in
+  return ()
+
+let write_blocks_to_output_dir ~current_chain ~output_dir =
+  let sorted_output =
+    List.map ~f:precomputed_block_to_block_file_output !current_chain
+    |> List.rev
+  in
+  let write_block_to_file (i : int) (block : BlockFileOutput.t) :
+      unit Deferred.t =
+    let block_json_str =
+      block |> BlockFileOutput.to_yojson |> Yojson.Safe.to_string
+    in
+    let output_file = sprintf "%s/block_%d.json" output_dir i in
+    Writer.save output_file ~contents:block_json_str
+  in
+  let%bind () =
+    Deferred.List.iteri sorted_output ~f:(fun i block ->
+        let%bind () = write_block_to_file i block in
         return () )
   in
   return ()
 
 let main () ~blocks_dir ~output_dir =
   let logger = Logger.create () in
+  let current_chain : Mina_block.Precomputed.t list ref = ref [] in
 
   [%log info] "Starting to read blocks dir"
     ~metadata:[ ("blocks_dir", `String blocks_dir) ] ;
@@ -172,24 +201,18 @@ let main () ~blocks_dir ~output_dir =
   | [] ->
       failwith "No blocks found"
   | first_block :: precomputed_blocks ->
+      [%log info] "Starting to process blocks"
+        ~metadata:[ ("num_blocks", `Int (List.length precomputed_blocks)) ] ;
       current_chain := [ first_block ] ;
-      let%bind () = process_precomputed_blocks ~context precomputed_blocks in
-      let file_output =
-        List.map ~f:precomputed_block_to_block_file_output !current_chain
-        |> List.rev
-      in
-      let write_block_to_file (i : int) (block : BlockFileOutput.t) :
-          unit Deferred.t =
-        let block_json = BlockFileOutput.to_yojson block in
-        let block_json_str = Yojson.Safe.to_string block_json in
-        let file_name = sprintf "%s/block_%d.json" output_dir i in
-        Writer.save file_name ~contents:block_json_str
-      in
       let%bind () =
-        Deferred.List.iteri file_output ~f:(fun i block ->
-            let%bind () = write_block_to_file i block in
-            return () )
+        process_precomputed_blocks ~context ~current_chain precomputed_blocks
       in
+      [%log info] "Finished processing blocks" ;
+
+      [%log info] "Starting to write blocks to output dir"
+        ~metadata:[ ("output_dir", `String output_dir) ] ;
+      let%bind () = write_blocks_to_output_dir ~current_chain ~output_dir in
+      [%log info] "Finished writing blocks to output dir" ;
       return ()
 
 let () =
