7e55c57549d19d1a1524c6ddc44a2e4ff4d8bce8
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2023-07-31 18:44:13 -0400

    undo of priority class namespace

--- automation/terraform/modules/o1-integration/priority_class.tf
+++ automation/terraform/modules/o1-integration/priority_class.tf
@@ -1,10 +1,6 @@
 resource "kubernetes_priority_class" "testnet_priority_class" {
-  depends_on = [
-    module.kubernetes_testnet.testnet_namespace
-  ]
   metadata {
-    name      = "${var.testnet_name}-nonpreemptible-priority-class"
-    namespace = module.kubernetes_testnet.testnet_namespace.metadata[0].name
+    name = "${var.testnet_name}-nonpreemptible-priority-class"
   }
 
   value             = var.pod_priority
