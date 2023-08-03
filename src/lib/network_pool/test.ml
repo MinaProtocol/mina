6b6accc99824e1ac719fa93c577701e9b3ae1284
georgeee <george.agapov@gmail.com>
2023-08-03 10:21:30 +0200

    Remove --verify-local-snark-works flag

--- src/lib/network_pool/test.ml
+++ src/lib/network_pool/test.ml
@@ -36,7 +36,6 @@ let%test_module "network pool test" =
     let config =
       Mock_snark_pool.Resource_pool.make_config ~verifier ~trust_system
         ~disk_location:"/tmp/snark-pool"
-        ~skip_local_snark_work_verification:true
 
     let%test_unit "Work that gets fed into apply_and_broadcast will be \
                    received in the pool's reader" =
