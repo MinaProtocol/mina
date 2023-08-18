a3c2cdda2d1c81764439db2e369b595124867e54
Gregor <gregor.mitscha-baude@gmx.at>
2023-08-18 02:44:43 +0200

    simplify pickles dummy changes

--- src/lib/pickles/unfinalized.ml
+++ src/lib/pickles/unfinalized.ml
@@ -69,8 +69,7 @@ module Constant = struct
          }
        in
        let evals =
-         Plonk_types.Evals.to_in_circuit
-           (Lazy.force Dummy.evals_combined).evals.evals
+         Plonk_types.Evals.to_in_circuit Dummy.(!evals_combined.evals.evals)
        in
        let env =
          let module Env_bool = struct
@@ -139,7 +138,7 @@ module Constant = struct
                }
            ; combined_inner_product = Shifted_value (tock ())
            ; xi = Scalar_challenge.create one_chal
-           ; bulletproof_challenges = Lazy.force Dummy.Ipa.Wrap.challenges
+           ; bulletproof_challenges = Dummy.(!Ipa.Wrap.challenges)
            ; b = Shifted_value (tock ())
            }
        ; should_finalize = false
