35d292a9cd2b7d15b449176a27efbab7b6707a5a
Danny Willems <be.danny.willems@gmail.com>
2023-08-24 22:07:53 +0200

    Add a test for multiple runtime lookups

--- src/lib/crypto/kimchi_backend/common/tests/test_lookup_table_constraint_kind.ml
+++ src/lib/crypto/kimchi_backend/common/tests/test_lookup_table_constraint_kind.ml
@@ -106,6 +106,70 @@ let test_compute_witness_returns_correctly_filled_runtime_tables_one_lookup () =
   let exp_rt = Array.init n (fun i -> if i = idx then v else Tick.Field.zero) in
   assert (Array.for_all2 Tick.Field.equal rt.data exp_rt)
 
+let test_compute_witness_returns_correctly_filled_runtime_tables_multiple_lookup
+    () =
+  let module Tick = Kimchi_backend.Pasta.Vesta_based_plonk in
+  let module Impl = Snarky_backendless.Snark.Run.Make (Tick) in
+  (* We have one table with ID 0, indexed from 0 to n, and we will fill with
+     some values using the constraint RuntimeLookup.
+     We start with one lookup
+  *)
+  let n = 10 in
+  let indexed_runtime_table_cfg = Array.init n Tick.Field.of_int in
+  let table_id = 0 in
+  let table_id_var = Snarky_backendless.Cvar.Var 0 in
+  let cs = Tick.R1CS_constraint_system.create () in
+  (* Config *)
+  Tick.R1CS_constraint_system.(
+    add_constraint cs
+      (T
+         (AddRuntimeTableCfg
+            { id = Int32.of_int table_id
+            ; first_column = indexed_runtime_table_cfg
+            } ) )) ;
+  (* nb of lookups *)
+  let m = Random.int n in
+  let exp_rt_data = Array.init n (fun _ -> Tick.Field.zero) in
+  (* For the external values to give to the compute witness fn *)
+  let ftable_id = Tick.Field.of_int table_id in
+  let external_values = Array.init (1 + (m * 2)) (fun _ -> Tick.Field.zero) in
+  external_values.(0) <- ftable_id ;
+  let _ =
+    List.init m (fun i ->
+        let j = (2 * i) + 1 in
+        let idx_var = Snarky_backendless.Cvar.Var j in
+        let val_var = Snarky_backendless.Cvar.Var (j + 1) in
+        (* One lookup *)
+        let _ =
+          Tick.R1CS_constraint_system.(
+            add_constraint cs
+              (T
+                 (RuntimeLookup
+                    { table_id = table_id_var; idx = idx_var; v = val_var } ) ))
+        in
+        (* Random value for the lookup *)
+        let idx = Random.int n in
+        let v = Tick.Field.random () in
+        external_values.(j) <- Tick.Field.of_int idx ;
+        external_values.(j + 1) <- v ;
+        exp_rt_data.(idx) <- v ;
+        (idx_var, val_var) )
+  in
+  let nb_aux = (2 * m) + 1 in
+  let () = Tick.R1CS_constraint_system.set_primary_input_size cs 0 in
+  let () = Tick.R1CS_constraint_system.set_auxiliary_input_size cs nb_aux in
+
+  let _ = Tick.R1CS_constraint_system.finalize cs in
+  let _witnesses, runtime_tables =
+    Tick.R1CS_constraint_system.compute_witness cs (Array.get external_values)
+  in
+  (* checking only one table has been created *)
+  assert (Array.length runtime_tables = 1) ;
+  let rt = runtime_tables.(0) in
+  (* with the correct ID *)
+  assert (Int32.(equal rt.id (of_int table_id))) ;
+  assert (Array.for_all2 Tick.Field.equal rt.data exp_rt_data)
+
 let () =
   let open Alcotest in
   run "Test constraint construction"
@@ -116,5 +180,7 @@ let () =
             test_finalize_and_get_gates_with_runtime_table_cfg
         ; test_case "Compute witness with one runtime table lookup" `Quick
             test_compute_witness_returns_correctly_filled_runtime_tables_one_lookup
+        ; test_case "Compute witness with multiple runtime table lookup" `Quick
+            test_compute_witness_returns_correctly_filled_runtime_tables_multiple_lookup
         ] )
     ]
