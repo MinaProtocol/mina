d297ec709c2a47269e175d4530b56f77fe061d31
georgeee <george.agapov@gmail.com>
2023-08-28 13:55:39 +0200

    Minor: address review comments

--- src/lib/mina_graphql/doc.ml
+++ src/lib/mina_graphql/doc.ml
@@ -5,5 +5,3 @@ let date ?(extra = "") s =
     "%s (stringified Unix time - number of milliseconds since January 1, \
      1970)%s"
     s extra
-
-let bin_prot = sprintf "%s (base58-encoded janestreet/bin_prot serialization)"
