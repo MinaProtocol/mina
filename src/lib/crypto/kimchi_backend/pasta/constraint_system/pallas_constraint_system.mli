3c1a7ac0c1bf1554e180399119ab11c45e3cda4d
Danny Willems <be.danny.willems@gmail.com>
2023-08-30 15:41:11 +0200

    Other solution than the previous one, keeping abstract the type in the interface

--- src/lib/crypto/kimchi_backend/pasta/constraint_system/pallas_constraint_system.mli
+++ src/lib/crypto/kimchi_backend/pasta/constraint_system/pallas_constraint_system.mli
@@ -2,3 +2,7 @@ include
   Intf.Full
     with type fp := Kimchi_pasta_basic.Fq.t
      and type gates := Kimchi_bindings.Protocol.Gates.Vector.Fq.t
+     and type t =
+      ( Kimchi_pasta_basic.Fq.t
+      , Kimchi_bindings.Protocol.Gates.Vector.Fq.t )
+      Kimchi_backend_common.Plonk_constraint_system.t
