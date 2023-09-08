b108ac4b07cd5fdc10dbe02d16d9415fde3bd046
Richard Bonichon <richard.bonichon@gmail.com>
2023-09-08 16:04:33 +0200

    Composition_types: rm unused arguments in typ

--- src/lib/pickles/composition_types/composition_types.mli
+++ src/lib/pickles/composition_types/composition_types.mli
@@ -1123,8 +1123,6 @@ module Step : sig
 
           val typ :
                'f Spec.impl
-            -> dummy_scalar:'a
-            -> dummy_scalar_challenge:'b Scalar_challenge.t
             -> challenge:
                  ( 'c
                  , 'd
@@ -1134,14 +1132,6 @@ module Step : sig
                    Snarky_backendless.Checked_runner.Simple.Types.Checked.t )
                  Snarky_backendless.Types.Typ.t
             -> scalar_challenge:('e, 'b, 'f) Snarky_backendless.Typ.t
-            -> bool:
-                 ( ('f Snarky_backendless.Cvar.t Snarky_backendless.Boolean.t
-                    as
-                    'boolean )
-                 , bool
-                 , 'f )
-                 Snarky_backendless.Typ.t
-            -> feature_flags:Opt.Flag.t Plonk_types.Features.t
             -> ('fp, 'a, 'f) Snarky_backendless.Typ.t
             -> ( ('c, 'e Scalar_challenge.t, 'fp) t
                , ('d, 'b Scalar_challenge.t, 'a) t
