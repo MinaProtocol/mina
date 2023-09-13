3dc8805e1691eea075d0f3affd43f8392ec17ac5
dkijania <dariusz@o1labs.org>
2023-09-13 09:22:13 +0200

    Revert "Auxiliary commit to revert individual files from 4ce1c53c0701c9b9c650ad97c18c00ea1bf5bed1"
    This reverts commit f77d1eef445785cf15b1984948827761858a4805.

--- src/lib/mina_networking/mina_networking.ml
+++ src/lib/mina_networking/mina_networking.ml
@@ -732,7 +732,7 @@ module Rpcs = struct
             ; k_block_hashes_and_timestamps :
                 (State_hash.Stable.V1.t * string) list
             ; git_commit : string
-            ; uptime_minutes : int64
+            ; uptime_minutes : int
             }
           [@@deriving to_yojson, of_yojson]
 
@@ -747,7 +747,7 @@ module Rpcs = struct
             ; k_block_hashes_and_timestamps =
                 status.k_block_hashes_and_timestamps
             ; git_commit = status.git_commit
-            ; uptime_minutes = 0
+            ; uptime_minutes = status.uptime_minutes
             ; block_height_opt = None
             }
         end
@@ -846,7 +846,7 @@ module Rpcs = struct
                 ; k_block_hashes_and_timestamps =
                     status.k_block_hashes_and_timestamps
                 ; git_commit = status.git_commit
-                ; uptime_minutes = Int64.zero
+                ; uptime_minutes = status.uptime_minutes
                 }
 
         let caller_model_of_response = function
