4370d48b4d591de54b6d5b9daaed59a3714d1f53
Sventimir <svantimir@gmail.com>
2023-09-15 09:53:30 +0200

    [buildkite] merges-cleanly.sh displays filenames.
    "Merges cleanly into x" jobs will now nicely print names of files
    where conflicts occur.

--- buildkite/scripts/merges-cleanly.sh
+++ buildkite/scripts/merges-cleanly.sh
@@ -8,21 +8,22 @@ echo 'Testing for conflicts between the current branch `'"${CURRENT}"'` and `'"$
 # The git merge-tree command shows the content of a 3-way merge without
 # touching the index, which we can then search for conflict markers.
 
-# Tell git where to find ssl certs
-git config --global http.sslCAInfo /etc/ssl/certs/ca-bundle.crt
 # Fetch a fresh copy of the repo
+git config http.sslVerify false
 git fetch origin
+git config http.sslVerify true
 # Check mergeability
-git merge-tree `git merge-base origin/$BRANCH HEAD` HEAD origin/$BRANCH | grep -A 25 "^+<<<<<<<"
+git merge-tree `git merge-base origin/$BRANCH HEAD` HEAD origin/$BRANCH \
+    | awk -f buildkite/scripts/merges-cleanly.awk
 
 RET=$?
 
 if [ $RET -eq 0 ]; then
+  echo "No conflicts found against upstream branch ${BRANCH}"
+  exit 0
+else
   # Found a conflict
   echo "[ERROR] This pull request conflicts with $BRANCH, please open a new pull request against $BRANCH at this link:"
   echo "https://github.com/MinaProtocol/mina/compare/${BRANCH}...${BUILDKITE_BRANCH}"
-  exit 1
-else
-  echo "No conflicts found against upstream branch ${BRANCH}"
-  exit 0
+  exit $RET
 fi
