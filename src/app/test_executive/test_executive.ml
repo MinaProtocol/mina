457ba2dfac5c8cf0c599556a5bf3e82c19babe8b
mrmr1993 <mr_1993@hotmail.co.uk>
2023-09-19 03:23:03 +0100

    Remove merge error

--- src/app/test_executive/test_executive.ml
+++ src/app/test_executive/test_executive.ml
@@ -55,7 +55,6 @@ let tests : test list =
   ; ( "chain-reliability"
     , (module Chain_reliability_test.Make : Intf.Test.Functor_intf) )
   ; ("payments", (module Payments_test.Make : Intf.Test.Functor_intf))
-  ; ("archive-node", (module Archive_node_test.Make : Intf.Test.Functor_intf))
   ; ("gossip-consis", (module Gossip_consistency.Make : Intf.Test.Functor_intf))
   ; ("medium-bootstrap", (module Medium_bootstrap.Make : Intf.Test.Functor_intf))
   ; ("zkapps", (module Zkapps.Make : Intf.Test.Functor_intf))
