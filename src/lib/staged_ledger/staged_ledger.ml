121dc2e092f03ba967bbbe974e32d11c01a49e59
Tang Jiawei <tang@o1labs.org>
2023-09-20 02:42:08 +0800

    tidy things up

--- src/lib/staged_ledger/staged_ledger.ml
+++ src/lib/staged_ledger/staged_ledger.ml
@@ -1,7 +1,7 @@
 [%%import "/src/config.mlh"]
 
 (* Only show stdout for failed inline tests. *)
-
+open Inline_test_quiet_logs
 open Core_kernel
 open Async
 open Mina_base
@@ -2163,7 +2163,7 @@ let%test_module "staged ledger tests" =
     let constraint_constants =
       Genesis_constants.Constraint_constants.for_unit_tests
 
-    let logger = Logger.create ()
+    let logger = Logger.null ()
 
     let `VK vk, `Prover zkapp_prover =
       Transaction_snark.For_tests.create_trivial_snapp ~constraint_constants ()
@@ -4254,170 +4254,184 @@ let%test_module "staged ledger tests" =
         let open Quickcheck.Generator.Let_syntax in
         let%bind spec_keypair_and_slot = gen_spec_keypair_and_global_slot in
         let%map signed_commands_or_zkapps =
-        (*
+          (*
           return [false;true;false;true;true]
         *)
           List.gen_with_length 5 Bool.quickcheck_generator
-
         in
 
         (spec_keypair_and_slot, signed_commands_or_zkapps |> List.to_array)
       in
-      Async.Thread_safe.block_on_async_exn @@ fun () ->
-          Async.Quickcheck.async_test ~trials:10 gen
-            ~f:(fun
-                 ( ({ init_ledger; _ }, new_kp, global_slot)
-                 , signed_commands_or_zkapps )
-               ->
-              let open Transaction_snark.For_tests in
-              [%log info]
-                "signed commands or zkapps"
-                ~metadata:[
-                  ("signed_commands_or_zkapps", `List (List.map (Array.to_list signed_commands_or_zkapps) ~f:(fun b -> `Bool b)))
-                ] ;
-              let fee = Fee.of_nanomina_int_exn 2_000_000 in
-              let amount = Amount.of_mina_int_exn 2 in
-              let mk_signed_command ~(fee_payer : Keypair.t)
-                  ~(receiver : Keypair.t) ~(nonce : Account.Nonce.t) =
-                let body =
-                  Signed_command_payload.Body.Payment
-                    Payment_payload.Poly.
-                      { receiver_pk = Public_key.compress receiver.public_key
-                      ; amount
-                      }
-                in
-                let payload =
-                  Signed_command.Payload.create ~fee
-                    ~fee_payer_pk:Public_key.(compress fee_payer.public_key)
-                    ~nonce ~memo:Signed_command_memo.dummy ~valid_until:None
-                    ~body
-                in
-                User_command.Signed_command
-                  (Signed_command.sign fee_payer payload)
-              in
-              let zkapp_pk = Public_key.compress new_kp.public_key in
-              let ledger =
-                Ledger.create ~depth:constraint_constants.ledger_depth ()
-              in
-              Mina_transaction_logic.For_tests.Init_ledger.init
-                (module Ledger.Ledger_inner)
-                init_ledger ledger ;
-              Transaction_snark.For_tests.create_trivial_zkapp_account
-                ~permissions:Permissions.user_default ~vk ~ledger zkapp_pk ;
-              let sl = Sl.create_exn ~constraint_constants ~ledger in
-              let mk_zkapp_command ~(fee_payer : Keypair.t)
-                  ~(nonce : Account.Nonce.t) =
-                let spec : Update_states_spec.t =
-                  { sender = (new_kp, Account.Nonce.zero)
-                  ; fee
-                  ; fee_payer = Some (fee_payer, nonce)
-                  ; receivers = []
+      Async.Thread_safe.block_on_async_exn
+      @@ fun () ->
+      Async.Quickcheck.async_test ~trials:20 gen
+        ~f:(fun
+             ( ({ init_ledger; _ }, new_kp, global_slot)
+             , signed_commands_or_zkapps )
+           ->
+          let open Transaction_snark.For_tests in
+          [%log info] "signed commands or zkapps"
+            ~metadata:
+              [ ( "signed_commands_or_zkapps"
+                , `List
+                    (List.map (Array.to_list signed_commands_or_zkapps)
+                       ~f:(fun b -> `Bool b) ) )
+              ] ;
+          let fee = Fee.of_nanomina_int_exn 2_000_000 in
+          let amount = Amount.of_mina_int_exn 2 in
+          let mk_signed_command ~(fee_payer : Keypair.t) ~(receiver : Keypair.t)
+              ~(nonce : Account.Nonce.t) =
+            let body =
+              Signed_command_payload.Body.Payment
+                Payment_payload.Poly.
+                  { receiver_pk = Public_key.compress receiver.public_key
                   ; amount
-                  ; zkapp_account_keypairs = [ new_kp ]
-                  ; memo = Signed_command_memo.dummy
-                  ; new_zkapp_account = false
-                  ; snapp_update =
-                      { Account_update.Update.dummy with
-                        delegate = Zkapp_basic.Set_or_keep.Set zkapp_pk
-                      }
-                  ; current_auth = Permissions.Auth_required.Signature
-                  ; call_data = Snark_params.Tick.Field.zero
-                  ; events = []
-                  ; actions = []
-                  ; preconditions = None
                   }
-                in
-                let%map zkapp_command =
-                  Transaction_snark.For_tests.update_states
-                    ~zkapp_prover_and_vk:(zkapp_prover, vk)
-                    ~constraint_constants spec
-                in
-                let valid_zkapp_command =
-                  Zkapp_command.Valid.to_valid ~status:Applied
-                    ~find_vk:
-                      (Zkapp_command.Verifiable.find_vk_via_ledger ~ledger
-                         ~get:Ledger.get
-                         ~location_of_account:Ledger.location_of_account )
-                    zkapp_command
-                  |> Or_error.ok_exn
-                in
-                User_command.Zkapp_command valid_zkapp_command
+            in
+            let payload =
+              Signed_command.Payload.create ~fee
+                ~fee_payer_pk:Public_key.(compress fee_payer.public_key)
+                ~nonce ~memo:Signed_command_memo.dummy ~valid_until:None ~body
+            in
+            User_command.Signed_command (Signed_command.sign fee_payer payload)
+          in
+          let zkapp_pk = Public_key.compress new_kp.public_key in
+          let ledger =
+            Ledger.create ~depth:constraint_constants.ledger_depth ()
+          in
+          Mina_transaction_logic.For_tests.Init_ledger.init
+            (module Ledger.Ledger_inner)
+            init_ledger ledger ;
+          Transaction_snark.For_tests.create_trivial_zkapp_account
+            ~permissions:Permissions.user_default ~vk ~ledger zkapp_pk ;
+          let sl = Sl.create_exn ~constraint_constants ~ledger in
+          let mk_zkapp_command ~(fee_payer : Keypair.t)
+              ~(nonce : Account.Nonce.t) =
+            let spec : Update_states_spec.t =
+              { sender = (new_kp, Account.Nonce.zero)
+              ; fee
+              ; fee_payer = Some (fee_payer, nonce)
+              ; receivers = []
+              ; amount
+              ; zkapp_account_keypairs = [ new_kp ]
+              ; memo = Signed_command_memo.dummy
+              ; new_zkapp_account = false
+              ; snapp_update =
+                  { Account_update.Update.dummy with
+                    delegate = Zkapp_basic.Set_or_keep.Set zkapp_pk
+                  }
+              ; current_auth = Permissions.Auth_required.Signature
+              ; call_data = Snark_params.Tick.Field.zero
+              ; events = []
+              ; actions = []
+              ; preconditions = None
+              }
+            in
+            let%map zkapp_command =
+              Transaction_snark.For_tests.update_states
+                ~zkapp_prover_and_vk:(zkapp_prover, vk) ~constraint_constants
+                spec
+            in
+            let valid_zkapp_command =
+              Zkapp_command.Valid.to_valid ~status:Applied
+                ~find_vk:
+                  (Zkapp_command.Verifiable.find_vk_via_ledger ~ledger
+                     ~get:Ledger.get
+                     ~location_of_account:Ledger.location_of_account )
+                zkapp_command
+              |> Or_error.ok_exn
+            in
+            User_command.Zkapp_command valid_zkapp_command
+          in
+          let mk_user_command ~signed_command_or_zkapp ~fee_payer ~receiver
+              ~nonce =
+            match signed_command_or_zkapp with
+            | true ->
+                return @@ mk_signed_command ~fee_payer ~receiver ~nonce
+            | false ->
+                mk_zkapp_command ~fee_payer ~nonce
+          in
+          let fee_payer, _ = init_ledger.(0) in
+          let receiver, _ = init_ledger.(1) in
+          let%bind valid_command_1 =
+            mk_user_command
+              ~signed_command_or_zkapp:signed_commands_or_zkapps.(0)
+              ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 0)
+          and valid_command_2 =
+            mk_user_command
+              ~signed_command_or_zkapp:signed_commands_or_zkapps.(1)
+              ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 1)
+          and invalid_command_3 =
+            mk_user_command
+              ~signed_command_or_zkapp:signed_commands_or_zkapps.(2)
+              ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 99)
+          and valid_command_4 =
+            mk_user_command
+              ~signed_command_or_zkapp:signed_commands_or_zkapps.(3)
+              ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 2)
+          and invalid_command_5 =
+            mk_user_command
+              ~signed_command_or_zkapp:signed_commands_or_zkapps.(4)
+              ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 98)
+          in
+          let global_slot =
+            Mina_numbers.Global_slot_since_genesis.of_int global_slot
+          in
+          let current_state, current_state_view =
+            dummy_state_and_view ~global_slot ()
+          in
+          let state_and_body_hash =
+            let state_hashes = Mina_state.Protocol_state.hashes current_state in
+            ( state_hashes.state_hash
+            , state_hashes.state_body_hash |> Option.value_exn )
+          in
+          match
+            Sl.create_diff ~constraint_constants ~global_slot sl ~logger
+              ~current_state_view
+              ~transactions_by_fee:
+                (Sequence.of_list
+                   [ valid_command_1
+                   ; valid_command_2
+                   ; invalid_command_3
+                   ; valid_command_4
+                   ; invalid_command_5
+                   ] )
+              ~get_completed_work:(stmt_to_work_zero_fee ~prover:self_pk)
+              ~coinbase_receiver ~supercharge_coinbase:false
+          with
+          | Error e ->
+              Error.raise (Pre_diff_info.Error.to_error e)
+          | Ok (diff, invalid_txns) -> (
+              let valid_commands =
+                Staged_ledger_diff.With_valid_signatures_and_proofs.commands
+                  diff
+                |> List.map ~f:(fun { data; _ } ->
+                       User_command.forget_check data )
               in
-              let mk_user_command ~signed_command_or_zkapp ~fee_payer ~receiver
-                  ~nonce =
-                match signed_command_or_zkapp with
-                | true ->
-                    return @@ mk_signed_command ~fee_payer ~receiver ~nonce
-                | false ->
-                    mk_zkapp_command ~fee_payer ~nonce
+              assert (
+                List.equal User_command.equal valid_commands
+                  ( [ valid_command_1; valid_command_2; valid_command_4 ]
+                  |> List.map ~f:(fun cmd -> User_command.forget_check cmd) ) ) ;
+              let invalid_commands =
+                List.map invalid_txns ~f:(fun (cmd, _) ->
+                    User_command.forget_check cmd )
               in
-              let fee_payer, _ = init_ledger.(0) in
-              let receiver, _ = init_ledger.(1) in
-              let%bind valid_command_1 =
-                mk_user_command
-                  ~signed_command_or_zkapp:signed_commands_or_zkapps.(0)
-                  ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 0)
-              and valid_command_2 =
-                mk_user_command
-                  ~signed_command_or_zkapp:signed_commands_or_zkapps.(1)
-                  ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 1)
-              and invalid_command_3 =
-                mk_user_command
-                  ~signed_command_or_zkapp:signed_commands_or_zkapps.(2)
-                  ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 99)
-              and valid_command_4 =
-                mk_user_command
-                  ~signed_command_or_zkapp:signed_commands_or_zkapps.(3)
-                  ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 2)
-              and invalid_command_5 =
-                mk_user_command
-                  ~signed_command_or_zkapp:signed_commands_or_zkapps.(4)
-                  ~fee_payer ~receiver ~nonce:(Account.Nonce.of_int 98)
-              in
-              let global_slot =
-                Mina_numbers.Global_slot_since_genesis.of_int global_slot
-              in
-              let current_state, current_state_view =
-                dummy_state_and_view ~global_slot ()
-              in
-              let _state_and_body_hash =
-                let state_hashes =
-                  Mina_state.Protocol_state.hashes current_state
-                in
-                ( state_hashes.state_hash
-                , state_hashes.state_body_hash |> Option.value_exn )
-              in
-              match
-                Sl.create_diff ~constraint_constants ~global_slot sl ~logger
-                  ~current_state_view
-                  ~transactions_by_fee:
-                    (Sequence.of_list
-                       [ valid_command_1
-                       ; valid_command_2
-                       ; invalid_command_3
-                       ; valid_command_4
-                       ; invalid_command_5
-                       ] )
-                  ~get_completed_work:(stmt_to_work_zero_fee ~prover:self_pk)
+              assert (
+                List.equal User_command.equal invalid_commands
+                  ( [ invalid_command_3; invalid_command_5 ]
+                  |> List.map ~f:(fun cmd -> User_command.forget_check cmd) ) ) ;
+
+              match%map
+                Sl.apply ~constraint_constants ~global_slot sl
+                  (Staged_ledger_diff.forget diff)
+                  ~logger ~verifier ~current_state_view ~state_and_body_hash
                   ~coinbase_receiver ~supercharge_coinbase:false
               with
+              | Ok _x ->
+                  ()
               | Error e ->
-                  Error.raise (Pre_diff_info.Error.to_error e)
-              | Ok (diff, invalid_txns) ->
-                  let valid_commands = Staged_ledger_diff.With_valid_signatures_and_proofs.commands diff in
-                  assert (
-                    List.length valid_commands
-                    =  3) ;
-                  assert (List.length invalid_txns = 2) ;
-                  Deferred.unit
-                  (*
-                  match%map Sl.apply ~constraint_constants ~global_slot sl (Staged_ledger_diff.forget diff) 
-                  ~logger ~verifier ~current_state_view ~state_and_body_hash ~coinbase_receiver ~supercharge_coinbase:false 
-              with 
-              | Ok _x -> () 
-              | Error e ->
-                [%log info] "Error %s" (Staged_ledger_error.to_string e) ; ()
-                 *) )
+                  [%log info] "Error %s" (Staged_ledger_error.to_string e) ;
+                  () ) )
 
     let%test_unit "Mismatched verification keys in zkApp accounts and \
                    transactions" =
