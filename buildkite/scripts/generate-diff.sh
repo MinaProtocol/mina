d8b0a3149fd61b246659ab162e259ba1b5a74e34
Nathan Holland <nholland94@gmail.com>
2023-09-19 20:58:27 -0600

    Print out the tag when we treat everything as dirty

--- buildkite/scripts/generate-diff.sh
+++ buildkite/scripts/generate-diff.sh
@@ -3,11 +3,11 @@
 TAG=$(git tag --points-at HEAD)
 
 # If this is not a PR build, or the HEAD is tagged, the entire build is dirty
-if [ -z ${BUILDKITE_PULL_REQUEST_BASE_BRANCH} ]; then
+if [ -z "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}" ]; then
   echo "This is not a PR build; considering all files dirty" >&2
   git ls-files
-elif [ -n ${TAG} ]; then
-  echo "This commit has been tagged; considering all files dirty" >&2
+elif [ -n "${TAG}" ]; then
+  echo "This commit has been tagged (${TAG}); considering all files dirty" >&2
   git ls-files
 else
   COMMIT=$(git log -1 --pretty=format:%H)
