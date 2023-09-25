b8475168552c4a84eb3c779990eb3400bb7dfe40
georgeee <george.agapov@gmail.com>
2023-09-25 08:30:40 +0200

    Revert "Merge pull request #14085 from MinaProtocol/libp2p-upgrade"
    This reverts commit 152303f340a0156027babdc4c7bace2d6874fe12, reversing
    changes made to 44b1f39a5ce98d6ac4eff123212fdae364449bd5.

--- src/lib/mina_net2/tests/all_ipc.ml
+++ src/lib/mina_net2/tests/all_ipc.ml
@@ -135,9 +135,6 @@ let%test_module "all-ipc test" =
         | Connected, false, true ->
             status := Disconnected ;
             true
-        | Disconnected, true, true ->
-            status := Connected ;
-            true
         | _, _, _ ->
             false
       in
