1c7749f580c005d2f857755c02786c471dd87486
Danny Willems <be.danny.willems@gmail.com>
2023-09-25 22:10:05 +0200

    Pickles/type: use doc-comments

--- src/lib/pickles/type.mli
+++ src/lib/pickles/type.mli
@@ -1,18 +1,15 @@
 type (_, _) t =
-  (* Polynomial commitment *)
-  | PC : ('g1, < g1 : 'g1 ; .. >) t
-  (* Scalar, used for challenges for instance *)
+  | PC : ('g1, < g1 : 'g1 ; .. >) t  (** Polynomial commitment *)
   | Scalar : ('s, < scalar : 's ; .. >) t
-  (* Polynomials *)
+      (** Scalar, used for challenges for instance *)
   | Without_degree_bound
       : ( 'g1 Pickles_types.Plonk_types.Poly_comm.Without_degree_bound.t
         , < g1 : 'g1 ; .. > )
-        t
+        t  (** Polynomials *)
   | With_degree_bound
       : ( 'g1_opt Pickles_types.Plonk_types.Poly_comm.With_degree_bound.t
         , < g1_opt : 'g1_opt ; .. > )
-        t
-  (* Field element *)
-  | Field : ('field, < base_field : 'field ; .. >) t
-  (* Concatenate two elements of this type as a list *)
+        t  (** Polynomial *)
+  | Field : ('field, < base_field : 'field ; .. >) t  (** Field element *)
   | ( :: ) : ('a, 'e) t * ('b, 'e) t -> ('a * 'b, 'e) t
+      (** Concatenate two elements of this type as a list *)
