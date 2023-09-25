6ae6b983d30604a9c74e6768ca03a72ada01394e
Danny Willems <be.danny.willems@gmail.com>
2023-09-25 21:13:29 +0200

    Update src/lib/pickles/tock_field_sponge.mli
    Co-authored-by: Richard Bonichon <richard.bonichon@o1labs.org>

--- src/lib/pickles/tock_field_sponge.mli
+++ src/lib/pickles/tock_field_sponge.mli
@@ -4,5 +4,5 @@ include module type of Make_sponge.Make (Backend.Tock.Field)
 
 (** Parameters for the permutation. It can be generated using the {{
     https://github.com/o1-labs/proof-systems/tree/master/poseidon } SAGE
-    script} *)
+    script}. *)
 val params : Backend.Tock.Field.t Sponge.Params.t
