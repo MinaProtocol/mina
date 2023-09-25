04e2a0fe74af97c10709fa9c5b09c966357cebde
Danny Willems <be.danny.willems@gmail.com>
2023-09-25 21:09:01 +0200

    Update src/lib/pickles/pseudo/pseudo.mli
    Co-authored-by: Richard Bonichon <richard.bonichon@o1labs.org>

--- src/lib/pickles/pseudo/pseudo.mli
+++ src/lib/pickles/pseudo/pseudo.mli
@@ -1,6 +1,6 @@
 (** Encode a mask on a vector with a one-hot vector. Can be used to select at
     compile time a bit of a vector. Users include the step/wrap circuits to select
-    the correct verification key *)
+    the correct verification key. *)
 
 module Make (Impl : Snarky_backendless.Snark_intf.Run) : sig
   (** The type parameter ['n] is the size of the vector, and ['a] is the domains
