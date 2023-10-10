1e873392457ee7f941825d38de384f46925c2325
mrmr1993 <mr_1993@hotmail.co.uk>
2023-10-10 07:06:28 +0100

    Set the max_poly_size for the constraint system

--- src/lib/crypto/kimchi_bindings/stubs/src/pasta_fp_plonk_index.rs
+++ src/lib/crypto/kimchi_bindings/stubs/src/pasta_fp_plonk_index.rs
@@ -9,7 +9,7 @@ use kimchi::circuits::{constraints::ConstraintSystem, gate::CircuitGate};
 use kimchi::{linearization::expr_linearization, prover_index::ProverIndex};
 use mina_curves::pasta::{Fp, Pallas, Vesta, VestaParameters};
 use mina_poseidon::{constants::PlonkSpongeConstantsKimchi, sponge::DefaultFqSponge};
-use poly_commitment::evaluation_proof::OpeningProof;
+use poly_commitment::{evaluation_proof::OpeningProof, SRS as _};
 use serde::{Deserialize, Serialize};
 use std::{
     fs::{File, OpenOptions},
@@ -70,6 +70,7 @@ pub fn caml_pasta_fp_plonk_index_create(
     let cs = match ConstraintSystem::<Fp>::create(gates)
         .public(public as usize)
         .prev_challenges(prev_challenges as usize)
+        .max_poly_size(Some(srs.0.max_poly_size()))
         .lookup(lookup_tables)
         .runtime(if runtime_tables.is_empty() {
             None
