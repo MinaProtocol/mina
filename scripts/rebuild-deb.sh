35476478b5d8b13cf15ddc2e7ece6c8dcc094a52
dkijania <dariusz@o1labs.org>
2023-10-11 15:44:49 +0200

    join rosetta with mina

--- scripts/rebuild-deb.sh
+++ scripts/rebuild-deb.sh
@@ -130,9 +130,25 @@ copy_common_daemon_configs() {
 
   # Copy signature-based Binaries (based on signature type $2 passed into the function)
   cp ./default/src/app/cli/src/mina_${2}_signatures.exe "${BUILDDIR}/usr/local/bin/mina"
+  
+  # Copy rosetta-based Binaries 
   cp ./default/src/app/rosetta/rosetta_${2}_signatures.exe "${BUILDDIR}/usr/local/bin/mina-rosetta"
   cp ./default/src/app/rosetta/ocaml-signer/signer_${2}_signatures.exe "${BUILDDIR}/usr/local/bin/mina-rosetta-ocaml-signer"
  
+  mkdir -p "${BUILDDIR}/etc/mina/rosetta"
+  mkdir -p "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
+  mkdir -p "${BUILDDIR}/etc/mina/rosetta/archive"
+  mkdir -p "${BUILDDIR}/etc/mina/rosetta/genesis_ledgers"
+
+  # --- Copy artifacts
+  cp ../src/app/rosetta/*.conf "${BUILDDIR}/etc/mina/rosetta"
+  cp ../src/app/rosetta/*.sh "${BUILDDIR}/etc/mina/rosetta"
+
+  cp ../src/app/rosetta/rosetta-cli-config/*.json "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
+  cp ../src/app/rosetta/rosetta-cli-config/*.ros "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
+  cp ../src/app/archive/*.sql "${BUILDDIR}/etc/mina/rosetta/archive"
+  cp -r ../genesis_ledgers/* ${BUILDDIR}/etc/mina/rosetta/genesis_ledgers/
+
   # Copy over Build Configs (based on $2)
   mkdir -p "${BUILDDIR}/etc/coda/build_config"
   # Use parameter expansion to either return "mainnet.mlh" or "devnet.mlh"
@@ -261,35 +277,6 @@ cp ./default/src/app/zkapp_test_transaction/zkapp_test_transaction.exe "${BUILDD
 build_deb mina-zkapp-test-transaction
 
 ##################################### END ZKAPP TEST TXN PACKAGE #######################################
-
-##################################### ROSETTA #######################################
-echo "------------------------------------------------------------"
-echo "--- Building Mina Rosetta :"
-
-create_control_file mina-rosetta "${SHARED_DEPS}${DAEMON_DEPS}" 'Rosetta implementation for Mina'
-
-mkdir -p "${BUILDDIR}/etc/mina/rosetta"
-mkdir -p "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
-mkdir -p "${BUILDDIR}/usr/local/bin/mina-rosetta"
-mkdir -p "${BUILDDIR}/etc/mina/rosetta/archive"
-mkdir -p "${BUILDDIR}/etc/mina/rosetta/genesis_ledgers"
-
-# --- Copy artifacts
-cp ../src/app/rosetta/*.conf "${BUILDDIR}/etc/mina/rosetta"
-cp ../src/app/rosetta/*.sh "${BUILDDIR}/etc/mina/rosetta"
-
-cp ../src/app/rosetta/rosetta-cli-config/*.json "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
-cp ../src/app/rosetta/rosetta-cli-config/*.ros "${BUILDDIR}/etc/mina/rosetta/rosetta-cli-config"
-cp ../src/app/archive/*.sql "${BUILDDIR}/etc/mina/rosetta/archive"
-cp -r ../genesis_ledgers/* ${BUILDDIR}/etc/mina/rosetta/genesis_ledgers/
-
-cp ./default/src/app/rosetta/rosetta_testnet_signatures.exe "${BUILDDIR}/usr/local/bin/mina-rosetta"
-cp ./default/src/app/rosetta/ocaml-signer/signer_testnet_signatures.exe "${BUILDDIR}/usr/local/bin/mina-rosetta-ocaml-signer"
- 
-build_deb mina-rosetta
-
-#################################### END ROSETTA PACKAGE #######################################
-
 
 ##################################### BERKELEY PACKAGE #######################################
 echo "------------------------------------------------------------"
