a5450df010fa6ff74d981a3e17921a01e6746922
dkijania <dariusz@o1labs.org>
2023-10-18 19:19:14 +0200

    fix script

--- buildkite/scripts/test-nix.sh
+++ buildkite/scripts/test-nix.sh
@@ -3,5 +3,10 @@
 mkdir -p "${XDG_CONFIG_HOME-${HOME}/.config}/nix"
 echo 'experimental-features = nix-command flakes' > "${XDG_CONFIG_HOME-${HOME}/.config}/nix/nix.conf"
 
+git config --global --add safe.directory /workdir
+
+./nix/pin.sh
+
 nix build mina --accept-flake-config
+
 nix develop mina
