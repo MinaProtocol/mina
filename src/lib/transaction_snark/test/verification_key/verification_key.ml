5384b4a62237062ae852d2e956e8921456da56b2
Tang Jiawei <tang@o1labs.org>
2023-10-21 07:24:32 +0800

    add transaction snark test

--- src/lib/transaction_snark/test/verification_key/verification_key.ml
+++ src/lib/transaction_snark/test/verification_key/verification_key.ml
@@ -40,4 +40,69 @@ let%test_module "Update account verification key" =
           Yojson.Safe.to_file path @@ Pickles.Proof_cache.to_yojson proof_cache
       | None ->
           ()
+
+          include Transaction_snark_tests.Test_zkapp_update.Make (Test_input)
+          open Core
+          open Currency
+          open Signature_lib
+          module U = Transaction_snark_tests.Util
+          module Spec = Transaction_snark.For_tests.Update_states_spec
+          open Test_input
+      
+          let `VK vk, `Prover zkapp_prover = Lazy.force U.trivial_zkapp
+      
+          let mk_update_perm_check ~current_auth ~account_perm ?txn_version
+              ?failure_expected () =
+            Quickcheck.test ~trials:1 U.gen_snapp_ledger
+              ~f:(fun ({ init_ledger; specs }, new_kp) ->
+                let fee = Fee.of_nanomina_int_exn 1_000_000 in
+                let amount = Amount.of_mina_int_exn 10 in
+                let spec = List.hd_exn specs in
+                let test_spec : Spec.t =
+                  { sender = spec.sender
+                  ; fee
+                  ; fee_payer = None
+                  ; receivers = []
+                  ; amount
+                  ; zkapp_account_keypairs = [ new_kp ]
+                  ; memo
+                  ; new_zkapp_account = false
+                  ; snapp_update
+                  ; current_auth
+                  ; call_data = Snark_params.Tick.Field.zero
+                  ; events = []
+                  ; actions = []
+                  ; preconditions = None
+                  }
+                in
+                U.test_snapp_update ?expected_failure:failure_expected
+                  ~snapp_permissions:
+                    (U.permissions_from_update snapp_update ~auth:account_perm
+                       ?txn_version )
+                  test_spec ~init_ledger ~vk ~zkapp_prover
+                  ~snapp_pk:(Public_key.compress new_kp.public_key) )
+      
+          let older_version =
+            let oldest = Mina_numbers.Txn_version.of_int 1 in
+            if Mina_numbers.Txn_version.equal_to_current oldest then
+              failwith "already oldest version"
+            else oldest
+      
+          let%test_unit "account update using Signature auth when perm is set to \
+                         Proof" =
+            mk_update_perm_check ~current_auth:Signature ~account_perm:Proof
+              ~txn_version:older_version ()
+      
+          let%test_unit "account update using Signature auth when perm is set to \
+                         Impossible" =
+            mk_update_perm_check ~current_auth:Signature ~account_perm:Impossible
+              ~txn_version:older_version ()
+      
+          let%test_unit "account update using Proof auth when perm is set to Proof" =
+            mk_update_perm_check ~current_auth:Proof ~account_perm:Proof
+              ~txn_version:older_version ~failure_expected:Test_input.failure_expected ()
+      
+          let%test_unit "account update using Proof auth when perm is set to Either" =
+            mk_update_perm_check ~current_auth:Proof ~account_perm:Either
+              ~txn_version:older_version ()
   end )
