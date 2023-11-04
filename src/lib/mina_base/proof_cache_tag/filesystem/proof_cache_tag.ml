736de055b5a252c0f6d109124ab463ca856c0283
mrmr1993 <mr_1993@hotmail.co.uk>
2023-11-04 15:35:52 +0000

    ocamlformat

--- src/lib/mina_base/proof_cache_tag/filesystem/proof_cache_tag.ml
+++ src/lib/mina_base/proof_cache_tag/filesystem/proof_cache_tag.ml
@@ -10,9 +10,9 @@ let prefix = Filename.temp_dir "mina" "proof_cache"
 
 let path i = prefix ^ Filename.dir_sep ^ Int.to_string i
 
-type t = {idx: int} [@@deriving compare, equal, sexp, yojson, hash]
+type t = { idx : int } [@@deriving compare, equal, sexp, yojson, hash]
 
-let unwrap ({idx = x} : t) : value =
+let unwrap ({ idx = x } : t) : value =
   (* Read from the file. *)
   In_channel.with_file ~binary:true (path x) ~f:(fun chan ->
       let str = In_channel.input_all chan in
@@ -23,11 +23,10 @@ let unwrap ({idx = x} : t) : value =
 let generate (x : value) : t =
   let new_counter = !counter in
   incr counter ;
-  let res = {idx = new_counter} in
+  let res = { idx = new_counter } in
   (* When this reference is GC'd, delete the file. *)
   Gc.Expert.add_finalizer_last_exn res (fun () ->
-      Core.Unix.unlink (path new_counter)
-    ) ;
+      Core.Unix.unlink (path new_counter) ) ;
   (* Write the proof to the file. *)
   Out_channel.with_file ~binary:true (path new_counter) ~f:(fun chan ->
       Out_channel.output_string chan
