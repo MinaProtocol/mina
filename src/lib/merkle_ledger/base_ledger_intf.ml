fc58d3c4cc087bf1f000875637c5abdc24ba948b
mrmr1993 <mr_1993@hotmail.co.uk>
2023-11-07 13:17:14 +0000

    Add merkle_path_batch

--- src/lib/merkle_ledger/base_ledger_intf.ml
+++ src/lib/merkle_ledger/base_ledger_intf.ml
@@ -134,6 +134,8 @@ module type S = sig
 
   val merkle_path_at_index_exn : t -> int -> Path.t
 
+  val merkle_path_batch : t -> Location.t list -> Path.t list
+
   val remove_accounts_exn : t -> account_id list -> unit
 
   (** Triggers when the ledger has been detached and should no longer be
