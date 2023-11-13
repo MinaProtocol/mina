07fbed781a867bc544471646900ab6122615b4c8
dkijania <dariusz@o1labs.org>
2023-11-13 23:19:52 +0100

    Deploy migration apps

--- buildkite/scripts/build-artifact.sh
+++ buildkite/scripts/build-artifact.sh
@@ -28,6 +28,8 @@ dune build "--profile=${DUNE_PROFILE}" \
   src/app/replayer/replayer.exe \
   src/app/extract_blocks/extract_blocks.exe \
   src/app/archive_blocks/archive_blocks.exe \
+  src/app/berkeley_migration/berkeley_migration.exe \
+  src/app/berkeley_account_tables/berkeley_account_tables.exe \
   src/app/batch_txn_tool/batch_txn_tool.exe \
   src/app/missing_blocks_auditor/missing_blocks_auditor.exe \
   src/app/swap_bad_balances/swap_bad_balances.exe \
