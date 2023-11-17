9ca41317a935ed074f19b01bcfead404b1dc4a44
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2023-11-18 00:27:57 +0300

    repairing berkeley testnet module compatibility

--- automation/terraform/modules/o1-testnet/testnet.tf
+++ automation/terraform/modules/o1-testnet/testnet.tf
@@ -28,20 +28,20 @@ module "kubernetes_testnet" {
   k8s_context    = var.k8s_context
   testnet_name   = var.testnet_name
 
-  use_local_charts   = true
-  mina_image         = var.mina_image
-  mina_archive_image = var.mina_archive_image
-  mina_agent_image   = var.mina_agent_image
-  mina_bots_image    = var.mina_bots_image
-  mina_points_image  = var.mina_points_image
-  watchdog_image     = var.watchdog_image
+  use_local_charts       = true
+  mina_image             = var.mina_image
+  mina_archive_image     = var.mina_archive_image
+  mina_agent_image       = var.mina_agent_image
+  mina_bots_image        = var.mina_bots_image
+  mina_points_image      = var.mina_points_image
+  watchdog_image         = var.watchdog_image
   itn_orchestrator_image = var.itn_orchestrator_image
 
   mina_faucet_amount = var.mina_faucet_amount
   mina_faucet_fee    = var.mina_faucet_fee
 
-  log_level           = var.log_level
-  log_txn_pool_gossip = var.log_txn_pool_gossip
+  log_level              = var.log_level
+  log_txn_pool_gossip    = var.log_txn_pool_gossip
   log_precomputed_blocks = var.log_precomputed_blocks
 
   agent_min_fee         = var.agent_min_fee
@@ -52,35 +52,36 @@ module "kubernetes_testnet" {
 
   additional_peers = [for peer in local.static_peers : peer.full_peer]
   runtime_config   = var.use_embedded_runtime_config ? "" : data.local_file.genesis_ledger.content
-  
+
   seed_zone   = var.seed_zone
   seed_region = var.seed_region
 
   archive_configs = local.archive_node_configs
 
-  mina_archive_schema = var.mina_archive_schema
+  mina_archive_schema           = var.mina_archive_schema
   mina_archive_schema_aux_files = var.mina_archive_schema_aux_files
 
   snark_coordinators = var.snark_coordinators
 
-  block_producer_key_pass = var.block_producer_key_pass
-  block_producer_configs = [for i, bp in local.block_producer_configs:
+  # block_producer_key_pass = var.block_producer_key_pass
+  block_producer_configs = [for i, bp in local.block_producer_configs :
     {
-      name                   = bp.name
-      class                  = bp.class
+      name  = bp.name
+      class = bp.class
       # id                     = bp.total_node_index
-      keypair_name     = "${bp.class}-${bp.unique_node_index}-key"
-      privkey_password = "naughty blue worm"
+      keypair_name           = "${bp.class}-${bp.unique_node_index}-key"
+      privkey_password       = "naughty blue worm"
       external_port          = bp.port
       libp2p_secret          = ""
       enable_gossip_flooding = false
       # run_with_user_agent    = bp.class =="whale" ? false : ( var.nodes_with_user_agent == [] ? true : contains(var.nodes_with_user_agent, bp.name) )
-      run_with_user_agent = bp.class =="whale" ? false : true
-      run_with_bots          = false
-      enable_peer_exchange   = true
-      isolated               = false
-      enableArchive          = false
-      archiveAddress         = length(local.archive_node_configs) != 0 ? "${element(local.archive_node_configs, i%(length(local.archive_node_configs)) )["name"]}:${element(local.archive_node_configs, i%(length(local.archive_node_configs)) )["serverPort"]}" : ""  
+      run_with_user_agent  = bp.class == "whale" ? false : true
+      run_with_bots        = false
+      enable_peer_exchange = true
+      isolated             = false
+      enableArchive        = false
+      archiveAddress       = length(local.archive_node_configs) != 0 ? "${element(local.archive_node_configs, i % (length(local.archive_node_configs)))["name"]}:${element(local.archive_node_configs, i % (length(local.archive_node_configs)))["serverPort"]}" : ""
+      persist_working_dir  = true
     }
   ]
 
@@ -88,29 +89,30 @@ module "kubernetes_testnet" {
 
   seed_configs = [
     for i in range(var.seed_count) : {
-      name               = local.seed_static_peers[i].name
-      class              = "seed"
+      name  = local.seed_static_peers[i].name
+      class = "seed"
       # id                 = i + 1
       # external_port      = local.seed_static_peers[i].port
-      libp2p_secret      = "seed-${i + 1}-key"
-      libp2p_secret_pw      = "naughty blue worm"
-      external_ip        = google_compute_address.seed_static_ip[i].address
+      libp2p_secret    = "seed-${i + 1}-key"
+      libp2p_secret_pw = "naughty blue worm"
+      external_ip      = google_compute_address.seed_static_ip[i].address
       # private_key_secret = "online-seeds-account-${i + 1}-key"
-      enableArchive      = length(local.archive_node_configs) > 0
-      archiveAddress     = length(local.archive_node_configs) > 0 ? "${element(local.archive_node_configs, i)["name"]}:${element(local.archive_node_configs, i)["serverPort"]}" : ""
+      enableArchive       = length(local.archive_node_configs) > 0
+      archiveAddress      = length(local.archive_node_configs) > 0 ? "${element(local.archive_node_configs, i)["name"]}:${element(local.archive_node_configs, i)["serverPort"]}" : ""
+      persist_working_dir = true
     }
   ]
 
   plain_node_configs = [
     for i in range(var.plain_node_count) : {
-      name               = "plain-node-${i+1}"
+      name = "plain-node-${i + 1}"
     }
   ]
 
-  cpu_request = var.cpu_request
-  mem_request= var.mem_request
+  cpu_request        = var.cpu_request
+  mem_request        = var.mem_request
   worker_cpu_request = var.worker_cpu_request
-  worker_mem_request= var.worker_mem_request
+  worker_mem_request = var.worker_mem_request
 
   upload_blocks_to_gcloud         = var.upload_blocks_to_gcloud
   restart_nodes                   = var.restart_nodes
@@ -119,7 +121,7 @@ module "kubernetes_testnet" {
   make_report_every_mins          = var.make_report_every_mins
   make_report_discord_webhook_url = var.make_report_discord_webhook_url
   make_report_accounts            = var.make_report_accounts
-  seed_peers_url                  = var.seed_peers_url
+  # seed_peers_url                  = var.seed_peers_url
 
   zkapps_dashboard_key = var.zkapps_dashboard_key
 }
