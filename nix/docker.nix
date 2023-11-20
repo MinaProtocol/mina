eabbca386d0a6e48ee8489d9003b1dd936e36440
Smorci <szekelyschmarton@gmail.com>
2023-11-20 10:34:17 +0000

    PM-633 Add debugging tools

--- nix/docker.nix
+++ nix/docker.nix
@@ -81,7 +81,7 @@ in {
   mina-delegation-verify-image = dockerTools.streamLayeredImage {
     name = "mina-delegation-verify";
     inherit created;
-    contents = [ ocamlPackages_mina.mina-delegation-verify.out ];
+    contents = [ ocamlPackages_mina.mina-delegation-verify.out coreutils bashInteractive ];
     config = {
       cmd = [ "/bin/delegation-verify" ];
     };
