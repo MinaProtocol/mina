3179aa267c94728e59c307f8518866c825d86692
georgeee <george.agapov@gmail.com>
2023-11-20 19:38:13 +0100

    Break relation from the scan state

--- src/lib/staged_ledger/staged_ledger.mli
+++ src/lib/staged_ledger/staged_ledger.mli
@@ -357,10 +357,15 @@ module Test_helpers : sig
     -> unit
     -> Zkapp_precondition.Protocol_state.View.t
 
-  val update_coinbase_stack_and_get_data :
-       t
-    -> logger:Logger.t
+  val update_coinbase_stack_and_get_data_impl :
+       logger:Logger.t
+    -> constraint_constants:Genesis_constants.Constraint_constants.t
     -> global_slot:Mina_numbers.Global_slot_since_genesis.t
+    -> first_partition_slots:int
+    -> no_second_partition:bool
+    -> is_new_stack:bool
+    -> Ledger.t
+    -> Pending_coinbase.t
     -> Transaction.t With_status.t list
     -> Zkapp_precondition.Protocol_state.View.t
     -> Frozen_ledger_hash.t * Frozen_ledger_hash.t
