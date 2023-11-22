eee11420f22e57799173c122cd91262330b312d6
Richard Bonichon <richard.bonichon@gmail.com>
2023-11-22 10:23:03 +0100

    Pickles: alcotest module Step_main_inputs

--- src/lib/pickles/test/main.ml
+++ src/lib/pickles/test/main.ml
@@ -1,6 +1,6 @@
 let () =
   let test =
-    Test_impls.tests @ Test_scalar_challenge.tests
-    @ Test_side_loaded_verification_key.tests
+    Test_impls.tests @ Test_sponge.tests @ Test_step.tests
+    @ Test_scalar_challenge.tests @ Test_side_loaded_verification_key.tests
   in
   Alcotest.run "Pickles" test
