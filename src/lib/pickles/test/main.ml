2861cb3ef316fa02190488b31f2b4f4e8c08cb80
Richard Bonichon <richard.bonichon@gmail.com>
2023-11-27 16:22:57 +0100

    Pickles: alcotest module Opt_sponge

--- src/lib/pickles/test/main.ml
+++ src/lib/pickles/test/main.ml
@@ -1,7 +1,7 @@
 let () =
   let tests =
-    Test_impls.tests @ Test_plonk_curve_ops.tests @ Test_sponge.tests
-    @ Test_step.tests @ Test_scalar_challenge.tests
+    Test_impls.tests @ Test_opt_sponge.tests @ Test_plonk_curve_ops.tests
+    @ Test_sponge.tests @ Test_step.tests @ Test_scalar_challenge.tests
     @ Test_side_loaded_verification_key.tests
   in
   Alcotest.run "Pickles" tests
