f80dcd4824a0113951ac85d506f0a051edbf40b3
Sventimir <svantimir@gmail.com>
2023-11-28 10:22:19 +0100

    [runtime_config] Inline explanations on Fork_config.t fields.

--- src/lib/runtime_config/runtime_config.ml
+++ src/lib/runtime_config/runtime_config.ml
@@ -1,12 +1,13 @@
 open Core_kernel
 
 module Fork_config = struct
-  (* previous_global_slot here is a global slot since genesis. previous
-     length is a number of blocks produced since genesis, which might be
-     smaller than previous_global_slot or equal if a block was produced
-     in every slot possible, which may or may not be the case. *)
+  (* Note that previous_length might be smaller than the gernesis_slot
+     or equal if a block was produced in every slot possible. *)
   type t =
-    { previous_state_hash : string; previous_length : int; genesis_slot : int }
+    { previous_state_hash : string
+    ; previous_length : int (* number of blocks produced since genesis *)
+    ; genesis_slot : int (* global slot since genesis *)
+    }
   [@@deriving yojson, dhall_type, bin_io_unversioned]
 
   let gen =
