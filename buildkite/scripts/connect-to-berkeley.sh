c1e2c52d72e2a0b7ccead7f6d988deb96a4e6a81
dkijania <dariusz@o1labs.org>
2023-12-06 22:54:39 +0100

    use correct package in connectivity test

--- buildkite/scripts/connect-to-berkeley.sh
+++ buildkite/scripts/connect-to-berkeley.sh
@@ -17,15 +17,16 @@ apt-get update
 apt-get install -y git apt-transport-https ca-certificates tzdata curl
 
 TESTNET_NAME="testworld-2-0"
+TESTNET_VERSION_NAME="berkeley"
 
 git config --global --add safe.directory /workdir
 
 source buildkite/scripts/export-git-env-vars.sh
 
-echo "Installing mina daemon package: mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
+echo "Installing mina daemon package: mina-${TESTNET_VERSION_NAME}=${MINA_DEB_VERSION}"
 echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
 apt-get update
-apt-get install --allow-downgrades -y "mina-${TESTNET_NAME}=${MINA_DEB_VERSION}"
+apt-get install --allow-downgrades -y "mina-${TESTNET_VERSION_NAME}=${MINA_DEB_VERSION}"
 
 # Remove lockfile if present
 rm ~/.mina-config/.mina-lock ||:
