dcf9cca0cb18c2e20894fe04d73632df086c0813
Smorci <szekelyschmarton@gmail.com>
2023-12-14 11:57:40 +0000

    Add aws, base image from nix image, add authentication

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,13 +1,22 @@
 { lib, dockerTools, buildEnv, ocamlPackages_mina, runCommand, dumb-init
 , coreutils, bashInteractive, python3, libp2p_helper, procps, postgresql, curl
 , jq, stdenv, rsync, bash, gnutar, gzip, currentTime, flockenzeit, tzdata
-, cqlsh-expansion, python3Packages }:
+, cqlsh-expansion, python3Packages, awscli, xorg }:
 let
 
   created = flockenzeit.lib.ISO-8601 currentTime;
 
   mkdir = name:
     runCommand "mkdir-${name}" { } "mkdir -p $out${lib.escapeShellArg name}";
+
+  nix-image = dockerTools.pullImage {
+    imageName = "lnl7/nix";
+    imageDigest =
+      "sha256:9ba4c0a01d5153f741ac87364b2fd7a9c8b8b92600325f56d35da18421917e95";
+    finalImageName = "lnl7-nix";
+    finalImageTag = "latest";
+    sha256 = "1zymy4rdwzbfhvcbpr1k3mr7gq08gkmrj33dg25ig0nbv9rka5si";
+  };
 
   mina-build-config = stdenv.mkDerivation {
     pname = "mina-build-config";
@@ -50,15 +59,23 @@ let
   };
 
   mina-delegation-verify-init = runCommand "mina-delegation-verify-init" { } ''
-
     mkdir -p $out
-
     export HOME=$out
     export PYTHONUSERBASE=${cqlsh-expansion}
-
     ${cqlsh-expansion}/bin/cqlsh-expansion.init
-
   '';
+
+  mina-delegation-verify-auth = stdenv.mkDerivation {
+    pname = "mina-delegation-verify-auth";
+    version = "dev";
+    src = ../src/app/delegation_verify/scripts;
+    outputs = [ "out" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp authenticate.sh $out/bin/authenticate.sh
+      chmod -R +x $out
+    '';
+  };
 
   mkFullImage = name: packages:
     dockerTools.streamLayeredImage {
@@ -95,10 +112,15 @@ in {
   mina-delegation-verify-image = dockerTools.streamLayeredImage {
     name = "mina-delegation-verify";
     inherit created;
+    fromImage = nix-image;
+    maxLayers = 300;
     contents = [
       ocamlPackages_mina.mina-delegation-verify.out
       cqlsh-expansion
       mina-delegation-verify-init
+      mina-delegation-verify-auth.out
+      awscli
+      jq
       coreutils
       bashInteractive
     ];
