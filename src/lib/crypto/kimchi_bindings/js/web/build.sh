5eeeba5792d5445ddc677928ab2e755eee19f103
Gregor <gregor.mitscha-baude@gmx.at>
2023-12-18 08:58:36 +0100

    move /kimchi/{wasm,js} to kimchi_bindings

--- src/lib/crypto/kimchi_bindings/js/web/build.sh
+++ src/lib/crypto/kimchi_bindings/js/web/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_WEB-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    # The version should stay in line with the one in kimchi/wasm/rust-toolchain.toml
+    # The version should stay in line with the one in kimchi_bindings/wasm/rust-toolchain.toml
     rustup run nightly-2023-09-01 wasm-pack build --target web --out-dir ../js/web ../../wasm -- -Z build-std=panic_abort,std
 else
     cp "$PLONK_WASM_WEB"/* -R .
