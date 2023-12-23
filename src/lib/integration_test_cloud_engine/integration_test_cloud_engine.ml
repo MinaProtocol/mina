ee55b3045f2a9301dbbadcef210d0c304b1ceb0d
Martin Minkov <minkovlmartin@gmail.com>
2023-12-22 21:15:28 -0800

    refactor graphql log engine to be functor and reuse with cloud and local
    The changes were done to make the graphql_polling_log_engine more reusable by making it a functor. This allows it to support different networks, such as the docker network. The old files were deleted as they are no longer needed. The error_json was added to the dune file in integration_test_lib for better error handling.

--- src/lib/integration_test_cloud_engine/integration_test_cloud_engine.ml
+++ src/lib/integration_test_cloud_engine/integration_test_cloud_engine.ml
@@ -3,4 +3,8 @@ let name = "cloud"
 module Network = Kubernetes_network
 module Network_config = Mina_automation.Network_config
 module Network_manager = Mina_automation.Network_manager
-module Log_engine = Graphql_polling_log_engine
+
+module Log_engine =
+  Integration_test_lib.Graphql_polling_log_engine
+  .Make_GraphQL_Polling_log_engine
+    (Kubernetes_network)
--- src/lib/integration_test_local_engine/integration_test_local_engine.ml
+++ src/lib/integration_test_local_engine/integration_test_local_engine.ml
@@ -3,4 +3,8 @@ let name = "local"
 module Network = Docker_network
 module Network_config = Mina_docker.Network_config
 module Network_manager = Mina_docker.Network_manager
-module Log_engine = Docker_pipe_log_engine
+
+module Log_engine =
+  Integration_test_lib.Graphql_polling_log_engine
+  .Make_GraphQL_Polling_log_engine
+    (Docker_network)
