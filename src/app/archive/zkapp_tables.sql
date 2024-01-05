7f2844158ebf4d2bd1e20e02e6cfdfded54520e2
Tang Jiawei <tang@o1labs.org>
2024-01-06 04:48:23 +0800

    add sql file to add indexes

--- src/app/archive/zkapp_tables.sql
+++ src/app/archive/zkapp_tables.sql
@@ -85,7 +85,6 @@ CREATE TABLE zkapp_events
 
 CREATE INDEX idx_zkapp_events_element_ids ON zkapp_events(element_ids);
 
-
 /* field elements derived from verification keys */
 CREATE TABLE zkapp_verification_key_hashes
 ( id                                    serial          PRIMARY KEY
