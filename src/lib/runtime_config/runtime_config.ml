708d76fbe0fc01ca00a2c50c41e0976091b3ae95
João Santos Reis <joaosreis@outlook.pt>
2024-01-08 09:35:32 +0000

    Fix setting hash for staking ledgers config

--- src/lib/runtime_config/runtime_config.ml
+++ src/lib/runtime_config/runtime_config.ml
@@ -985,7 +985,7 @@ module Epoch_data = struct
         { Ledger.base
         ; num_accounts = None
         ; balances = []
-        ; hash = None
+        ; hash
         ; name = None
         ; add_genesis_winner = Some false
         }
