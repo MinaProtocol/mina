eae62962f131d5277ec52b2cab442fa6fa256fb0
Richard Bonichon <richard.bonichon@gmail.com>
2024-01-08 10:24:59 +0100

    Move pickles implementation in own module
    The idea is to allow easily expanding/relaxing the exported signature without
    impacting the actual exported interface of the library.
    
    The pursued goal is to export the tests included within the modules to be
    included in the test directory.

--- src/lib/pickles/pickles.mli
+++ src/lib/pickles/pickles.mli
@@ -1,5 +1,1 @@
-include
-  Pickles_intf.S
-    with type Side_loaded.Verification_key.Stable.V2.t =
-      Mina_wire_types.Pickles.Side_loaded.Verification_key.V2.t
-     and type ('a, 'b) Proof.t = ('a, 'b) Mina_wire_types.Pickles.Proof.t
+include module type of Pickles_internal
