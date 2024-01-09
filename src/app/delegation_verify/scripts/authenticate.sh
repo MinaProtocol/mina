eb429ea1d6fb0bb11ab551831205192248f2b8f8
Smorci <szekelyschmarton@gmail.com>
2024-01-09 14:47:23 +0000

    Copy authentication script to docker image

--- src/app/delegation_verify/scripts/authenticate.sh
+++ src/app/delegation_verify/scripts/authenticate.sh
@@ -3,7 +3,7 @@
 set -x
 
 # The environment variables are set at the time of deployment in the init container
-CREDENTIALS=$(aws sts assume-role --role-session-name $DELEGATION_VERIFY_AWS_ROLE_SESSION_NAME --role-arn $DELEGATION_VERIFY_AWS_ROLE_ARN)
+CREDENTIALS=$(aws sts assume-role --role-session-name $AWS_ROLE_SESSION_NAME --role-arn $AWS_ROLE_ARN)
 
 ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
 
