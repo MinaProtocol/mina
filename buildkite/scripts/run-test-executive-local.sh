2f6b7e7389fac1ca21e57968885bc3d677146086
dkijania <dariusz@o1labs.org>
2024-01-09 11:48:48 +0100

    download always newest Test Executive

--- buildkite/scripts/run-test-executive-local.sh
+++ buildkite/scripts/run-test-executive-local.sh
@@ -12,8 +12,6 @@ TEST_NAME="$1"
 MINA_IMAGE="gcr.io/o1labs-192920/mina-daemon:$MINA_DOCKER_TAG-berkeley"
 ARCHIVE_IMAGE="gcr.io/o1labs-192920/mina-archive:$MINA_DOCKER_TAG"
 
-TE_DEB_VERSION="2.0.0rampup3-abstract-engine-5bbaf69"
-
 if [[ "${TEST_NAME:0:15}" == "block-prod-prio" ]] && [[ "$RUN_OPT_TESTS" == "" ]]; then
   echo "Skipping $TEST_NAME"
   exit 0
@@ -29,29 +27,18 @@ TESTNET_NAME="berkeley"
 
 git config --global --add safe.directory /workdir
 
-echo "Installing mina daemon package: mina-test-executive"
-echo "deb [trusted=yes] http://packages.o1test.net ubuntu stable" | tee /etc/apt/sources.list.d/mina.list
-apt-get update
-apt-get install -y "minimina"
-
 echo "deb [trusted=yes] https://apt.releases.hashicorp.com $MINA_DEB_CODENAME main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update
 apt-get install -y "terraform" "docker" "docker-compose-plugin" "docker-ce"
 
-echo "Installing mina daemon package: mina-test-executive=${TE_DEB_VERSION}"
+echo "Installing mina daemon package: mina-test-executive=${MINA_DEB_VERSION}"
 echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_RELEASE" | tee /etc/apt/sources.list.d/mina.list
 apt-get update
-apt-get install --allow-downgrades -y "mina-test-executive=$TE_DEB_VERSION" "mina-logproc=$TE_DEB_VERSION"
-
-git clone https://github.com/MinaFoundation/lucy-keypairs.git
-
-export MINIMINA_HOME=$PWD/minimina-home
-
-mkdir -p $MINIMINA_HOME
+apt-get install --allow-downgrades -y "mina-test-executive=$MINA_DEB_VERSION" "mina-logproc=$MINA_DEB_VERSION"
 
 mina-test-executive local "$TEST_NAME" \
   --mina-image "$MINA_IMAGE" \
   --archive-image "$ARCHIVE_IMAGE" \
   --debug
-  | tee "$TEST_NAME.test.log" \
+  | tee "$TEST_NAME.local.test.log" \
   | mina-logproc -i inline -f '!(.level in ["Debug", "Spam"])'
