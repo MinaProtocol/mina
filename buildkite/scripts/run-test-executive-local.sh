c759259e0a825f01dc72dba1e5bd562819b60b28
dkijania <dariusz@o1labs.org>
2024-01-10 16:54:36 +0100

    do cleanup before running test

--- buildkite/scripts/run-test-executive-local.sh
+++ buildkite/scripts/run-test-executive-local.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 set -oe pipefail -x
 
+function cleanup
+{
+  remove_active_stacks() {
+      for stack in $(docker stack ls --format "{{.Name}}"); do
+          echo "Removing stack: $stack"
+          docker stack rm $stack
+      done
+  }
+  while [[ $(docker stack ls | wc -l) -gt 1 ]]; do
+      echo "Active Docker stacks found. Removing them..."
+      remove_active_stacks
+      sleep 5 
+  done
+}
+
 # Set up a local docker swarm
 # Check if the current host is part of a Docker Swarm
 if docker info --format '{{.Swarm.LocalNodeState}}' | grep -q 'inactive'; then
     docker swarm init --advertise-addr 127.0.0.1
 fi
+
+cleanup
 
 TEST_NAME="$1"
 
@@ -36,25 +53,8 @@ echo "deb [trusted=yes] http://packages.o1test.net $MINA_DEB_CODENAME $MINA_DEB_
 apt-get update
 apt-get install --allow-downgrades -y "mina-test-executive=$MINA_DEB_VERSION" "mina-logproc=$MINA_DEB_VERSION"
 
-function cleanup
-{
-  remove_active_stacks() {
-      for stack in $(docker stack ls --format "{{.Name}}"); do
-          echo "Removing stack: $stack"
-          docker stack rm $stack
-      done
-  }
-  while [[ $(docker stack ls | wc -l) -gt 1 ]]; do
-      echo "Active Docker stacks found. Removing them..."
-      remove_active_stacks
-      sleep 5 
-  done
-}
-
 mina-test-executive local "$TEST_NAME" \
   --mina-image "$MINA_IMAGE" \
   --archive-image "$ARCHIVE_IMAGE"
   | tee "$TEST_NAME.local.test.log" \
   | mina-logproc -i inline -f '!(.level in ["Debug", "Spam"])'
-
-trap cleanup EXIT
