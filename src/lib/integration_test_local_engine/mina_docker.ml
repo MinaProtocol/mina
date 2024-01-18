70967fb4c25210025a49bca0d5c383020f775b8a
mrmr1993 <mr_1993@hotmail.co.uk>
2024-01-18 19:30:14 +0000

    Fix compilation error

--- src/lib/integration_test_local_engine/mina_docker.ml
+++ src/lib/integration_test_local_engine/mina_docker.ml
@@ -105,7 +105,7 @@ module Network_config = struct
     let add_accounts accounts_and_keypairs =
       List.map accounts_and_keypairs
         ~f:(fun
-             ( { Test_config.Test_Account.balance; account_name; timing }
+             ( { Test_config.Test_account.balance; account_name; timing }
              , (pk, sk) )
            ->
           let timing = runtime_timing_of_timing timing in
@@ -130,8 +130,8 @@ module Network_config = struct
     let ledger_is_prefix ledger1 ledger2 =
       List.is_prefix ledger2 ~prefix:ledger1
         ~equal:(fun
-                 ({ account_name = name1; _ } : Test_config.Test_Account.t)
-                 ({ account_name = name2; _ } : Test_config.Test_Account.t)
+                 ({ account_name = name1; _ } : Test_config.Test_account.t)
+                 ({ account_name = name2; _ } : Test_config.Test_account.t)
                -> String.equal name1 name2 )
     in
     let runtime_config =
@@ -177,7 +177,7 @@ module Network_config = struct
                 |> Result.ok_or_failwith
               in
               let ledger_of_epoch_accounts
-                  (epoch_accounts : Test_config.Test_Account.t list) =
+                  (epoch_accounts : Test_config.Test_account.t list) =
                 let epoch_ledger_accounts =
                   List.map epoch_accounts
                     ~f:(fun { account_name; balance; timing } ->
