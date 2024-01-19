6645cc3062d95aef892bcbe75d765ea915086c1a
João Santos Reis <joaosreis@outlook.pt>
2024-01-19 17:34:41 +0000

    Return sync_status.synced value on /network/status

--- src/app/rosetta/lib/network.ml
+++ src/app/rosetta/lib/network.ml
@@ -75,6 +75,14 @@ let sync_status_to_string = function
   | `SYNCED ->
       "Synced"
 
+let is_synced = function
+  | `SYNCED -> true
+  | `BOOTSTRAP
+  | `CATCHUP 
+  | `CONNECTING 
+  | `LISTENING 
+  | `OFFLINE -> false
+
 module Get_version =
 [%graphql
 {|
@@ -239,7 +247,7 @@ module Status = struct
                 Some ((latest_node_block.protocolState).consensusState).blockHeight
             ; target_index= None
             ; stage= Some (sync_status_to_string res.syncStatus)
-            ; synced = None
+            ; synced = (Some (is_synced res.syncStatus))
             } }
   end
 
