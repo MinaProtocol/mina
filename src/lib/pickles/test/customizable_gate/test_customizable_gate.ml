732f5a071726e07efec1ed94da3518f61dc3c49e
Joseph Spadavecchia <joseph@redtrie.com>
2024-01-24 14:58:25 +0000

    Format changes

--- src/lib/pickles/test/customizable_gate/test_customizable_gate.ml
+++ src/lib/pickles/test/customizable_gate/test_customizable_gate.ml
@@ -210,6 +210,7 @@ let () =
   if perform_step_tests then (
     (* Customised as ForeignFieldAdd gate; valid witness *)
     test ~step_only:true ~custom_gate_type:None ~valid_witness:true ;
+
     (* Customised as Conditional gate; valid witness *)
     (* Note: Requires Cache.Wrap.read_or_generate to have custom_gate_type passed to it *)
     (* test ~step_only:true ~custom_gate_type:conditional_gate ~valid_witness:true ; *)
@@ -223,20 +224,18 @@ let () =
         false
       with _ -> true
     in
-    assert test_failed ;
-
+    assert test_failed
     (* Customised as Conditional gate; invalid witness *)
     (* let test_failed =
-      try
-        let _cs =
-          test ~step_only:true ~custom_gate_type:conditional_gate
-            ~valid_witness:false
-        in
-        false
-      with _ -> true
-    in
-    assert test_failed ) *)
-    )
+         try
+           let _cs =
+             test ~step_only:true ~custom_gate_type:conditional_gate
+               ~valid_witness:false
+           in
+           false
+         with _ -> true
+       in
+       assert test_failed ) *) )
 
 (* Recursive tests *)
 let () =
--- src/lib/pickles/test/rpn_gatedef/test_rpn_gatedef.ml
+++ src/lib/pickles/test/rpn_gatedef/test_rpn_gatedef.ml
@@ -9,65 +9,69 @@ module Type1 = Plonk_checks.Make (Shifted_value.Type1) (Scalars.Tick)
 let () = Pickles.Backend.Tick.Keypair.set_urs_info []
 
 let () =
+  (* let env =
+       let module Env_bool = struct
+         include Boolean
 
-(* let env =
-  let module Env_bool = struct
-    include Boolean
+         type t = Boolean.var
+       end in
+       let module Env_field = struct
+         include Field
 
-    type t = Boolean.var
-  end in
-  let module Env_field = struct
-    include Field
+         type bool = Env_bool.t
 
-    type bool = Env_bool.t
-
-    let if_ (b : bool) ~then_ ~else_ =
-      match Impl.Field.to_constant (b :> t) with
-      | Some x ->
-          (* We have a constant, only compute the branch we care about. *)
-          if Impl.Field.Constant.(equal one) x then then_ () else else_ ()
-      | None ->
-          if_ b ~then_:(then_ ()) ~else_:(else_ ())
-  end in
-  Plonk_checks.scalars_env
-    (module Env_bool)
-    (module Env_field)
-    ~srs_length_log2:Common.Max_degree.wrap_log2 ~zk_rows:3
-    ~endo:(Impl.Field.constant Endo.Wrap_inner_curve.base)
-    ~mds:sponge_params.mds
-    ~field_of_hex:(fun s ->
-      Kimchi_pasta.Pasta.Bigint256.of_hex_string s
-      |> Kimchi_pasta.Pasta.Fq.of_bigint |> Field.constant )
-    ~domain plonk_minimal combined_evals
-in *)
+         let if_ (b : bool) ~then_ ~else_ =
+           match Impl.Field.to_constant (b :> t) with
+           | Some x ->
+               (* We have a constant, only compute the branch we care about. *)
+               if Impl.Field.Constant.(equal one) x then then_ () else else_ ()
+           | None ->
+               if_ b ~then_:(then_ ()) ~else_:(else_ ())
+       end in
+       Plonk_checks.scalars_env
+         (module Env_bool)
+         (module Env_field)
+         ~srs_length_log2:Common.Max_degree.wrap_log2 ~zk_rows:3
+         ~endo:(Impl.Field.constant Endo.Wrap_inner_curve.base)
+         ~mds:sponge_params.mds
+         ~field_of_hex:(fun s ->
+           Kimchi_pasta.Pasta.Bigint256.of_hex_string s
+           |> Kimchi_pasta.Pasta.Fq.of_bigint |> Field.constant )
+         ~domain plonk_minimal combined_evals
+     in *)
 
   (* Type1.evaluate_rpn (module Kimchi_pasta.Vesta_based_plonk.Field) ~_rpn_gate:[||] *)
   (* Type1.evaluate_rpn (module Field) ~_rpn_gate:[||] *)
   (* Type1.evaluate_rpn (module Backend.Tick.Field) ~_rpn_gate:[||] *)
   let conditional_gate =
-      Kimchi_types.
-        [| Cell { col = Index ForeignFieldAdd; row = Curr }
-         ; Cell { col = Witness 3; row = Curr }
-         ; Dup
-         ; Mul
-         ; Cell { col = Witness 3; row = Curr }
-         ; Sub
-         ; Alpha
-         ; Pow 1l
-         ; Cell { col = Witness 0; row = Curr }
-         ; Cell { col = Witness 3; row = Curr }
-         ; Cell { col = Witness 1; row = Curr }
-         ; Mul
-         ; Literal (Pickles.Backend.Tick.Field.of_int 1)
-         ; Cell { col = Witness 3; row = Curr }
-         ; Sub
-         ; Cell { col = Witness 2; row = Curr }
-         ; Mul
-         ; Add
-         ; Sub
-         ; Mul
-         ; Add
-         ; Mul
-        |] in
-  let _x = Type1.evaluate_rpn (module Pickles.Backend.Tick.Field) ~gate_rpn:conditional_gate in
+    Kimchi_types.
+      [| Cell { col = Index ForeignFieldAdd; row = Curr }
+       ; Cell { col = Witness 3; row = Curr }
+       ; Dup
+       ; Mul
+       ; Cell { col = Witness 3; row = Curr }
+       ; Sub
+       ; Alpha
+       ; Pow 1l
+       ; Cell { col = Witness 0; row = Curr }
+       ; Cell { col = Witness 3; row = Curr }
+       ; Cell { col = Witness 1; row = Curr }
+       ; Mul
+       ; Literal (Pickles.Backend.Tick.Field.of_int 1)
+       ; Cell { col = Witness 3; row = Curr }
+       ; Sub
+       ; Cell { col = Witness 2; row = Curr }
+       ; Mul
+       ; Add
+       ; Sub
+       ; Mul
+       ; Add
+       ; Mul
+      |]
+  in
+  let _x =
+    Type1.evaluate_rpn
+      (module Pickles.Backend.Tick.Field)
+      ~gate_rpn:conditional_gate
+  in
   ()
