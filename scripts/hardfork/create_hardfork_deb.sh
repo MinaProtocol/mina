c1d6c19cfc8c5ac69409f025f3aabe1ea947620e
mrmr1993 <mr_1993@hotmail.co.uk>
2024-01-30 17:09:09 +0000

    Add hardfork deb builder script

---
+++ scripts/hardfork/create_hardfork_deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source scripts/deb-builder-helpers.sh
+
+RUNTIME_CONFIG_JSON=${RUNTIME_CONFIG_JSON:=runtime_config.json}
+
+echo "------------------------------------------------------------"
+echo "--- Building mainnet deb with hard-fork ledger:"
+
+create_control_file mina-mainnet "${SHARED_DEPS}${DAEMON_DEPS}" 'Mina Protocol Client and Daemon'
+
+copy_common_daemon_configs mainnet mainnet 'mina-seed-lists/mainnet_seeds.txt'
+
+# Copy the overridden runtime config file to the config file location
+cp "${SCRIPTPATH}/../${RUNTIME_CONFIG_JSON}" "${BUILDDIR}/var/lib/coda/config_${GITHASH_CONFIG}.json"
+
+# TODO: call "${BUILDDIR}/usr/local/bin/mina-create-genesis" here to generate the tar files
+
+build_deb mina-mainnet-hardfork
