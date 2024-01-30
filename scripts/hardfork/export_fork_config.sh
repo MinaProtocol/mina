0baee0e8eac3cd44d780d755f3e8db49b1b06e99
mrmr1993 <mr_1993@hotmail.co.uk>
2024-01-30 21:16:07 +0000

    Exit with an error code on daemon failure

--- scripts/hardfork/export_fork_config.sh
+++ scripts/hardfork/export_fork_config.sh
@@ -3,7 +3,7 @@
 MINA_V1_DAEMON=${MINA_V1_DAEMON:=_build/default/src/app/cli/src/mina.exe}
 FORK_CONFIG_JSON=${FORK_CONFIG_JSON:=fork_config.json}
 
-$MINA_V1_DAEMON daemon --config-file genesis_ledgers/mainnet.json --peer-list-url https://storage.googleapis.com/seed-lists/mainnet_seeds.txt &
+$MINA_V1_DAEMON daemon --config-file genesis_ledgers/mainnet.json --peer-list-url https://storage.googleapis.com/seed-lists/mainnet_seeds.txt || exit 1 &
 
 
 function isNotSynced() {
