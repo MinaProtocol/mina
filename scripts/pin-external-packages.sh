72d7682c49fb0c8c83bfc3690eec3f17acbf46b8
ember arlynx <ember.arlynx@o1labs.org>
2024-02-01 17:42:23 -0500

    whoops, syntax

--- scripts/pin-external-packages.sh
+++ scripts/pin-external-packages.sh
@@ -11,5 +11,4 @@ for pkg in $PACKAGES; do
     opam pin -y add src/external/$pkg
 done
 
-opam pin add -y https://github.com/tweag/check_opam_switch#d0aa49884e0f9fd4bbb2cd1a32b798a12
-f84b603
+opam pin add -y https://github.com/tweag/check_opam_switch.git#d0aa49884e0f9fd4bbb2cd1a32b798a12
