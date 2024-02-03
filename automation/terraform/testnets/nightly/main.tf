e29ece295d151227c9d3e1fd13d389518c798da8
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2024-02-02 20:41:51 -0500

    fix faucet var name in nightly terraform

--- automation/terraform/testnets/nightly/main.tf
+++ automation/terraform/testnets/nightly/main.tf
@@ -57,10 +57,10 @@ variable "mina_archive_image" {
 
 locals {
   seed_region = "us-east4"
-  seed_zone = "us-east4-b"
+  seed_zone   = "us-east4-b"
   seed_discovery_keypairs = [
-  "CAESQBEHe2zCcQDHcSaeIydGggamzmTapdCS8SP0hb5FWvYhe9XEygmlUGV4zNu2P8zAIba4X84Gm4usQFLamjRywA8=,CAESIHvVxMoJpVBleMzbtj/MwCG2uF/OBpuLrEBS2po0csAP,12D3KooWJ9mNdbUXUpUNeMnejRumKzmQF15YeWwAPAhTAWB6dhiv",
-  "CAESQO+8qvMqTaQEX9uh4NnNoyOy4Xwv3U80jAsWweQ1J37AVgx7kgs4pPVSBzlP7NDANP1qvSvEPOTh2atbMMUO8EQ=,CAESIFYMe5ILOKT1Ugc5T+zQwDT9ar0rxDzk4dmrWzDFDvBE,12D3KooWFcGGeUmbmCNq51NBdGvCWjiyefdNZbDXADMK5CDwNRm5" ]
+    "CAESQBEHe2zCcQDHcSaeIydGggamzmTapdCS8SP0hb5FWvYhe9XEygmlUGV4zNu2P8zAIba4X84Gm4usQFLamjRywA8=,CAESIHvVxMoJpVBleMzbtj/MwCG2uF/OBpuLrEBS2po0csAP,12D3KooWJ9mNdbUXUpUNeMnejRumKzmQF15YeWwAPAhTAWB6dhiv",
+  "CAESQO+8qvMqTaQEX9uh4NnNoyOy4Xwv3U80jAsWweQ1J37AVgx7kgs4pPVSBzlP7NDANP1qvSvEPOTh2atbMMUO8EQ=,CAESIFYMe5ILOKT1Ugc5T+zQwDT9ar0rxDzk4dmrWzDFDvBE,12D3KooWFcGGeUmbmCNq51NBdGvCWjiyefdNZbDXADMK5CDwNRm5"]
 }
 
 module "ci_testnet" {
@@ -68,19 +68,19 @@ module "ci_testnet" {
   source    = "../../modules/o1-testnet"
 
 
-  cluster_name          = "coda-infra-east4"
-  cluster_region        = "us-east4"
-  k8s_context           = "gke_o1labs-192920_us-east4_coda-infra-east4"
-  testnet_name          = var.testnet_name
+  cluster_name   = "coda-infra-east4"
+  cluster_region = "us-east4"
+  k8s_context    = "gke_o1labs-192920_us-east4_coda-infra-east4"
+  testnet_name   = var.testnet_name
 
-  mina_image            = var.mina_image
-  mina_archive_image    = var.mina_archive_image
-  mina_agent_image      = "codaprotocol/coda-user-agent:0.1.5"
-  mina_bots_image       = "codaprotocol/coda-bots:0.0.13-beta-1"
-  mina_points_image     = "codaprotocol/coda-points-hack:32b.4"
+  mina_image         = var.mina_image
+  mina_archive_image = var.mina_archive_image
+  mina_agent_image   = "codaprotocol/coda-user-agent:0.1.5"
+  mina_bots_image    = "codaprotocol/coda-bots:0.0.13-beta-1"
+  mina_points_image  = "codaprotocol/coda-points-hack:32b.4"
 
-  coda_faucet_amount    = "10000000000"
-  coda_faucet_fee       = "100000000"
+  mina_faucet_amount = "10000000000"
+  mina_faucet_fee    = "100000000"
 
   agent_min_fee         = "0.05"
   agent_max_fee         = "0.1"
@@ -102,21 +102,21 @@ module "ci_testnet" {
 
   snark_coordinators = [
     {
-      snark_worker_replicas   = 1
-      snark_worker_fee        = "0.025"
-      snark_worker_public_key = "B62qk4nuKn2U5kb4dnZiUwXeRNtP1LncekdAKddnd1Ze8cWZnjWpmMU"
-      snark_coordinators_host_port  = 10401
+      snark_worker_replicas        = 1
+      snark_worker_fee             = "0.025"
+      snark_worker_public_key      = "B62qk4nuKn2U5kb4dnZiUwXeRNtP1LncekdAKddnd1Ze8cWZnjWpmMU"
+      snark_coordinators_host_port = 10401
     }
   ]
-  
-  whales= [
-    for i in range(var.whale_count):{
+
+  whales = [
+    for i in range(var.whale_count) : {
       duplicates = 1
     }
   ]
-  
-  fishes= [
-    for i in range(var.fish_count):{
+
+  fishes = [
+    for i in range(var.fish_count) : {
       duplicates = 1
     }
   ]
