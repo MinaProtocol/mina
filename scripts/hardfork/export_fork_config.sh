09d4d3692989563be8cabdb8408b38d94e1dfdcb
George Agapov <george.agapov@gmail.com>
2024-02-07 12:13:31 +0100

    Update export_fork_config.sh (fixup to previous commit)

--- scripts/hardfork/export_fork_config.sh
+++ scripts/hardfork/export_fork_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -beo pipefail
-
+FORK_CONFIG_JSON=${FORK_CONFIG_JSON:=fork_config.json}
 curl 'https://drive.usercontent.google.com/download?id=1r8BtQdp6QJz9WVevzBw8fn44EY5KQoNT&export=download&confirm=t' > $FORK_CONFIG_JSON
