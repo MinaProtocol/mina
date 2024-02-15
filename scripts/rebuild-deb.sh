f6d6fd1d8eb5d522d60c3f239a459ba543de0f1c
dkijania <dariusz@o1labs.org>
2024-02-15 20:32:06 +0100

    Merge pull request #15119 from MinaProtocol/ci/remove_leftover_scripts_from_testworld
    [CI] Fix test suite job & remove legacy artifacts from testworld 2.0

--- scripts/rebuild-deb.sh
+++ scripts/rebuild-deb.sh
@@ -17,7 +17,7 @@ if [ $# -eq 0 ]
     build_daemon_deb
     build_receipt_chain_hash_fix_deb
     build_test_executive_deb
-    build_test_suite_deb
+    build_functional_test_suite_deb
     build_zkapp_test_transaction_deb
 
   else 
