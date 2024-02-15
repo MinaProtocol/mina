7fe1a32c31fb9780c13f1b9f83c7e6c6793ac300
Richard Bonichon <richard.bonichon@gmail.com>
2024-02-15 22:11:45 +0100

    Merkle_ledger: minimizing module type declaration for database

--- src/lib/merkle_ledger/database.ml
+++ src/lib/merkle_ledger/database.ml
@@ -1,29 +1,6 @@
 open Core
 
-module type Inputs_intf = sig
-  include Base_inputs_intf.S
-
-  module Location : Location_intf.S
-
-  module Location_binable : Hashable.S_binable with type t := Location.t
-
-  module Kvdb : Intf.Key_value_database with type config := string
-
-  module Storage_locations : Intf.Storage_locations
-end
-
-module Make (Inputs : Inputs_intf) :
-  Database_intf.S
-    with module Location = Inputs.Location
-     and module Addr = Inputs.Location.Addr
-     and type key := Inputs.Key.t
-     and type token_id := Inputs.Token_id.t
-     and type token_id_set := Inputs.Token_id.Set.t
-     and type account := Inputs.Account.t
-     and type root_hash := Inputs.Hash.t
-     and type hash := Inputs.Hash.t
-     and type account_id := Inputs.Account_id.t
-     and type account_id_set := Inputs.Account_id.Set.t = struct
+module Make (Inputs : Database_intf.Inputs_intf) = struct
   (* The max depth of a merkle tree can never be greater than 253. *)
   open Inputs
 
--- src/lib/merkle_ledger/database.mli
+++ src/lib/merkle_ledger/database.mli
@@ -1,17 +1,4 @@
-module type Inputs_intf = sig
-  include Base_inputs_intf.S
-
-  module Location : Location_intf.S
-
-  module Location_binable :
-    Core_kernel.Hashable.S_binable with type t := Location.t
-
-  module Kvdb : Intf.Key_value_database with type config := string
-
-  module Storage_locations : Intf.Storage_locations
-end
-
-module Make (Inputs : Inputs_intf) :
+module Make (Inputs : Database_intf.Inputs_intf) :
   Database_intf.S
     with module Location = Inputs.Location
      and module Addr = Inputs.Location.Addr
