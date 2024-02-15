5dd4c1122d892e58ac0a824a8e5763831de01384
Richard Bonichon <richard.bonichon@gmail.com>
2024-02-15 16:53:17 +0100

    Merkle_ledger: actual interface for Merkle_path

---
+++ src/lib/merkle_ledger/merkle_path.mli
@@ -0,0 +1,21 @@
+module type S = sig
+  type hash
+
+  type elem = [ `Left of hash | `Right of hash ] [@@deriving sexp, equal]
+
+  val elem_hash : elem -> hash
+
+  type t = elem list [@@deriving sexp, equal]
+
+  val implied_root : t -> hash -> hash
+
+  val check_path : t -> hash -> hash -> bool
+end
+
+module Make (Hash : sig
+  type t [@@deriving sexp, equal]
+
+  val merge : height:int -> t -> t -> t
+
+  val equal : t -> t -> bool
+end) : S with type hash := Hash.t
