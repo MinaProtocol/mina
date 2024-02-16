caa09509ee8994c068c8dd009aaecb963e62a9fd
Richard Bonichon <richard.bonichon@gmail.com>
2024-02-16 15:43:26 +0100

    Merkle_ledger: mv Syncable_intf to Intf

--- src/lib/merkle_ledger/intf.ml
+++ src/lib/merkle_ledger/intf.ml
@@ -158,6 +158,40 @@ module type Storage_locations = sig
   val key_value_db_dir : string
 end
 
+module type SYNCABLE = sig
+  type root_hash
+
+  type hash
+
+  type account
+
+  type addr
+
+  type t [@@deriving sexp]
+
+  type path
+
+  val depth : t -> int
+
+  val num_accounts : t -> int
+
+  val merkle_path_at_addr_exn : t -> addr -> path
+
+  val get_inner_hash_at_addr_exn : t -> addr -> hash
+
+  val set_inner_hash_at_addr_exn : t -> addr -> hash -> unit
+
+  val set_all_accounts_rooted_at_exn : t -> addr -> account list -> unit
+
+  val set_batch_accounts : t -> (addr * account) list -> unit
+
+  (** Get all of the accounts that are in a subtree of the underlying Merkle
+    tree rooted at `address`. The accounts are ordered by their addresses. *)
+  val get_all_accounts_rooted_at_exn : t -> addr -> (addr * account) list
+
+  val merkle_root : t -> root_hash
+end
+
 module Inputs = struct
   module type Intf = sig
     module Key : Key
@@ -228,7 +262,7 @@ module Ledger = struct
     end
 
     include
-      Syncable_intf.S
+      SYNCABLE
         with type root_hash := root_hash
          and type hash := hash
          and type account := account
