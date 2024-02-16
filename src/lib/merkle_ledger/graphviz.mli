ef81113c8b5850a34dfe304660330b2a2c12cd0f
Richard Bonichon <richard.bonichon@gmail.com>
2024-02-16 14:19:30 +0100

    Merkle_ledger: apply intf trick for Graphviz
    See https://www.craigfe.io/posts/the-intf-trick

--- src/lib/merkle_ledger/graphviz.mli
+++ src/lib/merkle_ledger/graphviz.mli
@@ -1,50 +1,6 @@
 (** Visualizable_ledger shows a subgraph of a merkle_ledger using Graphviz *)
-module type S = sig
-  type addr
 
-  type ledger
-
-  type t
-
-  (* Visualize will enumerate through all edges of a subtree with a
-     initial_address. It will then interpret all of the edges and nodes into an
-     intermediate form that will be easy to write into a dot file *)
-  val visualize : ledger -> initial_address:addr -> t
-
-  (* Write will transform the intermediate form generate by visualize and save
-     the results into a dot file *)
-  val write : path:string -> name:string -> t -> unit Async.Deferred.t
-end
-
-module type Inputs_intf = sig
-  module Key : Intf.Key
-
-  module Token_id : Intf.Token_id
-
-  module Account_id :
-    Intf.Account_id with type key := Key.t and type token_id := Token_id.t
-
-  module Balance : Intf.Balance
-
-  module Account :
-    Intf.Account
-      with type account_id := Account_id.t
-       and type balance := Balance.t
-
-  module Hash : Intf.Hash with type account := Account.t
-
-  module Location : Location_intf.S
-
-  module Ledger :
-    Base_ledger_intf.S
-      with module Addr = Location.Addr
-       and module Location = Location
-       and type account_id := Account_id.t
-       and type account_id_set := Account_id.Set.t
-       and type hash := Hash.t
-       and type root_hash := Hash.t
-       and type account := Account.t
-end
-
-module Make (Inputs : Inputs_intf) :
-  S with type addr := Inputs.Location.Addr.t and type ledger := Inputs.Ledger.t
+module Make (Inputs : Graphviz_intf.Inputs_intf) :
+  Graphviz_intf.S
+    with type addr := Inputs.Location.Addr.t
+     and type ledger := Inputs.Ledger.t
