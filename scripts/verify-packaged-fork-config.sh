608d4a98313e5553c9d34c6304f1edf4ca62b8c7
ember arlynx <ember@lunar.town>
2024-02-22 17:40:51 -0500

    use create_runtime_config in verify-packaged-fork-config

--- scripts/verify-packaged-fork-config.sh
+++ scripts/verify-packaged-fork-config.sh
@@ -36,9 +36,13 @@ MINA_EXE=${MINA_EXE:-"mina"}
 MINA_GENESIS_EXE=${MINA_GENESIS_EXE:-"mina-create-genesis"}
 found_config=$(echo /var/lib/coda/config_*.json)
 PACKAGED_CONFIG_JSON=${PACKAGED_CONFIG_JSON:-$found_config}
+CREATE_RUNTIME_CONFIG=${CREATE_RUNTIME_CONFIG:-mina-hf-create-runtime-config}
+if [ ! -x "$CREATE_RUNTIME_CONFIG" ]; then
+    CREATE_RUNTIME_CONFIG=./scripts/hardfork/create_runtime_config.sh
+fi
 GENESIS_LEDGER_DIR=${GENESIS_LEDGER_DIR:-"/var/lib/coda"}
 SECONDS_PER_SLOT=${SECONDS_PER_SLOT:=180}
-FORKING_FROM_CONFIG_JSON=${FORKING_FROM_CONFIG_JSON:=/var/lib/coda/mainnet.json}
+export FORKING_FROM_CONFIG_JSON=${FORKING_FROM_CONFIG_JSON:=/var/lib/coda/mainnet.json}
 
 export MINA_LIBP2P_PASS=''
 
@@ -53,74 +57,18 @@ if [ ! -e "$workdir/keys/p2p" ]; then
     "$MINA_EXE" libp2p generate-keypair --privkey-path "$workdir/keys/p2p"
 fi
 
-# inspired by scripts/hardfork/create_runtime_config.sh
-function generate_ledgers_tar_from_config() {
-    config_file_input=$1
-    genesis_dir=$2
-    config_file_output=$3
-    
-    config_file_tmp=$(mktemp)
-    echo "preprocessing config file..."
-    cp "$config_file_input" "$config_file_tmp"
-    sed -i -e 's/"set_verification_key": "signature"/"set_verification_key": {"auth": "signature", "txn_version": "1"}/' "$config_file_tmp"
-    
-    echo "generating genesis ledger... (this may take a while)"
-    "$MINA_GENESIS_EXE" --config-file "$config_file_tmp" --genesis-dir "$genesis_dir" --hash-output-file "$workdir/hashes.json"
-    echo "genesis ledger generated"
-    
-    echo "updating config file..."
-    
-    GENESIS_TIMESTAMP=$(jq -r '.genesis.genesis_state_timestamp' "$PACKAGED_CONFIG_JSON")
-    
-    # Pull the original genesis timestamp from the pre-fork config file
-    ORIGINAL_GENESIS_TIMESTAMP=$(jq -r '.genesis.genesis_state_timestamp' "$FORKING_FROM_CONFIG_JSON")
-    
-    DIFFERENCE_IN_SECONDS=$(($(date -d "$GENESIS_TIMESTAMP" "+%s") - $(date -d "$ORIGINAL_GENESIS_TIMESTAMP" "+%s")))
-    DIFFERENCE_IN_SLOTS=$((DIFFERENCE_IN_SECONDS / SECONDS_PER_SLOT))
-    
-    jq "{\
-    genesis: {\
-        genesis_state_timestamp: \"$GENESIS_TIMESTAMP\"\
-    },\
-    proof: {\
-        fork: {\
-            state_hash: .proof.fork.state_hash,\
-            blockchain_length: .proof.fork.blockchain_length,\
-            global_slot_since_genesis: $DIFFERENCE_IN_SLOTS,\
-        },\
-    },\
-    ledger: {\
-        add_genesis_winner: false,\
-        hash: \$hashes[0].ledger.hash,\
-        s3_data_hash: \$hashes[0].ledger.s3_data_hash\
-    },\
-    epoch_data: {\
-        staking: {\
-            seed: .epoch_data.staking.seed,\
-            hash: \$hashes[0].epoch_data.staking.hash,\
-            s3_data_hash: \$hashes[0].epoch_data.staking.s3_data_hash\
-        },\
-        next: {\
-            seed: .epoch_data.next.seed,\
-            hash: \$hashes[0].epoch_data.next.hash,\
-            s3_data_hash: \$hashes[0].epoch_data.next.s3_data_hash\
-        }\
-    }\
-    }" -M \
-    --slurpfile hashes "$workdir/hashes.json" "$1" > "$config_file_output"
-}
+echo "generating genesis ledgers ... (this may take a while)"
 
-generate_ledgers_tar_from_config "$1" "$workdir/ledgers" "$workdir/config-substituted.json"
+cp "$1" "$workdir/config.json"
+sed -i -e 's/"set_verification_key": "signature"/"set_verification_key": {"auth": "signature", "txn_version": "1"}/' "$workdir/config.json"
+"$MINA_GENESIS_EXE" --config-file "$1"  --genesis-dir "$workdir/ledgers"  --hash-output-file "$workdir/hashes.json"
 
-result=$(jq --slurpfile a "$workdir/config-substituted.json" --slurpfile b "$PACKAGED_CONFIG_JSON" -n '
-  ($a[0].epoch_data.staking.hash == $b[0].epoch_data.staking.hash and
-   $a[0].epoch_data.next.hash == $b[0].epoch_data.next.hash and
-   $a[0].ledger.hash == $b[0].ledger.hash)')
+FORK_CONFIG_JSON="$1" \
+LEDGER_HASHES_JSON="$workdir/hashes.json" \
+GENESIS_TIMESTAMP=$(jq -r '.genesis.genesis_state_timestamp' "$PACKAGED_CONFIG_JSON") \
+"$CREATE_RUNTIME_CONFIG" > "$workdir/config-substituted.json"
 
-if [ "$result" != "true" ]; then
-    echo "Packaged config hashes in $PACKAGED_CONFIG_JSON not expected compared to $workdir/config-substituted.json"
-    exit 1
-fi
+echo "exporting ledgers from running node ... (this may take a while)"
 
 # export the packaged ledgers in a way where we know which one is which
 function extract_ledgers() {
@@ -142,8 +90,18 @@ extract_ledgers "$workdir/config-substituted.json" "$workdir/ledgers" "$workdir/
 mv -t /var/lib/coda "$workdir/ledgers-backup"/*
 
 echo "Performing final comparisons..."
-
 error=0
+
+result=$(jq --slurpfile a "$workdir/config-substituted.json" --slurpfile b "$PACKAGED_CONFIG_JSON" -n '
+  ($a[0].epoch_data.staking.hash == $b[0].epoch_data.staking.hash and
+   $a[0].epoch_data.next.hash == $b[0].epoch_data.next.hash and
+   $a[0].ledger.hash == $b[0].ledger.hash)')
+
+if [ "$result" != "true" ]; then
+    echo "Packaged config hashes in $PACKAGED_CONFIG_JSON not expected compared to $workdir/config-substituted.json"
+    error=1
+fi
+
 for file in "$workdir"/packaged-*.json; do
     name=$(basename "$file")
     name=${name%.json}
@@ -154,7 +112,6 @@ for file in "$workdir"/packaged-*.json; do
         error=1
     fi
 done
-
 
 for file in "$workdir"/ledgers/*.tar.gz; do
     tarname=$(basename "$file")
