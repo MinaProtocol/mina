6b878388c7ecd38164d74656fdd5b2f8e5a67666
Smorci <szekelyschmarton@gmail.com>
2024-02-22 14:05:05 +0000

    Suppress auth script output

--- src/app/delegation_verify/scripts/authenticate.sh
+++ src/app/delegation_verify/scripts/authenticate.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-set -x
-
 # The environment variables are set at the time of deployment in the init container
-CREDENTIALS=$(aws sts assume-role --role-session-name $AWS_ROLE_SESSION_NAME --role-arn $AWS_ROLE_ARN)
+CREDENTIALS=$(aws sts assume-role --role-session-name $AWS_ROLE_SESSION_NAME --role-arn $AWS_ROLE_ARN > /dev/null)
 
-ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
+ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId' > /dev/null)
 
-SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
+SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey' > /dev/null)
 
-SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken')
+SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken' > /dev/null)
 
 echo "export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$SESSION_TOKEN" > /var/mina-delegation-verify-auth/.env
