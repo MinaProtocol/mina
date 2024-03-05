6c8d9f8dd2075dfdbe8a06e5e80c1b4a5f6e272e
SanabriaRusso <sanabriarusso@gmail.com>
2024-03-05 12:15:11 -0500

    Being more verbose while failing in git diff

--- buildkite/scripts/build-release.sh
+++ buildkite/scripts/build-release.sh
@@ -20,4 +20,11 @@ echo "--- Upload debs to amazon s3 repo"
 ./buildkite/scripts/publish-deb.sh
 
 echo "--- Git diff after build is complete:"
-#git diff --exit-code -- .
+diff_output=$(git diff -- .)
+diff_length=${#diff_output}
+
+if [ $diff_length -ne 0 ]; then
+    echo "Warning: There are uncommitted changes." >&2
+else
+    echo "No changes detected."
+fi
