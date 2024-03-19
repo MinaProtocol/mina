dcce23bdf64d3098ccbb46bd3ed50a7f8f30fcc9
Richard Bonichon <richard.bonichon@gmail.com>
2024-03-19 14:21:18 +0100

    nix: overlay for ctypes
    This fixes an error happening during the compilation of `async_ssl` (apparently only on MacOS)
    
    ```
    error: builder for '/nix/store/hzypbdsxmyak6blfw2mdk81na54vxzn4-async_ssl-v0.14.0.drv' failed with exit code 1;
           last 22 log lines:
           > unpacking sources
           > unpacking source archive /nix/store/7yj6394crfc1w7waz3hqgwhwgqrrsjb6-async_ssl-v0.14.0.tar.gz
           > source root is async_ssl-v0.14.0
           > setting SOURCE_DATE_EPOCH to timestamp 1590481263 of file async_ssl-v0.14.0/stubgen/ffi_types_stubgen.mli
           > patching sources
           > updateAutotoolsGnuConfigScriptsPhase
           > configuring
           > patching script interpreter paths in .
           > building
           > preBuild
           > File "bindings/dune", line 19, characters 32-55:
           > 19 |  (libraries ctypes.stubs ctypes ctypes.foreign.threaded base)
           >                                      ^^^^^^^^^^^^^^^^^^^^^^^
           > Error: Library "ctypes.foreign.threaded" in
           > /nix/store/0cm3mqb1x094hsx4cw9lnnkk3xclyvgn-ctypes-0.17.1/lib/ocaml/4.14.0/site-lib/ctypes
           > is hidden (unsatisfied 'exist_if').
           > -> required by library "async_ssl.bindings" in _build/default/bindings
           > -> required by _build/default/META.async_ssl
           > -> required by _build/install/default/lib/async_ssl/META
           > -> required by _build/default/async_ssl.install
           > -> required by alias install
           >
    ```

--- nix/ocaml.nix
+++ nix/ocaml.nix
@@ -128,9 +128,18 @@ let
         '';
       });
 
+      # on MacOS, ctypes-foreign-threaded.cma is not installed otherwise
+      ctypes = super.ctypes.overrideAttrs
+        (oa: {
+          buildInputs = oa.buildInputs ++ [ pkgs.libffi ];
+          buildPhase = "make";
+        });
+
       # Doesn't have an explicit dependency on ctypes
       rpc_parallel = super.rpc_parallel.overrideAttrs
-        (oa: { buildInputs = oa.buildInputs ++ [ self.ctypes ]; });
+        (oa: {
+          buildInputs = oa.buildInputs ++ [ self.ctypes ];
+        });
 
       # Add specific linker configuration for MacOS ARM architecture
       postgresql = super.postgresql.overrideAttrs (_: {
