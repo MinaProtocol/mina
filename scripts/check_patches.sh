c2bbf2f2d8631d54c037b0caa367c913556273a8
ember arlynx <ember.arlynx@o1labs.org>
2024-03-28 02:21:47 -0400

    never catch me saying ASI was a mistake

--- scripts/check_patches.sh
+++ scripts/check_patches.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-function both() { git apply $1 && git apply -R $1 }
+function both() { git apply $1 && git apply -R $1; }
 both scripts/hardfork/localnet-patches/berkeley.patch
 both buildkite/scripts/caqti-upgrade.patch
 both buildkite/scripts/caqti-upgrade-plus-archive-init-speedup.patch
