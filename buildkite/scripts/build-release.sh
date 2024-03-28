4a92523580a28f20251bacab1a8b4596874dfde1
ember arlynx <ember.arlynx@o1labs.org>
2024-03-27 22:56:32 -0400

    @jrwashburn's runs shouldn't take over 24 hours

--- buildkite/scripts/build-release.sh
+++ buildkite/scripts/build-release.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 ([ -z ${DUNE_PROFILE+x} ] || [ -z ${MINA_DEB_CODENAME+x} ]) && echo "required env vars were not provided" && exit 1
 
 if [[ $1 = "archive_migration" ]] ; then
-    git apply ./buildkite/scripts/caqti-upgrade.patch
+    git apply ./buildkite/scripts/caqti-upgrade-plus-archive-init-speedup.patch
 fi
 
 source ~/.profile
@@ -24,7 +24,7 @@ echo "--- Upload debs to amazon s3 repo"
 ./buildkite/scripts/publish-deb.sh
 
 if [[ $1 = "archive_migration" ]] ; then 
-    git apply -R buildkite/scripts/caqti-upgrade.patch
+    git apply -R buildkite/scripts/caqti-upgrade-plus-archive-init-speedup.patch
 fi
 
 echo "--- Git diff after build is complete:"
