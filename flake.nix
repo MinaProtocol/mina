bb814f025e899ed5d4087ce2f616002d192985fa
georgeee <george.agapov@gmail.com>
2024-04-03 18:21:36 +0200

    After-merge fixes

--- flake.nix
+++ flake.nix
@@ -73,14 +73,6 @@
               [ (inDirectory "src") "dune" "dune-project"
                 "./graphql_schema.json" "opam.export" ];
           };
-      ocaml-src-caqti-patched = pkgs:
-        pkgs.stdenv.mkDerivation ({
-          name = "mina-src-caqti-patched";
-          src = ocaml-src;
-          phases = [ "unpackPhase" "patchPhase" "installPhase" ];
-          patches = [ ./buildkite/scripts/caqti-upgrade-plus-archive-init-speedup.patch ];
-          installPhase = "cp -R . $out";
-        });
     in {
       overlays = {
         misc = import ./nix/misc.nix;
@@ -91,10 +83,6 @@
           ocamlPackages_mina = requireSubmodules (import ./nix/ocaml.nix {
             inherit inputs pkgs;
             src = ocaml-src;
-          });
-          ocamlPackages_mina_caqti_patched = requireSubmodules (import ./nix/ocaml.nix {
-            inherit inputs pkgs;
-            src = ocaml-src-caqti-patched prev;
           });
         };
       };
@@ -310,8 +298,6 @@
         packages = rec {
           inherit (ocamlPackages)
             mina devnet mainnet mina_tests mina-ocaml-format mina_client_sdk test_executive with-instrumentation;
-          devnet-caqti-patched = pkgs.ocamlPackages_mina_caqti_patched.devnet;
-          mainnet-caqti-patched = pkgs.ocamlPackages_mina_caqti_patched.mainnet;
           inherit (pkgs)
             libp2p_helper kimchi_bindings_stubs snarky_js leaderboard
             validation trace-tool zkapp-cli;
