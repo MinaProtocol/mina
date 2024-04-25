cb00ca01e96e8f199517a562ba8cd18c76db21e6
dkijania <dariusz@o1labs.org>
2024-04-25 23:08:35 +0200

    BASE_BRANCH_COMMIT -> BUILDKITE_COMMIT

--- buildkite/scripts/version-linter-patch-missing-type-shapes.sh
+++ buildkite/scripts/version-linter-patch-missing-type-shapes.sh
@@ -16,15 +16,16 @@ release_branch=${REMOTE}/$1
 
 RELEASE_BRANCH_COMMIT=$(git log -n 1 --format="%h" --abbrev=7 --no-merges $release_branch)
 
+
 if ! $(gsutil ls gs://mina-type-shapes/$BUILDKITE_COMMIT 2>/dev/null); then
-    git checkout $BASE_BRANCH_COMMIT
+    git checkout $BUILDKITE_COMMIT
     eval $(opam config env)
     export PATH=/home/opam/.cargo/bin:/usr/lib/go/bin:$PATH
     export GO=/usr/lib/go/bin/go
     make -C src/app/libp2p_helper
 
-    dune exec src/app/cli/src/mina.exe internal dump-mina-shapes 2> ${BASE_BRANCH_COMMIT}_type-shapes.txt
-    gsutil cp ${BASE_BRANCH_COMMIT}_type-shapes.txt gs://mina-type-shapes
+    dune exec src/app/cli/src/mina.exe internal dump-mina-shapes 2> ${BUILDKITE_COMMIT}_type-shapes.txt
+    gsutil cp ${BUILDKITE_COMMIT}_type-shapes.txt gs://mina-type-shapes
 fi
 
 if ! $(gsutil ls gs://mina-type-shapes/$RELEASE_BRANCH_COMMIT 2>/dev/null); then
