4997108dc7fb2c5f59e7b9d923e3bcf25808bd2f
dkijania <dariusz@o1labs.org>
2024-04-25 16:56:09 +0200

    copy everything for devnet separetely

--- scripts/rebuild-deb.sh
+++ scripts/rebuild-deb.sh
@@ -220,6 +220,10 @@ fi
 
 ##################################### GENERATE MINA DEVNET PACKAGE #######################################
 if [[ "${DUNE_PROFILE}" == "devnet" ]]; then 
+  rm -rf "${BUILDDIR}"
+  mkdir -p "${BUILDDIR}/DEBIAN"
+
+
   ###### deb with testnet signatures
   echo "------------------------------------------------------------"
   echo "Building testnet signatures deb without keys:"
@@ -244,17 +248,52 @@ EOF
   echo "------------------------------------------------------------"
   echo "Control File:"
   cat "${BUILDDIR}/DEBIAN/control"
-
-
   echo "------------------------------------------------------------"
-  # Overwrite binaries (sudo to fix permissions error)
+
+  # Binaries
+  mkdir -p "${BUILDDIR}/usr/local/bin"
   sudo cp ./default/src/app/cli/src/mina_testnet_signatures.exe "${BUILDDIR}/usr/local/bin/mina"
   sudo cp ./default/src/app/rosetta/rosetta_testnet_signatures.exe "${BUILDDIR}/usr/local/bin/mina-rosetta"
+
+  libp2p_location=../src/app/libp2p_helper/result/bin
+  ls -l ../src/app/libp2p_helper/result/bin || libp2p_location=$HOME/app/
+  p2p_path="${BUILDDIR}/usr/local/bin/coda-libp2p_helper"
+  cp $libp2p_location/libp2p_helper $p2p_path
+  chmod +w $p2p_path
+  
+  # Only for nix builds
+  # patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 "${BUILDDIR}/usr/local/bin/coda-libp2p_helper"
+  chmod -w $p2p_path
+  # cp ./default/src/app/logproc/logproc.exe "${BUILDDIR}/usr/local/bin/mina-logproc"
+  cp ./default/src/app/runtime_genesis_ledger/runtime_genesis_ledger.exe "${BUILDDIR}/usr/local/bin/mina-create-genesis"
+ 
+  # Build Config
+  mkdir -p "${BUILDDIR}/etc/coda/build_config"
+  cp ../src/config/devnet.mlh "${BUILDDIR}/etc/coda/build_config/BUILD.mlh"
+  rsync -Huav ../src/config/* "${BUILDDIR}/etc/coda/build_config/."
+
+  # Copy the genesis ledgers and proofs as these are fairly small and very valueable to have l
+  # Genesis Ledger/proof/epoch ledger Copy
+  mkdir -p "${BUILDDIR}/var/lib/coda"
+  for f in /tmp/coda_cache_dir/genesis*; do
+      if [ -e "$f" ]; then
+          mv /tmp/coda_cache_dir/genesis* "${BUILDDIR}/var/lib/coda/."
+      fi
+  done
+
+    # Bash autocompletion
+  # NOTE: We do not list bash-completion as a required package,
+  #       but it needs to be present for this to be effective
+  mkdir -p "${BUILDDIR}/etc/bash_completion.d"
+  cwd=$(pwd)
+  export PATH=${cwd}/${BUILDDIR}/usr/local/bin/:${PATH}
+  env COMMAND_OUTPUT_INSTALLATION_BASH=1 mina  > "${BUILDDIR}/etc/bash_completion.d/mina"
 
   # Switch the default configuration to devnet.json:
   mkdir -p "${BUILDDIR}/var/lib/coda"
   sudo cp ../genesis_ledgers/devnet.json "${BUILDDIR}/var/lib/coda/config_${GITHASH_CONFIG}.json"
-
+  sudo cp ../genesis_ledgers/devnet.json "${BUILDDIR}/var/lib/coda/devnet.json"
+  
 
   mkdir -p "${BUILDDIR}/usr/lib/systemd/user"
   # Overwrite the mina.service with a new default PEERS_URL
