31a69ae5c742042224dfddf645711d90f1937329
dkijania <dariusz@o1labs.org>
2024-05-08 22:55:00 +0200

    create local deb folder

--- buildkite/scripts/debian/start_local_repo.sh
+++ buildkite/scripts/debian/start_local_repo.sh
@@ -16,6 +16,7 @@ apt-get install aptly
 
 # Download locally static debians (for example mina-legacy )
 
+mkdir -p $LOCAL_DEB_FOLDER
 gsutil -m cp "gs://buildkite_k8s/coda/shared/debs/$MINA_DEB_CODENAME/*" $LOCAL_DEB_FOLDER
 source ./buildkite/scripts/export-git-env-vars.sh
 source ./buildkite/scripts/download-artifact-from-cache.sh _build $MINA_DEB_CODENAME -r 
