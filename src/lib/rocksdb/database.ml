454acdf7c8fed2b57170ab6d312eadea43d1fd36
georgeee <george.agapov@gmail.com>
2024-05-09 18:44:39 +0200

    Revert "Fix a warning in key_value_database.ml"
    This reverts commit 6eaffa5e28a88dfbf6c277c607c731883cfdd4eb.

--- src/lib/rocksdb/database.ml
+++ src/lib/rocksdb/database.ml
@@ -31,8 +31,8 @@ let set t ~(key : Bigstring.t) ~(data : Bigstring.t) : unit =
   Rocks.put ?key_pos:None ?key_len:None ?value_pos:None ?value_len:None
     ?opts:None t.db key data
 
-let set_batch t ?(remove_keys : Bigstring.t list = [])
-    ~(key_data_pairs : (Bigstring.t * Bigstring.t) list) () : unit =
+let[@warning "-16"] set_batch t ?(remove_keys : Bigstring.t list = [])
+    ~(key_data_pairs : (Bigstring.t * Bigstring.t) list) : unit =
   let batch = Rocks.WriteBatch.create () in
   (* write to batch *)
   List.iter key_data_pairs ~f:(fun (key, data) ->
