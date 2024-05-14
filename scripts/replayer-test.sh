71f24dcfdeff95c9286a606095e87812555c7b31
sai <sai@o1labs.org>
2024-05-14 13:47:05 -0500

    adding slot tests to buildkite

--- scripts/replayer-test.sh
+++ scripts/replayer-test.sh
@@ -2,12 +2,12 @@
 
 # test replayer on known archive db
 
-REPLAYER_DIR=src/app/replayer
+ARCHIVE_DIR=src/test/archive
 REPLAYER_APP=_build/default/src/app/replayer/replayer.exe
-PG_CONN=PG_CONN=postgres://postgres:postgres@localhost:5433/archive
+PG_CONN=postgres://postgres:postgres@localhost:5433/archive
 
 while [[ "$#" -gt 0 ]]; do case $1 in
-  -d|--dir) REPLAYER_DIR="$2"; shift;;
+  -d|--dir) ARCHIVE_DIR="$2"; shift;;
   -a|--app) REPLAYER_APP="$2"; shift;;
   -p| --pg) PG_CONN="$2"; shift;;
   *) echo "Unknown parameter passed: $1"; exit 1;;
@@ -22,7 +22,7 @@ function report () {
 }
 
 echo "Running replayer"
-$REPLAYER_APP --archive-uri $PG_CONN --input-file $REPLAYER_DIR/test/input.json
+$REPLAYER_APP --archive-uri $PG_CONN --input-file $ARCHIVE_DIR/test/input.json
 
 RESULT=$?
 
