9158632c9fb4a6d6190abb87ca35b9d6a7d7bc26
amc-ie <72793054+amc-ie@users.noreply.github.com>
2024-06-07 14:54:34 +0100

    Updating silly CI config error

--- buildkite/scripts/export-git-env-vars.sh
+++ buildkite/scripts/export-git-env-vars.sh
@@ -58,7 +58,7 @@ fi
 
 # Determine the packages to build (mainnet y/N)
 case $GITBRANCH in
-    compatible|master|berkeley|release-automation-testing/*|release/1*|release/3*) # whitelist of branches that are "mainnet-like"
+    compatible|master|feature/allow-resending-local-txns|release-automation-testing/*|release/1*|release/3*) # whitelist of branches that are "mainnet-like"
       MINA_BUILD_MAINNET=true ;;
     *) # Other branches
       MINA_BUILD_MAINNET=false ;;
