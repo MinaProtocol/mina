ff841690fdef6c3b2dead663bef7038c290d8ea6
dkijania <dariusz@o1labs.org>
2024-06-19 00:08:53 +0200

    add architecture suffix for debian

--- buildkite/scripts/debian/install.sh
+++ buildkite/scripts/debian/install.sh
@@ -49,7 +49,7 @@ fi
 
 debs_with_version=()
 for i in "${debs[@]}"; do
-   debs_with_version+=("${i}_${MINA_DEB_VERSION}")
+   debs_with_version+=("${i}_${MINA_DEB_VERSION}_amd64")
 done
 
 # Install aptly
