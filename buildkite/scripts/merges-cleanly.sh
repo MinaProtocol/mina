fac204e319f8a81b0b25ef838a4541f02629a8fa
dkijania <dariusz@o1labs.org>
2024-07-03 23:47:56 +0200

    add safe.directory

--- buildkite/scripts/merges-cleanly.sh
+++ buildkite/scripts/merges-cleanly.sh
@@ -5,6 +5,8 @@ CURRENT=$(git branch --show-current)
 echo 'Testing for conflicts between the current branch `'"${CURRENT}"'` and `'"${BRANCH}"'`...'
 
 
+git config --global --add safe.directory /workdir
+
 # Adapted from this stackoverflow answer: https://stackoverflow.com/a/10856937
 # The git merge-tree command shows the content of a 3-way merge without
 # touching the index, which we can then search for conflict markers.
