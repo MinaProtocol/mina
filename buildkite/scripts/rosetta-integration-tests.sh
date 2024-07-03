d6ccccc07bc24f7e829cff780c69adc3a17a5aa1
sai <sai@o1labs.org>
2024-07-03 17:27:28 -0400

    removing bad check

--- buildkite/scripts/rosetta-integration-tests.sh
+++ buildkite/scripts/rosetta-integration-tests.sh
@@ -253,13 +253,8 @@ fi
 mina client status --json
 next_block_time=$(mina client status --json | jq '.next_block_production.timing[1].time' | tr -d '"') curr_time=$(date +%s%N | cut -b1-13)
 sleep_time=$((($next_block_time - $curr_time) / 1000))
-# Check if sleep_time is negative
-if [ $sleep_time -lt 0 ]; then
-    echo "Sleep time is negative. Skipping sleep..."
-else
-    echo "Sleeping for ${sleep_time}s until next block is created..."
-    sleep $sleep_time
-fi
+echo "Sleeping for ${sleep_time}s until next block is created..."
+sleep $sleep_time
 
 # Mina Rosetta Checks (spec construction data perf)
 echo "============ ROSETTA CLI: VALIDATE CONF FILE ${ROSETTA_CONFIGURATION_FILE} =============="
