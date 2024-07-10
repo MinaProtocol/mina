413f39a9faf9a72eafa1bc34c7e6abbdbc1d2d78
Geometer1729 <16kuhnb@gmail.com>
2024-07-10 11:42:44 -0400

    could work

--- src/lib/mina_ledger/lmdb_kvdb.ml
+++ src/lib/mina_ledger/lmdb_kvdb.ml
@@ -3,14 +3,19 @@ open Core_kernel
 
 module Kvdb = struct
       type t =
-          { mutable lmdb : (Bigstring.t,Bigstring.t,[ `Uni ]) Lmdb.Map.t
-          ; path : string
-          }
-
-      let t_of_sexp _sexp = failwith "Can't deserialize a database handle?"
-      let sexp_of_t _t = failwith "TODO serialize?"
+        { mutable lmdb : ((Bigstring.t,Bigstring.t,[ `Uni ]) Lmdb.Map.t [@sexp.opaque])
+        ; path : string
+        ; uuid : Uuid.t
+        }
+      [@@deriving sexp]
 
       let trivial_conv : Bigstring.t Lmdb.Conv.t = Lmdb.Conv.make ~serialise:(Fn.const Fn.id) ~deserialise:Fn.id ()
+      (* TODO is this good enough? what other data can I use to make it more trustably unique? *)
+      let name_to_uuid name = String.to_array name
+                  |> Array.map ~f:Char.to_int
+                  |> Random.State.make
+                  |> Uuid.create_random
+
 
       let create (conf : string) = let
         lmdb =
@@ -19,10 +24,12 @@ module Kvdb = struct
         (Lmdb.Env.create Rw conf)
         ~key:trivial_conv
         ~value:trivial_conv
-        in { lmdb ; path = conf }
+      in { lmdb
+         ; path = conf
+         ; uuid = name_to_uuid conf
+         }
 
       let close t = Lmdb.Env.close (Lmdb.Map.env t.lmdb)
-
 
       let create_checkpoint t name =
       let env = Lmdb.Map.env t.lmdb in
@@ -32,11 +39,12 @@ module Kvdb = struct
         let new_db = Lmdb.Map.open_existing Nodup ~key:trivial_conv ~value:trivial_conv (Lmdb.Env.create Ro name) in
         { lmdb = new_db
         ; path = name
+        ; uuid = name_to_uuid name
+          (* Can this be the same, if not what can I use instead of the path? *)
         }
 
       (* AFAICT this can be a no-op because it doesn't return a handle so why copy? *)
       let make_checkpoint _t _name = ()
-
 
       let get t ~key = try Some(Lmdb.Map.get t.lmdb key) with
           | Not_found_s _ -> None
@@ -81,5 +89,5 @@ module Kvdb = struct
               handle init entry
             )
 
-      let get_uuid _t = failwith "TODO"
+      let get_uuid t = t.uuid
 end
