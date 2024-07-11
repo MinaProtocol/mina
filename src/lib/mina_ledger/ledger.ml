4f7d43268b92e51b4fff159be6504f955d72340f
Geometer1729 <16kuhnb@gmail.com>
2024-07-11 08:52:52 -0400

    Update src/lib/mina_ledger/ledger.ml
    Co-authored-by: Richard Bonichon <richard.bonichon@o1labs.org>

--- src/lib/mina_ledger/ledger.ml
+++ src/lib/mina_ledger/ledger.ml
@@ -27,7 +27,7 @@ module Ledger_inner = struct
     include Hashable.Make_binable (Arg) [@@deriving sexp, compare, hash, yojson]
   end
 
-  module Kvdb : Intf.Key_value_database with type config := string = Kvdb
+  module Kvdb : Intf.Key_value_database with type config := string = Lmdb_kvdb.Kvdb
 
   module Storage_locations : Intf.Storage_locations = struct
     let key_value_db_dir = "mina_key_value_db"
