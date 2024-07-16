07f31359952b22a97147889ec55935d58129b105
Richard Bonichon <richard.bonichon@gmail.com>
2024-07-16 20:40:27 -0300

    Test free list: direct use of module
    - do not rely on Alcotest.testable, there is no need to

--- src/lib/merkle_ledger_tests/test_free_list.ml
+++ src/lib/merkle_ledger_tests/test_free_list.ml
@@ -11,14 +11,6 @@ module L = Test.Location
 module A = L.Addr
 module F = Merkle_ledger.Free_list.Make (Test.Location)
 
-let enumerate_unique_leaves_combinations max_depth =
-  Sequence.range 0 max_depth
-  |> Sequence.fold ~init:[ [] ] ~f:(fun acc _ ->
-         List.map acc ~f:(List.cons Direction.Left)
-         @ List.map acc ~f:(List.cons Direction.Right) )
-
-let freelist_testable = Alcotest.testable F.pp F.equal
-
 let gen max_depth =
   let open Quickcheck.Generator.Let_syntax in
   let%bind ledger_depth = Int.gen_incl 1 max_depth in
@@ -30,7 +22,8 @@ let test_de_serializaion =
       Quickcheck.test (gen 5) ~f:(fun (ledger_depth, free_list) ->
           let bs = F.serialize ~ledger_depth free_list in
           let deserialized = F.deserialize ~ledger_depth bs in
-          Alcotest.check freelist_testable
+          Alcotest.check
+            (module F)
             "serialized and deserialized free lists are the same" free_list
             deserialized ) )
 
