e7fae90686e911c4bbe19e30070ee80a35245999
martyall <martyall@protonmail.com>
2024-07-17 16:20:18 -0700

    Doh

--- buildkite/scripts/unit-test.sh
+++ buildkite/scripts/unit-test.sh
@@ -14,6 +14,8 @@ source ~/.profile
 
 export MINA_LIBP2P_PASS="naughty blue worm"
 export NO_JS_BUILD=1 # skip some JS targets which have extra implicit dependencies
+export USE_LAGRANGE_CACHE=true
+echo "The value of USE_LAGRANGE_CACHE is in the dev unit test script is $USE_LAGRANGE_CACHE"
 
 echo "--- Make build"
 export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
@@ -29,8 +31,6 @@ time dune runtest "src/app/print_blockchain_snark_vk" --profile="${profile}" -j1
 # be updated.
 export ERROR_ON_PROOF=true
 
-export USE_LAGRANGE_CACHE=true
-echo "The value of USE_LAGRANGE_CACHE is in the dev unit test script is $USE_LAGRANGE_CACHE"
 
 # Note: By attempting a re-run on failure here, we can avoid rebuilding and
 # skip running all of the tests that have already succeeded, since dune will
