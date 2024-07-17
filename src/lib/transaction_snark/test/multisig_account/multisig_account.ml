ab3e678348f10399d48d57afd01c059e4931448f
Richard Bonichon <richard.bonichon@gmail.com>
2024-07-17 10:33:03 -0300

    Tests: propagate delete_account more

--- src/lib/transaction_snark/test/multisig_account/multisig_account.ml
+++ src/lib/transaction_snark/test/multisig_account/multisig_account.ml
@@ -357,6 +357,7 @@ let%test_module "multisig_account" =
                     ; use_full_commitment = false
                     ; may_use_token = No
                     ; authorization_kind = Signature
+                    ; delete_account = false
                     }
                 ; authorization = Signature Signature.dummy
                 }
@@ -383,6 +384,7 @@ let%test_module "multisig_account" =
                     ; use_full_commitment = false
                     ; may_use_token = No
                     ; authorization_kind = Proof (With_hash.hash vk)
+                    ; delete_account = false
                     }
                 ; authorization =
                     Proof (Lazy.force Mina_base.Proof.transaction_dummy)
