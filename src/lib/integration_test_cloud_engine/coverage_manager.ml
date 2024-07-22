d46fce041705fabc6553036cdd936c39d04ac857
dkijania <dariusz@o1labs.org>
2024-07-23 00:31:33 +0200

    fmt

--- src/lib/integration_test_cloud_engine/coverage_manager.ml
+++ src/lib/integration_test_cloud_engine/coverage_manager.ml
@@ -17,39 +17,39 @@ module Coverage_manager = struct
     let root = "/root/.mina-config" in
     let open Malleable_error.Let_syntax in
     [%log' info t.logger] "Generating test coverage data..." ;
-    let%bind coverage_files = nodes |> Map.to_alist |> Malleable_error.List.map ~f:(fun (_id,pod) -> 
-        let pod_id = Node.id pod in
-        let%bind () = Node.stop pod in
-        [%log' info t.logger] "Mina process in '$pod' killed."
-          ~metadata:[ ("pod", `String pod_id) ] ;
-        let%bind files_in_root = Node.list_files pod ~root in
-        [%log' debug t.logger] "Listing files in  '$pod' '$list'"
-          ~metadata:
-            [ ("pod", `String pod_id)
-            ; ( "list"
-              , `List (List.map files_in_root ~f:(fun x -> `String x)) )
-            ] ;
-        let%bind coverage_files =
-          files_in_root
-          |> List.filter ~f:(String.is_substring ~substring:".coverage")
-          |> Malleable_error.List.map ~f:(fun coverage_file ->
-                 [%log' info t.logger]
-                   "Downloading coverage file to ($file) from $pod'."
-                   ~metadata:
-                     [ ("file", `String coverage_file)
-                     ; ("pod", `String pod_id)
-                     ] ;
-                 let%bind () =
-                   Node.download_file_legacy pod
-                     ~source_file:
-                       (String.concat ~sep:"/" [ root; coverage_file ])
-                     ~target_file:coverage_file
-                 in
-                 Malleable_error.return coverage_file )
-        in
-        Malleable_error.return coverage_files
-      )
-      in
-      Malleable_error.return (List.concat coverage_files )
-      
+    let%bind coverage_files =
+      nodes |> Map.to_alist
+      |> Malleable_error.List.map ~f:(fun (_id, pod) ->
+             let pod_id = Node.id pod in
+             let%bind () = Node.stop pod in
+             [%log' info t.logger] "Mina process in '$pod' killed."
+               ~metadata:[ ("pod", `String pod_id) ] ;
+             let%bind files_in_root = Node.list_files pod ~root in
+             [%log' debug t.logger] "Listing files in  '$pod' '$list'"
+               ~metadata:
+                 [ ("pod", `String pod_id)
+                 ; ( "list"
+                   , `List (List.map files_in_root ~f:(fun x -> `String x)) )
+                 ] ;
+             let%bind coverage_files =
+               files_in_root
+               |> List.filter ~f:(String.is_substring ~substring:".coverage")
+               |> Malleable_error.List.map ~f:(fun coverage_file ->
+                      [%log' info t.logger]
+                        "Downloading coverage file to ($file) from $pod'."
+                        ~metadata:
+                          [ ("file", `String coverage_file)
+                          ; ("pod", `String pod_id)
+                          ] ;
+                      let%bind () =
+                        Node.download_file_legacy pod
+                          ~source_file:
+                            (String.concat ~sep:"/" [ root; coverage_file ])
+                          ~target_file:coverage_file
+                      in
+                      Malleable_error.return coverage_file )
+             in
+             Malleable_error.return coverage_files )
+    in
+    Malleable_error.return (List.concat coverage_files)
 end
--- src/lib/integration_test_local_engine/coverage_manager.ml
+++ src/lib/integration_test_local_engine/coverage_manager.ml
@@ -17,32 +17,33 @@ module Coverage_manager = struct
     let open Docker_network in
     let root = "/root/.mina-config" in
     [%log' info t.logger] "Generating test coverage data..." ;
-    let%bind coverage_files =  nodes |> Map.to_alist
-    |> Malleable_error.List.map ~f:(fun (_id, node) ->
-           let%bind () = Node.stop node in
-           let container_id = Node.id node in
-           let%bind files_in_root =
-             Node.list_files ~logger:t.logger node ~root
-           in
-           let%bind coverage_files =
-             files_in_root
-             |> List.filter ~f:(String.is_substring ~substring:".coverage")
-             |> Malleable_error.List.map ~f:(fun coverage_file ->
-                    [%log' info t.logger]
-                      "Downloading coverage file to ($file) from $pod'."
-                      ~metadata:
-                        [ ("file", `String coverage_file)
-                        ; ("container", `String container_id)
-                        ] ;
-                    let%bind _ =
-                      Node.copy_file_from_container node
-                        (Printf.sprintf "%s/%s" root coverage_file)
-                        coverage_file
-                    in
-                    Malleable_error.return coverage_file
-              ) in
-          Malleable_error.return coverage_files
-     ) in
+    let%bind coverage_files =
+      nodes |> Map.to_alist
+      |> Malleable_error.List.map ~f:(fun (_id, node) ->
+             let%bind () = Node.stop node in
+             let container_id = Node.id node in
+             let%bind files_in_root =
+               Node.list_files ~logger:t.logger node ~root
+             in
+             let%bind coverage_files =
+               files_in_root
+               |> List.filter ~f:(String.is_substring ~substring:".coverage")
+               |> Malleable_error.List.map ~f:(fun coverage_file ->
+                      [%log' info t.logger]
+                        "Downloading coverage file to ($file) from $pod'."
+                        ~metadata:
+                          [ ("file", `String coverage_file)
+                          ; ("container", `String container_id)
+                          ] ;
+                      let%bind _ =
+                        Node.copy_file_from_container node
+                          (Printf.sprintf "%s/%s" root coverage_file)
+                          coverage_file
+                      in
+                      Malleable_error.return coverage_file )
+             in
+             Malleable_error.return coverage_files )
+    in
 
-      Malleable_error.return (List.concat coverage_files )
+    Malleable_error.return (List.concat coverage_files)
 end
