de9ece9d729ac3d582875b0557b52344bd36c79c
Dariusz Kijania <dariusz.kijania@gmail.com>
2024-07-23 18:42:48 +0200

    Merge pull request #15858 from MinaProtocol/terraform-tests
    Fix Terraform tests by synthetically creating required seed peerid files

--- automation/terraform/modules/o1-testnet/files.tf
+++ automation/terraform/modules/o1-testnet/files.tf
@@ -15,10 +15,23 @@
 #   ]
 # }
 
+# Generate random strings for peer IDs
+resource "random_id" "peer_id" {
+  for_each = var.create_libp2p_files ? toset(local.seed_names) : []
+  byte_length = 32
+}
+
+# Create the files with the generated peer IDs
+resource "local_file" "libp2p_seed_peers" {
+  for_each = var.create_libp2p_files ? toset(local.seed_names) : []
+  filename = "${var.artifact_path}/keys/libp2p-keys/${each.key}.peerid"
+  content  = random_id.peer_id[each.key].hex
+}
+
 data "local_file" "libp2p_seed_peers" {
   for_each = toset(local.seed_names)
   filename = "${var.artifact_path}/keys/libp2p-keys/${each.key}.peerid"
-  #   depends_on = [
-  #     null_resource.block_producer_key_generation
-  #   ]
+  depends_on = [
+    local_file.libp2p_seed_peers
+  ]
 }
