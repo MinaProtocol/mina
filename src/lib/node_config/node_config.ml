d5798864be2c0406e47135db1a21c27ae445a509
martyall <martyall@protonmail.com>
2024-07-25 17:52:33 -0700

    wip

--- src/lib/node_config/node_config.ml
+++ src/lib/node_config/node_config.ml
@@ -1,172 +1,3 @@
-[%%import "/src/config.mlh"]
-
-(** This file consists of compile-time constants that are not in
-    Genesis_constants.
-    This file includes all of the constants defined at compile-time for both
-    tests and production.
-*)
-
-[%%inject "ledger_depth", ledger_depth]
-
-[%%inject "curve_size", curve_size]
-
-[%%inject "coinbase", coinbase]
-
-[%%inject "k", k]
-
-[%%inject "delta", delta]
-
-[%%inject "slots_per_epoch", slots_per_epoch]
-
-[%%inject "slots_per_sub_window", slots_per_sub_window]
-
-[%%inject "sub_windows_per_window", sub_windows_per_window]
-
-[%%inject "grace_period_slots", grace_period_slots]
-
-[%%inject "scan_state_with_tps_goal", scan_state_with_tps_goal]
-
-[%%ifndef scan_state_transaction_capacity_log_2]
-
-let scan_state_transaction_capacity_log_2 : int option = None
-
-[%%else]
-
-[%%inject
-"scan_state_transaction_capacity_log_2", scan_state_transaction_capacity_log_2]
-
-let scan_state_transaction_capacity_log_2 =
-  Some scan_state_transaction_capacity_log_2
-
-[%%endif]
-
-[%%inject "scan_state_work_delay", scan_state_work_delay]
-
-[%%inject "debug_logs", debug_logs]
-
-[%%inject "call_logger", call_logger]
-
-[%%inject "cache_exceptions", cache_exceptions]
-
-[%%inject "record_async_backtraces", record_async_backtraces]
-
-[%%inject "proof_level", proof_level]
-
-[%%inject "pool_max_size", pool_max_size]
-
-[%%inject "account_creation_fee_int", account_creation_fee_int]
-
-[%%inject "default_transaction_fee", default_transaction_fee]
-
-[%%inject "default_snark_worker_fee", default_snark_worker_fee]
-
-[%%inject "minimum_user_command_fee", minimum_user_command_fee]
-
-[%%inject "protocol_version_transaction", protocol_version_transaction]
-
-[%%inject "protocol_version_network", protocol_version_network]
-
-[%%inject "protocol_version_patch", protocol_version_patch]
-
-[%%inject "supercharged_coinbase_factor", supercharged_coinbase_factor]
-
-[%%inject "time_offsets", time_offsets]
-
-[%%inject "plugins", plugins]
-
-[%%inject "genesis_ledger", genesis_ledger]
-
-[%%inject "genesis_state_timestamp", genesis_state_timestamp]
-
-[%%inject "block_window_duration", block_window_duration]
-
-[%%inject "integration_tests", integration_tests]
-
-[%%inject "force_updates", force_updates]
-
-[%%inject "download_snark_keys", download_snark_keys]
-
-[%%inject "generate_genesis_proof", generate_genesis_proof]
-
-[%%inject "itn_features", itn_features]
-
-[%%ifndef compaction_interval]
-
-let compaction_interval = None
-
-[%%else]
-
-[%%inject "compaction_interval", compaction_interval]
-
-let compaction_interval = Some compaction_interval
-
-[%%endif]
-
-[%%inject "network", network]
-
-[%%inject "vrf_poll_interval", vrf_poll_interval]
-
-[%%ifndef zkapp_cmd_limit]
-
-let zkapp_cmd_limit = None
-
-[%%else]
-
-[%%inject "zkapp_cmd_limit", zkapp_cmd_limit]
-
-let zkapp_cmd_limit = Some zkapp_cmd_limit
-
-[%%endif]
-
-[%%ifndef slot_tx_end]
-
-let slot_tx_end : int option = None
-
-[%%else]
-
-[%%inject "slot_tx_end", slot_tx_end]
-
-let slot_tx_end = Some slot_tx_end
-
-[%%endif]
-
-[%%ifndef slot_chain_end]
-
-let slot_chain_end : int option = None
-
-[%%else]
-
-[%%inject "slot_chain_end", slot_chain_end]
-
-let slot_chain_end = Some slot_chain_end
-
-[%%endif]
-
-[%%ifndef scan_state_tps_goal_x10]
-
-let scan_state_tps_goal_x10 : int option = None
-
-[%%else]
-
-[%%inject "scan_state_tps_goal_x10", scan_state_tps_goal_x10]
-
-let scan_state_tps_goal_x10 = Some scan_state_tps_goal_x10
-
-[%%endif]
-
-(*
-let load_config_file filename =
-  let open Deferred.Or_error.Let_syntax in
-  Monitor.try_with_join_or_error ~here:[%here] (fun () ->
-      let%map json = load_config_json filename in
-      match Runtime_config.of_yojson json with
-      | Ok config ->
-          Ok config
-      | Error err ->
-          Or_error.error_string err )
-
-*)
-
 type t = {
     ledger_depth : int
   ; curve_size : int
@@ -213,50 +44,203 @@ type t = {
   ; scan_state_tps_goal_x10 : int option
 } [@@deriving to_yojson, of_yojson]
 
-let config: t = 
-  { ledger_depth
-  ; curve_size
-  ; coinbase
-  ; k
-  ; delta
-  ; slots_per_epoch
-  ; slots_per_sub_window
-  ; sub_windows_per_window
-  ; grace_period_slots
-  ; scan_state_with_tps_goal
-  ; scan_state_transaction_capacity_log_2
-  ; scan_state_work_delay
-  ; debug_logs
-  ; call_logger
-  ; cache_exceptions
-  ; record_async_backtraces
-  ; proof_level
-  ; pool_max_size
-  ; account_creation_fee_int
-  ; default_transaction_fee
-  ; default_snark_worker_fee
-  ; minimum_user_command_fee
-  ; protocol_version_transaction
-  ; protocol_version_network
-  ; protocol_version_patch
-  ; supercharged_coinbase_factor
-  ; time_offsets
-  ; plugins
-  ; genesis_ledger
-  ; genesis_state_timestamp
-  ; block_window_duration
-  ; integration_tests
-  ; force_updates
-  ; download_snark_keys
-  ; generate_genesis_proof
-  ; itn_features
-  ; compaction_interval
-  ; vrf_poll_interval
-  ; network
-  ; zkapp_cmd_limit
-  ; slot_tx_end
-  ; slot_chain_end
-  ; scan_state_tps_goal_x10
-  }
+type network
+  = Dev
+  | Lightnet
+  | Devnet
+  | Mainnet
 
-let () = to_yojson config |> Yojson.Safe.pretty_to_string |> print_endline
+let read_network_env_var = 
+  match Option.map String.lowercase_ascii @@ Sys.getenv_opt "MINA_NETWORK" with
+    | Some "lightnet" -> Lightnet
+    | Some "devnet" -> Devnet
+    | Some "mainnet" -> Mainnet
+    | _ -> Dev
+
+let read_network_config config_dir= 
+  let config_file = match read_network_env_var with
+    | Dev -> "dev.json"
+    | Lightnet -> "lightnet.json"
+    | Devnet -> "devnet.json"
+    | Mainnet -> "mainnet.json"
+  in 
+    Yojson.Safe.from_file (String.cat config_dir config_file)
+
+let config = lazy (
+  read_network_config "node_config/" |> of_yojson |> Result.get_ok
+)
+
+let get () = Lazy.force config
+
+let ledger_depth = 
+  let c = get ()
+  in c.ledger_depth
+
+let curve_size =
+  let c = get ()
+  in c.curve_size
+
+let coinbase =
+  let c = get ()
+  in c.coinbase
+
+let k =
+  let c = get ()
+  in c.k
+
+let delta =
+  let c = get ()
+  in c.delta
+
+let slots_per_epoch =
+  let c = get ()
+  in c.slots_per_epoch
+
+let slots_per_sub_window =
+  let c = get ()
+  in c.slots_per_sub_window
+
+let sub_windows_per_window =
+  let c = get ()
+  in c.sub_windows_per_window
+
+let grace_period_slots =
+  let c = get ()
+  in c.grace_period_slots
+
+let scan_state_with_tps_goal =
+  let c = get ()
+  in c.scan_state_with_tps_goal
+
+let scan_state_transaction_capacity_log_2 =
+  let c = get ()
+  in c.scan_state_transaction_capacity_log_2
+
+let scan_state_work_delay =
+  let c = get ()
+  in c.scan_state_work_delay
+
+let debug_logs =
+  let c = get ()
+  in c.debug_logs
+
+let call_logger =
+  let c = get ()
+  in c.call_logger
+
+let cache_exceptions =
+  let c = get ()
+  in c.cache_exceptions
+
+let record_async_backtraces =
+  let c = get ()
+  in c.record_async_backtraces
+
+let proof_level =
+  let c = get ()
+  in c.proof_level
+
+
+let pool_max_size =
+  let c = get ()
+  in c.pool_max_size
+
+let account_creation_fee_int =
+  let c = get ()
+  in c.account_creation_fee_int
+
+let default_transaction_fee =
+  let c = get ()
+  in c.default_transaction_fee
+
+let default_snark_worker_fee =
+  let c = get ()
+  in c.default_snark_worker_fee
+
+let minimum_user_command_fee =
+  let c = get ()
+  in c.minimum_user_command_fee
+
+let protocol_version_transaction =
+  let c = get ()
+  in c.protocol_version_transaction
+
+let protocol_version_network =
+  let c = get ()
+  in c.protocol_version_network
+
+let protocol_version_patch =
+  let c = get ()
+  in c.protocol_version_patch
+
+let supercharged_coinbase_factor =
+  let c = get ()
+  in c.supercharged_coinbase_factor
+
+let time_offsets =
+  let c = get ()
+  in c.time_offsets
+
+let plugins =
+  let c = get ()
+  in c.plugins
+
+let genesis_ledger =
+  let c = get ()
+  in c.genesis_ledger
+
+let genesis_state_timestamp =
+  let c = get ()
+  in c.genesis_state_timestamp
+
+let block_window_duration =
+  let c = get ()
+  in c.block_window_duration
+
+let integration_tests =
+  let c = get ()
+  in c.integration_tests
+
+let force_updates =
+  let c = get ()
+  in c.force_updates
+
+let download_snark_keys =
+  let c = get ()
+  in c.download_snark_keys
+
+let generate_genesis_proof =
+  let c = get ()
+  in c.generate_genesis_proof
+
+let itn_features =
+  let c = get ()
+  in c.itn_features
+
+let compaction_interval =
+  let c = get ()
+  in c.compaction_interval
+
+let vrf_poll_interval =
+  let c = get ()
+  in c.vrf_poll_interval
+
+let network =
+  let c = get ()
+  in c.network
+
+let zkapp_cmd_limit =
+  let c = get ()
+  in c.zkapp_cmd_limit
+
+let slot_tx_end =
+  let c = get ()
+  in c.slot_tx_end
+
+let slot_chain_end =
+  let c = get ()
+  in c.slot_chain_end
+
+let scan_state_tps_goal_x10 =
+  let c = get ()
+  in c.scan_state_tps_goal_x10
