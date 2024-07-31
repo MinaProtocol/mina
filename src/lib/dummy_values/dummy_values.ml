1db041843fd7acecba3fdd2cd27edfb3c84c8ca7
martyall <martyall@protonmail.com>
2024-07-30 23:21:24 -0700

    delay evaluation of pseudorandom function with lazy

--- src/lib/dummy_values/dummy_values.ml
+++ src/lib/dummy_values/dummy_values.ml
@@ -1,7 +1,7 @@
 open Pickles_types
 
-let blockchain_proof () =
-  Pickles.Proof.dummy Nat.N2.n Nat.N2.n Nat.N2.n ~domain_log2:16
+let blockchain_proof =
+  lazy (Pickles.Proof.dummy Nat.N2.n Nat.N2.n Nat.N2.n ~domain_log2:16)
 
-let transaction_proof () =
-  Pickles.Proof.dummy Nat.N2.n Nat.N2.n Nat.N0.n ~domain_log2:14
+let transaction_proof =
+  lazy (Pickles.Proof.dummy Nat.N2.n Nat.N2.n Nat.N0.n ~domain_log2:14)
