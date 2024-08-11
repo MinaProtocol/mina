4783efbe826f5163abffd09629d5c6ea114e7ef5
mrmr1993 <mr_1993@hotmail.co.uk>
2024-08-11 19:47:38 +0100

    Move interface into its own library

---
+++ src/lib/node_config/intf/node_config_intf.mli
@@ -0,0 +1,69 @@
+module type S = sig
+  val ledger_depth : int
+
+  val curve_size : int
+
+  val coinbase : string
+
+  val k : int
+
+  val delta : int
+
+  val slots_per_epoch : int
+
+  val slots_per_sub_window : int
+
+  val sub_windows_per_window : int
+
+  val grace_period_slots : int
+
+  val scan_state_with_tps_goal : bool
+
+  val scan_state_transaction_capacity_log_2 : int option
+
+  val scan_state_work_delay : int
+
+  val cache_exceptions : bool
+
+  val proof_level : string
+
+  val pool_max_size : int
+
+  val account_creation_fee_int : string
+
+  val default_transaction_fee : string
+
+  val default_snark_worker_fee : string
+
+  val minimum_user_command_fee : string
+
+  val protocol_version_transaction : int
+
+  val protocol_version_network : int
+
+  val protocol_version_patch : int
+
+  val supercharged_coinbase_factor : int
+
+  val plugins : bool
+
+  val genesis_state_timestamp : string
+
+  val block_window_duration : int
+
+  val itn_features : bool
+
+  val compaction_interval : int option
+
+  val vrf_poll_interval : int
+
+  val network : string
+
+  val zkapp_cmd_limit : int option
+
+  val slot_tx_end : int option
+
+  val slot_chain_end : int option
+
+  val scan_state_tps_goal_x10 : int option
+end
