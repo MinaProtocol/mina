4b111b1d29fc661ecb69c8455feb8bf5c64fd82a
martyall <martyall@protonmail.com>
2024-08-20 09:28:29 -0700

    remove value from config module, give config mli file

--- src/lib/genesis_constants/compiled/genesis_constants_compiled.ml
+++ src/lib/genesis_constants/compiled/genesis_constants_compiled.ml
@@ -1,1 +1,13 @@
-include Genesis_constants.Make (Node_config)
+type t = { 
+  proof_level: Genesis_constants.Proof_level.t;
+  constraint_constants: Genesis_constants.Constraint_constants.t;
+  genesis_constants: Genesis_constants.t;
+}
+
+
+module M =  Genesis_constants.Make(Node_config)
+let compiled_config = 
+  { proof_level = M.Proof_level.t
+  ; constraint_constants = M.Constraint_constants.t
+  ; genesis_constants = M.t
+  }
