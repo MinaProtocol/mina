e6c62d4f3a01cab811ff75b273fc01b646a5f778
Nathan Holland <nholland94@gmail.com>
2024-08-21 20:13:04 -0400

    Refactor Mina RPC implementations into unified location

--- src/lib/network_peer/network_peer.ml
+++ src/lib/network_peer/network_peer.ml
@@ -1,14 +1,14 @@
+open Async_kernel
+open Async_rpc_kernel
 module Peer = Peer
 module Envelope = Envelope
-module Rpc_intf = Rpc_intf
 
+(* TODO: move this out of Network_peer *)
 type query_peer =
   { query :
       'r 'q.
          Peer.t
-      -> (   Async_rpc_kernel.Versioned_rpc.Connection_with_menu.t
-          -> 'q
-          -> 'r Async_kernel.Deferred.Or_error.t )
+      -> (Versioned_rpc.Connection_with_menu.t -> 'q -> 'r Deferred.Or_error.t)
       -> 'q
-      -> 'r Async_kernel.Deferred.Or_error.t
+      -> 'r Deferred.Or_error.t
   }
