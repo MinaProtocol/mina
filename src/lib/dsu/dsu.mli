4cc94748f104733aa6795b97e11c0b5296ac6ee2
svv232 <saidcooldude@gmail.com>
2024-09-06 17:30:48 -0400

    correcting the grammar in the mli

--- src/lib/dsu/dsu.mli
+++ src/lib/dsu/dsu.mli
@@ -11,7 +11,7 @@ end
     For more information on design and usage of the DSU, see:
     https://www.notion.so/minaprotocol/Bit-catchup-algorithm-8a3fa7a2630c46d98505b25ebb20c8e7#e364c78e09c74c03835bc7ed8634dc47
 
-    this was also useful to me for a more general introduction: https://cp-algorithms.com/data_structures/disjoint_set_union.html
+    Also read: https://cp-algorithms.com/data_structures/disjoint_set_union.html
 *)
 module Dsu : functor (Key : Hashtbl.Key) (D : Data) -> sig
   type element_t = { data : D.t option; parent : int; rank : int }
