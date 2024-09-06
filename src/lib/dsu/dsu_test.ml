10dc6bb2c63aa2e7246f5963fd3c9a555d83f3c1
svv232 <saidcooldude@gmail.com>
2024-09-06 18:19:32 -0400

    Found bug in dsu implementation

--- src/lib/dsu/dsu_test.ml
+++ src/lib/dsu/dsu_test.ml
@@ -16,16 +16,30 @@ let test_create () =
   let dsu = IntKeyDsu.create () in
   Alcotest.(check (option int)) "empty DSU" (IntKeyDsu.get ~key:1 dsu) None
 
+(* Test adding a single element to the dsu *)
 let test_add () =
   let dsu = IntKeyDsu.create () in
   IntKeyDsu.add_exn ~key:1 ~data:1 dsu;
   Alcotest.(check (option int)) "added element" (IntKeyDsu.get ~key:1 dsu) (Some 1)
+
+(* Test adding an array of arbitrary elements *)
+let test_add_array () = 
+  let dsu = IntKeyDsu.create () in
+  (* generate a qcheck array of ints *)
+  let arr = QCheck.Gen.(generate1 (array_size (int_range 100 200) int)) in
+  (* log the arrays size *)
+  Printf.printf "Array size: %d\n" (Array.length arr);
+  
+  (* add each element to the dsu *)
+  Array.iter arr ~f:(fun x -> IntKeyDsu.add_exn ~key:x ~data:x dsu);
+  Array.iter arr ~f:(fun x -> Alcotest.(check (option int)) "added element" (IntKeyDsu.get ~key:x dsu) (Some x))
 
 
 (* alcotest harness *)
 let tests = [
   "test_create", `Quick, test_create;
   "test_add", `Quick, test_add;
+  "test_add_array", `Quick, test_add_array;
 ]
 
 let () =
