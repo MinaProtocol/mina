364a3ec316792aec43ad2a87e2983da386d7001c
svv232 <saidcooldude@gmail.com>
2024-09-08 22:38:43 -0400

    add a reallocation test

--- src/lib/dsu/dsu_test.ml
+++ src/lib/dsu/dsu_test.ml
@@ -40,14 +40,29 @@ let test_add_array () =
 
 let test_reallocation () =
   let dsu = IntKeyDsu.create () in
-  let arr_size = QCheck.Gen.(int_range 10 15) in
+  let arr_size = QCheck.Gen.(int_range 50 60) in
   let arr = QCheck.Gen.(generate1 (array_size arr_size int)) in
+  (* a ref to an array size int*)
+  let arr_size = ref (Array.length arr) in
+  Array.iter arr ~f:(fun x -> 
+    match (IntKeyDsu.get ~key:x dsu) with
+    | Some _ -> 
+      (* in case qcheck generates repeats we don't want the test to fail *)
+      arr_size := (!arr_size - 1)
+    | None ->
+      IntKeyDsu.add_exn ~key:x ~value:x dsu
+    ) ;
+  Alcotest.(check int)
+    "verifying capacity is 64 i.e min capacity" (IntKeyDsu.capacity dsu) 64 ;
+  Alcotest.(check int)
+  (* we add 1 to account for the default 0 element used for dynamic deletions in the dsu *)
+    "verifying occupancy is the size of the array additions" (!arr_size + 1) (IntKeyDsu.occupancy dsu);
+  (* add 400 more items for resize *)
+  (* generates on a normal distribution so there are low risk of repeats to skip allocation *)
+  let arr = QCheck.Gen.(generate1 (array_size (int_range 400 401) int)) in
   Array.iter arr ~f:(fun x -> IntKeyDsu.add_exn ~key:x ~value:x dsu) ;
-  let arr_size = Array.length arr in
   Alcotest.(check int)
-    "verifying length is above min capacity" (IntKeyDsu.capacity dsu) 64 ;
-  Alcotest.(check int)
-    "verifying length is above min capacity" (IntKeyDsu.capacity dsu) arr_size
+    "verifying capacity is 512 meaning there have been 4 re-allocations" (IntKeyDsu.capacity dsu) 512
 
 (* alcotest harness *)
 let tests =
