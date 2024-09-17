35f969654752b528506c4e5cd36d912e1f52d96a
Dariusz Kijania <dariusz.kijania@gmail.com>
2024-09-17 22:11:45 +0200

    Update src/test/archive/patch_archive_test/patch_archive_test.ml
    Co-authored-by: George Agapov <george.agapov@gmail.com>

--- src/test/archive/patch_archive_test/patch_archive_test.ml
+++ src/test/archive/patch_archive_test/patch_archive_test.ml
@@ -77,7 +77,7 @@ let main ~db_uri ~network_data_folder () =
 
   let n =
     List.range 0 missing_blocks_count
-    |> List.map ~f:(fun _ -> Random.int (List.length extensional_files))
+    List.init missing_blocks_count ~f:(fun _ -> Random.int (List.length extensional_files))
   in
 
   let unpatched_extensional_files =
