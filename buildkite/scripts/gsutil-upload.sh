65b6b0980ea1085f3fd7fed95a3faf5cd9c0f926
dkijania <dariusz@o1labs.org>
2024-09-18 18:06:37 +0200

    fix path to key.json

--- buildkite/scripts/gsutil-upload.sh
+++ buildkite/scripts/gsutil-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEY_FILE=/var/secrets/gcloud/key.json
+KEY_FILE=/var/secrets/google/key.json
 
 if [ ! -f $KEY_FILE ]; then
     echo "Cannot use gsutil for upload as key file cannot be foud in $KEY_FILE"
