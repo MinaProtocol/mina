733ec28f599d9bba4d608523291d518ab020f11c
martyall <martyall@protonmail.com>
2024-09-23 13:05:02 -0700

    modify zkapp_metrics.sh to add --config-file arg

--- scripts/zkapp_metrics.sh
+++ scripts/zkapp_metrics.sh
@@ -5,12 +5,14 @@ set -e
 echo "Building heap usage app"
 make heap_usage
 
+# Looking at CI, this would built via the devnet profile
 echo "Running heap usage app"
-./_build/default/src/app/heap_usage/heap_usage.exe
+./_build/default/src/app/heap_usage/heap_usage.exe --config-file ./genesis_ledgers/devnet.json
 
 
 echo "Building zkapp limits app"
 make zkapp_limits
 
+# Looking at CI, this is built via the devnet profile
 echo "Running zkapp limits app"
-./_build/default/src/app/zkapp_limits/zkapp_limits.exe
+./_build/default/src/app/zkapp_limits/zkapp_limits.exe --config-file ./genesis_ledgers/devnet.json
