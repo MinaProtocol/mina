700d0c3dbdedfc187241a71057430f23d011ebcd
Florian <florian@technotro.com>
2024-09-23 12:58:45 +0200

    adjust state module

--- src/lib/mina_base/zkapp_state.ml
+++ src/lib/mina_base/zkapp_state.ml
@@ -1,6 +1,10 @@
 open Core_kernel
 open Pickles_types
-module Max_state_size = Nat.N8
+
+module Max_state_size = struct
+  module V1 = Nat.N8
+  module V2 = Nat.N32
+end
 
 module V = struct
   (* Think about versioning here! These vector types *will* change
@@ -13,27 +17,34 @@ module V = struct
   module Stable = struct
     [@@@no_toplevel_latest_type]
 
+    module V2 = struct
+      type 'a t = 'a Vector.Vector_32.Stable.V1.t
+      [@@deriving compare, yojson, sexp, hash, equal]
+
+      let of_list_exn = Vector.Vector_32.of_list_exn
+    end
+
     module V1 = struct
       type 'a t = 'a Vector.Vector_8.Stable.V1.t
       [@@deriving compare, yojson, sexp, hash, equal]
+
+      let of_list_exn = Vector.Vector_8.of_list_exn
     end
   end]
 
-  type 'a t = 'a Vector.Vector_8.t
-  [@@deriving compare, yojson, sexp, hash, equal]
-
   let map = Vector.map
-
-  let of_list_exn = Vector.Vector_8.of_list_exn
 
   let to_list = Vector.to_list
 end
 
 let _type_equal :
-    type a. (a V.t, a Vector.With_length(Max_state_size).t) Type_equal.t =
+    type a.
+    ( a V.Stable.Latest.t
+    , a Vector.With_length(Max_state_size.V2).t )
+    Type_equal.t =
   Type_equal.T
 
-let typ t = Vector.typ t Max_state_size.n
+let typ t = Vector.typ t Max_state_size.V2.n
 
 open Core_kernel
 
@@ -43,24 +54,25 @@ module Value = struct
     [@@@no_toplevel_latest_type]
 
     module V1 = struct
-      type t = Zkapp_basic.F.Stable.V1.t V.Stable.V1.t
+      type t = Zkapp_basic.F.Stable.V1.t V.Stable.V2.t
       [@@deriving sexp, equal, yojson, hash, compare]
 
       let to_latest = Fn.id
     end
   end]
 
-  type t = Zkapp_basic.F.t V.t [@@deriving sexp, equal, yojson, hash, compare]
+  type t = Zkapp_basic.F.t V.Stable.V2.t
+  [@@deriving sexp, equal, yojson, hash, compare]
 
   let (_ : (t, Stable.Latest.t) Type_equal.t) = Type_equal.T
 end
 
-let to_input (t : _ V.t) ~f =
+let to_input (t : _ V.Stable.V2.t) ~f =
   Vector.(reduce_exn (map t ~f) ~f:Random_oracle_input.Chunked.append)
 
 let deriver inner obj =
   let open Fields_derivers_zkapps.Derivers in
-  iso ~map:V.of_list_exn ~contramap:V.to_list
-    ((list ~static_length:(Nat.to_int Max_state_size.n) @@ inner @@ o ())
+  iso ~map:V.Stable.V2.of_list_exn ~contramap:V.to_list
+    ((list ~static_length:(Nat.to_int Max_state_size.V2.n) @@ inner @@ o ())
        (o ()) )
     obj
