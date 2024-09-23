a02176075c5ddf8e5b91d8a8263700bf9e20d015
dkijania <dariusz@o1labs.org>
2024-09-23 20:25:19 +0200

    fix correct return handling for download-docker command

--- buildkite/scripts/check-compatibility.sh
+++ buildkite/scripts/check-compatibility.sh
@@ -26,7 +26,6 @@ function try_docker_shas {
 
         set +e
         download-docker $sha
-        set -e
 
         if [ $? -eq 0 ] ; then
             GOT_DOCKER=1
@@ -35,7 +34,13 @@ function try_docker_shas {
         else
             echo "No docker available for SHA=$sha"
         fi
+        set -e
     done
+
+    if [[ $GOT_DOCKER == 0 ]]; then
+        echo "docker cannot be found for given shas: $DOCKER_SHAS"
+        exit 1
+    fi
 }
 
 function image_id {
