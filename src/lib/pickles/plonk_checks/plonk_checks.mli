d3024498c5e6380e741d6891e3e657ceb7a98f21
querolita <anais.querol@gmail.com>
2024-09-24 11:15:22 +0200

    revert spaces

--- src/lib/pickles/plonk_checks/plonk_checks.mli
+++ src/lib/pickles/plonk_checks/plonk_checks.mli
@@ -99,8 +99,8 @@ val scalars_env :
   -> ('t * 't, 'a) Pickles_types.Plonk_types.Evals.In_circuit.t
   -> 't Scalars.Env.t
 
-  module Make (Shifted_value : Pickles_types.Shifted_value.S) (_ : Scalars.S) : sig
-    val ft_eval0 :
+module Make (Shifted_value : Pickles_types.Shifted_value.S) (_ : Scalars.S) : sig
+  val ft_eval0 :
        't field
     -> domain:< shifts : 't array ; .. >
     -> env:'t Scalars.Env.t
