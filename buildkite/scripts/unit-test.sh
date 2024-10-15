cee77c33cbde9843e3cb1018909f270803d3d184
mrmr1993 <mr_1993@hotmail.co.uk>
2024-10-15 12:03:08 +0100

    Run unit tests without core contention

--- buildkite/scripts/unit-test.sh
+++ buildkite/scripts/unit-test.sh
@@ -21,22 +21,26 @@ export LIBP2P_NIXLESS=1 PATH=/usr/lib/go/bin:$PATH GO=/usr/lib/go/bin/go
 time make build
 
 echo "--- Build all targets"
-dune build "${path}" --profile="${profile}" -j16
+dune build "${path}" --profile="${profile}"
 
 echo "--- Check for changes to verification keys"
-time dune runtest "src/app/print_blockchain_snark_vk" --profile="${profile}" -j16
+time dune runtest "src/app/print_blockchain_snark_vk" --profile="${profile}"
 
 # Turn on the proof-cache assertion, so that CI will fail if the proofs need to
 # be updated.
 export ERROR_ON_PROOF=true
 
+# Split cores between rayon and dune, to minimize thread swapping
+export NUM_CORES=$(nproc --all)
+export RAYON_NUM_THREADS=4
+export NUM_DUNE_JOBS=$((${NUM_CORES} / ${RAYON_NUM_THREADS}))
 
 # Note: By attempting a re-run on failure here, we can avoid rebuilding and
 # skip running all of the tests that have already succeeded, since dune will
 # only retry those tests that failed.
 echo "--- Run unit tests"
-time dune runtest "${path}" --profile="${profile}" -j16 || \
+time dune runtest "${path}" --profile="${profile}" -j${NUM_DUNE_JOBS} || \
 (./scripts/link-coredumps.sh && \
  echo "--- Retrying failed unit tests" && \
- time dune runtest "${path}" --profile="${profile}" -j16 || \
+ time dune runtest "${path}" --profile="${profile}" -j${NUM_DUNE_JOBS} || \
  (./scripts/link-coredumps.sh && false))
