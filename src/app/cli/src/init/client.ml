7fcfda1ebb6cebbedb495adf877b0aac0fa132e6
svv232 <saidcooldude@gmail.com>
2024-10-15 02:02:11 -0400

    importing changes for gossip layer and client to ipc stack

--- src/app/cli/src/init/client.ml
+++ src/app/cli/src/init/client.ml
@@ -1612,16 +1612,20 @@ let generate_libp2p_keypair_do privkey_path =
     (let open Deferred.Let_syntax in
     (* FIXME: I'd like to accumulate messages into this logger and only dump them out in failure paths. *)
     let logger = Logger.null () in
+    let compile_config = Mina_compile_config.Compiled.t in
     (* Using the helper only for keypair generation requires no state. *)
+    let on_bitswap_update ~tag:_ _ _ = () in
     File_system.with_temp_dir "mina-generate-libp2p-keypair" ~f:(fun tmpd ->
         match%bind
           Mina_net2.create ~logger ~conf_dir:tmpd ~all_peers_seen_metric:false
             ~pids:(Child_processes.Termination.create_pid_table ())
             ~on_peer_connected:ignore ~on_peer_disconnected:ignore ()
+            ~block_window_duration:compile_config.block_window_duration
+            ~on_bitswap_update ()
         with
         | Ok net ->
             let%bind me = Mina_net2.generate_random_keypair net in
-            let%bind () = Mina_net2.shutdown net in
+            let%bind _ = Mina_net2.shutdown net in
             let%map () =
               Secrets.Libp2p_keypair.Terminal_stdin.write_exn ~privkey_path me
             in
@@ -1645,11 +1649,14 @@ let dump_libp2p_keypair_do privkey_path =
     (let open Deferred.Let_syntax in
     let logger = Logger.null () in
     (* Using the helper only for keypair generation requires no state. *)
+    let on_bitswap_update ~tag:_ _ _ = () in
     File_system.with_temp_dir "mina-dump-libp2p-keypair" ~f:(fun tmpd ->
         match%bind
           Mina_net2.create ~logger ~conf_dir:tmpd ~all_peers_seen_metric:false
             ~pids:(Child_processes.Termination.create_pid_table ())
             ~on_peer_connected:ignore ~on_peer_disconnected:ignore ()
+            ~block_window_duration:compile_config.block_window_duration
+            ~on_bitswap_update ()
         with
         | Ok net ->
             let%bind () = Mina_net2.shutdown net in
