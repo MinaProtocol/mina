b8aa1cdac420842bd30b7a935bce7591350d65fa
dkijania <dariusz@o1labs.org>
2024-10-16 21:55:35 +0200

    install requirements before using benchmarks

--- buildkite/scripts/bench/install.sh
+++ buildkite/scripts/bench/install.sh
@@ -14,3 +14,5 @@ git config --global --add safe.directory /workdir
 source buildkite/scripts/export-git-env-vars.sh
 
 source buildkite/scripts/debian/install.sh "mina-test-suite,mina-$TESTNET_NAME" 1
+
+pip3 install -r scripts/benchmarks/requirements.txt
