ddf4831e6e40f4306a76368493fc9889549527b2
dkijania <dariusz@o1labs.org>
2024-10-23 21:00:14 +0200

    allow lightnet suffix for mina daemon

--- scripts/debian/builder-helpers.sh
+++ scripts/debian/builder-helpers.sh
@@ -319,11 +319,11 @@ build_daemon_devnet_deb() {
   echo "------------------------------------------------------------"
   echo "--- Building testnet signatures deb without keys:"
 
-  create_control_file mina-devnet "${SHARED_DEPS}${DAEMON_DEPS}" 'Mina Protocol Client and Daemon for the Devnet Network' "${SUGGESTED_DEPS}"
+  create_control_file "${MINA_DEB_NAME}" "${SHARED_DEPS}${DAEMON_DEPS}" 'Mina Protocol Client and Daemon for the Devnet Network' "${SUGGESTED_DEPS}"
 
   copy_common_daemon_configs devnet testnet 'seed-lists/devnet_seeds.txt'
 
-  build_deb mina-devnet
+  build_deb "${MINA_DEB_NAME}"
 }
 ##################################### END DEVNET PACKAGE #######################################
 
