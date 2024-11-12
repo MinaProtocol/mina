ce54929f380c06c5eaea2a3e49cda68cf0cc51e6
mrmr1993 <mr_1993@hotmail.co.uk>
2024-11-12 21:52:38 +0000

    Remove unused `Typ.t`s

--- src/lib/pickles/composition_types/composition_types.mli
+++ src/lib/pickles/composition_types/composition_types.mli
@@ -697,26 +697,6 @@ module Wrap : sig
       -> app_state:('b -> 'f Core_kernel.Array.t)
       -> g:('g -> 'f list)
       -> 'f Core_kernel.Array.t
-
-    val typ :
-         ('a, 'b) Step_impl.Typ.t
-      -> ('d, 'e) Step_impl.Typ.t
-      -> ('f, 'g) Step_impl.Typ.t
-      -> ('h, 'i) Step_impl.Typ.t
-      -> 'j Nat.nat
-      -> ( ('a, 'f, ('d, 'j) Vector.vec, 'h) t
-         , ('b, 'g, ('e, 'j) Vector.vec, 'i) t )
-         Step_impl.Typ.t
-
-    val wrap_typ :
-         ('a, 'b) Wrap_impl.Typ.t
-      -> ('d, 'e) Wrap_impl.Typ.t
-      -> ('f, 'g) Wrap_impl.Typ.t
-      -> ('h, 'i) Wrap_impl.Typ.t
-      -> 'j Nat.nat
-      -> ( ('a, 'f, ('d, 'j) Vector.vec, 'h) t
-         , ('b, 'g, ('e, 'j) Vector.vec, 'i) t )
-         Wrap_impl.Typ.t
   end
 
   module Lookup_parameters : sig
