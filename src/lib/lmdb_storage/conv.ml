e48c65db49cfeb6f950116e68ad7367de1136dda
Dariusz Kijania <dariusz@o1labs.org>
2024-11-15 15:37:16 +0100

    Update src/lib/lmdb_storage/conv.ml
    Co-authored-by: George Agapov <george.agapov@gmail.com>

--- src/lib/lmdb_storage/conv.ml
+++ src/lib/lmdb_storage/conv.ml
@@ -2,7 +2,10 @@ open Core_kernel
 
 let uint32_be =
   Lmdb.Conv.make
-    ~flags:Lmdb.Conv.Flags.(integer_key + integer_dup + dup_fixed)
+    ~flags:Lmdb.Conv.Flags.(
+        if Sys.big_endian && is_int_size 4
+          then (integer_key + integer_dup + dup_fixed)
+          else dup_fixed    )
     ~serialise:(fun alloc x ->
       let a = alloc 4 in
       Bigstring.set_uint32_be_exn a ~pos:0 x ;
