12fb30434536ebc4ef5a4b97a16ad068c5152549
Deepthi S Kumar <deepthi.s.kumar8@gmail.com>
2024-11-21 01:39:36 -0800

    tests for requested subtree depth > max subtree depth

--- src/lib/syncable_ledger/test/test.ml
+++ src/lib/syncable_ledger/test/test.ml
@@ -83,6 +83,12 @@ module Context_subtree_depth88 = Make_context (struct
   let sync_ledger_default_subtree_depth = 8
 end)
 
+module Context_subtree_depth68 = Make_context (struct
+  let sync_ledger_max_subtree_depth = 6
+
+  let sync_ledger_default_subtree_depth = 8
+end)
+
 module Make_test
     (Input : Input_intf) (Input' : sig
       val num_accts : int
@@ -119,12 +125,10 @@ struct
     don't_wait_for
       (Linear_pipe.iter_unordered ~max_concurrency:3 qr
          ~f:(fun (root_hash, query) ->
-           let%bind answ_opt =
+           let%bind answ_or_error =
              Sync_responder.answer_query sr (Envelope.Incoming.local query)
            in
-           let answ =
-             Option.value_exn ~message:"refused to answer query" answ_opt
-           in
+           let answ = Or_error.ok_exn answ_or_error in
            let%bind () =
              if match query with What_contents _ -> true | _ -> false then
                Clock_ns.after
@@ -180,13 +184,11 @@ struct
                      : [ `New | `Repeat | `Update_data ] ) ;
                  Deferred.unit )
                else
-                 let%bind answ_opt =
+                 let%bind answ_or_error =
                    Sync_responder.answer_query !sr
                      (Envelope.Incoming.local query)
                  in
-                 let answ =
-                   Option.value_exn ~message:"refused to answer query" answ_opt
-                 in
+                 let answ = Or_error.ok_exn answ_or_error in
                  Linear_pipe.write aw
                    (!desired_root, query, Envelope.Incoming.local answ)
              in
@@ -209,6 +211,92 @@ struct
               (Ledger.merkle_root mt)
         | `Target_changed _ ->
             failwith "the target changed again" )
+end
+
+module Make_test_failure_cases (Input : Input_intf) = struct
+  open Input
+  module Sync_responder = Sync_ledger.Responder
+
+  let trust_system = Trust_system.null ()
+
+  let num_accts = 1026
+
+  let () =
+    Async.Scheduler.set_record_backtraces true ;
+    Core.Backtrace.elide := false
+
+  let check_answer (query : Ledger.addr Syncable_ledger.Query.t) answer =
+    match query with
+    | What_child_hashes (_, depth) -> (
+        let invalid_depth =
+          depth > Context.compile_config.sync_ledger_max_subtree_depth
+        in
+        match answer with
+        | Error s ->
+            (*fail if requested depth is > max depth configured*)
+            if
+              invalid_depth
+              && String.is_substring (Error.to_string_hum s)
+                   ~substring:
+                     "Invalid depth requested in What_child_hashes request"
+            then `Failure_as_expected
+            else
+              failwithf
+                "Expected failure due to invalid subtree depth, returned %s"
+                (Error.to_string_hum s) ()
+        | Ok a ->
+            if invalid_depth then
+              failwith
+                "Expected failure due to invalid subtree depth, returned a \
+                 successful answer"
+            else `Answer a )
+    | _ ->
+        `Answer (Or_error.ok_exn answer)
+
+  let%test "try full_sync_entirely_different with failures" =
+    let l1, _k1 = Ledger.load_ledger 1 1 in
+    let l2, _k2 = Ledger.load_ledger num_accts 2 in
+    let desired_root = Ledger.merkle_root l2 in
+    let got_failure_ivar = Ivar.create () in
+
+    let lsync = Sync_ledger.create l1 ~context:(module Context) ~trust_system in
+    let qr = Sync_ledger.query_reader lsync in
+    let aw = Sync_ledger.answer_writer lsync in
+    let sr =
+      Sync_responder.create l2 ignore ~context:(module Context) ~trust_system
+    in
+    don't_wait_for
+      (Linear_pipe.iter_unordered ~max_concurrency:3 qr
+         ~f:(fun (root_hash, query) ->
+           let%bind answ_or_error =
+             Sync_responder.answer_query sr (Envelope.Incoming.local query)
+           in
+           match check_answer query answ_or_error with
+           | `Answer answ ->
+               let%bind () =
+                 if match query with What_contents _ -> true | _ -> false then
+                   Clock_ns.after
+                     (Time_ns.Span.randomize (Time_ns.Span.of_ms 0.2)
+                        ~percent:(Percent.of_percentage 20.) )
+                 else Deferred.unit
+               in
+               Linear_pipe.write aw
+                 (root_hash, query, Envelope.Incoming.local answ)
+           | `Failure_as_expected ->
+               Ivar.fill got_failure_ivar true ;
+               Deferred.unit ) ) ;
+    Async.Thread_safe.block_on_async_exn (fun () ->
+        let deferred_res =
+          match%map
+            Sync_ledger.fetch lsync desired_root ~data:() ~equal:(fun () () ->
+                true )
+          with
+          | `Ok mt ->
+              Root_hash.equal desired_root (Ledger.merkle_root mt)
+          | `Target_changed _ ->
+              false
+        in
+        Deferred.any [ deferred_res; Ivar.read got_failure_ivar ] )
 end
 
 module Root_hash = struct
@@ -334,6 +422,13 @@ module Db = struct
         let depth = 16
       end)
 
+  module DB16_subtree_depths68 =
+    Make
+      (Context_subtree_depth68)
+      (struct
+        let depth = 16
+      end)
+
   module TestDB3_3 =
     Make_test
       (DB3)
@@ -390,6 +485,11 @@ module Db = struct
       (struct
         let num_accts = 1026
       end)
+
+  module TestDB16_1026_subtree_depth68_Failure =
+    Make_test_failure_cases (DB16_subtree_depths68)
+  module TestDB16_1026_subtree_depth86_Success =
+    Make_test_failure_cases (DB16_subtree_depths86)
 end
 
 module Mask = struct
@@ -549,6 +649,15 @@ module Mask = struct
         let mask_layers = 2
       end)
 
+  module Mask16_Layer2_Depth68 =
+    Make
+      (Context_subtree_depth68)
+      (struct
+        let depth = 16
+
+        let mask_layers = 2
+      end)
+
   module TestMask3_Layer1_3 =
     Make_test
       (Mask3_Layer1)
@@ -611,4 +720,9 @@ module Mask = struct
       (struct
         let num_accts = 1024
       end)
+
+  module TestMask16_Layer2_1024_Depth68_Failure =
+    Make_test_failure_cases (Mask16_Layer2_Depth68)
+  module TestMask16_Layer2_1024_Depth86_Success =
+    Make_test_failure_cases (Mask16_Layer2_Depth86)
 end
