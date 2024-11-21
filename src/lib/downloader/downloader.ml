aafd07cffc7740048640f9a96a59224772c7325c
mrmr1993 <mr_1993@hotmail.co.uk>
2024-11-21 11:50:44 +0000

    Propagate logger to `Downloader

--- src/lib/downloader/downloader.ml
+++ src/lib/downloader/downloader.ml
@@ -82,6 +82,7 @@ end) : sig
   val create :
        max_batch_size:int
     -> stop:unit Deferred.t
+    -> logger:Logger.t
     -> trust_system:Trust_system.t
     -> get:(Peer.t -> Key.t list -> Result.t list Deferred.Or_error.t)
     -> knowledge_context:Knowledge_context.t Broadcast_pipe.Reader.t
@@ -951,7 +952,7 @@ end = struct
   let mark_preferred t peer ~now =
     Useful_peers.Preferred_heap.add t.useful_peers.all_preferred (peer, now)
 
-  let create ~max_batch_size ~stop ~trust_system ~get ~knowledge_context
+  let create ~max_batch_size ~stop ~logger ~trust_system ~get ~knowledge_context
       ~knowledge ~peers ~preferred =
     let%map all_peers = peers () in
     let pipe ~name c =
@@ -972,7 +973,7 @@ end = struct
       ; useful_peers = Useful_peers.create ~all_peers ~preferred
       ; get
       ; max_batch_size
-      ; logger = Logger.create ()
+      ; logger
       ; trust_system
       ; downloading = Key.Table.create ()
       ; stop
