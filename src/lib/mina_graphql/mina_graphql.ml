246872ba7d46c3ba8cc5ace4da8db382558eb651
Anne-Laure <anne-laure@o1labs.org>
2024-12-02 21:03:38 +0100

    Mina_graphql: remove get_pending_work function
    This function was initially added to unify code. At this point this is no longer relevant, as it is now used only in one place.

--- src/lib/mina_graphql/mina_graphql.ml
+++ src/lib/mina_graphql/mina_graphql.ml
@@ -2202,21 +2202,19 @@ module Queries = struct
         Mina_lib.snark_pool mina |> Network_pool.Snark_pool.resource_pool
         |> Network_pool.Snark_pool.Resource_pool.all_completed_work )
 
-  (* Auxiliary function to fetch all pending work *)
-  let get_pending_work mina =
-    let snark_job_state = Mina_lib.snark_job_state mina in
-    let snark_pool = Mina_lib.snark_pool mina in
-    let fee_opt =
-      Mina_lib.(
-        Option.map (snark_worker_key mina) ~f:(fun _ -> snark_work_fee mina))
-    in
-    Work_selector.pending_work_statements ~snark_pool ~fee_opt snark_job_state
-
   let pending_snark_work =
     field "pendingSnarkWork" ~doc:"List of snark works that are yet to be done"
       ~args:Arg.[]
       ~typ:(non_null @@ list @@ non_null Types.pending_work)
-      ~resolve:(fun { ctx = mina; _ } () -> get_pending_work mina)
+      ~resolve:(fun { ctx = mina; _ } () ->
+        let snark_job_state = Mina_lib.snark_job_state mina in
+        let snark_pool = Mina_lib.snark_pool mina in
+        let fee_opt =
+          Mina_lib.(
+            Option.map (snark_worker_key mina) ~f:(fun _ -> snark_work_fee mina))
+        in
+        Work_selector.pending_work_statements ~snark_pool ~fee_opt
+          snark_job_state )
 
   let pending_snark_work_range =
     field "pendingSnarkWorkRange"
