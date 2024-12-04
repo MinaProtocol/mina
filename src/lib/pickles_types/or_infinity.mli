b465160e1b60bcf84655f0f3b75d1f02532964c7
martyall <martyall@protonmail.com>
2024-12-04 15:16:29 -0800

    use pickles_types as a reexport library to not break code

--- src/lib/pickles_types/or_infinity.mli
+++
@@ -1,6 +0,0 @@
-(* Representation for values that may be infinite *)
-
-(** [t] is the type of possibly infinite values *)
-type 'a t = Infinity | Finite of 'a
-
-val finite_exn : 'a t -> 'a
--- src/lib/pickles_types/test/main.ml
+++
@@ -1,3 +0,0 @@
-let tests = Test_pcs_batch.tests @ Test_vector.tests
-
-let () = Alcotest.run "Pickles types" tests
