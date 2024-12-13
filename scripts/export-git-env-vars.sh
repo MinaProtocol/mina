f9ce7b3d1b605412390b0fc3e972b491adceb7a9
dkijania <dariusz@o1labs.org>
2024-12-13 21:02:13 +0100

    fix missing envs

--- scripts/export-git-env-vars.sh
+++ scripts/export-git-env-vars.sh
@@ -15,7 +15,15 @@ export GITHASH=$(git rev-parse --short=7 HEAD)
 export GITBRANCH=$(git name-rev --name-only $GITHASH | sed "s/remotes\/origin\///g" | sed 's!/!-!g; s!_!-!g; s!#!-!g' )
 export GITTAG=$(find_most_recent_numeric_tag HEAD)
 
+export THIS_COMMIT_TAG=$(git tag --points-at HEAD)
+[[ -n ${THIS_COMMIT_TAG} ]] && export MINA_COMMIT_TAG="${THIS_COMMIT_TAG}"
+
 export MINA_DEB_VERSION="${GITTAG}-${GITBRANCH}-${GITHASH}"
-export MINA_DEB_RELEASE="unstable"
+export RELEASE=unstable
+export MINA_DEB_RELEASE="${RELEASE}"
 export MINA_DEB_CODENAME=${MINA_DEB_CODENAME:=bullseye}
 export MINA_DOCKER_TAG="$(echo "${MINA_DEB_VERSION}-${MINA_DEB_CODENAME}" | sed 's!/!-!g; s!_!-!g')"
+
+export MINA_REPO="https://github.com/MinaProtocol/mina.git"
+export PROJECT="mina"
+
