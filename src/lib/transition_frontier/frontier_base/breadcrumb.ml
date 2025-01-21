6a98543997b94f6c1f158fed79dd43d8abb51e7b
georgeee <george.agapov@gmail.com>
2025-01-21 12:44:46 +0000

    Decouple Staged_ledger_diff.{Stable.Latest.t, t}
    Remove `Stable.Latest.t = t` from interfaces. This is a preparation step
    for removing the equality in the implementation as well.

--- src/lib/transition_frontier/frontier_base/breadcrumb.ml
+++ src/lib/transition_frontier/frontier_base/breadcrumb.ml
@@ -455,9 +455,8 @@ module For_tests = struct
           ~timestamp:(Block_time.now @@ Block_time.Controller.basic ~logger)
           ~staged_ledger_hash ~genesis_ledger_hash
           ~body_reference:
-            (Body.compute_reference
-               ~tag:Mina_net2.Bitswap_tag.(to_enum Body)
-               body )
+            ( Body.compute_reference ~tag:Mina_net2.Bitswap_tag.(to_enum Body)
+            @@ Body.read_all_proofs_from_disk body )
           ~ledger_proof_statement
       in
       let previous_state_hashes =
