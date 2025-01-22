71bd217f2f498a077bb47be3720d0d119522112a
georgeee <george.agapov@gmail.com>
2025-01-22 11:17:16 +0000

    Rename Genesis_ledger_helper.init_from_config_file
    Rename: init_from_config_file -> Config_loader.init_from_config_file_legacy

--- src/lib/mina_lib/tests/tests.ml
+++ src/lib/mina_lib/tests/tests.ml
@@ -66,7 +66,7 @@ let%test_module "Epoch ledger sync tests" =
           }
         in
         match%map
-          Genesis_ledger_helper.init_from_config_file
+          Genesis_ledger_helper.Config_loader.init_from_config_file_legacy
             ~genesis_dir:(make_dirname "genesis_dir")
             ~constraint_constants ~genesis_constants ~logger
             ~proof_level:No_check runtime_config ~cli_proof_level:None
