e62f35de7e6f59aba9754be85db68f1dac569f40
Geometer1729 <16kuhnb@gmail.com>
2025-01-23 11:18:53 -0500

    add script to regenerate archive

--- src/test/archive/sample_db/convert_chain_to_canonical.sh
+++ src/test/archive/sample_db/convert_chain_to_canonical.sh
@@ -1,7 +1,9 @@
-#/bin/bash 
+#/bin/bash
 
 CONN_STR=$1
-LAST_BLOCK_HASH=$2
+LAST_BLOCK_HASH=$(psql $CONN_STR -t -c \
+  'SELECT state_hash from blocks where global_slot_since_genesis = (SELECT MAX(global_slot_since_genesis) from blocks);' \
+  | head -n1 | xargs )
 
 GENESIS_HASH=$(psql $CONN_STR -t -c  \
     "select state_hash from blocks where id = 1;" | xargs)
@@ -25,7 +27,7 @@ done
 
 psql $CONN_STR -c "update blocks set chain_status = 'orphaned' where height <= $HEIGHT;"
 
-for block in ${canon_chain[@]}; do 
+for block in ${canon_chain[@]}; do
     psql $CONN_STR -c "update blocks set chain_status = 'canonical' where state_hash = '$block'"
 done
 
