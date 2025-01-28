8b3ecedd28ad28183d896b4253888a383093ac65
Danny Willems <be.danny.willems@gmail.com>
2025-01-28 10:16:41 +0100

    kimchi-bindings/js: back on nightly and use 2023-09-18
    nightly has been initially removed as it seemed to make vendoring all the
    dependencies more complicated because some std lib must be included.
    However, removing nightly introduces issues like "DataCloneError", see
    https://github.com/o1-labs/o1js/issues/1989, which makes the reliance on
    nightly inevitable.
    Removing --offline because stdlib missing for now (like `compiler_builtins`).
    
    Another note is that cargo vendor has been introduced to provide a
    consistent build environment for all platforms and all users, including
    the CI. The lockfile Cargo.lock is meant to provide a locked environment
    with all the dependencies, including the transitive ones. However, it is
    only used by binaries. In our build process, we do build a static
    library (libwires_15_stubs.a), not a binary. And Rust doesn't use the
    lockfile in this case.
    Another solution than `cargo vendor` would have been to strictly enforce
    the version in the top-level Cargo.toml, but it implies to add all the
    transitive dependencies (as otherwise, when a new transitive dependency
    is released, cargo will fetch it, making the environment inconsistent).
    This solution is not viable as the purpose of Cargo.toml is to enforce
    versions on direct dependencies only, and leave the cargo solver doing
    its job for the transitive dependencies.
    For these reasons, the solution of vendoring the dependencies to build
    the static library has been selected.

--- src/lib/crypto/kimchi_bindings/js/node_js/build.sh
+++ src/lib/crypto/kimchi_bindings/js/node_js/build.sh
@@ -3,8 +3,7 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_NODEJS-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    # The version should stay in line with the one in kimchi_bindings/wasm/rust-toolchain.toml
-    rustup run 1.72 wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- --features nodejs --offline
+    wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Zbuild-std=std,panic_abort --features nodejs --offline
 else
     cp "$PLONK_WASM_NODEJS"/* -R .
 fi
