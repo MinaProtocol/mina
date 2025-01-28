88093b0429aa23303b3924722025f9d3d7460652
Geometer1729 <16kuhnb@gmail.com>
2025-01-28 16:50:25 -0500

    fix regenerate.sh crashing

--- src/test/archive/sample_db/regenerate.sh
+++ src/test/archive/sample_db/regenerate.sh
@@ -16,15 +16,7 @@ psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 # start mina-local-network
 ./scripts/mina-local-network/mina-local-network.sh -a -r -pu postgres -ppw postgres -zt -vt -lp &
 
-PID=$!
-rec_cleanup(){
-  for p in $(pgrep -P $1);
-  do
-    rec_cleanup $p
-  done
-  kill $1
-}
-trap "rec_cleanup $PID" EXIT
+trap "pkill -f mina-local-network" EXIT
 
 # stop mina-local-network once enough blocks have been produced
 while true; do
@@ -32,27 +24,30 @@ while true; do
   # psql outputs "    " until there are blocks in the db, the +0 defaults that to 0
   BLOCKS="$(( $(psql -U postgres archive -t -c  "select MAX(global_slot_since_genesis) from blocks" 2> /dev/null) +0))"
   echo Generated $BLOCKS/25 blocks
-  if [ "$((BLOCKS+0))" -ge  4 ] ; then
-    rec_cleanup $PID
+  if [ "$((BLOCKS+0))" -ge  5 ] ; then
+    pkill -f mina-local-network
     break
   fi
 done
 
+echo Converting canonical blocks
+
 # make the blocks canonical
 ./src/test/archive/sample_db/convert_chain_to_canonical.sh postgres://postgres:postgres@localhost:5432/archive
 
-# regenerate precomputed_blocks.tar.xz
+echo Regenerateing precomputed_blocks.tar.xz
+rm -rf precomputed_blocks || true
 mkdir precomputed_blocks
 find ~/.mina-network -name 'precomputed_blocks.log' | xargs -I ! ./scripts/mina-local-network/split_precomputed_log.sh ! precomputed_blocks
-rm ./src/test/archive/sample_db/precomputed_blocks.tar.xz
+rm ./src/test/archive/sample_db/precomputed_blocks.tar.xz || true
 tar cvf ./src/test/archive/sample_db/precomputed_blocks.tar.xz precomputed_blocks
 rm -rf precomputed_blocks
 
-# regenerate archive_db.sql
+echo Regenerateing archive_db.sql
 pg_dump -U postgres -d archive > ./src/test/archive/sample_db/archive_db.sql
 
 
-# regenerate input file
+echo Regenerateing input file
 cp ~/.mina-network/mina-local-network-2-1-1/genesis_ledger.json _tmp1.json
 cat _tmp1.json | jq '.accounts' > _tmp2.json
 echo '{ "genesis_ledger": { "accounts": '$(cat _tmp2.json)' } }' | jq > _tmp3.json
@@ -60,9 +55,11 @@ NEW_HASH=$(psql -U postgres archive -t -c  'SELECT state_hash from blocks where 
 cat _tmp3.json | jq -c '.+{"target_epoch_ledgers_state_hash": "'$NEW_HASH'"}' > ./src/test/archive/sample_db/replayer_input_file.json
 rm _tmp*.json
 
-# regenerate genesis_ledger
+echo Regenerateing genesis_ledger
 cat src/test/archive/sample_db/genesis.json | jq ".ledger=$(cat ~/.mina-network/mina-local-network-2-1-1/genesis_ledger.json | jq -c)"  > _tmp.json
 mv _tmp.json src/test/archive/sample_db/genesis.json
+
+echo finished regenerate testing replay
 
 sudo -u postgres dropdb archive
 psql -U postgres -c 'CREATE DATABASE archive'
