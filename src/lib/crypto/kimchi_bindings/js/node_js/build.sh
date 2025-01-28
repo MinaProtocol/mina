b2ba551637a0d08033d107b33aec8176700d67c5
Danny Willems <be.danny.willems@gmail.com>
2025-01-28 13:55:43 +0100

    kimchi-bindings/js: removing offline for now

--- src/lib/crypto/kimchi_bindings/js/node_js/build.sh
+++ src/lib/crypto/kimchi_bindings/js/node_js/build.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_NODEJS-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Zbuild-std=std,panic_abort --features nodejs --offline
+    # FIXME: reintroduce --offline
+    # The stdlib must be vendored
+    # Hoping for now that wasm-pack uses Cargo.lock to get reproducible builds
+    wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Zbuild-std=std,panic_abort --features nodejs
 else
     cp "$PLONK_WASM_NODEJS"/* -R .
 fi
--- src/lib/crypto/kimchi_bindings/js/web/build.sh
+++ src/lib/crypto/kimchi_bindings/js/web/build.sh
@@ -3,7 +3,10 @@ set -euo pipefail
 
 if [[ -z "${PLONK_WASM_WEB-}" ]]; then
     export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    wasm-pack build --target web --out-dir ../js/web ../../wasm -- --offline -Zbuild-std=std,panic_abort
+    # FIXME: reintroduce --offline
+    # The stdlib must be vendored
+    # Hoping for now that wasm-pack uses Cargo.lock to get reproducible builds
+    wasm-pack build --target web --out-dir ../js/web ../../wasm -- -Zbuild-std=std,panic_abort
 else
     cp "$PLONK_WASM_WEB"/* -R .
 fi
