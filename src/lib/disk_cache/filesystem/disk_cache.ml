5d5a9d5a41bd03a7f486613ebd28164704317a43
georgeee <george.agapov@gmail.com>
2025-01-30 10:19:23 +0000

    Extract initialize_dir to utils.ml

--- src/lib/disk_cache/filesystem/disk_cache.ml
+++ src/lib/disk_cache/filesystem/disk_cache.ml
@@ -1,7 +1,6 @@
 (* Cache proofs using the filesystem, one file per proof. *)
 
 open Core
-open Async
 
 module Make (B : sig
   include Binable.S
@@ -11,32 +10,9 @@ struct
 
   type id = { idx : int }
 
-  let failed_to_get_cache_folder_status ~logger ~(error_msg : string) ~path =
-    [%log error] "%s" error_msg ~metadata:[ ("path", `String path) ] ;
-    failwithf "%s (%s)" error_msg path ()
-
   let initialize path ~logger =
-    let open Deferred.Let_syntax in
-    match%bind Sys.is_directory path with
-    | `Yes ->
-        let%bind () = File_system.clear_dir path in
-        Deferred.Result.return (path, ref 0)
-    | `No -> (
-        match%bind Sys.is_file path with
-        | `Yes ->
-            failed_to_get_cache_folder_status ~logger
-              ~error_msg:
-                "Invalid path to proof cache folder. Path points to a file"
-              ~path
-        | `No ->
-            let%bind () = File_system.create_dir path in
-            Deferred.Result.return (path, ref 0)
-        | `Unknown ->
-            failed_to_get_cache_folder_status ~logger
-              ~error_msg:"Cannot evaluate existence of cache folder" ~path )
-    | `Unknown ->
-        failed_to_get_cache_folder_status ~logger
-          ~error_msg:"Cannot evaluate existence of cache folder" ~path
+    Async.Deferred.Result.map (Utils.initialize_dir path ~logger)
+      ~f:(fun path -> (path, ref 0))
 
   let path root i = root ^ Filename.dir_sep ^ Int.to_string i
 
