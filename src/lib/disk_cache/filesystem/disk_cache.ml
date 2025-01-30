c2a87c77cbd86540816858eb4bf47e537381951f
georgeee <george.agapov@gmail.com>
2025-01-30 10:46:30 +0000

    Fix count method in Disk_cache

--- src/lib/disk_cache/filesystem/disk_cache.ml
+++ src/lib/disk_cache/filesystem/disk_cache.ml
@@ -16,23 +16,23 @@ struct
 
   let path root i = root ^ Filename.dir_sep ^ Int.to_string i
 
-  let get ((root, _counter) : t) (id : id) : B.t =
+  let get ((root, _) : t) (id : id) : B.t =
     (* Read from the file. *)
     In_channel.with_file ~binary:true (path root id.idx) ~f:(fun chan ->
         let str = In_channel.input_all chan in
         Binable.of_string (module B) str )
 
-  let put ((root, counter) : t) x : id =
-    let new_counter = !counter in
-    incr counter ;
-    let res = { idx = new_counter } in
+  let put ((root, next_idx) : t) x : id =
+    let idx = !next_idx in
+    incr next_idx ;
+    let res = { idx } in
     (* When this reference is GC'd, delete the file. *)
     Core.Gc.Expert.add_finalizer_last_exn res (fun () ->
-        Core.Unix.unlink (path root new_counter) ) ;
+        Core.Unix.unlink (path root idx) ) ;
     (* Write the proof to the file. *)
-    Out_channel.with_file ~binary:true (path root new_counter) ~f:(fun chan ->
+    Out_channel.with_file ~binary:true (path root idx) ~f:(fun chan ->
         Out_channel.output_string chan @@ Binable.to_string (module B) x ) ;
     res
 
-  let count ((_, counter) : t) = !counter
+  let count ((path, _) : t) = Sys.ls_dir path |> List.length
 end
