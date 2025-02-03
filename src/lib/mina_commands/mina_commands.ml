511353c213a45bd61ae682616de3eb64905008f4
georgeee <george.agapov@gmail.com>
2025-02-03 10:12:32 +0000

    Prepare for switch of zkapp_command types
    Remainder of changes in preparation of removing hashes from
    Zkapp_command.Stable.Latest.t.

--- src/lib/mina_commands/mina_commands.ml
+++ src/lib/mina_commands/mina_commands.ml
@@ -97,7 +97,9 @@ let setup_and_submit_user_command t (user_command_input : User_command_input.t)
                 | `Not_broadcasted ->
                     "not_broadcasted" ) )
           ; ( "valid_commands"
-            , `List (List.map ~f:User_command.to_yojson valid_commands) )
+            , `List
+                (List.map ~f:User_command.Stable.Latest.to_yojson valid_commands)
+            )
           ; ( "invalid_commands"
             , `List
                 (List.map invalid_commands ~f:(fun (_cmd, diff_err) ->
@@ -118,7 +120,8 @@ let setup_and_submit_user_commands t user_command_list =
       [ ("mina_command", `String "scheduling a batch of user transactions") ] ;
   Mina_lib.add_transactions t user_command_list
 
-let setup_and_submit_zkapp_commands t (zkapp_commands : Zkapp_command.t list) =
+let setup_and_submit_zkapp_commands t
+    (zkapp_commands : Zkapp_command.Stable.Latest.t list) =
   let open Participating_state.Let_syntax in
   (* hack to get types to work out *)
   let%map () = return () in
@@ -167,7 +170,8 @@ let setup_and_submit_zkapp_commands t (zkapp_commands : Zkapp_command.t list) =
   | Error e ->
       Error e
 
-let setup_and_submit_zkapp_command t (zkapp_command : Zkapp_command.t) =
+let setup_and_submit_zkapp_command t
+    (zkapp_command : Zkapp_command.Stable.Latest.t) =
   let res = setup_and_submit_zkapp_commands t [ zkapp_command ] in
   let%map.Participating_state res' = res in
   match%map.Deferred res' with
@@ -226,8 +230,12 @@ let chain_id_inputs (t : Mina_lib.t) =
   , protocol_transaction_version
   , protocol_network_version )
 
-let verify_payment t (addr : Account_id.t) (verifying_txn : User_command.t)
-    (init_receipt, proof) =
+let verify_payment t (addr : Account_id.t)
+    (verifying_txn : User_command.Stable.Latest.t)
+    (init_receipt, proof_unwrapped) =
+  let proof =
+    List.map ~f:User_command.write_all_proofs_to_disk proof_unwrapped
+  in
   let open Participating_state.Let_syntax in
   let%map account = get_account t addr in
   let account = Option.value_exn account in
@@ -238,11 +246,14 @@ let verify_payment t (addr : Account_id.t) (verifying_txn : User_command.t)
       (Receipt_chain_verifier.verify ~init:init_receipt proof resulting_receipt)
       ~error:(Error.createf "Merkle list proof of payment is invalid")
   in
-  if List.exists proof ~f:(fun txn -> User_command.equal verifying_txn txn) then
-    Ok ()
+  if
+    List.exists proof_unwrapped ~f:(fun txn ->
+        User_command.Stable.Latest.equal verifying_txn txn )
+  then Ok ()
   else
     Or_error.errorf
-      !"Merkle list proof does not contain payment %{sexp:User_command.t}"
+      !"Merkle list proof does not contain payment \
+        %{sexp:User_command.Stable.Latest.t}"
       verifying_txn
 
 type active_state_fields =
