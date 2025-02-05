de57abce8488ba498fca20db21f0e01d2ec48c64
dkijania <dariusz@o1labs.org>
2025-02-05 16:29:29 +0100

    use Mina_base.Proof.Stable.Latest instead of Pickles.Proof.... fix cache_db type

--- src/lib/proof_cache_tag/lmdb/proof_cache_tag.ml
+++ src/lib/proof_cache_tag/lmdb/proof_cache_tag.ml
@@ -1,30 +1,23 @@
 open Async
 open Core
-module Cache = Disk_cache.Make (Pickles.Proof.Proofs_verified_2.Stable.Latest)
+module Cache = Disk_cache.Make (Mina_base.Proof.Stable.Latest)
 
 type cache_db = Lmdb_cache of Cache.t | Identity_cache
 
 type t =
-  | Lmdb of { cache_id : Cache.id; cache_db : cache_db }
+  | Lmdb of { cache_id : Cache.id; cache_db : Cache.t }
   | Identity of Mina_base.Proof.t
 
 let unwrap = function
-  | Lmdb t -> (
-      match t.cache_db with
-      | Identity_cache ->
-          failwith
-            "internal error for proof cache tag. Identity_cache shouldn't be \
-             used in production lmdb implementation"
-      | Lmdb_cache cache_db ->
-          Cache.get cache_db t.cache_id )
+  | Lmdb t ->
+      Cache.get t.cache_db t.cache_id
   | Identity proof ->
       proof
 
 let generate db proof =
   match db with
   | Lmdb_cache cache_db ->
-      Lmdb
-        { cache_id = Cache.put cache_db proof; cache_db = Lmdb_cache cache_db }
+      Lmdb { cache_id = Cache.put cache_db proof; cache_db }
   | Identity_cache ->
       Identity proof
 
