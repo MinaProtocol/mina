a726ea710be04a1d841032187a6219689d397d0b
ymekuria <ymekuria@gmail.com>
2025-02-05 11:40:16 -0800

    feat(node_backend.js): add plonk_native object to the global environment for better accessibility and modularity

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -1,2 +1,6 @@
 // Provides: plonk_wasm
 var plonk_wasm = require('./plonk_wasm.js');
+
+// inject a global plonk_native object into js environment
+// //Provides: plonk_native
+// var plonk_native = require('...Path to file');
