ff42026add45032195f30a78ac047f221c515238
dkijania <dariusz@o1labs.org>
2025-02-05 15:24:56 +0100

    reorg lmdb cache. Introduced Identity cache for tests

--- src/lib/proof_cache_tag/lmdb/proof_cache_tag.ml
+++ src/lib/proof_cache_tag/lmdb/proof_cache_tag.ml
@@ -2,35 +2,38 @@ open Async
 open Core
 module Cache = Disk_cache.Make (Pickles.Proof.Proofs_verified_2.Stable.Latest)
 
-type cache_db = Cache.t
+type cache_db = Lmdb_cache of Cache.t | Identity_cache
 
-type t = { cache_id : Cache.id; cache_db : cache_db }
+type t =
+  | Lmdb of { cache_id : Cache.id; cache_db : cache_db }
+  | Identity of Mina_base.Proof.t
 
-let create_db = Cache.initialize
+let unwrap = function
+  | Lmdb t -> (
+      match t.cache_db with
+      | Identity_cache ->
+          failwith
+            "internal error for proof cache tag. Identity_cache shouldn't be \
+             used in production lmdb implementation"
+      | Lmdb_cache cache_db ->
+          Cache.get cache_db t.cache_id )
+  | Identity proof ->
+      proof
 
-let unwrap t = Cache.get t.cache_db t.cache_id
+let generate db proof =
+  match db with
+  | Lmdb_cache cache_db ->
+      Lmdb
+        { cache_id = Cache.put cache_db proof; cache_db = Lmdb_cache cache_db }
+  | Identity_cache ->
+      Identity proof
 
-let generate cache_db proof =
-  let cache_id = Cache.put cache_db proof in
-  { cache_id; cache_db }
+let create_db path ~logger =
+  Cache.initialize ~logger path
+  |> Deferred.Result.map ~f:(fun cache -> Lmdb_cache cache)
 
 module For_tests = struct
-  let create_db () =
-    let open Deferred.Let_syntax in
-    Thread_safe.block_on_async_exn (fun () ->
-        let%bind db =
-          create_db
-            (Core.Filename.temp_dir "mina" "proof_cache.lmdb")
-            ~logger:(Logger.null ())
-        in
-        match db with
-        | Ok db ->
-            return db
-        | Error err -> (
-            match err with
-            | `Initialization_error err ->
-                failwithf "cannot initialize db for tests %s"
-                  (Error.to_string_hum err) () ) )
+  let create_db () = Identity_cache
 
   let blockchain_dummy =
     Lazy.map
