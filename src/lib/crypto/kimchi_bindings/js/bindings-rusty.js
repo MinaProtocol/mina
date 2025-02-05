551381789b60bb107003ee3e04938d34495d1340
ymekuria <ymekuria@gmail.com>
2025-02-05 11:33:20 -0800

    refactor(bindings-rusty.js): update global and require statements to use plonk_native instead of plonk_wasm for consistency and clarity

--- src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
+++ src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
@@ -1,8 +1,16 @@
-/* global plonk_wasm
+/* global plonk_native
  */
-
 // Provides: caml_do_cool_thingies
-// Requires: plonk_wasm
+// Requires: plonk_native
 var caml_do_cool_thingies = function () {
-  return plonk_wasm.caml_do_cool_thingies();
+  return plonk_native.caml_do_cool_thingies();
 };
+
+// /* global plonk_wasm
+//  */
+
+// // Provides: caml_do_cool_thingies
+// // Requires: plonk_wasm
+// var caml_do_cool_thingies = function () {
+//   return plonk_wasm.caml_do_cool_thingies();
+// };
