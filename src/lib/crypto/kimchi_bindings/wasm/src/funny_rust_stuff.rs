dbb2836435ea4aede666a90d42ca8ed0fd138794
ymekuria <ymekuria@gmail.com>
2025-02-05 13:59:16 -0800

    refactor(funny_rust_stuff.rs): update the return message in caml_do_cool_thingies wasm

--- src/lib/crypto/kimchi_bindings/wasm/src/funny_rust_stuff.rs
+++ src/lib/crypto/kimchi_bindings/wasm/src/funny_rust_stuff.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn caml_do_cool_thingies() -> JsValue {
-    return "hello from rust".into();
+    return "hello from rust this is Yoni".into();
 }
