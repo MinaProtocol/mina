533d352fe26005318b19697c1bc565fd2dc0b83d
ymekuria <ymekuria@gmail.com>
2025-02-05 14:17:13 -0800

    refactor(node_backend.js): update import path for plonk_native to point to the correct file location in the project feat(node_backend.js): add logging statement to output plonk_native object for debugging purposes

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -1,6 +1,8 @@
 // Provides: plonk_wasm
-var plonk_wasm = require('./plonk_wasm.js');
+// var plonk_wasm = require('./plonk_wasm.js');
 
 // inject a global plonk_native object into js environment
 // //Provides: plonk_native
-// var plonk_native = require('...Path to file');
+var plonk_native = require('../../native_prover/index.node'); // TODO: add to dune
+
+console.log('plonk_native', plonk_native);
