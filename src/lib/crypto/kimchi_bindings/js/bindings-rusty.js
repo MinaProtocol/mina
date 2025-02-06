77de21f8748242dafbcf4e996731d8e135d48d3f
ymekuria <ymekuria@gmail.com>
2025-02-05 18:43:48 -0800

    feat(bindings-rusty.js): add plonk_native global object

--- src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
+++ src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
@@ -1,16 +1,8 @@
 /* global plonk_native
  */
+
 // Provides: caml_do_cool_thingies
 // Requires: plonk_native
 var caml_do_cool_thingies = function () {
   return plonk_native.caml_do_cool_thingies();
 };
-
-// /* global plonk_wasm
-//  */
-
-// // Provides: caml_do_cool_thingies
-// // Requires: plonk_wasm
-// var caml_do_cool_thingies = function () {
-//   return plonk_wasm.caml_do_cool_thingies();
-// };
