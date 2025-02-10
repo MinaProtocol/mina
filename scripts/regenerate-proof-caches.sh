b1163eda6bd657f57b6ed3e913bbea5b572837d2
Geometer1729 <16kuhnb@gmail.com>
2025-02-10 15:29:26 -0500

    tweak script maybe fixing it?

--- scripts/regenerate-proof-caches.sh
+++ scripts/regenerate-proof-caches.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-
-for DIR in $(find -name proof_cache.json | sed 's/\/proof_cache.json//'); do
+rm -rf _build
+for DIR in $(find ./src -name proof_cache.json | sed 's/\/proof_cache.json//'); do
   # Initialize the target file
-  echo [] > $DIR/proof_cache.json;
+  echo [] > $DIR/proof_cache_new.json;
   # Stage the new file for a git commit
-  git add -N $DIR/proof_cache.json;
   # Generate the cache file by running the tests
-  PROOF_CACHE_OUT=$PWD/$DIR/proof_cache.json dune runtest $DIR;
+  PROOF_CACHE_OUT=$PWD/$DIR/proof_cache_new.json dune runtest $DIR;
   # Re-run the tests using the cache. Throws an error if the test is
   # non-deterministic and caused a cache miss.
+  mv $PWD/$DIR/proof_cache_new.json $PWD/$DIR/proof_cache.json
+  git add $PWD/$DIR/proof_cache.json
   ERROR_ON_PROOF=true dune runtest $DIR;
 done
