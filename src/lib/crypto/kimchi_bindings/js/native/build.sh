a1adabe7e27353431165abea790e6e13d22d7e30
ymekuria <ymekuria@gmail.com>
2025-02-10 17:07:47 -0800

    chore(kimchi_bindings): move the built 'index.node' file to the correct directory for Neon addon

--- src/lib/crypto/kimchi_bindings/js/native/build.sh
+++ src/lib/crypto/kimchi_bindings/js/native/build.sh
@@ -4,11 +4,14 @@ set -euo pipefail
 +chmod -R +w ../../native_prover
 cd ../../native_prover
 
-# build the Neon addon (produces 'index.node').
-npm run build
-popd
 
-cp ../../native_prover/index.node plonk.node
+cd ../../native_prover
+
+# build the Neon addon (produces 'index.node').
+npm install --no-audit --ignore-scripts
+npm run build
+
+cp index.node ../js/native/plonk.node
 
 echo "Neon project built and 'index.node' copied to the 'native' folder."
 
