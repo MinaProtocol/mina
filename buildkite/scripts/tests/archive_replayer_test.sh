6dd20ff727d1b949d3aff61916663c55757bf71a
dkijania <dariusz@o1labs.org>
2025-02-12 10:59:53 +0100

    setup git env vars for tests

--- buildkite/scripts/tests/archive_replayer_test.sh
+++ buildkite/scripts/tests/archive_replayer_test.sh
@@ -4,5 +4,6 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" || exit 1 ; pwd -P )"
 
+source "${SCRIPTPATH}/../export-git-env-vars.sh"
 source "${SCRIPTPATH}/../debian/install.sh" "mina-test-suite"
 source "${SCRIPTPATH}/../../scripts/replayer-test.sh -i /etc/mina/test/archive/sample_db/replayer_input_file.json -p $PG_CONN -a mina-replayer
