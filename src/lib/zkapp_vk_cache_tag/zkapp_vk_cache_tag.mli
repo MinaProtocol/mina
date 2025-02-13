670048d7ee7c08d880954bb40b5f73413db0ce60
dkijania <dariusz@o1labs.org>
2025-02-13 22:49:02 +0100

    POC for caching vk keys

--- src/lib/zkapp_vk_cache_tag/zkapp_vk_cache_tag.mli
+++ src/lib/zkapp_vk_cache_tag/zkapp_vk_cache_tag.mli
@@ -10,6 +10,10 @@ val create_db :
   -> logger:Logger.t
   -> (cache_db, [> `Initialization_error of Error.t ]) Deferred.Result.t
 
-val read_key_from_disk : cache_db -> t -> Verification_key_wire.t
+val read_key_from_disk : t -> Verification_key_wire.t
 
 val write_key_to_disk : cache_db -> Verification_key_wire.t -> t
+
+module For_tests : sig
+  val create_db : unit -> cache_db
+end
