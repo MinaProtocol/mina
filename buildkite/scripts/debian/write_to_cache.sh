7ecec216644a78a9cae53a3c163fe0f50a5efeba
dkijania <dariusz@o1labs.org>
2025-02-14 19:22:22 +0100

    properly execure cache.sh script

--- buildkite/scripts/debian/write_to_cache.sh
+++ buildkite/scripts/debian/write_to_cache.sh
@@ -3,5 +3,5 @@
 CODENAME=$1
 
 for entry in _build/*.deb; do
-  source ./buildkite/scripts/cache.sh write $entry debians/${CODENAME}
+  ./buildkite/scripts/cache.sh write "$entry" debians/${CODENAME}
 done 
