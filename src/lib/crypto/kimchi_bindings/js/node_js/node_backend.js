6ae619d69f0b35032d9f456e6f5c66dd8cd458c0
ymekuria <ymekuria@gmail.com>
2025-02-15 00:46:19 -0800

    refactor(node_backend.js): update import path for plonk_native module to point to the correct location in the project structure

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -2,6 +2,4 @@
 var plonk_wasm = require('./plonk_wasm.js');
 
 // Provides: plonk_native
-var plonk_native = require('./index.node');
-
-// console.log('plonk_native', plonk_native);
+var plonk_native = require('./native/plonk_native.node');
