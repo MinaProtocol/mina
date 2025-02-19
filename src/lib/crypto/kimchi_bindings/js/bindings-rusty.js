4505a1325c0883143c602480e537a388aabeee91
ymekuria <ymekuria@gmail.com>
2025-02-18 20:47:05 -0800

    feat(bindings-rusty.js): add fp_poseidon_block_cipher function for Poseidon block cipher support

--- src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
+++ src/lib/crypto/kimchi_bindings/js/bindings-rusty.js
@@ -6,3 +6,7 @@
 var caml_do_cool_thingies = function () {
   return plonk_native.caml_do_cool_thingies();
 };
+
+var fp_poseidon_block_cipher = function () {
+  return plonk_native.fp_poseidon_block_cipher();
+};
