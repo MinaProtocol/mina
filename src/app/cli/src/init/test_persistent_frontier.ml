eba727af1eb858beaaa708355d54ebc7b4ed9894
glyh <lyhokia@gmail.com>
2025-02-21 16:25:36 +0800

    add timing in test-persistent-frontier

--- src/app/cli/src/init/test_persistent_frontier.ml
+++ src/app/cli/src/init/test_persistent_frontier.ml
@@ -1,24 +1,21 @@
 open Persistent_frontier
 open Core
 
-let deserialize_root_hash ~logger ~frontier_db_path =
-  [%log info] "Start `deserializae_root_value_from_db`, Current DB path: %s"
-    frontier_db_path ;
-  [%log info] "Loading database" ;
-  let db = Database.create ~logger ~directory:frontier_db_path in
+let deserialize_root_hash ~logger ~db =
   [%log info] "Querying root hash from database and attempt to deserialize it" ;
-  ( match Database.get_root_hash db with
+  match Database.get_root_hash db with
   | Ok hash ->
       [%log info] "Got hash %s" (Pasta_bindings.Fp.to_string hash)
   | Error _ ->
-      [%log info] "No root hash found" ) ;
-  [%log info] "Done deserialize_root_hash" ;
-  Database.close db
+      [%log error] "No root hash found"
 
 let root_hash_deserialization_limit = Time_ns.Span.of_ms 2000.0
 
 let main ~frontier_db_path ~root_compatible () =
   let logger = Logger.create () in
+  [%log info] "Current DB path: %s" frontier_db_path ;
+  [%log info] "Loading database" ;
+  let db = Database.create ~logger ~directory:frontier_db_path in
   ( match root_compatible with
   | Some false ->
       ()
@@ -27,11 +24,15 @@ let main ~frontier_db_path ~root_compatible () =
          To maintain compatibility, run a deserialization round first,
          to ensure we have `root_hash` and `root_common` inside the DB
       *)
-      deserialize_root_hash ~logger ~frontier_db_path ) ;
+      deserialize_root_hash ~logger ~db ) ;
   let start_time = Time_ns.now () in
-  let () = deserialize_root_hash ~logger ~frontier_db_path in
+  let () = deserialize_root_hash ~logger ~db in
   let end_time = Time_ns.now () in
   let duration = Time_ns.diff end_time start_time in
+  [%log info]
+    "Querying root hash on patched persistence frontier database takes %s"
+    (Time_ns.Span.to_string duration) ;
+  Database.close db ;
   assert (Time_ns.Span.compare duration root_hash_deserialization_limit < 0)
 
 let command =
