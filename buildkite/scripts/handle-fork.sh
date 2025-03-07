972031052b308a481e1fe2c8eb0e23fe4e58d776
dkijania <dariusz@o1labs.org>
2025-03-07 19:17:33 +0100

    remove fetch mina from handle fork. do it on demand

--- buildkite/scripts/handle-fork.sh
+++ buildkite/scripts/handle-fork.sh
@@ -8,7 +8,6 @@ if [ "${BUILDKITE_REPO}" ==  ${MINA_REPO} ]; then
     export FORK=0
 else
     git remote add mina ${MINA_REPO} || true
-    git fetch mina
     export REMOTE="mina"
     export FORK=1
 fi
