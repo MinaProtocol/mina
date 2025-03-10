1680263b11170fb93d1e5d4821c1b563dd034c74
dkijania <dariusz@o1labs.org>
2025-03-10 15:39:28 +0100

    refactor executor module. Introduce Make_Executor functor

--- src/test/mina_automation/missing_blocks_auditor.ml
+++ src/test/mina_automation/missing_blocks_auditor.ml
@@ -2,10 +2,12 @@
 Module to run missing_block_auditor scripts which finds gaps in given Postgresql database
 *)
 
-open Executor
-include Executor
+module Paths = struct
+  let dune_name = "src/app/missing_blocks_auditor/missing_blocks_auditor.exe"
 
-let of_context context =
-  Executor.of_context ~context
-    ~dune_name:"src/app/missing_blocks_auditor/missing_blocks_auditor.exe"
-    ~official_name:"mina-missing-blocks-auditor"
+  let official_name = "mina-missing-blocks-auditor"
+end
+
+module PathFinder = Executor.Make_PathFinder (Paths)
+
+let path = PathFinder.standalone_path_exn
