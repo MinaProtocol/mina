ad2ff78dc72a8c59f53e6ca3b1a8f3eeed459ac3
glyh <lyhokia@gmail.com>
2025-03-20 21:30:29 +0800

    Introduce a `transaction_pool_proxy` that can pass access to find_by_hash in a transaction_pool down to Staged_ledger.check_commands

--- src/lib/transition_frontier/frontier_base/breadcrumb.mli
+++ src/lib/transition_frontier/frontier_base/breadcrumb.mli
@@ -29,6 +29,7 @@ val create :
 
 val build :
      ?skip_staged_ledger_verification:[ `All | `Proofs ]
+  -> ?transaction_pool_proxy:Staged_ledger.transaction_pool_proxy
   -> proof_cache_db:Proof_cache_tag.cache_db
   -> logger:Logger.t
   -> precomputed_values:Precomputed_values.t
