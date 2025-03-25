096c264f3e925245395acb678a086df13b6cf263
dkijania <dariusz@o1labs.org>
2025-03-25 13:00:58 +0100

    allow current branch to upload data to influx db

--- buildkite/scripts/bench/run.sh
+++ buildkite/scripts/bench/run.sh
@@ -10,7 +10,7 @@ EXTRA_ARGS="${EXTRA_ARGS:-}"
 
 source buildkite/scripts/bench/install.sh
 
-MAINLINE_BRANCHES="-m develop -m compatible -m master -m dkijania/enhance_benchmarks"
+MAINLINE_BRANCHES="-m develop -m compatible -m master -m dkijania/bench_for_ledger_test"
 while [[ "$#" -gt 0 ]]; do case $1 in
   heap-usage) BENCHMARK="heap-usage"; ;;
   mina-base) BENCHMARK="mina-base"; ;;
