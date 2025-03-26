5c08d79c13bc71b2315ec831ac6357cdb0a6cf81
glyh <lyhokia@gmail.com>
2025-03-26 15:18:39 +0800

    replace immediate hash recalculation in `remove_account_and_update_hashes` and `set` in Masking_merkle_tree with temporarily storing the changed account at t.unhashed_accounts

--- src/lib/merkle_mask/masking_merkle_tree.ml
+++ src/lib/merkle_mask/masking_merkle_tree.ml
@@ -583,7 +583,7 @@ module Make (Inputs : Inputs_intf.S) = struct
     (* given a Merkle path corresponding to a starting address, calculate
        addresses and hashes for each node affected by the starting hash; that is,
        along the path from the account address to root *)
-    let addresses_and_hashes_from_merkle_path_exn merkle_path starting_address
+    let _addresses_and_hashes_from_merkle_path_exn merkle_path starting_address
         starting_hash : (Addr.t * Hash.t) list =
       let get_addresses_hashes height accum node =
         let last_address, last_hash = List.hd_exn accum in
@@ -635,16 +635,7 @@ module Make (Inputs : Inputs_intf.S) = struct
                 t.current_location <- Some prev_loc
             | None ->
                 t.current_location <- None ) ;
-      (* update hashes *)
-      let account_address = Location.to_path_exn location in
-      let account_hash = Hash.empty_account in
-      let merkle_path = merkle_path t location in
-      let addresses_and_hashes =
-        addresses_and_hashes_from_merkle_path_exn merkle_path account_address
-          account_hash
-      in
-      List.iter addresses_and_hashes ~f:(fun (addr, hash) ->
-          self_set_hash t addr hash )
+      t.unhashed_accounts <- (None, location) :: t.unhashed_accounts
 
     let set_account_unsafe t location account =
       assert_is_attached t ;
@@ -661,16 +652,7 @@ module Make (Inputs : Inputs_intf.S) = struct
     let set t location account =
       assert_is_attached t ;
       set_account_unsafe t location account ;
-      (* Update merkle path. *)
-      let account_address = Location.to_path_exn location in
-      let account_hash = Hash.hash_account account in
-      let merkle_path = merkle_path t location in
-      let addresses_and_hashes =
-        addresses_and_hashes_from_merkle_path_exn merkle_path account_address
-          account_hash
-      in
-      List.iter addresses_and_hashes ~f:(fun (addr, hash) ->
-          self_set_hash t addr hash )
+      t.unhashed_accounts <- (Some account, location) :: t.unhashed_accounts
 
     (* if the mask's parent sets an account, we can prune an entry in the mask
        if the account in the parent is the same in the mask
