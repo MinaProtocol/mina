e1a61474f00c034a5f0b7898391659c71e462575
Danny Willems <be.danny.willems@gmail.com>
2025-03-26 19:52:04 +0100

    plonk-wasm: remove unused SeqAccess structure
    Making clippy a bit happier

--- src/lib/crypto/kimchi_bindings/wasm/src/wasm_ocaml_serde/de.rs
+++ src/lib/crypto/kimchi_bindings/wasm/src/wasm_ocaml_serde/de.rs
@@ -5,24 +5,6 @@ use wasm_bindgen::{JsCast, JsValue, UnwrapThrowExt};
 
 use super::{Error, Result};
 
-struct SeqAccess {
-    iter: js_sys::IntoIter,
-}
-
-impl<'de> de::SeqAccess<'de> for SeqAccess {
-    type Error = Error;
-
-    fn next_element_seed<T: de::DeserializeSeed<'de>>(
-        &mut self,
-        seed: T,
-    ) -> Result<Option<T::Value>> {
-        Ok(match self.iter.next().transpose()? {
-            Some(value) => Some(seed.deserialize(Deserializer::from(value))?),
-            None => None,
-        })
-    }
-}
-
 struct ObjectAccess {
     data: Array,
     fields: std::slice::Iter<'static, &'static str>,
