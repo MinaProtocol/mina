b540d9abfee48a3661fcfe408a4d57ba556016c6
glyh <lyhokia@gmail.com>
2025-03-26 12:26:32 +0800

    refactor test persistent frontier using core_bench

--- src/app/cli/src/init/test_persistent_frontier.ml
+++ src/app/cli/src/init/test_persistent_frontier.ml
@@ -1,5 +1,6 @@
 open Persistent_frontier
 open Core
+open Core_bench
 
 let deserialize_root_hash ~logger ~db =
   [%log info] "Querying root hash from database and attempt to deserialize it" ;
@@ -9,71 +10,23 @@ let deserialize_root_hash ~logger ~db =
   | Error _ ->
       [%log error] "No root hash found"
 
-let root_hash_deserialization_limit_ms = 2000.0
-
-let main ~frontier_db_path ~no_root_compatible ~num_of_samples () =
-  let logger = Logger.create () in
-  [%log info] "Current DB path: %s" frontier_db_path ;
-  [%log info] "Loading database" ;
-  let db = Database.create ~logger ~directory:frontier_db_path in
-  if not no_root_compatible then (
-    (*
-      To maintain compatibility, run a deserialization round first,
-      to ensure we have `root_hash` and `root_common` inside the DB
-      *)
-    [%log info] "Updating the database to a clean state" ;
-    Database.update_root_clean db
-    |> Result.ok
-    |> Option.value_exn ~message:"Can't update root to a clean state" ) ;
-  assert (Database.is_root_replaced_by_common_and_hash db) ;
-  let rec sample cnt =
-    match cnt with
-    | 0 ->
-        []
-    | _ ->
-        let start_time = Time_ns.now () in
-        deserialize_root_hash ~logger ~db ;
-        let end_time = Time_ns.now () in
-        let duration = Time_ns.diff end_time start_time in
-        Time_ns.Span.to_ms duration :: sample (cnt - 1)
-  in
-  assert (num_of_samples >= 1) ;
-  let duration_ms_samples = sample num_of_samples in
-  let max_duration_ms =
-    List.max_elt duration_ms_samples ~compare:Float.compare |> Option.value_exn
-  in
-  let min_duration_ms =
-    List.min_elt duration_ms_samples ~compare:Float.compare |> Option.value_exn
-  in
-  let avg_duration_ms =
-    List.sum (module Float) duration_ms_samples ~f:ident
-    /. Float.of_int (List.length duration_ms_samples)
-  in
-
-  [%log info] "Querying root hash on patched persistence frontier database"
-    ~metadata:
-      [ ("min_duration_ms", `Float min_duration_ms)
-      ; ("max_duration_ms", `Float max_duration_ms)
-      ; ("avg_duration_ms", `Float avg_duration_ms)
-      ] ;
-  Database.close db ;
-  assert (Float.compare max_duration_ms root_hash_deserialization_limit_ms < 0)
-
 let command =
-  let open Command.Let_syntax in
-  Command.basic ~summary:"tests for persistent frontier"
-    (let%map_open frontier_db_path =
-       flag "--frontier-db" ~aliases:[ "-f" ]
-         ~doc:"Path to frontier DB this app is testing on" (required string)
-     and num_of_samples =
-       flag "--samples" ~aliases:[ "-s" ]
-         ~doc:"Number of rounds of hash query should we run. (default 5)"
-         (optional_with_default 5 int)
-     and no_root_compatible =
-       flag "--no-root-compatible" ~aliases:[ "-r" ]
-         ~doc:
-           "Do not perform hash query once to ensure the compatibility with \
-            old frontier database"
-         no_arg
-     in
-     main ~frontier_db_path ~no_root_compatible ~num_of_samples )
+  Bench.make_command
+    [ Bench.Test.create ~name:"hello" (fun () -> ignore (10 : int))
+    ; Bench.Test.create_with_initialization
+        ~name:"Test Persistent Frontier Root Hash query" (fun `init ->
+          let frontier_db_path =
+            Sys.getenv_exn "TEST_PERSISTENT_FRONTIER_DB_PATH"
+          in
+          let logger = Logger.create () in
+          [%log info] "Current DB path: %s" frontier_db_path ;
+          [%log info] "Loading database" ;
+          let db = Database.create ~logger ~directory:frontier_db_path in
+          (* To maintain compatibility, call `update_root_clean` first *)
+          [%log info] "Updating the database to a clean state" ;
+          Database.update_root_clean db
+          |> Result.ok
+          |> Option.value_exn ~message:"Can't update root to a clean state" ;
+          assert (Database.is_root_replaced_by_common_and_hash db) ;
+          fun () -> deserialize_root_hash ~logger ~db )
+    ]
