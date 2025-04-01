ffcb8e089a913f1233a7bbc14105b80504ef8b4e
Danny Willems <be.danny.willems@gmail.com>
2025-04-01 17:27:35 +0200

    kimchi-bindings/wasm: use code from proof-systems

--- src/lib/crypto/kimchi_bindings/js/node_js/build.sh
+++ src/lib/crypto/kimchi_bindings/js/node_js/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+CURRENT_DIRECTORY=$(pwd)
+
 if [[ -z "${PLONK_WASM_NODEJS-}" ]]; then
-    export RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals -C link-arg=--no-check-features -C link-arg=--max-memory=4294967296"
-    # The version should stay in line with the one in kimchi_bindings/wasm/rust-toolchain.toml
-    rustup run nightly-2024-06-13 wasm-pack build --target nodejs --out-dir ../js/node_js ../../wasm -- -Z build-std=panic_abort,std --features nodejs
+    PLONK_WASM_NODEJS_OUTDIR=${CURRENT_DIRECTORY} make -C ../../../proof-systems build-nodejs
 else
     cp "$PLONK_WASM_NODEJS"/* -R .
 fi
