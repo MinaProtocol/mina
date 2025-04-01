0021cea0cf3d48c6ba43c4dd8b339235acfeea8b
Brian Kuhns <16kuhnb@gmail.com>
2025-04-01 11:07:18 -0400

    Update src/lib/crypto/kimchi_bindings/js/wrap_wasm/wrap.js
    Co-authored-by: Richard Pringle <rpring9@gmail.com>

--- src/lib/crypto/kimchi_bindings/js/wrap_wasm/wrap.js
+++ src/lib/crypto/kimchi_bindings/js/wrap_wasm/wrap.js
@@ -1,7 +1,7 @@
 // Provides: wrap_wasm
 // Requires: wrap_wasm_proof,wrap_wasm_oracles,wrap_wasm_prover_index,tsBindings,wrap_wasm_verifier_index,wrap_wasm_srs,wrap_wasm_gate_vector
 function wrap_wasm(plonk_wasm){
-  // jsoo hates the spread operator but this is a shalow coppy
+  // jsoo hates the spread operator but this is a shalow copy
   var plonk_intf = Object.assign({}, plonk_wasm);
   var tsRustConversion = tsBindings.rustConversion(plonk_wasm);
 
