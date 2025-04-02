d14e260ff01f6c45546baaee02350031850edcf2
Christian Despres <christian@o1labs.org>
2025-04-02 11:02:31 -0400

    Separate hash inputs and hashing for wrap proof

--- src/lib/pickles/wrap_hack.ml
+++ src/lib/pickles/wrap_hack.ml
@@ -42,10 +42,10 @@ let pad_accumulator (xs : (Tock.Proof.Challenge_polynomial.t, _) Vector.t) =
       }
   |> Vector.to_list
 
-(* Hash the me only, padding first. *)
-let hash_messages_for_next_wrap_proof (type n) (_max_proofs_verified : n Nat.t)
+let hash_message_inputs_for_next_wrap_proof (type n)
+    (_max_proofs_verified : n Nat.t)
     (t :
-      ( Tick.Curve.Affine.t
+      ( _
       , (_, n) Vector.t )
       Composition_types.Wrap.Proof_state.Messages_for_next_wrap_proof.t ) =
   let t =
@@ -53,10 +53,17 @@ let hash_messages_for_next_wrap_proof (type n) (_max_proofs_verified : n Nat.t)
       old_bulletproof_challenges = pad_challenges t.old_bulletproof_challenges
     }
   in
-  Tock_field_sponge.digest Tock_field_sponge.params
-    (Composition_types.Wrap.Proof_state.Messages_for_next_wrap_proof
-     .to_field_elements t ~g1:(fun ((x, y) : Tick.Curve.Affine.t) -> [ x; y ])
-    )
+  Composition_types.Wrap.Proof_state.Messages_for_next_wrap_proof
+  .to_field_elements t ~g1:(fun ((x, y) : Tick.Curve.Affine.t) -> [ x; y ] )
+
+(* Hash the me only, padding first. *)
+let hash_messages_for_next_wrap_proof (type n) (max_proofs_verified : n Nat.t)
+    (t :
+      ( Tick.Curve.Affine.t
+      , (_, n) Vector.t )
+      Composition_types.Wrap.Proof_state.Messages_for_next_wrap_proof.t ) =
+  let input = hash_message_inputs_for_next_wrap_proof max_proofs_verified t in
+  Tock_field_sponge.digest Tock_field_sponge.params input
 
 (* Pad the messages_for_next_wrap_proof of a proof *)
 let pad_proof (type mlmb) (T p : (mlmb, _) Proof.t) :
