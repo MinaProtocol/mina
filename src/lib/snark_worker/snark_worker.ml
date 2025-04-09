f0507d95e4a79b7f4633efaf7efbdfbc974f64f7
glyh <lyhokia@gmail.com>
2025-04-09 22:21:58 +0800

    add comments on different modules inside a snark worker

--- src/lib/snark_worker/snark_worker.ml
+++ src/lib/snark_worker/snark_worker.ml
@@ -1,7 +1,13 @@
+(* module interacting with CLIs *)
 module Cli_helper = Cli_helper
+
+(* module providing versioned RPCs *)
 module Rpcs_versioned = Rpcs_versioned
+
+(* module containing work type this snark worker could deal with *)
 module Concrete_work = Concrete_work
 
+(* module providing swappable implementation for worker *)
 module Impl = struct
   module Prod = Worker_impl_prod.Impl
   module Debug = Worker_impl_debug.Impl
