96152128941dac58dbfa2a7ca0589b33256694a8
glyh <lyhokia@gmail.com>
2025-04-14 15:38:10 +0800

    ro: N2 -> N3

--- src/lib/pickles/ro.mli
+++ src/lib/pickles/ro.mli
@@ -20,8 +20,8 @@ val tick : unit -> Backend.Tick.Field.t
 
 val scalar_chal :
      unit
-  -> (Core_kernel.Int64.t, Pickles_types.Nat.N2.n) Pickles_types.Vector.t
+  -> (Core_kernel.Int64.t, Pickles_types.Nat.N3.n) Pickles_types.Vector.t
      Import.Scalar_challenge.t
 
 val chal :
-  unit -> (Core_kernel.Int64.t, Pickles_types.Nat.N2.n) Pickles_types.Vector.t
+  unit -> (Core_kernel.Int64.t, Pickles_types.Nat.N3.n) Pickles_types.Vector.t
