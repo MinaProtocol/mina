096fd95cd60bdeb4af1bd6f8c64cd5bd3de33ce4
mrmr1993 <mr_1993@hotmail.co.uk>
2025-04-16 18:21:57 +0100

    Remove misleading comment

--- src/lib/transaction_inclusion_status/transaction_inclusion_status.ml
+++ src/lib/transaction_inclusion_status/transaction_inclusion_status.ml
@@ -38,8 +38,6 @@ let get_status ~frontier_broadcast_pipe ~transaction_pool cmd =
         |> Mina_block.Validated.valid_commands
         |> List.exists ~f:(fun { data = found; _ } ->
                let found' = User_command.forget_check found in
-               (* We do equality check w/o proofs and hashes first to ensure
-                  we don't read proofs from disk without necessity. *)
                User_command.equal_ignoring_proofs_and_hashes cmd found'
                && User_command.Stable.Latest.equal cmd
                     (User_command.read_all_proofs_from_disk found') )
