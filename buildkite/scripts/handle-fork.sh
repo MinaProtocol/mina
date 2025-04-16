a3487b9e3401d5484f2c61405283c49e96e51363
dkijania <dariusz@o1labs.org>
2025-04-16 20:38:18 +0200

    revert usage of REMOTE env var

--- buildkite/scripts/handle-fork.sh
+++ buildkite/scripts/handle-fork.sh
@@ -5,30 +5,15 @@ set -xeo pipefail
 export MINA_REPO="https://github.com/MinaProtocol/mina.git"
 
 if [ -z "${BUILDKITE_PULL_REQUEST_REPO}" ]; then
-    echo "This is not a Forked repo, skipping..."
-    export REMOTE="origin"
+    echo "Unable to detect fork without BUILDKITE_PULL_REQUEST_REPO"
+    echo "Looks like CI was ran without ci-build-me! skipping..."
     export FORK=0
     exit 0
 fi
 
 if [[ "${BUILDKITE_PULL_REQUEST_REPO}" ==  "${MINA_REPO}" ]]; then
     echo "This is not a Forked repo, skipping..."
-    export REMOTE="origin"
     export FORK=0
 else
-    export REMOTE="fork"
     export FORK=1
-    if ! git remote -v | grep "${BUILDKITE_PULL_REQUEST_REPO}"; then
-        git remote add fork ${BUILDKITE_PULL_REQUEST_REPO}
-        git fetch fork --recurse-submodules --tags
-        # This is a workaround for missing git-lfs
-        rm -f .git/hooks/post-checkout || true
-        git switch -c ${BUILDKITE_BRANCH}
-    else
-        git remote set-url fork ${BUILDKITE_PULL_REQUEST_REPO}
-        git fetch fork --recurse-submodules --tags
-        # This is a workaround for missing git-lfs
-        rm -f .git/hooks/post-checkout || true
-        git switch ${BUILDKITE_BRANCH}
-    fi
 fi
