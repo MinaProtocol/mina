4b2184fcb387a5ad5b357411445895537bcbfc51
martyall <martyall@protonmail.com>
2025-04-17 08:08:54 -0700

    Restore comments that were accidentally deleted

--- src/lib/pickles/inductive_rule.ml
+++ src/lib/pickles/inductive_rule.ml
@@ -24,10 +24,6 @@ module type Intf = sig
     end
   end
 
-  (** This type relates the types of the input and output types of an inductive
-    rule's [main] function to the type of the public input to the resulting
-    circuit.
-*)
   type ( 'var
        , 'value
        , 'input_var
@@ -189,14 +185,57 @@ module Make (P : Proof_intf) : Intf with type 'a proof = 'a P.t = struct
   type ('prev_vars, 'widths, 'public_output, 'auxiliary_output) main_return =
     { previous_proof_statements :
         ('prev_vars, 'widths) H2.T(Previous_proof_statement).t
+          (** A list of booleans, determining whether each previous proof must
+          verify.
+      *)
     ; public_output : 'public_output
+          (** The publicly-exposed output from the circuit's main function. *)
     ; auxiliary_output : 'auxiliary_output
+          (** The auxiliary output from the circuit's main function. This value
+        is returned to the prover, but not exposed to or used by verifiers.
+    *)
     }
 
   module Make (M : sig
     type _ t
   end) =
   struct
+    (** This type models an "inductive rule". It includes
+     - the list of previous statements which this one assumes
+     - the snarky main function
+ 
+     The types parameters are:
+     - ['prev_vars] the tuple-list of public input circuit types to the previous
+       proofs.
+       - For example, [Boolean.var * (Boolean.var * unit)] represents 2 previous
+         proofs whose public inputs are booleans
+     - ['prev_values] the tuple-list of public input non-circuit types to the
+       previous proofs.
+       - For example, [bool * (bool * unit)] represents 2 previous proofs whose
+         public inputs are booleans.
+     - ['widths] is a tuple list of the maximum number of previous proofs each
+       previous proof itself had.
+       - For example, [Nat.z Nat.s * (Nat.z * unit)] represents 2 previous
+         proofs where the first has at most 1 previous proof and the second had
+         zero previous proofs.
+     - ['heights] is a tuple list of the number of inductive rules in each of
+       the previous proofs
+       - For example, [Nat.z Nat.s Nat.s * (Nat.z Nat.s * unit)] represents 2
+         previous proofs where the first had 2 inductive rules and the second
+         had 1.
+     - ['a_var] is the in-circuit type of the [main] function's public input.
+     - ['a_value] is the out-of-circuit type of the [main] function's public
+       input.
+     - ['ret_var] is the in-circuit type of the [main] function's public output.
+     - ['ret_value] is the out-of-circuit type of the [main] function's public
+       output.
+     - ['auxiliary_var] is the in-circuit type of the [main] function's
+       auxiliary data, to be returned to the prover but not exposed in the
+       public input.
+     - ['auxiliary_value] is the out-of-circuit type of the [main] function's
+       auxiliary data, to be returned to the prover but not exposed in the
+       public input.
+   *)
     type ( 'prev_vars
          , 'prev_values
          , 'widths
@@ -241,8 +280,6 @@ module Make (P : Proof_intf) : Intf with type 'a proof = 'a P.t = struct
 
   module Promise = Make (Promise)
   module Deferred = Make (Async_kernel.Deferred)
-
-  (* This is the key fix - we need to include the Id-based module to match the MLI *)
   include Make (Id)
 end
 
