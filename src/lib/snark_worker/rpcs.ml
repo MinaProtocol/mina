ca5a0a2d8446a2f79f49d502e97e32a3df2a4703
glyh <lyhokia@gmail.com>
2025-04-18 18:27:11 +0800

    remove functor from Snark_worker.Rpcs

--- src/lib/snark_worker/rpcs.ml
+++ src/lib/snark_worker/rpcs.ml
@@ -1,6 +1,7 @@
-open Core_kernel
 open Async
+open Core_kernel
 open Signature_lib
+open Snark_work_lib
 
 (* for versioning of the types here, see
 
@@ -13,79 +14,74 @@ open Signature_lib
    return types with bin_io; the versioned modules are defined in snark_worker.ml
 *)
 
-module Make (Inputs : Intf.Inputs_intf) = struct
-  open Inputs
-  open Snark_work_lib
+module Get_work = struct
+  module Master = struct
+    let name = "get_work"
 
-  module Get_work = struct
-    module Master = struct
-      let name = "get_work"
+    module T = struct
+      type query = unit
 
-      module T = struct
-        type query = unit
-
-        type response =
-          ( ( Transaction_witness.Stable.Latest.t
-            , Ledger_proof.t )
-            Work.Single.Spec.t
-            Work.Spec.t
-          * Public_key.Compressed.t )
-          option
-      end
-
-      module Caller = T
-      module Callee = T
-    end
-
-    include Master.T
-    include Versioned_rpc.Both_convert.Plain.Make (Master)
-  end
-
-  module Submit_work = struct
-    module Master = struct
-      let name = "submit_work"
-
-      module T = struct
-        type query =
-          ( ( Transaction_witness.Stable.Latest.t
-            , Ledger_proof.t )
-            Work.Single.Spec.t
-            Work.Spec.t
+      type response =
+        ( ( Transaction_witness.Stable.Latest.t
           , Ledger_proof.t )
-          Work.Result.t
-
-        type response = unit
-      end
-
-      module Caller = T
-      module Callee = T
+          Work.Single.Spec.t
+          Work.Spec.t
+        * Public_key.Compressed.t )
+        option
     end
 
-    include Master.T
-    include Versioned_rpc.Both_convert.Plain.Make (Master)
+    module Caller = T
+    module Callee = T
   end
 
-  module Failed_to_generate_snark = struct
-    module Master = struct
-      let name = "failed_to_generate_snark"
+  include Master.T
+  include Versioned_rpc.Both_convert.Plain.Make (Master)
+end
 
-      module T = struct
-        type query =
-          Error.t
-          * ( Transaction_witness.Stable.Latest.t
-            , Ledger_proof.t )
-            Work.Single.Spec.t
-            Work.Spec.t
-          * Public_key.Compressed.t
+module Submit_work = struct
+  module Master = struct
+    let name = "submit_work"
 
-        type response = unit
-      end
+    module T = struct
+      type query =
+        ( ( Transaction_witness.Stable.Latest.t
+          , Ledger_proof.t )
+          Work.Single.Spec.t
+          Work.Spec.t
+        , Ledger_proof.t )
+        Work.Result.t
 
-      module Caller = T
-      module Callee = T
+      type response = unit
     end
 
-    include Master.T
-    include Versioned_rpc.Both_convert.Plain.Make (Master)
+    module Caller = T
+    module Callee = T
   end
+
+  include Master.T
+  include Versioned_rpc.Both_convert.Plain.Make (Master)
+end
+
+module Failed_to_generate_snark = struct
+  module Master = struct
+    let name = "failed_to_generate_snark"
+
+    module T = struct
+      type query =
+        Error.t
+        * ( Transaction_witness.Stable.Latest.t
+          , Ledger_proof.t )
+          Work.Single.Spec.t
+          Work.Spec.t
+        * Public_key.Compressed.t
+
+      type response = unit
+    end
+
+    module Caller = T
+    module Callee = T
+  end
+
+  include Master.T
+  include Versioned_rpc.Both_convert.Plain.Make (Master)
 end
