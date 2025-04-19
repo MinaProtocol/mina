64d50c95e123d09b07f9c00e31ad81a32de988e3
glyh <lyhokia@gmail.com>
2025-04-19 21:44:54 +0800

    Add Work_partitioner.Work_partitioner.Zkapp_command_job_with_status

--- src/lib/work_partitioner/work_partitioner.ml
+++ src/lib/work_partitioner/work_partitioner.ml
@@ -1,1 +1,6 @@
 module Shared = Shared
+module Partitioned_work = Snark_work_lib.Partitioned
+
+module Zkapp_command_job_with_status = With_job_status.Make (struct
+  type t = Partitioned_work.Zkapp_command_job.Stable.Latest.t
+end)
