25d99187136cd520940b099827e14df6171c708a
glyh <lyhokia@gmail.com>
2025-04-19 16:42:31 +0800

    add Work_partitioner.t and corresponding initializer

--- src/lib/work_partitioner/work_partitioner.ml
+++ src/lib/work_partitioner/work_partitioner.ml
@@ -58,3 +58,43 @@ module Sent_job_pool =
   Job_pool.Make
     (Partitioned_work.Zkapp_command_job.UUID)
     (Zkapp_command_job_with_status)
+
+type t =
+  { logger : Logger.t
+  ; transaction_snark : (module Transaction_snark.S)
+        (* WARN: we're mixing UUID for `pairing_pool` and `zkapp_command_jobs.
+           Should be fine *)
+  ; uuid_generator : Uuid_generator.t (* NOTE: Fields for pooling *)
+  ; pairing_pool : (Partitioned_work.Pairing.UUID.t, Single_work.t) Hashtbl.t
+        (* if one single work from underlying Work_selector is completed but
+           not the other. throw it here. *)
+  ; zkapp_command_jobs : Zkapp_command_job_pool.t
+        (* NOTE: Fields for reissue pooling*)
+  ; reassignment_timeout : Time.Span.t
+  ; jobs_sent_by_partitioner : Sent_job_pool.t
+        (* we only track tasks created by a Work_partitioner here. For reissue
+           of regular jobs, we still turn to the underlying Work_selector *)
+        (* WARN: we're assuming everything in this queue is sorted in time from old to new.
+           So queue head is the oldest task.
+        *)
+  ; mutable first_in_pair : Work_types.Compact.Single.Spec.t option
+        (* When receving a `Two works from the underlying Work_selector, store one of them here,
+           so we could issue them to another worker.
+        *)
+  }
+
+let create ~(reassignment_timeout : Time.Span.t) ~(logger : Logger.t) : t =
+  let module M = Transaction_snark.Make (struct
+    let constraint_constants = Genesis_constants.Compiled.constraint_constants
+
+    let proof_level = Genesis_constants.Compiled.proof_level
+  end) in
+  { logger
+  ; transaction_snark = (module M)
+  ; uuid_generator = Uuid_generator.create ()
+  ; pairing_pool = Hashtbl.create (module Partitioned_work.Pairing.UUID)
+  ; zkapp_command_jobs = Zkapp_command_job_pool.create ()
+  ; reassignment_timeout
+  ; jobs_sent_by_partitioner = Sent_job_pool.create ()
+  ; first_in_pair = None
+  }
