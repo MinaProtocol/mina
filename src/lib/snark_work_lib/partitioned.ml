136b5a6bcbd7908321c0e0d3733e7cf2a4404007
glyh <lyhokia@gmail.com>
2025-04-20 15:17:32 +0800

    move Work_selector.{work, in_memory_work} to Snark_work_lib.Partitioned.Selector_work

--- src/lib/snark_work_lib/partitioned.ml
+++ src/lib/snark_work_lib/partitioned.ml
@@ -133,3 +133,32 @@ module Zkapp_command_job = struct
       ({ spec; pairing_id; job_uuid } : Stable.Latest.t) : t =
     { spec = Spec.cache ~proof_cache_db spec; pairing_id; job_uuid }
 end
+
+module Selector_work = struct
+  [%%versioned
+  module Stable = struct
+    [@@@no_toplevel_latest_type]
+
+    module V1 = struct
+      type t =
+        ( Transaction_witness.Stable.V2.t
+        , Ledger_proof.Stable.V2.t )
+        Work.Single.Spec.Stable.V2.t
+      [@@deriving sexp, yojson]
+
+      let to_latest = Fn.id
+    end
+  end]
+
+  type t = (Transaction_witness.t, Ledger_proof.Cached.t) Work.Single.Spec.t
+
+  let materialize : t -> Stable.Latest.t =
+    Work.Single.Spec.map
+      ~f_witness:Transaction_witness.read_all_proofs_from_disk
+      ~f_proof:Ledger_proof.Cached.read_proof_from_disk
+
+  let cache ~(proof_cache_db : Proof_cache_tag.cache_db) : Stable.Latest.t -> t
+      =
+    Work.Single.Spec.map ~f_witness:Transaction_witness.write_all_proofs_to_disk
+      ~f_proof:(Ledger_proof.Cached.write_proof_to_disk ~proof_cache_db)
+end
