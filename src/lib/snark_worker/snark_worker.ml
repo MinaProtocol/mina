89a388a68b2d54d42aef6eaec6f29f700a69e8af
glyh <lyhokia@gmail.com>
2025-04-21 15:59:53 +0800

    completely replace Snark_worker with a another module

--- src/lib/snark_worker/snark_worker.ml
+++ src/lib/snark_worker/snark_worker.ml
@@ -1,119 +1,23 @@
-module Prod = Prod
-module Intf = Intf
-module Inputs = Prod.Inputs
+(* NOTE: These documentation might be helpful
+   - https://docs.minaprotocol.com/mina-protocol/snark-workers
+*)
 
-module Worker = struct
-  include Functor.Make (Inputs)
+(* module interacting with CLIs *)
+module Entry = Entry
 
-  type Structured_log_events.t +=
-    | Generating_snark_work_failed of { error : Yojson.Safe.t }
-    [@@deriving
-      register_event { msg = "Failed to generate SNARK work: $error" }]
-
-  module Rpcs_versioned = struct
-    open Core_kernel
-    open Signature_lib
-
-    module Work = struct
-      type ledger_proof = Inputs.Ledger_proof.t
-
-      include Work
-    end
-
-    [%%versioned_rpc
-    module Get_work = struct
-      module V2 = struct
-        module T = struct
-          type query = unit
-
-          type response =
-            ( ( Transaction_witness.Stable.V2.t
-              , Inputs.Ledger_proof.Stable.V2.t )
-              Snark_work_lib.Work.Single.Spec.Stable.V2.t
-              Snark_work_lib.Work.Spec.Stable.V1.t
-            * Public_key.Compressed.Stable.V1.t )
-            option
-
-          let query_of_caller_model = Fn.id
-
-          let callee_model_of_query = Fn.id
-
-          let response_of_callee_model :
-              Rpcs.Get_work.Master.Callee.response -> response =
-            Fn.id
-
-          let caller_model_of_response = Fn.id
-        end
-
-        include T
-        include Rpcs.Get_work.Register (T)
-      end
-
-      module Latest = V2
-    end]
-
-    [%%versioned_rpc
-    module Submit_work = struct
-      module V2 = struct
-        module T = struct
-          type query =
-            ( ( Transaction_witness.Stable.V2.t
-              , Ledger_proof.Stable.V2.t )
-              Snark_work_lib.Work.Single.Spec.Stable.V2.t
-              Snark_work_lib.Work.Spec.Stable.V1.t
-            , Ledger_proof.Stable.V2.t )
-            Snark_work_lib.Work.Result.Stable.V1.t
-
-          type response = unit
-
-          let query_of_caller_model = Fn.id
-
-          let callee_model_of_query = Fn.id
-
-          let response_of_callee_model = Fn.id
-
-          let caller_model_of_response = Fn.id
-        end
-
-        include T
-        include Rpcs.Submit_work.Register (T)
-      end
-
-      module Latest = V2
-    end]
-
-    [%%versioned_rpc
-    module Failed_to_generate_snark = struct
-      module V2 = struct
-        module T = struct
-          type query =
-            Bounded_types.Wrapped_error.Stable.V1.t
-            * ( Transaction_witness.Stable.V2.t
-              , Inputs.Ledger_proof.Stable.V2.t )
-              Snark_work_lib.Work.Single.Spec.Stable.V2.t
-              Snark_work_lib.Work.Spec.Stable.V1.t
-            * Public_key.Compressed.Stable.V1.t
-
-          type response = unit
-
-          let query_of_caller_model = Fn.id
-
-          let callee_model_of_query = Fn.id
-
-          let response_of_callee_model = Fn.id
-
-          let caller_model_of_response = Fn.id
-        end
-
-        include T
-        include Rpcs.Failed_to_generate_snark.Register (T)
-      end
-
-      module Latest = V2
-    end]
-  end
-
-  let command = command_from_rpcs (module Rpcs_versioned)
+(* module providing versioned RPCs *)
+module Rpcs = struct
+  module Get_work = Get_work
+  module Submit_work = Submit_work
+  module Failed_to_generate_snark = Failed_to_generate_snark
 end
 
-include Worker
+(* module providing swappable implementation for worker *)
+module Impl = struct
+  module Prod = Prod.Impl
+  module Debug = Debug.Impl
+end
+
+type Structured_log_events.t +=
+  | Generating_snark_work_failed of { error : Yojson.Safe.t }
+  [@@deriving register_event { msg = "Failed to generate SNARK work: $error" }]
