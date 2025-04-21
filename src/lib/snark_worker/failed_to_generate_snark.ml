4419d11827060e26f58343d11479ab8f5371210f
glyh <lyhokia@gmail.com>
2025-04-21 12:43:13 +0800

    Update Failed_to_generate_snark RPC

--- src/lib/snark_worker/failed_to_generate_snark.ml
+++ src/lib/snark_worker/failed_to_generate_snark.ml
@@ -10,7 +10,7 @@ module Rpc = struct
     module T = struct
       type query =
         Bounded_types.Wrapped_error.t
-        * Work.Selector.Spec.t
+        * Work.Partitioned.Spec.t
         * Public_key.Compressed.t
 
       type response = unit
@@ -27,21 +27,22 @@ end
 module Rpc_stable = struct
   [%%versioned_rpc
   module Get_work = struct
-    module V2 = struct
+    module V3 = struct
       module T = struct
         type query =
           Bounded_types.Wrapped_error.Stable.V1.t
-          * Work.Selector.Spec.Stable.V1.t
+          * Work.Partitioned.Spec.Stable.V1.t
           * Public_key.Compressed.Stable.V1.t
 
         type response = unit
 
         let query_of_caller_model : Rpc.Master.Caller.query -> query =
-          Tuple3.map_snd ~f:Work.Selector.Spec.materialize
+          Tuple3.map_snd ~f:Work.Partitioned.Spec.materialize
 
-        let callee_model_of_query =
+        let callee_model_of_query : query -> Rpc.Master.Callee.query =
           Tuple3.map_snd
-            ~f:(Work.Selector.Spec.cache ~proof_cache_db:Proof_cache.cache_db)
+            ~f:
+              (Work.Partitioned.Spec.cache ~proof_cache_db:Proof_cache.cache_db)
 
         let response_of_callee_model = Fn.id
 
@@ -52,6 +53,43 @@ module Rpc_stable = struct
       include Rpc.Register (T)
     end
 
-    module Latest = V2
+    module V2 = struct
+      module T = struct
+        type query =
+          Bounded_types.Wrapped_error.Stable.V1.t
+          * Work.Selector.Spec.Stable.V1.t
+          * Public_key.Compressed.Stable.V1.t
+
+        type response = unit
+
+        let query_of_caller_model (query : Rpc.Master.Caller.query) : query =
+          let err, spec, key = query in
+          let spec =
+            Work.Partitioned.Spec.to_selector_spec spec
+            |> Option.value_exn
+                 ~message:
+                   "FATAL: V2 Worker failed on a `Zkapp_command_segment` job \
+                    where the coordinator can't aggregate, this shouldn't \
+                    happen as the work is issued by the coordinator"
+            |> Work.Selector.Spec.materialize
+          in
+          (err, spec, key)
+
+        let callee_model_of_query : query -> Rpc.Master.Callee.query =
+          Tuple3.map_snd
+            ~f:
+              (Fn.compose Work.Partitioned.Spec.of_selector_spec
+                 (Work.Selector.Spec.cache ~proof_cache_db:Proof_cache.cache_db) )
+
+        let response_of_callee_model = Fn.id
+
+        let caller_model_of_response = Fn.id
+      end
+
+      include T
+      include Rpc.Register (T)
+    end
+
+    module Latest = V3
   end]
 end
