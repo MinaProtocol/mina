d748f836d01f1afc3b7cdfaf805dbb8b75375fe0
Christian Despres <christian@o1labs.org>
2025-04-22 19:39:12 -0400

    Derive bin_io_unversioned for signature kind

--- src/lib/signature_kind/mina_signature_kind.mli
+++ src/lib/signature_kind/mina_signature_kind.mli
@@ -2,6 +2,7 @@ type t = Mina_signature_kind_type.t =
   | Testnet
   | Mainnet
   | Other_network of string
+[@@deriving bin_io]
 
 (** The Mina_signature_kind_type in the compiled config. Deprecated - will be
     replaced by a runtime-derived value. *)
