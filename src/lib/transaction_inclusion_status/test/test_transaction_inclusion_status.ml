ffa24df0d2d12362a03e2abacaa4faa574c79b37
Danny Willems <be.danny.willems@gmail.com>
2025-04-23 15:56:34 +0200

    WIP

--- src/lib/transaction_inclusion_status/test/test_transaction_inclusion_status.ml
+++ src/lib/transaction_inclusion_status/test/test_transaction_inclusion_status.ml
@@ -1,14 +1,14 @@
 open Core_kernel
 open Async
 open Mina_base
-open Pipe_lib
-open Network_pool
-open Transaction_inclusion_status
 open Mina_numbers
+open Network_pool
+open Pipe_lib
+open Transaction_inclusion_status
 
-let max_length = 10
+(* let max_length = 10 *)
 
-let frontier_size = 1
+(* let frontier_size = 1 *)
 
 let logger = Logger.null ()
 
@@ -43,10 +43,6 @@ let pool_max_size = precomputed_values.genesis_constants.txpool_max_size
 let block_window_duration =
   Mina_compile_config.For_unit_tests.t.block_window_duration
 
-let verifier () =
-  Async.Thread_safe.block_on_async_exn (fun () ->
-      Verifier.For_tests.default ~constraint_constants ~logger ~proof_level () )
-
 let key_gen =
   let open Quickcheck.Generator in
   let open Quickcheck.Generator.Let_syntax in
@@ -55,10 +51,6 @@ let key_gen =
   ( Genesis_ledger.largest_account_keypair_exn ()
   , Signature_lib.Keypair.of_private_key_exn (Option.value_exn random_key_opt)
   )
-
-let gen_frontier verifier =
-  Transition_frontier.For_tests.gen ~logger ~precomputed_values ~verifier
-    ~trust_system ~max_length ~size:frontier_size ()
 
 (* TODO: Generate zkApps txns *)
 let gen_user_command =
@@ -94,103 +86,99 @@ let create_pool ~frontier_broadcast_pipe verifier =
   let%map () = Async.Scheduler.yield_until_no_jobs_remain () in
   (transaction_pool, local_sink)
 
-let test_unknown_status_when_no_frontier () =
-  let verifier = verifier () in
-  Quickcheck.test ~trials:1 gen_user_command ~f:(fun user_command ->
+let test_unknown_status_when_no_frontier () : unit Deferred.t =
+  Quickcheck.async_test ~trials:1 gen_user_command ~f:(fun user_command ->
       Backtrace.elide := false ;
-      Async.Thread_safe.block_on_async_exn (fun () ->
-          let frontier_broadcast_pipe, _ = Broadcast_pipe.create None in
-          let%bind transaction_pool, local_diffs_writer =
-            create_pool ~frontier_broadcast_pipe verifier
-          in
-          let%bind () =
-            Transaction_pool.Local_sink.push local_diffs_writer
-              ([ Signed_command user_command ], Fn.const ())
-          in
-          let%map () = Async.Scheduler.yield_until_no_jobs_remain () in
-          let state_testable =
-            Alcotest.testable
-              (fun fmt state -> Format.fprintf fmt "%s" (State.to_string state))
-              State.equal
-          in
-          Alcotest.(check state_testable)
-            "Transaction status should be Unknown when frontier doesn't exist"
-            State.Unknown
-            (get_status ~frontier_broadcast_pipe ~transaction_pool
-               (Signed_command user_command) ) ) )
+      let%bind verifier =
+        Verifier.For_tests.default ~constraint_constants ~logger ~proof_level ()
+      in
+      let frontier_broadcast_pipe, _ = Broadcast_pipe.create None in
+      let%bind transaction_pool, local_diffs_writer =
+        create_pool ~frontier_broadcast_pipe verifier
+      in
+      let%bind () =
+        Transaction_pool.Local_sink.push local_diffs_writer
+          ([ Signed_command user_command ], Fn.const ())
+      in
+      let%map () = Async.Scheduler.yield_until_no_jobs_remain () in
+      let status =
+        get_status ~frontier_broadcast_pipe ~transaction_pool
+          (Signed_command user_command)
+      in
+      assert (State.equal State.Unknown status) )
 
-let test_pending_transaction () =
-  let verifier = verifier () in
-  Quickcheck.test ~trials:1
-    (Quickcheck.Generator.tuple2 (gen_frontier verifier) gen_user_command)
-    ~f:(fun (frontier, user_command) ->
-      Async.Thread_safe.block_on_async_exn (fun () ->
-          let frontier_broadcast_pipe, _ =
-            Broadcast_pipe.create (Some frontier)
-          in
-          let%bind transaction_pool, local_diffs_writer =
-            create_pool ~frontier_broadcast_pipe verifier
-          in
-          let%bind () =
-            Transaction_pool.Local_sink.push local_diffs_writer
-              ([ Signed_command user_command ], Fn.const ())
-          in
-          let%map () = Async.Scheduler.yield_until_no_jobs_remain () in
-          let status =
-            get_status ~frontier_broadcast_pipe ~transaction_pool
-              (Signed_command user_command)
-          in
-          Alcotest.(check bool)
-            "Transaction status should be Pending when in pool but not best \
-             path"
-            true
-            (State.equal State.Pending status) ) )
+(* let test_pending_transaction () = *)
+(*   let verifier = verifier () in *)
+(*   Quickcheck.test ~trials:1 *)
+(*     (Quickcheck.Generator.tuple2 (gen_frontier verifier) gen_user_command) *)
+(*     ~f:(fun (frontier, user_command) -> *)
+(*       Async.Thread_safe.block_on_async_exn (fun () -> *)
+(*           let frontier_broadcast_pipe, _ = *)
+(*             Broadcast_pipe.create (Some frontier) *)
+(*           in *)
+(*           let%bind transaction_pool, local_diffs_writer = *)
+(*             create_pool ~frontier_broadcast_pipe verifier *)
+(*           in *)
+(*           let%bind () = *)
+(*             Transaction_pool.Local_sink.push local_diffs_writer *)
+(*               ([ Signed_command user_command ], Fn.const ()) *)
+(*           in *)
+(*           let%map () = Async.Scheduler.yield_until_no_jobs_remain () in *)
+(*           let status = *)
+(*             get_status ~frontier_broadcast_pipe ~transaction_pool *)
+(*               (Signed_command user_command) *)
+(*           in *)
+(*           Alcotest.(check bool) *)
+(* "Transaction status should be Pending when in pool but not best \ *)
+   (*              path" *)
+(*             true *)
+(*             (State.equal State.Pending status) ) ) *)
 
-let test_unknown_transaction () =
-  let verifier = verifier () in
-  let user_commands_generator =
-    let open Quickcheck.Generator in
-    let open Let_syntax in
-    let%bind head_user_command = gen_user_command in
-    let%map tail_user_commands =
-      Quickcheck.Generator.list_with_length 10 gen_user_command
-    in
-    Mina_stdlib.Nonempty_list.init head_user_command tail_user_commands
-  in
-  Quickcheck.test ~trials:1
-    (Quickcheck.Generator.tuple2 (gen_frontier verifier) user_commands_generator)
-    ~f:(fun (frontier, user_commands) ->
-      Async.Thread_safe.block_on_async_exn (fun () ->
-          let frontier_broadcast_pipe, _ =
-            Broadcast_pipe.create (Some frontier)
-          in
-          let%bind transaction_pool, local_diffs_writer =
-            create_pool ~frontier_broadcast_pipe verifier
-          in
-          let unknown_user_command, pool_user_commands =
-            Mina_stdlib.Nonempty_list.uncons user_commands
-          in
-          let%bind () =
-            Transaction_pool.Local_sink.push local_diffs_writer
-              ( List.map pool_user_commands ~f:(fun x ->
-                    User_command.Signed_command x )
-              , Fn.const () )
-          in
-          let%map () = Async.Scheduler.yield_until_no_jobs_remain () in
-          Alcotest.(check bool)
-            "Transaction status should be Unknown when not in pool or frontier"
-            true
-            (State.equal State.Unknown
-               (get_status ~frontier_broadcast_pipe ~transaction_pool
-                  (Signed_command unknown_user_command) ) ) ) )
+(* let test_unknown_transaction () = *)
+(*   let verifier = verifier () in *)
+(*   let user_commands_generator = *)
+(*     let open Quickcheck.Generator in *)
+(*     let open Let_syntax in *)
+(*     let%bind head_user_command = gen_user_command in *)
+(*     let%map tail_user_commands = *)
+(*       Quickcheck.Generator.list_with_length 10 gen_user_command *)
+(*     in *)
+(*     Mina_stdlib.Nonempty_list.init head_user_command tail_user_commands *)
+(*   in *)
+(*   Quickcheck.test ~trials:1 *)
+(*     (Quickcheck.Generator.tuple2 (gen_frontier verifier) user_commands_generator) *)
+(*     ~f:(fun (frontier, user_commands) -> *)
+(*       Async.Thread_safe.block_on_async_exn (fun () -> *)
+(*           let frontier_broadcast_pipe, _ = *)
+(*             Broadcast_pipe.create (Some frontier) *)
+(*           in *)
+(*           let%bind transaction_pool, local_diffs_writer = *)
+(*             create_pool ~frontier_broadcast_pipe verifier *)
+(*           in *)
+(*           let unknown_user_command, pool_user_commands = *)
+(*             Mina_stdlib.Nonempty_list.uncons user_commands *)
+(*           in *)
+(*           let%bind () = *)
+(*             Transaction_pool.Local_sink.push local_diffs_writer *)
+(*               ( List.map pool_user_commands ~f:(fun x -> *)
+(*                     User_command.Signed_command x ) *)
+(*               , Fn.const () ) *)
+(*           in *)
+(*           let%map () = Async.Scheduler.yield_until_no_jobs_remain () in *)
+(*           Alcotest.(check bool) *)
+(*             "Transaction status should be Unknown when not in pool or frontier" *)
+(*             true *)
+(*             (State.equal State.Unknown *)
+(*                (get_status ~frontier_broadcast_pipe ~transaction_pool *)
+(*                   (Signed_command unknown_user_command) ) ) ) ) *)
 
-let tests =
-  [ ( "transaction_status"
-    , [ Alcotest.test_case "unknown status when no frontier" `Quick
-          test_unknown_status_when_no_frontier
-      ; Alcotest.test_case "pending transaction" `Quick test_pending_transaction
-      ; Alcotest.test_case "unknown transaction" `Quick test_unknown_transaction
-      ] )
-  ]
-
-let () = Alcotest.run "Transaction_inclusion_status" tests
+let () =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let tests =
+        [ ( "transaction_status"
+          , [ Alcotest_async.test_case "unknown status when no frontier" `Quick
+                test_unknown_status_when_no_frontier
+            ] )
+        ]
+      in
+      Alcotest_async.run "Transaction_inclusion_status" tests )
