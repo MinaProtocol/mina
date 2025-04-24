a371ab56435a238a0fa3f72165e757f28421b653
glyh <lyhokia@gmail.com>
2025-04-24 17:57:20 +0800

    (Snark worker) add documents for All versioned RPC

--- src/lib/snark_worker/rpc_failed_to_generate_snark.ml
+++ src/lib/snark_worker/rpc_failed_to_generate_snark.ml
@@ -7,6 +7,11 @@ open Signature_lib
 open Worker_proof_cache
 module Work = Snark_work_lib
 
+(* For versioning of the types here, see:
+   - RFC 0013: https://github.com/MinaProtocol/mina/blob/develop/rfcs/0013-rpc-versioning.md
+   - https://ocaml.org/p/async_rpc_kernel/v0.14.0/doc/Async_rpc_kernel/Versioned_rpc/index.html
+*)
+
 module Master = struct
   let name = "failed_to_generate_snark"
 
--- src/lib/snark_worker/rpc_submit_work.ml
+++ src/lib/snark_worker/rpc_submit_work.ml
@@ -6,6 +6,11 @@ open Core
 open Worker_proof_cache
 module Work = Snark_work_lib
 
+(* For versioning of the types here, see:
+   - RFC 0013: https://github.com/MinaProtocol/mina/blob/develop/rfcs/0013-rpc-versioning.md
+   - https://ocaml.org/p/async_rpc_kernel/v0.14.0/doc/Async_rpc_kernel/Versioned_rpc/index.html
+*)
+
 module Master = struct
   let name = "submit_work"
 
