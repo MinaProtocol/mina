205bfab18f824f3697bf22f408a75994969ee32b
glyh <lyhokia@gmail.com>
2025-04-24 14:44:06 +0800

    (Snark worker): Put Submit_work RPC V2 in Rpc_submit_work

--- src/lib/snark_worker/rpc_submit_work.ml
+++ src/lib/snark_worker/rpc_submit_work.ml
@@ -1,7 +1,9 @@
 (* External Libraries *)
 open Async
+open Core
 
 (* Local Libraries *)
+open Worker_proof_cache
 module Work = Snark_work_lib
 
 module Master = struct
@@ -18,3 +20,27 @@ module Master = struct
 end
 
 include Versioned_rpc.Both_convert.Plain.Make (Master)
+
+[%%versioned_rpc
+module Stable = struct
+  module V2 = struct
+    module T = struct
+      type query = Work.Selector.Result.Stable.V1.t
+
+      type response = unit
+
+      let query_of_caller_model : Master.Caller.query -> query =
+        Work.Selector.Result.materialize
+
+      let callee_model_of_query : query -> Master.Callee.query =
+        Work.Selector.Result.cache ~proof_cache_db
+
+      let response_of_callee_model = Fn.id
+
+      let caller_model_of_response = Fn.id
+    end
+
+    include T
+    include Register (T)
+  end
+end]
