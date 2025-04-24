c1f8adb65c265fa69814612f59ba8352e4f5e589
glyh <lyhokia@gmail.com>
2025-04-24 14:52:07 +0800

    (Snark worker): Put Failed_to_generate_snark RPC V2 in Rpc_Failed_to_generate_snark

--- src/lib/snark_worker/rpc_failed_to_generate_snark.ml
+++ src/lib/snark_worker/rpc_failed_to_generate_snark.ml
@@ -4,6 +4,7 @@ open Core
 
 (* Local Libraries *)
 open Signature_lib
+open Worker_proof_cache
 module Work = Snark_work_lib
 
 module Master = struct
@@ -23,3 +24,32 @@ module Master = struct
 end
 
 include Versioned_rpc.Both_convert.Plain.Make (Master)
+
+[%%versioned_rpc
+module Stable = struct
+  module V2 = struct
+    module T = struct
+      type query =
+        Bounded_types.Wrapped_error.Stable.V1.t
+        * Work.Selector.Spec.Stable.V1.t
+        * Public_key.Compressed.Stable.V1.t
+
+      type response = unit
+
+      let query_of_caller_model ((err, spec, key) : Master.Caller.query) : query
+          =
+        (err, Work.Selector.Spec.materialize spec, key)
+
+      let callee_model_of_query ((err, spec, key) : query) : Master.Callee.query
+          =
+        (err, Work.Selector.Spec.cache ~proof_cache_db spec, key)
+
+      let response_of_callee_model = Fn.id
+
+      let caller_model_of_response = Fn.id
+    end
+
+    include T
+    include Register (T)
+  end
+end]
