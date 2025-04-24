a6af87d1c2f852a3d052872bb96fe3f08dad6d44
glyh <lyhokia@gmail.com>
2025-04-24 13:42:34 +0800

    (Snark_worker) Add an interface Single_worker in Snark_worker.Intf, this is basically just Inputs_intf, except we don't track ledger proofs any more because there's no point doing so.

--- src/lib/snark_worker/intf.ml
+++ src/lib/snark_worker/intf.ml
@@ -3,6 +3,32 @@ open Async
 
 let command_name = "snark-worker"
 
+module Work = Snark_work_lib
+
+(* A worker that would only deal with one single work a time. This exists solely
+   to pay off the tech debt introduced due to work partitioner distributing a
+   One_or_two work.
+*)
+module type Single_worker = sig
+  module Worker_state : sig
+    type t
+
+    val create :
+         constraint_constants:Genesis_constants.Constraint_constants.t
+      -> proof_level:Genesis_constants.Proof_level.t
+      -> unit
+      -> t Deferred.t
+
+    val worker_wait_time : float
+  end
+
+  val perform_single :
+       Worker_state.t
+    -> message:Mina_base.Sok_message.t
+    -> Work.Selector.Single.Spec.t
+    -> (Ledger_proof.t * Time.Span.t) Deferred.Or_error.t
+end
+
 module type Inputs_intf = sig
   open Snark_work_lib
 
