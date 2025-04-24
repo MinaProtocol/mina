ae6aabde2456c7f155b99b449e494ac0c4b5d160
dkijania <dariusz@o1labs.org>
2025-04-24 11:07:40 +0200

    fetch only a base branch

--- buildkite/scripts/refresh_code.sh
+++ buildkite/scripts/refresh_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git branch -D $BUILDKITE_BRANCH 2>/dev/null || true
-git fetch
+git fetch origin "$BUILDKITE_PULL_REQUEST_BASE_BRANCH"
 git checkout -b $BUILDKITE_BRANCH
 git reset --hard $BUILDKITE_COMMIT
