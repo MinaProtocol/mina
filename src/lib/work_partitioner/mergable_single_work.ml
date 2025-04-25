896edba90ef83d51aed01f49a05ce9455b355444
glyh <lyhokia@gmail.com>
2025-04-25 13:59:24 +0800

    (Work Partitioner) Add Mergable_single_work

---
+++ src/lib/work_partitioner/mergable_single_work.ml
@@ -0,0 +1,40 @@
+(* This is half work to be merged into a Two of type _ One_or_two.t *)
+
+open Core_kernel
+module Partitioned_work = Snark_work_lib.Partitioned
+module Selector_work = Snark_work_lib.Selector
+
+type t =
+  { which_half : [ `First | `Second ]
+  ; proof : Ledger_proof.Cached.t
+  ; metric : Core.Time.Span.t * [ `Merge | `Transition ]
+  ; spec : Selector_work.Single.Spec.t
+  ; prover : Signature_lib.Public_key.Compressed.t
+  ; fee : Currency.Fee.t
+  }
+
+let merge_to_one_result_exn (left : t) (right : t) : Selector_work.Result.t =
+  assert (
+    List.for_all ~f:Fn.id
+      [ phys_equal left.which_half `First
+      ; phys_equal right.which_half `Second
+      ; Signature_lib.Public_key.Compressed.equal left.prover right.prover
+      ; Currency.Fee.equal left.fee right.fee
+      ] ) ;
+  let unwrap_metric_as_old (metric_time, metric_ty) =
+    match metric_ty with
+    | `Merge ->
+        (metric_time, `Merge)
+    | `Transition ->
+        (metric_time, `Transition)
+    | _ ->
+        failwith "Trying to merge 2 `Sub_zkapp_command into single work result"
+  in
+  let metrics =
+    `Two (left.metric, right.metric) |> One_or_two.map ~f:unwrap_metric_as_old
+  in
+  { proofs = `Two (left.proof, right.proof)
+  ; metrics
+  ; spec = { instances = `Two (left.spec, right.spec); fee = left.fee }
+  ; prover = left.prover
+  }
