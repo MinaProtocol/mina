42b1d9b56bce67d6b7285a92a986c50b4f0a9fef
glyh <lyhokia@gmail.com>
2025-04-25 14:06:50 +0800

    Work Partitioner: Add Zkapp_command_job_pool, Sent_job_pool

--- src/lib/work_partitioner/work_partitioner.ml
+++ src/lib/work_partitioner/work_partitioner.ml
@@ -2,3 +2,9 @@ module Snark_worker_shared = Snark_worker_shared
 module Work = Snark_work_lib
 module Zkapp_command_job_with_status =
   With_job_status.Make (Work.Partitioned.Zkapp_command_job)
+module Zkapp_command_job_pool =
+  Job_pool.Make (Work.Partitioned.Pairing) (Pending_zkapp_command)
+module Sent_job_pool =
+  Job_pool.Make
+    (Work.Partitioned.Zkapp_command_job.ID)
+    (Zkapp_command_job_with_status)
