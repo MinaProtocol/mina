1d2c8c3d3f9dc6af6b75b9459b673bd791a00181
glyh <lyhokia@gmail.com>
2025-04-27 10:56:41 +0800

    Snark Worker: Update Submit_work RPC master

--- src/lib/snark_worker/rpc_submit_work.ml
+++ src/lib/snark_worker/rpc_submit_work.ml
@@ -15,9 +15,12 @@ module Master = struct
   let name = "submit_work"
 
   module T = struct
-    type query = Work.Selector.Result.t
+    type query = Work.Partitioned.Result.t
 
-    type response = unit
+    (* NOTE: this `Slashed case is suppoed to track all duplicated case,
+       but since we didn't touch on Work_selector, this is not true.
+       We should get back to Work_partitioner implementation to fix this. *)
+    type response = [ `Ok | `Slashed | `SchemeUnmatched ]
   end
 
   module Caller = T
@@ -34,15 +37,39 @@ module Stable = struct
 
       type response = unit
 
-      let query_of_caller_model : Master.Caller.query -> query =
-        Work.Selector.Result.read_all_proofs_from_disk
+      let query_of_caller_model (query : Master.Caller.query) : query =
+        Work.Partitioned.Result.to_selector_result query
+        |> Option.value_exn
+             ~message:
+               "FATAL: V2 Worker completed a `Sub_zkapp_command job where the \
+                coordinator can't aggregate, this shouldn't happen as the work \
+                is issued by the coordinator"
+        |> Work.Selector.Result.read_all_proofs_from_disk
 
-      let callee_model_of_query : query -> Master.Callee.query =
-        Work.Selector.Result.write_all_proofs_to_disk ~proof_cache_db
+      let callee_model_of_query (query : query) : Master.Callee.query =
+        Work.Selector.Result.write_all_proofs_to_disk ~proof_cache_db query
+        |> Work.Partitioned.Result.of_selector_result |> Or_error.ok
+        |> Option.value_exn
+             ~message:
+               "Selector result has invalid shape so can't convert to \
+                Partitioned result"
 
-      let response_of_callee_model = Fn.id
+      let response_of_callee_model = function
+        | `Ok ->
+            ()
+        | `Slashed ->
+            printf
+              "Trying to notify worker that the work they submitted is \
+               slashed(completed by others, or timeouted), but they're too old \
+               to receive this message."
+        | `SchemeUnmatched ->
+            printf
+              "Trying to notify worker that the work they submitted is in a \
+               wrong shape, but they're too old to receive this message."
 
-      let caller_model_of_response = Fn.id
+      (* There's no way we can tell if the proof we submitted is duplicated/timeouted,
+         just assume everything is fine on worker's side *)
+      let caller_model_of_response = const `Ok
     end
 
     include T
