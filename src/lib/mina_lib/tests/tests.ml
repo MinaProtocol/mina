a9265d4a2f8dd6adad3cb2e8fb526f76854ed756
glyh <lyhokia@gmail.com>
2025-04-28 11:55:51 +0800

    Mina Lib: wire transaction pool proxy

--- src/lib/mina_lib/tests/tests.ml
+++ src/lib/mina_lib/tests/tests.ml
@@ -359,7 +359,7 @@ let%test_module "Epoch ledger sync tests" =
           ~get_completed_work:(Fn.const None) ~catchup_mode:`Super
           ~network_transition_reader:block_reader ~producer_transition_reader
           ~get_most_recent_valid_block ~most_recent_valid_block_writer
-          ~notify_online ()
+          ~notify_online ?transaction_pool_proxy:None ()
       in
       let%bind () = Ivar.read initialization_finish_signal in
       let tr_tm1 = Unix.gettimeofday () in
