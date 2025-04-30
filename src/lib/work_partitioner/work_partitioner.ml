d976debfe1df7377609c3beb90bd98894a45abbd
glyh <lyhokia@gmail.com>
2025-04-30 16:54:59 +0800

    Work Partitioner: Add Work_partitioner.Work_partitioner.{issue_from_tmp_slot, convert_single_work_from_selector, issue_job_from_partitioner}

--- src/lib/work_partitioner/work_partitioner.ml
+++ src/lib/work_partitioner/work_partitioner.ml
@@ -53,6 +53,8 @@ let create ~(reassignment_timeout : Time.Span.t) ~(logger : Logger.t) : t =
   ; tmp_slot = None
   }
 
+let epoch_now () = Time.(now () |> to_span_since_epoch)
+
 (* Logics for work requesting *)
 let reissue_old_task ~(partitioner : t) () : Work.Partitioned.Spec.t option =
   let job_is_old (job : Work.Partitioned.Zkapp_command_job.t) : bool =
@@ -66,12 +68,10 @@ let reissue_old_task ~(partitioner : t) () : Work.Partitioned.Spec.t option =
   with
   | None ->
       None
-  | Some (id, job_with_status) ->
-      let issued_since_unix_epoch = Time.(now () |> to_span_since_epoch) in
+  | Some (id, job) ->
+      let issued_since_unix_epoch = epoch_now () in
       let reissued =
-        { job_with_status with
-          common = { job_with_status.common with issued_since_unix_epoch }
-        }
+        { job with common = { job.common with issued_since_unix_epoch } }
       in
       Sent_job_pool.replace ~id ~job:reissued
         partitioner.jobs_sent_by_partitioner ;
@@ -91,7 +91,7 @@ let issue_from_zkapp_command_work_pool ~(partitioner : t) () :
       (Id_generator.next_id partitioner.id_generator)
   in
   let fee_of_full = pending_zkapp_command.fee_of_full in
-  let issued_since_unix_epoch = Time.(now () |> to_span_since_epoch) in
+  let issued_since_unix_epoch = epoch_now () in
   let spec =
     Work.Partitioned.Zkapp_command_job.Poly.
       { spec
@@ -104,3 +104,95 @@ let issue_from_zkapp_command_work_pool ~(partitioner : t) () :
     partitioner.jobs_sent_by_partitioner ;
 
   Work.Partitioned.Spec.Poly.Sub_zkapp_command { spec; metric = () }
+
+let rec issue_from_tmp_slot ~(partitioner : t) () =
+  match partitioner.tmp_slot with
+  | Some spec ->
+      partitioner.tmp_slot <- None ;
+      Some (convert_single_work_from_selector ~partitioner ~spec)
+  | None ->
+      None
+
+(* try to issue a single work received from the underlying Work_selector
+   `one_or_two` tracks which task is it inside a `One_or_two`*)
+and convert_single_work_from_selector ~(partitioner : t)
+    ~spec:(single_spec, pairing, fee_of_full) : Work.Partitioned.Spec.t =
+  match single_spec with
+  | Transition (input, witness) as work -> (
+      (* WARN: a smilar copy of this exists in `Snark_worker.Worker_impl_prod` *)
+      match witness.transaction with
+      | Command (Zkapp_command zkapp_command) -> (
+          match
+            Async.Thread_safe.block_on_async (fun () ->
+                let witness =
+                  Transaction_witness.read_all_proofs_from_disk witness
+                in
+                Snark_worker_shared.extract_zkapp_segment_works
+                  ~m:partitioner.transaction_snark ~input ~witness
+                  ~zkapp_command )
+          with
+          | Ok (Ok (_ :: _ as all)) ->
+              let unscheduled_segments =
+                all
+                |> List.map ~f:(fun (witness, spec, statement) ->
+                       Work.Partitioned.Zkapp_command_job.Spec.Poly.Segment
+                         { statement; witness; spec } )
+                |> Queue.of_list
+              in
+              let pending_mergable_proofs = Deque.create () in
+              let merge_remaining = Queue.length unscheduled_segments - 1 in
+              let pending_zkapp_command =
+                Pending_zkapp_command.
+                  { unscheduled_segments
+                  ; pending_mergable_proofs
+                  ; merge_remaining
+                  ; spec = work
+                  ; elapsed = Time.Span.zero
+                  ; fee_of_full
+                  }
+              in
+              let pairing =
+                Work.Partitioned.Pairing.Sub_zkapp.of_single
+                  (fun () ->
+                    Pairing_ID (Id_generator.next_id partitioner.id_generator)
+                    )
+                  pairing
+              in
+              assert (
+                phys_equal `Ok
+                  (Zkapp_command_job_pool.attempt_add ~key:pairing
+                     ~job:pending_zkapp_command partitioner.zkapp_command_jobs ) ) ;
+              issue_job_from_partitioner ~partitioner ()
+              |> Option.value_exn
+                   ~message:
+                     "FATAL: we already inserted work into partitioner so this \
+                      shouldn't happen"
+          | Ok (Ok []) ->
+              failwith "No witness generated"
+          | Ok (Error e) ->
+              failwith (Error.to_string_hum e)
+          | Error e ->
+              failwith (Exn.to_string e) )
+      | Command (Signed_command _) | Fee_transfer _ | Coinbase _ ->
+          Single
+            { single_spec
+            ; pairing
+            ; metric = ()
+            ; common = { fee_of_full; issued_since_unix_epoch = epoch_now () }
+            } )
+  | Merge _ ->
+      Single
+        { single_spec
+        ; pairing
+        ; metric = ()
+        ; common = { fee_of_full; issued_since_unix_epoch = epoch_now () }
+        }
+
+and issue_job_from_partitioner ~(partitioner : t) () :
+    Work.Partitioned.Spec.t option =
+  List.find_map
+    ~f:(fun f -> f ())
+    [ reissue_old_task ~partitioner
+    ; issue_from_tmp_slot ~partitioner
+    ; issue_from_zkapp_command_work_pool ~partitioner
+    ]
