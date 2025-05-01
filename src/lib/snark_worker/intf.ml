4a6f607f82572e0bf67ca8efaa6f3b9e0ce0a335
glyh <lyhokia@gmail.com>
2025-05-01 14:08:06 +0800

    Snark Worker: Rewrite Worker interface's document

--- src/lib/snark_worker/intf.ml
+++ src/lib/snark_worker/intf.ml
@@ -15,10 +15,21 @@ module type Worker = sig
     val worker_wait_time : float
   end
 
-  (* NOTE:
+  (**
+     [perform ~state ~spec ~sok_digest] returns a [One_or_two.t] of triple
+     ([proof], [time_elapsed], [tag]) following the specification [spec] with
+     digest [sok_digest]. The proof is created by a worker with state [state].
+
+     The [tag] is used to indicate what kind of specification is provided, and
+     is used when logging metrics. ['a One_or_two.t] is leaky abstraction due to
+     the fact that a Work Selector issue 1 or 2 specs, and that has been encoded
+     in old RPCs in this library. And there's no way to get rid of them without
+     sacrificing compatibility.
+
      We're using Stable.Latest type as we would be dealing with the internal of
-     the specs/proofs, and stuff will have to go across the RPC boundary. Unless
-     we have architectural refactor, there's no point using cached type here.
+     the specs/proofs, and input/output of all 3 RPCs will have to go across the
+     RPC boundary. Unless we have architectural refactor, there's no point using
+     cached type here.
   *)
   val perform :
        state:Worker_state.t
