4d11b63e8a9be8dc79b607ef03ce8e6767e281fb
glyh <lyhokia@gmail.com>
2025-05-03 17:34:52 +0800

    Snark Worker: update Get_work master RPC

--- src/lib/snark_worker/rpc_get_work.ml
+++ src/lib/snark_worker/rpc_get_work.ml
@@ -12,10 +12,10 @@ module Master = struct
   let name = "get_work"
 
   module T = struct
-    type query = unit
+    type query = [ `V2 | `V3 ]
 
     type response =
-      (Work.Selector.Spec.Stable.Latest.t * Public_key.Compressed.t) option
+      (Work.Partitioned.Spec.Stable.Latest.t * Public_key.Compressed.t) option
   end
 
   module Caller = T
@@ -34,13 +34,42 @@ module Stable = struct
         (Work.Selector.Spec.Stable.V1.t * Public_key.Compressed.Stable.V1.t)
         option
 
-      let query_of_caller_model = Fn.id
+      let query_of_caller_model = const ()
 
-      let callee_model_of_query = Fn.id
+      let callee_model_of_query = const `V2
 
-      let response_of_callee_model = Fn.id
+      let response_of_callee_model (resp : Master.Callee.response) : response =
+        match resp with
+        | Some (spec, key) -> (
+            match Work.Partitioned.Spec.Poly.to_selector_spec spec with
+            | None ->
+                (* WARN: we'd better report to the coordinator we failed rather *)
+                (*          than ignoring the work*)
+                printf
+                  "WARN: V2 Worker receving work `Zkapp_command_segment`, \
+                   which is out of its capability, work dropped" ;
+                None
+            | Some spec ->
+                Some (spec, key) )
+        | None ->
+            None
 
-      let caller_model_of_response = Fn.id
+      let caller_model_of_response : response -> Master.Caller.response =
+        function
+        | Some (spec, key) ->
+            (* Old worker can't tell when it received the job,
+               so just assume it's taking 0s
+            *)
+            let issued_since_unix_epoch =
+              Time.(now () |> to_span_since_epoch)
+            in
+            let spec =
+              Work.Partitioned.Spec.Poly.of_selector_spec
+                ~issued_since_unix_epoch spec
+            in
+            Some (spec, key)
+        | None ->
+            None
     end
 
     include T
