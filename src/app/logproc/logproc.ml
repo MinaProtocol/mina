76e22d90bc5a4ad8cd5c871987a9fb76147909d1
Danny Willems <be.danny.willems@gmail.com>
2025-05-04 10:11:29 +0200

    logproc: fix warning regarding deprecated methods

--- src/app/logproc/logproc.ml
+++ src/app/logproc/logproc.ml
@@ -291,6 +291,6 @@ let () =
           , {|logproc -f '.message match /broadcast/ && .metadata.peer.host == "182.9.63.3" || .metadata.peer.discover_port == 8302|}
           ) ]
     in
-    Term.info ~version:"0.1" ~doc ~exits:Term.default_exits ~man "logproc"
+    Cmd.info ~version:"0.1" ~doc ~exits:Cmd.Exit.defaults ~man "logproc"
   in
-  Term.(exit @@ eval (main_term, main_info))
+  exit (Cmd.eval (Cmd.v main_info main_term))
