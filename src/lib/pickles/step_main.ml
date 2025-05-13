2d5afcc7e03b6594906b79a436631c4fe6faaa36
marcbeunardeau88 <marc@o1labs.org>
2025-05-13 18:19:12 +0200

    remove one_hot dead code

--- src/lib/pickles/step_main.ml
+++ src/lib/pickles/step_main.ml
@@ -3,19 +3,6 @@ open Hlist
 open Import
 open Impls.Step
 
-(* Converts from the one hot vector representation of a number
-   0 <= i < n
-
-     0  1  ... i-1  i  i+1       n-1
-   [ 0; 0; ... 0;   1; 0;   ...; 0 ]
-
-   to the numeric representation i. *)
-
-let _one_hot_vector_to_num (type n) (v : n Per_proof_witness.One_hot_vector.t) :
-    Field.t =
-  let n = Vector.length (v :> (Boolean.var, n) Vector.t) in
-  let open Step_verifier in
-  Pseudo.choose (v, Vector.init n ~f:Field.of_int) ~f:Fn.id
 
 module Make (Inductive_rule : Inductive_rule.Intf) = struct
   module B = Inductive_rule.B
