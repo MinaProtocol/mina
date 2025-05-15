730b661d273719d3fe88704baaa3a308513fd811
georgeee <george.agapov@gmail.com>
2025-05-15 11:00:02 +0000

    Use nixos-24.11-small

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
 
   inputs.utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11-small";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11-small";
   inputs.nixpkgs-old.url = "github:nixos/nixpkgs/nixos-23.05-small";
 
   inputs.mix-to-nix.url = "github:serokell/mix-to-nix";
@@ -413,6 +413,6 @@
 
         inherit checks;
 
-        formatter = pkgs.nixfmt;
+        formatter = pkgs.nixfmt-classic;
       });
 }
