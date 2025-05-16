d256b17d2323b25186d0364726c5f300763ebab1
glyh <lyhokia@gmail.com>
2025-05-16 18:55:35 +0800

    Snark Work Lib: track single spec(Poly/Selector) in a self contained file

--- src/lib/snark_work_lib/selector.ml
+++ src/lib/snark_work_lib/selector.ml
@@ -12,36 +12,7 @@
 open Core_kernel
 
 module Single = struct
-  module Spec = struct
-    [%%versioned
-    module Stable = struct
-      [@@@no_toplevel_latest_type]
-
-      module V1 = struct
-        type t =
-          ( Transaction_witness.Stable.V2.t
-          , Ledger_proof.Stable.V2.t )
-          Work.Single.Spec.Stable.V2.t
-        [@@deriving sexp, yojson]
-
-        let to_latest = Fn.id
-      end
-    end]
-
-    type t = (Transaction_witness.t, Ledger_proof.Cached.t) Work.Single.Spec.t
-
-    let read_all_proofs_from_disk : t -> Stable.Latest.t =
-      Work.Single.Spec.map
-        ~f_witness:Transaction_witness.read_all_proofs_from_disk
-        ~f_proof:Ledger_proof.Cached.read_proof_from_disk
-
-    let write_all_proofs_to_disk ~(proof_cache_db : Proof_cache_tag.cache_db) :
-        Stable.Latest.t -> t =
-      Work.Single.Spec.map
-        ~f_witness:
-          (Transaction_witness.write_all_proofs_to_disk ~proof_cache_db)
-        ~f_proof:(Ledger_proof.Cached.write_proof_to_disk ~proof_cache_db)
-  end
+  module Spec = Single_spec
 end
 
 module Spec = struct
