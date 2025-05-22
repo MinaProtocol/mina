7731608068305e216bdca21cc46e922b0831a120
glyh <lyhokia@gmail.com>
2025-05-22 09:41:09 +0800

    Snark Work Lib: define partitioned spec

---
+++ src/lib/snark_work_lib/partitioned_spec.ml
@@ -0,0 +1,117 @@
+open Core_kernel
+
+module Poly = struct
+  [%%versioned
+  module Stable = struct
+    module V1 = struct
+      type ('witness, 'ledger_proof, 'sub_zkapp_spec, 'data) t =
+        | Single of
+            { job :
+                ( ('witness, 'ledger_proof) Single_spec.Poly.Stable.V2.t
+                , Id.Single.Stable.V1.t )
+                With_job_meta.Stable.V1.t
+            ; data : 'data
+            }
+        | Sub_zkapp_command of
+            { job :
+                ( 'sub_zkapp_spec
+                , Id.Sub_zkapp.Stable.V1.t )
+                With_job_meta.Stable.V1.t
+            ; data : 'data
+            }
+      [@@deriving sexp, yojson]
+    end
+  end]
+
+  let drop_data : _ t -> _ t = function
+    | Single { job; _ } ->
+        Single { job; data = () }
+    | Sub_zkapp_command { job; _ } ->
+        Sub_zkapp_command { job; data = () }
+
+  let map ~f_witness ~f_subzkapp_spec ~f_proof ~f_data = function
+    | Single { job; data } ->
+        Single
+          { job =
+              With_job_meta.map
+                ~f_spec:(fun spec ->
+                  Single_spec.Poly.map ~f_witness ~f_proof spec )
+                job
+          ; data = f_data data
+          }
+    | Sub_zkapp_command { job; data } ->
+        Sub_zkapp_command
+          { job = With_job_meta.map ~f_spec:f_subzkapp_spec job
+          ; data = f_data data
+          }
+
+  let sok_message : _ t -> Mina_base.Sok_message.t = function
+    | Single { job; _ } ->
+        job.sok_message
+    | Sub_zkapp_command { job; _ } ->
+        job.sok_message
+end
+
+[%%versioned
+module Stable = struct
+  [@@@no_toplevel_latest_type]
+
+  module V1 = struct
+    type t =
+      ( Transaction_witness.Stable.V2.t
+      , Ledger_proof.Stable.V2.t
+      , Sub_zkapp_spec.Stable.V1.t
+      , unit )
+      Poly.Stable.V1.t
+    [@@deriving sexp, yojson]
+
+    let to_latest = Fn.id
+
+    let statement : t -> Transaction_snark.Statement.t = function
+      | Single { job = { spec; _ }; _ } ->
+          Single_spec.Poly.statement spec
+      | Sub_zkapp_command { job = { spec; _ }; _ } ->
+          Sub_zkapp_spec.Stable.Latest.statement spec
+
+    let map_with_statement (t : t) ~f : _ Poly.t =
+      match t with
+      | Single { job = { spec; _ } as job; data } ->
+          let stmt = Single_spec.Poly.statement spec in
+          Single { job; data = f stmt data }
+      | Sub_zkapp_command { job = { spec; _ } as job; data } ->
+          Sub_zkapp_command
+            { job; data = f (Sub_zkapp_spec.Stable.Latest.statement spec) data }
+  end
+end]
+
+type t =
+  (Transaction_witness.t, Ledger_proof.Cached.t, Sub_zkapp_spec.t, unit) Poly.t
+
+let read_all_proofs_from_disk : t -> Stable.Latest.t = function
+  | Single { job; data } ->
+      let job =
+        With_job_meta.map ~f_spec:Single_spec.read_all_proofs_from_disk job
+      in
+      Single { job; data }
+  | Sub_zkapp_command { job; data } ->
+      let job =
+        With_job_meta.map ~f_spec:Sub_zkapp_spec.read_all_proofs_from_disk job
+      in
+      Sub_zkapp_command { job; data }
+
+let write_all_proofs_to_disk ~(proof_cache_db : Proof_cache_tag.cache_db) :
+    Stable.Latest.t -> t = function
+  | Single { job; data } ->
+      let job =
+        With_job_meta.map
+          ~f_spec:(Single_spec.write_all_proofs_to_disk ~proof_cache_db)
+          job
+      in
+      Single { job; data }
+  | Sub_zkapp_command { job; data } ->
+      let job =
+        With_job_meta.map
+          ~f_spec:(Sub_zkapp_spec.write_all_proofs_to_disk ~proof_cache_db)
+          job
+      in
+      Sub_zkapp_command { job; data }
---
+++ src/lib/snark_work_lib/partitioned_spec.mli
@@ -0,0 +1,73 @@
+open Core_kernel
+
+module Poly : sig
+  [%%versioned:
+  module Stable : sig
+    module V1 : sig
+      type ('witness, 'ledger_proof, 'sub_zkapp_spec, 'data) t =
+        | Single of
+            { job :
+                ( ('witness, 'ledger_proof) Single_spec.Poly.Stable.V2.t
+                , Id.Single.Stable.V1.t )
+                With_job_meta.Stable.V1.t
+            ; data : 'data
+            }
+        | Sub_zkapp_command of
+            { job :
+                ( 'sub_zkapp_spec
+                , Id.Sub_zkapp.Stable.V1.t )
+                With_job_meta.Stable.V1.t
+            ; data : 'data
+            }
+      [@@deriving sexp, yojson]
+    end
+  end]
+
+  val drop_data : ('a, 'b, 'c, 'd) t -> ('a, 'b, 'c, unit) t
+
+  val map :
+       f_witness:('a -> 'b)
+    -> f_subzkapp_spec:('c -> 'd)
+    -> f_proof:('e -> 'f)
+    -> f_data:('g -> 'h)
+    -> ('a, 'e, 'c, 'g) t
+    -> ('b, 'f, 'd, 'h) t
+
+  val sok_message : ('a, 'b, 'c, 'd) t -> Mina_base.Sok_message.t
+end
+
+[%%versioned:
+module Stable : sig
+  [@@@no_toplevel_latest_type]
+
+  module V1 : sig
+    type t =
+      ( Transaction_witness.Stable.V2.t
+      , Ledger_proof.Stable.V2.t
+      , Sub_zkapp_spec.Stable.V1.t
+      , unit )
+      Poly.Stable.V1.t
+    [@@deriving sexp, yojson]
+
+    val to_latest : t -> t
+
+    val statement : t -> Transaction_snark.Statement.t
+
+    val map_with_statement :
+         t
+      -> f:(Transaction_snark.Statement.Stable.Latest.t -> unit -> 'a)
+      -> ( Transaction_witness.Stable.V2.t
+         , Mina_wire_types.Transaction_snark.V2.t
+         , Sub_zkapp_spec.Stable.V1.t
+         , 'a )
+         Poly.t
+  end
+end]
+
+type t =
+  (Transaction_witness.t, Ledger_proof.Cached.t, Sub_zkapp_spec.t, unit) Poly.t
+
+val read_all_proofs_from_disk : t -> Stable.Latest.t
+
+val write_all_proofs_to_disk :
+  proof_cache_db:Proof_cache_tag.cache_db -> Stable.Latest.t -> t
