bb59b75b00dc28539f53d501c6735fcc06a21268
georgeee <george.agapov@gmail.com>
2025-05-27 05:41:51 +0000

    Rename replacable -> swappable in test

--- src/lib/concurrency/pipe_lib/test/test_pipe_lib.ml
+++ src/lib/concurrency/pipe_lib/test/test_pipe_lib.ml
@@ -3,23 +3,23 @@ open Core_kernel
 open Pipe_lib
 
 let test_reader_hang () =
-  let replacable =
-    Strict_pipe.Swappable.create ~name:"replacable"
+  let swappable =
+    Strict_pipe.Swappable.create ~name:"swappable"
       (Buffered (`Capacity 50, `Overflow (Drop_head (const ()))))
   in
   let%bind _hanging_reader =
-    Strict_pipe.Swappable.swap_reader ~reader_name:"hanging reader" replacable
+    Strict_pipe.Swappable.swap_reader ~reader_name:"hanging reader" swappable
   in
-  let () = Strict_pipe.Swappable.write replacable () in
+  let () = Strict_pipe.Swappable.write swappable () in
   let%bind good_reader =
-    Strict_pipe.Swappable.swap_reader ~reader_name:"good reader" replacable
+    Strict_pipe.Swappable.swap_reader ~reader_name:"good reader" swappable
   in
   let timeouted = ref true in
   after (Time.Span.of_sec 1.5)
   >>| (fun () ->
         if !timeouted then failwith "Swappable strict pipe hangs, timeout!" )
   |> Deferred.don't_wait_for ;
-  let () = Strict_pipe.Swappable.write replacable () in
+  let () = Strict_pipe.Swappable.write swappable () in
   match%map Strict_pipe.Reader.read good_reader with
   | `Ok () ->
       timeouted := false
