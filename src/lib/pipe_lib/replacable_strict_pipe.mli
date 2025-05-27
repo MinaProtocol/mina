e50ecef92b8fe482e4ea51f62209ec1a43034cc2
glyh <lyhokia@gmail.com>
2025-05-27 10:29:40 +0800

    Pipe Lib: add doc comments for replacable strict pipe

--- src/lib/pipe_lib/replacable_strict_pipe.mli
+++ src/lib/pipe_lib/replacable_strict_pipe.mli
@@ -1,19 +1,31 @@
 open Async_kernel
 
+(** the type for a replacable strict pipe *)
 type ('data_in_pipe, 'pipe_kind, 'write_return) t
 
+(** [create ?warn_on_drop ~name type] creates a replacable strict pipe
+    designated ~name. Warning on drop behavior depends on ?warn_on_drop, and it
+    has underlying pipe of type `type`. *)
 val create :
      ?warn_on_drop:bool
   -> name:string
   -> ('data_in_pipe, 'pipe_kind, 'write_return) Strict_pipe.type_
   -> ('data_in_pipe, 'pipe_kind, 'write_return) t
 
+(** [write t data] writes to a replacable strict pipe, it would return value
+    designated by the strict pipe type. All ownership is managed inside this
+    data structure so no direct access to long live writer is provided. *)
 val write :
   ('data_in_pipe, 'pipe_kind, 'write_return) t -> 'data_in_pipe -> 'write_return
 
+(** [request_reader ~reader_name t] requests replacable to prepare a new pipe,
+    replacing the old pipe, and returns the reader. All ownership is managed
+    inside this data structure so no writer is provided. *)
 val request_reader :
      reader_name:string
   -> ('data_in_pipe, 'pipe_kind, 'write_return) t
   -> 'data_in_pipe Strict_pipe.Reader.t Deferred.t
 
+(** [kill t] kills a replacable strict pipe, it will close all related writer in
+    itself. *)
 val kill : ('data_in_pipe, 'pipe_kind, 'write_return) t -> unit
