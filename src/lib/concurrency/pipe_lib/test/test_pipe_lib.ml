3878082eef56b87dd21fc6fa5a054a71625a38ec
glyh <lyhokia@gmail.com>
2025-05-27 11:00:41 +0800

    Pipe Lib: add test for swappable pipe where reader hangs

---
+++ src/lib/concurrency/pipe_lib/test/test_pipe_lib.ml
@@ -0,0 +1,36 @@
+open Async
+open Core_kernel
+open Pipe_lib
+
+let test_reader_hang () =
+  let replacable =
+    Strict_pipe.Swappable.create ~name:"replacable"
+      (Buffered (`Capacity 50, `Overflow (Drop_head (const ()))))
+  in
+  let%bind _hanging_reader =
+    Strict_pipe.Swappable.swap_reader ~reader_name:"hanging reader" replacable
+  in
+  let () = Strict_pipe.Swappable.write replacable () in
+  let%bind good_reader =
+    Strict_pipe.Swappable.swap_reader ~reader_name:"good reader" replacable
+  in
+  let timeouted = ref true in
+  after (Time.Span.of_sec 1.5)
+  >>| (fun () ->
+        if !timeouted then failwith "Swappable strict pipe hangs, timeout!" )
+  |> Deferred.don't_wait_for ;
+  let () = Strict_pipe.Swappable.write replacable () in
+  match%map Strict_pipe.Reader.read good_reader with
+  | `Ok () ->
+      timeouted := false
+  | `Eof ->
+      failwith "written () to pipe got `Eof"
+
+let () =
+  Async.Thread_safe.block_on_async_exn (fun () ->
+      let open Alcotest_async in
+      run "Pipe_lib"
+        [ ( "Strict_pipe.Swappable"
+          , [ Alcotest_async.test_case "reader hangs" `Quick test_reader_hang ]
+          )
+        ] )
