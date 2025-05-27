2fffa6fb9ac07566980b778cb5fb047c78d3c8dc
glyh <lyhokia@gmail.com>
2025-05-27 15:38:47 +0800

    simplify

--- src/lib/work_partitioner/pending_combined_result.ml
+++ src/lib/work_partitioner/pending_combined_result.ml
@@ -8,10 +8,16 @@ type half = [ `First | `Second ] [@@deriving equal]
 type submitted_half = [ `First | `Second | `One ] [@@deriving equal]
 
 type t =
-  { spec : Work.Spec.Single.t One_or_two.t
-  ; single_result : (Work.Result.Single.t * half) option
-  ; sok_message : Mina_base.Sok_message.t
-  }
+  | SpecOnly of
+      { spec : Work.Spec.Single.t One_or_two.t
+      ; sok_message : Mina_base.Sok_message.t
+      }
+  | One_of_two of
+      { other_spec : Work.Spec.Single.t
+      ; sok_message : Mina_base.Sok_message.t
+      ; in_pool_half : half
+      ; in_pool_result : Work.Result.Single.t
+      }
 
 type merge_outcome =
   | Pending of t
@@ -22,66 +28,67 @@ type merge_outcome =
       ; spec : Work.Spec.Single.t One_or_two.t
       }
 
-let merge_single_result (in_pool : t)
+let merge_single_result (current : t)
     ~(submitted_result : (unit, Ledger_proof.Cached.t) Work.Result.Single.Poly.t)
     ~(submitted_half : submitted_half) : merge_outcome =
-  let { spec; single_result; sok_message = { prover; fee } } = in_pool in
-  match single_result with
-  | None -> (
-      match (spec, submitted_half) with
-      | `One spec, `One ->
-          let submitted_result =
-            Work.Result.Single.Poly.map ~f_spec:Fn.id
-              ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
-          in
-          let Work.Result.Single.Poly.{ proof; _ } = submitted_result in
-          let statements = `One (Work.Spec.Single.Poly.statement spec) in
-          Done
-            ( statements
-            , Mina_wire_types.Network_pool_priced_proof.V1.
-                { proof = `One proof; fee = { fee; prover } } )
-      | `Two (spec, _), (`First as submitted_half)
-      | `Two (_, spec), (`Second as submitted_half) ->
-          Pending
-            { in_pool with
-              single_result =
-                Some ({ submitted_result with spec }, submitted_half)
-            }
-      | (`One _ as spec), ((`First | `Second) as submitted_half) ->
-          NoSuchHalf { submitted_half; spec }
-      | (`Two _ as spec), (`One as submitted_half) ->
-          NoSuchHalf { submitted_half; spec } )
-  | Some (in_pool_result, in_pool_half) -> (
-      match (spec, submitted_half) with
-      | _, `One ->
-          HalfMismatch { submitted_half; in_pool = in_pool_half }
-      | (`One _ as spec), ((`First | `Second) as submitted_half) ->
-          NoSuchHalf { submitted_half; spec }
-      | `Two (_, spec), (`Second as submitted_half)
-      | `Two (spec, _), (`First as submitted_half) ->
-          assert (not (equal_half in_pool_half submitted_half)) ;
-          let submitted_result =
-            Work.Result.Single.Poly.map ~f_spec:(const spec)
-              ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
-          in
-          let in_pool_result =
-            Work.Result.Single.Poly.map ~f_spec:Fn.id
-              ~f_proof:Ledger_proof.Cached.read_proof_from_disk in_pool_result
-          in
-          let results =
-            match submitted_half with
-            | `First ->
-                `Two (submitted_result, in_pool_result)
-            | `Second ->
-                `Two (in_pool_result, submitted_result)
-          in
-          let statements =
-            One_or_two.map
-              ~f:(fun result -> Work.Spec.Single.Poly.statement result.spec)
-              results
-          in
-          let proof = One_or_two.map ~f:(fun result -> result.proof) results in
-          Done
-            ( statements
-            , Mina_wire_types.Network_pool_priced_proof.V1.
-                { proof; fee = { fee; prover } } ) )
+  match (current, submitted_half) with
+  | SpecOnly { spec = `One spec; sok_message = { fee; prover } }, `One ->
+      let submitted_result =
+        Work.Result.Single.Poly.map ~f_spec:Fn.id
+          ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
+      in
+      let Work.Result.Single.Poly.{ proof; _ } = submitted_result in
+      let statements = `One (Work.Spec.Single.Poly.statement spec) in
+      Done
+        ( statements
+        , Mina_wire_types.Network_pool_priced_proof.V1.
+            { proof = `One proof; fee = { fee; prover } } )
+  | ( SpecOnly { spec = `Two (spec, other_spec); sok_message }
+    , (`First as submitted_half) )
+  | ( SpecOnly { spec = `Two (other_spec, spec); sok_message }
+    , (`Second as submitted_half) ) ->
+      Pending
+        (One_of_two
+           { other_spec
+           ; sok_message
+           ; in_pool_half = submitted_half
+           ; in_pool_result = { submitted_result with spec }
+           } )
+  | ( One_of_two
+        { other_spec
+        ; in_pool_half
+        ; in_pool_result
+        ; sok_message = { fee; prover }
+        }
+    , ((`First | `Second) as submitted_half) ) ->
+      assert (not (equal_half in_pool_half submitted_half)) ;
+      let submitted_result =
+        Work.Result.Single.Poly.map ~f_spec:(const other_spec)
+          ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
+      in
+      let in_pool_result =
+        Work.Result.Single.Poly.map ~f_spec:Fn.id
+          ~f_proof:Ledger_proof.Cached.read_proof_from_disk in_pool_result
+      in
+      let results =
+        match submitted_half with
+        | `First ->
+            `Two (submitted_result, in_pool_result)
+        | `Second ->
+            `Two (in_pool_result, submitted_result)
+      in
+      let statements =
+        One_or_two.map
+          ~f:(fun result -> Work.Spec.Single.Poly.statement result.spec)
+          results
+      in
+      let proof = One_or_two.map ~f:(fun result -> result.proof) results in
+      Done
+        ( statements
+        , Mina_wire_types.Network_pool_priced_proof.V1.
+            { proof; fee = { fee; prover } } )
+  | SpecOnly { spec = `One _ as spec; _ }, ((`First | `Second) as submitted_half)
+  | SpecOnly { spec = `Two _ as spec; _ }, (`One as submitted_half) ->
+      NoSuchHalf { submitted_half; spec }
+  | One_of_two { in_pool_half = in_pool; _ }, (`One as submitted_half) ->
+      HalfMismatch { submitted_half; in_pool }
