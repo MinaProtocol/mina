b29e8f295cddcc06fb270199c57a8a4dee774702
Geometer1729 <16kuhnb@gmail.com>
2025-05-27 09:32:26 -0400

    fix proof-cache regeneration and move to script

---
+++ scripts/regenerate-proof-cache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# state in _build can cause non-determinism in proof_caches
+rm -rf _build
+for DIR in $(find ./src -name proof_cache.json | sed 's/\/proof_cache.json//'); do
+  # I'm not sure why but using proof_cache.json directly causes non-determinism
+  # Initialize the target file
+  echo [] > $DIR/proof_cache_new.json;
+  # Generate the cache file by running the tests
+  PROOF_CACHE_OUT=$PWD/$DIR/proof_cache_new.json dune runtest $DIR;
+  mv $PWD/$DIR/proof_cache_new.json $PWD/$DIR/proof_cache.json
+  # Stage the new file for a git commit
+  git add $PWD/$DIR/proof_cache.json
+  # Re-run the tests using the cache. Throws an error if the test is
+  # non-deterministic and caused a cache miss.
+  ERROR_ON_PROOF=true dune runtest $DIR;
+done
