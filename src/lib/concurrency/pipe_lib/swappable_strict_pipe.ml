11b6203ffaf2d96bf6821677bdcbcfe9b6a5a369
glyh <lyhokia@gmail.com>
2025-05-27 10:58:25 +0800

    Pipe Lib: add implementation for swappable strict pipe

---
+++ src/lib/concurrency/pipe_lib/swappable_strict_pipe.ml
@@ -0,0 +1,111 @@
+open Core_kernel
+open Async_kernel
+
+(* NOTE: Data flow:
+      long_live_writer
+   -external-producer-> long_live_reader
+   -actor-> tmp_swapped_writer
+   -external-consumer-> tmp_swapped_reader (tracked out of this structure)
+*)
+
+(* NOTE: We don't know exactly when a writer could be freed, hence a reference
+   counter is here. *)
+type 'data_in_pipe tmp_swapped =
+  { mutable rc : int
+  ; writer :
+      ( 'data_in_pipe
+      , Strict_pipe.synchronous
+      , unit Deferred.t )
+      Strict_pipe.Writer.t
+  }
+
+let tmp_swapped_incr (tmp_swapped : _ tmp_swapped) =
+  tmp_swapped.rc <- tmp_swapped.rc + 1
+
+let tmp_swapped_decr (tmp_swapped : _ tmp_swapped) =
+  tmp_swapped.rc <- tmp_swapped.rc - 1 ;
+  if 0 = tmp_swapped.rc then Strict_pipe.Writer.kill tmp_swapped.writer
+
+type ('data_in_pipe, 'pipe_kind, 'write_return) t =
+  { name : string
+  ; long_live_writer :
+      ('data_in_pipe, 'pipe_kind, 'write_return) Strict_pipe.Writer.t
+  ; long_live_reader : 'data_in_pipe Strict_pipe.Reader.t
+  ; mutable should_terminate : bool
+  ; mutable reader_request :
+      (string * 'data_in_pipe Strict_pipe.Reader.t Ivar.t) option
+  ; mutable tmp_swapped : 'data_in_pipe tmp_swapped option
+  }
+
+let run (t : _ t) =
+  let process_reader_request () =
+    let%map.Option tmp_pipe_name, response = t.reader_request in
+    let tmp_reader, new_tmp_writer =
+      Strict_pipe.create ~name:tmp_pipe_name Strict_pipe.Synchronous
+    in
+    Option.iter ~f:tmp_swapped_decr t.tmp_swapped ;
+    t.tmp_swapped <- Some { writer = new_tmp_writer; rc = 1 } ;
+    Ivar.fill response tmp_reader ;
+    t.reader_request <- None ;
+    `Worked
+  in
+  let process_write () =
+    match t.tmp_swapped with
+    | None ->
+        Deferred.return None
+    | Some tmp_swapped -> (
+        match%map Strict_pipe.Reader.read t.long_live_reader with
+        | `Ok data ->
+            tmp_swapped_incr tmp_swapped ;
+            Deferred.don't_wait_for
+              (let%map () = Strict_pipe.Writer.write tmp_swapped.writer data in
+               tmp_swapped_decr tmp_swapped ) ;
+            Some `Worked
+        | `Eof ->
+            t.should_terminate <- true ;
+            Some `Worked )
+  in
+  let step t =
+    if t.should_terminate then (
+      Strict_pipe.Writer.kill t.long_live_writer ;
+      Option.iter ~f:tmp_swapped_decr t.tmp_swapped ;
+      Deferred.return (`Finished ()) )
+    else
+      match process_reader_request () with
+      | Some `Worked ->
+          let%map _ = process_write () in
+          `Repeat t
+      | None -> (
+          match%bind process_write () with
+          | Some `Worked ->
+              Deferred.return (`Repeat t)
+          | None ->
+              let%map () = Async_kernel_scheduler.yield () in
+              `Repeat t )
+  in
+  Deferred.repeat_until_finished t step
+
+let create ?warn_on_drop ~name type_ =
+  let long_live_reader, long_live_writer =
+    Strict_pipe.create ~name ?warn_on_drop type_
+  in
+  let actor =
+    { name
+    ; long_live_writer
+    ; long_live_reader
+    ; should_terminate = false
+    ; reader_request = None
+    ; tmp_swapped = None
+    }
+  in
+  don't_wait_for (run actor) ;
+  actor
+
+let write { long_live_writer; _ } = Strict_pipe.Writer.write long_live_writer
+
+let swap_reader ~reader_name t : 'data_in_pipe Strict_pipe.Reader.t Deferred.t =
+  let response = Ivar.create () in
+  t.reader_request <- Some (reader_name, response) ;
+  Ivar.read response
+
+let kill t = t.should_terminate <- true
---
+++ src/lib/concurrency/pipe_lib/swappable_strict_pipe.mli
@@ -0,0 +1,31 @@
+open Async_kernel
+
+(** the type for a replaceable strict pipe *)
+type ('data_in_pipe, 'pipe_kind, 'write_return) t
+
+(** [create ?warn_on_drop ~name type] creates a replaceable strict pipe
+    designated ~name. Warning on drop behavior depends on ?warn_on_drop, and it
+    has underlying pipe of type `type`. *)
+val create :
+     ?warn_on_drop:bool
+  -> name:string
+  -> ('data_in_pipe, 'pipe_kind, 'write_return) Strict_pipe.type_
+  -> ('data_in_pipe, 'pipe_kind, 'write_return) t
+
+(** [write t data] writes to a replaceable strict pipe, it would return value
+    designated by the strict pipe type. All ownership is managed inside this
+    data structure so no direct access to long live writer is provided. *)
+val write :
+  ('data_in_pipe, 'pipe_kind, 'write_return) t -> 'data_in_pipe -> 'write_return
+
+(** [swap_reader ~reader_name t] requests replaceable to prepare a new pipe,
+    replacing the old pipe, and returns the reader. All ownership is managed
+    inside this data structure so no writer is provided. *)
+val swap_reader :
+     reader_name:string
+  -> ('data_in_pipe, 'pipe_kind, 'write_return) t
+  -> 'data_in_pipe Strict_pipe.Reader.t Deferred.t
+
+(** [kill t] kills a replaceable strict pipe, it will close all related writer in
+    itself. *)
+val kill : ('data_in_pipe, 'pipe_kind, 'write_return) t -> unit
