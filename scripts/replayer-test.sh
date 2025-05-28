9697fb74429e480c4a770d7bb6ead89bd472b944
Geometer1729 <16kuhnb@gmail.com>
2025-05-28 09:52:14 -0400

    download archive from gcp

--- scripts/replayer-test.sh
+++ scripts/replayer-test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
 # test replayer on known archive db
 set -x
+
+REPLAY="v0"
+ARCHIVE_URL="https://storage.googleapis.com/o1labs-ci-test-data/replay/$REPLAY/archive_db.sql"
+PRECOMPUTED_BLOCKS="https://storage.googleapis.com/o1labs-ci-test-data/replay/$REPLAY/precomputed_blocks.tar.xz"
 
 INPUT_FILE=src/test/archive/sample_db/replayer_input_file.json
 REPLAYER_APP=_build/default/src/app/replayer/replayer.exe
@@ -13,6 +18,11 @@ while [[ "$#" -gt 0 ]]; do case $1 in
   -p|--pg) PG_CONN="$2"; shift;;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
+
+pushd "src/test/archive/sample_db"
+wget "$ARCHIVE_URL"
+wget "$PRECOMPUTED_BLOCKS"
+popd
 
 function report () {
  if [[ $1 == 0 ]]; then
