50deec48fbc44b90b966e880c0a5cac361c18c3c
glyh <lyhokia@gmail.com>
2025-06-01 12:53:02 +0800

    Work Partitioner.Pending zkapp command: styling & doc changes requested by @georgeee

--- src/lib/work_partitioner/pending_zkapp_command.ml
+++ src/lib/work_partitioner/pending_zkapp_command.ml
@@ -1,27 +1,25 @@
-(* NOTE: this module is where the real optimization happen. One assumption we
-   have is the order of merging is irrelvant to the correctness of the final
-   proof. Hence we're only using a counter `merge_remaining` to track have we
-   reach the final proof
-*)
+(* NOTE: Assumption: order of merging segment proofs is irrelvant to the
+   correctness of the final proof. *)
 
 open Core_kernel
 open Snark_work_lib
 
 type t =
   { job : (Spec.Single.t, Id.Single.t) With_job_meta.t
-        (** the original work being splitted, should be identical to
-            Work_selector.work *)
+        (** the original work being split, contains `Work_selector.work` with
+            some metadata. *)
   ; unscheduled_segments : Spec.Sub_zkapp.Stable.Latest.t Queue.t
   ; pending_mergeable_proofs : Ledger_proof.t Deque.t
-        (** we may need to insert proofs to merge back to the queue, hence a
-            Deque *)
+        (* we may need to insert proofs to merge back to the queue, hence a Deque
+         *)
   ; mutable elapsed : Time.Stable.Span.V1.t
         (** The total work time for all SNARK workers combined to prove this
-            specific zkapp command. Or, the time it would take a single SNARK
+            specific zkapp command. I.e. the time it would take a single SNARK
             worker to generate the final proof of this command. *)
   ; mutable proofs_in_flights : int
         (** The number of proofs we need to wait for before being sure we could
-            continue with the only proof on hand as the final proof. *)
+            continue with the only proof contained in [pending_mergeable_proofs]
+            as the final proof, provided [unscheduled_segments] being empty. *)
   }
 
 let create ~job ~unscheduled_segments ~pending_mergeable_proofs =
@@ -32,20 +30,20 @@ let create ~job ~unscheduled_segments ~pending_mergeable_proofs =
   ; proofs_in_flights = 0
   }
 
+let try_take2 (q : 'a Deque.t) : ('a * 'a) option =
+  match Deque.dequeue_front q with
+  | None ->
+      None
+  | Some fst -> (
+      match Deque.dequeue_front q with
+      | Some snd ->
+          Some (fst, snd)
+      | None ->
+          Deque.enqueue_front q fst ; None )
+
 (** [next_merge t] attempts dequeuing 2 proofs from [t.pending_mergeable_proofs]
     and generate a sub-zkapp level spec merging them together. *)
 let next_merge (t : t) =
-  let try_take2 (q : 'a Deque.t) : ('a * 'a) option =
-    match Deque.dequeue_front q with
-    | None ->
-        None
-    | Some fst -> (
-        match Deque.dequeue_front q with
-        | Some snd ->
-            Some (fst, snd)
-        | None ->
-            Deque.enqueue_front q fst ; None )
-  in
   let open Option.Let_syntax in
   let%map proof1, proof2 = try_take2 t.pending_mergeable_proofs in
   t.proofs_in_flights <- t.proofs_in_flights + 1 ;
@@ -59,7 +57,7 @@ let next_segment (t : t) =
   t.proofs_in_flights <- t.proofs_in_flights + 1 ;
   segment
 
-let generate_job_spec (t : t) : Spec.Sub_zkapp.Stable.Latest.t option =
+let next_job_spec (t : t) : Spec.Sub_zkapp.Stable.Latest.t option =
   match next_merge t with Some _ as ret -> ret | None -> next_segment t
 
 let submit_proof (t : t) ~(proof : Ledger_proof.t)
@@ -70,8 +68,8 @@ let submit_proof (t : t) ~(proof : Ledger_proof.t)
 
 let try_finalize (t : t) =
   if
-    0 = t.proofs_in_flights
-    && 0 = Queue.length t.unscheduled_segments
-    && 1 = Deque.length t.pending_mergeable_proofs
+    t.proofs_in_flights = 0
+    && Queue.is_empty t.unscheduled_segments
+    && Deque.length t.pending_mergeable_proofs = 1
   then Some (t.job, Deque.dequeue_back_exn t.pending_mergeable_proofs, t.elapsed)
   else None
--- src/lib/work_partitioner/pending_zkapp_command.mli
+++ src/lib/work_partitioner/pending_zkapp_command.mli
@@ -1,6 +1,8 @@
 open Core_kernel
 open Snark_work_lib
 
+(** [t] is a pool for zkapp segments and mergeable proofs corresponding to a
+    single zkapp transaction. *)
 type t
 
 val create :
@@ -9,12 +11,16 @@ val create :
   -> pending_mergeable_proofs:Ledger_proof.t Deque.t
   -> t
 
-val generate_job_spec : t -> Spec.Sub_zkapp.Stable.Latest.t option
+(** [next_job_spec t] extracts another job spec from t, mutating the internal
+    state of [t]. Once any job spec returned is completed, it's expected to be
+    submitted back to [t] with [submit_proof] *)
+val next_job_spec : t -> Spec.Sub_zkapp.Stable.Latest.t option
 
 val submit_proof :
   t -> proof:Ledger_proof.t -> elapsed:Core_kernel_private.Span_float.t -> unit
 
-(** [try_finalize t] attempts to unwrap completed proof, metric and spec from [t] if the proof is completed *)
+(** [try_finalize t] attempts to unwrap completed proof, metric and spec from
+    [t] if the proof for entire zkapp transaction is completed *)
 val try_finalize :
      t
   -> ( (Spec.Single.t, Id.Single.t) With_job_meta.t
