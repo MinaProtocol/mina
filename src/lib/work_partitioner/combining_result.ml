cdfe9586ce3f80d9da88bc0c8e9a6c50ca139b47
glyh <lyhokia@gmail.com>
2025-06-02 20:58:48 +0800

    Work Partitioner > Combining Result: lift out finalize_{one, two}

--- src/lib/work_partitioner/combining_result.ml
+++ src/lib/work_partitioner/combining_result.ml
@@ -25,24 +25,56 @@ type merge_outcome =
   | StructureMismatch of
       { spec : Snark_work_lib.Selector.Single.Spec.t One_or_two.t }
 
+let finalize_one ~submitted_result ~spec ~fee ~prover =
+  let submitted_result =
+    Snark_work_lib.Result.Single.Poly.map ~f_spec:Fn.id
+      ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
+  in
+  let Snark_work_lib.Result.Single.Poly.{ proof; _ } = submitted_result in
+  let statements =
+    `One (Snark_work_lib.Selector.Single.Spec.Poly.statement spec)
+  in
+  Done
+    ( statements
+    , Mina_wire_types.Network_pool_priced_proof.V1.
+        { proof = `One proof; fee = { fee; prover } } )
+
+let finalize_two ~submitted_result ~other_spec ~in_pool_result ~submitted_half
+    ~fee ~prover =
+  let submitted_result =
+    Snark_work_lib.Result.Single.Poly.map ~f_spec:(const other_spec)
+      ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
+  in
+  let in_pool_result =
+    Snark_work_lib.Result.Single.Poly.map ~f_spec:Fn.id
+      ~f_proof:Ledger_proof.Cached.read_proof_from_disk in_pool_result
+  in
+  let results =
+    match submitted_half with
+    | `First ->
+        `Two (submitted_result, in_pool_result)
+    | `Second ->
+        `Two (in_pool_result, submitted_result)
+  in
+  let statements =
+    One_or_two.map
+      ~f:(fun result ->
+        Snark_work_lib.Selector.Single.Spec.Poly.statement result.spec )
+      results
+  in
+  let proof = One_or_two.map ~f:(fun result -> result.proof) results in
+  Done
+    ( statements
+    , Mina_wire_types.Network_pool_priced_proof.V1.
+        { proof; fee = { fee; prover } } )
+
 let merge_single_result (current : t)
     ~(submitted_result :
        (unit, Ledger_proof.Cached.t) Snark_work_lib.Result.Single.Poly.t )
     ~(submitted_half : submitted_half) : merge_outcome =
   match (current, submitted_half) with
   | Spec_only { spec = `One spec; sok_message = { fee; prover } }, `One ->
-      let submitted_result =
-        Snark_work_lib.Result.Single.Poly.map ~f_spec:Fn.id
-          ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
-      in
-      let Snark_work_lib.Result.Single.Poly.{ proof; _ } = submitted_result in
-      let statements =
-        `One (Snark_work_lib.Selector.Single.Spec.Poly.statement spec)
-      in
-      Done
-        ( statements
-        , Mina_wire_types.Network_pool_priced_proof.V1.
-            { proof = `One proof; fee = { fee; prover } } )
+      finalize_one ~submitted_result ~spec ~fee ~prover
   | ( Spec_only { spec = `Two (spec, other_spec); sok_message }
     , (`First as submitted_half) )
   | ( Spec_only { spec = `Two (other_spec, spec); sok_message }
@@ -62,32 +94,8 @@ let merge_single_result (current : t)
         }
     , ((`First | `Second) as submitted_half) )
     when not (equal_half in_pool_half submitted_half) ->
-      let submitted_result =
-        Snark_work_lib.Result.Single.Poly.map ~f_spec:(const other_spec)
-          ~f_proof:Ledger_proof.Cached.read_proof_from_disk submitted_result
-      in
-      let in_pool_result =
-        Snark_work_lib.Result.Single.Poly.map ~f_spec:Fn.id
-          ~f_proof:Ledger_proof.Cached.read_proof_from_disk in_pool_result
-      in
-      let results =
-        match submitted_half with
-        | `First ->
-            `Two (submitted_result, in_pool_result)
-        | `Second ->
-            `Two (in_pool_result, submitted_result)
-      in
-      let statements =
-        One_or_two.map
-          ~f:(fun result ->
-            Snark_work_lib.Selector.Single.Spec.Poly.statement result.spec )
-          results
-      in
-      let proof = One_or_two.map ~f:(fun result -> result.proof) results in
-      Done
-        ( statements
-        , Mina_wire_types.Network_pool_priced_proof.V1.
-            { proof; fee = { fee; prover } } )
+      finalize_two ~submitted_result ~other_spec ~in_pool_result ~submitted_half
+        ~fee ~prover
   | Spec_only { spec = `One _ as spec; _ }, (`First | `Second)
   | Spec_only { spec = `Two _ as spec; _ }, `One ->
       StructureMismatch { spec }
