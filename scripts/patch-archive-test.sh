45c63f6ad1da6d9a286084f3945a83459efa4483
Geometer1729 <16kuhnb@gmail.com>
2025-06-02 12:48:16 -0400

    try direct curl here

--- scripts/patch-archive-test.sh
+++ scripts/patch-archive-test.sh
@@ -3,7 +3,7 @@
 set -x
 # test replayer on known archive db
 
-NETWORK_DATA_FOLDER=_build/default/src/test/archive/sample_db
+NETWORK_DATA_FOLDER=src/test/archive/sample_db
 PATCH_ARCHIVE_TEST_APP=${PATCH_ARCHIVE_TEST_APP:-_build/default/src/test/archive/patch_archive_test/patch_archive_test.exe}
 PG_PORT=${PG_PORT:-5433}
 POSTGRES_USER=${POSTGRES_USER:-postgres}
@@ -13,7 +13,8 @@ CONN=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${PG_PORT}
 
 
 # Download the archive data
-dune b "${NETWORK_DATA_FOLDER}"
+# TODO this can't be hard coded in so many places
+curl -L -o a"${NETWORK_DATA_FOLDER}/archive_db.sql" "https://storage.googleapis.com/o1labs-ci-test-data/replay/v0/archive_db.sql"
 
 echo "Running patch archive test"
 "${PATCH_ARCHIVE_TEST_APP}" --source-uri "${CONN}" \
