5d29dda0275ab14462faf9f2231b0404972d675d
dkijania <dariusz@o1labs.org>
2025-06-05 23:47:52 +0200

    pip3 -> pip

--- buildkite/scripts/bench/install.sh
+++ buildkite/scripts/bench/install.sh
@@ -15,4 +15,4 @@ source buildkite/scripts/export-git-env-vars.sh
 
 source buildkite/scripts/debian/install.sh "mina-test-suite,mina-$TESTNET_NAME" 1
 
-pip3 install -r scripts/benchmarks/requirements.txt
+pip install -r scripts/benchmarks/requirements.txt
