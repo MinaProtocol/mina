a92e73551d4e85788780850639d6282c2e1431dc
dkijania <dariusz@o1labs.org>
2025-06-05 08:47:10 +0200

    dynamically fetch port from postgres instead of hardcode it

--- buildkite/scripts/setup-database-for-archive-node.sh
+++ buildkite/scripts/setup-database-for-archive-node.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 user=$1
 password=$2
 db=$3
-port=$4
 
 
 sudo service postgresql start
@@ -13,4 +12,16 @@ sudo -u postgres psql -c "CREATE USER ${user} WITH LOGIN SUPERUSER PASSWORD '${p
 sudo pg_isready
 service postgresql status
 sudo -u postgres createdb -O "${user}" "${db}"
+
+# detects up a PostgreSQL database for an archive node.
+port=$(sudo -u postgres psql -t -c "SHOW port;" | xargs)
+if [[ -z "$port" ]]; then
+  echo "Failed to detect PostgreSQL port."
+  exit 1
+fi
+  echo "$port"
+
+
 PGPASSWORD="${password}" psql -h localhost -p "${port}" -U "${user}" -d "${db}" -a -f src/app/archive/create_schema.sql
+
+export MINA_TEST_POSTGRES="postgres://${user}:${password}@localhost:${port}/${db}"
