7c39c38235e58cfc64c9010b41a7cd2785ecdfe7
glyh <lyhokia@gmail.com>
2025-06-05 16:38:31 +0800

    rename modules so to reflex what scope are they testing

--- src/lib/merkle_ledger/test/test_merkle_ledger.ml
+++ src/lib/merkle_ledger/test/test_merkle_ledger.ml
@@ -8,6 +8,7 @@
 
 let () =
   let tests =
-    Test_database.tests @ Test.tests @ Test_mask.tests @ Test_converting.tests
+    Test_database_in_mem.tests @ Test_database_integration.tests
+    @ Test_mask.tests @ Test_converting.tests
   in
   Alcotest.run "Merkle ledger" tests
