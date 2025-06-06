3a077616b0eea87df56c06dc3c845c3d78f39cf6
glyh <lyhokia@gmail.com>
2025-06-06 18:11:29 +0800

    Work Partitioner: add interface

---
+++ src/lib/work_partitioner/work_partitioner.mli
@@ -0,0 +1,15 @@
+open Core_kernel
+module Snark_worker_shared = Snark_worker_shared
+
+type t
+
+val create : reassignment_timeout:Time.Span.t -> logger:Logger.t -> t
+
+type work_from_selector =
+  unit -> Snark_work_lib.Spec.Single.t One_or_two.t option
+
+val request_partitioned_work :
+     sok_message:Mina_base.Sok_message.t
+  -> work_from_selector:work_from_selector
+  -> partitioner:t
+  -> Snark_work_lib.Spec.Partitioned.Stable.Latest.t option
