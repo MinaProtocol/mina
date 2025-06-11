1fb0cdfbc6c7514a25f9192ffac3b2057cc38508
Geometer1729 <16kuhnb@gmail.com>
2025-06-11 12:26:31 -0400

    format fix

--- src/lib/staged_ledger_diff/staged_ledger_diff.ml
+++ src/lib/staged_ledger_diff/staged_ledger_diff.ml
@@ -4,4 +4,5 @@ module Bitswap_block = Bitswap_block
 
 let genesis_body_reference =
   (* Tag Body is fixed to integer value 0 *)
-  Body.compute_reference ~tag:0 (Body.Stable.Latest.create Stable.Latest.empty_diff)
+  Body.compute_reference ~tag:0
+    (Body.Stable.Latest.create Stable.Latest.empty_diff)
