cc71620c9282984686b8b47646aac61b61f62319
dkijania <dariusz@o1labs.org>
2025-06-16 12:15:25 +0200

    This reverts commit c4d030bf42935f99bd89541e9a4632c638d880fc.

--- src/test/mina_automation/utils.ml
+++ src/test/mina_automation/utils.ml
@@ -11,16 +11,6 @@ let sed ~search ~replacement ~input =
 let untar ~archive ~output =
   Util.run_cmd_exn "." "tar" [ "-xf"; archive; "-C"; output ]
 
-let dedup_and_sort_archive_files files : string list =
-  files
-  |> List.dedup_and_sort ~compare:(fun left right ->
-         let scan_height name = Scanf.sscanf name "%_s@-%d-%_s" Fn.id in
-
-         let left_height = scan_height left in
-         let right_height = scan_height right in
-
-         Int.compare left_height right_height )
-
 let force_kill process =
   Process.send_signal process Core.Signal.kill ;
   Deferred.map (Process.wait process) ~f:Or_error.return
