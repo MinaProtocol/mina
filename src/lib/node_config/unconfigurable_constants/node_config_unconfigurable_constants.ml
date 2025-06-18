25cd6c2551160f6a7d9a74ec0645b6f5e4574956
glyh <lyhokia@gmail.com>
2025-06-18 15:20:23 +0800

    Unconfigurable Constants: remove outdated comment info on zkapp command limit

--- src/lib/node_config/unconfigurable_constants/node_config_unconfigurable_constants.ml
+++ src/lib/node_config/unconfigurable_constants/node_config_unconfigurable_constants.ml
@@ -1,18 +1,5 @@
 (* FIXME: These should be configurable. *)
 
-(** limits on Zkapp_command.t size
-    10.26*np + 10.08*n2 + 9.14*n1 < 69.45
-    where np: number of single proof updates
-    n2: number of pairs of signed/no-auth update
-    n1: number of single signed/no-auth update
-    and their coefficients representing the cost
-  The formula was generated based on benchmarking data conducted on bare
-  metal i9 processor with room to include lower spec.
-  69.45 was the total time for a combination of updates that was considered
-  acceptable.
-  The method used to estimate the cost was linear least squares.
-*)
-
 let max_zkapp_segment_per_transaction = 16
 
 let max_event_elements = 100
