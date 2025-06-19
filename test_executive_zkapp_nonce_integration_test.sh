65924d76cb7fbc0bf0304d736dae458627c3491d
glyh <lyhokia@gmail.com>
2025-06-19 19:53:26 +0800

    Implement shell script running mock coordinator

---
+++ test_executive_zkapp_nonce_integration_test.sh
@@ -0,0 +1,12 @@
+export MINA_DOCKER_TAG=3.1.1-alpha1-lyh-update-snark-worker-rpc-jun-12-72a8461-bullseye
+
+export MINA_IMAGE="gcr.io/o1labs-192920/mina-daemon:$MINA_DOCKER_TAG-berkeley"
+export ARCHIVE_IMAGE="gcr.io/o1labs-192920/mina-archive:$MINA_DOCKER_TAG"
+export TEST_NAME=zkapps-nonce
+
+./_build/default/src/app/test_executive/test_executive.exe \
+local $TEST_NAME \
+--mina-image=$MINA_IMAGE \
+--archive-image=$ARCHIVE_IMAGE \
+| tee "$TEST_NAME.local.test.log" \
+| ./_build/default/src/app/logproc/logproc.exe -i inline -f '!(.level in ["Spam"])'
