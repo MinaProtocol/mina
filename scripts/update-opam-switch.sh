66e8cc1681dc5bd11a08cef22c4f2c98c19ecdf3
Danny Willems <be.danny.willems@gmail.com>
2025-07-01 13:06:50 +0200

    Docker/non-nix: pin to a specific commit of ocaml/opam-repository

--- scripts/update-opam-switch.sh
+++ scripts/update-opam-switch.sh
@@ -16,6 +16,11 @@ switch_dir=opam_switches/"$sum"
 # - scripts/update_opam_switch.sh
 ocaml_version=4.14.2
 
+# The version must be the same as the version used in:
+# - dockerfiles/1-build-deps
+# - scripts/update_opam_switch.sh
+opam_repository_commit=08d8c16c16dc6b23a5278b06dff0ac6c7a217356
+
 if [[ -d _opam ]]; then
     read -rp "Directory '_opam' exists and will be removed. Continue? [y/N] " \
          confirm
@@ -34,6 +39,13 @@ if [[ ! -d "${switch_dir}" ]]; then
     # (if it's repeated, it's a no-op)
     opam repository add --yes --all --set-default o1-labs \
          https://github.com/o1-labs/opam-repository.git
+    # The default opam repository is set to a specific commit as some of our
+    # dependencies have been archived.
+    # See https://github.com/MinaProtocol/mina/pull/17450
+    opam repository \
+         set-url \
+         default \
+         "git+https://github.com/ocaml/opam-repository.git#${opam_repository_commit}"
     opam update
     opam switch import -y --switch . opam.export
     mkdir -p opam_switches
