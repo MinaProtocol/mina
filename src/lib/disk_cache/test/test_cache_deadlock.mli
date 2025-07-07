5c5bd004f1f235e9df9ad047e4564bfca6c9aa2c
Danny Willems <be.danny.willems@gmail.com>
2025-07-07 11:44:43 +0200

    Disk_cache/test: support matching scenario expected output

--- src/lib/disk_cache/test/test_cache_deadlock.mli
+++ src/lib/disk_cache/test/test_cache_deadlock.mli
@@ -4,4 +4,5 @@ module type Cache_intf = sig
   module Make : Disk_cache_intf.F
 end
 
-val test_cache_deadlock : (module Cache_intf) -> unit Deferred.t
+val test_cache_deadlock :
+  (module Cache_intf) -> [ `Success | `Timeout ] Deferred.t
--- src/lib/disk_cache/test/test_filesystem_deadlock.ml
+++ src/lib/disk_cache/test/test_filesystem_deadlock.ml
@@ -4,5 +4,12 @@ open Async
 let () =
   printf "Running Filesystem cache control test...\n%!" ;
   Thread_safe.block_on_async_exn (fun () ->
-      Test_cache_deadlock_lib.Test_cache_deadlock.test_cache_deadlock
-        (module Disk_cache) )
+      let%bind res =
+        Test_cache_deadlock_lib.Test_cache_deadlock.test_cache_deadlock
+          (module Disk_cache)
+      in
+      match res with
+      | `Success ->
+          printf "Success" ; Deferred.unit
+      | `Timeout ->
+          failwith "The process should not time out" )
