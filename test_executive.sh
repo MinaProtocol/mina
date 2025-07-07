60cb40037dcad55160527a7194bfaf5daa4d273b
glyh <lyhokia@gmail.com>
2025-07-07 19:52:08 +0800

    docker set log level to internal

---
+++ test_executive.sh
@@ -0,0 +1,18 @@
+export MINA_DOCKER_TAG=3.1.1-alpha1-lyh-update-snark-worker-rpc-jun-12-with-deadlock-fix-678c2f7-bullseye
+
+export MINA_IMAGE="gcr.io/o1labs-192920/mina-daemon:$MINA_DOCKER_TAG-berkeley"
+
+export ARCHIVE_IMAGE="gcr.io/o1labs-192920/mina-archive:$MINA_DOCKER_TAG"
+
+# docker pull $MINA_IMAGE
+# docker pull $ARCHIVE_IMAGE
+
+export TEST_NAME=payment
+
+./_build/default/src/app/test_executive/test_executive.exe \
+local $TEST_NAME \
+--mina-image=$MINA_IMAGE \
+--archive-image=$ARCHIVE_IMAGE \
+--debug \
+| tee "$TEST_NAME.local.test.log" \
+| ./_build/default/src/app/logproc/logproc.exe -i inline -f '!(.level in ["Spam"])'
