4b360e01968ca8ab480856af2b6ff88382a065e9
glyh <lyhokia@gmail.com>
2025-07-10 10:11:24 +0800

    add scripts

---
+++ zkapp_nonce.sh
@@ -0,0 +1,16 @@
+export MINA_DOCKER_TAG=3.1.1-alpha1-lyh-update-snark-worker-rpc-jun-12-testing-8692b08-bullseye
+
+export MINA_IMAGE="gcr.io/o1labs-192920/mina-daemon:$MINA_DOCKER_TAG-berkeley"
+export ARCHIVE_IMAGE="gcr.io/o1labs-192920/mina-archive:$MINA_DOCKER_TAG"
+
+docker pull $MINA_IMAGE
+docker pull $ARCHIVE_IMAGE
+
+# export TEST_NAME=zkapps-nonce
+#
+# ./_build/default/src/app/test_executive/test_executive.exe \
+# local $TEST_NAME \
+# --mina-image=$MINA_IMAGE \
+# --archive-image=$ARCHIVE_IMAGE \
+# | tee "$TEST_NAME.local.test.log" \
+# | ./_build/default/src/app/logproc/logproc.exe -i inline -f '!(.level in ["Spam"])'
