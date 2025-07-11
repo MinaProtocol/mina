bda3b838d191781c892040e73f5391e84ff4be04
Danny Willems <be.danny.willems@gmail.com>
2025-07-11 16:51:29 +0200

    mina-local-network: delete trailing whitespaces

--- scripts/mina-local-network/mina-local-network.sh
+++ scripts/mina-local-network/mina-local-network.sh
@@ -578,11 +578,11 @@ fi
 
 if [ ! -z "${OVERRIDE_SLOT_TIME_MS}" ]; then
   echo 'Modifying configuration to override slot time'
-  
+
   if [ ! -f "${CONFIG}" ]; then
     reset-genesis-ledger "${LEDGER_FOLDER}" "${CONFIG}"
   fi
-  
+
   printf "\n"
   tmp=$(mktemp)
   jq ".proof.block_window_duration_ms=${OVERRIDE_SLOT_TIME_MS}" "${CONFIG}" >"$tmp" && mv -f "$tmp" "${CONFIG}"
