127d3ed190242158797a127f6b3c0de9f5378b8e
Danny Willems <be.danny.willems@gmail.com>
2025-07-11 19:52:46 +0200

    mina-local-network: remove unecessary ${} when performing arithmetic

--- scripts/mina-local-network/mina-local-network.sh
+++ scripts/mina-local-network/mina-local-network.sh
@@ -168,14 +168,10 @@ exec-daemon() {
   BASE_PORT=${1}
   shift
   CLIENT_PORT=${BASE_PORT}
-  # shellcheck disable=SC2004
-  REST_PORT=$((${BASE_PORT} + 1))
-  # shellcheck disable=SC2004
-  EXTERNAL_PORT=$((${BASE_PORT} + 2))
-  # shellcheck disable=SC2004
-  DAEMON_METRICS_PORT=$((${BASE_PORT} + 3))
-  # shellcheck disable=SC2004
-  LIBP2P_METRICS_PORT=$((${BASE_PORT} + 4))
+  REST_PORT=$((BASE_PORT + 1))
+  EXTERNAL_PORT=$((BASE_PORT + 2))
+  DAEMON_METRICS_PORT=$((BASE_PORT + 3))
+  LIBP2P_METRICS_PORT=$((BASE_PORT + 4))
 
   # shellcheck disable=SC2068
   exec ${MINA_EXE} daemon \
@@ -423,7 +419,7 @@ fi
 # ================================================
 # Configure the Seed Peer ID
 
-SEED_PEER_ID="/ip4/127.0.0.1/tcp/$((${SEED_START_PORT} + 2))/p2p/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr"
+SEED_PEER_ID="/ip4/127.0.0.1/tcp/$((SEED_START_PORT + 2))/p2p/12D3KooWAFFq2yEQFFzhU5dt64AWqawRuomG9hL8rSmm5vxhAsgr"
 
 # ================================================
 #
@@ -662,7 +658,7 @@ for ((i = 0; i < WHALES; i++)); do
   FOLDER="${NODES_FOLDER}"/whale_"${i}"
   KEY_FILE="${LEDGER_FOLDER}"/online_whale_keys/online_whale_account_"${i}"
   mkdir -p "${FOLDER}"
-  spawn-node "${FOLDER}" $((${WHALE_START_PORT} + (${i} * 5))) \
+  spawn-node "${FOLDER}" $((WHALE_START_PORT + (i * 5))) \
              -peer ${SEED_PEER_ID} \
              -block-producer-key "${KEY_FILE}" \
              -libp2p-keypair "${LEDGER_FOLDER}"/libp2p_keys/whale_${i} \
@@ -676,7 +672,7 @@ for ((i = 0; i < FISH; i++)); do
   FOLDER="${NODES_FOLDER}"/fish_${i}
   KEY_FILE="${LEDGER_FOLDER}"/online_fish_keys/online_fish_account_${i}
   mkdir -p "${FOLDER}"
-  spawn-node "${FOLDER}" $((${FISH_START_PORT} + (${i} * 5))) \
+  spawn-node "${FOLDER}" $((FISH_START_PORT + (i * 5))) \
              -peer ${SEED_PEER_ID} \
              -block-producer-key "${KEY_FILE}" \
              -libp2p-keypair "${LEDGER_FOLDER}"/libp2p_keys/fish_${i} \
@@ -689,7 +685,7 @@ done
 for ((i = 0; i < NODES; i++)); do
   FOLDER="${NODES_FOLDER}"/node_${i}
   mkdir -p "${FOLDER}"
-  spawn-node "${FOLDER}" $((${NODE_START_PORT} + (${i} * 5))) \
+  spawn-node "${FOLDER}" $((NODE_START_PORT + (i * 5))) \
              -peer ${SEED_PEER_ID} \
              -libp2p-keypair "${LEDGER_FOLDER}"/libp2p_keys/node_${i} \
              "${ARCHIVE_ADDRESS_CLI_ARG}"
@@ -736,8 +732,7 @@ if [ "${WHALES}" -gt "0" ]; then
   for ((i = 0; i < WHALES; i++)); do
     echo -e "\t\tInstance #${i}:"
     echo -e "\t\t  pid ${WHALE_PIDS[${i}]}"
-    # shellcheck disable=SC2004
-    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((${WHALE_START_PORT} + (${i} * 5)))"
+    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((WHALE_START_PORT + (i * 5)))"
     echo -e "\t\t  logs: cat ${NODES_FOLDER}/whale_${i}/log.txt | ${LOGPROC_EXE}"
   done
 fi
@@ -747,8 +742,7 @@ if [ "${FISH}" -gt "0" ]; then
   for ((i = 0; i < FISH; i++)); do
     echo -e "\t\tInstance #${i}:"
     echo -e "\t\t  pid ${FISH_PIDS[${i}]}"
-    # shellcheck disable=SC2004
-    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((${FISH_START_PORT} + (${i} * 5)))"
+    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((FISH_START_PORT + (i * 5)))"
     echo -e "\t\t  logs: cat ${NODES_FOLDER}/fish_${i}/log.txt | ${LOGPROC_EXE}"
   done
 fi
@@ -758,8 +752,7 @@ if [ "${NODES}" -gt "0" ]; then
   for ((i = 0; i < NODES; i++)); do
     echo -e "\t\tInstance #${i}:"
     echo -e "\t\t  pid ${NODE_PIDS[${i}]}"
-    # shellcheck disable=SC2004
-    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((${NODE_START_PORT} + (${i} * 5)))"
+    echo -e "\t\t  status: ${MINA_EXE} client status -daemon-port $((NODE_START_PORT + (i * 5)))"
     echo -e "\t\t  logs: cat ${NODES_FOLDER}/node_${i}/log.txt | ${LOGPROC_EXE}"
   done
 fi
