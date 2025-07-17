ac79c1bc336fadd493c94bcf1737bfacd200f8da
dkijania <dariusz@o1labs.org>
2025-07-17 15:36:55 +0200

    implement process memeory getter

--- src/test/mina_automation/archive.ml
+++ src/test/mina_automation/archive.ml
@@ -68,6 +68,9 @@ module Process = struct
            return
            @@ [%log debug] "Archive stdout: $stdout"
                 ~metadata:[ ("stdout", `String stdout) ] )
+
+  let get_memory_usage_mb t =
+    Utils.get_memory_usage_mb @@ (Process.pid t.process |> Pid.to_int)
 end
 
 (** [start t] starts the archive process using the given configuration [t].
