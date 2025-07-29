a5ddd3da3ef195e745e5509b59ec43ec11a63e41
glyh <lyhokia@gmail.com>
2025-07-29 11:39:07 +0800

    Bump max_{event,action}_elements per zkapp command {100 -> 1024}

--- src/lib/node_config/unconfigurable_constants/node_config_unconfigurable_constants.ml
+++ src/lib/node_config/unconfigurable_constants/node_config_unconfigurable_constants.ml
@@ -21,9 +21,9 @@ let zkapp_signed_single_update_cost = 9.14
 
 let zkapp_transaction_cost_limit = 69.45
 
-let max_event_elements = 100
+let max_event_elements = 1024
 
-let max_action_elements = 100
+let max_action_elements = 1024
 
 let zkapp_cmd_limit_hardcap = 128
 
