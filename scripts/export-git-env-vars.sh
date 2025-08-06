7d5ddf85fc54b30577485f60ae3213a529e58953
dkijania <dariusz@o1labs.org>
2025-08-06 21:17:44 +0200

    reuse export-git-env-vars from scripts

--- scripts/export-git-env-vars.sh
+++ scripts/export-git-env-vars.sh
@@ -2,16 +2,20 @@
 set -euo pipefail
 set -x
 
+KEEP_MY_TAGS_INTACT=${KEEP_MY_TAGS_INTACT:-1}
 
 # In case of running this script on detached head, script has difficulties in finding out
 # what is the current 
 echo "Exporting Git Variables: "
 
-git fetch
-
 function find_most_recent_numeric_tag() {
-    # We use the --prune flag because we've had problems with buildkite agents getting conflicting results here
-    git fetch --tags --prune --prune-tags --force
+
+    if [[ "$KEEP_MY_TAGS_INTACT" != 1 ]]; then
+        # We use the --prune flag because we've had problems with buildkite agents getting conflicting results here
+        git fetch --tags --prune --prune-tags --force
+    else
+        git fetch --tags --force
+    fi
     TAG=$(git describe --always --abbrev=0 $1 | sed 's!/!-!g; s!_!-!g; s!#!-!g')
     if [[ $TAG != [0-9]* ]]; then
         TAG=$(find_most_recent_numeric_tag $TAG~)
@@ -19,8 +23,10 @@ function find_most_recent_numeric_tag() {
     echo $TAG
 }
 
+
 export GITHASH=$(git rev-parse --short=7 HEAD)
 export THIS_COMMIT_TAG=$(git tag --points-at HEAD)
+export MINA_DEB_CODENAME=${MINA_DEB_CODENAME:=bullseye}
 
 if [[ -v BRANCH_NAME ]]; then
    export GITBRANCH=$(echo "$BRANCH_NAME" | sed 's!/!-!g; s!_!-!g; s!#!-!g')
@@ -34,5 +40,3 @@ export MINA_DEB_VERSION="${GITTAG}-${GITBRANCH}-${GITHASH}"
 export MINA_DOCKER_TAG="$(echo "${MINA_DEB_VERSION}-${MINA_DEB_CODENAME}" | sed 's!/!-!g; s!_!-!g')"
 
 [[ -v THIS_COMMIT_TAG ]] && export MINA_COMMIT_TAG="${THIS_COMMIT_TAG}"
-
-echo "after commit tag"
