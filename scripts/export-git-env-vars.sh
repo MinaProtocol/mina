ac7c3b6ced0286c8c0f7f396aa5ceafd6d444d12
dkijania <dariusz@o1labs.org>
2025-08-12 13:53:35 +0200

    remove duplicated env vars and functions from export-git-env-vars scripts

--- scripts/export-git-env-vars.sh
+++ scripts/export-git-env-vars.sh
@@ -3,15 +3,22 @@ set -euo pipefail
 set -x
 
 
+# If enabled, keep my tags intact, it won't run git fetch --prune
+KEEP_MY_TAGS_INTACT=${KEEP_MY_TAGS_INTACT:-1}
+
 # In case of running this script on detached head, script has difficulties in finding out
-# what is the current 
+# what is the current
 echo "Exporting Git Variables: "
 
-git fetch
-
 function find_most_recent_numeric_tag() {
-    # We use the --prune flag because we've had problems with buildkite agents getting conflicting results here
-    git fetch --tags --prune --prune-tags --force
+
+    local keep_tags_values=("1" "true" "t" "T" "y" "yes" "Y" "YES")
+    if [[ ! " ${keep_tags_values[*]} " =~  ${KEEP_MY_TAGS_INTACT}  ]]; then
+        # We use the --prune flag because we've had problems with buildkite agents getting conflicting results here
+        git fetch --tags --prune --prune-tags --force
+    else
+        git fetch --tags --force
+    fi
     TAG=$(git describe --always --abbrev=0 $1 | sed 's!/!-!g; s!_!-!g; s!#!-!g')
     if [[ $TAG != [0-9]* ]]; then
         TAG=$(find_most_recent_numeric_tag $TAG~)
@@ -19,20 +26,26 @@ function find_most_recent_numeric_tag() {
     echo $TAG
 }
 
-export GITHASH=$(git rev-parse --short=7 HEAD)
-export THIS_COMMIT_TAG=$(git tag --points-at HEAD)
+GITHASH=$(git rev-parse --short=7 HEAD)
+THIS_COMMIT_TAG=$(git tag --points-at HEAD)
 
 if [[ -v BRANCH_NAME ]]; then
-   export GITBRANCH=$(echo "$BRANCH_NAME" | sed 's!/!-!g; s!_!-!g; s!#!-!g')
+   GITBRANCH=$(echo "$BRANCH_NAME" | sed 's!/!-!g; s!_!-!g; s!#!-!g')
 else
-   export GITBRANCH=$(git name-rev --name-only $GITHASH | sed "s/remotes\/origin\///g" | sed 's!/!-!g; s!_!-!g; s!#!-!g' )
+   GITBRANCH=$(git name-rev --name-only $GITHASH | sed "s/remotes\/origin\///g" | sed 's!/!-!g; s!_!-!g; s!#!-!g' )
 fi
 
-export GITTAG=$(find_most_recent_numeric_tag HEAD)
+GITTAG=$(find_most_recent_numeric_tag HEAD)
 
-export MINA_DEB_VERSION="${GITTAG}-${GITBRANCH}-${GITHASH}"
-export MINA_DOCKER_TAG="$(echo "${MINA_DEB_VERSION}-${MINA_DEB_CODENAME}" | sed 's!/!-!g; s!_!-!g')"
+MINA_DEB_VERSION="${GITTAG}-${GITBRANCH}-${GITHASH}"
+MINA_DOCKER_TAG=$(echo "${MINA_DEB_VERSION}-${MINA_DEB_CODENAME}" | sed 's!/!-!g; s!_!-!g')
 
 [[ -v THIS_COMMIT_TAG ]] && export MINA_COMMIT_TAG="${THIS_COMMIT_TAG}"
 
-echo "after commit tag"
+export GITTAG
+export GITHASH
+export GITBRANCH
+export MINA_DEB_VERSION
+export MINA_DOCKER_TAG
+export THIS_COMMIT_TAG
+export MINA_DEB_CODENAME=${MINA_DEB_CODENAME:=bullseye}
