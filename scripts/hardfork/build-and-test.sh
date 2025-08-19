f728712a7d376c7ef462731828094cbb1346a2eb
dkijania <dariusz@o1labs.org>
2025-08-19 23:07:02 +0200

    fix perms

--- scripts/hardfork/build-and-test.sh
+++ scripts/hardfork/build-and-test.sh
@@ -114,8 +114,8 @@ build_branch() {
       if [[ "$CONTEXT" == "local" ]]; then
         export BYPASS_OPAM_SWITCH_UPDATE=1
       fi
-      docker run --rm -v "$PWD:/workdir" -w /workdir "$DOCKER_TOOLCHAIN" sh -c \
-      "chown -R \"${USER}\" /workdir \
+      sudo chown -R opam .
+      docker run --rm -v "$PWD:/workdir" -w /workdir "$DOCKER_TOOLCHAIN" sh -c "
       && git config --global --add safe.directory /workdir \
       && eval $(opam env) \
       && make build \
