4adfb1a2cbe244e7814d39a914881a8610f9ebf3
glyh <lyhokia@gmail.com>
2025-08-27 15:17:56 +0800

    Add Hardfork mode cli arg

--- src/lib/mina_lib/config.ml
+++ src/lib/mina_lib/config.ml
@@ -67,5 +67,6 @@ type t =
   ; zkapp_cmd_limit : int option ref
   ; compile_config : Mina_compile_config.t
   ; itn_features : bool
+  ; hardfork_mode : Cli_lib.Arg_type.Hardfork_mode.t option [@default None]
   }
 [@@deriving make]
