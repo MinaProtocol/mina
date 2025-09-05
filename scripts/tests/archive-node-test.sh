7075a0ad46279bd540574f28827dd3d3544434be
Dariusz Kijania <dariusz@o1labs.org>
2025-09-05 16:09:20 +0200

    Merge pull request #17716 from MinaProtocol/dkijania/apt_update_limit
    replace apt-get update with specialized update script

--- scripts/tests/archive-node-test.sh
+++ scripts/tests/archive-node-test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -x
-# test archive node on known archive db
 
+buildkite/scripts/debian/update.sh --verbose
+
+apt-get install -y procps
+
+# test archive node on known archive db
 NETWORK_DATA_FOLDER=${NETWORK_DATA_FOLDER:-src/test/archive/sample_db}
 ARCHIVE_TEST_APP=${ARCHIVE_TEST_APP:-_build/default/src/test/archive/archive_node_tests/archive_node_tests.exe}
+
 # This env var is used in the test app
 # shellcheck disable=SC2034
 MINA_TEST_POSTGRES_URI=${POSTGRES_URI:-"postgres://postgres:postgres@localhost:5432"}
