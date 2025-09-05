480c1bfcf29284c7e244dbd412ddd6825cbba215
glyh <lyhokia@gmail.com>
2025-09-05 12:18:05 +0800

    Snark Work Lib > Metrics: remove legacy metric tracking

--- src/lib/snark_work_lib/metrics.ml
+++ src/lib/snark_work_lib/metrics.ml
@@ -16,44 +16,10 @@ module Transaction_type = struct
         `Fee_transfer
 end
 
-(* TODO: remove [emit_zkapp_metrics_legacy] on full delivery
-   of new snark coordinator <> worker interaction protocol. *)
-let emit_zkapp_metrics_legacy elapsed zkapp_command =
-  let init =
-    match
-      (Mina_base.Account_update.of_fee_payer
-         zkapp_command.Mina_base.Zkapp_command.Poly.fee_payer )
-        .authorization
-    with
-    | Proof _ ->
-        (1.0, 1.0)
-    | _ ->
-        (1.0, 0.0)
-  in
-  let parties_count, proof_parties_count =
-    Mina_base.Zkapp_command.Call_forest.fold zkapp_command.account_updates ~init
-      ~f:(fun (count, proof_updates_count) account_update ->
-        ( count +. 1.0
-        , if
-            Mina_base.Control.(
-              Tag.equal Proof
-                (tag account_update.Mina_base.Account_update.Poly.authorization))
-          then proof_updates_count +. 1.0
-          else proof_updates_count ) )
-  in
-  Mina_metrics.(
-    Cryptography.(
-      Counter.inc zkapp_transaction_length parties_count ;
-      Counter.inc zkapp_proof_updates proof_parties_count ;
-      Counter.inc snark_work_zkapp_base_time_sec (Time.Span.to_sec elapsed)))
-
-(* TODO: remove optional param [on_zkapp_command] on full delivery
-   of new snark coordinator <> worker interaction protocol. *)
 let emit_single_metrics ~logger ~(single_spec : _ Single_spec.Poly.t)
     ~data:
       ({ data = elapsed; _ } :
-        (Core.Time.Stable.Span.V1.t, _) Proof_carrying_data.t )
-    ?(on_zkapp_command = fun _ _ -> ()) () =
+        (Core.Time.Stable.Span.V1.t, _) Proof_carrying_data.t ) =
   match single_spec with
   | Single_spec.Poly.Merge (_, _, _) ->
       Perf_histograms.add_span ~name:"snark_worker_merge_time" elapsed ;
@@ -64,10 +30,8 @@ let emit_single_metrics ~logger ~(single_spec : _ Single_spec.Poly.t)
         ~metadata:[ ("elapsed", `Float (Time.Span.to_sec elapsed)) ]
   | Transition (_, ({ transaction; _ } : Transaction_witness.Stable.Latest.t))
     ->
-      ( match transaction with
-      | Mina_transaction.Transaction.Command
-          (Mina_base.User_command.Zkapp_command zkapp_command) ->
-          on_zkapp_command elapsed zkapp_command ;
+      ( match Transaction_type.of_transaction transaction with
+      | `Zkapp_command ->
           Perf_histograms.add_span ~name:"snark_worker_zkapp_transition_time"
             elapsed ;
           Mina_metrics.(
@@ -93,7 +57,7 @@ let emit_partitioned_metrics ~logger
   Mina_metrics.(Counter.inc_one Snark_work.completed_snark_work_received_rpc) ;
   match result_with_spec with
   | Partitioned_spec.Poly.Single { job; data; _ } ->
-      emit_single_metrics ~logger ~single_spec:job.spec ~data ()
+      emit_single_metrics ~logger ~single_spec:job.spec ~data
   | Sub_zkapp_command
       { job = { spec = Sub_zkapp_spec.Stable.Latest.Segment { spec; _ }; _ }
       ; data = Proof_carrying_data.{ data = elapsed; _ }
