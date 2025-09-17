64a93e07485d1f4e14eddd8b5808d0462300f485
glyh <lyhokia@gmail.com>
2025-09-17 10:38:31 +0800

    fix typo

--- src/lib/bootstrap_controller/bootstrap_controller.mli
+++ src/lib/bootstrap_controller/bootstrap_controller.mli
@@ -23,7 +23,7 @@ type Structured_log_events.t += Bootstrap_complete [@@deriving register_event]
     it would return a transition frontier with the root breadcrumb and a list
     of transitions collected during bootstrap.
 
-    Bootstrap controller would do the following steps to contrust the
+    Bootstrap controller would do the following steps to construct the
     transition frontier:
     1. Download the root snarked_ledger.
     2. Download the scan state and pending coinbases.
