16db348e43ff2e12d6b7299905fe5aed8c951fe1
Anaïs Querol <anais.querol@gmail.com>
2025-09-30 17:00:32 +0200

    backend: overwrite poseidon ffi wasm definitions for native variants

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -1,2 +1,16 @@
 // Provides: plonk_wasm
 var plonk_wasm = require('./plonk_wasm.js');
+var native = null;
+try { 
+    native = require('../native/plonk_napi.node'); 
+} catch (e) {
+    // native not available, keep WASM
+}
+
+// Overwrite only the functions that are already available in native
+if (native && native.caml_pasta_fp_poseidon_block_cipher) {
+  plonk_wasm.caml_pasta_fp_poseidon_block_cipher = native.caml_pasta_fp_poseidon_block_cipher;
+}
+if (native && native.caml_pasta_fq_poseidon_block_cipher) {
+  plonk_wasm.caml_pasta_fq_poseidon_block_cipher = native.caml_pasta_fq_poseidon_block_cipher;
+}
