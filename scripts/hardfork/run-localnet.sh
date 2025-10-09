ec976d433dc8eaa6f68ccbb661a72256a17d373e
georgeee <george.agapov@gmail.com>
2025-10-09 14:09:29 +0000

    Don't print run-localnet.sh unless needed

--- scripts/hardfork/run-localnet.sh
+++ scripts/hardfork/run-localnet.sh
@@ -37,9 +37,11 @@ GENESIS_LEDGER_DIR=${GENESIS_LEDGER_DIR:-}
 # Slot duration (a.k.a. block window duration), seconds
 SLOT=${SLOT:-30}
 
-echo "Creates a quick-epoch-turnaround configuration in localnet/ and launches two Mina nodes" >&2
-echo "Usage: $0 [-m|--mina $MINA_EXE] [-i|--tx-interval $TX_INTERVAL] [-d|--delay-min $DELAY_MIN] [-s|--slot $SLOT] [--develop] [-c|--config ./config.json] [--slot-tx-end 100] [--slot-chain-end 130] [--genesis-ledger-dir ./genesis]" >&2
-echo "Consider reading script's code for information on optional arguments" >&2
+usage() {
+  echo "Creates a quick-epoch-turnaround configuration in localnet/ and launches two Mina nodes" >&2
+  echo "Usage: $0 [-m|--mina $MINA_EXE] [-i|--tx-interval $TX_INTERVAL] [-d|--delay-min $DELAY_MIN] [-s|--slot $SLOT] [--develop] [-c|--config ./config.json] [--slot-tx-end 100] [--slot-chain-end 130] [--genesis-ledger-dir ./genesis]" >&2
+  echo "Consider reading script's code for information on optional arguments" >&2
+}
 
 ##########################################################
 # Parse arguments
@@ -47,6 +49,8 @@ echo "Consider reading script's code for information on optional arguments" >&2
 
 while [[ $# -gt 0 ]]; do
   case $1 in
+    -h|--help)
+      usage; exit 0 ;;
     -d|--delay-min)
       DELAY_MIN="$2"; shift; shift ;;
     -i|--tx-interval)
@@ -66,7 +70,9 @@ while [[ $# -gt 0 ]]; do
     --genesis-ledger-dir)
       GENESIS_LEDGER_DIR="$2"; shift; shift ;;
     -*)
-      echo "Unknown option $1"; exit 1 ;;
+      echo "Unknown option $1" >&2
+      usage
+      exit 1 ;;
     *)
       KEYS+=("$1") ; shift ;;
   esac
@@ -74,6 +80,7 @@ done
 
 if [[ "$CONF_SUFFIX" != "" ]] && [[ "$CUSTOM_CONF" != "" ]]; then
   echo "Can't use both --develop and --config options" >&2
+  usage
   exit 1
 fi
 
