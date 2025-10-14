ae2786a190b809480f315f2b7715c7c8b7442e8d
dkijania <dariusz@o1labs.org>
2025-10-14 14:22:27 +0200

    review remarks

--- frontend/ci-build-me/src/index.js
+++ frontend/ci-build-me/src/index.js
@@ -112,9 +112,9 @@ const handler = async (event, req) => {
     req.body.issue.pull_request.url &&
     req.body.comment.body == "!approved-for-mainnet"
   ) {
+     // TODO: Actually look at @MinaProtocol/stakeholder-reviewers team instead of hardcoding the users here
     if (
       req.body.sender.login == "amc-ie" ||
-      req.body.sender.login == "bkase" ||
       req.body.sender.login == "deepthiskumar" ||
       req.body.sender.login == "georgeee" ||
       req.body.sender.login == "dannywillems"
