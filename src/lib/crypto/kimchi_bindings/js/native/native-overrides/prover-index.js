0b59ce2c8c0682c70eb0631a31abb7c8c729de32
Florian <florian@technotro.com>
2025-10-15 21:52:58 +0700

    use temp copy for all

--- src/lib/crypto/kimchi_bindings/js/native/native-overrides/prover-index.js
+++ src/lib/crypto/kimchi_bindings/js/native/native-overrides/prover-index.js
@@ -3,13 +3,23 @@
 
 // Provides: caml_pasta_fp_plonk_index_max_degree
 // Requires: plonk_wasm
-var caml_pasta_fp_plonk_index_max_degree =
-  plonk_wasm.caml_pasta_fp_plonk_index_max_degree;
+function caml_pasta_fp_plonk_index_max_degree(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+  
+  return plonk_wasm.caml_pasta_fp_plonk_index_max_degree(index);
+}
 
 // Provides: caml_pasta_fp_plonk_index_public_inputs
 // Requires: plonk_wasm
-var caml_pasta_fp_plonk_index_public_inputs =
-  plonk_wasm.caml_pasta_fp_plonk_index_public_inputs;
+function caml_pasta_fp_plonk_index_public_inputs(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fp_plonk_index_public_inputs(index);
+}
 
 // Provides: caml_pasta_fp_plonk_index_domain_d1_size
 // Requires: plonk_wasm
@@ -23,14 +33,23 @@ function caml_pasta_fp_plonk_index_domain_d1_size(prover_index) {
 
 // Provides: caml_pasta_fp_plonk_index_domain_d4_size
 // Requires: plonk_wasm
-var caml_pasta_fp_plonk_index_domain_d4_size =
-  plonk_wasm.caml_pasta_fp_plonk_index_domain_d4_size;
+function caml_pasta_fp_plonk_index_domain_d4_size(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fp_plonk_index_domain_d4_size(index);
+}
 
 // Provides: caml_pasta_fp_plonk_index_domain_d8_size
 // Requires: plonk_wasm
-var caml_pasta_fp_plonk_index_domain_d8_size =
-  plonk_wasm.caml_pasta_fp_plonk_index_domain_d8_size;
+function caml_pasta_fp_plonk_index_domain_d8_size(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
 
+  return plonk_wasm.caml_pasta_fp_plonk_index_domain_d8_size(index);
+}
 
 
 //////// FQ ////////
@@ -38,31 +57,50 @@ var caml_pasta_fp_plonk_index_domain_d8_size =
 
 // Provides: caml_pasta_fq_plonk_index_max_degree
 // Requires: plonk_wasm
-var caml_pasta_fq_plonk_index_max_degree =
-  plonk_wasm.caml_pasta_fq_plonk_index_max_degree;
+function caml_pasta_fq_plonk_index_max_degree(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fq_plonk_index_max_degree(index);
+}
 
 // Provides: caml_pasta_fq_plonk_index_public_inputs
 // Requires: plonk_wasm
-var caml_pasta_fq_plonk_index_public_inputs =
-  plonk_wasm.caml_pasta_fq_plonk_index_public_inputs;
+function caml_pasta_fq_plonk_index_public_inputs(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fq_plonk_index_public_inputs(index);
+}
 
 // Provides: caml_pasta_fq_plonk_index_domain_d1_size
 // Requires: plonk_wasm
 function caml_pasta_fq_plonk_index_domain_d1_size(prover_index) {
-    var bytes = prover_index.serialize()
-    var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
-    // TODO: ^ remove the round trip when napi has direct access to the object
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
 
-    return plonk_wasm.caml_pasta_fq_plonk_index_domain_d1_size(index);
+  return plonk_wasm.caml_pasta_fq_plonk_index_domain_d1_size(index);
 }
-
 
 // Provides: caml_pasta_fq_plonk_index_domain_d4_size
 // Requires: plonk_wasm
-var caml_pasta_fq_plonk_index_domain_d4_size =
-  plonk_wasm.caml_pasta_fq_plonk_index_domain_d4_size;
+function caml_pasta_fq_plonk_index_domain_d4_size(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fq_plonk_index_domain_d4_size(index);
+}
 
 // Provides: caml_pasta_fq_plonk_index_domain_d8_size
 // Requires: plonk_wasm
-var caml_pasta_fq_plonk_index_domain_d8_size =
-  plonk_wasm.caml_pasta_fq_plonk_index_domain_d8_size;
+function caml_pasta_fq_plonk_index_domain_d8_size(prover_index) {
+  var bytes = prover_index.serialize()
+  var index = plonk_wasm.prover_index_fq_from_bytes(bytes);
+  // TODO: ^ remove the round trip when napi has direct access to the object
+
+  return plonk_wasm.caml_pasta_fq_plonk_index_domain_d8_size(index);
+}
