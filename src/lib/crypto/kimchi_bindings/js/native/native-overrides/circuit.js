5178750b9106cb3da8f11f796c00e3f3d90eb021
Florian <florian@technotro.com>
2025-10-15 21:29:34 +0700

    fix function name

--- src/lib/crypto/kimchi_bindings/js/native/native-overrides/circuit.js
+++ src/lib/crypto/kimchi_bindings/js/native/native-overrides/circuit.js
@@ -5,7 +5,7 @@
 // Requires: plonk_wasm
 function prover_to_json(prover_index) {
   var bytes = prover_index.serialize()
-  var index = plonk_wasm.prover_index_from_bytes(bytes);
+  var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
   // TODO: ^ remove the round trip when napi has direct access to the object
   
   return plonk_wasm.prover_to_json(index);
