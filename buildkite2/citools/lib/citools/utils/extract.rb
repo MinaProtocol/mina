fad28ef4e1da938b3e7bb127fe07335c327e8d78
glyh <lyhokia@gmail.com>
2025-10-16 13:41:14 +0800

    Implement extraction helper

---
+++ buildkite2/citools/lib/citools/utils/extract.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Citools
+  module Utils
+    # Utils extracting zips
+    module Extract
+      def tar_gz(tar_path, target_dir)
+        Zlib::GzipReader.open(tar_path) do |gz|
+          Gem::Package::TarReader.new(gz) do |tar|
+            tar.each do |entry|
+              dest = File.join(target_dir, entry.full_name)
+              if entry.directory?
+                FileUtils.mkdir_p(dest)
+              else
+                FileUtils.mkdir_p(File.dirname(dest))
+                File.write(dest, entry.read, mode: "wb")
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
