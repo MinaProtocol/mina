c7ef3b4f425ac843c99280aac4850dd4d9323016
dkijania <dariusz@o1labs.org>
2025-10-20 12:18:25 +0200

    fix bash checks

--- scripts/rocksdb-compatibility/install-rocksdb.sh
+++ scripts/rocksdb-compatibility/install-rocksdb.sh
@@ -4,7 +4,9 @@ set -euox pipefail
 
 ROCKSDB_VERSION=10.5.1
 
-ROCKSDB_SOURCE=`mktemp -d --tmpdir rocksdb-$ROCKSDB_VERSION.XXXXXX`
+ROCKSDB_SOURCE=$(mktemp -d --tmpdir rocksdb-$ROCKSDB_VERSION.XXXXXX)
+
+# shellcheck disable=SC2064
 trap "rm -rf $ROCKSDB_SOURCE" EXIT
 
 curl -L https://github.com/facebook/rocksdb/archive/refs/tags/v${ROCKSDB_VERSION}.tar.gz | tar xz -C $ROCKSDB_SOURCE
@@ -16,4 +18,4 @@ cd $ROCKSDB_SOURCE/rocksdb-${ROCKSDB_VERSION}
 # util/compression.cc:684:40: error: unused parameter ‘args’ [-Werror=unused-parameter]
 # 684 |   Status ExtractUncompressedSize(Args& args) override {
 #     |                                  ~~~~~~^~~~
-EXTRA_CXXFLAGS="-Wno-unused-parameter" make -j$(nproc) install
+EXTRA_CXXFLAGS="-Wno-unused-parameter" make -j"$(nproc)" install
