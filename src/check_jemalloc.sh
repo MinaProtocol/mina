a148ea77d726ce4285005828f36e4d0dfcd73997
glyh <lyhokia@gmail.com>
2025-10-21 23:00:15 +0800

    Rewrite jemalloc check with pkg-config

--- src/check_jemalloc.sh
+++ src/check_jemalloc.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
 printf 'Checking if jemalloc is available... ' 1>&2
-if [ -z "${CC-}" ]; then
-  if command -v gcc >/dev/null 2>&1; then
-    CC=gcc
-  else
-    CC=clang
-  fi
-fi
 
-# Try to link against jemalloc
-if echo 'int main(){}' | "$CC" -x c - -ljemalloc -o /dev/null >/dev/null 2>&1; then
+if pkg-config --exists jemalloc; then
   echo 'Yes, using jemalloc as an allocator' 1>&2
-  printf jemalloc
+  # Print the linker flag
+  printf "jemalloc"
 else
   echo 'No, using the default allocator from libc' 1>&2
-  printf c
+  printf "c"
 fi
