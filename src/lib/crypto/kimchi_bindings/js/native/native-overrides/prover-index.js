6d67e95d25a20dd087a55f8fa3d0be8472dedbb4
querolita <anais.querol@gmail.com>
2025-10-28 12:15:02 +0100

    jsoo: move prover index read/write to native overrides

--- src/lib/crypto/kimchi_bindings/js/native/native-overrides/prover-index.js
+++ src/lib/crypto/kimchi_bindings/js/native/native-overrides/prover-index.js
@@ -1,5 +1,4 @@
-/* global plonk_wasm, tsRustConversionNative */
-
+/* global plonk_wasm, tsRustConversionNative, caml_jsstring_of_string */
 
 // Provides: caml_pasta_fp_plonk_index_max_degree
 // Requires: plonk_wasm
@@ -146,6 +145,57 @@ var caml_pasta_fp_plonk_index_create = function (
   return free_on_finalize(wasm_t);
 };
 
+// Provides: caml_pasta_fp_plonk_index_create_bytecode
+// Requires: caml_pasta_fp_plonk_index_create
+var caml_pasta_fp_plonk_index_create_bytecode = function (
+  gates,
+  public_inputs,
+  caml_lookup_tables,
+  caml_runtime_table_cfgs,
+  prev_challenges,
+  urs,
+  lazy_mode
+) {
+  return caml_pasta_fp_plonk_index_create(
+    gates,
+    public_inputs,
+    caml_lookup_tables,
+    caml_runtime_table_cfgs,
+    prev_challenges,
+    urs,
+    lazy_mode
+  );
+};
+
+// Provides: caml_pasta_fp_plonk_index_read
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_pasta_fp_plonk_index_read = function (offset, urs, path) {
+  if (offset === 0) {
+    offset = undefined;
+  } else {
+    offset = offset[1];
+  }
+  return plonk_wasm.caml_pasta_fp_plonk_index_read(
+    offset,
+    urs,
+    caml_jsstring_of_string(path)
+  );
+};
+
+// Provides: caml_pasta_fp_plonk_index_write
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_pasta_fp_plonk_index_write = function (append, t, path) {
+  if (append === 0) {
+    append = undefined;
+  } else {
+    append = append[1];
+  }
+  return plonk_wasm.caml_pasta_fp_plonk_index_write(
+    append,
+    t,
+    caml_jsstring_of_string(path)
+  );
+};
 
 // Provides: caml_pasta_fq_plonk_index_create
 // Requires: plonk_wasm, free_on_finalize, tsRustConversion
@@ -187,3 +237,55 @@ var caml_pasta_fq_plonk_index_create = function (
 
   return free_on_finalize(wasm_t);
 }; 
+
+// Provides: caml_pasta_fq_plonk_index_create_bytecode
+// Requires: caml_pasta_fq_plonk_index_create
+var caml_pasta_fq_plonk_index_create_bytecode = function (
+  gates,
+  public_inputs,
+  caml_lookup_tables,
+  caml_runtime_table_cfgs,
+  prev_challenges,
+  urs,
+  lazy_mode
+) {
+  return caml_pasta_fq_plonk_index_create(
+    gates,
+    public_inputs,
+    caml_lookup_tables,
+    caml_runtime_table_cfgs,
+    prev_challenges,
+    urs,
+    lazy_mode
+  );
+};
+
+// Provides: caml_pasta_fq_plonk_index_read
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_pasta_fq_plonk_index_read = function (offset, urs, path) {
+  if (offset === 0) {
+    offset = undefined;
+  } else {
+    offset = offset[1];
+  }
+  return plonk_wasm.caml_pasta_fq_plonk_index_read(
+    offset,
+    urs,
+    caml_jsstring_of_string(path)
+  );
+};
+
+// Provides: caml_pasta_fq_plonk_index_write
+// Requires: plonk_wasm, caml_jsstring_of_string
+var caml_pasta_fq_plonk_index_write = function (append, t, path) {
+  if (append === 0) {
+    append = undefined;
+  } else {
+    append = append[1];
+  }
+  return plonk_wasm.caml_pasta_fq_plonk_index_write(
+    append,
+    t,
+    caml_jsstring_of_string(path)
+  );
+};
