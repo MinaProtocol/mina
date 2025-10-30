683b4aa20e88b5459a1ca10c253524f72ad2956e
dkijania <dariusz@o1labs.org>
2025-10-30 21:20:25 +0100

    build mina-test-signer

--- buildkite/scripts/single-node-tests.sh
+++ buildkite/scripts/single-node-tests.sh
@@ -13,6 +13,14 @@ source buildkite/scripts/debian/update.sh --verbose
 
 source buildkite/scripts/debian/install.sh "mina-test-suite,mina-berkeley-lightnet" 1
 
+# --- Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+sudo apt-get install --no-install-recommends -y nodejs=18.*
+rm -rf /var/lib/apt/lists/*
+
+# --- Copy and install mina-test-signer
+npm install -g ./scripts/tests/mina-signer
+
 export MINA_LIBP2P_PASS="naughty blue worm"
 export MINA_PRIVKEY_PASS="naughty blue worm"
 
