ab35872d241eb2d5538fe9247a5ff616de8c5c89
Florian <florian@technotro.com>
2025-10-30 21:16:21 +0700

    rm logging

--- src/lib/crypto/kimchi_bindings/js/native/native-overrides/oracles.js
+++ src/lib/crypto/kimchi_bindings/js/native/native-overrides/oracles.js
@@ -5,8 +5,6 @@
 // Requires: plonk_wasm, tsRustConversionNative
 function caml_pasta_fq_poseidon_block_cipher(_fake_params, fq_vector) {
 
-    console.log("overriding the old wasm caml_pasta_fq_poseidon_block_cipher, now using the native one")
-
   // 1. get permuted field vector from rust
   var wasm_flat_vector = plonk_wasm.caml_pasta_fq_poseidon_block_cipher(
     tsRustConversionNative.fq.vectorToRust(fq_vector)
@@ -22,8 +20,6 @@ function caml_pasta_fq_poseidon_block_cipher(_fake_params, fq_vector) {
 // Provides: caml_pasta_fp_poseidon_block_cipher
 // Requires: plonk_wasm, tsRustConversionNative
 function caml_pasta_fp_poseidon_block_cipher(_fake_params, fp_vector) {
-
-    console.log("overriding the old wasm caml_pasta_fp_poseidon_block_cipher, now using the native one")
 
 
   // 1. get permuted field vector from rust
