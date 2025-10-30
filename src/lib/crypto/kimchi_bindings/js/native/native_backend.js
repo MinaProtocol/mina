4c083b0c9411f3a7c04da690b727bce19a427d35
leon chou <leon@o1labs.org>
2025-10-30 17:20:50 -0400

    fixed build and overrides

--- src/lib/crypto/kimchi_bindings/js/native/native_backend.js
+++ src/lib/crypto/kimchi_bindings/js/native/native_backend.js
@@ -1,4 +1,2 @@
 // Provides: plonk_napi
-var plonk_napi = require('@o1labs/o1js-darwin-arm64');
-
-module.exports = plonk_napi;
+var plonk_napi = require('@o1js/native-' + process.platform + '-' + process.arch)
