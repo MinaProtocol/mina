4d28733ed35ff10acb009af203ecbf91532d5fd5
leon chou <leon@o1labs.org>
2025-10-31 16:04:08 -0400

    rework native bindings

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -5,14 +5,31 @@ var plonk_wasm = (function() {
   try {
     var native =  require('@o1js/native-' + process.platform + '-' + process.arch)
 
-    wasm["caml_pasta_fp_poseidon_block_cipher"] = native["caml_pasta_fp_poseidon_block_cipher"]
-    wasm["caml_pasta_fq_poseidon_block_cipher"] = native["caml_pasta_fq_poseidon_block_cipher"]
+    // THIS IS A RUNTIME OVERRIDE
+    // YOU HAVE TO RUN IT TO SEE IF IT BREAKS
+    // IT WON'T CRASH UNLESS O1JS_REQUIRE_NATIVE_BINDINGS
+    // IS SET
+    var overrides = [
+      "prover_to_json",
+      "prover_index_from_bytes",
+      "prover_index_to_bytes",
+      "caml_pasta_fp_poseidon_block_cipher",
+      "caml_pasta_fq_poseidon_block_cipher",
+      "caml_pasta_fp_plonk_proof_create",
+    ]
+
+    overrides.forEach(function (override) {
+      wasm[override] = native[override]
+    })
+
+    wasm.native = true;
   } catch (e) {
-    console.error(e)
-    console.log("native didn't load")
-    process.exit(1);
+    if (process.env.O1JS_REQUIRE_NATIVE_BINDINGS) {
+      console.error(e)
+      console.log("native didn't load")
+      process.exit(1);
+    }
   }
 
   return wasm
 })()
-
