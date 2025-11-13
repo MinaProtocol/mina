7e2caa3f914ab604c672eb45b9a7ff40feb567fd
Florian <florian@technotro.com>
2025-11-13 19:46:29 +0700

    add custom overwrite log

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -92,7 +92,10 @@ var plonk_wasm = (function() {
     ];
 
     overrides.forEach(function (override) {
-      wasm[override] = native[override]
+      wasm[override] = function (x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12) {
+        console.log("calling native override for " + override);
+        return native[override](x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12);
+      }
     })
     wasm.native = true;
   } catch (e) {
