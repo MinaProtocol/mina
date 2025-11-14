a7e7ab7957d2cc9d26c4e0a2cc79b2d34e7d9e9b
Florian <florian@technotro.com>
2025-11-14 10:38:01 +0700

    wip dont use napi types just yet

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -20,8 +20,6 @@ var plonk_wasm = (function() {
       "caml_pasta_fp_plonk_proof_create",
       "caml_pasta_fp_plonk_verifier_index_shifts",
       "caml_pasta_fq_plonk_verifier_index_shifts",
-      "WasmFpPolyComm",
-      "WasmFqPolyComm",
       "caml_pasta_fp_plonk_gate_vector_create",
       "caml_pasta_fq_plonk_gate_vector_create",
       "caml_pasta_fp_plonk_gate_vector_add",
@@ -84,18 +82,18 @@ var plonk_wasm = (function() {
       "caml_fq_srs_batch_accumulator_check",
       "caml_fq_srs_batch_accumulator_generate",
       "caml_fq_srs_h",
-      "WasmFpPolyComm",
-      "WasmFqPolyComm",
-      "WasmGPallas",
-      "WasmGVesta",
+/*       "WasmFpPolyComm",
+      "WasmFqPolyComm", */
+/*       "WasmGPallas",
+      "WasmGVesta", */
       "WasmPastaFp",
       "WasmPastaFq",
     ];
 
     overrides.forEach(function (override) {
-      wasm[override] = function(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10) {
+      wasm[override] = function(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12) {
         console.log("calling native override:", override);
-        return native[override](x1, x2, x3, x4, x5, x6, x7, x8, x9, x10);
+        return native[override](x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12);
       }
     })
     wasm.native = true;
