857e2a8402e67d4a3931bdd60043123890046df7
querolita <anais.querol@gmail.com>
2025-11-17 18:01:03 +0100

    backend: don't include fq yet

--- src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
+++ src/lib/crypto/kimchi_bindings/js/node_js/node_backend.js
@@ -18,7 +18,6 @@ var plonk_wasm = (function() {
       "caml_pasta_fp_poseidon_block_cipher",
       "caml_pasta_fq_poseidon_block_cipher",
       "caml_pasta_fp_plonk_proof_create",
-      "caml_pasta_fq_plonk_proof_create",
       "caml_pasta_fp_plonk_verifier_index_shifts",
       "caml_pasta_fq_plonk_verifier_index_shifts",
       "caml_pasta_fp_plonk_gate_vector_create",
