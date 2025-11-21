518d55e508085e92d3d61fb35106c8ee3d62ecf4
georgeee <george.agapov@gmail.com>
2025-11-21 09:56:53 +0100

    Remove unused base_jobs_on_latest_tree
    (cherry picked from commit fb1b96c2b5950de7be9b4f79c9e72c5b8b6d56ee)

--- src/lib/parallel_scan/parallel_scan.ml
+++ src/lib/parallel_scan/parallel_scan.ml
@@ -1523,13 +1523,6 @@ let last_emitted_value t = t.acc
 
 let current_job_sequence_number t = t.curr_job_seq_no
 
-let base_jobs_on_latest_tree t =
-  let depth = Int.ceil_log2 t.max_base_jobs in
-  List.filter_map
-    (Tree.jobs_on_level ~depth ~level:depth
-       (Mina_stdlib.Nonempty_list.head t.trees) )
-    ~f:(fun job -> match job with Base d -> Some d | Merge _ -> None)
-
 (* 0-based indexing, so 0 indicates next-to-latest tree *)
 let base_jobs_on_earlier_tree t ~index =
   let depth = Int.ceil_log2 t.max_base_jobs in
