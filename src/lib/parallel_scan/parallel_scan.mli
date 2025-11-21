3d7977744e9cd04f325733fcfc326f547ce2d8f6
georgeee <george.agapov@gmail.com>
2025-11-21 09:56:53 +0100

    Remove unused base_jobs_on_latest_tree
    (cherry picked from commit fb1b96c2b5950de7be9b4f79c9e72c5b8b6d56ee)

--- src/lib/parallel_scan/parallel_scan.mli
+++ src/lib/parallel_scan/parallel_scan.mli
@@ -294,11 +294,6 @@ val view_jobs_with_position :
   -> ('base -> 'c)
   -> 'c Job_view.t list list
 
-(** All the base jobs that are part of the latest tree being filled
- * i.e., does not include base jobs that are part of previous trees not
- * promoted to the merge jobs yet*)
-val base_jobs_on_latest_tree : ('merge, 'base) State.t -> 'base list
-
 (** All the base jobs that are part of a tree before the latest tree
     index is 0-based, 0 is the next-to-latest tree
 *)
