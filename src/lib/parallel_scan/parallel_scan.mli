b4f02c9602c6ac584b54da58da85e556650605be
georgeee <george.agapov@gmail.com>
2025-11-21 09:57:42 +0100

    Remove unused base_jobs_on_earlier_tree

--- src/lib/parallel_scan/parallel_scan.mli
+++ src/lib/parallel_scan/parallel_scan.mli
@@ -288,12 +288,6 @@ val view_jobs_with_position :
   -> ('base -> 'c)
   -> 'c Job_view.t list list
 
-(** All the base jobs that are part of a tree before the latest tree
-    index is 0-based, 0 is the next-to-latest tree
-*)
-val base_jobs_on_earlier_tree :
-  ('merge, 'base) State.t -> index:int -> 'base list
-
 (** Returns true only if the next 'd that could be enqueued is
 on a new tree*)
 val next_on_new_tree : ('merge, 'base) State.t -> bool
