cb752459651527c0b67e13c30fcba2be3edbd297
dkijania <dariusz@o1labs.org>
2025-11-25 15:39:16 +0100

    exclude pxx and take only stem from path

--- buildkite/scripts/apps/write_to_cache.sh
+++ buildkite/scripts/apps/write_to_cache.sh
@@ -3,7 +3,15 @@
 CODENAME=$1
 
 find _build -type f -name "*.exe" | while read -r entry; do
-  # Transform entry: add 'mina' at beginning and convert _ to -
-  new_entry="mina-${entry//_/-}"
+  # Exclude files ending with ppx.exe
+  if [[ "$entry" == *ppx.exe ]]; then
+    continue
+  fi
+
+    # Get stem from filename (remove path and extension)
+  filename=$(basename "$entry")
+  stem="${filename%.exe}"
+  # Transform stem: add 'mina' at beginning and convert _ to -
+  new_entry="mina-${stem//_/-}"
   ./buildkite/scripts/cache/manager.sh write "$new_entry" "apps/${CODENAME}/"
 done
