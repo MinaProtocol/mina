2950886eec00632519d1be413b232de43f8f08ce
georgeee <george.agapov@gmail.com>
2025-11-26 19:30:37 +0100

    Add materializing tag type for block

--- src/lib/transition_frontier/frontier_base/network_types.ml
+++ src/lib/transition_frontier/frontier_base/network_types.ml
@@ -1,6 +1,32 @@
 open Core_kernel
 open Mina_base
 
+module Make (Data : Binable.S) = struct
+  type t = Tag of Data.t State_hash.File_storage.tag | Data of Data.t
+
+  type data_tag = Data.t State_hash.File_storage.tag
+
+  let to_latest = Fn.id
+
+  module Arg = struct
+    type nonrec t = t
+
+    let to_binable = function
+      | Tag x ->
+          (* TODO This code deserializes the data stored in the file
+             and serializes it back. This is a bad thing to do.
+             But in future we will have a special communication
+             protocol between libp2p helper and daemon to push file reading
+             to the helper, and then we won't have deserialization anymore.
+          *)
+          State_hash.File_storage.read (module Data) x |> Or_error.ok_exn
+      | Data x ->
+          x
+
+    let of_binable x = Data x
+  end
+end
+
 module Staged_ledger_aux_and_pending_coinbases = struct
   module Data = struct
     [%%versioned
@@ -20,42 +46,35 @@ module Staged_ledger_aux_and_pending_coinbases = struct
     end]
   end
 
-  type data_tag = Data.Stable.Latest.t State_hash.File_storage.tag
+  module M = Make (Data.Stable.V1)
+
+  type data_tag = M.data_tag
 
   [%%versioned_binable
   module Stable = struct
     module V1 = struct
-      module T = struct
-        type t =
-          | Tag of Data.Stable.Latest.t State_hash.File_storage.tag
-          | Data of Data.Stable.Latest.t
+      type t = M.t
 
-        let to_latest = Fn.id
-      end
+      let to_latest = M.to_latest
 
-      include T
+      include Binable.Of_binable_without_uuid (Data.Stable.V1) (M.Arg)
+    end
+  end]
+end
 
-      include
-        Binable.Of_binable_without_uuid
-          (Data.Stable.V1)
-          (struct
-            type t = T.t
+module Block = struct
+  module M = Make (Mina_block.Stable.V2)
 
-            let to_binable = function
-              | Tag x ->
-                  (* TODO This code deserializes the data stored in the file
-                     and serializes it back. This is a bad thing to do.
-                     But in future we will have a special communication
-                     protocol between libp2p helper and daemon to push file reading
-                     to the helper, and then we won't have deserialization anymore.
-                  *)
-                  State_hash.File_storage.read (module Data.Stable.Latest) x
-                  |> Or_error.ok_exn
-              | Data x ->
-                  x
+  type data_tag = M.data_tag
 
-            let of_binable x = Data x
-          end)
+  [%%versioned_binable
+  module Stable = struct
+    module V1 = struct
+      type t = M.t
+
+      let to_latest = M.to_latest
+
+      include Binable.Of_binable_without_uuid (Mina_block.Stable.V2) (M.Arg)
     end
   end]
 end
