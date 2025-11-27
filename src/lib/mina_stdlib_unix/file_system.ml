a3d1aaecbbf4af8e027715625c6a130c4004a88f
georgeee <george.agapov@gmail.com>
2025-11-27 23:51:18 +0100

    Print RAM used after frontier loading
    (cherry picked from commit cd722218ef96d23b0aef98b4fa44b3364f8469ab)

--- src/lib/mina_stdlib_unix/file_system.ml
+++ src/lib/mina_stdlib_unix/file_system.ml
@@ -78,3 +78,26 @@ let create_dir ?(clear_if_exists = false) dir =
       if clear_if_exists then clear_dir dir else return ()
   | _ ->
       return (Core.Unix.mkdir_p dir)
+
+(* Read RSS (Resident Set Size) from /proc/<pid>/status *)
+let read_rss_kb pid_opt =
+  try
+    let proc_file =
+      match pid_opt with
+      | None ->
+          "/proc/self/status"
+      | Some pid ->
+          Printf.sprintf "/proc/%d/status" (Pid.to_int pid)
+    in
+    let ic = In_channel.create proc_file in
+    let rec find_vmrss () =
+      let%bind.Option line = In_channel.input_line ic in
+      match Option.try_with (fun () -> Scanf.sscanf line "VmRSS: %f" Fn.id) with
+      | None ->
+          find_vmrss ()
+      | Some kb ->
+          Some kb
+    in
+    let result = find_vmrss () in
+    In_channel.close ic ; result
+  with _ -> None
