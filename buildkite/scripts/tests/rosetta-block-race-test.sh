07c77155bf9afc9a04284b7360c283e5a27c59d6
glyh <lyhokia@gmail.com>
2025-12-02 12:18:53 +0800

    rosetta block race test: combine curl + tar to single pipeline

--- buildkite/scripts/tests/rosetta-block-race-test.sh
+++ buildkite/scripts/tests/rosetta-block-race-test.sh
@@ -4,16 +4,10 @@
 # So env vars from docker buildkite step are passed down to the script
 
 LEDGER_URL="https://storage.googleapis.com/o1labs-ci-test-data/ledgers/single-bp-ledger.tar"
-LEDGER_ARCHIVE="ledger.tar"
 LEDGER_DIR="ledger"
 
-echo "Downloading ledger archive from $LEDGER_URL ..."
-curl -L -o "$LEDGER_ARCHIVE" "$LEDGER_URL"
-
-mkdir -p "$LEDGER_DIR"
-
-echo "  Extracting ledger archive to $LEDGER_DIR ..."
-tar -xf "$LEDGER_ARCHIVE" -C "$LEDGER_DIR"
+echo "Downloading&extracting ledger archive from $LEDGER_URL ..."
+curl -L "$LEDGER_URL" | tar -x -C "$LEDGER_DIR"
 
 chmod 700 "$LEDGER_DIR"
 
