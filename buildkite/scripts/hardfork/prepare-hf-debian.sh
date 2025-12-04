3b97ac0df866e78cb8a60de5f4353cf43278eee9
dkijania <dariusz@o1labs.org>
2025-12-04 10:59:31 +0100

    ls -al

--- buildkite/scripts/hardfork/prepare-hf-debian.sh
+++ buildkite/scripts/hardfork/prepare-hf-debian.sh
@@ -4,10 +4,12 @@ set -eox pipefail
 
 ./buildkite/scripts/cache/manager.sh read --root ${CACHED_BUILDKITE_BUILD_ID} debians/${CODENAME}/mina-${NETWORK_NAME}* ./
 
-./buildkite/scripts/cache/manager.sh read hardfork/ledgers/*.tar.gz hardfork_ledgers/
+./buildkite/scripts/cache/manager.sh read hardfork/ledgers/*.tar.gz ./hardfork_ledgers/
 ./buildkite/scripts/cache/manager.sh read hardfork/new_config.json .
 
 MINA_DEB_FILE=$(ls mina-${NETWORK_NAME}_*.deb | head -n 1)
+
+ls -al
 
 ./scripts/hardfork/convert-debian-to-hf.sh -d "$MINA_DEB_FILE" -c "./new_config.json" -l "./hardfork_ledgers/"
 
