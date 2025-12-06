2e041028c7874a043768fb02224518957fae2ca0
glyh <lyhokia@gmail.com>
2025-12-06 16:36:35 +0800

    port perf histogram to core.v0.16

--- src/lib/perf_histograms/rpc.ml
+++ src/lib/perf_histograms/rpc.ml
@@ -5,25 +5,26 @@ let decorate_dispatch ~name (dispatch : ('q, 'r) Intf.dispatch) :
     ('q, 'r) Intf.dispatch =
  fun conn q ->
   let open Deferred.Or_error.Let_syntax in
-  let start = Time.now () in
+  let start = Time_float.now () in
   let%map r = dispatch conn q in
-  let span = Time.diff (Time.now ()) start in
+  let span = Time_float.diff (Time_float.now ()) start in
   Perf_histograms0.add_span ~name:(sprintf "rpc_dispatch_%s" name) span ;
   Mina_metrics.(
     Network.Rpc_latency_histogram.observe Network.rpc_latency_ms_summary
-      (Time.Span.to_ms span)) ;
-  Mina_metrics.(Gauge.set (Network.rpc_latency_ms ~name) (Time.Span.to_ms span)) ;
+      (Time_float.Span.to_ms span) ) ;
+  Mina_metrics.(
+    Gauge.set (Network.rpc_latency_ms ~name) (Time_float.Span.to_ms span) ) ;
   r
 
 let decorate_impl ~name (impl : ('q, 'r, 'state) Intf.impl) :
     ('q, 'r, 'state) Intf.impl =
  fun state ~version q ->
   let open Deferred.Let_syntax in
-  let start = Time.now () in
+  let start = Time_float.now () in
   let%map r = impl state ~version q in
   Perf_histograms0.add_span
     ~name:(sprintf "rpc_impl_%s" name)
-    (Time.diff (Time.now ()) start) ;
+    (Time_float.diff (Time_float.now ()) start) ;
   r
 
 module Plain = struct
