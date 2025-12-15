b121d9ec7fc5c34214224db990074b3f6d616b5f
glyh <lyhokia@gmail.com>
2025-12-15 17:41:45 +0800

    harden connect to network test, and test for error code 500

--- buildkite/scripts/connect/connect-to-network.sh
+++ buildkite/scripts/connect/connect-to-network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -eux -o pipefail
 
 if [[ $# -ne 4 ]]; then
     echo "Usage: $0 '<mina-debian-network>''<testnet-name>' '<wait-between-polling-graphql>''<wait-after-final-check>' "
@@ -62,11 +62,16 @@ else
 fi
 
 # Check network id
-NETWORK_ID=$(curl 'http://localhost:3085/graphql' \
+NETWORK_ID=$(curl -f --show-error 'http://localhost:3085/graphql' \
    -H 'accept: application/json' \
    -H 'content-type: application/json' \
    --data-raw '{"query":"query MyQuery {\n  networkID\n}\n","variables":null,"operationName":"MyQuery"}' \
-  | jq -r .data.networkID)
+  | jq -r '.data.networkID')
+
+if [ $? -ne 0 ]; then
+    echo "GraphQL query 'networkID' failed!" >&2
+    exit 1
+fi
 
 EXPECTED_NETWORK=mina:$NETWORK_NAME
 
@@ -77,3 +82,51 @@ else
     exit 1
 fi
 
+# Check bestTip 
+BEST_TIP_STATE_HASH=$(curl -f --show-error 'http://localhost:3085/graphql' \
+   -H 'accept: application/json' \
+   -H 'content-type: application/json' \
+   --data-raw '{"query":"query MyQuery {\n bestChain(maxLength: 1) {\n stateHash \n}\n}\n","variables":null,"operationName":"MyQuery"}' \
+  | jq -r '.data.bestChain[0].stateHash')
+
+if [ $? -ne 0 ]; then
+    echo "GraphQL query 'bestChain' failed!" >&2
+    exit 1
+fi
+
+echo "Found best tip state hash ${BEST_TIP_STATE_HASH}"
+
+SNARKED_LEDGER_HASH=$(curl -f --show-error 'http://localhost:3085/graphql' \
+   -H 'accept: application/json' \
+   -H 'content-type: application/json' \
+   --data-raw '{
+     "query": "query MyBlock($stateHash: String!) { block(stateHash: $stateHash) { protocolState { blockchainState { snarkedLedgerHash } } } }",
+     "variables": { "stateHash": "'"${BEST_TIP_STATE_HASH}"'" },
+     "operationName": "MyBlock"
+   }' \
+  | jq -r '.data.block.protocolState.blockchainState.snarkedLedgerHash')
+
+if [ $? -ne 0 ]; then
+    echo "GraphQL query 'block' failed!" >&2
+    exit 1
+fi
+
+echo "Best tip's Snarked Ledger Hash: ${SNARKED_LEDGER_HASH}"
+
+ANCIENT_STATE_HASH_DEVNET="3NL5hv4ysELXF2Tg5UZDMgBFcQLTM1tGtRRzMhgyLa5EzvbeDQhq"
+HTTP_CODE=$(curl -o /dev/null -s -w "%{http_code}" 'http://localhost:3085/graphql' \
+   -H 'accept: application/json' \
+   -H 'content-type: application/json' \
+   --data-raw '{
+     "query": "query MyBlock($stateHash: String!) { block(stateHash: $stateHash) { protocolState { blockchainState { snarkedLedgerHash } } } }",
+     "variables": { "stateHash": "'"${ANCIENT_STATE_HASH_DEVNET}"'" },
+     "operationName": "MyBlock"
+     }' || true)
+  
+
+if [[ "$HTTP_CODE" == "500" ]]; then
+    echo "OK: Querying ancient state hash on node, got HTTP CODE 500"
+else
+    echo "FAIL: Querying ancient state hash on node, got HTTP CODE ${HTTP_CODE}, expected 500"
+    exit 1
+fi
