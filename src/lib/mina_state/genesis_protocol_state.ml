a0677da857045559f1a57080e7dfb63bc0b32bed
martyall <martyall@protonmail.com>
2025-12-17 08:55:53 -0800

    Fix mina_state to use hashed variants

--- src/lib/mina_state/genesis_protocol_state.ml
+++ src/lib/mina_state/genesis_protocol_state.ml
@@ -2,15 +2,14 @@ open Core_kernel
 
 let t ~genesis_ledger ~genesis_epoch_data ~constraint_constants
     ~consensus_constants ~genesis_body_reference =
-  let genesis_ledger_forced =
-    Lazy.force @@ Genesis_ledger.Packed.t genesis_ledger
-  in
-  let genesis_ledger_hash =
-    Mina_ledger.Ledger.merkle_root genesis_ledger_forced
-  in
+  let genesis_ledger = Consensus.Genesis_data.Ledger.to_hashed genesis_ledger in
   let protocol_constants =
     Consensus.Constants.to_protocol_constants consensus_constants
   in
+  let genesis_epoch_data =
+    Consensus.Genesis_data.Epoch.to_hashed genesis_epoch_data
+  in
+
   let negative_one_protocol_state_hash =
     Protocol_state.(
       hashes
@@ -32,7 +31,9 @@ let t ~genesis_ledger ~genesis_epoch_data ~constraint_constants
            ~f:(fun { state_hash; _ } -> state_hash) )
       ~blockchain_state:
         (Blockchain_state.genesis ~constraint_constants ~consensus_constants
-           ~genesis_ledger_hash ~genesis_body_reference )
+           ~genesis_ledger_hash:
+             (Consensus.Genesis_data.Hashed.hash genesis_ledger)
+           ~genesis_body_reference )
       ~consensus_state:genesis_consensus_state ~constants:protocol_constants
   in
   With_hash.of_data ~hash_data:Protocol_state.hashes state
