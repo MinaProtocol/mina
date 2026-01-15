98df41456494ca9b7e95300d6aaf4c030e3ccd90
querolita <anais.querol@gmail.com>
2026-01-15 15:39:47 +0100

    jsoo: remove logs for native build

--- src/lib/crypto/kimchi_bindings/js/native/native-overrides/circuit.js
+++ src/lib/crypto/kimchi_bindings/js/native/native-overrides/circuit.js
@@ -6,7 +6,5 @@
 function prover_to_json(prover_index) {
   var bytes = prover_index.serialize()
   var index = plonk_wasm.prover_index_fp_from_bytes(bytes);
-  // TODO: ^ remove the round trip when napi has direct access to the object
-  
   return plonk_wasm.prover_to_json(index);
 }
