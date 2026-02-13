05e3bf6c934677956e315197b187fb9c722bbabb
Christian Despres <christian@o1labs.org>
2026-02-13 02:17:31 -0500

    Add random transactions to stepper

--- src/app/test_block_stepper/test_block_stepper.ml
+++ src/app/test_block_stepper/test_block_stepper.ml
@@ -1,5 +1,7 @@
 open Core
 open Async
+open Signature_lib
+open Mina_base
 
 let blockchain_of_breadcrumb breadcrumb =
   let block = Frontier_base.Breadcrumb.block breadcrumb in
@@ -25,7 +27,148 @@ let load_and_initialize_config ~logger ~config_file ~genesis_dir =
     ~genesis_dir runtime_config
   >>| Or_error.ok_exn
 
-let run ~logger ~keypair ~config_file ~num_blocks ~genesis_dir ~state_dir =
+let generate_event =
+  Snark_params.Tick.Field.gen |> Quickcheck.Generator.map ~f:(fun x -> [| x |])
+
+let mk_payment ~signature_kind
+    ~(valid_until : Mina_numbers.Global_slot_since_genesis.t) ~signer_pk ~nonce
+    signer_keypair =
+  let fresh_keypair = Keypair.create () in
+  let receiver_pk = Public_key.compress fresh_keypair.Keypair.public_key in
+  let common =
+    { Signed_command_payload.Common.Poly.fee =
+        Currency.Fee.of_nanomina_int_exn 1_000_000
+    ; fee_payer_pk = signer_pk
+    ; nonce
+    ; valid_until
+    ; memo = Signed_command_memo.empty
+    }
+  in
+  let payload =
+    { Signed_command_payload.Poly.common
+    ; body =
+        Signed_command_payload.Body.Payment
+          { receiver_pk; amount = Currency.Amount.of_mina_int_exn 3 }
+    }
+  in
+  let signature =
+    Signed_command.sign_payload ~signature_kind
+      signer_keypair.Keypair.private_key payload
+  in
+  { Signed_command.Poly.signer = signer_keypair.public_key; signature; payload }
+
+let mk_zkapp_tx ~constraint_constants keypair nonce =
+  let num_acc_updates = 8 in
+  let event_elements = 12 in
+  let action_elements = 12 in
+  let signaturespec : Transaction_snark.For_tests.Signature_transfers_spec.t =
+    let fee_payer = None in
+    let generated_values =
+      let open Base_quickcheck.Generator.Let_syntax in
+      let%bind receivers =
+        Base_quickcheck.Generator.list_with_length ~length:num_acc_updates
+        @@ let%map kp = Keypair.gen in
+           (First kp, Currency.Amount.zero)
+      in
+      let%bind events =
+        Quickcheck.Generator.list_with_length event_elements generate_event
+      in
+      let%map actions =
+        Quickcheck.Generator.list_with_length action_elements generate_event
+      in
+      (receivers, events, actions)
+    in
+    let receivers, events, actions =
+      Quickcheck.random_value
+        ~seed:(`Deterministic ("test-apply-" ^ Unsigned.UInt32.to_string nonce))
+        generated_values
+    in
+    let zkapp_account_keypairs = [] in
+    let new_zkapp_account = false in
+    let snapp_update = Account_update.Update.dummy in
+    let call_data = Snark_params.Tick.Field.zero in
+    let preconditions = Some Account_update.Preconditions.accept in
+    { fee = Currency.Fee.of_mina_int_exn 1
+    ; sender = (keypair, nonce)
+    ; fee_payer
+    ; receivers
+    ; amount =
+        Currency.Amount.(
+          scale
+            (of_fee
+               constraint_constants
+                 .Genesis_constants.Constraint_constants.account_creation_fee )
+            num_acc_updates)
+        |> Option.value_exn ~here:[%here]
+    ; zkapp_account_keypairs
+    ; memo = Signed_command_memo.empty
+    ; new_zkapp_account
+    ; snapp_update
+    ; actions
+    ; events
+    ; transfer_parties_get_actions_events = true
+    ; call_data
+    ; preconditions
+    }
+  in
+  let receiver_auth = Some Control.Tag.Signature in
+  Transaction_snark.For_tests.signature_transfers ?receiver_auth
+    ~constraint_constants signaturespec
+
+let generate_txs ~signature_kind ~valid_until ~init_nonce ~n_zkapp_txs
+    ~n_payments ~n_blocks ~constraint_constants keypair :
+    User_command.Valid.t Sequence.t list =
+  let signer_pk = Public_key.compress keypair.Keypair.public_key in
+  let generate_for_block block_i =
+    Sequence.init (n_payments + n_zkapp_txs) ~f:(fun i ->
+        let nonce =
+          Mina_numbers.Account_nonce.(
+            add init_nonce @@ of_int
+            @@ ((block_i * (n_payments + n_zkapp_txs)) + i))
+        in
+        let command =
+          if i < n_payments then
+            User_command.Signed_command
+              (mk_payment ~signature_kind ~valid_until ~nonce ~signer_pk keypair)
+          else Zkapp_command (mk_zkapp_tx ~constraint_constants keypair nonce)
+        in
+        let (`If_this_is_used_it_should_have_a_comment_justifying_it
+              valid_command ) =
+          User_command.to_valid_unsafe command
+        in
+        valid_command )
+  in
+  List.init n_blocks ~f:generate_for_block
+
+let generate_all_transactions ~(precomputed_values : Precomputed_values.t)
+    ~n_blocks ~n_zkapp_txs ~n_payments ~keypair breadcrumb =
+  let genesis_ledger =
+    Staged_ledger.ledger (Frontier_base.Breadcrumb.staged_ledger breadcrumb)
+  in
+  let signer_account_id = Account_id.of_public_key keypair.Keypair.public_key in
+  let init_nonce =
+    Mina_ledger.Ledger.location_of_account genesis_ledger signer_account_id
+    |> Option.value_exn ~message:"Sender's account not found in ledger"
+    |> Mina_ledger.Ledger.get genesis_ledger
+    |> Option.value_exn
+         ~message:"Sender's account not found in ledger by location"
+    |> Account.nonce
+  in
+  let genesis_slot =
+    Frontier_base.Breadcrumb.protocol_state breadcrumb
+    |> Mina_state.Protocol_state.consensus_state
+    |> Consensus.Data.Consensus_state.global_slot_since_genesis
+  in
+  let valid_until =
+    Mina_numbers.Global_slot_since_genesis.add genesis_slot
+      (Mina_numbers.Global_slot_span.of_int @@ (n_blocks * 10))
+  in
+  generate_txs ~signature_kind:precomputed_values.signature_kind ~valid_until
+    ~init_nonce ~n_payments ~n_zkapp_txs ~n_blocks
+    ~constraint_constants:precomputed_values.constraint_constants keypair
+
+let run ~logger ~keypair ~config_file ~num_blocks ~genesis_dir ~state_dir
+    ~n_payments ~n_zkapp_txs =
   let%bind precomputed_values =
     load_and_initialize_config ~logger ~config_file ~genesis_dir
   in
@@ -58,14 +201,28 @@ let run ~logger ~keypair ~config_file ~num_blocks ~genesis_dir ~state_dir =
         "Genesis block proof verification: ERROR (verifier issue) - %s"
         (Error.to_string_hum e) ;
       failwith "Genesis block proof verification error" ) ;
+  let all_transactions =
+    if n_payments > 0 || n_zkapp_txs > 0 then (
+      [%log info]
+        "Generating transactions: %d payments + %d zkapp txs per block"
+        n_payments n_zkapp_txs ;
+      generate_all_transactions ~precomputed_values ~n_blocks:num_blocks
+        ~n_zkapp_txs ~n_payments ~keypair genesis_breadcrumb )
+    else (
+      [%log info] "No transactions requested, blocks will be empty" ;
+      List.init num_blocks ~f:(fun _ -> Sequence.empty) )
+  in
   [%log info] "Generating and verifying %d blocks" num_blocks ;
   let%map _final_stepper =
     Deferred.List.foldi (List.init num_blocks ~f:Fn.id) ~init:stepper
       ~f:(fun i stepper _block_index ->
         let block_num = i + 1 in
-        [%log info] "Stepping block %d/%d" block_num num_blocks ;
+        let transactions = List.nth_exn all_transactions i in
+        [%log info] "Stepping block %d/%d with %d transactions" block_num
+          num_blocks
+          (Sequence.length transactions) ;
         let%bind breadcrumb, stepper =
-          Block_stepper.step stepper ~transactions:Sequence.empty
+          Block_stepper.step stepper ~transactions
         in
         [%log info] "Block %d produced, verifying proof" block_num ;
         let blockchain = blockchain_of_breadcrumb breadcrumb in
@@ -116,6 +273,14 @@ let command =
           "DIR Directory containing genesis ledger tarballs (default: \
            <state-dir>/genesis)"
         (optional string)
+    and n_payments =
+      flag "--num-payments"
+        ~doc:"NUM Number of payment transactions per block (default: 0)"
+        (optional_with_default 0 int)
+    and n_zkapp_txs =
+      flag "--num-zkapp-txs"
+        ~doc:"NUM Number of zkApp transactions per block (default: 0)"
+        (optional_with_default 0 int)
     in
     Cli_lib.Exceptions.handle_nicely
     @@ fun () ->
@@ -164,7 +329,8 @@ let command =
       Secrets.Keypair.Terminal_stdin.read_exn ~which:"Mina keypair" privkey_path
     in
     Parallel.init_master () ;
-    run ~logger ~keypair ~config_file ~num_blocks ~genesis_dir ~state_dir)
+    run ~logger ~keypair ~config_file ~num_blocks ~genesis_dir ~state_dir
+      ~n_payments ~n_zkapp_txs)
 
 let () =
   Command.group ~summary:"Block stepper test"
