aaf378533367b3431a9a8b766a4a1ae2ff7d309c
Christian Despres <christian.j.j.despres@gmail.com>
2026-02-18 02:58:36 -0500

    Gate total currency visualization behind env var

--- src/lib/merkle_mask/maskable_merkle_tree.ml
+++ src/lib/merkle_mask/maskable_merkle_tree.ml
@@ -53,20 +53,26 @@ module Make (Inputs : Inputs_intf) = struct
     let display_attached_mask mask =
       let root_hash = Mask.Attached.merkle_root mask in
       let num_accounts = Mask.Attached.num_accounts mask in
-      (* This total_currency computation is temporarily disabled - it causes the
+      (* The total_currency computation is disabled by default - it causes the
          frontier mask visualization that happens at shutdown to be
-         exceptionally slow. See #17501 and #18196, though if we decide we
-         really need this then we could also consider optionally caching the
-         total_currency in ledger masks, and then recomputing the currency
-         change each mask would cause in the ledger. *)
-      (* let total_currency =
-           Mask.Attached.foldi mask ~init:0 ~f:(fun _ total_currency account ->
-               (* only default token matters for total currency *)
-               if Token_id.equal (Account.token account) Token_id.default then
-                 total_currency + (Balance.to_int @@ Account.balance account)
-               else total_currency )
-         in *)
-      let total_currency = 0 in
+         exceptionally slow on mainnet. See #17501 and #18196. Set the
+         MINA_FRONTIER_CURRENCY_VISUALIZATION environment variable to enable it.
+         Note that this might be useful in debugging changes to the total
+         currency in the ledgers, but it isn't expected to be equal to any total
+         currency value in the consensus data beyond the genesis epochs; as of
+         writing there is a quirk/bug that causes a slight desync in the total
+         currency updating across epoch boundaries. *)
+      let total_currency =
+        match Sys.getenv "MINA_FRONTIER_CURRENCY_VISUALIZATION" with
+        | Some _ ->
+            Mask.Attached.foldi mask ~init:0 ~f:(fun _ total_currency account ->
+                (* only default token matters for total currency *)
+                if Token_id.equal (Account.token account) Token_id.default then
+                  total_currency + (Balance.to_int @@ Account.balance account)
+                else total_currency )
+        | None ->
+            0
+      in
       let uuid = format_uuid mask in
       { hash =
           Visualization.display_prefix_of_string
