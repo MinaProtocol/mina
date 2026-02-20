3519a6ff80ea2c5cdc718aa8042493733ac9d3d1
georgeee <george.agapov@gmail.com>
2026-02-20 19:19:40 +0100

    Print default account input

--- src/app/dump_account_hashes/dump_account_hashes.ml
+++ src/app/dump_account_hashes/dump_account_hashes.ml
@@ -16,11 +16,40 @@ let () =
     Lazy.force Mina_base.Zkapp_account.zkapp_uri_non_preimage_hash
   in
 
+  let default_account_input = Mina_base.Zkapp_account.(to_input default) in
+  let default_account_input_packed =
+    Random_oracle.pack_input default_account_input
+  in
+
   (* Convert to string using the same format as used in ledger JSON implementation *)
   let digest_string = Snark_params.Tick.Field.to_string default_zkapp_digest in
   let vk_hash_string = Snark_params.Tick.Field.to_string dummy_vk_hash_value in
   let zkapp_uri_string =
     Snark_params.Tick.Field.to_string zkapp_uri_non_preimage
+  in
+
+  (* Extract and format the default account input components *)
+  let field_elements_json =
+    `List
+      ( Array.to_list default_account_input.field_elements
+      |> List.map ~f:(fun field ->
+             `String (Snark_params.Tick.Field.to_string field) ) )
+  in
+
+  let packeds_json =
+    `List
+      ( Array.to_list default_account_input.packeds
+      |> List.map ~f:(fun (field, bits) ->
+             `List
+               [ `String (Snark_params.Tick.Field.to_string field); `Int bits ] )
+      )
+  in
+
+  let packed_input_json =
+    `List
+      ( Array.to_list default_account_input_packed
+      |> List.map ~f:(fun field ->
+             `String (Snark_params.Tick.Field.to_string field) ) )
   in
 
   (* Create JSON object with the hashes *)
@@ -29,6 +58,12 @@ let () =
       [ ("default_zkapp_account_digest", `String digest_string)
       ; ("dummy_vk_hash", `String vk_hash_string)
       ; ("zkapp_uri_non_preimage_hash", `String zkapp_uri_string)
+      ; ( "default_account_input"
+        , `Assoc
+            [ ("fieldElements", field_elements_json)
+            ; ("packeds", packeds_json)
+            ] )
+      ; ("default_account_input_packed", packed_input_json)
       ]
   in
 
