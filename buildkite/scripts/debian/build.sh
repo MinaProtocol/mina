c08eafdf8182454e088120d0c9c5a8d0a291445d
dkijania <dariusz@o1labs.org>
2026-02-21 20:06:05 +0100

    write hf debian to cache for each debian

--- buildkite/scripts/debian/build.sh
+++ buildkite/scripts/debian/build.sh
@@ -32,11 +32,10 @@ IFS=',' read -ra CODENAMES_ARRAY <<< "$CODENAMES"
 
 for codename in "${CODENAMES_ARRAY[@]}"; do
 	ARCHITECTURE="$ARCH" MINA_DEB_CODENAME="$codename" BRANCH_NAME="$BUILDKITE_BRANCH" ./scripts/debian/build.sh "${PARAMS[@]}"
+	./buildkite/scripts/cache/manager.sh write-to-dir '_build/mina-*.deb' debians/$codename/
 done
 
 if [[ -z "${LOCAL_BK_RUN+x}" ]]; then
 	echo "--- Git diff after build is complete:"
 	git diff --exit-code -- .
 fi
-
-./buildkite/scripts/cache/manager.sh write-to-dir '_build/mina-*.deb' debians/$MINA_DEB_CODENAME/
