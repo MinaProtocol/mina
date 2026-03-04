2600fb6fbdc3e7697468caa138fee0dd51607cb2
dkijania <dariusz@o1labs.org>
2026-03-04 22:26:37 +0100

    added variant in place of simple boolean for handling debians installation

---
+++ buildkite/scripts/debian/read_all_from_cache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$MINA_DEB_CODENAME" ]; then
+  echo "MINA_DEB_CODENAME is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$ROOT" ]; then
+  echo "ROOT is not set. Exiting."
+  exit 1
+fi
+
+if [ -z "$LOCAL_DEB_FOLDER" ]; then
+  echo "LOCAL_DEB_FOLDER is not set. Exiting."
+  exit 1
+fi
+
+mkdir -p $LOCAL_DEB_FOLDER
+source ./buildkite/scripts/export-git-env-vars.sh
+./buildkite/scripts/cache/manager.sh read --root legacy/debians "$MINA_DEB_CODENAME/*" "${LOCAL_DEB_FOLDER}"
+./buildkite/scripts/cache/manager.sh read --root "${ROOT}" "debians/$MINA_DEB_CODENAME/*" "${LOCAL_DEB_FOLDER}"
