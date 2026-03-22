7ee41e54f8cc1b8eb66cdfca4e3a9f4009429855
martyall <martyall@protonmail.com>
2026-03-21 20:53:13 -0700

    dump_spec_pack compiles and runs: 40 entries definitive layout

--- src/lib/crypto/pickles/dump_spec_pack/dump_spec_pack.ml
+++ src/lib/crypto/pickles/dump_spec_pack/dump_spec_pack.ml
@@ -4,15 +4,16 @@ open Composition_types
 open Kimchi_pasta_snarky_backend.Step_impl
 
 let () =
-  let feature_flags = Plonk_types.Features.none_bool in
-  let feature_flags_full =
-    Plonk_types.Features.map feature_flags ~f:(fun b ->
+  let feature_flags_bool = Plonk_types.Features.none_bool in
+  let feature_flags_opt =
+    Plonk_types.Features.map feature_flags_bool ~f:(fun b ->
         if b then Opt.Flag.Yes else Opt.Flag.No )
   in
   let lookup_parameters : _ Wrap.Lookup_parameters.t =
     { use = No
     ; zero =
-        { var = { challenge = Field.zero; scalar = Shifted_value.Type1.Shifted_value Field.zero }
+        { var = { challenge = Field.zero
+                ; scalar = Shifted_value.Type1.Shifted_value Field.zero }
         ; value =
             { challenge = Limb_vector.Challenge.Constant.zero
             ; scalar = Shifted_value.Type1.Shifted_value Field.Constant.zero
@@ -24,31 +25,36 @@ let () =
     Wrap.Statement.In_circuit.spec
       (module Kimchi_pasta_snarky_backend.Step_impl)
       lookup_parameters
-      feature_flags_full
+      feature_flags_opt
   in
-  (* Create minimal dummy statement data *)
-  let f = Field.zero in
-  let c = Limb_vector.Challenge.Constant.zero in
-  let sc = Kimchi_backend_common.Scalar_challenge.create c in
-  let sv x = Shifted_value.Type1.Shifted_value x in
+  (* Use field_var (circuit variable) types, not constants *)
+  let fv = Field.zero in  (* field_var *)
+  let sc = Kimchi_backend_common.Scalar_challenge.create fv in
+  let sv = Shifted_value.Type1.Shifted_value fv in
   let bp = Bulletproof_challenge.{ prechallenge = sc } in
-  let bd = Branch_data.{ proofs_verified = Pickles_base.Proofs_verified.N0
-                       ; domain_log2 = Branch_data.Domain_log2.of_int_exn 15 } in
+  let bd = Branch_data.Checked.Step.{
+    proofs_verified_mask = Vector.[ Boolean.false_; Boolean.false_ ] ;
+    domain_log2 = fv
+  } in
+  let feature_flags_var =
+    Plonk_types.Features.map feature_flags_bool ~f:(fun _ -> Boolean.false_)
+  in
   let bp_vec = Vector.init Backend.Tick.Rounds.n ~f:(fun _ -> bp) in
   let statement_data =
     Wrap.Statement.In_circuit.to_data
       { proof_state =
           { deferred_values =
               { plonk =
-                  { alpha = sc; beta = c; gamma = c; zeta = sc
-                  ; zeta_to_srs_length = sv f; zeta_to_domain_size = sv f
-                  ; perm = sv f; feature_flags; joint_combiner = None }
-              ; xi = sc; combined_inner_product = sv f; b = sv f
+                  { alpha = sc; beta = fv; gamma = fv; zeta = sc
+                  ; zeta_to_srs_length = sv; zeta_to_domain_size = sv
+                  ; perm = sv; feature_flags = feature_flags_var
+                  ; joint_combiner = Opt.nothing }
+              ; xi = sc; combined_inner_product = sv; b = sv
               ; bulletproof_challenges = bp_vec; branch_data = bd }
-          ; sponge_digest_before_evaluations = Field.Constant.zero
-          ; messages_for_next_wrap_proof = Field.Constant.zero }
-      ; messages_for_next_step_proof = Field.Constant.zero }
-      ~option_map:Option.map
+          ; sponge_digest_before_evaluations = fv
+          ; messages_for_next_wrap_proof = fv }
+      ; messages_for_next_step_proof = fv }
+      ~option_map:Opt.map
   in
   let packed =
     Spec.pack
