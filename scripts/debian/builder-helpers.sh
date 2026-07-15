ae991bad2527102642cb5ae4046bc3f5431df65b
dkijania <dariusz@o1labs.org>
2026-07-15 18:13:21 +0200

    rc2 related configuration (seed list + config)

--- scripts/debian/builder-helpers.sh
+++ scripts/debian/builder-helpers.sh
@@ -549,7 +549,7 @@ build_daemon_deb() {
       ;;
     devnet)
       package_name="${MINA_DEB_NAME}"
-      seed_list_url="seed-lists/${network}_seeds.txt"
+      seed_list_url="o1labs-gitops-infrastructure/pre-mesa/pre-mesa-seeds.txt"
       ;;
     *)
       echo "Unknown network name provided: ${network}"; exit 1
@@ -637,7 +637,7 @@ build_daemon_generic_deb() {
 
   copy_common_daemon_apps "${network}"
 
-  copy_common_daemon_utils "seed-lists/${network}_seeds.txt"
+  copy_common_daemon_utils "o1labs-gitops-infrastructure/pre-mesa/pre-mesa-seeds.txt"
 
   build_deb "${MINA_GENERIC_DEB_NAME}"
 
